e15dac7bcc38dcd35b2ac5ece62c448c9f74c075
matthieu_ <matthieu_@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2007-09-21 14:51:10 +0000

    fixing a small bug

--- modules/ExceptionHandler.php
+++ modules/ExceptionHandler.php
@@ -7,12 +7,19 @@ function Piwik_ExceptionHandler(Exception $exception)
 {
 	try	{
 		Zend_Registry::get('logger_exception')->log($exception);
-	} catch(Exception $e) {
+	} catch(Exception $exception) {
 		// case when the exception is raised before the logger being ready
 		// we handle the exception a la mano, but using the Logger formatting properties
 		require_once "Log/Exception.php";
+		$event = array();
+		$event['errno'] 	= $exception->getCode();
+		$event['message'] 	= $exception->getMessage();
+		$event['errfile'] 	= $exception->getFile();
+		$event['errline'] 	= $exception->getLine();
+		$event['backtrace'] = $exception->getTraceAsString();
+		
 		$formatter = new Piwik_Log_Formatter_Exception_ScreenFormatter;
-		$message = $formatter->format($e);
+		$message = $formatter->format($event);
 		Piwik::exitWithErrorMessage( $message );
 	}
 }
