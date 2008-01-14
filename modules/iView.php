943594d0e1e2d5f4f92544da59332ae773813938
mattpiwik <matthieu.aubry@gmail.com>
2008-01-14 04:56:04 +0000

    git-svn-id: http://dev.piwik.org/svn/trunk@166 59fd770c-687e-43c8-a1e3-f5a4ff64c105

---
+++ modules/iView.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ * 
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
+ * @version $Id: APIable.php 162 2008-01-14 04:27:21Z matt $
+ */
+
+interface Piwik_iView
+{
+	function render();
+}
