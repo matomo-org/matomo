febae5d908dd2a7fab06d0b8f03e736ac10d20e6
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2008-01-14 00:29:02 +0000

    - adding doc

--- misc/api_internal_call.php
+++ misc/api_internal_call.php
@@ -1,13 +1,12 @@
 {{{
 #!php
 <?php
-
 define('PIWIK_INCLUDE_PATH', '..');
 define('ENABLE_DISPATCH', false);	
 require_once PIWIK_INCLUDE_PATH . "/index.php";
 require_once PIWIK_INCLUDE_PATH . "/modules/API/Request.php";
 
-// we call the API from a php code
+// We call the API from a php code
 // it will check that you have the necessary rights
 // - either you are loggued in piwik and have a cookie in your browser
 // - or you will have to add the token_auth=XXX to the request string to authenticate
@@ -15,12 +14,13 @@ require_once PIWIK_INCLUDE_PATH . "/modules/API/Request.php";
 $request = new Piwik_API_Request('
 			method=UserSettings.getResolution
 			&idSite=1
- 			&date=yesterday
+			&date=yesterday
 			&period=week
-			&format=xml
+			&format=JSON
 			&filter_limit=5
-			&filter_offset=0
 ');
 $result = $request->process();
 echo $result;
+
+?>
 }}}
