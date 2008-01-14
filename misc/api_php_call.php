6fe84a16a82d2ecacf939537180d4711a0d13185
mattpiwik <matthieu.aubry@gmail.com>
2008-01-14 00:29:02 +0000

    - adding doc
    git-svn-id: http://dev.piwik.org/svn/trunk@154 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- misc/api_php_call.php
+++ misc/api_php_call.php
@@ -1,16 +1,19 @@
+{{{
+#!php
 <?php
-// the token will be given in the User page in piwik
-// but we can build it because we know the piwik internals
-$token = md5('root'.'nintendo');
+// this token is used to authenticate your API request. 
+// You can get the token on the API page inside your Piwik interface
+$token_auth = '0b809661490d605bfd77f57ed11f0b14';
 
 // we call the REST API and request the 100 first keywords for the last month for the idsite=1
-$url = "http://localhost/dev/piwiktrunk/";
-$url .= "?module=API&method=Referers.getKeywords&idSite=1&period=month&date=yesterday";
+$url = "http://127.0.0.1/svn-dev/trunk/";
+$url .= "?module=API&method=Referers.getKeywords";
+$url .= "&idSite=1&period=month&date=yesterday";
 $url .= "&format=PHP&filter_limit=100";
-$url .= "&token=$token";
+$url .= "&token_auth=$token_auth";
 
 $fetched = file_get_contents($url);
-$content = @unserialize($fetched);
+$content = unserialize($fetched);
 
 // case error
 if(!$content)
@@ -18,12 +21,13 @@ if(!$content)
 	print("Error, content fetched = ".$fetched);
 }
 
-print("<h2>Keywords for the last month</h2>");
+print("<h1>Keywords for the last month</h1>");
 foreach($content as $row)
 {
-	$keyword = urldecode($row['columns']['label']);
-	$hits = $row['columns']['nb_visits'];
+	$keyword = urldecode($row['label']);
+	$hits = $row['nb_visits'];
 	
 	print("<b>$keyword</b> ($hits hits)<br>");
 }
-
+?>
+}}}
