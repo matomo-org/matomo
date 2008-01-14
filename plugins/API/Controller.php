0589a35ee861de880ed3b71bf302d2200899bf9c
mattpiwik <matthieu.aubry@gmail.com>
2008-01-14 01:12:26 +0000

    working on http://dev.piwik.org/trac/wiki/API
    git-svn-id: http://dev.piwik.org/svn/trunk@156 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- plugins/API/Controller.php
+++ plugins/API/Controller.php
@@ -10,22 +10,8 @@ class Piwik_API_Controller extends Piwik_Controller
 		echo $request->process();
 	}
 	
-	function listAllAPI()
+	function init()
 	{
-//?module=API&method=Referers.getKeywords&idSite=1&period=month&date=today&format=xml
-//
-//or yesterday visits information in JSON
-//?module=API&method=VisitsSummary.get&idSite=1&period=month&date=yesterday&format=json
-
-//		http://127.0.0.1/svn-dev/trunk/?module=API&method=UserSettings.getResolution&idSite=1&period=day&date=today&format=xml&token_auth=0b809661490d605bfd77f57ed11f0b14
-
-		$token_auth = Zend_Registry::get('auth')->getTokenAuth();
-		echo "<style>body{ font-family:georgia,arial; font-size:0.95em;} </style>";
-		echo "<h1>API quick documentation</h1>";
-		echo "<p>If you don't have data for today you can first <a href='misc/generateVisits.php' target=_blank>generate some data</a> using the Visits Generator script.</p>";
-		echo "<p>You can try the different formats available for every method. It is very easy to extract any data you want from piwik!</p>";
-		echo "<p>If you want to <b>request the data without being logged in to Piwik</b> you need to add the parameter <code><u>&token_auth=$token_auth</u></code> to the API calls URLs that require authentication.</p>";
-		$errors = '';
 		$plugins = Piwik_PluginsManager::getInstance()->getLoadedPluginsName();
 		
 		$loaded = 0;
@@ -38,10 +24,32 @@ class Piwik_API_Controller extends Piwik_Controller
 				$loaded++;
 			}
 			catch(Exception $e){
-				$errors .= "<br>\n" . $e->getMessage();
+//				$errors .= "<br>\n" . $e->getMessage();
 			}
 		}
+		return $loaded;
+	}
+	
+	function listAllMethods()
+	{
+		$this->init();
+		echo Piwik_API_Proxy::getInstance()->getAllInterfaceString( $outputExampleUrls = false );
+	}
+	
+	
+	function listAllAPI()
+	{
+		$token_auth = Zend_Registry::get('auth')->getTokenAuth();
+		echo "<style>body{ font-family:georgia,arial; font-size:0.95em;} </style>";
+		echo "<h1>API quick documentation</h1>";
+		echo "<p>If you don't have data for today you can first <a href='misc/generateVisits.php' target=_blank>generate some data</a> using the Visits Generator script.</p>";
+		echo "<p>You can try the different formats available for every method. It is very easy to extract any data you want from piwik!</p>";
+		echo "<p>If you want to <b>request the data without being logged in to Piwik</b> you need to add the parameter <code><u>&token_auth=$token_auth</u></code> to the API calls URLs that require authentication.</p>";
+		$errors = '';
+		
+		$loaded = $this->init();
 		echo "<p><i> Loaded successfully $loaded APIs</i></p>\n";
+		
 		echo Piwik_API_Proxy::getInstance()->getAllInterfaceString();
 		
 		echo "<p>Notice = " . $errors . "</p>\n";
