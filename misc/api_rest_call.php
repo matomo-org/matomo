07cceb2b7ac12d83785188cc7bbc062fda3e3148
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2008-01-18 16:54:41 +0000

    Doc

--- misc/api_rest_call.php
+++ misc/api_rest_call.php
@@ -1,13 +1,13 @@
 <?php
 // this token is used to authenticate your API request. 
 // You can get the token on the API page inside your Piwik interface
-$token_auth = '0b809661490d605bfd77f57ed11f0b14';
+$token_auth = 'anonymous';
 
 // we call the REST API and request the 100 first keywords for the last month for the idsite=1
-$url = "http://127.0.0.1/svn-dev/trunk/";
+$url = "http://piwik.org/demo/";
 $url .= "?module=API&method=Referers.getKeywords";
 $url .= "&idSite=1&period=month&date=yesterday";
-$url .= "&format=PHP&filter_limit=100";
+$url .= "&format=PHP&filter_limit=20";
 $url .= "&token_auth=$token_auth";
 
 $fetched = file_get_contents($url);
