5575f4d6e91cd7cc2267b410bc8a818533b7ccf2
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2008-01-20 01:15:04 +0000

    Fixed

--- plugins/Widgetize/Controller.php
+++ plugins/Widgetize/Controller.php
@@ -37,7 +37,7 @@ class Piwik_Widgetize_Controller extends Piwik_Controller
 	{
 		$view = new Piwik_View('Widgetize/templates/test_jsinclude.tpl');
 		$view->url1 = '?module=Widgetize&action=js&moduleToWidgetize=Home&actionToWidgetize=getBrowser&idSite=1&period=day&date=yesterday';
-		$view->url2 = '?module=Widgetize&action=js&moduleToWidgetize=Home&actionToWidgetize=getBrowser&idSite=1&period=day&date=yesterday&viewDataTable=cloud&showDataTableFooter=0';
+		$view->url2 = '?module=Widgetize&action=js&moduleToWidgetize=Home&actionToWidgetize=getCountry&idSite=1&period=day&date=yesterday&viewDataTable=cloud&showDataTableFooter=0';
 		echo $view->render();
 	}
 	
