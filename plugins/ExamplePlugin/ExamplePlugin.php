51dd493b8255240b1f98f317816f291a4e69d374
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2008-02-04 19:00:37 +0000

    - adding simple feedburner integration plugin

--- plugins/ExamplePlugin/ExamplePlugin.php
+++ plugins/ExamplePlugin/ExamplePlugin.php
@@ -23,6 +23,16 @@ class Piwik_ExamplePlugin extends Piwik_Plugin
 			'translationAvailable' => false,
 		);
 	}
+	
+	function install()
+	{
+		Piwik_Query('ALTER TABLE '.Piwik::prefixTable('site'). " ADD `feedburnerName` VARCHAR( 100 ) NOT NULL ;");
+	}
+	function uninstall()
+	{
+		Piwik_Query('ALTER TABLE '.Piwik::prefixTable('site'). " DROP `feedburnerName`");
+	}
+	
 }
 
 class Piwik_ExamplePlugin_Controller extends Piwik_Controller
@@ -33,6 +43,7 @@ class Piwik_ExamplePlugin_Controller extends Piwik_Controller
 		// render view
 		// do stuff...
 	}
+
 	function exampleWidget()
 	{
 		echo "Hello world! <br> You can output whatever you want in widgets, and put them on dashboard or everywhere on the web (in your blog, website, etc.).
@@ -45,16 +56,34 @@ class Piwik_ExamplePlugin_Controller extends Piwik_Controller
 	{
 		echo '
 		<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" type="application/x-shockwave-flash" width="400px" height="338px" id="InsertWidget_3fe0d93d-bae5-42f9-bec4-4235cb6285a8" align="middle">
-	<param name="movie" value="http://widgetserver.com/syndication/flash/wrapper/InsertWidget.swf?appId=3fe0d93d-bae5-42f9-bec4-4235cb6285a8"/>
-	<param name="quality" value="high" />
-	<param name="wmode" value="transparent" />
-	<param name="menu" value="false" />
- <embed src="http://widgetserver.com/syndication/flash/wrapper/InsertWidget.swf?appId=3fe0d93d-bae5-42f9-bec4-4235cb6285a8"  name="InsertWidget_3fe0d93d-bae5-42f9-bec4-4235cb6285a8"  width="400px" height="338px" quality="high" menu="false" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" wmode="transparent" align="middle" /> </object>
-<img style="visibility:hidden;width:0px;height:0px;" border="0" width="0" height="0" src="http://runtime.widgetbox.com/syndication/track/3fe0d93d-bae5-42f9-bec4-4235cb6285a8.gif" />
-		
+			<param name="movie" value="http://widgetserver.com/syndication/flash/wrapper/InsertWidget.swf?appId=3fe0d93d-bae5-42f9-bec4-4235cb6285a8"/>
+			<param name="quality" value="high" />
+			<param name="wmode" value="transparent" />
+			<param name="menu" value="false" />
+		<embed src="http://widgetserver.com/syndication/flash/wrapper/InsertWidget.swf?appId=3fe0d93d-bae5-42f9-bec4-4235cb6285a8"  name="InsertWidget_3fe0d93d-bae5-42f9-bec4-4235cb6285a8"  width="400px" height="338px" quality="high" menu="false" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" wmode="transparent" align="middle" /> </object>
+		<img style="visibility:hidden;width:0px;height:0px;" border="0" width="0" height="0" src="http://runtime.widgetbox.com/syndication/track/3fe0d93d-bae5-42f9-bec4-4235cb6285a8.gif" />
 		';
+	}
+	
+	function feedburner()
+	{
+		$view = new Piwik_View('ExamplePlugin/feedburner.tpl');
+		$feedburnerFeedName = Piwik_FetchOne('SELECT feedburnerName FROM '.Piwik::prefixTable('site').
+								' WHERE idsite = ?', Piwik_Common::getRequestVar('idSite',1,'int') );
+		if(empty($feedburnerFeedName))
+		{
+			$feedburnerFeedName = 'BlogGiik';
+		}
+		$view->feedburnerFeedName = $feedburnerFeedName;
+		echo $view->render();
+	}
+	function saveFeedburnerName()
+	{
+		Piwik_Query('UPDATE '.Piwik::prefixTable('site').' SET feedburnerName = ?',
+							Piwik_Common::getRequestVar('name','','string'));
 	}
 }
 
 Piwik_AddWidget('ExamplePlugin', 'exampleWidget', 'Example widget');
+Piwik_AddWidget('ExamplePlugin', 'feedburner', 'Feedburner statistics');
 Piwik_AddWidget('ExamplePlugin', 'blogMatthieu', 'Blog matthieu RSS');
