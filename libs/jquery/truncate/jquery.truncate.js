186abaa6b80f5253f43fba8e38c3b0eeb6b0623d
julien <julien@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2008-02-28 20:29:53 +0000

    Minified javascript libraries (see trac #95)

--- libs/jquery/truncate/jquery.truncate.js
+++ libs/jquery/truncate/jquery.truncate.js
@@ -1,23 +1,3 @@
-jQuery.fn.truncate = function(max) {
-    return this.each(function() {
-    	var trail = '...';
-       	if (jQuery(this).children().length == 0) 
-        {
-            v = jQuery.trim(jQuery(this).text());
-            while (max < v.length) {
-                c = v.charAt(max);
-                newStringTruncated = v.substring(0,max) + trail;
-				
-				charToRemove = '"';
-				regExp = new RegExp("["+charToRemove+"]","g");
-                vCleaned = v.replace(regExp,"'");
 
-                html = '<span class="truncated" title="'+ vCleaned +'">'+newStringTruncated+'</span>';
-                jQuery(this).html( html );
-
-                break;
-                max--;
-            }
-        }
-    });
-};
+jQuery.fn.truncate=function(max){return this.each(function(){var trail='...';if(jQuery(this).children().length==0)
+{v=jQuery.trim(jQuery(this).text());while(max<v.length){c=v.charAt(max);newStringTruncated=v.substring(0,max)+trail;charToRemove='"';regExp=new RegExp("["+charToRemove+"]","g");vCleaned=v.replace(regExp,"'");html='<span class="truncated" title="'+vCleaned+'">'+newStringTruncated+'</span>';jQuery(this).html(html);break;max--;}}});};
