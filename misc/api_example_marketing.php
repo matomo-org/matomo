76e5e6a8b88547c5bcab8c43c8362aee5982cab8
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2008-03-10 18:07:40 +0000

    - updating tutorial

--- misc/api_example_marketing.php
+++ misc/api_example_marketing.php
@@ -1,5 +1,5 @@
 <h1>Ratio of visitors that try the Piwik online demo</h1>
-<p>This report is automatically computed in PHP using the <a href='http://dev.piwik.org/trac/wiki/API/Reference'>Piwik APIs</a>. In 10 lines of PHP you can get the data and generate this kind of report.
+<p>This report is automatically computed in PHP using the <a href='http://dev.piwik.org/trac/wiki/API/Reference'>Piwik APIs</a>. In <a href='http://dev.piwik.org/trac/browser/trunk/misc/api_example_marketing.php'>a few lines of simple PHP</a> (you could use any other language) you can get the data and generate this kind of report.
 </p><p>This report is generated in order to see how <a href='http://piwik.org'>Piwik.org</a> visitors are interested in Piwik, which we determine by the ratio of visitors that look the <a href='http://piwik.org/demo'>online demo</a>.</p>
 <?php
 $visitsDemo = file_get_contents('http://piwik.org/demo/?module=API&method=Actions.getActions&idSite=1&period=day&date=last7&format=php&filter_column=label&filter_pattern=demo');
