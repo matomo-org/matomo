91a158b8274043a191c69fe82aa1464442ca1dda
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2008-03-10 01:04:42 +0000

    fixing bug in graph embed code on IE when debugging is ON

--- modules/ViewDataTable/Graph.php
+++ modules/ViewDataTable/Graph.php
@@ -111,17 +111,14 @@ abstract class Piwik_ViewDataTable_Graph extends Piwik_ViewDataTable
 				<noscript>
 				';
 		}
-		$this->codeEmbed = "<embed src='".$pathToLibraryOpenChart."open-flash-chart.swf?data=" . $url ."' quality='high' bgcolor='#FFFFFF' width='". $width ."' height='". $height ."' name='open-flash-chart' align='middle' allowScriptAccess='sameDomain' type='application/x-shockwave-flash' pluginspage='http://www.macromedia.com/go/getflashplayer' id='". $obj_id ."'/>";
+		$urlGraph = $pathToLibraryOpenChart."open-flash-chart.swf?data=" . $url;
+		$this->codeEmbed = "<embed src='$urlGraph' quality='high' bgcolor='#FFFFFF' width='". $width ."' height='". $height ."' name='open-flash-chart' type='application/x-shockwave-flash' id='". $obj_id ."'/>";
 		
 		$return .= '
-			<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" 
-			width="' . $width . '" height="' . $height . '" id="'. $obj_id .'" align="middle">
-			<param name="allowScriptAccess" value="sameDomain" />
-			<param name="movie" value="'.$pathToLibraryOpenChart.'open-flash-chart.swf?width='. $width .'&height='. $height . '&data='. $url .'" />
-			<param name="quality" value="high" />
-			<param name="bgcolor" value="#FFFFFF" />
+			<div><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="' . $width . '" height="' . $height . '" id="'. $obj_id .'" >
+			<param name="movie" value="'.$urlGraph.'" />
 			'.$this->codeEmbed.'
-			</object>';
+			</object></div>';
 	
 		if ( $use_swfobject ) {
 			$return .= '</noscript>';
