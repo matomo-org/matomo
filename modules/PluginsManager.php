559dfa02b1d1dda8c408ca7cbcb33354f9598c79
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2008-03-14 20:09:06 +0000

    - Piwik_Fetch renamed in Piwik_FetchAll to match Zend notation

--- modules/PluginsManager.php
+++ modules/PluginsManager.php
@@ -432,7 +432,7 @@ function Piwik_AddAction( $hookName, $function )
 
 /**
  * Executes a SQL query on the DB and returns the Zend_Db_Statement object
- * If you want to fetch data from the DB you should use the function Piwik_Fetch()
+ * If you want to fetch data from the DB you should use the function Piwik_FetchAll()
  * 
  * See also http://framework.zend.com/manual/en/zend.db.statement.html
  *
@@ -452,7 +452,7 @@ function Piwik_Query( $sqlQuery, $parameters = array())
  * @param array Parameters to bind in the query, array( param1 => value1, param2 => value2)
  * @return array (one row in the array per row fetched in the DB)
  */
-function Piwik_Fetch( $sqlQuery, $parameters = array())
+function Piwik_FetchAll( $sqlQuery, $parameters = array())
 {
 	return Zend_Registry::get('db')->fetchAll( $sqlQuery, $parameters );
 }
