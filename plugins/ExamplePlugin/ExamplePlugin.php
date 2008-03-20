31575ea26470d8466613d3634381199853a756c2
mattpiwik <matthieu.aubry@gmail.com>
2008-03-20 12:31:55 +0000

    - fix #153 "After reinstalling piwik Dashboard plugin is not enabled"
    git-svn-id: http://dev.piwik.org/svn/trunk@393 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- plugins/ExamplePlugin/ExamplePlugin.php
+++ plugins/ExamplePlugin/ExamplePlugin.php
@@ -26,7 +26,16 @@ class Piwik_ExamplePlugin extends Piwik_Plugin
 	
 	function install()
 	{
-		Piwik_Query('ALTER TABLE '.Piwik::prefixTable('site'). " ADD `feedburnerName` VARCHAR( 100 ) NOT NULL ;");
+		try{
+			Piwik_Query('ALTER TABLE '.Piwik::prefixTable('site'). " ADD `feedburnerName` VARCHAR( 100 ) NOT NULL ;");
+		} catch(Zend_Db_Statement_Exception $e){
+			// mysql code error 1060: column already exists
+			// if there is another error we throw the exception, otherwise it is OK as we are simply reinstalling the plugin
+			if(!ereg('1060',$e->getMessage()))
+			{
+				throw $e;
+			}
+		}
 	}
 	function uninstall()
 	{
