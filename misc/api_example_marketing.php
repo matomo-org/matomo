c8c31666cd745d43e53843d18ec97e3e0cf0eade
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2008-04-05 19:28:39 +0000

    - updating the marketing example API page

--- misc/api_example_marketing.php
+++ misc/api_example_marketing.php
@@ -1,4 +1,9 @@
-<h1>Ratio of visitors that try the Piwik online demo</h1>
+<h1>Ratio of visitors that download Piwik</h1>
+<style>
+thead{
+font-weight: bold;
+}
+</style>
 <p>This report is automatically computed in PHP using the <a href='http://dev.piwik.org/trac/wiki/API/Reference'>Piwik APIs</a>. In <a href='http://dev.piwik.org/trac/browser/trunk/misc/api_example_marketing.php'>a few lines of simple PHP</a> (you could use any other language) you can get the data and generate this kind of report.
 </p><p>This report is generated in order to see how <a href='http://piwik.org'>Piwik.org</a> visitors are interested in Piwik, which we determine by the ratio of visitors that download the software. We also report the number of visitors looking at the <a href='http://piwik.org/demo'>online demo</a>.</p>
 <?php
@@ -10,9 +15,9 @@ $visitsDemo = unserialize($visitsDemo);
 $visitsAll = unserialize($visitsAll);
 $downloads = unserialize($downloads);
 
-$ratioSum = $count = 0;
+$ratioSum = $ratioDemoSum = $count = 0;
 
-print('<table border=1 cellpadding=4><thead><td>Date</td><td>Nb visitors</td><td>Visitors on /demo</td><td>Download last.zip</td><td>Ratio visitors/download</td></thead>');
+print('<table border=1 cellpadding=4><thead><td>Date</td><td>Nb visitors</td><td>Visitors on /demo</td><td>Download last.zip</td><td>Ratio visitors/demo</td><td>Ratio visitors/download</td></thead>');
 foreach($visitsDemo as $date => $values)
 {
 	$nbVisitsDemo = $values[0]['nb_uniq_visitors'];
@@ -22,9 +27,14 @@ foreach($visitsDemo as $date => $values)
 	$ratio = round($nbDownloads * 100 / $nbVisits, 0);
 	$ratioSum += $ratio; $count++;
 	
-	print("<tr><td>$date</td><td>$nbVisits</td><td>$nbVisitsDemo</td><td>$nbDownloads<td>$ratio %</td></tr>");
+	$ratioDemo = round($nbVisitsDemo * 100 / $nbVisits, 0);
+	$ratioDemoSum += $ratioDemo;
+		
+	print("<tr><td>$date</td><td>$nbVisits</td><td>$nbVisitsDemo</td><td>$nbDownloads</td><td>$ratioDemo %</td><td>$ratio %</td></tr>");
 }
 print('</table>');
 
 $averageRatio = round($ratioSum / $count, 0);
+$averageRatioDemo = round($ratioDemoSum / $count, 0);
+print("<p><b>Average visitors visiting the demo = $averageRatioDemo %</b></p>");
 print("<p><b>Average visitors downloading piwik = $averageRatio %</b></p>");
