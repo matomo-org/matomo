bbf160900148fb607e6895d397817ac941f172b5
johmathe <johmathe@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2008-04-11 13:38:22 +0000

    Convert txt files to unix format

--- libs/Zend/Auth/Storage/NonPersistent.php
+++ libs/Zend/Auth/Storage/NonPersistent.php
@@ -17,7 +17,7 @@
  * @package    Zend_Auth
  * @copyright  Copyright (c) 2005-2007 Zend Technologies USA Inc. (http://www.zend.com)
  * @license    http://framework.zend.com/license/new-bsd     New BSD License
- * @version    $Id:$
+ * @version    $Id$
  */
 
 
