311c08111198a5a4e85be571fe2c8578dd5d74b5
mattpiwik <matthieu.aubry@gmail.com>
2008-08-14 18:30:34 +0000

    - OMG i totally messed up with SVN: NEVER rename a directory using your OS rename, but use SVN rename, I nearly lost a couple of hours of work
    git-svn-id: http://dev.piwik.org/svn/trunk@599 59fd770c-687e-43c8-a1e3-f5a4ff64c105

---
+++ plugins/Dashboard/Dashboard.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ * 
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
+ * @version $Id: ExamplePlugin.php 169 2008-01-14 05:41:15Z matt $
+ * 
+ * @package Piwik_ExamplePlugin
+ */
+
+class Piwik_Dashboard extends Piwik_Plugin
+{
+	public function getInformation()
+	{
+		return array(
+			'name' => 'Dashboard',
+			'description' => 'Your Web Analytics Dashboard. You can customize Your Dashboard: add new widgets, change the order of your widgets. Each user can access his own custom Dashboard.',
+			'author' => 'Piwik',
+			'homepage' => 'http://piwik.org/',
+			'version' => '0.1',
+		);
+	}
+
+	public function getListHooksRegistered()
+	{
+		return array( 
+			'template_js_import' => 'js',
+			'template_css_import' => 'css',
+		);
+	}
+
+	function js()
+	{
+		echo '
+<script type="text/javascript" src="plugins/Dashboard/templates/AddWidget.js"></script>
+<script type="text/javascript" src="plugins/Dashboard/templates/Dashboard.js"></script>
+		';
+	}
+
+	function css()
+	{
+		echo '<link rel="stylesheet" type="text/css" href="plugins/Dashboard/templates/dashboard.css" />';
+	}
+	
+	public function install()
+	{
+		// we catch the exception
+		try{
+			$sql = "CREATE TABLE ". Piwik::prefixTable('user_dashboard')." (
+					login VARCHAR( 20 ) NOT NULL ,
+					iddashboard INT NOT NULL ,
+					layout TEXT NOT NULL,
+					PRIMARY KEY ( login , iddashboard )
+					) " ;
+			Piwik_Query($sql);
+		} catch(Zend_Db_Statement_Exception $e){
+			// mysql code error 1050:table already exists
+			// see bug #153 http://dev.piwik.org/trac/ticket/153
+			if(ereg('1050',$e->getMessage()))
+			{
+				return;
+			}
+			else
+			{
+				throw $e;
+			}
+		}
+	}
+	
+	public function uninstall()
+	{
+		$sql = "DROP TABLE ". Piwik::prefixTable('user_dashboard') ;
+		Piwik_Query($sql);		
+	}
+	
+}
+
+Piwik_AddMenu('Dashboard', '', array('module' => 'Dashboard', 'action' => 'embeddedIndex'));
