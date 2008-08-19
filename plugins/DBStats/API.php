acab46cb1dfb345494976e0922683aafb34a73bd
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2008-08-19 01:22:09 +0000

    Slightly improving interface for the database usage report

--- plugins/DBStats/API.php
+++ plugins/DBStats/API.php
@@ -44,16 +44,21 @@ class Piwik_DBStats_API extends Piwik_Apiable
 		return $status;
 	}
 	
-	static private function get_size($size) {
-	$bytes = array('','K','M','G','T');
-	  foreach($bytes as $val) {
-	   if($size > 1024){
-	    $size = $size / 1024;
-	   }else{
-	    break;
-	   }
-	  }
-	  return round($size, 1)." ".$val;
+	static private function get_size($size)
+	{
+		$bytes = array('','K','M','G','T');
+		foreach($bytes as $val) 
+		{
+			if($size > 1024)
+			{
+				$size = $size / 1024;
+			}
+			else
+			{
+	    		break;
+			}
+		}
+		return round($size, 1)." ".$val;
 	}
 	
 	static public function getTableStatus($table, $field = '') 
@@ -79,16 +84,26 @@ class Piwik_DBStats_API extends Piwik_Apiable
 		$db = Zend_Registry::get('db');
 		// http://dev.mysql.com/doc/refman/5.1/en/show-table-status.html
 		$tablesPiwik =  Piwik::getTablesInstalled();
+		$total = array('Name' => 'Total', 'Data_length' => 0, 'Index_length' => 0, 'Rows' => 0);
 		foreach($tablesPiwik as $table) 
 		{
 			$t = self::getTableStatus($table);
+			$total['Data_length'] += $t['Data_length'];
+			$total['Index_length'] += $t['Index_length'];
+			$total['Rows'] += $t['Rows'];
+			
+			$t['Total_length'] = self::get_size($t['Index_length']+$t['Data_length']);
 			$t['Data_length'] = self::get_size($t['Data_length']);
 			$t['Index_length'] = self::get_size($t['Index_length']);
 			$t['Rows'] = self::get_size($t['Rows']);
 			$tables[] = $t;
 		}
+		$total['Total_length'] = self::get_size($total['Data_length']+$total['Index_length']);
+		$total['Data_length'] = self::get_size($total['Data_length']);
+		$total['Index_length'] = self::get_size($total['Index_length']);
+		$total['TotalRows'] = self::get_size($total['Rows']);
+		$tables['Total'] = $total;
 		
 		return $tables;
 	}
 }
-
