2a6e0084d64569dda9972e9f753f0c9fc81842f0
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2008-10-10 14:38:08 +0000

    - rel #300. cleaning comments on misc/generateVisits.php

--- misc/generateVisits.php
+++ misc/generateVisits.php
@@ -1,10 +1,19 @@
 <?php
+/*
+ * The script can be used to generate huge number of visits and actions
+ * for a given number of days.
+ */
+$minVisits = 5;
+$maxVisits = 15;
+$nbActions = 15;
+$daysToCompute = 1;
+$idSite = Piwik_Common::getRequestVar('idSite', 1, 'int');
+
+//-----------------------------------------------------------------------------
 error_reporting(E_ALL|E_NOTICE);
 define('PIWIK_INCLUDE_PATH', '..');
-
 ignore_user_abort(true);
 set_time_limit(0);
-
 set_include_path(PIWIK_INCLUDE_PATH 
 					. PATH_SEPARATOR . PIWIK_INCLUDE_PATH . '/libs/'
 					. PATH_SEPARATOR . PIWIK_INCLUDE_PATH . '/plugins/'
@@ -12,7 +21,6 @@ set_include_path(PIWIK_INCLUDE_PATH
 					. PATH_SEPARATOR . get_include_path() );
 					
 $GLOBALS['DEBUGPIWIK'] = false;
-
 ob_start();
 
 // first check that user has privileges to create some random data in the DB -> he must be super user
@@ -21,7 +29,6 @@ require_once "index.php";
 require_once "FrontController.php";
 
 Piwik_FrontController::getInstance()->init();
-
 Piwik::checkUserIsSuperUser();
 
 require_once "PluginsManager.php";
@@ -34,37 +41,25 @@ require_once "LogStats/Db.php";
 require_once "LogStats/Visit.php";
 require_once "LogStats/Generator.php";
 
-// unload all loaded plugins
 //Piwik_PluginsManager::getInstance()->unloadPlugins();
 
-// we unload the Provider plugin otherwise it tries to lookup the IP for ahostname, and there is no dns server here
+// we have to unload the Provider plugin otherwise it tries to lookup the IP for ahostname, and there is no dns server here
 Piwik_PluginsManager::getInstance()->unloadPlugin('Provider');
 
 // we set the DO NOT load plugins so that the LogStats generator doesn't load the plugins we've just disabled.
 // if for some reasons you want to load the plugins, comment this line, and disable the plugin Provider in the plugins interface
-// (see explanation above)
 Piwik_PluginsManager::getInstance()->doNotLoadPlugins();
 
 $generator = new Piwik_LogStats_Generator;
 $generator->setMaximumUrlDepth(3);
 //$generator->disableProfiler();
-$idSite = Piwik_Common::getRequestVar('idSite', 1, 'int');
 $generator->setIdSite( $idSite );
-$minVisits = 5;
-$maxVisits = 15;
-$nbActions = 15;
-$daysToCompute = 1;
 
 $nbActionsTotal = 0;
-
 //$generator->emptyAllLogTables();
 $generator->init();
 
 $t = new Piwik_Timer;
-
-/*
- * Generate visits / actions for the last 31 days
- */
 
 // do NOT edit this line
 $startTime = time() - ($daysToCompute-1)*86400;
@@ -72,14 +67,8 @@ while($startTime <= time())
 {
 	$visits = rand($minVisits,$maxVisits);
 	$actions=$nbActions;
-	
 	$generator->setTimestampToUse($startTime);
-	
-	// we add silent fail because of headers already sent error.
-	// although this should'nt happen because we use a OB_START at the top of this page...
-	// but I couldnt find where those headers were sent...
 	$nbActionsTotalThisDay = $generator->generate($visits,$actions);
-	
 	$actionsPerVisit = round($nbActionsTotalThisDay / $visits);
 	print("Generated $visits visits and $actionsPerVisit actions per visit for the ".date("Y-m-d", $startTime)."<br>\n");
 	$startTime+=86400;
@@ -92,5 +81,4 @@ echo "<br>Total requests per sec: ". round($nbActionsTotal / $t->getTime(),0);
 echo "<br>".$t;
 
 $generator->end();
-
 ob_end_flush();
