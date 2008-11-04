2a49d682fda9f833d3db505fff7c7a22abc831b4
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2008-11-04 18:26:30 +0000

    - adding autoload concept for options

--- core/Option.php
+++ core/Option.php
@@ -1,28 +1,68 @@
 <?php
-//TODO 
-// - design Caching, in terms of API, piwik.php loading data for a given website (from one file)
-// - design Options, how WP plugins make use of it, reuse same pattern
+class Piwik_Option
+{
+	private $all = array();
+
+	public function __construct()
+	{
+	}
+
+	public function get($name)
+	{
+		$this->autoload();
+		if(isset($this->all[$name]))
+		{
+			return $this->all[$name];
+		}
+		$value = Piwik_FetchOne( 'SELECT option_value 
+							FROM ' . Piwik::prefixTable('option') . ' 
+							WHERE option_name = ?', $name);
+		if($value === false)
+		{
+			return false;
+		}
+		$this->all[$name] = $value;
+		return $value;
+	}
+	
+	public function set($name, $value, $autoload = 0)
+	{
+		$autoload = (int)$autoload;
+		Piwik_Query('INSERT INTO '. Piwik::prefixTable('option') . ' (option_name, option_value, autoload) '.
+					' VALUES (?, ?, ?) '.
+					' ON DUPLICATE KEY UPDATE option_value = ?', 
+					array($name, $value, $autoload, $value));
+		$this->all[$name] = $value;
+	}
+	
+	private function autoload()
+	{
+		static $loaded = false;
+		if($loaded)
+		{
+			return;
+		}
+		try {
+			$all = Piwik_FetchAll('SELECT option_value, option_name ' .
+									' FROM '. Piwik::prefixTable('option') . 
+									' WHERE autoload = 1');
+		} catch(Exception $e) {
+			// this would fail for users who upgraded between 0.2.10 and 0.2.13 where option table didn't have the autoload field yet
+		}
+		foreach($this->all as $option)
+		{
+			$this->all[$option['option_name']] = $option['option_value'];
+		}
+		$loaded = true;
+	}
+}
+
 function Piwik_GetOption($name)
 {
-	try {
-		return Piwik_FetchOne( 'SELECT option_value 
-							FROM ' . Piwik::prefixTable('option') . ' 
-							WHERE option_name = ?', 
-							$name); 
-	} catch(Exception $e) {
-		return false;
-	}
+	return Zend_Registry::get('option')->get($name);
 }
 
-function Piwik_UpdateOption($name, $value)
+function Piwik_UpdateOption($name, $value, $autoload = 0)
 {
-	try {
-		return Piwik_Query('INSERT INTO '. Piwik::prefixTable('option') . ' (option_name, option_value) 
-						VALUES (?, ?) 
-						ON DUPLICATE KEY UPDATE option_value = ?', 
-						array($name, $value, $value));
-	} catch(Exception $e) {
-		return false;
-	}
+	Zend_Registry::get('option')->set($name, $value, $autoload);
 }
-
