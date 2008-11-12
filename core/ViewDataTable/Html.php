2088152a8d30fe472457ce2aca9f5fdac5a081ac
mattpiwik <matthieu.aubry@gmail.com>
2008-11-12 16:05:32 +0000

    - adding htmlspecialchars after the global urlencode (xss)
    git-svn-id: http://dev.piwik.org/svn/trunk@690 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- core/ViewDataTable/Html.php
+++ core/ViewDataTable/Html.php
@@ -76,7 +76,14 @@ class Piwik_ViewDataTable_Html extends Piwik_ViewDataTable
 		$this->mainAlreadyExecuted = true;
 		
 		$this->loadDataTableFromAPI();
-			
+	
+		// We apply a filter to the DataTable, decoding the label column (useful for keywords for example)
+		$filter = new Piwik_DataTable_Filter_ColumnCallbackReplace(
+									$this->dataTable, 
+									'label', 
+									create_function('$txt', 'return htmlspecialchars(urldecode($txt));')
+								);
+		
 		$view = new Piwik_View($this->dataTableTemplate);
 		
 		
