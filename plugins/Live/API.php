812e3fd2c17e556c69842175e6c2df7817b4ebc6
mattpiwik <matthieu.aubry@gmail.com>
2008-12-17 17:31:24 +0000

    - commiting dev version of Live! plugin
    git-svn-id: http://dev.piwik.org/svn/trunk@840 59fd770c-687e-43c8-a1e3-f5a4ff64c105

---
+++ plugins/Live/API.php
@@ -0,0 +1,143 @@
+<?php
+require_once "Live/Visitor.php";
+
+class Piwik_Live_API 
+{
+	static private $instance = null;
+	
+	/*
+	 * @return Piwik_Live_API
+	 */
+	static public function getInstance()
+	{
+		if (self::$instance == null)
+		{            
+			$c = __CLASS__;
+			self::$instance = new $c();
+		}
+		return self::$instance;
+	}
+
+	/*
+	 * @return Piwik_DataTable
+	 */
+	public function getLastVisitForVisitor( $visitorId, $idSite = null )
+	{
+		return $this->getLastVisitsForVisitor($visitorId, $idSite, 1);
+	}
+	
+	/*
+	 * @return Piwik_DataTable
+	 */
+	public function getLastVisitsForVisitor( $visitorId, $idSite, $limit = 10 )
+	{
+		if(is_null($idSite))
+		{
+			Piwik::checkUserIsSuperUser();
+		}
+		else
+		{
+			Piwik::checkUserHasViewAccess($idSite);
+		}
+		$visitorDetails = self::loadLastVisitorDetailsFromDatabase($visitorId, $idSite, $limit);
+		$table = self::getCleanedVisitorsFromDetails($visitorDetails);
+		return $table;
+	}
+
+	/*
+	 * @return Piwik_DataTable
+	 */
+	public function getLastVisits( $idSite = null, $limit = 10, $minIdVisit = null )
+	{
+		if(is_null($idSite))
+		{
+			Piwik::checkUserIsSuperUser();
+		}
+		else
+		{
+			Piwik::checkUserHasViewAccess($idSite);
+		}
+		$visitorDetails = self::loadLastVisitorDetailsFromDatabase(null, $idSite, $limit, $minIdVisit);
+		$table = self::getCleanedVisitorsFromDetails($visitorDetails);
+		return $table;
+	}
+	
+	/*
+	 * @return Piwik_DataTable
+	 */
+	static private function getCleanedVisitorsFromDetails($visitorDetails)
+	{
+		$table = new Piwik_DataTable();
+		foreach($visitorDetails as $visitorDetail)
+		{
+			self::cleanVisitorDetails($visitorDetail);
+			$visitor = new Piwik_Live_Visitor($visitorDetail);
+			$visitorDetailsArray = $visitor->getAllVisitorDetails();
+			$dateTimeVisit = Piwik_Date::factory($visitorDetailsArray['firstActionTimestamp']);
+			$visitorDetailsArray['serverDatePretty'] = $dateTimeVisit->getLocalized('%a %d %b');
+			$visitorDetailsArray['serverTimePretty'] = $dateTimeVisit->getLocalized('%X');
+			$table->addRowFromArray( array(Piwik_DataTable_Row::COLUMNS => $visitorDetailsArray));
+		}
+		return $table;
+	}
+	
+	/*
+	 * @return array
+	 */
+	private function loadLastVisitorDetailsFromDatabase($visitorId = null, $idSite = null, $limit = null, $minIdVisit = null )
+	{
+		$where = $whereBind = array();
+		
+		if(!is_null($idSite))
+		{
+			$where[] = " idsite = ? ";
+			$whereBind[] = $idSite;
+		}
+		
+		if(!is_null($visitorId))
+		{
+			$where[] = " visitor_idcookie = ? ";
+			$whereBind[] = $visitorId;
+		}
+		
+		if(!is_null($minIdVisit))
+		{
+			$where[] = " idvisit > ? ";
+			$whereBind[] = $minIdVisit;
+		}
+		
+		$sqlWhere = "";
+		if(count($where) > 0)
+		{
+			$sqlWhere = " WHERE " . join(' AND ', $where);
+		}
+		
+		$sql = "SELECT 	*
+				FROM " . Piwik::prefixTable('log_visit') . "
+				$sqlWhere 
+				ORDER BY idvisit DESC
+				LIMIT $limit";
+				
+		return Piwik_FetchAll($sql, $whereBind);
+	}
+
+	/*
+	 * @return void
+	 */
+	static private function cleanVisitorDetails( &$visitorDetails )
+	{
+		$toUnset = array('config_md5config');		
+		if(!Piwik::isUserIsSuperUser())
+		{
+			$toUnset[] = 'visitor_idcookie';
+			$toUnset[] = 'location_ip';
+		}
+		foreach($toUnset as $keyName)
+		{
+			if(isset($visitorDetails[$keyName]))
+			{
+				unset($visitorDetails[$keyName]);
+			}
+		}
+	}
+}
---
+++ plugins/Live/Visitor.php
@@ -0,0 +1,258 @@
+<?php
+//TODO add api to get actions name/count/first/last/etc
+require_once "Referers/functions.php";
+require_once "UserCountry/functions.php";
+require_once "UserSettings/functions.php";
+require_once "Provider/functions.php";
+
+class Piwik_Live_Visitor
+{
+	function __construct($visitorRawData)
+	{
+		$this->details = $visitorRawData;
+	}
+	
+	function getAllVisitorDetails()
+	{
+		return array(
+			'ip' => $this->getIp(),
+			'idVisit' => $this->getIdVisit(),
+			'countActions' => $this->getNumberOfActions(),
+			'isVisitorReturning' => $this->isVisitorReturning(),
+			'country' => $this->getCountryName(),
+			'countryFlag' => $this->getCountryFlag(),
+			'continent' => $this->getContinent(),
+			'provider' => $this->getProvider(),
+			'providerUrl' => $this->getProviderUrl(),
+			'idSite' => $this->getIdSite(),
+			'serverDate' => $this->getServerDate(),
+			'visitLength' => $this->getVisitLength(),
+			'visitLengthPretty' => $this->getVisitLengthPretty(),
+			'firstActionTimestamp' => $this->getTimestampFirstAction(),
+			'lastActionTimestamp' => $this->getTimestampLastAction(),
+		
+			'refererType' => $this->getRefererType(),
+			'refererName' => $this->getRefererTypeName(),
+			'keywords' => $this->getKeywords(),
+			'refererUrl' => $this->getRefererUrl(),
+			'refererName' => $this->getRefererName(),
+			'searchEngineUrl' => $this->getSearchEngineUrl(),
+			'searchEngineIcon' => $this->getSearchEngineIcon(),
+			
+			'operatingSystem' => $this->getOperatingSystem(),
+			'operatingSystemShortName' => $this->getOperatingSystemShortName(),
+			'operatingSystemIcon' => $this->getOperatingSystemIcon(),
+			'browserFamily' => $this->getBrowserFamily(),
+			'browserFamilyDescription' => $this->getBrowserFamilyDescription(),
+	 		'browser' => $this->getBrowser(),
+			'browserIcon' => $this->getBrowserIcon(),
+			'screen' => $this->getScreenType(),
+			'resolution' => $this->getResolution(),
+			'screenIcon' => $this->getScreenTypeIcon(),
+			'plugins' => $this->getPlugins(),
+		);
+	}
+	
+	function getServerDate()
+	{
+		return $this->details['visit_server_date'];
+	}
+	
+	function getIp()
+	{
+		return long2ip($this->details['location_ip']);
+	}
+	
+	function getIdVisit()
+	{
+		return $this->details['idvisit'];
+	}
+	
+	function getIdSite()
+	{
+		return $this->details['idsite'];
+	}
+	
+	function getNumberOfActions()
+	{
+		return $this->details['visit_total_actions'];
+	}
+
+	function getVisitLength()
+	{
+		return $this->details['visit_total_time'];
+	}
+	
+	function getVisitLengthPretty()
+	{
+		return Piwik_getPrettyTimeFromSeconds($this->details['visit_total_time']);
+	}
+	
+	function isVisitorReturning()
+	{
+		return $this->details['visitor_returning'];
+	}
+	
+	function getTimestampFirstAction()
+	{
+		return strtotime($this->details['visit_first_action_time']);
+	}
+	
+	function getTimestampLastAction()
+	{
+		return strtotime($this->details['visit_last_action_time']);
+	}
+	
+	function getCountryName()
+	{
+		return Piwik_CountryTranslate($this->details['location_country']);
+	}
+	
+	function getCountryFlag()
+	{
+		return Piwik_getFlagFromCode($this->details['location_country']);
+	}
+	
+	function getContinent()
+	{
+		return Piwik_ContinentTranslate($this->details['location_continent']);
+	}
+	
+	function getRefererType()
+	{
+		$map = array(
+			Piwik_Common::REFERER_TYPE_SEARCH_ENGINE => 'searchEngine',
+			Piwik_Common::REFERER_TYPE_WEBSITE => 'website',
+			Piwik_Common::REFERER_TYPE_DIRECT_ENTRY => 'directEntry',
+			Piwik_Common::REFERER_TYPE_CAMPAIGN => 'campaign',
+		);
+		if(isset($map[$this->details['referer_type']]))
+		{
+			return $map[$this->details['referer_type']];
+		}
+		return $map[Piwik_Common::REFERER_TYPE_DIRECT_ENTRY];
+	}
+	
+	function getRefererTypeName()
+	{
+		return Piwik_getRefererTypeLabel($this->details['referer_type']);
+	}
+	
+	function getKeywords()
+	{
+		return $this->details['referer_keyword'];
+	}
+	
+	function getRefererUrl()
+	{
+		return $this->details['referer_url'];		
+	}
+	
+	function getRefererName()
+	{
+		return $this->details['referer_name'];		
+	}
+	
+	function getSearchEngineUrl()
+	{
+		if($this->getRefererType() == 'searchEngine'
+			&& !empty($this->details['referer_name']))
+		{
+			return Piwik_getSearchEngineUrlFromName($this->details['referer_name']);
+		}
+		return null;
+	}
+	
+	function getSearchEngineIcon()
+	{
+		$searchEngine = $this->getSearchEngineUrl();
+		if( !is_null($searchEngine) )
+		{
+			return Piwik_getSearchEngineLogoFromName($searchEngine);
+		}
+		return null;
+	}
+
+	function getPlugins()
+	{
+		$plugins = array( 
+	 		'config_pdf',
+	 		'config_flash',
+	 		'config_java',
+	 		'config_director',
+	 		'config_quicktime',
+	 		'config_realplayer',
+	 		'config_windowsmedia'
+		);
+		$return = array();
+		foreach($plugins as $plugin)
+		{
+			if($this->details[$plugin] == 1) 
+			{
+				$pluginShortName = substr($plugin, 7);
+				$return[] = $pluginShortName; 
+			}
+		}
+		return implode(", ", $return);
+	}
+
+	function getOperatingSystem()
+	{
+		return Piwik_getOSLabel($this->details['config_os']);
+	}
+	
+	function getOperatingSystemShortName()
+	{
+		return Piwik_getOSShortLabel($this->details['config_os']);
+	}
+	
+	function getOperatingSystemIcon()
+	{
+		return Piwik_getOSLogo($this->details['config_os']);
+	}
+
+	function getBrowserFamilyDescription()
+	{
+		return Piwik_getBrowserTypeLabel($this->getBrowserFamily());
+	}
+	
+	function getBrowserFamily()
+	{
+		return Piwik_getBrowserFamily($this->details['config_browser_name']);
+	}
+	
+	function getBrowser()
+	{
+		return Piwik_getBrowserLabel($this->details['config_browser_name'] . ";" . $this->details['config_browser_version']);
+	}
+	
+	function getBrowserIcon()
+	{
+		return Piwik_getBrowsersLogo($this->details['config_browser_name'] . ";" . $this->details['config_browser_version']);
+	}
+	
+	function getScreenType()
+	{
+		return Piwik_getScreenTypeFromResolution($this->details['config_resolution']);
+	}
+	
+	function getResolution()
+	{
+		return $this->details['config_resolution'];
+	}
+	
+	function getScreenTypeIcon()
+	{
+		return Piwik_getScreensLogo($this->getScreenType());
+	}
+	
+	function getProvider()
+	{
+		return Piwik_getHostnameName($this->details['location_provider']);
+	}
+	
+	function getProviderUrl()
+	{
+		return Piwik_getHostnameUrl($this->details['location_provider']);
+	}
+}
