5573a64226a158aa626228f70f769ce7a3a52079
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2009-02-28 08:10:37 +0000

    - fix #26 (The user config file cannot only define a variable, it has to define the whole "category" of variables)

---
+++ tests/core/Config.test.php
@@ -0,0 +1,42 @@
+<?php
+if(!defined("PATH_TEST_TO_ROOT")) {
+	define('PATH_TEST_TO_ROOT', getcwd().'/../..');
+}
+if(!defined('CONFIG_TEST_INCLUDED'))
+{
+	require_once PATH_TEST_TO_ROOT . "/tests/config_test.php";
+}
+
+class Test_Piwik_Blank extends UnitTestCase
+{
+	function __construct( $title = '')
+	{
+		parent::__construct( $title );
+	}
+	
+	public function setUp()
+	{
+	}
+	
+	public function tearDown()
+	{
+	}
+	
+    
+    /**
+     * -> exception
+     */
+    public function testUserConfigOverwritesSectionGlobalConfigValue()
+    {
+    	$userFile = 'tests/resources/Config/config.ini.php';
+    	$globalFile = 'tests/resources/Config/global.ini.php';
+    	
+    	$config = new Piwik_Config($userFile, $globalFile);
+    	
+    	$this->assertEqual($config->Category->key1, "value_overwritten");
+    	$this->assertEqual($config->Category->key2, "value2");
+    	$this->assertEqual($config->General->login, "test");
+    	$this->assertEqual($config->CategoryOnlyInGlobalFile->key3, "value3");
+    }
+}
+
