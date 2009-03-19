a6102899a7b5cde5afcaa1502a3de46d11395a65
mattpiwik <matthieu.aubry@gmail.com>
2009-03-19 03:20:04 +0000

    - trying to setup unit tests in hudson
    git-svn-id: http://dev.piwik.org/svn/trunk@989 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- tests/TestFrameworkConfig/classes/PiwikTestEnv.php
+++ tests/TestFrameworkConfig/classes/PiwikTestEnv.php
@@ -1,69 +1,27 @@
 <?php
 
-require_once dirname(__FILE__) . '/../../../application/config.php';
 require_once dirname(__FILE__) . '/../../TestFramework/classes/AbstractTestEnv.php';
-require_once dirname(__FILE__) . '/../../../library/doctrine/Doctrine.php';
-require_once dirname(__FILE__) . '/../../../library/OX/Doctrine/cfg/doctrine-init.php';
-require_once dirname(__FILE__) . '/../../../library/OX/Common/zend-init.php';
-require_once dirname(__FILE__) . '/../../../library/OX/Doctrine/DB.php';
-require_once dirname(__FILE__) . '/../../../library/OX/Common/Config.php';
-require_once dirname(__FILE__) . '/../../../application/model-utils/SampleDataGenerator.php';
 
 class PiwikTestEnv extends AbstractTestEnv
 {
     public function ensureTestDatabaseConnection()
     {
-        $connection = Doctrine_Manager::connection();
-        $dsn = OX_Doctrine_DB::getDsn(OX_DSN_STRING, 
-            OX_Common_Config::instance('database', 
-                $this->getConfigFilename())->toArray());
-        $newConnection = Doctrine_Manager::connection($dsn);
-        if (0 != strcmp($newConnection->getOption('dsn'), 
-          $connection->getOption('dsn'))) {
-            
-          Doctrine_Manager::getInstance()->closeConnection(
-              $connection);
-          $connection = Doctrine_Manager::getInstance(
-              )->openConnection($dsn, null, TRUE);
-          global $conn;
-          $conn = $connection; 
-        }
-        Doctrine_Manager::getInstance()->closeConnection(
-            $newConnection);
     }
     
     public function setupDB($ignore_errors = false)
     {
-        $this->ensureTestDatabaseConnection();
-        $connection = Doctrine_Manager::connection();
-        
-        if (isset($connection)) {
-            $result = $connection->createDatabase();
-            
-            if (!$ignore_errors) {
-                if (!is_string($result)) {
-                	throw new TestFrameworkException(
-                	   $result->errorMessage());
-                }
-            }
-        }
     }
 
     public function setupCoreTables()
     {
-        Doctrine::createTablesFromModels();
     }
 
     public function setupDefaultData()
     {
-        SampleDataGenerator::generateData(false);
     }
 
     public function teardownDB()
     {
-        $this->ensureTestDatabaseConnection();
-        $connection = Doctrine_Manager::connection();
-        $connection->dropDatabase();
     }
 
     public function getConfigFilename()
