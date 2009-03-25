82e5ad5eb4d2dbcf2e8c5dd0b3aabf01f978cf92
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2009-03-25 06:03:45 +0000

    - refs #310 adding updates so that all existing tables and all fields are converted to utf8 for consistent piwik tables

--- core/Updates/0.2.33.php
+++ core/Updates/0.2.33.php
@@ -1,5 +1,14 @@
 <?php
 require_once "SitesManager/API.php";
+
+// alter table to set the utf8 collation
+$tablesToAlter = Piwik::getTablesInstalled(true);
+foreach($tablesToAlter as $table) {
+	Piwik_Query("ALTER TABLE `". $table . "` 
+				 CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci ");
+}
+// force regeneration of cache files as we add 'hosts' entry in it
 Piwik::setUserIsSuperUser();
 $allSiteIds = Piwik_SitesManager_API::getAllSitesId();
 Piwik_Common::regenerateCacheWebsiteAttributes($allSiteIds);
+
