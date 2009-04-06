3216a7d0e47200859d573d1a0f668fab194f67d9
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2009-04-06 06:52:10 +0000

    adding language file

---
+++ plugins/ExamplePlugin/lang/en.php
@@ -0,0 +1,6 @@
+<?php 
+$translations = array(
+	'ExamplePlugin_exampleWidget' => 'Example widget',
+	'ExamplePlugin_blogPiwikRss' => 'Blog Piwik RSS',
+	'ExamplePlugin_photostreamMatt' => 'Matt\'s Photos',
+);
