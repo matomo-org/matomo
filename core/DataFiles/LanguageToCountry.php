4f00109012bf0e6d5e041175ae22550c03e06c99
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2009-06-28 20:57:13 +0000

    use single quotes for these constant strings

--- core/DataFiles/LanguageToCountry.php
+++ core/DataFiles/LanguageToCountry.php
@@ -23,44 +23,44 @@
 if(!isset($GLOBALS['Piwik_LanguageToCountry']))
 {
 	$GLOBALS['Piwik_LanguageToCountry'] = array(
-			"bg" => "bg",	// Bulgarian  => Bulgaria
-			"ca" => "es",	// Catalan    => Spain
-			"cs" => "cz",	// Czech      => Czech Republic
-			"da" => "dk",	// Danish     => Denmark
-			"de" => "de",	// German     => Germany
-			"el" => "gr",	// Greek      => Greece
-			"es" => "es",	// Spanish    => Spain
-			"et" => "ee",	// Estonian   => Estonia
-			"fa" => "ir",	// Farsi      => Iran
-			"fi" => "fi",	// Finnish    => Finland
-			"fr" => "fr",	// French     => France
-			"he" => "il",	// Hebrew     => Israel
-			"hr" => "hr",	// Croatian   => Croatia
-			"hu" => "hu",	// Hungarian  => Hungary
-			"id" => "id",	// Indonesian => Indonesia
-			"is" => "is",	// Icelandic  => Iceland
-			"it" => "it",	// Italian    => Italy
-			"ja" => "jp",	// Japanese   => Japan
-			"ko" => "kr",	// Korean     => South Korea
-			"lt" => "lt",	// Lithuanian => Lithuania
-			"lv" => "lv",	// Latvian    => Latvia
-			"mk" => "mk",	// Macedonian => Macedonia
-			"ms" => "my",	// Malay      => Malaysia
-			"nb" => "no",	// Bokmål     => Norway
-			"nl" => "nl",	// Dutch      => Netherlands
-			"nn" => "no",	// Nynorsk    => Norway
-			"no" => "no",	// Norwegian  => Norway
-			"pl" => "pl",	// Polish     => Poland
-			"pt" => "pt",	// Portugese  => Portugal
-			"ro" => "ro",	// Romanian   => Romania
-			"ru" => "ru",	// Russian    => Russia
-			"sk" => "sk",	// Slovak     => Slovakia
-			"sl" => "si",	// Slovene    => Slovenia
-			"sq" => "al",	// Albanian   => Albania
-			"sr" => "rs",	// Serbian    => Serbia
-			"sv" => "se",	// Swedish    => Sweden
-			"th" => "th",	// Thai       => Thailand
-			"tr" => "tr",	// Turkish    => Turkey
-			"uk" => "ua",	// Ukrainian  => Ukraine
+			'bg' => 'bg',	// Bulgarian  => Bulgaria
+			'ca' => 'es',	// Catalan    => Spain
+			'cs' => 'cz',	// Czech      => Czech Republic
+			'da' => 'dk',	// Danish     => Denmark
+			'de' => 'de',	// German     => Germany
+			'el' => 'gr',	// Greek      => Greece
+			'es' => 'es',	// Spanish    => Spain
+			'et' => 'ee',	// Estonian   => Estonia
+			'fa' => 'ir',	// Farsi      => Iran
+			'fi' => 'fi',	// Finnish    => Finland
+			'fr' => 'fr',	// French     => France
+			'he' => 'il',	// Hebrew     => Israel
+			'hr' => 'hr',	// Croatian   => Croatia
+			'hu' => 'hu',	// Hungarian  => Hungary
+			'id' => 'id',	// Indonesian => Indonesia
+			'is' => 'is',	// Icelandic  => Iceland
+			'it' => 'it',	// Italian    => Italy
+			'ja' => 'jp',	// Japanese   => Japan
+			'ko' => 'kr',	// Korean     => South Korea
+			'lt' => 'lt',	// Lithuanian => Lithuania
+			'lv' => 'lv',	// Latvian    => Latvia
+			'mk' => 'mk',	// Macedonian => Macedonia
+			'ms' => 'my',	// Malay      => Malaysia
+			'nb' => 'no',	// Bokmål     => Norway
+			'nl' => 'nl',	// Dutch      => Netherlands
+			'nn' => 'no',	// Nynorsk    => Norway
+			'no' => 'no',	// Norwegian  => Norway
+			'pl' => 'pl',	// Polish     => Poland
+			'pt' => 'pt',	// Portugese  => Portugal
+			'ro' => 'ro',	// Romanian   => Romania
+			'ru' => 'ru',	// Russian    => Russia
+			'sk' => 'sk',	// Slovak     => Slovakia
+			'sl' => 'si',	// Slovene    => Slovenia
+			'sq' => 'al',	// Albanian   => Albania
+			'sr' => 'rs',	// Serbian    => Serbia
+			'sv' => 'se',	// Swedish    => Sweden
+			'th' => 'th',	// Thai       => Thailand
+			'tr' => 'tr',	// Turkish    => Turkey
+			'uk' => 'ua',	// Ukrainian  => Ukraine
 		);
 }
