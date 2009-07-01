0fec5e0f7588a2047d1ba7af2999cd38e471face
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2009-07-01 05:51:49 +0000

    Quick fixes #844 - __isset() must be public for php 5.3; same fix applied in ZF standard/trunk

--- libs/Zend/Config.php
+++ libs/Zend/Config.php
@@ -181,7 +181,7 @@ class Zend_Config implements Countable, Iterator
      * @param string $name
      * @return boolean
      */
-    protected function __isset($name)
+    public function __isset($name)
     {
         return isset($this->_data[$name]);
     }
