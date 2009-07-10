c5309174c3fd02f959d93735f7e3fe3df67ea8ba
mattpiwik <matthieu.aubry@gmail.com>
2009-07-10 17:28:26 +0000

    adding check for javascript strings in the translations
    git-svn-id: http://dev.piwik.org/svn/trunk@1302 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- tests/core/LanguagesManager.test.php
+++ tests/core/LanguagesManager.test.php
@@ -26,6 +26,7 @@ class Test_Languages_Manager extends UnitTestCase
 			ob_start(); 
 			$strings = Piwik_LanguagesManager_API::getTranslationsForLanguage($language);
 			$content = ob_get_flush();
+			$this->assertTrue(strpos(serialize($strings), "<script") === false, " language file containing javascript");
 			$this->assertTrue(count($strings) > 100); // at least 100 translations in the language file
 			$this->assertTrue(strlen($content) == 0, "buffer was ".strlen($content)." long but should be zero. Translation file for '$language' must be buggy.");
 		}
