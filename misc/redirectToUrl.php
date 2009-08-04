e354a599b000cd09c2480442ce215f8e46a51cd5
robocoder <anthon.pang@gmail.com>
2009-08-04 15:20:40 +0000

    filter urls
    git-svn-id: http://dev.piwik.org/svn/trunk@1363 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- misc/redirectToUrl.php
+++ misc/redirectToUrl.php
@@ -2,6 +2,7 @@
 // we redirect to the website instead of linking directly because we don't want
 // to expose the referer on the piwik demo
 $url = htmlentities($_GET['url']);
+if(!preg_match('~http://(forum\.)?piwik.org(/|$)~', $url)) { die; }
 ?>
 <html><head>
 <meta http-equiv="refresh" content="0;url=<?php echo $url; ?>"/>
