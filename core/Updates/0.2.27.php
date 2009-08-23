a74e576153975164edf1380dc48952675011e671
robocoder <anthon.pang@gmail.com>
2009-08-23 15:29:12 +0000

    add phpdoc header for consistency; ignore if possible
    git-svn-id: http://dev.piwik.org/svn/trunk@1433 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- core/Updates/0.2.27.php
+++ core/Updates/0.2.27.php
@@ -1,5 +1,18 @@
 <?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
+ * @version $Id$
+ *
+ * @category Piwik
+ * @package Updates
+ */
 
+/**
+ * @package Updates
+ */
 class Piwik_Updates_0_2_27 implements Piwik_iUpdate
 {
 	static function update()
