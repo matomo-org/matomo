588961ee011e1ded63c2d083e49bcf3fe0985ef0
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2009-09-07 22:18:31 +0000

    Use Zend_Db_Adapter-like naming

--- core/Tracker/Db/Mysqli.php
+++ core/Tracker/Db/Mysqli.php
@@ -5,21 +5,22 @@
  * @link http://piwik.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
  * @version $Id$
- * 
+ *
  * @category Piwik
  * @package Piwik
  */
 
 /**
- * PDO MySQL wrapper
+ * mysqli wrapper
  *
  * @package Piwik
  * @subpackage Piwik_Tracker
  */
-class Piwik_Tracker_Db_PdoMySql extends Piwik_Tracker_Db
+class Piwik_Tracker_Db_Mysqli extends Piwik_Tracker_Db
 {
 	private $connection = null;
-	private $dsn;
+	private $host;
+	private $dbname;
 	private $username;
 	private $password;
 	
@@ -30,16 +31,17 @@ class Piwik_Tracker_Db_PdoMySql extends Piwik_Tracker_Db
 	{
 		if(isset($dbInfo['unix_socket']) && $dbInfo['unix_socket'][0] == '/')
 		{
-			$this->dsn = $driverName.":dbname=${dbInfo['dbname']};unix_socket=${dbInfo['unix_socket']}";
+			$this->host = ':' . $dbInfo['unix_socket'];
 		}
 		else if ($dbInfo['port'][0] == '/')
 		{
-			$this->dsn = $driverName.":dbname=${dbInfo['dbname']};unix_socket=${dbInfo['port']}";
+			$this->host = ':' . $dbInfo['port'];
 		}
 		else
 		{
-			$this->dsn = $driverName.":dbname=${dbInfo['dbname']};host=${dbInfo['host']};port=${dbInfo['port']}";
+			$this->host = $dbInfo['host'] . ':' . $dbInfo['port'];
 		}
+		$this->dbname = $dbInfo['dbname'];
 		$this->username = $dbInfo['username'];
 		$this->password = $dbInfo['password'];
 	}
@@ -61,11 +63,9 @@ class Piwik_Tracker_Db_PdoMySql extends Piwik_Tracker_Db
 			$timer = $this->initProfiler();
 		}
 		
-		$this->connection = new PDO($this->dsn, $this->username, $this->password);
-		$this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		// we may want to setAttribute(PDO::ATTR_TIMEOUT ) to a few seconds (default is 60) in case the DB is locked
-		// the piwik.php would stay waiting for the database... bad!
-		// we delete the password from this object "just in case" it could be printed 
+		$this->connection = mysql_connect($this->host, $this->username, $this->password);
+		$result = mysql_select_db($this->dbname);
+
 		$this->password = '';
 		
 		if(self::$profiling)
@@ -97,13 +97,14 @@ class Piwik_Tracker_Db_PdoMySql extends Piwik_Tracker_Db
 	public function fetchAll( $query, $parameters = array() )
 	{
 		try {
-			$sth = $this->query( $query, $parameters );
-			if($sth === false)
+			$query = $this->prepare( $query, $parameters );
+			$rs = mysql_query($query);
+			while($row = mysql_fetch_array($rs, MYSQL_ASSOC)) 
 			{
-				return false;
+				$rows[] = $row;
 			}
-			return $sth->fetchAll(PDO::FETCH_ASSOC);
-		} catch (PDOException $e) {
+			return $rows;
+		} catch (Exception $e) {
 			throw new Exception("Error query: ".$e->getMessage());
 		}
 	}
@@ -120,24 +121,26 @@ class Piwik_Tracker_Db_PdoMySql extends Piwik_Tracker_Db
 	public function fetch( $query, $parameters = array() )
 	{
 		try {
-			$sth = $this->query( $query, $parameters );
-			if($sth === false)
+			$query = $this->prepare( $query, $parameters );
+			$rs = mysql_query($query);
+			if($rs === false)
 			{
 				return false;
 			}
-			return $sth->fetch(PDO::FETCH_ASSOC);			
-		} catch (PDOException $e) {
+			$row = mysql_fetch_array($rs, MYSQL_ASSOC);
+			return $row;
+		} catch (Exception $e) {
 			throw new Exception("Error query: ".$e->getMessage());
 		}
 	}
-
+	
 	/**
 	 * Executes a query, using optional bound parameters.
 	 * 
 	 * @param string Query 
 	 * @param array|string Parameters to bind array('idsite'=> 1)
 	 * 
-	 * @return PDOStatement or false if failed
+	 * @return bool|resource false if failed
 	 * @throws Exception if an exception occured
 	 */
 	public function query($query, $parameters = array()) 
@@ -156,15 +159,15 @@ class Piwik_Tracker_Db_PdoMySql extends Piwik_Tracker_Db
 			{
 				$parameters = array( $parameters );
 			}
-			$sth = $this->connection->prepare($query);
-			$sth->execute( $parameters );
+			$query = $this->prepare( $query, $parameters );
+			$result = mysql_query($query);
 			
 			if(self::$profiling)
 			{
 				$this->recordQueryProfile($query, $timer);
 			}
-			return $sth;
-		} catch (PDOException $e) {
+			return $result;
+		} catch (Exception $e) {
 			throw new Exception("Error query: ".$e->getMessage() . "
 								In query: $query
 								Parameters: ".var_export($parameters, true));
@@ -173,12 +176,30 @@ class Piwik_Tracker_Db_PdoMySql extends Piwik_Tracker_Db
 	
 	/**
 	 * Returns the last inserted ID in the DB
-	 * Wrapper of PDO::lastInsertId()
 	 * 
 	 * @return int
 	 */
 	public function lastInsertId()
 	{
-		return $this->connection->lastInsertId();
+		return mysql_insert_id();
+	}
+
+	/**
+	 * Input is a prepared SQL statement and parameters
+	 * Returns the SQL statement
+	 *
+	 * @param string $query
+	 * @param array $parameters 
+	 * @return string
+	 */
+	private function prepare($query, $parameters) {
+		foreach($parameters as $i=>$p) 
+		{
+			$parameters[$i] = addslashes($p);
+		}
+		$query = str_replace('?', "'%s'", $query);
+		array_unshift($parameters, $query);
+		$query = call_user_func_array(sprintf, $parameters);
+		return $query;
 	}
 }
