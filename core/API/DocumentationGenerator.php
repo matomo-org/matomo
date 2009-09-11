1eb9a1319a143f02db147d29f4da91fce77e5e55
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2009-09-11 00:58:22 +0000

    fixes #953 - API doesn't handle default null parameters; remove checkParametersAreNotNull

--- core/API/DocumentationGenerator.php
+++ core/API/DocumentationGenerator.php
@@ -158,7 +158,7 @@ class Piwik_API_DocumentationGenerator
 		{
 			// if there isn't a default value for a given parameter, 
 			// we need a 'know default value' or we can't generate the link
-			if($defaultValue === Piwik_API_Proxy::NO_DEFAULT_VALUE)
+			if($defaultValue instanceof Piwik_API_Proxy_NoDefaultValue)
 			{
 				if(isset($knowExampleDefaultParametersValues[$nameVariable]))
 				{
@@ -190,7 +190,7 @@ class Piwik_API_DocumentationGenerator
 		foreach($aParameters as $nameVariable=> $defaultValue)
 		{
 			$str = $nameVariable;
-			if($defaultValue !== Piwik_API_Proxy::NO_DEFAULT_VALUE)
+			if(!($defaultValue instanceof Piwik_API_Proxy_NoDefaultValue))
 			{
 				$str .= " = '$defaultValue'";
 			}
