e9c63d0419d6555889332448246b974cb16d77dc
robocoder <anthon.pang@gmail.com>
2009-10-29 12:49:32 +0000

    Use Piwik_Exec() for DDL queries as earlier versions of mysqli can't prepare these statements.
    git-svn-id: http://dev.piwik.org/svn/trunk@1549 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- plugins/ExampleFeedburner/ExampleFeedburner.php
+++ plugins/ExampleFeedburner/ExampleFeedburner.php
@@ -30,7 +30,7 @@ class Piwik_ExampleFeedburner extends Piwik_Plugin
 	function install()
 	{
 		try{
-			Piwik_Query('ALTER TABLE '.Piwik::prefixTable('site'). " ADD `feedburnerName` VARCHAR( 100 ) DEFAULT NULL");
+			Piwik_Exec('ALTER TABLE '.Piwik::prefixTable('site'). " ADD `feedburnerName` VARCHAR( 100 ) DEFAULT NULL");
 		} catch(Zend_Db_Statement_Exception $e){
 			// mysql code error 1060: column already exists
 			// if there is another error we throw the exception, otherwise it is OK as we are simply reinstalling the plugin
