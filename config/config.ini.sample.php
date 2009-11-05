5127d3f1867de4f6ed912d91e4a2027c3274cfe1
robocoder <anthon.pang@gmail.com>
2009-11-05 13:45:59 +0000

    refs #991 - add comment to config.ini.sample.php to not copy this file
    git-svn-id: http://dev.piwik.org/svn/trunk@1569 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- config/config.ini.sample.php
+++ config/config.ini.sample.php
@@ -1,8 +1,9 @@
 ; <?php exit; ?> DO NOT REMOVE THIS LINE
-; this file is just here for documentation purpose
+; this file for documentation purposes; DO NOT COPY this file to config.ini.php;
 ; the config.ini.php is normally created during the installation process
-; when this file is absent it triggers the Installation process
-; the config.ini.php file contains information about the super user and the database access
+; (see http://piwik.org/docs/installation)
+; when this file is absent it triggers the Installation process to create
+; config.ini.php; that file will contain the superuser and database access info
 
 [superuser]
 login			= yourSuperUserLogin
