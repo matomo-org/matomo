a3ea3d2fc463d32d2dc62c828a422f18d5e1fb2b
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2009-12-09 14:21:28 +0000

    Moving time intensive operations at the end of the update process to try and make all the critical updates run first.

--- core/Updates/0.5.php
+++ core/Updates/0.5.php
@@ -19,15 +19,15 @@ class Piwik_Updates_0_5 implements Piwik_iUpdate
 	{
 		Piwik_Updater::updateDatabase(__FILE__, array(
 			'ALTER TABLE ' . Piwik::prefixTable('log_action'). ' ADD COLUMN `hash` INTEGER(10) UNSIGNED NOT NULL AFTER `name`;' => false,
-			'UPDATE '.  Piwik::prefixTable('log_action'). ' SET `hash` = CRC32(name);' => false,
-			'CREATE INDEX index_type_hash ON '. Piwik::prefixTable('log_action') .' (type, hash);' => false,
-			'DROP INDEX index_type_name ON '. Piwik::prefixTable('log_action') .';' => false,
 			'ALTER TABLE '. Piwik::prefixTable('log_visit') .' CHANGE visit_exit_idaction visit_exit_idaction_url INTEGER(11) NOT NULL;' => false,
 			'ALTER TABLE '. Piwik::prefixTable('log_visit') .' CHANGE visit_entry_idaction visit_entry_idaction_url INTEGER(11) NOT NULL;' => false,
 			'ALTER TABLE ' . Piwik::prefixTable('log_link_visit_action'). ' CHANGE `idaction_ref` `idaction_url_ref` INTEGER(10) UNSIGNED NOT NULL;' => false,
 			'ALTER TABLE ' . Piwik::prefixTable('log_link_visit_action'). ' CHANGE `idaction` `idaction_url` INTEGER(10) UNSIGNED NOT NULL;' => false,
 			'ALTER TABLE ' . Piwik::prefixTable('log_link_visit_action'). ' ADD COLUMN `idaction_name` INTEGER(10) UNSIGNED AFTER `idaction_url_ref`;' => false,
 			'ALTER TABLE ' . Piwik::prefixTable('log_conversion'). ' CHANGE `idaction` `idaction_url` INTEGER(11) UNSIGNED NOT NULL;' => false,
+			'UPDATE '.  Piwik::prefixTable('log_action'). ' SET `hash` = CRC32(name);' => false,
+			'CREATE INDEX index_type_hash ON '. Piwik::prefixTable('log_action') .' (type, hash);' => false,
+			'DROP INDEX index_type_name ON '. Piwik::prefixTable('log_action') .';' => false,
 		));
 	}
 }
