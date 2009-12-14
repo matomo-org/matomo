855ac5db40b2fba208c090272c68d0399e21470d
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2009-12-14 18:07:16 +0000

    refs #524 - add standard header

--- plugins/MultiSites/Controller.php
+++ plugins/MultiSites/Controller.php
@@ -1,5 +1,19 @@
 <?php
+/**
+ * Piwik - Open source web analytics
+ * 
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
+ * @version $Id$
+ * 
+ * @category Piwik_Plugins
+ * @package Piwik_MultiSites
+ */
 
+/**
+ *
+ * @package Piwik_MultiSites
+ */
 class Piwik_MultiSites_Controller extends Piwik_Controller
 {
 	protected $orderBy = 'names';
@@ -133,4 +147,3 @@ class Piwik_MultiSites_Controller extends Piwik_Controller
 		return $this->renderView($view, $fetch);
 	}
 }
-?>
