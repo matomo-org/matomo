0edc4898194613c410c2eebee6c0cb56b4cd8e7e
mattpiwik <matthieu.aubry@gmail.com>
2009-12-14 18:12:31 +0000

    Update to 0.5.3 enables the new MultiSites plugin by default
    git-svn-id: http://dev.piwik.org/svn/trunk@1699 59fd770c-687e-43c8-a1e3-f5a4ff64c105

---
+++ core/Updates/0.5.3.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @package Updates
+ */
+class Piwik_Updates_0_5_3 implements Piwik_iUpdate
+{
+	static function update()
+	{
+		$config = Zend_Registry::get('config');
+		try {
+			if(is_writable( Piwik_Config::getDefaultUserConfigPath() )) {
+				$plugins = $config->Plugins->toArray();
+				$plugins[] = 'MultiSites';
+				$config->Plugins = $plugins;
+				$config->__destruct();
+				Piwik::createConfigObject();
+				return;
+			}
+		} catch(Exception $e) { }
+		throw new Piwik_Updater_UpdateErrorException("Edit config.ini.php and add to <code>[Plugins]</code> section: <code>Plugins[] = MultiSites</code>");
+	}
+}
