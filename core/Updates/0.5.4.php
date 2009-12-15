b7f28a11e34e159a37c4763df6eb40ebe449468f
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2009-12-15 22:24:44 +0000

    mandatory vs optional update

---
+++ core/Updates/0.5.4.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
+ * @version $Id$
+ *
+ * @category Piwik
+ * @package Updates
+ */
+
+/**
+ * @package Updates
+ */
+class Piwik_Updates_0_5_4 implements Piwik_iUpdate
+{
+	static function update()
+	{
+		$config = Zend_Registry::get('config');
+		$salt = Piwik_Common::generateUniqId();
+		if(!isset($config->superuser->salt))
+		{
+			try {
+				if(is_writable( Piwik_Config::getDefaultUserConfigPath() ))
+				{
+					$superuser_info = $config->superuser->toArray();
+					$superuser_info['salt'] = $salt;
+					$config->superuser = $superuser_info;
+
+					$config->__destruct();
+					Piwik::createConfigObject();
+				}
+				else
+				{
+					throw new Exception('mandatory update failed');
+				}
+			} catch(Exception $e) {
+				throw new Piwik_Updater_UpdateErrorException("Edit config.ini.php and add below <code>[superuser]</code> the following line <br/><code>salt = $salt</code>");
+			}
+		}
+
+		$config = Zend_Registry::get('config');
+		$plugins = $config->Plugins->toArray();
+		if(!in_array('MultiSites', $plugins))
+		{
+			try {
+				if(is_writable( Piwik_Config::getDefaultUserConfigPath() ))
+				{
+					$plugins[] = 'MultiSites';
+					$config->Plugins = $plugins;
+
+					$config->__destruct();
+					Piwik::createConfigObject();
+				}
+				else
+				{
+					throw new Exception('optional update failed');
+				}
+			} catch(Exception $e) {
+				throw new Exception("You can now enable the new MultiSites plugin in the Plugins screen in the Piwik admin!");
+			}
+		}
+	}
+}
