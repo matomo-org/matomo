c8415c7e6a2c2da6909f397b7b1ca6f198a3b736
robocoder <anthon.pang@gmail.com>
2010-02-10 08:32:03 +0000

    refs #1122 - fix core usage
    git-svn-id: http://dev.piwik.org/svn/trunk@1834 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- core/Updates/0.2.34.php
+++ core/Updates/0.2.34.php
@@ -19,7 +19,7 @@ class Piwik_Updates_0_2_34 implements Piwik_iUpdate
 	{
 		// force regeneration of cache files following #648
 		Piwik::setUserIsSuperUser();
-		$allSiteIds = Piwik_SitesManager_API::getAllSitesId();
+		$allSiteIds = Piwik_SitesManager_API::getInstance()->getAllSitesId();
 		Piwik_Common::regenerateCacheWebsiteAttributes($allSiteIds);
 	}
 }
