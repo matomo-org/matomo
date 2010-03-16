1a1aa317349f15d1e08bd2614614f3738d056456
robocoder <anthon.pang@gmail.com>
2010-03-16 03:13:33 +0000

    fixes #1091 - update the Feedback plugin per ticket; also i18n-ize the remaining text strings, replace iframe with jquery-ui modal dialog, and add another example of using a nonce
    git-svn-id: http://dev.piwik.org/svn/trunk@1920 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- plugins/Feedback/Controller.php
+++ plugins/Feedback/Controller.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Piwik - Open source web analytics
- * 
+ *
  * @link http://piwik.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
  * @version $Id$
- * 
+ *
  * @category Piwik_Plugins
  * @package Piwik_Feedback
  */
@@ -15,10 +15,11 @@
  * @package Piwik_Feedback
  */
 class Piwik_Feedback_Controller extends Piwik_Controller
-{	
+{
 	function index()
-	{		
+	{
 		$view = Piwik_View::factory('index');
+		$view->nonce = Piwik::getNonce('Piwik_Feedback.sendFeedback', 3600);
 		echo $view->render();
 	}
 
@@ -27,16 +28,18 @@ class Piwik_Feedback_Controller extends Piwik_Controller
 	 */
 	function sendFeedback()
 	{
-		$body = Piwik_Common::getRequestVar('body', '', 'string');
 		$email = Piwik_Common::getRequestVar('email', '', 'string');
+		$body = Piwik_Common::getRequestVar('body', '', 'string');
+		$category = Piwik_Common::getRequestVar('category', '', 'string');
+		$nonce = Piwik_Common::getRequestVar('nonce', '', 'string');
 
 		$view = Piwik_View::factory('sent');
-		try 
+		try
 		{
 			$minimumBodyLength = 35;
 			if(strlen($body) < $minimumBodyLength)
 			{
-				throw new Exception(sprintf("Message must be at least %s characters long.", $minimumBodyLength));
+				throw new Exception(Piwik_TranslateException('Feedback_ExceptionBodyLength', array($minimumBodyLength)));
 			}
 			if(!Piwik::isValidEmailString($email))
 			{
@@ -44,14 +47,21 @@ class Piwik_Feedback_Controller extends Piwik_Controller
 			}
 			if(strpos($body, 'http://') !== false)
 			{
-				throw new Exception("The message cannot contain a URL, to avoid spams messages.");
+				throw new Exception(Piwik_TranslateException('Feedback_ExceptionNoUrls'));
 			}
-			
+			if(!Piwik::verifyNonce('Piwik_Feedback.sendFeedback', $nonce))
+			{
+				throw new Exception(Piwik_TranslateException('General_ExceptionNonceMismatch'));
+			}
+
 			$mail = new Piwik_Mail();
-			$mail->setFrom($email);
-			$mail->addTo('hello@piwik.org','Piwik Team');
-			$mail->setSubject('[ Feedback form - Piwik ]');
-			$mail->setBodyText($body);
+			$mail->setFrom(Piwik_Common::unsanitizeInputValue($email));
+			$mail->addTo('hello@piwik.org', 'Piwik Team');
+			$mail->setSubject('[ Feedback form - Piwik ] ' . $category);
+			$mail->setBodyText(Piwik_Common::unsanitizeInputValue($body) . "\n"
+				. 'Piwik ' . Piwik_Version::VERSION . "\n"
+				. 'IP: ' . Piwik_Common::getIpString() . "\n"
+				. 'URL: ' . Piwik_Url::getReferer() . "\n");
 			@$mail->send();
 		}
 		catch(Exception $e)
@@ -59,7 +69,7 @@ class Piwik_Feedback_Controller extends Piwik_Controller
 			$view->ErrorString = $e->getMessage();
 			$view->message = $body;
 		}
-		
+
 		echo $view->render();
 	}
 }
--- plugins/Feedback/Feedback.php
+++ plugins/Feedback/Feedback.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Piwik - Open source web analytics
- * 
+ *
  * @link http://piwik.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html Gpl v3 or later
  * @version $Id$
- * 
+ *
  * @category Piwik_Plugins
  * @package Piwik_Feedback
  */
@@ -30,10 +30,16 @@ class Piwik_Feedback extends Piwik_Plugin
 	function getListHooksRegistered()
 	{
 		return array(
+			'template_css_import' => 'css',
 			'template_js_import' => 'js',
 		);
 	}
-	
+
+	function css()
+	{
+		echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"plugins/Feedback/templates/styles.css\" />\n";
+	}
+
 	function js()
 	{
 		echo "<script type=\"text/javascript\" src=\"plugins/Feedback/templates/feedback.js\"></script>\n";
