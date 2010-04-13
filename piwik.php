0c4913b273df017a64e573820ece269eae75643e
mattpiwik <matthieu.aubry@gmail.com>
2010-04-13 06:32:19 +0000

    Explicitely enable tracking
    git-svn-id: http://dev.piwik.org/svn/trunk@2093 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- piwik.php
+++ piwik.php
@@ -16,7 +16,7 @@ if(file_exists('bootstrap.php'))
 
 $GLOBALS['PIWIK_TRACKER_DEBUG'] = false;
 $GLOBALS['PIWIK_TRACKER_MODE'] = true;
-
+define('PIWIK_ENABLE_TRACKING', true);
 error_reporting(E_ALL|E_NOTICE);
 
 define('PIWIK_DOCUMENT_ROOT', dirname(__FILE__)=='/'?'':dirname(__FILE__));
