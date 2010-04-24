3a556c148c01d9d29431024789dc4e6c90734a37
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2010-04-24 06:04:16 +0000

    refs #2117 - fix a couple of unit tests

--- plugins/SitesManager/tests/SitesManager.test.php
+++ plugins/SitesManager/tests/SitesManager.test.php
@@ -7,7 +7,7 @@ if(!defined('PIWIK_CONFIG_TEST_INCLUDED'))
 	require_once PIWIK_PATH_TEST_TO_ROOT . "/tests/config_test.php";
 }
 
-require_once "Database.test.php";
+require_once PIWIK_PATH_TEST_TO_ROOT . '/tests/core/Database.test.php';
 
 class Test_Piwik_SitesManager extends Test_Database
 {
