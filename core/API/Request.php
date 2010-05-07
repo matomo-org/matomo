f9d8ec5b78b47ece238adf5cedeb4af5f76cbd1c
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2010-05-07 08:57:27 +0000

    Fixes #1260 Widgetize now explicitely loads privileges based on the specified token_auth from the request. If not found, the request will be authenticated using the fallback login mechanism (cookie).

--- core/API/Request.php
+++ core/API/Request.php
@@ -101,13 +101,7 @@ class Piwik_API_Request
 			}
 			$module = "Piwik_" . $module . "_API";
 
-			// if a token_auth is specified in the API request, we load the right permissions
-			$token_auth = Piwik_Common::getRequestVar('token_auth', '', 'string', $this->request);
-			if($token_auth)
-			{
-				Piwik_PostEvent('API.Request.authenticate', $token_auth);
-				Zend_Registry::get('access')->reloadAccess();
-			}
+			self::reloadAuthUsingTokenAuth($this->request);
 			
 			// call the method 
 			$returnedValue = Piwik_API_Proxy::getInstance()->call($module, $method, $this->request);
@@ -119,6 +113,25 @@ class Piwik_API_Request
 		return $toReturn;
 	}
 
+	/**
+	 * If the token_auth is found in the $request parameter, 
+	 * the current session will be authenticated using this token_auth.
+	 * It will overwrite the previous Auth object.
+	 * 
+	 * @param $request If null, uses the default request ($_GET)
+	 * @return void
+	 */
+	static public function reloadAuthUsingTokenAuth($request = null)
+	{
+		// if a token_auth is specified in the API request, we load the right permissions
+		$token_auth = Piwik_Common::getRequestVar('token_auth', '', 'string', $request);
+		if($token_auth)
+		{
+			Piwik_PostEvent('API.Request.authenticate', $token_auth);
+			Zend_Registry::get('access')->reloadAccess();
+		}
+	}
+	
 	/**
 	 * Returns array( $class, $method) from the given string $class.$method
 	 * 
