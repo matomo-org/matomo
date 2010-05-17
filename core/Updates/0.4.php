8c513a90ce378e16829eb645147825284c227952
robocoder <anthon.pang@gmail.com>
2010-05-17 05:30:15 +0000

    remove duplicate ALTER statements
    git-svn-id: http://dev.piwik.org/svn/trunk@2192 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- core/Updates/0.4.php
+++ core/Updates/0.4.php
@@ -18,18 +18,13 @@ class Piwik_Updates_0_4 extends Piwik_Updates
 	static function getSql($adapter = 'PDO_MYSQL')
 	{
 		return array(
+			// 0.4 [1140]
 			'UPDATE `'. Piwik::prefixTable('log_visit') .'`
 				SET location_ip=location_ip+CAST(POW(2,32) AS UNSIGNED) WHERE location_ip < 0' => false,
 			'ALTER TABLE `'. Piwik::prefixTable('log_visit') .'`
 				CHANGE `location_ip` `location_ip` BIGINT UNSIGNED NOT NULL' => false,
 			'UPDATE `'. Piwik::prefixTable('logger_api_call') .'`
 				SET caller_ip=caller_ip+CAST(POW(2,32) AS UNSIGNED) WHERE caller_ip < 0' => false,
-			'ALTER TABLE `'. Piwik::prefixTable('logger_api_call') .'`
-				CHANGE `caller_ip` `caller_ip` BIGINT UNSIGNED' => false,
-
-			// 0.4 [1140]
-			'ALTER TABLE `'. Piwik::prefixTable('log_visit') .'`
-				CHANGE `location_ip` `location_ip` BIGINT UNSIGNED NOT NULL' => false,
 			'ALTER TABLE `'. Piwik::prefixTable('logger_api_call') .'`
 				CHANGE `caller_ip` `caller_ip` BIGINT UNSIGNED' => false,
 		);
