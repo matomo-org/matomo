0459dd7baa8b44a1e78a94b5b3222de733c4be2e
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2010-05-23 22:25:02 +0000

    handle negative Content-Length; in libcurl >= 7.12.1, the curl extension will ignore negative Content-Length

--- core/Http.php
+++ core/Http.php
@@ -330,14 +330,14 @@ class Piwik_Http
 			@fclose($file);
 
 			$fileSize = filesize($destinationPath);
-			if(($contentLength && ($fileLength != $contentLength)) || ($fileSize != $fileLength))
+			if((($contentLength > 0) && ($fileLength != $contentLength)) || ($fileSize != $fileLength))
 			{
 				throw new Exception('File size error: '.$destinationPath.'; expected '.$contentLength.' bytes; received '.$fileLength.' bytes; saved '.$fileSize.' bytes to file');
 			}
 			return true;
 		}
 
-		if($contentLength && ($fileLength != $contentLength))
+		if(($contentLength > 0) && ($fileLength != $contentLength))
 		{
 			throw new Exception('Content length error: expected '.$contentLength.' bytes; received '.$fileLength.' bytes');
 		}
