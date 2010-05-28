6b7ec9a603dddc1e01b7e4484d9264113d0774a6
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2010-05-28 07:19:37 +0000

    Adding tests to check that language files have the right number of parameters $s %1$s etc. in the translation. Disabling the test for now, but in the future will remove invalid strings from the translation files.

--- plugins/LanguagesManager/tests/LanguagesManager.test.php
+++ plugins/LanguagesManager/tests/LanguagesManager.test.php
@@ -19,6 +19,19 @@ class Test_Languages_Manager extends UnitTestCase
 	// test all languages
 	function test_getTranslationsForLanguages()
 	{
+		$englishStrings = Piwik_LanguagesManager_API::getInstance()->getTranslationsForLanguage('en');
+		$englishStringsWithParameters = array();
+		foreach($englishStrings as $englishString)
+		{
+			$stringLabel = $englishString['label'];
+			$stringValue = $englishString['value'];
+			$count = $this->getCountParametersToReplace($stringValue);
+			if($count > 0)
+			{
+				$englishStringsWithParameters[$stringLabel] = $count;
+			}
+		}
+		
 		// we also test that none of the language php files outputs any character on the screen (eg. space before the <?php)
 		$languages = Piwik_LanguagesManager_API::getInstance()->getAvailableLanguages();
 		foreach($languages as $language)
@@ -29,10 +42,36 @@ class Test_Languages_Manager extends UnitTestCase
 			$this->assertTrue(strpos(serialize($strings), "<script") === false, " language file containing javascript");
 			$this->assertTrue(count($strings) > 100); // at least 100 translations in the language file
 			$this->assertTrue(strlen($content) == 0, "buffer was ".strlen($content)." long but should be zero. Translation file for '$language' must be buggy.");
+			
+			// checking that translated strings have the same number of %s as the english source strings
+			foreach($strings as $string)
+			{
+				$stringLabel = $string['label'];
+				$stringValue = $string['value'];
+				
+				if(!empty($stringValue)
+					&& isset($englishStringsWithParameters[$stringLabel]))
+				{
+					$englishParametersCount = $englishStringsWithParameters[$stringLabel];
+					$countTranslation = $this->getCountParametersToReplace($stringValue);
+					//$this->assertEqual($countTranslation, $englishParametersCount,
+					//			"The string $stringLabel has $englishParametersCount parameters in English, but $countTranslation in the language $language.");
+				}
+			}
 		}
 		$this->pass();
 	}
 	
+	private function getCountParametersToReplace($string)
+	{
+		$sprintfParameters = array('%s', '%1$s', '%2$s', '%3$s', '%4$s', '%5$s', '%6$s');
+		$count = 0;
+		foreach($sprintfParameters as $parameter)
+		{
+			$count += substr_count($string, $parameter);
+		}
+		return $count;
+	}
 	//test language when it's not defined
 	function test_getTranslationsForLanguages_not()
 	{
