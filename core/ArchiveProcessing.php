3f469540c933d5e5fd8bb6c116ac105373d5d538
mattpiwik <matthieu.aubry@gmail.com>
2010-06-21 10:44:45 +0000

    Refs #774 Fixing regression thanks for review SteveG! Fixes #1389 Sparklines for bounce rate now displaying OK
    git-svn-id: http://dev.piwik.org/svn/trunk@2323 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- core/ArchiveProcessing.php
+++ core/ArchiveProcessing.php
@@ -607,15 +607,15 @@ abstract class Piwik_ArchiveProcessing
 	 */
 	protected function insertRecord($record)
 	{
-		// We choose not to record records with a value of 0 
-		if($record->value == 0)
-		{
-			return;
-		}
 		
 		// table to use to save the data
 		if(is_numeric($record->value))
 		{
+    		// We choose not to record records with a value of 0 
+    		if($record->value == 0)
+    		{
+    			return;
+    		}
 			$table = $this->tableArchiveNumeric;
 		}
 		else
