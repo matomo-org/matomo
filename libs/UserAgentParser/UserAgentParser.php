6da53af0c5b4d13afb24e7451e86b44bf0bd1a4e
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2010-06-27 05:08:29 +0000

    fix typos; sorry, Matt ;)

--- libs/UserAgentParser/UserAgentParser.php
+++ libs/UserAgentParser/UserAgentParser.php
@@ -18,7 +18,7 @@
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
  *
- * * Neither the name of Matthieu Aubrey nor the names of its contributors
+ * * Neither the name of Matthieu Aubry nor the names of its contributors
  *   may be used to endorse or promote products derived from this
  *   software without specific prior written permission.
  *
