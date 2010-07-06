920f02e3095d653ccedd6de8502d888b50252bbd
sgiehl <stefangiehl@gmail.com>
2010-07-06 21:08:33 +0000

    refs #1464 remove year restriction
    git-svn-id: http://dev.piwik.org/svn/trunk@2439 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- plugins/VisitsSummary/Controller.php
+++ plugins/VisitsSummary/Controller.php
@@ -73,11 +73,8 @@ class Piwik_VisitsSummary_Controller extends Piwik_Controller
 		
 		$dataTableVisit = self::getVisitsSummary();
 		$dataRow = $dataTableVisit->getFirstRow();
-		if($view->period != 'year')
-		{
-			$view->urlSparklineNbUniqVisitors 	= $this->getUrlSparkline( 'getEvolutionGraph', array('columns' => array('nb_uniq_visitors')));
-			$view->nbUniqVisitors = $dataRow->getColumn('nb_uniq_visitors');
-		}
+		$view->urlSparklineNbUniqVisitors 	= $this->getUrlSparkline( 'getEvolutionGraph', array('columns' => array('nb_uniq_visitors')));
+		$view->nbUniqVisitors = $dataRow->getColumn('nb_uniq_visitors');
 		$nbVisits = $dataRow->getColumn('nb_visits');
 		$view->nbVisits = $nbVisits;
 		$view->nbActions = $dataRow->getColumn('nb_actions');
