efadb8dcce793473c30f3c864e18b5f48ecfb1c2
mattpiwik <matthieu.aubry@gmail.com>
2010-07-06 21:48:08 +0000

    Refs #1446  * Adding test covering tracking on multiple websites, test requesting all periods
    git-svn-id: http://dev.piwik.org/svn/trunk@2441 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- tests/core/Url.test.php
+++ tests/core/Url.test.php
@@ -20,24 +20,24 @@ class Test_Piwik_Url extends UnitTestCase
     	$this->assertEqual(Piwik_Url::getCurrentUrl(), Piwik_Url::getCurrentUrlWithoutQueryString());
     	$this->assertEqual(Piwik_Url::getCurrentUrl(), Piwik_Url::getCurrentScheme() . '://' . Piwik_Url::getCurrentHost() . Piwik_Url::getCurrentScriptName() );
     	
+    	print("<br>\nPiwik_Url::getCurrentUrl() -> "
+    				. Piwik_Url::getCurrentUrl());
+    	print("<br>\nPiwik_Url::getCurrentUrlWithoutQueryString() -> "
+    				. Piwik_Url::getCurrentUrlWithoutQueryString());
+    	print("<br>\nPiwik_Url::getCurrentUrlWithoutFileName() -> "
+    				. Piwik_Url::getCurrentUrlWithoutFileName());
+    	print("<br>\nPiwik_Url::getCurrentScriptPath() -> "
+    				. Piwik_Url::getCurrentScriptPath());
+    	print("<br>\nPiwik_Url::getCurrentHost() -> "
+    				. Piwik_Url::getCurrentHost());
+    	print("<br>\nPiwik_Url::getCurrentScriptName() -> "
+    				. Piwik_Url::getCurrentScriptName());
+    	print("<br>\nPiwik_Url::getCurrentQueryString() -> "
+    				. Piwik_Url::getCurrentQueryString());
+    	print("<br>\nPiwik_Url::getArrayFromCurrentQueryString() -> ");
+    	var_dump(Piwik_Url::getArrayFromCurrentQueryString());
     	print("<br>\nPiwik_Url::getCurrentQueryStringWithParametersModified() "
     				. Piwik_Url::getCurrentQueryStringWithParametersModified(array()));
-    	print("<br>\nPiwik_Url::getCurrentUrl() "
-    				. Piwik_Url::getCurrentUrl());
-    	print("<br>\nPiwik_Url::getCurrentUrlWithoutQueryString() "
-    				. Piwik_Url::getCurrentUrlWithoutQueryString());
-    	print("<br>\nPiwik_Url::getCurrentUrlWithoutFileName() "
-    				. Piwik_Url::getCurrentUrlWithoutFileName());
-    	print("<br>\nPiwik_Url::getCurrentScriptName() "
-    				. Piwik_Url::getCurrentScriptName());
-    	print("<br>\nPiwik_Url::getCurrentScriptPath() "
-    				. Piwik_Url::getCurrentScriptPath());
-    	print("<br>\nPiwik_Url::getCurrentHost() "
-    				. Piwik_Url::getCurrentHost());
-    	print("<br>\nPiwik_Url::getCurrentQueryString() "
-    				. Piwik_Url::getCurrentQueryString());
-    	print("<br>\nPiwik_Url::getArrayFromCurrentQueryString() ");
-    	var_dump(Piwik_Url::getArrayFromCurrentQueryString());
     	
         // setting parameter to null should remove it from url
         // test on Url.test.php?test=value
--- tests/integration/Main.test.php
+++ tests/integration/Main.test.php
@@ -25,33 +25,20 @@ require_once PIWIK_INCLUDE_PATH . '/tests/integration/Integration.php';
  */
 class Test_Piwik_Integration_Main extends Test_Integration
 {
+	/*
+	 * Path where expected/processed output files are stored
+	 */
 	public function getPathToTestDirectory()
 	{
 		return PIWIK_INCLUDE_PATH . '/tests/integration';
 	}
 	
-	/**
-	 *  API Tests
-	 * request day/week/month/year for full feature set
-	 * request all formats for full feature set
-	 * request multiple sites
-	 * 
-	 * Fetch API methods, get those get* with idSite, date, period parameters
-	 * Known rules for expanded, idSubtable, Live! parameters, typeReferer 
-	 * Blacklist of API, UsersManager, SitesManager,LanguagesManager
-	 * Force today archive, then make sure all tests use same cached archive.
-	 * Call all API methods, check XML and record new output in directory.
-	 * 
-	 *  Widget Tests
-	 * Loop over all widgets and call with standard parameters. Check HTML output.
-	 * 
-	 */
-	// test periods
-	// test several websites
-
 	/*
-	 * Testing various wrong Tracker requests. 
+	 * Testing various wrong Tracker requests and check that they behave as expected:
+	 * not throwing errors and not recording data.
+	 *  
 	 * API will archive and output empty stats.
+	 * 
 	 */
 	function test_noVisit()
 	{
@@ -59,7 +46,6 @@ class Test_Piwik_Integration_Main extends Test_Integration
 		$idSite = $this->createWebsite($dateTime);
 		
         $t = $this->getTracker($idSite, $dateTime, $defaultInit = true);
-		// TODO Check piwik.php GIF output on all requests
 		
 		// Trigger wrong website
         $trackerWrongWebsite = $this->getTracker($idSiteFake = 33, $dateTime, $defaultInit = true);
@@ -116,15 +102,19 @@ class Test_Piwik_Integration_Main extends Test_Integration
     	Piwik_SitesManager_API::getInstance()->updateSite($idSite, 'new name', null, null, $parameterToExclude . ',anotherParameter');
 
     	// Record 1st page view
-        $t->setUrl( 'http://example.org/index.htm?excluded_Parameter=SHOULD_NOT_DISPLAY?parameter=Should display' );
+    	$urlPage1 = 'http://example.org/index.htm?excluded_Parameter=SHOULD_NOT_DISPLAY?parameter=Should display';
+        $t->setUrl( $urlPage1 );
         $this->checkResponse($t->doTrackPageView( 'incredible title!'));
         
         // Testing that / and index.htm above record with different URLs
         // Recording the 2nd page after 3 minutes
         $t->setForceVisitDateTime(Piwik_Date::factory($dateTime)->addHour(0.05)->getDatetime());
-        $t->setUrl( 'http://example.org/' );
+        $urlPage2 = 'http://example.org/' ;
+        $t->setUrl( $urlPage2 );
+        $t->setUrlReferer($urlPage1);
         $this->checkResponse($t->doTrackPageView( 'Second page view - should be registered as URL /'));
         
+        $t->setUrlReferer($urlPage2);
         // Click on external link after 6 minutes (3rd action)
         $t->setForceVisitDateTime(Piwik_Date::factory($dateTime)->addHour(0.1)->getDatetime());
         $this->checkResponse($t->doTrackAction( 'http://dev.piwik.org/svn', 'link' ));
@@ -161,27 +151,85 @@ class Test_Piwik_Integration_Main extends Test_Integration
         $this->callGetApiCompareOutput(__FUNCTION__, 'xml', $idSite, $dateTime);
 	}
 
+	/*
+	 * Tests Tracker several websites, different days.
+	 * Tests API for period=day/week/month/year, requesting data for both websites, 
+	 * and requesting data for last N periods.
+	 */
+	function test_TwoVisitors_twoWebsites_differentDays()
+	{
+		// Tests run in UTC, the Tracker in UTC
+    	$dateTime = '2010-01-03 11:22:33';
+    	$idSite = $this->createWebsite($dateTime);
+    	$idSite2 = $this->createWebsite($dateTime);
+    	$this->setApiToCall(array('VisitsSummary.get', 'Referers.getWebsites'));
+    	ob_start();
+    	
+    	// -
+    	// First visitor on Idsite 1: one page view
+        $t = $this->getTracker($idSite, $dateTime, $defaultInit = true);
+        $t->setUrlReferer( 'http://referer.com/page.htm?param=valuewith some spaces');
+        $t->setUrl('http://example.org/homepage');
+        $this->checkResponse($t->doTrackPageView(''));
+        
+        // - 
+    	// Second new visitor on Idsite 1: one page view
+    	$t->setIp('1.5.6.8');
+    	$t->setForceVisitDateTime(Piwik_Date::factory($dateTime)->addHour(1)->getDatetime());
+        $t->setUrlReferer( '' );
+    	$t->setUserAgent('Opera/9.63 (Windows NT 5.1; U; en) Presto/2.1.1');
+    	$t->setUrl('http://example.org/products');
+    	$this->checkResponse($t->doTrackPageView('second visitor, first page view'));
+
+    	// -
+    	// Second visitor again on Idsite 1: 2 page views 2 days later
+    	$t->setForceVisitDateTime(Piwik_Date::factory($dateTime)->addHour(48)->getDatetime());
+        $t->setUrlReferer( 'http://referer.com/Other_Page.htm' );
+    	$t->setUrl('http://example.org/purchase');
+    	$t->doTrackPageView('second visitor, two days later a new visit');
+    	$t->setUrl('http://example.org/thankyou');
+    	$this->checkResponse($t->doTrackPageView('second pageview'));
+    	
+    	// -
+    	// First visitor on Idsite 2: one page view, with Website referer
+        $t2 = $this->getTracker($idSite2, Piwik_Date::factory($dateTime)->addHour(24)->getDatetime(), $defaultInit = true);
+        $t2->setUserAgent('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0;)');
+        $t2->setUrlReferer('http://only-homepage-referer.com/');
+        $t2->setUrl('http://example2.com/home');
+        $this->checkResponse($t2->doTrackPageView('Website 2 page view'));
+        
+        // -
+    	// Test Referer.get* methods in XML
+    	$periods = array('day', 'week', 'month', 'year');
+    	// Request data for both websites at once
+    	$idSite = 'all';
+    	// Request data for the last 6 periods
+        $this->callGetApiCompareOutput(__FUNCTION__, 'xml', $idSite = 'all', $dateTime, $periods, $setDateLastN = true);
+	}
 	
 	
 	/**
 	 * To do list (to be converted as a ticket)
-	 * - test std API parameters filter_*
-	 * - report on performance of tracker and API/archiving. Reuse in a load testing suite. 
-	 *   How to report on historical data though? possible with Hudson? 
-	 * - Unit test for Goal API add/update/delete, LanguagesManager, DBStats, SEO.getRank, Live.getLast*
-	 * - Tracker API should forward http headers 'HTTP_CLIENT_IP' 'HTTP_X_FORWARDED_FOR', used in the tracking algorithm
-	 *   which would allow for testing their parsing
-	 * - Tracker test/set/forward request/response cookies  
-	 * - Test API with subtables
+     * - test widgets HTML output?
+	 * - test Std API parameters filter_*
+	 * - Add unit tests for: Goal API add/update/delete, LanguagesManager, DBStats, SEO.getRank, Live.getLast*
+	 * - Call API to fetch custom subtables
 	 *        23 => string 'Piwik_Referers_API.getSearchEnginesFromKeywordId' (length=48)
      *        24 => string 'Piwik_Referers_API.getKeywordsFromSearchEngineId' (length=48)
 	 *        25 => string 'Piwik_Referers_API.getKeywordsFromCampaignId' (length=44)
 	 *        26 => string 'Piwik_Referers_API.getUrlsFromWebsiteId' (length=39)
 	 *  - Test API authentication with token_auth
+	 *  
 	 *  PiwikTracker:
 	 *  - Unknown visitor, detect outlink on one of the alias host -> is it new visit, or request to ignore? probably ignore
-	 *  - API_URL should be set when file is downloaded
 	 *  - set cookies in request
  	 *  - set cookies from response (optional)
+ 	 *  - negative id site should not throw exception
+	 * - Tracker API should forward http headers 'HTTP_CLIENT_IP' 'HTTP_X_FORWARDED_FOR', used in the tracking algorithm
+	 *   which would allow for testing their parsing
+	 *   
+	 *   Later
+	 * - report on performance of tracker and API/archiving. Reuse in a load testing suite. 
+	 *   How to report on historical data though? possible with Hudson? 
 	 */
 }
