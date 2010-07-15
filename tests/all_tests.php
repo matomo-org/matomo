e40689ce0abc762a22c96a77aa1865f794d90f3f
robocoder <anthon.pang@gmail.com>
2010-07-15 09:45:30 +0000

    refs #818 - PATH_INFO and SCRIPT_NAME are also accepted
    git-svn-id: http://dev.piwik.org/svn/trunk@2503 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- tests/all_tests.php
+++ tests/all_tests.php
@@ -37,7 +37,7 @@ foreach(Piwik::globr(PIWIK_INCLUDE_PATH . '/plugins', '*/tests/*.php') as $file)
 array_unshift($toInclude, PIWIK_INCLUDE_PATH . '/tests/core/Database.test.php');
 $toInclude[] = PIWIK_INCLUDE_PATH . '/tests/core/ReleaseCheckList.test.php';
 
-if(isset($_SERVER['REQUEST_URI']) && isset($_SERVER['HTTP_HOST']))
+if((isset($_SERVER['PATH_INFO']) || isset($_SERVER['REQUEST_URI']) || isset($_SERVER['SCRIPT_NAME'])) && isset($_SERVER['HTTP_HOST']))
 {
 	foreach(Piwik::globr(PIWIK_INCLUDE_PATH . '/tests/integration', '*.test.php') as $file)
 	{
