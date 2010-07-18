835be7812a9ed276fd41279f633609c072cb4421
mattpiwik <matthieu.aubry@gmail.com>
2010-07-18 14:39:53 +0000

    git-svn-id: http://dev.piwik.org/svn/trunk@2546 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- misc/api_internal_call.php
+++ misc/api_internal_call.php
@@ -2,15 +2,13 @@
 define('PIWIK_INCLUDE_PATH', '..');
 define('PIWIK_ENABLE_DISPATCH', false);
 define('PIWIK_ENABLE_ERROR_HANDLER', false);
+define('PIWIK_ENABLE_SESSION_START', false);
 require_once PIWIK_INCLUDE_PATH . "/index.php";
 require_once PIWIK_INCLUDE_PATH . "/core/API/Request.php";
 
 Piwik_FrontController::getInstance()->init();
 
-// We call the API from a php code
-// it will check that you have the necessary rights
-// - either you are loggued in piwik and have a cookie in your browser
-// - or you replace the token_auth=xxx to the request string to authenticate
+// This inits the API Request with the specified parameters
 $request = new Piwik_API_Request('
 			method=UserSettings.getResolution
 			&idSite=1
@@ -20,6 +18,7 @@ $request = new Piwik_API_Request('
 			&filter_limit=3
 			&token_auth=anonymous
 ');
+// Calls the API and fetch XML data back
 $result = $request->process();
 echo $result;
 
