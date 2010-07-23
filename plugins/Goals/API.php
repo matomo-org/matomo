ba603c67351452df7092fc49e68ca3691718c96e
mattpiwik <matthieu.aubry@gmail.com>
2010-07-23 19:23:51 +0000

    Fixes issue with Goals API throwing errors when called with lastX
    git-svn-id: http://dev.piwik.org/svn/trunk@2643 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- plugins/Goals/API.php
+++ plugins/Goals/API.php
@@ -165,63 +165,6 @@ class Piwik_Goals_API
 		Piwik_Common::regenerateCacheWebsiteAttributes($idSite);
 	}
 	
-//	public function getConversionsReturningVisitors( $idSite, $period, $date, $idGoal = false )
-//	{
-//		
-//	}
-//	
-//	public function getConversionsNewVisitors( $idSite, $period, $date, $idGoal = false )
-//	{
-//		
-//	}
-	
-	public function getConversionRateReturningVisitors( $idSite, $period, $date, $idGoal = false )
-	{
-		// visits converted for returning for all goals = call Frequency API
-		if($idGoal === false)
-		{
-			$request = new Piwik_API_Request("method=VisitFrequency.getConvertedVisitsReturning&idSite=$idSite&period=$period&date=$date&format=original");
-			$nbVisitsConvertedReturningVisitors = $request->process();
-		}
-		// visits converted for returning = nb conversion for this goal
-		else
-		{
-			$nbVisitsConvertedReturningVisitors = $this->getNumeric($idSite, $period, $date, Piwik_Goals::getRecordName('nb_conversions', $idGoal, 1));
-		}
-		// all returning visits
-		$request = new Piwik_API_Request("method=VisitFrequency.getVisitsReturning&idSite=$idSite&period=$period&date=$date&format=original");
-		$nbVisitsReturning = $request->process();
-//		echo $nbVisitsConvertedReturningVisitors;
-//		echo "<br />". $nbVisitsReturning;exit;
-
-		return Piwik::getPercentageSafe($nbVisitsConvertedReturningVisitors, $nbVisitsReturning, Piwik_Goals::ROUNDING_PRECISION);
-	}
-
-	public function getConversionRateNewVisitors( $idSite, $period, $date, $idGoal = false )
-	{
-		// new visits converted for all goals = nb visits converted - nb visits converted for returning
-		if($idGoal == false)
-		{
-			$request = new Piwik_API_Request("method=VisitsSummary.getVisitsConverted&idSite=$idSite&period=$period&date=$date&format=original");
-			$convertedVisits = $request->process();
-			$request = new Piwik_API_Request("method=VisitFrequency.getConvertedVisitsReturning&idSite=$idSite&period=$period&date=$date&format=original");
-			$convertedReturningVisits = $request->process();
-			$convertedNewVisits = $convertedVisits - $convertedReturningVisits;
-		}
-		// new visits converted for a given goal = nb conversion for this goal for new visits
-		else
-		{
-			$convertedNewVisits = $this->getNumeric($idSite, $period, $date, Piwik_Goals::getRecordName('nb_conversions', $idGoal, 0));
-		}
-		// all new visits = all visits - all returning visits 
-		$request = new Piwik_API_Request("method=VisitFrequency.getVisitsReturning&idSite=$idSite&period=$period&date=$date&format=original");
-		$nbVisitsReturning = $request->process();
-		$request = new Piwik_API_Request("method=VisitsSummary.getVisits&idSite=$idSite&period=$period&date=$date&format=original");
-		$nbVisits = $request->process();
-		$newVisits = $nbVisits - $nbVisitsReturning;
-		return Piwik::getPercentageSafe($convertedNewVisits, $newVisits, Piwik_Goals::ROUNDING_PRECISION);
-	}
-	
 	/**
 	 * Returns Goals data
 	 * 
