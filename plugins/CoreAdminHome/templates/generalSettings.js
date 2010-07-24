12f1638b2c782f2b15709fc6676c46f18d3107b8
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2010-07-24 16:48:45 +0000

    Fixes #1509 - Thanks Jeremy for initial code! Refs #71
    Note: I haven't tested SMTP myself. will require beta tests

--- plugins/CoreAdminHome/templates/generalSettings.js
+++ plugins/CoreAdminHome/templates/generalSettings.js
@@ -10,15 +10,40 @@ function getGeneralSettingsAJAX()
 	request += '&enableBrowserTriggerArchiving='+enableBrowserTriggerArchiving;
 	request += '&todayArchiveTimeToLive='+todayArchiveTimeToLive;
  	request += '&token_auth=' + piwik.token_auth;
+ 	request += '&mailUseSmtp=' + isSmtpEnabled();
+ 	request += '&mailPort=' + $('#mailPort').val();
+ 	request += '&mailHost=' + $('#mailHost').val();
+ 	request += '&mailType=' + $('#mailType').val();
+ 	request += '&mailUsername=' + $('#mailUsername').val();
+ 	request += '&mailPassword=' + $('#mailPassword').val();
 	ajaxRequest.data = request;
 	return ajaxRequest;
 }
+function showSmtpSettings(value)
+{
+	if(value == 1)
+	{
+		$('#smtpSettings').show();
+	}
+	else
+	{
+		$('#smtpSettings').hide();
+	}
+}
+function isSmtpEnabled()
+{
+	return $('input[name="mailUseSmtp"]:checked').attr('value');
+}
 
 $(document).ready( function() {
+	showSmtpSettings(isSmtpEnabled());
 	$('#generalSettingsSubmit').click( function() {
 		$.ajax( getGeneralSettingsAJAX() );
 	});
 
+	$('input[name=mailUseSmtp]').click(function(){
+		 showSmtpSettings($(this).attr('value'));
+	});
 	$('input').keypress( function(e) {
 			var key=e.keyCode || e.which;
 			if (key==13) {
