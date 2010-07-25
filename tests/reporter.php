f326c7ae46eb36d6ea74d7a2b2260198498cf0d5
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2010-07-25 20:36:23 +0000

    fix tags

--- tests/reporter.php
+++ tests/reporter.php
@@ -37,7 +37,7 @@ class HtmlTimerReporter extends HtmlReporter
         print "<strong>" . $this->getPassCount() . "</strong> passes, ";
         print "<strong>" . $this->getFailCount() . "</strong> fails and ";
         print "<strong>" . $this->getExceptionCount() . "</strong> exceptions.";
-        print "<br> ";
+        print "<br/> ";
         print $this->timer;
         print " - 	";
         print $this->timer->getMemoryLeak();
