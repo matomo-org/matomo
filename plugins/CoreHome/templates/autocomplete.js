36d86cf4190ce9efc3acb034dca6a967628809d9
clearcode <mzawadzinski@gmail.com>
2010-07-27 21:50:54 +0000

    fixes #288 added missing autocomplete.js
    git-svn-id: http://dev.piwik.org/svn/trunk@2727 59fd770c-687e-43c8-a1e3-f5a4ff64c105

---
+++ plugins/CoreHome/templates/autocomplete.js
@@ -0,0 +1,78 @@
+
+$('.but').bind('click', function(e)
+{
+	if($('#keyword').val() != '')
+		$('#keyword').autocomplete('search', $('#keyword').val() + '%%%');
+	return false;
+});
+$(function() {
+	$("#keyword").click(function(e)
+	{
+		$(this).val('');
+	});
+	$('#keyword').keyup(function(e)
+	{
+		if(parseInt($(this).val().length) == 0)
+		{
+			reset();
+		}
+	});
+	$("#keyword").autocomplete({
+		minLength: 1,
+		source: '?module=SitesManager&action=getSitesForAutocompleter',
+		select: function(event, ui) {
+			if(piwik.idSite == ui.item.id)
+			{
+				hide();
+				$("#sitesSelectionSearch .custom_select_block").toggleClass("custom_select_block_show");
+			}
+			else
+			{
+				broadcast.propagateNewPage('idSite='+ui.item.id );
+			}
+			return false;
+		},
+		focus: function(event, ui) {
+			$('#keyword').val(ui.item.name);
+			return false;
+		},
+		search: function(event, ui) {
+			$("#reset").show();
+			$("#sitesSelectionSearch .custom_select_main_link").addClass("custom_select_loading");
+		},
+		open: function(event, ui) {
+			widthSitesSelection = $("#sitesSelectionSearch ul").width();
+			$("#sitesSelectionSearch .custom_select_main_link").removeClass("custom_select_loading");
+			if(widthSitesSelection > $('#max_sitename_width').val())
+			{
+				$('#max_sitename_width').val(widthSitesSelection);
+			}
+			else
+			{
+				widthSitesSelection = $('#max_sitename_width').val();
+			}
+
+			$('#custom_select_container').append($('.ui-autocomplete'));
+			$('.custom_select_ul_list').hide();
+			$(".ui-autocomplete").show();
+			$(".ui-autocomplete").css('top', '0px');
+			$(".ui-autocomplete").css('left', '-6px');
+			$(".ui-autocomplete").width(parseInt(widthSitesSelection));
+			$(".custom_select_block_show").width(parseInt(widthSitesSelection));
+
+		}
+	});
+	function reset()
+	{
+		$('#keyword').val('');
+		$('.custom_select_ul_list').show();
+		$(".ui-autocomplete").hide();
+		$("#reset").hide();
+	}
+	$("#reset").click(function(e)
+	{
+		reset();
+	});
+
+});
+		
