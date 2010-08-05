82cff151df5481d948e101ef926ebe5d93779af5
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2010-08-05 13:48:35 +0000

    update URLs

--- misc/redirectToUrl.php
+++ misc/redirectToUrl.php
@@ -2,9 +2,9 @@
 // we redirect to the website instead of linking directly because we don't want
 // to expose the referer on the piwik demo
 $url = htmlentities($_GET['url']);
-if(!preg_match('~http://(dev\.|forum\.)?piwik.org(/|$)~', $url)
+if(!preg_match('~^http://(qa\.|demo\.|dev\.|forum\.)?piwik.org(/|$)~', $url)
 && !in_array($url, array(
-	'http://blogvertising.pl/',
+	'http://clearcode.cc/',
 ))) { die; }
 ?>
 <html><head>
