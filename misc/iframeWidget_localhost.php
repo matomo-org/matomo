34e73cbf3328d75e3204c16a7f027e2ccb06f55e
mattpiwik <matthieu.aubry@gmail.com>
2010-08-06 17:21:34 +0000

    git-svn-id: http://dev.piwik.org/svn/trunk@2871 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- misc/iframeWidget_localhost.php
+++ misc/iframeWidget_localhost.php
@@ -6,7 +6,8 @@ $idSite = 1;
 <html>
 <body>
 <h3 style="color:#143974">Embedding the Piwik Country widget in an Iframe</h3>
+<p>Loads a widget from localhost/trunk/ with login=root, pwd=test</p>
 <div id="widgetIframe"><iframe width="500" height="350" 
-src="http://localhost/trunk/index.php?module=Widgetize&action=iframe&moduleToWidgetize=UserCountry&actionToWidgetize=getCountry&idSite=<?=$idSite;?>&period=<?=$period;?>&date=<?=$date;?>&disableLink=1" scrolling="no" frameborder="0" marginheight="0" marginwidth="0"></iframe></div>
+src="http://localhost/trunk/index.php?token_auth=0b809661490d605bfd77f57ed11f0b14&module=Widgetize&action=iframe&moduleToWidgetize=UserCountry&actionToWidgetize=getCountry&idSite=<?=$idSite;?>&period=<?=$period;?>&date=<?=$date;?>&disableLink=1" scrolling="no" frameborder="0" marginheight="0" marginwidth="0"></iframe></div>
 
 </body></html>
