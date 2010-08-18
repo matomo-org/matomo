0b5a63f711d8b5bc4cf401d86fa7c765f9120284
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2010-08-18 06:50:57 +0000

    refs #1099 - more precise version number for the compatibility test
    This is ok in php 5.3.x's because mysqlnd returns >= 5.0.5 as the client version.

--- core/Db/Adapter/Pdo/Mysql.php
+++ core/Db/Adapter/Pdo/Mysql.php
@@ -83,8 +83,9 @@ class Piwik_Db_Adapter_Pdo_Mysql extends Zend_Db_Adapter_Pdo_Mysql implements Pi
 	{
 		$serverVersion = $this->getServerVersion();
 		$clientVersion = $this->getClientVersion();
-		if(version_compare($serverVersion, '5') >= 0
-			&& version_compare($clientVersion, '5') < 0)
+		// incompatible change to DECIMAL implementation in 5.0.3
+		if(version_compare($serverVersion, '5.0.3') >= 0
+			&& version_compare($clientVersion, '5.0.3') < 0)
 		{
 			throw new Exception(Piwik_TranslateException('General_ExceptionIncompatibleClientServerVersions', array('MySQL', $clientVersion, $serverVersion)));
 		}
