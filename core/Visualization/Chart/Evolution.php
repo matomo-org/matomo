90d0001f2e0114f618a36e049a193971927bf8e2
robocoder <anthon.pang@gmail.com>
2010-08-30 21:18:22 +0000

    fixes #1562
    git-svn-id: http://dev.piwik.org/svn/trunk@3033 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- core/Visualization/Chart/Evolution.php
+++ core/Visualization/Chart/Evolution.php
@@ -60,12 +60,17 @@ class Piwik_Visualization_Chart_Evolution extends Piwik_Visualization_Chart
 			$labelName = $this->yLabels[$dataSetToDisplay];
 			$lineValues = array();
 			$j = 0;
+			$unit = $this->yUnit;
 			foreach($this->xLabels as $label) {
 				$value = (float)$yValues[$j];
+				$whole = (int)$value;
+				if(($value - $whole) >= 0.005)
+				{
+					$value = sprintf('%.2f', $value);
+				}
 				$lineValue = new hollow_dot($value);
-				
-				$unit = $this->yUnit;
 				$lineValue->tooltip("$label<br><b>$value$unit</b> $labelName");
+
 				if(!empty($this->xOnClick))
 				{
 					$lineValue->on_click("piwikHelper.redirectToUrl('".$this->xOnClick[$j]."')");
