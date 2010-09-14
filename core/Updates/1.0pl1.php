50b56958d201beb123f9a1729bef974c9fe265a2
robocoder <anthon.pang@gmail.com>
2010-09-14 21:41:49 +0000

    fixes #1699 - add an update warning if superuser login is no longer a valid format
    git-svn-id: http://dev.piwik.org/svn/trunk@3154 59fd770c-687e-43c8-a1e3-f5a4ff64c105

---
+++ core/Updates/1.0pl1.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ * @version $Id$
+ *
+ * @category Piwik
+ * @package Updates
+ */
+
+/**
+ * @package Updates
+ */
+class Piwik_Updates_1_0pl1 extends Piwik_Updates
+{
+	static function update($schema = 'Myisam')
+	{
+		$rootLogin = Zend_Registry::get('config')->superuser->login;
+		try {
+			// throws an exception if invalid
+			Piwik::checkValidLoginString($rootLogin);
+		} catch(Exception $e) {
+			throw new Exception('Superuser login name "' . $rootLogin . '" is no longer a valid format. '
+						. $e->getMessage()
+						. ' Edit your config/config.ini.php to change it.');
+		}
+	}
+}
