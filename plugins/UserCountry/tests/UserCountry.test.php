a75df1a5e2319c6c05f9b0c1eb33a99235006bb4
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2010-09-24 18:49:23 +0000

    fixes #1726 - add Antarctica and Central America; use MaxMind's country-to-continent mapping (with the exception of Central America which it groups with North America).

--- plugins/UserCountry/tests/UserCountry.test.php
+++ plugins/UserCountry/tests/UserCountry.test.php
@@ -19,5 +19,38 @@ class Test_Piwik_UserCountry extends UnitTestCase
 		$flag = Piwik_getFlagFromCode("foo");
 		$this->assertEqual( basename($flag), "xx.png" );
 	}
+
+	public function test_flagsAndContinents()
+	{
+		require_once PIWIK_PATH_TEST_TO_ROOT . '/core/DataFiles/Countries.php';
+
+		$continents = array('unk', 'afr', 'amn', 'amc', 'ams', 'ant', 'asi', 'eur', 'oce');
+
+		// Get list of existing flag icons
+		$flags = scandir(PIWIK_PATH_TEST_TO_ROOT . '/plugins/UserCountry/flags/');
+
+		// Get list of countries
+		foreach($GLOBALS['Piwik_CountryList'] as $country => $continent)
+		{
+			// test continent
+			$this->assertTrue(in_array($continent, $continents), "$country => $continent");
+
+			// test flag
+			$this->assertTrue(in_array($country . '.png', $flags), $country);
+		}
+
+		foreach($flags as $filename)
+		{
+			if($filename == '.' || $filename == '..' || $filename == '.svn')
+			{
+				continue;
+			}
+
+			$country = substr($filename, 0, strpos($filename, '.png'));
+
+			// test country
+			$this->assertTrue(array_key_exists($country, $GLOBALS['Piwik_CountryList']), $filename);
+		}
+	}
 }
 
