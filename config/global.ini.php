f96c82c893177d55cf70cf2d93f69009decbedb6
robocoder <anthon.pang@gmail.com>
2010-09-25 04:03:58 +0000

    fixes #1728
    git-svn-id: http://dev.piwik.org/svn/trunk@3177 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- config/global.ini.php
+++ config/global.ini.php
@@ -161,7 +161,7 @@ use_ajax_cdn = 0
 
 ; required AJAX library versions
 jquery_version = 1.4.2
-jqueryui_version = 1.8.4
+jqueryui_version = 1.8.5
 swfobject_version = 2.2
 
 ; If set to 0, Flash widgets require separate HTTP requests
