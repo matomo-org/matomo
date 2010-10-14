2b4d28b92e9d7a2521c21df5b615a2186c5b66f8
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2010-10-14 09:01:17 +0000

    fixes #1768

--- js/piwik.js
+++ js/piwik.js
@@ -1,8 +1,11 @@
 /*!
  * Piwik - Web Analytics
  *
+ * JavaScript tracking client
+ *
  * @link http://piwik.org
- * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ * @source http://dev.piwik.org/trac/browser/trunk/js/piwik.js
+ * @license http://www.opensource.org/licenses/bsd-license.php Simplified BSD
  * @version $Id$
  */
 
