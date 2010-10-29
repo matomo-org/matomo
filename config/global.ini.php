9bed82f3ddffdced928379b86566b58443a0329a
robocoder <anthon.pang@gmail.com>
2010-10-29 14:46:28 +0000

    refs #1014
    git-svn-id: http://dev.piwik.org/svn/trunk@3271 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- config/global.ini.php
+++ config/global.ini.php
@@ -295,7 +295,6 @@ error_reporting = E_ALL|E_NOTICE
 Plugins[] 		= CorePluginsAdmin
 Plugins[] 		= CoreAdminHome
 Plugins[] 		= CoreHome
-Plugins[]		= Proxy
 Plugins[] 		= API
 Plugins[] 		= Widgetize
 Plugins[] 		= LanguagesManager
