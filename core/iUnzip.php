7e4c8766fde32474a4a46c378e6dcb4109443bf7
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2010-11-13 04:26:27 +0000

    fixes #1814

---
+++ core/iUnzip.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ * 
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ * @version $Id$
+ *
+ * @category Piwik
+ * @package Piwik
+ */
+
+/**
+ * Unzip interface
+ *
+ * @package Piwik
+ */
+interface Piwik_iUnzip
+{
+	/**
+	 * Constructor
+	 *
+	 * @param string $filename Name of the .zip archive
+	 */
+	function __construct($filename);
+
+	/**
+	 * Extract files from archive to target directory
+	 *
+	 * @param string $pathExtracted Absolute path of target directory
+	 * @return mixed Array of filenames if successful; or 0 if an error occurred
+	 */
+	public function extract($pathExtracted);
+
+	/**
+	 * Get error status string for the latest error
+	 *
+	 * @return string
+	 */
+	public function errorInfo();
+}
---
+++ tests/core/Unzip.test.php
@@ -0,0 +1,108 @@
+<?php
+if(!defined('PIWIK_CONFIG_TEST_INCLUDED'))
+{
+	require_once dirname(__FILE__)."/../../tests/config_test.php";
+}
+
+class Test_Piwik_Unzip extends UnitTestCase
+{
+    public function test_relativePath()
+    {
+		if(defined('ZIPARCHIVE::ER_OK'))
+		{
+			$test = 'relative';
+			$filename = dirname(__FILE__) . '/' . $test . '.zip';
+			$zip = new ZipArchive;
+			$zip->open($filename, ZIPARCHIVE::OVERWRITE);
+			$zip->addFromString($test .'.txt', 'test');
+			$zip->close();
+			$this->assertTrue(file_exists($filename));
+
+			$extractDir = PIWIK_USER_PATH . '/tmp/latest/';
+
+			$unzip = new Piwik_Unzip_ZipArchive($filename);
+			$res = $unzip->extract($extractDir);
+			$this->assertEqual(count($res), 1);
+			$this->assertTrue(file_exists($extractDir . $test . '.txt'));
+			$this->assertFalse(file_exists(dirname(__FILE__) . '/' . $test . '.txt'));
+			$this->assertFalse(file_exists(dirname(__FILE__) . '/../../tests/' . $test . '.txt'));
+			unlink($extractDir . $test . '.txt');
+
+			$unzip = new Piwik_Unzip_PclZip($filename);
+			$res = $unzip->extract($extractDir);
+			$this->assertEqual(count($res), 1);
+			$this->assertTrue(file_exists($extractDir . $test . '.txt'));
+			$this->assertFalse(file_exists(dirname(__FILE__) . '/' . $test . '.txt'));
+			$this->assertFalse(file_exists(dirname(__FILE__) . '/../../tests/' . $test . '.txt'));
+			unlink($extractDir . $test . '.txt');
+
+			unlink($filename);
+		}
+	}
+
+    public function test_relativePathAttack()
+    {
+		if(defined('ZIPARCHIVE::ER_OK'))
+		{
+			$test = 'zaatt';
+			$filename = dirname(__FILE__) . '/' . $test . '.zip';
+			$zip = new ZipArchive;
+			$zip->open($filename, ZIPARCHIVE::OVERWRITE);
+			$zip->addFromString('../' . $test .'.txt', 'test');
+			$zip->close();
+			$this->assertTrue(file_exists($filename));
+
+			$extractDir = PIWIK_USER_PATH . '/tmp/latest/';
+
+			$unzip = new Piwik_Unzip_ZipArchive($filename);
+			$res = $unzip->extract($extractDir);
+			$this->assertEqual($res, 0);
+			$this->assertFalse(file_exists($extractDir . $test . '.txt'));
+			$this->assertFalse(file_exists($extractDir . '../' . $test . '.txt'));
+			$this->assertFalse(file_exists(dirname(__FILE__) . '/' . $test . '.txt'));
+			$this->assertFalse(file_exists(dirname(__FILE__) . '/../' . $test . '.txt'));
+			$this->assertFalse(file_exists(dirname(__FILE__) . '/../../' . $test . '.txt'));
+
+			$unzip = new Piwik_Unzip_PclZip($filename);
+			$res = $unzip->extract($extractDir);
+			$this->assertEqual($res, 0);
+			$this->assertFalse(file_exists($extractDir . $test . '.txt'));
+			$this->assertFalse(file_exists($extractDir . '../' . $test . '.txt'));
+			$this->assertFalse(file_exists(dirname(__FILE__) . '/' . $test . '.txt'));
+			$this->assertFalse(file_exists(dirname(__FILE__) . '/../' . $test . '.txt'));
+			$this->assertFalse(file_exists(dirname(__FILE__) . '/../../' . $test . '.txt'));
+
+			unlink($filename);
+		}
+	}
+
+    public function test_absolutePathAttack()
+    {
+		if(defined('ZIPARCHIVE::ER_OK'))
+		{
+			$test = 'zaabs';
+			$filename = dirname(__FILE__) . '/' . $test . '.zip';
+			$zip = new ZipArchive;
+			$zip->open($filename, ZIPARCHIVE::OVERWRITE);
+			$zip->addFromString(dirname(__FILE__) . '/' . $test .'.txt', 'test');
+			$zip->close();
+			$this->assertTrue(file_exists($filename));
+
+			$extractDir = PIWIK_USER_PATH . '/tmp/latest/';
+
+			$unzip = new Piwik_Unzip_ZipArchive($filename);
+			$res = $unzip->extract($extractDir);
+			$this->assertEqual($res, 0);
+			$this->assertFalse(file_exists($extractDir . $test . '.txt'));
+			$this->assertFalse(file_exists(dirname(__FILE__) . '/' . $test . '.txt'));
+
+			$unzip = new Piwik_Unzip_PclZip($filename);
+			$res = $unzip->extract($extractDir);
+			$this->assertEqual($res, 0);
+			$this->assertFalse(file_exists($extractDir . $test . '.txt'));
+			$this->assertFalse(file_exists(dirname(__FILE__) . '/' . $test . '.txt'));
+
+			unlink($filename);
+		}
+	}
+}
