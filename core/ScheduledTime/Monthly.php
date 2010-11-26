77ab07efc054c72b9be95d3a4cdd4c6d279233b3
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2010-11-26 07:17:07 +0000

    typo

--- core/ScheduledTime/Monthly.php
+++ core/ScheduledTime/Monthly.php
@@ -33,7 +33,7 @@ class Piwik_ScheduledTime_Monthly extends Piwik_ScheduledTime
 									date('Y', $currentTime)
 									);
 
-		$nextMonthLength = date('t', $rescheduledTimeU);
+		$nextMonthLength = date('t', $rescheduledTime);
 
 		// Sets scheduled day
 		if ( $this->day !== null )
