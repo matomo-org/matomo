5f6947d53ebfcd108f439519f6b1167e00a65b3a
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2010-12-13 05:21:52 +0000

    refs #1470 - add nightly build script; tweak the webtest build script

--- tests/build-nightly.sh
+++ tests/build-nightly.sh
@@ -10,26 +10,16 @@ function cleanup() {
 }
 
 #
-# Check build environment
-#
-
-if [ ! -e "${WORKSPACE}/build/core/Version.php" ]; then
-	echo "Piwik source files not present!"
-	exit 2;
-fi
-
-#
 # Clean up build environment
 #
 cleanup
-rm -rf 1.0
 rm -f latest.zip
 
 #
 # Package into a release
 #
 
-cp -R build piwik
+svn export http://dev.piwik.org/svn/trunk piwik
 rm -rf piwik/tmp/*
 rm -f piwik/misc/db-schema*
 rm -f piwik/misc/diagram_general_request*
@@ -45,25 +35,5 @@ cp piwik/misc/WebAppGallery/*.xml .
 find piwik -type f -printf '%s ' -exec md5sum {} \; | fgrep -v 'manifest.inc.php' | sed '1,$ s/\([0-9]*\) \([a-z0-9]*\) *piwik\/\(.*\)/\t\t"\3" => array("\1", "\2"),/; 1 s/^/<?php\nclass Manifest {\n\tstatic $files=array(\n/; $ s/$/\n\t);\n}/' > piwik/config/manifest.inc.php
 
 zip -q -r latest.zip piwik How\ to\ install\ Piwik.html *.xml > /dev/null 2> /dev/null
-
-#
-# Set-up infrastructure proxies for testing
-#
-
-VERSION=`fgrep VERSION build/core/Version.php  | sed -e "s/\tconst VERSION = '//" | sed -e "s/'.*//"`
-
-mkdir 1.0
-
-mkdir 1.0/getLatestVersion
-cat >1.0/getLatestVersion/index.php <<GET_LATEST_VERSION
-<?php
-	echo "${VERSION}";
-GET_LATEST_VERSION
-
-mkdir 1.0/subscribeNewsletter
-cat >1.0/subscribeNewsletter/index.php <<SUBSCRIBE_NEWSLETTER
-<?php
-	echo "ok";
-SUBSCRIBE_NEWSLETTER
 
 cleanup
