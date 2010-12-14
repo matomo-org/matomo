545a653b62dd51219f2b25a8909c4301bc14a877
robocoder <anthon.pang@gmail.com>
2010-12-14 00:39:19 +0000

    refs #1885 - add install.sql per Pete Harris @ Microsoft's recommendations
    git-svn-id: http://dev.piwik.org/svn/trunk@3438 59fd770c-687e-43c8-a1e3-f5a4ff64c105

---
+++ misc/WebAppGallery/install.sql
@@ -0,0 +1,15 @@
+USE PlaceholderForDbName;
+
+DROP PROCEDURE IF EXISTS add_user ;
+
+CREATE PROCEDURE add_user()
+BEGIN
+DECLARE EXIT HANDLER FOR 1044 BEGIN END;
+GRANT ALL PRIVILEGES ON PlaceholderForDbName.* to 'PlaceholderForDbUser'@'PlaceholderForServer' IDENTIFIED BY 'PlaceholderForDbPassword';
+FLUSH PRIVILEGES;
+END
+//
+
+CALL add_user() //
+
+DROP PROCEDURE IF EXISTS add_user //
