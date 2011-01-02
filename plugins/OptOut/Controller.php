6775ad8ddee04c76124b3839f1d3543c4793d695
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2011-01-02 05:59:53 +0000

    fixes #1929 - thanks halfdan; more refactoring

--- plugins/OptOut/Controller.php
+++ plugins/OptOut/Controller.php
@@ -17,31 +17,26 @@
  */
 class Piwik_OptOut_Controller extends Piwik_Controller
 {
-    /**
-     * Shows the "Track Visits" checkbox.
-     */
-    public function index()
-    {
-        $view = Piwik_View::factory('CheckBox');
-        $view->trackVisits = !Piwik_Tracker_Cookie::isIgnoreCookieFound();
-        $view->nonce = Piwik_Nonce::getNonce('Piwik_OptOut', 3600);
-        echo $view->render();
-    }
+	/**
+	 * Shows the "Track Visits" checkbox.
+	 */
+	public function index()
+	{
+		$trackVisits = !Piwik_Tracker_Cookie::isIgnoreCookieFound();
 
-    /**
-     * Public interface of the controller to change
-     * the status. Checks the nonce for correctness.
-     */
-    public function changeStatus()
-    {
-        $trackVisits = Piwik_Common::getRequestVar('trackVisits', false);
-        $nonce = Piwik_Common::getRequestVar('nonce');
-
-        if (Piwik_Nonce::verifyNonce('Piwik_OptOut', $nonce)) {
+		$nonce = Piwik_Common::getRequestVar('nonce', false);
+		if($nonce !== false && Piwik_Nonce::verifyNonce('Piwik_OptOut', $nonce))
+		{
+			// toggle setting
 			Piwik_Tracker_Cookie::setIgnoreCookie();
-        } else {
-            throw new Exception(Piwik_Translate('OptOut_WrongNonce'));
-        }
-        Piwik::redirectToModule('OptOut', 'index');
-    }
+			$trackVisits = !$trackVisits;
+		}
+
+		$view = Piwik_View::factory('index');
+		$view->control = Piwik_Common::getRequestVar('control', 'checkbox');
+		$view->css = Piwik_Common::getRequestVar('css', false);
+		$view->trackVisits = $trackVisits;
+		$view->nonce = Piwik_Nonce::getNonce('Piwik_OptOut', 3600);
+		echo $view->render();
+	}
 }
