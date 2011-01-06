ec6d8e60d1bc4f9b0530adba17613cb96b73f97c
robocoder <anthon.pang@gmail.com>
2011-01-06 05:10:03 +0000

    refs #1897, refs #1945 - yank this update code ... that's not the desired effect
    git-svn-id: http://dev.piwik.org/svn/trunk@3643 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- core/Updates/1.1.php
+++ core/Updates/1.1.php
@@ -28,35 +28,5 @@ class Piwik_Updates_1_1 extends Piwik_Updates
 						. $e->getMessage()
 						. ' Edit your config/config.ini.php to change it.');
 		}
-
-		// Warning!
-		// This code below will copy in the config.ini.php ALL the values found in the 
-		// General section (including the ones in global.ini.php). This is not desired!
-		// We leave this code as is since this migration does impact a fraction of users
-		// Do not reuse this code for future migration of config file values.
-		$generalInfo = $config->General->toArray();
-		if(!isset($generalInfo['proxy_client_headers']) && count($headers = Piwik_ProxyHeaders::getProxyClientHeaders()) > 0)
-		{
-			$generalInfo['proxy_client_headers'] = $headers;
-		}
-		if(!isset($generalInfo['proxy_host_headers']) && count($headers = Piwik_ProxyHeaders::getProxyHostHeaders()) > 0)
-		{
-			$generalInfo['proxy_host_headers'] = $headers;
-		}
-		if(isset($headers))
-		{
-			if(is_writable( Piwik_Config::getDefaultUserConfigPath() ))
-			{
-				$config->General = $generalInfo;
-				$config->__destruct();
-				Piwik::createConfigObject();
-			}
-			else
-			{
-				throw new Exception('You appear to be using a proxy server.  Edit your config/config.ini.php to configure proxy_client_headers[] and/or proxy_host_headers[].');
-			}
-		}
-
-		Piwik_Updater::updateDatabase(__FILE__, self::getSql());
 	}
 }
