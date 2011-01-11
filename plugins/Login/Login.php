18c229b5289a3ef943c26beaafc59764864cb5d9
robocoder <anthon.pang@gmail.com>
2011-01-11 22:17:07 +0000

    delete invalid cookies, refs #1958
    git-svn-id: http://dev.piwik.org/svn/trunk@3709 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- plugins/Login/Login.php
+++ plugins/Login/Login.php
@@ -118,15 +118,18 @@ class Piwik_Login extends Piwik_Plugin
 		$auth->setLogin($login);
 		$auth->setTokenAuth($tokenAuth);
 		$authResult = $auth->authenticate();
-		if(!$authResult->isValid())
-		{
-			throw new Exception(Piwik_Translate('Login_LoginPasswordNotCorrect'));
-		}
 
 		$authCookieName = Zend_Registry::get('config')->General->login_cookie_name;
 		$authCookieExpiry = $rememberMe ? time() + Zend_Registry::get('config')->General->login_cookie_expire : 0;
 		$authCookiePath = Zend_Registry::get('config')->General->login_cookie_path;
 		$cookie = new Piwik_Cookie($authCookieName, $authCookieExpiry, $authCookiePath);
+		if(!$authResult->isValid())
+
+		{
+			$cookie->delete();
+			throw new Exception(Piwik_Translate('Login_LoginPasswordNotCorrect'));
+		}
+
 		$cookie->set('login', $login);
 		$cookie->set('token_auth', $auth->getHashTokenAuth($login, $authResult->getTokenAuth()));
 		$cookie->setSecure(Piwik::isHttps());
