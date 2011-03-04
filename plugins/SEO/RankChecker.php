aab9fb2aed7ae73221788da72605bb92bd61cd20
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2011-03-04 16:28:41 +0000

    refs #1459 - domain age parser improvement

--- plugins/SEO/RankChecker.php
+++ plugins/SEO/RankChecker.php
@@ -111,7 +111,7 @@ class Piwik_SEO_RankChecker
 		$url = preg_replace('/^www\./', '', $this->url);
 		$url = 'http://www.who.is/whois/'.urlencode($url);
 		$data = $this->getPage($url);
-		preg_match('#Creation Date:\s+([a-z0-9/-]+)#si', $data, $p);
+		preg_match('#(?:Creation Date|Created On):\s*([a-z0-9/-]+)#si', $data, $p);
 		if(!isset($p[1]))
 		{
 			return null;
