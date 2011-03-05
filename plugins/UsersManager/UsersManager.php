2289ac9a20808a9e79aefa9084c3973e3d420bab
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2011-03-05 18:34:55 +0000

    fixes #2148, refs #585

--- plugins/UsersManager/UsersManager.php
+++ plugins/UsersManager/UsersManager.php
@@ -11,11 +11,19 @@
  */
 
 /**
- * 
+ * Manage Piwik users
+ *
  * @package Piwik_UsersManager
  */
 class Piwik_UsersManager extends Piwik_Plugin
-{		
+{
+	/**
+	 * Plugin information
+	 *
+	 * @see Piwik_Plugin
+	 *
+	 * @return array
+	 */
 	public function getInformation()
 	{
 		$info = array(
@@ -24,36 +32,67 @@ class Piwik_UsersManager extends Piwik_Plugin
 			'author_homepage' => 'http://piwik.org/',
 			'version' => Piwik_Version::VERSION,
 		);
-		
+
 		return $info;
 	}
-	
+
+	/**
+	 * Get list of hooks to register.
+	 *
+	 * @see Piwik_PluginsManager.loadPlugin()
+	 *
+	 * @return array
+	 */
 	function getListHooksRegistered()
 	{
 		return array(
 				'AdminMenu.add' => 'addMenu',
-				'AssetManager.getJsFiles' => 'getJsFiles'
+				'AssetManager.getJsFiles' => 'getJsFiles',
+				'SitesManager.deleteSite' => 'deleteSite',
 		);
 	}
-	
+
+	/**
+	 * Delete user preferences associated with a particular site
+	 *
+	 * @param Event_Notification $notification
+	 */
+	function deleteSite( $notification )
+	{
+		$idSite = &$notification->getNotificationObject();
+
+		Piwik_Option::getInstance()->deleteLike('%\_'.Piwik_UsersManager_API::PREFERENCE_DEFAULT_REPORT, $idSite);
+	}
+
+	/**
+	 * Return list of plug-in specific JavaScript files to be imported by the asset manager
+	 *
+	 * @see Piwik_AssetManager
+	 *
+	 * @param Event_Notification $notification
+	 */
 	function getJsFiles( $notification )
 	{
 		$jsFiles = &$notification->getNotificationObject();
-		
-		$jsFiles[] = "plugins/UsersManager/templates/UsersManager.js";	
+
+		$jsFiles[] = "plugins/UsersManager/templates/UsersManager.js";
 		$jsFiles[] = "plugins/UsersManager/templates/userSettings.js";
 	}
-	
+
+	/**
+	 * Add admin menu items
+	 *
+	 * @param Event_Notification $notification (not used)
+	 */
 	function addMenu()
 	{
 		Piwik_AddAdminMenu('UsersManager_MenuUsers', 
 							array('module' => 'UsersManager', 'action' => 'index'),
 							Piwik::isUserHasSomeAdminAccess(),
-							$order = 3);		
+							$order = 3);
 		Piwik_AddAdminMenu('UsersManager_MenuUserSettings', 
 							array('module' => 'UsersManager', 'action' => 'userSettings'),
 							Piwik::isUserHasSomeViewAccess(),
-							$order = 1);		
+							$order = 1);
 	}
 }
-
