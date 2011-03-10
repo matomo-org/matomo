53f264a8e44e349799bb438cfbc036a6429f9c46
sgiehl <stefangiehl@gmail.com>
2011-03-10 22:13:12 +0000

    auto expand dashboard widget if extended datatable or goal view is opened. close dialog on click on grey layer
    git-svn-id: http://dev.piwik.org/svn/trunk@4060 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- plugins/Dashboard/templates/Dashboard.js
+++ plugins/Dashboard/templates/Dashboard.js
@@ -232,8 +232,14 @@ dashboard.prototype =
 				mydialog.dialog("destroy");
 				$('#placeholder').replaceWith(mydialog);
 				mydialog.removeAttr('style');
+				self.saveLayout();
 			}
 		});
+        $('body').click(function(ev) {
+            if(ev.target.className == "ui-widget-overlay") {
+                mydialog.dialog("close");
+            }
+        });
 	},
 	
 	
@@ -259,7 +265,12 @@ dashboard.prototype =
 	setDataTableViewChanged: function(uniqueId, newViewDataTable)
 	{
 		this.viewDataTableToSave[uniqueId] = newViewDataTable;
-		this.saveLayout();
+		if(newViewDataTable == 'tableAllColumns' || newViewDataTable == 'tableGoals') {
+		    $('#maximise', $('#'+uniqueId)).click();
+		}
+		if(!this.isMaximised) {
+		    this.saveLayout();
+		}
 	},
 	
 	saveLayout: function()
