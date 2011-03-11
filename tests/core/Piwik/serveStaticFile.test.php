97d1303fd7d9790e918ec33c631e88ebbcc46fdd
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2011-03-11 22:34:49 +0000

    refs #2177 - typo

--- tests/core/Piwik/serveStaticFile.test.php
+++ tests/core/Piwik/serveStaticFile.test.php
@@ -17,7 +17,7 @@ if(!defined("PIWIK_PATH_TEST_TO_ROOT")) {
 	define('PIWIK_PATH_TEST_TO_ROOT', realpath(dirname(__FILE__) . '/../../..'));
 }
 
-require_once PIWIK_PATH_TO_TEST_TO_ROOT . '/core/testMinimumPhpVersion.php';
+require_once PIWIK_PATH_TEST_TO_ROOT . '/core/testMinimumPhpVersion.php';
 
 // This is Piwik logo, the static file used in this test suit
 define("TEST_FILE_LOCATION", PIWIK_PATH_TEST_TO_ROOT . "/tests/core/Piwik/lipsum.txt");
