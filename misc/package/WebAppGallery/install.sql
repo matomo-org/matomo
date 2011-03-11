f60badc0307c5741d2ef161614cc62acb4f981a9
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2011-03-11 18:42:24 +0000

    refs #2176 - update WAG config

--- misc/package/WebAppGallery/install.sql
+++ misc/package/WebAppGallery/install.sql
@@ -5,7 +5,7 @@ DROP PROCEDURE IF EXISTS add_user ;
 CREATE PROCEDURE add_user()
 BEGIN
 DECLARE EXIT HANDLER FOR 1044 BEGIN END;
-GRANT ALL PRIVILEGES ON PlaceholderForDbName.* to 'PlaceholderForDbUser'@'PlaceholderForServer' IDENTIFIED BY 'PlaceholderForDbPassword';
+GRANT ALL PRIVILEGES ON PlaceholderForDbName.* to 'PlaceholderForDbUsername'@'PlaceholderForServer' IDENTIFIED BY 'PlaceholderForDbPassword';
 FLUSH PRIVILEGES;
 END
 //
