93cd842b6eff166300041890604d580c5fd0b95d
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2011-03-17 02:14:31 +0000

    refs #1728 - jqueryUI 1.8.10

--- config/global.ini.php
+++ config/global.ini.php
@@ -189,7 +189,7 @@ use_ajax_cdn = 0
 
 ; required AJAX library versions
 jquery_version = 1.5.1
-jqueryui_version = 1.8.9
+jqueryui_version = 1.8.10
 swfobject_version = 2.2
 
 ; Set to 1 if you're using https on your Piwik server and Piwik can't detect it,
