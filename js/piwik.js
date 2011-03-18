2d4d0436d9b5dc047495357776e5f097f25b3849
robocoder <anthon.pang@gmail.com>
2011-03-18 14:38:08 +0000

    fixes #2203
    git-svn-id: http://dev.piwik.org/svn/trunk@4127 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- js/piwik.js
+++ js/piwik.js
@@ -81,7 +81,9 @@ if (!this.JSON2) {
     }
 
     var cx = new RegExp('[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]', 'g'),
-        escapable = new RegExp('[\\\\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]', 'g'),
+	// hack: workaround Snort false positive (sid 8443)
+        pattern = '\\\\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]',
+        escapable = new RegExp('[' + pattern, 'g'),
         gap,
         indent,
         meta = {    // table of character substitutions
