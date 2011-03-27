4ac09a49a093dec5a59df4f6406c6bda22bc6328
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2011-03-27 02:45:11 +0000

    Fixes #2220  * Dropping unused INDEX index_idsite_idvisit (the primary key will do just as good, no need for this INDEX)  * Dropping bad INDEX & adding proper one, as per discussion in ticket

---
+++ core/Updates/1.2.3.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ * @version $Id$
+ *
+ * @category Piwik
+ * @package Updates
+ */
+
+/**
+ * @package Updates
+ */
+class Piwik_Updates_1_2_3 extends Piwik_Updates
+{
+	static function getSql($schema = 'Myisam')
+	{
+		return array(
+			// Various performance improvements schema updates
+		    'ALTER TABLE `'. Piwik_Common::prefixTable('log_visit') .'` 
+				DROP INDEX index_idsite_datetime_config,
+				DROP INDEX index_idsite_idvisit,
+				ADD INDEX index_idsite_config_datetime (idsite, config_id, visit_last_action_time),
+				ADD INDEX index_idsite_datetime (idsite, visit_last_action_time)' => false,
+		);
+	}
+
+	static function update()
+	{
+		Piwik_Updater::updateDatabase(__FILE__, self::getSql());
+	}
+}
+
