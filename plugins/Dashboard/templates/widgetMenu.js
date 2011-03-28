582a35dde266f8edd620fcd3795260f9e45ffa31
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2011-03-28 22:25:06 +0000

    Refs #572 Fixing Widgetize screen with Custom Date Range. All known bug fixed! RC release soon?

--- plugins/Dashboard/templates/widgetMenu.js
+++ plugins/Dashboard/templates/widgetMenu.js
@@ -57,7 +57,7 @@ widgetsHelper.getLoadWidgetAjaxRequest = function (widgetUniqueId, widgetParamet
 		async: true,
 		error: piwikHelper.ajaxHandleError,		
 		success: onWidgetLoadedCallback,
-		data: piwikHelper.getQueryStringFromParameters(widgetParameters) + "&idSite="+piwik.idSite+"&period="+piwik.period+"&date="+broadcast.getValueFromHash('date')
+		data: piwikHelper.getQueryStringFromParameters(widgetParameters) + "&idSite="+piwik.idSite+"&period="+piwik.period+"&date="+broadcast.getValueFromUrl('date')
 	};
 	return ajaxRequest;
 };
