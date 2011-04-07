e3dca49cb0b7b53e6aaa2018383065a4ca78d8e5
matt <matt@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2011-04-07 20:34:31 +0000

    Now that Live! is working fine, we might as well make it very visible on the default dashboard

--- plugins/Dashboard/Controller.php
+++ plugins/Dashboard/Controller.php
@@ -18,6 +18,7 @@ class Piwik_Dashboard_Controller extends Piwik_Controller
 {
 	protected function getDashboardView($template)
 	{
+//		echo '';exit; //DEBUG do not load dashboard
 		$view = Piwik_View::factory($template);
 		$this->setGeneralVariablesView($view);
 
@@ -180,8 +181,8 @@ class Piwik_Dashboard_Controller extends Piwik_Controller
 		$defaultLayout = '[
     		[
     			{"uniqueId":"widgetVisitsSummarygetEvolutionGraphcolumnsArray","parameters":{"module":"VisitsSummary","action":"getEvolutionGraph","columns":"nb_visits"}},
+    			{"uniqueId":"widgetLivewidget","parameters":{"module":"Live","action":"widget"}},
     			{"uniqueId":"widgetVisitorInterestgetNumberOfVisitsPerVisitDuration","parameters":{"module":"VisitorInterest","action":"getNumberOfVisitsPerVisitDuration"}},
-    			{"uniqueId":"widgetUserSettingsgetBrowser","parameters":{"module":"UserSettings","action":"getBrowser"}},
     			{"uniqueId":"widgetExampleFeedburnerfeedburner","parameters":{"module":"ExampleFeedburner","action":"feedburner"}}
     		],
     		[
@@ -190,7 +191,7 @@ class Piwik_Dashboard_Controller extends Piwik_Controller
     		],
     		[
     			{"uniqueId":"widgetUserCountryMapworldMap","parameters":{"module":"UserCountryMap","action":"worldMap"}},
-    			{"uniqueId":"widgetLivewidget","parameters":{"module":"Live","action":"widget"}},
+    			{"uniqueId":"widgetUserSettingsgetBrowser","parameters":{"module":"UserSettings","action":"getBrowser"}},
     			{"uniqueId":"widgetReferersgetSearchEngines","parameters":{"module":"Referers","action":"getSearchEngines"}},
     			{"uniqueId":"widgetVisitTimegetVisitInformationPerServerTime","parameters":{"module":"VisitTime","action":"getVisitInformationPerServerTime"}},
     			{"uniqueId":"widgetExampleRssWidgetrssPiwik","parameters":{"module":"ExampleRssWidget","action":"rssPiwik"}}
