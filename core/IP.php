cdb8ecc331af526949fb0a956ed0e3cdbff20bdc
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2011-04-22 23:46:01 +0000

    fixes #2351 - use LGPL PHP_Compat_Functions

--- core/IP.php
+++ core/IP.php
@@ -387,3 +387,92 @@ class Piwik_IP
 		return gethostbyaddr($ipStr);
 	}
 }
+
+/**
+ * Replace inet_ntop()
+ *
+ * @category    PHP
+ * @package     PHP_Compat
+ * @license     LGPL - http://www.gnu.org/licenses/lgpl.html
+ * @copyright   2004-2007 Aidan Lister <aidan@php.net>, Arpad Ray <arpad@php.net>
+ * @link        http://php.net/inet_ntop
+ * @author      Arpad Ray <arpad@php.net>
+ * @version     $Revision: 269597 $
+ * @since       PHP 5.1.0
+ * @require     PHP 4.0.0 (long2ip)
+ */
+function php_compat_inet_ntop($in_addr)
+{
+    switch (strlen($in_addr)) {
+        case 4:
+            list(,$r) = unpack('N', $in_addr);
+            return long2ip($r);
+
+        case 16:
+            $r = substr(chunk_split(bin2hex($in_addr), 4, ':'), 0, -1);
+            $r = preg_replace(
+                array('/(?::?\b0+\b:?){2,}/', '/\b0+([^0])/e'),
+                array('::', '(int)"$1"?"$1":"0$1"'),
+                $r);
+            return $r;
+    }
+
+    return false;
+}
+
+
+// Define
+if (!function_exists('inet_ntop')) {
+    function inet_ntop($in_addr)
+    {
+        return php_compat_inet_ntop($in_addr);
+    }
+}
+
+/**
+ * Replace inet_pton()
+ *
+ * @category    PHP
+ * @package     PHP_Compat
+ * @license     LGPL - http://www.gnu.org/licenses/lgpl.html
+ * @copyright   2004-2007 Aidan Lister <aidan@php.net>, Arpad Ray <arpad@php.net>
+ * @link        http://php.net/inet_pton
+ * @author      Arpad Ray <arpad@php.net>
+ * @version     $Revision: 269597 $
+ * @since       PHP 5.1.0
+ * @require     PHP 4.2.0 (array_fill)
+ */
+function php_compat_inet_pton($address)
+{
+    $r = ip2long($address);
+    if ($r !== false && $r != -1) {
+        return pack('N', $r);
+    }
+
+    $delim_count = substr_count($address, ':');
+    if ($delim_count < 1 || $delim_count > 7) {
+        return false;
+    }
+
+    $r = explode(':', $address);
+    $rcount = count($r);
+    if (($doub = array_search('', $r, 1)) !== false) {
+        $length = (!$doub || $doub == $rcount - 1 ? 2 : 1);
+        array_splice($r, $doub, $length, array_fill(0, 8 + $length - $rcount, 0));
+    }
+
+    $r = array_map('hexdec', $r);
+    array_unshift($r, 'n*');
+    $r = call_user_func_array('pack', $r);
+
+    return $r;
+}
+
+
+// Define
+if (!function_exists('inet_pton')) {  
+    function inet_pton($address)
+    {
+        return php_compat_inet_pton($address);
+    }
+}
