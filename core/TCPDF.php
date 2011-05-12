6cb8d91cb85d04df4ff43bf0a796e45df2711a85
robocoder <anthon.pang@gmail.com>
2011-05-12 04:06:00 +0000

    phpdocs
    git-svn-id: http://dev.piwik.org/svn/trunk@4679 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- core/TCPDF.php
+++ core/TCPDF.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Piwik - Open source web analytics
  *
@@ -7,44 +6,79 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  * @version $Id$
  * 
- * @category Library
+ * @category Piwik
+ * @package Piwik
  */
 
+/**
+ * @see libs/tcpdf/tcpdf.php
+ */
 require_once PIWIK_INCLUDE_PATH . '/libs/tcpdf/tcpdf.php';
 
+/**
+ * TCPDF class wrapper.
+ *
+ * @package Piwik
+ */
 class Piwik_TCPDF extends TCPDF
 {
-    protected $footerContent = null;
-    protected $currentPageNo = null;
+	protected $footerContent = null;
+	protected $currentPageNo = null;
 
-    function Footer()
-    {
-        //Don't show footer on the frontPage
-        if ($this->currentPageNo > 1) {
-            $this->SetY(-15);
-            $this->SetFont($this->footer_font[0], $this->footer_font[1], $this->footer_font[2]);
-            $this->Cell(0, 10, $this->footerContent . Piwik_Translate('PDFReports_Pagination', array($this->getAliasNumPage(), $this->getAliasNbPages())), 0, false, 'C', 0, '', 0, false, 'T', 'M');
-        }
-    }
+	/**
+	 * Render page footer
+	 *
+	 * @see TCPDF::Footer()
+	 */
+	function Footer()
+	{
+		//Don't show footer on the frontPage
+		if ($this->currentPageNo > 1)
+		{
+			$this->SetY(-15);
+			$this->SetFont($this->footer_font[0], $this->footer_font[1], $this->footer_font[2]);
+			$this->Cell(0, 10, $this->footerContent . Piwik_Translate('PDFReports_Pagination', array($this->getAliasNumPage(), $this->getAliasNbPages())), 0, false, 'C', 0, '', 0, false, 'T', 'M');
+		}
+	}
 
-    function setCurrentPageNo()
-    {
-        if (empty($this->currentPageNo)) {
-            $this->currentPageNo = 1;
-        } else {
-            $this->currentPageNo++;
-        }
-    }
+	/**
+	 * Set current page number
+	 */
+	function setCurrentPageNo()
+	{
+		if (empty($this->currentPageNo))
+		{
+			$this->currentPageNo = 1;
+		}
+		else
+		{
+			$this->currentPageNo++;
+		}
+	}
 
-    function AddPage($orientation = '')
-    {
-        parent::AddPage($orientation);
-        $this->setCurrentPageNo();
-    }
+	/**
+	 * Add page to document
+	 *
+	 * @see TCPDF::AddPage()
+	 *
+	 * @param string $orientation
+	 * @param mixed $format
+	 * @param bool $keepmargins
+	 * @param bool $tocpage
+	 */
+	function AddPage($orientation='', $format='', $keepmargins=false, $tocpage=false)
+	{
+		parent::AddPage($orientation);
+		$this->setCurrentPageNo();
+	}
 
-    function SetFooterContent($footerContent)
-    {
-        $this->footerContent = $footerContent;
-    }
-
+	/**
+	 * Set footer content
+	 *
+	 * @param string $footerContent
+	 */
+	function SetFooterContent($footerContent)
+	{
+		$this->footerContent = $footerContent;
+	}
 }
