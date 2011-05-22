2d0b73c09d4dde3ed838e142357c7f71c2fa575b
robocoder <anthon.pang@gmail.com>
2011-05-22 16:03:55 +0000

    refs #1279 - add update script and bump version
    git-svn-id: http://dev.piwik.org/svn/trunk@4762 59fd770c-687e-43c8-a1e3-f5a4ff64c105

---
+++ core/Updates/1.5-b5.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ * @version $Id$
+ *
+ * @category Piwik
+ * @package Updates
+ */
+
+/**
+ * @package Updates
+ */
+class Piwik_Updates_1_5_b4 extends Piwik_Updates
+{
+	static function getSql($schema = 'Myisam')
+	{
+		$tables = Piwik::getTablesCreateSql();
+
+		return array(
+			$tables['session'] => false,
+		);
+	}
+
+	static function update()
+	{
+		Piwik_Updater::updateDatabase(__FILE__, self::getSql());
+	}
+}
