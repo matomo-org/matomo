c6b0f39a8d72d444dff5276d4420b9cd3bdf2acf
mattpiwik <matthieu.aubry@gmail.com>
2011-08-09 07:40:52 +0000

    git-svn-id: http://dev.piwik.org/svn/trunk@5089 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- misc/cron/archive.php
+++ misc/cron/archive.php
@@ -9,17 +9,17 @@
  * - Calls API to get the list of all websites Ids with new visits since the last archive.php succesful run
  * - Calls API to get the list of segments to pre-process
  * The script then loops over these websites & segments and calls the API to pre-process these reports.
- * It does try to achieve Near real time for "daily" reports, processing them as often as possible.
  * 
  * Notes about the algorithm:
- * - To improve performance, API is called with date=last1 whenever possible, instead of last52 
+ * - To improve performance, API is called with date=last1 whenever possible, instead of last52.
+ *   To do so, the script logs the last time it executed correctly.
+ * - The script tries to achieve Near real time for "today" reports, processing "period=day" as often as possible.
  * - The script will only process (or re-process) reports for Current week / Current month  
- * 	 or Current year at most once per hour. 
- *   You can change this timeout as a parameter of the archive.php script.
+ * 	 or Current year at most once per hour. To do so, the script logs last execution time for each website.
+ *   You can change this <current_periods_timeout> timeout parameter when calling archive.php script.
  *   The concept is to archive daily report as often as possible, to stay near real time on "daily" reports,
  *   while allowing less real time weekly/monthly/yearly reporting. 
  */
-
 /**
  * TODO/Ideas
  * - Hide from public HTTP access by default (rename .php.ignore?) for security + avoiding info leak
@@ -28,7 +28,6 @@
  * - Add "report last processed X s ago" in UI grey box "About"
  * - piwik.org update FAQ / online doc
  * - check that when ran as crontab, it will email errors when there is any
- * 
  */
 define('PIWIK_INCLUDE_PATH', realpath('../../'));
 define('PIWIK_USER_PATH', PIWIK_INCLUDE_PATH);
