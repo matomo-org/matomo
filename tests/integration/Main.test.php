39bfbb3492aaa1703239bfedaec9b555bf4ff06b
mattpiwik <matthieu.aubry@gmail.com>
2011-09-19 01:41:27 +0000

    Fix tests?
    git-svn-id: http://dev.piwik.org/svn/trunk@5188 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- tests/integration/Main.test.php
+++ tests/integration/Main.test.php
@@ -44,18 +44,20 @@ class Test_Piwik_Integration_Main extends Test_Integration
     	// VISIT NO 1
         $t->setUrl( 'http://example.org/index.htm' );
         $category = 'Electronics & Cameras';
+        $price = 1111;
         
         // VIEW product page
-        $t->setEcommerceView('SKU2', 'PRODUCT name', $category);
+        $t->setEcommerceView('SKU2', 'PRODUCT name', $category, $price);
         $t->setCustomVariable(5, 'VisitorType', 'NewLoggedOut', 'visit');
         $this->assertTrue($t->getCustomVariable(3, 'page') == array('_pks','SKU2'));
         $this->assertTrue($t->getCustomVariable(4, 'page') == array('_pkn','PRODUCT name'));
         $this->assertTrue($t->getCustomVariable(5, 'page') == array('_pkc',$category));
+        $this->assertTrue($t->getCustomVariable(2, 'page') == array('_pkp',$price));
         $this->assertTrue($t->getCustomVariable(5, 'visit') == array('VisitorType','NewLoggedOut' ));
         $this->checkResponse($t->doTrackPageView( 'incredible title!'));
         
         $t->setForceVisitDateTime(Piwik_Date::factory($dateTime)->addHour(0.1)->getDatetime());
-        $t->setEcommerceView($sku = 'SKU VERY nice indeed', $name = 'PRODUCT name' , $category);
+        $t->setEcommerceView($sku = 'SKU VERY nice indeed', $name = 'PRODUCT name' , $category, $price=666);
         $this->checkResponse($t->doTrackPageView( 'Another Product page'));
 
         $t->setForceVisitDateTime(Piwik_Date::factory($dateTime)->addHour(0.2)->getDatetime());
@@ -248,7 +250,7 @@ class Test_Piwik_Integration_Main extends Test_Integration
         // Website2
 		$this->setApiToCall( array('Goals.get', 'Goals.getItemsSku', 'Goals.getItemsName', 'Goals.getItemsCategory'	) );
         $this->callGetApiCompareOutput(__FUNCTION__ . '_Website2', 'xml', $idSite2, $dateTime, $periods = array('week'));
-//        exit;
+        exit;
 	}
 	
 	function test_trackGoals_allowMultipleConversionsPerVisit()
