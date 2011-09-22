1b4505af19faafe65a85d6e672d417a919817a6d
SteveG <SteveG@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2011-09-22 19:53:11 +0000

    refactored main navigation. removed superfish

--- plugins/CoreHome/templates/menu.js
+++ plugins/CoreHome/templates/menu.js
@@ -7,68 +7,53 @@
 
 function menu()
 {
-	this.param = {};
+    this.param = {};
 }
 
 menu.prototype =
-{	
-	menuSectionLoaded: function (content, urlLoaded)
-	{
-		if(urlLoaded == menu.prototype.lastUrlRequested)
-		{
-			$('#content').html( content ).show();
-			piwikHelper.hideAjaxLoading();
-			menu.prototype.lastUrlRequested = null;
-		}
-	},
-	
-	customAjaxHandleError: function ()
-	{
-		menu.prototype.lastUrlRequested = null;
-		piwikHelper.ajaxHandleError();		
-	},
-	
-	overMainLI: function ()
-	{
-		$(this).siblings().removeClass('sfHover');
-	},
-	
-	outMainLI: function ()
-	{
-	},
-	
-	init: function()
-	{
-		var self = this;
-		
-		
-		//sub LI auto height
-		$('.nav li li a').each(function(){$(this).css({width:$(this).width()+30, paddingLeft:0, paddingRight:0});});
-		
-		
-		this.param.superfish = $('.nav')
-			.superfish({
-				pathClass : 'current',
-				animation : {opacity:'show'},
-				delay : 2000
-			});
-		this.param.superfish.find("ul")
-			.click( function(e){ e.stopPropagation()} )
-			;
-		// using onclick attribute instead of jquery.bind(click) 
-		// to keep middle click for open in new tab still working
-		this.param.superfish.find("li a")
-			.attr( 'onclick', "$('ul.nav').trigger('piwikSwitchPage'); broadcast.propagateAjax($(this).attr('name')); return false;" )
-			;
-
-		this.param.superfish
-			.find("li:has(ul)")
-			.hover(self.overMainLI, self.outMainLI)
-			;
+{    
+    menuSectionLoaded: function (content, urlLoaded)
+    {
+        if(urlLoaded == menu.prototype.lastUrlRequested)
+        {
+            $('#content').html( content ).show();
+            piwikHelper.hideAjaxLoading();
+            menu.prototype.lastUrlRequested = null;
+        }
+    },
+    
+    customAjaxHandleError: function ()
+    {
+        menu.prototype.lastUrlRequested = null;
+        piwikHelper.ajaxHandleError();        
+    },
+    
+    overMainLI: function ()
+    {
+        $(this).siblings().removeClass('sfHover');
+        $(this).addClass('sfHover');
+    },
+    
+    onItemClick: function (item)
+    {
+        $('ul.nav').trigger('piwikSwitchPage');
+        broadcast.propagateAjax($(item).attr('name'));
+        return false;
+    },
+    
+    init: function()
+    {
+        this.menuNode = $('.nav');
+        
+        //sub LI auto height
+        $('.nav li li a').each(function(){$(this).css({width:$(this).width()+30, paddingLeft:0, paddingRight:0});});
+        
+        this.menuNode.find("li:has(ul)").bind('mouseenter' ,this.overMainLI);
+        
         // add id to all li menu to suport menu identification.
         // for all sub menu we want to have a unique id based on their module and action
         // for main menu we want to add just the module as its id.
-        this.param.superfish.find('li').each(function(){
+        this.menuNode.find('li').each(function(){
             var url = $(this).find('a').attr('name');
             var module = broadcast.getValueFromUrl("module",url);
             var action = broadcast.getValueFromUrl("action",url);
@@ -80,70 +65,54 @@ menu.prototype =
             }
             else
             {
-				// so Goals plugin is a little different than other
-				// we can't identify by it's modules_action so we uses its idGoals.
-				if(idGoal != '') {
-					$(this).attr({id: module + '_' + action + '_' + idGoal});
-				}
-				else {
-					$(this).attr({id: module + '_' + action});
-				}
+                // so Goals plugin is a little different than other
+                // we can't identify by it's modules_action so we uses its idGoals.
+                if(idGoal != '') {
+                    $(this).attr({id: module + '_' + action + '_' + idGoal});
+                }
+                else {
+                    $(this).attr({id: module + '_' + action});
+                }
             }
         });
-	},
+    },
 
     activateMenu : function(module,action,idGoal)
     {
-		// getting the right li is a little tricky since goals uses idGoal, and overview is index.
-		var $li = '';
-		// So, if module is Goals, idGoal is present, and action is not Index, must be one of the goals
-		if(module == 'Goals' && idGoal != '' && (action != 'index')) {
-			$li = $("#" + module + "_" + action + "_" + idGoal);
-		} else {
-			$li = $("#" + module + "_" + action);
-		}
-		
-		if(this.activeLI) this.activeLI.removeClass('sfActive');
-		this.activeLI=($li.attr("id")?$li.parent().parent():$("#" + module)).addClass('sfActive');
+        this.menuNode.find('li').removeClass('sfHover').removeClass('sfActive');
+        
+        // getting the right li is a little tricky since goals uses idGoal, and overview is index.
+        var $li = '';
+        // So, if module is Goals, idGoal is present, and action is not Index, must be one of the goals
+        if(module == 'Goals' && idGoal != '' && (action != 'index')) {
+            $li = $("#" + module + "_" + action + "_" + idGoal);
+        } else {
+            $li = $("#" + module + "_" + action);
+        }
+        
+        var mainLi = $("#" + module);
+        if(!mainLi.length) {
+            mainLi = $li.parents('li');
+        }
+        
+        mainLi.addClass('sfActive').addClass('sfHover');
 
-		// we can't find this li based on Module_action? then li only be the main menu. e.g Dashboard.
-		var no_sub_menu = false;
-		if($li.size() == 0) {
-			$li = $("#" + module);
-			no_sub_menu = true;
-		}
-		
-		piwikMenu.param.superfish.find("li").removeClass('sfHover');
-		if($li.find('ul li').size() != 0 || no_sub_menu == true) {
-			// we clicked on a MAIN LI
-			$.fn.superfish.currentActiveMenu = $li;
-			$li.find('>ul li:first').addClass('sfHover');
-			$li.find('ul').addClass("hidden");//css({'display':'block','visibility': 'visible'});
-		} else {
-		// we are in the SUB UL LI
-			$.fn.superfish.currentActiveMenu = $li.parents('li');
-			$li.addClass('sfHover');
-			$li.parents('ul').css({'display':'block','visibility': 'visible'});
-		}
-	    $.fn.superfish.currentActiveMenu.showSuperfishUl().siblings().hideSuperfishUl();
+        $li.addClass('sfHover');
     },
 
-	loadFirstSection: function()
-	{
-		var self=this;
+    loadFirstSection: function()
+    {
         if(broadcast.isHashExists() == false) {
-            $('li:first a:first', self.param.superfish)
-		    .click()
-		    .each(function(){ $(this).showSuperfishUl(); });
+            $('li:first a:first', this.menuNode).click().addClass('sfHover').addClass('sfActive');
         }
-	}
+    }
 };
 
 $(document).ready( function(){
-	if($('.nav').size()) {
-		piwikMenu = new menu();
-		piwikMenu.init();
-		piwikMenu.loadFirstSection();
-		broadcast.init();
-	}
+    if($('.nav').size()) {
+        piwikMenu = new menu();
+        piwikMenu.init();
+        piwikMenu.loadFirstSection();
+        broadcast.init();
+    }
 });
