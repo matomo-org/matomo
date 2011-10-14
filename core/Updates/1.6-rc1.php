82aead60e9b7bec394902c70831037d1a38d57d1
mattpiwik <matthieu.aubry@gmail.com>
2011-10-14 06:25:09 +0000

    Enabling ImageGraph on update for existing users
    git-svn-id: http://dev.piwik.org/svn/trunk@5305 59fd770c-687e-43c8-a1e3-f5a4ff64c105

---
+++ core/Updates/1.6-rc1.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ * @version $Id$
+ *
+ * @category Piwik
+ * @package Updates
+ */
+
+/**
+ * @package Updates
+ */
+class Piwik_Updates_1_6_rc1 extends Piwik_Updates
+{
+	static function update()
+	{
+		try {
+			Piwik_PluginsManager::getInstance()->activatePlugin('ImageGraph');
+		} catch(Exception $e) {
+		}
+	}
+}
+
