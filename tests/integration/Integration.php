bbafc1c02e5debf522c1e01a308d7ab36fb728d4
mattpiwik <matthieu.aubry@gmail.com>
2011-10-14 05:27:58 +0000

    Refs #2670  * Fixing few bugs found by integration tests and hopefully fixing build!
    git-svn-id: http://dev.piwik.org/svn/trunk@5298 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- tests/integration/Integration.php
+++ tests/integration/Integration.php
@@ -460,6 +460,8 @@ abstract class Test_Integration extends Test_Database
     			$response = $this->removeXmlElement($response, 'sum_daily_nb_uniq_visitors');
     			$expected = $this->removeXmlElement($expected, 'nb_visits_converted');
     			$response = $this->removeXmlElement($response, 'nb_visits_converted');
+    			$expected = $this->removeXmlElement($expected, 'imageGraphUrl');
+    			$response = $this->removeXmlElement($response, 'imageGraphUrl');
     			$removeEndOfLines =true;
     		}
     		
