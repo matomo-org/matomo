e710b80647bd19c1296a9cd1fe05c01ed4e601b8
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2011-11-10 21:57:22 +0000

    fixes #2769

--- core/Visualization/Cloud.php
+++ core/Visualization/Cloud.php
@@ -43,6 +43,9 @@ class Piwik_Visualization_Cloud implements Piwik_View_Interface
 
 	public function render()
 	{
+		$strlen = function_exists('mb_strlen') ? 'mb_strlen' : 'strlen';
+		$substr = function_exists('mb_substr') ? 'mb_substr' : 'substr';
+
 		$this->shuffleCloud();
 		$return = array();
 		if(empty($this->wordsArray)) {
@@ -52,9 +55,9 @@ class Piwik_Visualization_Cloud implements Piwik_View_Interface
 		foreach ($this->wordsArray as $word => $popularity)
 		{
 			$wordTruncated = $word;
-			if(strlen($word) > $this->truncatingLimit)
+			if($strlen($word) > $this->truncatingLimit)
 			{
-				$wordTruncated = substr($word, 0, $this->truncatingLimit - 3).'...';
+				$wordTruncated = $substr($word, 0, $this->truncatingLimit - 3).'...';
 			}
 			
 			// case hideFutureHoursWhenToday=1 shows hours with no visits
