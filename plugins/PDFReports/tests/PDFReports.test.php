cc83947a4d1d191c91e59dfe2366136a7e09a7f5
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2011-12-02 14:04:48 +0000

    refs #2608 - revert regression in [5515]

--- plugins/PDFReports/tests/PDFReports.test.php
+++ plugins/PDFReports/tests/PDFReports.test.php
@@ -58,25 +58,25 @@ class Test_Piwik_PDFReports extends Test_Database
 
     	$idReportTwo = $this->_createReport($dataWebsiteTwo);
     	// Testing getReports without parameters
-    	$report = Piwik_PDFReports_API::getInstance()->getReports();
+    	$report = reset(Piwik_PDFReports_API::getInstance()->getReports());
     	$this->_checkReportsEqual($report, $dataWebsiteTwo);
 
     	$idReport = $this->_createReport($data);
     	
     	// Passing 3 parameters
-    	$report = Piwik_PDFReports_API::getInstance()->getReports($this->idSiteAccess, $data['period'], $idReport);
+    	$report = reset(Piwik_PDFReports_API::getInstance()->getReports($this->idSiteAccess, $data['period'], $idReport));
     	$this->_checkReportsEqual($report, $data);
 
     	// Passing only idsite
-    	$report = Piwik_PDFReports_API::getInstance()->getReports($this->idSiteAccess);
+    	$report = reset(Piwik_PDFReports_API::getInstance()->getReports($this->idSiteAccess));
     	$this->_checkReportsEqual($report, $data);
     	
     	// Passing only period
-    	$report = Piwik_PDFReports_API::getInstance()->getReports($idSite=false, $data['period']);
+    	$report = reset(Piwik_PDFReports_API::getInstance()->getReports($idSite=false, $data['period']));
     	$this->_checkReportsEqual($report, $data);
     	
     	// Passing only idreport
-    	$report = Piwik_PDFReports_API::getInstance()->getReports($idSite=false,$period=false, $idReport);
+    	$report = reset(Piwik_PDFReports_API::getInstance()->getReports($idSite=false,$period=false, $idReport));
     	$this->_checkReportsEqual($report, $data);
     	
     }
@@ -135,7 +135,7 @@ class Test_Piwik_PDFReports extends Test_Database
     	$idReport = $this->_createReport($dataBefore);
     	$dataAfter = $this->_getYetAnotherAddReportData();
     	$this->_updateReport($idReport, $dataAfter);
-    	$newReport = Piwik_PDFReports_API::getInstance()->getReports($idSite=false,$period=false, $idReport);
+    	$newReport = reset(Piwik_PDFReports_API::getInstance()->getReports($idSite=false,$period=false, $idReport));
     	$this->_checkReportsEqual($newReport, $dataAfter);
     }
     
