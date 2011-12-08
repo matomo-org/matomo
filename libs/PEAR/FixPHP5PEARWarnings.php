74c4b8d72c8f5b313e433fe72c4481e4881694c9
vipsoft <vipsoft@59fd770c-687e-43c8-a1e3-f5a4ff64c105>
2011-12-08 20:43:30 +0000

    refs #2806 - these were caught by php -l

--- libs/PEAR/FixPHP5PEARWarnings.php
+++ libs/PEAR/FixPHP5PEARWarnings.php
@@ -1,7 +1,7 @@
 <?php
 if ($skipmsg) {
-    $a = &new $ec($code, $mode, $options, $userinfo);
+    $a = new $ec($code, $mode, $options, $userinfo);
 } else {
-    $a = &new $ec($message, $code, $mode, $options, $userinfo);
+    $a = new $ec($message, $code, $mode, $options, $userinfo);
 }
 ?>
--- tests/simpletest/extensions/pear_test_case.php
+++ tests/simpletest/extensions/pear_test_case.php
@@ -44,9 +44,9 @@
          */
         function assertEquals($first, $second, $message = "%s", $delta = 0) {
             if ($this->_loosely_typed) {
-                $expectation = &new EqualExpectation($first);
+                $expectation = new EqualExpectation($first);
             } else {
-                $expectation = &new IdenticalExpectation($first);
+                $expectation = new IdenticalExpectation($first);
             }
             $this->assert($expectation, $second, $message);
         }
@@ -80,7 +80,7 @@
          *    @public
          */
         function assertSame(&$first, &$second, $message = "%s") {
-            $dumper = &new SimpleDumper();
+            $dumper = new SimpleDumper();
             $message = sprintf(
                     $message,
                     "[" . $dumper->describeValue($first) .
@@ -101,7 +101,7 @@
          *    @public
          */
         function assertNotSame(&$first, &$second, $message = "%s") {
-            $dumper = &new SimpleDumper();
+            $dumper = new SimpleDumper();
             $message = sprintf(
                     $message,
                     "[" . $dumper->describeValue($first) .
