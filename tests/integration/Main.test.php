7358c182ca2e94e1b175bc1516cdd194efcb7afd
mattpiwik <matthieu.aubry@gmail.com>
2011-12-23 03:24:16 +0000

    fixes #2744  * Now link in Visitor Log and Live! real time widget will pointto the search result page for the google search for the domaain that was used, instead of linking to google.XX/url which redirects to the tracked website's landing page
    git-svn-id: http://dev.piwik.org/svn/trunk@5605 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- tests/integration/Main.test.php
+++ tests/integration/Main.test.php
@@ -1180,6 +1180,9 @@ class Test_Piwik_Integration_Main extends Test_Integration
 		
 	/*
 	 * Test empty google kwd works nicely in Live! output and Top keywords
+	 * 
+	 * Also test that Live! will link to the search result page URL rather than the exact referrer URL
+	 * when the referrer URL is google.XX/url.... which is a redirect to landing page rather than the search result URL 
 	 */
 	function test_OneVisitor_NoKeywordSpecified() 
 	{
@@ -1190,16 +1193,20 @@ class Test_Piwik_Integration_Main extends Test_Integration
     	$idSite = $this->createWebsite($dateTime);
         $t = $this->getTracker($idSite, $dateTime, $defaultInit = true, $useThirdPartyCookie = 1);
         
+        // VISIT 1 = Referrer is "Keyword not defined"
+        // Alsotrigger goal to check that attribution goes to this keyword
         $t->setUrlReferrer( 'http://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&ved=0CC&url=http%3A%2F%2Fpiwik.org%2F&ei=&usg=');
-
-        // Record 1st page view
         $t->setUrl( 'http://example.org/this%20is%20cool!');
         $this->checkResponse($t->doTrackPageView( 'incredible title!'));
-        
-        // Create Goal 1: Triggered by JS, after 18 minutes
         $idGoal = Piwik_Goals_API::getInstance()->addGoal($idSite, 'triggered js', 'manually', '', '');
         $t->setForceVisitDateTime(Piwik_Date::factory($dateTime)->addHour(0.3)->getDatetime());
         $this->checkResponse($t->doTrackGoal($idGoal, $revenue = 42));
+        
+        // VISIT 2 = Referrer has keyword, but the URL should be rewritten 
+        // in Live Output to point to google search result page
+        $t->setForceVisitDateTime(Piwik_Date::factory($dateTime)->addHour(2)->getDatetime());
+        $t->setUrlReferrer( 'http://www.google.com.vn/url?sa=t&rct=j&q=%3C%3E%26%5C%22the%20pdo%20extension%20is%20required%20for%20this%20adapter%20but%20the%20extension%20is%20not%20loaded&source=web&cd=4&ved=0FjAD&url=http%3A%2F%2Fforum.piwik.org%2Fread.php%3F2%2C1011&ei=y-HHAQ&usg=AFQjCN2-nt5_GgDeg&cad=rja');
+        $this->checkResponse($t->doTrackPageView( 'incredible title!'));
         
         $this->callGetApiCompareOutput(__FUNCTION__, 'xml', $idSite, $dateTime, $periods = false,
 			$setDateLastN = false, $language = 'fr');
