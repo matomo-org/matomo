838a8a729d385e5d18cd21cee83d4fa38e02dc14
mattpiwik <matthieu.aubry@gmail.com>
2012-02-11 11:47:06 +0000

    Fixing notice when url not defined
    git-svn-id: http://dev.piwik.org/svn/trunk@5808 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- core/Common.php
+++ core/Common.php
@@ -401,6 +401,8 @@ class Piwik_Common
 				$name = $value;
 				$value = false;
 			}
+			$name = Piwik_Common::sanitizeInputValue($name);
+			$value = Piwik_Common::sanitizeInputValue($value);
 
 			// if array without indexes
 			$count = 0;
