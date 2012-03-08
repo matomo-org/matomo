42e85b9ae5847dfd97ae84add8cb3eb09f1f7e9c
mattpiwik <matthieu.aubry@gmail.com>
2012-03-08 19:29:05 +0000

    Minor
    git-svn-id: http://dev.piwik.org/svn/trunk@6005 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- tests/TestRunner.php
+++ tests/TestRunner.php
@@ -71,10 +71,10 @@ class TestRunner
 		$this->dirsToGlob = $dirs;
 	}
 	
-	static public $testsHelpLinks = "<ul><li><a href=\"core\">Run each test separately</a> or each <a href='integration/'>each integration test separately</a></li>\n
-					<li><a href=\"all_tests.php\">Run all tests</a> or <a href='all_integration_tests.php'>Integration tests only</a> (please be patient, it will take at least 5-10 minutes)</li>\n
-					<li><a href=\"all_integration_tests.php?apiTestingLevel=none&widgetTestingLevel=check_errors \">Call all Widgets tests and check for error only</a> or <a href=\"all_integration_tests.php?apiTestingLevel=none&widgetTestingLevel=compare_output \">call all Widgets and compare output (tests will fail at this stage, this is normal!)</a></li>\n
-					<li><a href=\"javascript/\">Run piwik.js Javascript unit & integration tests</a></li>\n
+	static public $testsHelpLinks = "<ul><li><a href=\"core\">Run each unit test separately</a> or each <a href='integration/'>each integration test separately</a></li>\n
+					<li><a href=\"all_tests.php\">Run all tests (unit + integration tests)</a> or <a href='all_integration_tests.php'>all integration tests</a>. <br/><i>Note: please be patient, it will take at least 5-10 minutes</i></li>\n
+					<li><a href=\"all_integration_tests.php?apiTestingLevel=none&widgetTestingLevel=check_errors \">Call all Widgets tests and check for error only</a> or <a href=\"all_integration_tests.php?apiTestingLevel=none&widgetTestingLevel=compare_output \">call all Widgets and compare output</a>. <br/><i>Note: Tests are currently failing, this is a work in progress, see <a href='http://dev.piwik.org/trac/ticket/2908'>#2908</a></i></li>\n
+					<li><a href=\"javascript/\">Run piwik.js Javascript unit & integration tests</a>. <br/><i>Note: the Javascript tests are not executed in Jenkins so must be run manually on major browsers after any change to piwik.js</i></li>\n
 				</ul>\n";
 	public function run()
 	{
