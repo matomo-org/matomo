702b27955f0598cf62f8321b987f4f889dcb934c
mattpiwik <matthieu.aubry@gmail.com>
2012-05-28 09:42:39 +0000

    Refs #3034
    git-svn-id: http://dev.piwik.org/svn/trunk@6348 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- plugins/ExamplePlugin/Controller.php
+++ plugins/ExamplePlugin/Controller.php
@@ -44,7 +44,7 @@ class Piwik_ExamplePlugin_Controller extends Piwik_Controller
 
 	function photostreamMatt()
 	{
-		echo '<object width="400" height="500"> <param name="flashvars" value="offsite=true&lang=en-us&page_show_url=%2Fphotos%2Fmatthieu-aubry%2Fsets%2F72157602308487455%2Fshow%2F&page_show_back_url=%2Fphotos%2Fmatthieu-aubry%2Fsets%2F72157602308487455%2F&set_id=72157602308487455&jump_to="></param> <param name="movie" value="http://www.flickr.com/apps/slideshow/show.swf?v=109615"></param> <param name="allowFullScreen" value="true"></param><embed type="application/x-shockwave-flash" src="http://www.flickr.com/apps/slideshow/show.swf?v=109615" allowFullScreen="true" flashvars="offsite=true&lang=en-us&page_show_url=%2Fphotos%2Fmatthieu-aubry%2Fsets%2F72157602308487455%2Fshow%2F&page_show_back_url=%2Fphotos%2Fmatthieu-aubry%2Fsets%2F72157602308487455%2F&set_id=72157602308487455&jump_to=" width="400" height="500"></embed></object>';
+		echo '<object width="400" height="400"> <param name="flashvars" value="offsite=true&lang=en-us&page_show_url=%2Fphotos%2Fmatthieu-aubry%2Fsets%2F72157602308487455%2Fshow%2F&page_show_back_url=%2Fphotos%2Fmatthieu-aubry%2Fsets%2F72157602308487455%2F&set_id=72157602308487455&jump_to="></param> <param name="movie" value="http://www.flickr.com/apps/slideshow/show.swf?v=109615"></param> <param name="allowFullScreen" value="true"></param><embed type="application/x-shockwave-flash" src="http://www.flickr.com/apps/slideshow/show.swf?v=109615" allowFullScreen="true" flashvars="offsite=true&lang=en-us&page_show_url=%2Fphotos%2Fmatthieu-aubry%2Fsets%2F72157602308487455%2Fshow%2F&page_show_back_url=%2Fphotos%2Fmatthieu-aubry%2Fsets%2F72157602308487455%2F&set_id=72157602308487455&jump_to=" width="400" height="400"></embed></object>';
 	}
 	
 	/**
--- plugins/ExamplePlugin/lang/en.php
+++ plugins/ExamplePlugin/lang/en.php
@@ -14,7 +14,6 @@ $translations = array(
 	'ExamplePlugin_PluginDescription' => 'Example Plugin: This plugin shows how to create a very simple plugin, that exports two widgets in the Dashboard.',
 	'ExamplePlugin_exampleWidgets' => 'Example Widgets',
 	'ExamplePlugin_exampleWidget' => 'Example widget',
-	'ExamplePlugin_blogPiwikRss' => 'Blog Piwik RSS',
 	'ExamplePlugin_photostreamMatt' => 'Matt\'s Photos',
 	'ExamplePlugin_piwikForumVisits' => 'Piwik Forums visits',
 	'ExamplePlugin_PiwikForumReceivedVisits' => 'On %s, Piwik forums received %s visits.'
