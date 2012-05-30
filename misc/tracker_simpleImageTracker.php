8598251423c94515d7f017dabd04381be5bad803
mattpiwik <matthieu.aubry@gmail.com>
2012-05-30 09:04:01 +0000

    minor
    git-svn-id: http://dev.piwik.org/svn/trunk@6399 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- misc/tracker_simpleImageTracker.php
+++ misc/tracker_simpleImageTracker.php
@@ -1,3 +1,14 @@
+<html><body>
+
 This page loads a Simple Tracker request to Piwik website id=1
 
-<img src='http://localhost/trunk/piwik.php?idsite=1&rec=1'>
+<img src='http://localhost/trunk/piwik.php?idsite=1&amp;rec=1'>
+
+<?php
+// -- Piwik Tracking API init --
+require_once "../libs/PiwikTracker/PiwikTracker.php";
+PiwikTracker::$URL = 'http://example.org/piwik/';
+// Example 1: Tracks a pageview for Website id = {$IDSITE}
+echo '<img src="'. str_replace("&","&amp;", Piwik_getUrlTrackPageView( $idSite = 1, $customTitle = 'This title will appear in the report Actions > Page titles')) . '" alt="" />';
+?>
+</body></html>
