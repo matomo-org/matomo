5caae7e0fdd77caa12218c0ae73ced7f58b8f9db
Gregor Aisch <contact@vis4.net>
2012-06-03 18:09:47 +0200

    update

--- UserCountryMap.php
+++ UserCountryMap.php
@@ -44,6 +44,8 @@ class Piwik_UserCountryMap extends Piwik_Plugin
      */
     public function getJsFiles($notification)
     {
+        var_dump($notification);
+        die();
         $jsFiles = &$notification->getNotificationObject();
 
         $jsFiles[] = "plugins/UserCountryMap/js/vendor/raphael-min.js";
