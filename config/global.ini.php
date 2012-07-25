727cbcb6b08c8046269bea9ceb04ab7a3edd8cd9
robocoder <anthon.pang@gmail.com>
2012-07-25 00:32:24 +0000

    refs #3273 - update to jquery ui 1.8.22
    git-svn-id: http://dev.piwik.org/svn/trunk@6547 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- config/global.ini.php
+++ config/global.ini.php
@@ -248,7 +248,7 @@ use_ajax_cdn = 0
 
 ; required AJAX library versions
 jquery_version = 1.7.2
-jqueryui_version = 1.8.21
+jqueryui_version = 1.8.22
 swfobject_version = 2.2
 
 ; Set to 1 if you're using https on your Piwik server and Piwik can't detect it,
