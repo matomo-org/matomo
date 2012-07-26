63c7c6f9943250f61c0998d59e951eedf61a4b62
sgiehl <stefangiehl@gmail.com>
2012-07-26 15:57:50 +0000

    refs #3227 added more integration tests
    git-svn-id: http://dev.piwik.org/svn/trunk@6568 59fd770c-687e-43c8-a1e3-f5a4ff64c105

---
+++ tests/PHPUnit/Integration/FlattenReportsTest.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link    http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ * @version $Id$
+ */
+
+/**
+ * Tests the flattening of reports.
+ */
+class Test_Piwik_Integration_FlattenReports extends IntegrationTestCase
+{
+    protected $dateTime = '2010-03-06 11:22:33';
+    protected $idSite   = 1;
+
+    /**
+     * @dataProvider getApiForTesting
+     * @group        Integration
+     * @group        FlattenReports
+     */
+    public function testApi($api, $params)
+    {
+        $this->runApiTests($api, $params);
+    }
+
+    public function getApiForTesting()
+    {
+        $return = array();
+
+        // referrers
+        $return[] = array(
+            'Referers.getWebsites',
+            array(
+                'idSite'                 => $this->idSite,
+                'date'                   => $this->dateTime,
+                'otherRequestParameters' => array(
+                    'flat'     => '1',
+                    'expanded' => '0'
+                )
+            ));
+
+        // urls
+        $return[] = array(
+            'Actions.getPageUrls',
+            array(
+                'idSite'                 => $this->idSite,
+                'date'                   => $this->dateTime,
+                'otherRequestParameters' => array(
+                    'flat'     => '1',
+                    'expanded' => '0'
+                )
+            ));
+        $return[] = array(
+            'Actions.getPageUrls',
+            array(
+                'idSite'                 => $this->idSite,
+                'date'                   => $this->dateTime,
+                'testSuffix'             => '_withAggregate',
+                'otherRequestParameters' => array(
+                    'flat'                   => '1',
+                    'include_aggregate_rows' => '1',
+                    'expanded'               => '0'
+                )
+            ));
+
+        // custom variables for multiple days
+        $return[] = array('CustomVariables.getCustomVariables', array(
+            'idSite'                 => $this->idSite,
+            'date'                   => $this->dateTime,
+            'otherRequestParameters' => array(
+                'date'                   => '2010-03-06,2010-03-08',
+                'flat'                   => '1',
+                'include_aggregate_rows' => '1',
+                'expanded'               => '0'
+            )
+        ));
+
+        return $return;
+    }
+
+    public function getOutputPrefix()
+    {
+        return 'FlattenReports';
+    }
+
+    protected function setUpWebsitesAndGoals()
+    {
+        $this->createWebsite($this->dateTime);
+    }
+
+    protected function trackVisits()
+    {
+        $dateTime = $this->dateTime;
+        $idSite   = $this->idSite;
+
+        for ($referrerSite = 1; $referrerSite < 4; $referrerSite++) {
+            for ($referrerPage = 1; $referrerPage < 3; $referrerPage++) {
+                $offset = $referrerSite * 3 + $referrerPage;
+                $t      = $this->getTracker($idSite, Piwik_Date::factory($dateTime)->addHour($offset)->getDatetime());
+                $t->setUrlReferrer('http://www.referrer' . $referrerSite . '.com/sub/dir/page' . $referrerPage . '.html');
+                $t->setCustomVariable(1, 'CustomVarVisit', 'CustomVarValue' . $referrerPage, 'visit');
+                for ($page = 0; $page < 3; $page++) {
+                    $t->setUrl('http://example.org/dir' . $referrerSite . '/sub/dir/page' . $page . '.html');
+                    $t->setCustomVariable(1, 'CustomVarPage', 'CustomVarValue' . $page, 'page');
+                    $this->checkResponse($t->doTrackPageView('title'));
+                }
+            }
+        }
+
+        $t = $this->getTracker($idSite, Piwik_Date::factory($dateTime)->addHour(24)->getDatetime());
+        $t->setCustomVariable(1, 'CustomVarVisit', 'CustomVarValue1', 'visit');
+        $t->setUrl('http://example.org/sub/dir/dir1/page1.html');
+        $t->setCustomVariable(1, 'CustomVarPage', 'CustomVarValue1', 'page');
+        $this->checkResponse($t->doTrackPageView('title'));
+    }
+}
+
