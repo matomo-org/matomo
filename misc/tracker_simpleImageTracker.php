d28dc62d1e08ca8b781e77ea702ec1146616fd83
mattpiwik <matthieu.aubry@gmail.com>
2012-08-10 04:00:46 +0000

    Doc feedback html validation
    git-svn-id: http://dev.piwik.org/svn/trunk@6707 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- misc/tracker_simpleImageTracker.php
+++ misc/tracker_simpleImageTracker.php
@@ -2,13 +2,12 @@
 
 This page loads a Simple Tracker request to Piwik website id=1
 
-<img src='http://localhost/trunk/piwik.php?idsite=1&amp;rec=1'>
-
 <?php
 // -- Piwik Tracking API init --
 require_once "../libs/PiwikTracker/PiwikTracker.php";
 PiwikTracker::$URL = 'http://example.org/piwik/';
 // Example 1: Tracks a pageview for Website id = {$IDSITE}
-echo '<img src="'. str_replace("&","&amp;", Piwik_getUrlTrackPageView( $idSite = 1, $customTitle = 'This title will appear in the report Actions > Page titles')) . '" alt="" />';
+$trackingURL = Piwik_getUrlTrackPageView( $idSite = 1, $customTitle = 'This title will appear in the report Actions > Page titles');
+echo '<img src="'. htmlentities($trackingURL) . '" alt="" />';
 ?>
 </body></html>
