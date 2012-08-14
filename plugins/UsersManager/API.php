fdae5f143776771527d8c74604e7a51b6464f2d1
mattpiwik <matthieu.aubry@gmail.com>
2012-08-14 05:04:38 +0000

    attempting to debug why the build is failing
    git-svn-id: http://dev.piwik.org/svn/trunk@6747 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- plugins/UsersManager/API.php
+++ plugins/UsersManager/API.php
@@ -700,6 +700,8 @@ class Piwik_UsersManager_API
 	{
 		if(strlen($md5Password) != 32) 
 		{
+			//FIXME TODO TEMP
+			throw new Exception("what's going on here! '$md5Password' " . var_export($_REQUEST,true));
 			throw new Exception(Piwik_TranslateException('UsersManager_ExceptionPasswordMD5HashExpected'));
 		}
 		return md5($userLogin . $md5Password );
