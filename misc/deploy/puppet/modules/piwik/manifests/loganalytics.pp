04964c8f9ac1b5350d4ca1c6ad5926f3dd661e68
tsteur <thomas.steur@googlemail.com>
2012-08-15 22:01:22 +0000

    refs #3314 install simplejson via pip and install those packages only if they are not already installed
    git-svn-id: http://dev.piwik.org/svn/trunk@6779 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- misc/deploy/puppet/modules/piwik/manifests/loganalytics.pp
+++ misc/deploy/puppet/modules/piwik/manifests/loganalytics.pp
@@ -11,10 +11,16 @@ class piwik::loganalytics {
         ensure => latest;
   }
 
-  # TODO add onlyif => ''
-  exec { "install simplejson":
-    command => "easy_install simplejson",
-    require => [ Package['python-setuptools'], Package['python-dev'], Package['build-essential'] ];
+  exec { "easy_install_pip":
+    command => "easy_install pip",
+    require => [ Package['python-setuptools'], Package['python-dev'], Package['build-essential'] ],
+    unless  => "pip --help";
+  }
+
+  exec { "install_simplejson":
+    command => "pip install simplejson",
+    require => Exec['easy_install_pip'],
+    unless  => "pip search simplejson";
   }
 
 }
