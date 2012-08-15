5be300de27de2d4baa9c15ebd06763a489f5dbaf
tsteur <thomas.steur@googlemail.com>
2012-08-15 11:41:42 +0000

    refs #3314 include PHPUnit and lots of php qa tools
    git-svn-id: http://dev.piwik.org/svn/trunk@6765 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- misc/deploy/puppet/php-piwik.pp
+++ misc/deploy/puppet/php-piwik.pp
@@ -32,3 +32,32 @@ file { "/etc/php5/conf.d/mysqli.ini":
 #   content => 'extension=mysqli.so',
 #   notify => Class['php::fpm::service'],
 # }
+
+include pear
+include phpqatools
+
+pear::package { "PHPUnit_MockObject":
+  repository => "pear.phpunit.de",
+  require => Pear::Package["PEAR"],
+}
+
+pear::package { "PHP_CodeCoverage":
+  repository => "pear.phpunit.de",
+  require => Pear::Package["PEAR"],
+}
+
+pear::package { "PHPUnit_Selenium":
+  repository => "pear.phpunit.de",
+  require => Pear::Package["PEAR"],
+}
+
+# todo add channels... we should fork pear module and send pull requests
+# pear module should allow to add channels, do upgrade and install a
+# package only if not already installed
+# pear upgrade pear
+# pear channel-discover pear.phpunit.de
+# pear channel-discover pear.symfony-project.com
+# pear channel-discover components.ez.no
+# pear update-channels
+# pear upgrade-all
+# pear install --alldeps phpunit/PHPUnit
