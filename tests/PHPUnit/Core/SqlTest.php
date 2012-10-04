6865ef1abaee3b3250c202250bafac0040a96f09
benakamoorthi <benaka.moorthi@gmail.com>
2012-10-04 09:50:10 +0000

    Refs #3289, change test name + filename.
    git-svn-id: http://dev.piwik.org/svn/trunk@7098 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- tests/PHPUnit/Core/SqlTest.php
+++ tests/PHPUnit/Core/SqlTest.php
@@ -6,7 +6,7 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  * @version $Id$
  */
-class UnzipTest extends DatabaseTestCase
+class SqlTest extends DatabaseTestCase
 {
 	public function setUp()
 	{
