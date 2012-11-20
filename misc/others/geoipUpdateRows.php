ee5903ab7ee760dfd4a94806ffab2a5888350a12
benakamoorthi <benaka.moorthi@gmail.com>
2012-11-20 07:09:15 +0000

    Fixes #3559, allow geoipUpdateRows.php to run in browser w/o timing out by using successive AJAX requests.
    git-svn-id: http://dev.piwik.org/svn/trunk@7492 59fd770c-687e-43c8-a1e3-f5a4ff64c105

--- misc/others/geoipUpdateRows.php
+++ misc/others/geoipUpdateRows.php
@@ -30,11 +30,109 @@ define('PIWIK_ENABLE_DISPATCH', false);
 Piwik_Config::getInstance()->log['logger_message'][] = 'screen';
 Piwik_FrontController::getInstance()->init();
 
+$query = "SELECT count(*) FROM ".Piwik_Common::prefixTable('log_visit');
+$count = Piwik_FetchOne($query);
+
+// when script run via browser, check for Super User & output html page to do conversion via AJAX
+if (!Piwik_Common::isPhpCliMode())
+{
+	// the 'start' query param will be supplied by the AJAX requests, so if it's not there, the
+	// user is viewing the page in the browser.
+	if (Piwik_Common::getRequestVar('start', false) === false)
+	{
+		try {
+			Piwik::checkUserIsSuperUser();
+		} catch(Exception $e) {
+			Piwik::log('[error] You must be logged in as Super User to run this script. Please login in to Piwik and refresh this page.');
+			exit;
+		}
+		
+		// output HTML page that runs update via AJAX
+	?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+	<script type="text/javascript" src="../../libs/jquery/jquery.js"></script>
+	<script type="text/javascript">
+		(function($){
+			var count = <?php echo $count; ?>;
+			var doIteration = function(start)
+			{
+				if (start >= count)
+				{
+					return;
+				}
+				
+				var end = Math.min(start + 100, count);
+				$.ajax({
+					type: 'POST',
+					url: 'geoipUpdateRows.php',
+					data: {
+						start: start,
+						end: end
+					},
+					async: true,
+					error: function(xhr, status, error) {
+						$('body')
+							.append(xhr.responseText)
+							.append('<div style="color:red"><strong>An error occured!</strong></div>');
+					},
+					success: function(response) {
+						doIteration(end);
+						$('body').append(response);
+						var body = $('body')[0];
+						body.scrollTop = body.scrollHeight;
+					}
+				});
+			};
+	
+			doIteration(0);
+		}(jQuery));
+	</script>
+</head>
+<body>
+</body>
+</html>
+	<?php
+		exit;
+	}
+	else
+	{
+		$start = Piwik_Common::getRequestVar('start', 0, 'int');
+		$end = min($count, Piwik_Common::getRequestVar('end', $count, 'int'));
+		$limit = $end - $start;
+	}
+}
+else // command line
+{
+	$start = 0;
+	$end = $count;
+	$limit = 1000;
+}
+
+function geoipUpdateError($message)
+{
+	Piwik::log($message);
+	if (!Piwik_Common::isPhpCliMode())
+	{
+		@header('HTTP/1.1 500 Internal Server Error', $replace = true, $responseCode = 500);
+	}
+	exit;
+}
+
+// only display notes if on command line (where start will == 0 for that part of script) or on
+// first AJAX call by browser
+$displayNotes = $start == 0;
+
 // try getting the pecl location provider
 $provider = new Piwik_UserCountry_LocationProvider_GeoIp_Pecl();
 if (!$provider->isAvailable())
 {
-	Piwik::log("[note] The GeoIP PECL extension is not installed.");
+	if ($displayNotes)
+	{
+		Piwik::log("[note] The GeoIP PECL extension is not installed.");
+	}
+	
 	$provider = null;
 }
 else
@@ -42,7 +140,10 @@ else
 	$workingOrError = $provider->isWorking();
 	if ($workingOrError !== true)
 	{
-		Piwik::log("[note] The GeoIP PECL extension is broken: $workingOrError");
+		if ($displayNotes)
+		{
+			Piwik::log("[note] The GeoIP PECL extension is broken: $workingOrError");
+		}
 		if (Piwik_Common::isPhpCliMode())
 		{
 			Piwik::log("[note] Make sure your command line PHP is configured to use the PECL extension.");
@@ -54,12 +155,18 @@ else
 // use php api if pecl extension cannot be used
 if (is_null($provider))
 {
-	Piwik::log("[note] Falling back to PHP API. This may become too slow for you. If so, you can read this link on how to install the PECL extension: http://piwik.org/faq/how-to/#faq_164");
+	if ($displayNotes)
+	{
+		Piwik::log("[note] Falling back to PHP API. This may become too slow for you. If so, you can read this link on how to install the PECL extension: http://piwik.org/faq/how-to/#faq_164");
+	}
 	
 	$provider = new Piwik_UserCountry_LocationProvider_GeoIp_Php();
 	if (!$provider->isAvailable())
 	{
-		Piwik::log("[note] The GeoIP PHP API is not available. This means you do not have a GeoIP location database in your ./misc directory. The database must be named either GeoIP.dat or GeoIPCity.dat based on the type of database it is.");
+		if ($displayNotes)
+		{
+			Piwik::log("[note] The GeoIP PHP API is not available. This means you do not have a GeoIP location database in your ./misc directory. The database must be named either GeoIP.dat or GeoIPCity.dat based on the type of database it is.");
+		}
 		$provider = null;
 	}
 	else
@@ -67,7 +174,10 @@ if (is_null($provider))
 		$workingOrError = $provider->isWorking();
 		if ($workingOrError !== true)
 		{
-			Piwik::log("[note] The GeoIP PHP API is broken: $workingOrError");
+			if ($displayNotes)
+			{
+				Piwik::log("[note] The GeoIP PHP API is broken: $workingOrError");
+			}
 			$provider = null;
 		}
 	}
@@ -75,22 +185,13 @@ if (is_null($provider))
 
 if (is_null($provider))
 {
-	Piwik::log("\n[error] There is no location provider that can be used with this script. Only the GeoIP PECL module or the GeoIP PHP API can be used at present. Please install and configure one of these first.");
-	exit;
+	geoipUpdateError("\n[error] There is no location provider that can be used with this script. Only the GeoIP PECL module or the GeoIP PHP API can be used at present. Please install and configure one of these first.");
 }
 
 $info = $provider->getInfo();
-Piwik::log("[note] Found working provider: {$info['id']}");
-
-// when script run via browser, check for Super User
-if (!Piwik_Common::isPhpCliMode()) 
+if ($displayNotes)
 {
-    try {
-    	Piwik::checkUserIsSuperUser();
-    } catch(Exception $e) {
-    	Piwik::log('[error] You must be logged in as Super User to run this script. Please login in to Piwik and refresh this page.');
-    	exit;
-    }
+	Piwik::log("[note] Found working provider: {$info['id']}");
 }
 
 // perform update
@@ -100,15 +201,13 @@ $logVisitFieldsToUpdate = array('location_country'   => Piwik_UserCountry_Locati
 								'location_latitude'  => Piwik_UserCountry_LocationProvider::LATITUDE_KEY,
 								'location_longitude' => Piwik_UserCountry_LocationProvider::LONGITUDE_KEY);
 
-$query = "SELECT count(*) FROM ".Piwik_Common::prefixTable('log_visit');
-$count = Piwik_FetchOne($query);
-$start = 0;
-$limit = 1000;
-
-Piwik::log("\n$count rows to process in ".Piwik_Common::prefixTable('log_visit')
-	. " and ".Piwik_Common::prefixTable('log_conversion')."...");
+if ($displayNotes)
+{
+	Piwik::log("\n$count rows to process in ".Piwik_Common::prefixTable('log_visit')
+		. " and ".Piwik_Common::prefixTable('log_conversion')."...");
+}
 flush();
-for (; $start < $count; $start += $limit)
+for (; $start < $end; $start += $limit)
 {
 	$rows = Piwik_FetchAll("SELECT idvisit, location_ip, ".implode(',', array_keys($logVisitFieldsToUpdate))."
 						FROM ".Piwik_Common::prefixTable('log_visit')." 
@@ -179,7 +278,10 @@ for (; $start < $count; $start += $limit)
 	Piwik::log(round($start * 100 / $count) . "% done...");
 	flush();
 }
-Piwik::log("100% done!");
-Piwik::log("");
-Piwik::log("[note] Now that you've geolocated your old visits, you need to force your reports to be re-processed. See this FAQ entry: http://piwik.org/faq/how-to/#faq_59");
+if ($start >= $count)
+{
+	Piwik::log("100% done!");
+	Piwik::log("");
+	Piwik::log("[note] Now that you've geolocated your old visits, you need to force your reports to be re-processed. See this FAQ entry: http://piwik.org/faq/how-to/#faq_59");
+}
 
