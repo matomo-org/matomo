f4f7abcf1695df4fab64d7d3b5e958e9a1dc22df
Gregor Aisch <contact@vis4.net>
2013-01-14 06:52:51 +0100

    'auto commit'

--- js/realtime-map.js
+++ js/realtime-map.js
@@ -128,20 +128,20 @@ RealTimeMap.run = function(config) {
                 oldest = lastVisits[lastVisits.length-1].lastActionTimestamp;
 
                 // remove symbols that are too old
-                console.log('before', $('circle').length, visitSymbols.symbols.length);
+                //console.log('before', $('circle').length, visitSymbols.symbols.length);
                 var _removed = 0;
                 visitSymbols.remove(function(r) {
                     if (r.lastActionTimestamp < oldest) _removed++;
                     return r.lastActionTimestamp < oldest;
                 });
-                console.log('removed',_removed, 'now', $('circle').length);
+                //console.log('removed',_removed, 'now', $('circle').length);
 
                 // update symbols that remain
                 visitSymbols.update({
                     attrs: visitSymbolAttrs
                 });
 
-                console.log('updated', $('circle').length);
+                //console.log('updated', $('circle').length);
 
                 // add new symbols
                 var newSymbols = [];
@@ -149,13 +149,13 @@ RealTimeMap.run = function(config) {
                     if (r.latitude !== null) newSymbols.push(visitSymbols.add(r));
                 });
 
-                console.log('added', newSymbols.length, visitSymbols.symbols.length, $('circle').length);
+                //console.log('added', newSymbols.length, visitSymbols.symbols.length, $('circle').length);
 
                 lastTimestamp = report[0].lastActionTimestamp;
 
                 visitSymbols.layout().render();
 
-                console.log('rendered', visitSymbols.symbols.length, $('circle').length);
+                //console.log('rendered', visitSymbols.symbols.length, $('circle').length);
 
                 $.each(newSymbols, function(i, s) {
                     if (i>10) return false;
@@ -174,7 +174,7 @@ RealTimeMap.run = function(config) {
                     }, s.data.lastActionTimestamp + config.liveRefreshAfterMs - now);
                 });
 
-                console.log('animated', visitSymbols.symbols.length, $('circle').length);
+                //console.log('animated', visitSymbols.symbols.length, $('circle').length);
             }
 
         });
