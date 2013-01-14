4d6f717600e375888d9044c5fcae026625d02ca5
Gregor Aisch <contact@vis4.net>
2013-01-14 06:44:52 +0100

    'auto commit'

--- js/realtime-map.js
+++ js/realtime-map.js
@@ -119,19 +119,29 @@ RealTimeMap.run = function(config) {
 
             if (report.length) {
 
+                // filter results without location
+                report = report.filter(function(r) {
+                    return r.latitude !== null;
+                });
+
                 lastVisits = [].concat(report).concat(lastVisits).slice(0, maxVisits);
                 oldest = lastVisits[lastVisits.length-1].lastActionTimestamp;
-                //now = lastVisits[0].lastActionTimestamp;
 
                 // remove symbols that are too old
+                console.log('before', $('circle').length, visitSymbols.symbols.length);
+                var _removed = 0;
                 visitSymbols.remove(function(r) {
+                    if (r.lastActionTimestamp < oldest) _removed++;
                     return r.lastActionTimestamp < oldest;
                 });
+                console.log('removed',_removed, 'now', $('circle').length);
 
                 // update symbols that remain
                 visitSymbols.update({
                     attrs: visitSymbolAttrs
                 });
+
+                console.log('updated', $('circle').length);
 
                 // add new symbols
                 var newSymbols = [];
@@ -139,11 +149,13 @@ RealTimeMap.run = function(config) {
                     if (r.latitude !== null) newSymbols.push(visitSymbols.add(r));
                 });
 
-                console.log(visitSymbols.symbols.length);
+                console.log('added', newSymbols.length, visitSymbols.symbols.length, $('circle').length);
 
                 lastTimestamp = report[0].lastActionTimestamp;
 
                 visitSymbols.layout().render();
+
+                console.log('rendered', visitSymbols.symbols.length, $('circle').length);
 
                 $.each(newSymbols, function(i, s) {
                     if (i>10) return false;
@@ -161,6 +173,8 @@ RealTimeMap.run = function(config) {
                         s.path.animate({ fill: col, r: rad }, 700, 'bounce');
                     }, s.data.lastActionTimestamp + config.liveRefreshAfterMs - now);
                 });
+
+                console.log('animated', visitSymbols.symbols.length, $('circle').length);
             }
 
         });
