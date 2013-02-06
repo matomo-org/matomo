84a2e9e92e909fa7d60abfde6c78e4f79ded4c6b
Fabian Becker <halfdan@xnorfz.de>
2013-02-06 01:19:13 +0000

    Warning is now shown on General Settings page to warn user when tracking is disabled via record_statistics = 0.
    fixes #3551

--- plugins/CoreAdminHome/Controller.php
+++ plugins/CoreAdminHome/Controller.php
@@ -27,7 +27,7 @@ class Piwik_CoreAdminHome_Controller extends Piwik_Controller_Admin
 	{
 		Piwik::checkUserHasSomeAdminAccess();
 		$view = Piwik_View::factory('generalSettings');
-		
+
 		if(Piwik::isUserIsSuperUser())
 		{
 			$enableBrowserTriggerArchiving = Piwik_ArchiveProcessing::isBrowserTriggerArchivingEnabled();
@@ -46,14 +46,19 @@ class Piwik_CoreAdminHome_Controller extends Piwik_Controller_Admin
 			{
 				$view->configFileNotWritable = true;
 			}
+
+			$statsEnabled = Piwik_Config::getInstance()->Tracker['record_statistics'];
+			if($statsEnabled == "0"){
+				$view->statisticsNotRecorded = true;
+			}
 			$view->mail = Piwik_Config::getInstance()->mail;
 
 			$view->branding = Piwik_Config::getInstance()->branding;
-			
+
 			$directoryWritable = is_writable(PIWIK_DOCUMENT_ROOT.'/themes/');
 			$logoFilesWriteable = is_writeable(PIWIK_DOCUMENT_ROOT.'/themes/logo.png') && is_writeable(PIWIK_DOCUMENT_ROOT.'/themes/logo-header.png');
 			$view->logosWriteable = ($logoFilesWriteable || $directoryWritable) && ini_get('file_uploads') == 1;
-			
+
 			$trustedHosts = array();
 			if (isset(Piwik_Config::getInstance()->General['trusted_hosts']))
 			{
