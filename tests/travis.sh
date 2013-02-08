46e62bd7afbd164e1cb696d8f88d6b7bdaa5ee57
Fabian Becker <halfdan@xnorfz.de>
2013-02-08 00:02:34 +0000

    PHPUnit tests for piwik require a running webserver.
    travis/setup_webserver.sh will install nginx + php-fpm
    and create configuration files.
    
    travis.sh adjusted to reflect changes.
    
    Moved everything to tests/
    
    refs #3722

--- tests/travis.sh
+++ tests/travis.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+set -e
 
 # Copy Piwik configuration
+echo "Install config.ini.php"
 cp ./tests/PHPUnit/config.ini.travis.php ./config/config.ini.php
+
+# Prepare phpunit.xml
+echo "Adjusting phpunit.xml"
 cp ./tests/PHPUnit/phpunit.xml.dist ./tests/PHPUnit/phpunit.xml
+sed -i 's/@REQUEST_URI/\//g' ./tests/PHPUnit/phpunit.xml
 
 # Create tmp/ sub-directories
 mkdir ./tmp/assets
