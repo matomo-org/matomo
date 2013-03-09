e1d5ccb487c261c7e71d8c64d28f3f7de13d34aa
Fabian Becker <halfdan@xnorfz.de>
2013-03-09 15:45:30 +0100

    Restructure travis build. This now builds PHP 5.3/5.4/5.5 x 3 Testsuites + all PHP x whole test (for code coverage)

---
+++ tests/PHPUnit/travis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -n "$TEST_SUITE"]
+then
+	phpunit --configuration phpunit.xml --testsuite $TEST_SUITE --coverage-text --colors
+else
+	phpunit --configuration phpunit.xml --coverage-text --colors
+fi
