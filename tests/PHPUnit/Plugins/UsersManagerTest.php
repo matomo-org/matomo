c144eb6934c29ed9ceb35af0d1c8d53883eacf01
pebosi <mail@pebosi.net>
2013-04-05 11:11:34 +0200

    fix test to allow apostroph

--- tests/PHPUnit/Plugins/UsersManagerTest.php
+++ tests/PHPUnit/Plugins/UsersManagerTest.php
@@ -225,7 +225,7 @@ class UsersManagerTest extends DatabaseTestCase
     public function getWrongEmailTestData()
     {
         return array(
-            array("geggeqgeqag", "geqgeagae", "ema'il@email.com", "alias"),
+            array("geggeqgeqag", "geqgeagae", "alias"),
             array("geggeqgeqag", "geqgeagae", "@email.com", "alias"),
             array("geggeqgeqag", "geqgeagae", "email@.com", "alias"),
             array("geggeqgeqag", "geqgeagae", "email@4.", "alias"),
