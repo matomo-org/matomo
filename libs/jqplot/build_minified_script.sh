91e38e4f595acf17a58dba08fcab38c3855235af
Fabian Becker <halfdan@xnorfz.de>
2013-04-08 13:16:38 +0200

    Fix build script to not include -custom.min.js

--- libs/jqplot/build_minified_script.sh
+++ libs/jqplot/build_minified_script.sh
@@ -1,7 +1,7 @@
 # build jqplot-custom.min.js
 # the yuicompressor needs to be set up in piwik/js (see piwik/js/README.md)
 
-find . -name 'jqplot*.js' -print|xargs cat > jqplot-custom.min.js-temp
+find . -name 'jqplot.*.js' -print|xargs cat > jqplot-custom.min.js-temp
 
 java -jar ../../js/yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar --type js --line-break 1000 jqplot-custom.min.js-temp > jqplot-custom.min.js 
 
