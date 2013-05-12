5ba38f4fa11a1b33f075acc7ebaa23f92f1e6343
mattab <matthieu.aubry@gmail.com>
2013-05-12 22:00:46 +1200

    Fixing a bug with row evolution label containing commas

--- plugins/API/API.php
+++ plugins/API/API.php
@@ -1162,11 +1162,9 @@ class Piwik_API_API
         }
 
         $label = Piwik_API_ResponseBuilder::unsanitizeLabelParameter($label);
-        $label = urldecode($label);
         if ($label) {
             $labels = explode(',', $label);
             $labels = array_unique($labels);
-//            $labels = array_map('urldecode', $labels);
         } else {
             $labels = array();
         }
