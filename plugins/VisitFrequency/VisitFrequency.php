48c7a1e945d945d6d8bd998a1f355ed4c02f23d3
mattab <matthieu.aubry@gmail.com>
2013-06-06 14:07:11 +1200

    // OMG THIS IS SO WRONG!     // use segment instead

--- plugins/VisitFrequency/VisitFrequency.php
+++ plugins/VisitFrequency/VisitFrequency.php
@@ -78,72 +78,23 @@ class Piwik_VisitFrequency extends Piwik_Plugin
         Piwik_AddMenu('General_Visitors', 'VisitFrequency_SubmenuFrequency', array('module' => 'VisitFrequency', 'action' => 'index'));
     }
 
-    /**
-     * @param Piwik_Event_Notification $notification  notification object
-     * @return mixed
-     */
+    function archiveDay($notification)
+    {
+        $archiveProcessing = $notification->getNotificationObject();
+        if (!$archiveProcessing->shouldProcessReportsForPlugin($this->getPluginName())) return;
+
+        $archiving = new Piwik_VisitFrequency_Archiving();
+        $archiving->archiveDay($archiveProcessing);
+    }
+
     function archivePeriod($notification)
     {
         $archiveProcessing = $notification->getNotificationObject();
-
         if (!$archiveProcessing->shouldProcessReportsForPlugin($this->getPluginName())) return;
 
-        $numericToSum = array(
-            'nb_visits_returning',
-            'nb_actions_returning',
-            'sum_visit_length_returning',
-            'bounce_count_returning',
-            'nb_visits_converted_returning',
-        );
-        $archiveProcessing->archiveNumericValuesSum($numericToSum);
-        $archiveProcessing->archiveNumericValuesMax('max_actions_returning');
+        $archiving = new Piwik_VisitFrequency_Archiving();
+        $archiving->archivePeriod($archiveProcessing);
     }
 
-    /**
-     * @param Piwik_Event_Notification $notification  notification object
-     * @return mixed
-     */
-    function archiveDay($notification)
-    {
-        /* @var $archiveProcessing Piwik_ArchiveProcessing */
-        $archiveProcessing = $notification->getNotificationObject();
-
-        if (!$archiveProcessing->shouldProcessReportsForPlugin($this->getPluginName())) return;
-
-        $select = "count(distinct log_visit.idvisitor) as nb_uniq_visitors_returning,
-				count(*) as nb_visits_returning,
-				sum(log_visit.visit_total_actions) as nb_actions_returning,
-				max(log_visit.visit_total_actions) as max_actions_returning,
-				sum(log_visit.visit_total_time) as sum_visit_length_returning,
-				sum(case log_visit.visit_total_actions when 1 then 1 when 0 then 1 else 0 end) as bounce_count_returning,
-				sum(case log_visit.visit_goal_converted when 1 then 1 else 0 end) as nb_visits_converted_returning";
-
-        $from = "log_visit";
-
-        $where = "log_visit.visit_last_action_time >= ?
-				AND log_visit.visit_last_action_time <= ?
-		 		AND log_visit.idsite = ?
-		 		AND log_visit.visitor_returning >= 1";
-
-        $bind = array($archiveProcessing->getStartDatetimeUTC(),
-                      $archiveProcessing->getEndDatetimeUTC(), $archiveProcessing->idsite);
-
-        $query = $archiveProcessing->getSegment()->getSelectQuery($select, $from, $where, $bind);
-
-        $row = $archiveProcessing->db->fetchRow($query['sql'], $query['bind']);
-
-        if ($row === false || $row === null) {
-            $row['nb_visits_returning'] = 0;
-            $row['nb_actions_returning'] = 0;
-            $row['max_actions_returning'] = 0;
-            $row['sum_visit_length_returning'] = 0;
-            $row['bounce_count_returning'] = 0;
-            $row['nb_visits_converted_returning'] = 0;
-        }
-
-        foreach ($row as $name => $value) {
-            $archiveProcessing->insertNumericRecord($name, $value);
-        }
-    }
 }
 
