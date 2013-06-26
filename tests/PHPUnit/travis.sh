bd0123e9615e96491a7ae981a42f8873d8b83860
Benaka Moorthi <benaka.moorthi@gmail.com>
2013-06-25 22:18:02 -0700

    Investigating travis build failure... (first commit)

--- tests/PHPUnit/travis.sh
+++ tests/PHPUnit/travis.sh
@@ -2,7 +2,12 @@
 
 if [ -n "$TEST_SUITE" ]
 then
-	phpunit --configuration phpunit.xml --testsuite $TEST_SUITE --colors
+  if [ "$TEST_SUITE" -eq "Integration" ]
+  then
+	  phpunit --configuration phpunit.xml --filter Test_Piwik_Integration_NoVisit --colors Integration
+	else
+	  phpunit --configuration phpunit.xml --testsuite $TEST_SUITE --colors
+	fi
 else
 	phpunit --configuration phpunit.xml --coverage-text --colors
 fi
