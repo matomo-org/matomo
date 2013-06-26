3add290ae52d4baa49e7cc851f7e95989ae38ad5
Benaka Moorthi <benaka.moorthi@gmail.com>
2013-06-26 00:07:01 -0700

    Investigating travis build failure... (15th commit)

--- core/Tracker/Visit.php
+++ core/Tracker/Visit.php
@@ -964,6 +964,7 @@ class Piwik_Tracker_Visit implements Piwik_Tracker_Visit_Interface
     protected function isUserAgentExcluded($ua)
     {
         $websiteAttributes = Piwik_Tracker_Cache::getCacheWebsiteAttributes($this->idsite);
+        echo print_r($websiteAttributes, true);exit;
         if (!empty($websiteAttributes['excluded_user_agents'])) {
             foreach ($websiteAttributes['excluded_user_agents'] as $excludedUserAgent) {
                 // if the excluded user agent string part is in this visit's user agent, this visit should be excluded
