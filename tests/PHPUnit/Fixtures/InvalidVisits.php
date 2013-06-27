a0d61d54f1f42243f934f291d00369ccae941c36
Benaka Moorthi <benaka.moorthi@gmail.com>
2013-06-26 20:04:22 -0700

    Investigating travis build failure... (40th commit)

--- tests/PHPUnit/Fixtures/InvalidVisits.php
+++ tests/PHPUnit/Fixtures/InvalidVisits.php
@@ -72,7 +72,7 @@ class Test_Piwik_Fixture_InvalidVisits extends Test_Piwik_BaseFixture
             // test w/ global excluded User Agent
             $t->setUserAgent('Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.2.6) Gecko/20100625 Firefox/3.6.6 (.NET CLR 3.5.30729) (globalexcludeduseragent)');
             $t->setIp('211.1.2.3');
-            self::checkResponse($t->doTrackPageView('visit from global excluded User Agent'));
+            //self::checkResponse($t->doTrackPageView('visit from global excluded User Agent'));
 
             // test with excluded IP
             $t->setUserAgent('Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.2.6) Gecko/20100625 Firefox/3.6.6 (.NET CLR 3.5.30729)'); // restore normal user agent
