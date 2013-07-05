ad1c26ba3c6125056398e621da00243792f8ff27
Benaka Moorthi <benaka.moorthi@gmail.com>
2013-07-04 21:11:56 -0400

    Moved event dispatching logic to new singleton object.

--- core/PluginsManager.php
+++ core/PluginsManager.php
@@ -16,6 +16,7 @@
  * @see core/Menu/Top.php
  * @see core/PluginsFunctions/WidgetsList.php
  * @see core/PluginsFunctions/Sql.php
+ * @see core/EventDispatcher.php
  */
 require_once PIWIK_INCLUDE_PATH . '/core/Menu/Abstract.php';
 require_once PIWIK_INCLUDE_PATH . '/core/Menu/Main.php';
@@ -23,6 +24,7 @@ require_once PIWIK_INCLUDE_PATH . '/core/Menu/Admin.php';
 require_once PIWIK_INCLUDE_PATH . '/core/Menu/Top.php';
 require_once PIWIK_INCLUDE_PATH . '/core/PluginsFunctions/WidgetsList.php';
 require_once PIWIK_INCLUDE_PATH . '/core/PluginsFunctions/Sql.php';
+require_once PIWIK_INCLUDE_PATH . '/core/EventDispatcher.php';
 
 /**
  * Plugin manager
@@ -32,11 +34,6 @@ require_once PIWIK_INCLUDE_PATH . '/core/PluginsFunctions/Sql.php';
  */
 class Piwik_PluginsManager
 {
-    // implementation details for postEvent
-    const EVENT_CALLBACK_GROUP_FIRST = 0;
-    const EVENT_CALLBACK_GROUP_SECOND = 1;
-    const EVENT_CALLBACK_GROUP_THIRD = 2;
-    
     protected $pluginsToLoad = array();
 
     protected $doLoadPlugins = true;
@@ -55,27 +52,6 @@ class Piwik_PluginsManager
         'Proxy',
         'LanguagesManager',
     );
-    
-    /**
-     * Array of observers (callbacks attached to events) that are not methods
-     * of plugin classes.
-     * 
-     * @var array
-     */
-    private $extraObservers = array();
-    
-    /**
-     * Array storing information for all pending events. Each item in the array
-     * will be an array w/ two elements:
-     * 
-     * array(
-     *     'Event.Name',                  // the event name
-     *     array('event', 'parameters')   // the parameters to pass to event observers
-     * )
-     * 
-     * @var array
-     */
-    private $pendingEvents = array();
 
     static private $instance = null;
 
@@ -440,7 +416,7 @@ class Piwik_PluginsManager
 
         $this->addLoadedPlugin($pluginName, $newPlugin);
         
-        $this->postPendingEventsTo($newPlugin);
+        Piwik_EventDispatcher::getInstance()->postPendingEventsTo($newPlugin);
 
         return $newPlugin;
     }
@@ -630,130 +606,6 @@ class Piwik_PluginsManager
             Piwik_Config::getInstance()->forceSave();
         }
     }
-    
-    /**
-     * Triggers an event, executing all callbacks associated with it.
-     * 
-     * @param string $eventName The name of the event, ie, API.getReportMetadata.
-     * @param array $params The parameters to pass to each callback when executing.
-     * @param bool $pending Whether this event should be posted again for plugins
-     *                      loaded after the event is fired.
-     * @param array|null $plugins The plugins to post events to. If null, the event
-     *                            is posted to all plugins. The elements of this array
-     *                            can be either the Piwik_Plugin objects themselves
-     *                            or their string names.
-     */
-    public function postEvent($eventName, $params, $pending = false, $plugins = null)
-    {
-        if ($pending) {
-            $this->pendingEvents[] = array($eventName, $params);
-        }
-        
-        if (empty($plugins)) {
-            $plugins = $this->getLoadedPlugins();
-        }
-        
-        $callbacks = array();
-        
-        // collect all callbacks to execute
-        foreach ($plugins as $plugin) {
-            if (is_string($plugin)) {
-                $plugin = $this->getLoadedPlugin($plugin);
-            }
-            
-            $hooks = $plugin->getListHooksRegistered();
-            
-            if (isset($hooks[$eventName])) {
-                list($pluginFunction, $callbackGroup) = $this->getCallbackFunctionAndGroupNumber($hooks[$eventName]);
-                
-                $callbacks[$callbackGroup][] = array($plugin, $pluginFunction);
-            }
-        }
-        
-        if (isset($this->extraObservers[$eventName])) {
-            foreach ($this->extraObservers[$eventName] as $callbackInfo) {
-                list($callback, $callbackGroup) = $this->getCallbackFunctionAndGroupNumber($callbackInfo);
-                
-                $callbacks[$callbackGroup][] = $callback;
-            }
-        }
-        
-        // execute callbacks in order
-        foreach ($callbacks as $callbackGroup) {
-            foreach ($callbackGroup as $callback) {
-                call_user_func_array($callback, $params);
-            }
-        }
-    }
-    
-    private function getCallbackFunctionAndGroupNumber($hookInfo)
-    {
-        if (is_array($hookInfo)
-            && !empty($hookInfo['function'])
-        ) {
-            $pluginFunction = $hookInfo['function'];
-            if (!empty($hookInfo['before'])) {
-                $callbackGroup = self::EVENT_CALLBACK_GROUP_FIRST;
-            } else if (!empty($hookInfo['after'])) {
-                $callbackGroup = self::EVENT_CALLBACK_GROUP_SECOND;
-            } else {
-                $callbackGroup = self::EVENT_CALLBACK_GROUP_THIRD;
-            }
-        } else {
-            $pluginFunction = $hookInfo;
-            $callbackGroup = self::EVENT_CALLBACK_GROUP_SECOND;
-        }
-        
-        return array($pluginFunction, $callbackGroup);
-    }
-    
-    /**
-     * Associates a callback that is not a plugin class method with an event
-     * name.
-     * 
-     * @param string $eventName
-     * @param array $callback This can be a normal PHP callback or an array
-     *                        that looks like this:
-     *                        array(
-     *                            'function' => $callback,
-     *                            'before' => true
-     *                        )
-     *                        or this:
-     *                        array(
-     *                            'function' => $callback,
-     *                            'after' => true
-     *                        )
-     *                        If 'before' is set, the callback will be executed
-     *                        before normal & 'after' ones. If 'after' then it
-     *                        will be executed after normal ones.
-     */
-    public function addObserver($eventName, $callback)
-    {
-        $this->extraObservers[$eventName][] = $callback;
-    }
-    
-    /**
-     * Removes all registered observers for an event name. Only used for testing.
-     * 
-     * @param string $eventName
-     */
-    public function clearObservers($eventName)
-    {
-        $this->extraObservers[$eventName] = array();
-    }
-    
-    /**
-     * Re-posts all pending events to the given plugin.
-     * 
-     * @param Piwik_Plugin $plugin
-     */
-    private function postPendingEventsTo($plugin)
-    {
-        foreach ($this->pendingEvents as $eventInfo) {
-            list($eventName, $eventParams) = $eventInfo;
-            $this->postEvent($eventName, $eventParams, $pending = false, array($plugin));
-        }
-    }
 }
 
 /**
@@ -770,28 +622,5 @@ class Piwik_PluginsManager_PluginException extends Exception
 				entry in your config/config.ini.php file:
 				PluginsInstalled[] = $pluginName");
     }
-}
-
-/**
- * Post an event to the dispatcher which will notice the observers.
- * 
- * @param string $eventName  The event name.
- * @param array $params The parameter array to forward to observer callbacks.
- * @return void
- */
-function Piwik_PostEvent($eventName, $params = array(), $pending = false, $plugins = null)
-{
-    Piwik_PluginsManager::getInstance()->postEvent($eventName, $params, $pending, $plugins);
-}
-
-/**
- * Register an action to execute for a given event
- *
- * @param string $eventName  Name of event
- * @param function $function  Callback hook
- */
-function Piwik_AddAction($eventName, $function)
-{
-    Piwik_PluginsManager::getInstance()->addObserver($eventName, $function);
 }
 
