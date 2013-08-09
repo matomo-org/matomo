84d2c1789ae50e179232e0bea140e2acfd7f188f
mattab <matthieu.aubry@gmail.com>
2013-08-09 18:21:20 +0200

    Fixing tracker namespace

--- core/Tracker.php
+++ core/Tracker.php
@@ -485,7 +485,7 @@ class Tracker
     {
         switch ($configDb['adapter']) {
             case 'PDO\MYSQL':
-            case 'PDO_MYSQL':
+            case 'PDO_MYSQL': // old format pre Piwik 2
                 require_once PIWIK_INCLUDE_PATH . '/core/Tracker/Db/Pdo/Mysql.php';
                 return new Mysql($configDb);
 
