8b01230fc5f0424e2dc8721c29737f7f4b25498c
sgiehl <stefan@piwik.org>
2013-08-30 18:44:34 +0200

    added translations filter to filter encoded entities

---
+++ core/Translate/Filter/EncodedEntities.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ * @category Piwik
+ * @package Piwik
+ */
+
+namespace Piwik\Translate\Filter;
+
+use Piwik\Translate\Filter\FilterAbstract;
+use Piwik\TranslationWriter;
+
+/**
+ * @package Piwik
+ * @subpackage Piwik_Translate
+ */
+class EncodedEntities extends FilterAbstract
+{
+    /**
+     * Filter the given translations
+     *
+     * @param array $translations
+     *
+     * @return array   filtered translations
+     *
+     */
+    public function filter($translations)
+    {
+        foreach ($translations AS $pluginName => $pluginTranslations) {
+            foreach ($pluginTranslations AS $key => $translation) {
+
+                // remove encoded entities
+                $decoded = TranslationWriter::clean($translation);
+                if ($translation != $decoded) {
+                    $this->_filteredData[$pluginName][$key] = $translation;
+                    $translations[$pluginName][$key] = $decoded;
+                    continue;
+                }
+
+            }
+        }
+
+        return $translations;
+    }
+}
