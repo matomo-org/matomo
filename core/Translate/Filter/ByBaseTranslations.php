30c244736fe04a6c2f3fee40ed6ecae0071fc655
sgiehl <stefan@piwik.org>
2013-08-30 18:39:11 +0200

    added translations filter to filter by available base translations

---
+++ core/Translate/Filter/ByBaseTranslations.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ * @category Piwik
+ * @package Piwik
+ */
+
+namespace Piwik\Translate\Filter;
+
+use Piwik\Translate\Filter\FilterAbstract;
+
+/**
+ * @package Piwik
+ * @subpackage Piwik_Translate
+ */
+class ByBaseTranslations extends FilterAbstract
+{
+    /**
+     * Filter the given translations
+     *
+     * @param array $translations
+     *
+     * @return array   filtered translations
+     *
+     */
+    public function filter($translations)
+    {
+        $cleanedTranslations = array();
+
+        // filter out all translations that don't exist in english translations
+        foreach ($translations AS $pluginName => $pluginTranslations) {
+
+            if (empty($this->_baseTranslations[$pluginName])) {
+                $this->_filteredData[$pluginName] = $pluginTranslations;
+                continue;
+            }
+
+            foreach ($pluginTranslations as $key => $translation) {
+                if (isset($this->_baseTranslations[$pluginName][$key])) {
+                    $cleanedTranslations[$pluginName][$key] = $translation;
+                }
+            }
+
+            if (!empty($cleanedTranslations[$pluginName])) {
+                $diff = array_diff($translations[$pluginName], $cleanedTranslations[$pluginName]);
+            } else {
+                $diff = $translations[$pluginName];
+            }
+            if (!empty($diff)) $this->_filteredData[$pluginName] = $diff;
+        }
+
+        return $cleanedTranslations;
+    }
+}
