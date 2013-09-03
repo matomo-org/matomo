ec0bf53b89eca973601eb9acef901a12f7dcd169
Marcin Czołnowski <marcin@czolnowski.net>
2013-09-03 13:42:15 +0200

    #2781 Segments paramater into Goals API getItems* methods.

--- plugins/Goals/API.php
+++ plugins/Goals/API.php
@@ -215,14 +215,14 @@ class API
      * Returns a datatable of Items SKU/name or categories and their metrics
      * If $abandonedCarts set to 1, will return items abandoned in carts. If set to 0, will return items ordered
      */
-    protected function getItems($recordName, $idSite, $period, $date, $abandonedCarts)
+    protected function getItems($recordName, $idSite, $period, $date, $segment, $abandonedCarts)
     {
         Piwik::checkUserHasViewAccess($idSite);
         $recordNameFinal = $recordName;
         if ($abandonedCarts) {
             $recordNameFinal = Archiver::getItemRecordNameAbandonedCart($recordName);
         }
-        $archive = Archive::build($idSite, $period, $date);
+        $archive = Archive::build($idSite, $period, $date, $segment);
         $dataTable = $archive->getDataTable($recordNameFinal);
 
         $dataTable->filter('Sort', array(Metrics::INDEX_ECOMMERCE_ITEM_REVENUE));
@@ -332,19 +332,19 @@ class API
         $dataTable->addDataTable($ecommerceViews);
     }
 
-    public function getItemsSku($idSite, $period, $date, $abandonedCarts = false)
+    public function getItemsSku($idSite, $period, $date, $segment = false, $abandonedCarts = false)
     {
-        return $this->getItems('Goals_ItemsSku', $idSite, $period, $date, $abandonedCarts);
+        return $this->getItems('Goals_ItemsSku', $idSite, $period, $date, $segment, $abandonedCarts);
     }
 
-    public function getItemsName($idSite, $period, $date, $abandonedCarts = false)
+    public function getItemsName($idSite, $period, $date, $segment = false, $abandonedCarts = false)
     {
-        return $this->getItems('Goals_ItemsName', $idSite, $period, $date, $abandonedCarts);
+        return $this->getItems('Goals_ItemsName', $idSite, $period, $date, $segment, $abandonedCarts);
     }
 
-    public function getItemsCategory($idSite, $period, $date, $abandonedCarts = false)
+    public function getItemsCategory($idSite, $period, $date, $segment = false, $abandonedCarts = false)
     {
-        return $this->getItems('Goals_ItemsCategory', $idSite, $period, $date, $abandonedCarts);
+        return $this->getItems('Goals_ItemsCategory', $idSite, $period, $date, $segment, $abandonedCarts);
     }
 
     /**
