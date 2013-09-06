03bff6db73721e9d7d843db586092bb4fee983d4
mattab <matthieu.aubry@gmail.com>
2013-09-06 16:05:42 +1200

    Revert this change

--- misc/log-analytics/import_logs.py
+++ misc/log-analytics/import_logs.py
@@ -808,7 +808,7 @@ class Piwik(object):
         try:
             return json.loads(res)
         except ValueError:
-            truncate_after = 3000
+            truncate_after = 300
             raise urllib2.URLError('Piwik returned an invalid response: ' + res[:truncate_after])
 
 
@@ -825,7 +825,7 @@ class Piwik(object):
                     if on_failure is not None:
                         error_message = on_failure(response, kwargs.get('data'))
                     else:
-                        truncate_after = 2000
+                        truncate_after = 300
                         truncated_response = (response[:truncate_after] + '..') if len(response) > truncate_after else response
                         error_message = "didn't receive the expected response. Response was %s " % truncated_response
 
