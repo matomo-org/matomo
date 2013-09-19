dbf1fcac60a2d4273d36baab4227b50f3326399d
Benaka Moorthi <benaka.moorthi@gmail.com>
2013-09-19 15:45:06 -0400

    Display link to diffviewer in upload_artifacts.sh.

--- tests/travis/upload_artifacts.sh
+++ tests/travis/upload_artifacts.sh
@@ -31,6 +31,8 @@ else
             
             tar -cjf screenshot-diffs.tar.bz2 screenshot-diffs
             curl -X POST --data-binary @screenshot-diffs.tar.bz2 "$url_base&artifact_name=screenshot-diffs"
+
+            echo "View UI failures (if any) here: http://builds-artifacts.piwik.org/ui-tests.master/$TRAVIS_JOB_NUMBER/screenshot-diffs/diffviewer.php"
         fi
     else
         echo "No artifacts for $TEST_SUITE tests."
