64615b167487e55757be99e0e052cadd8b08bf08
mattab <matthieu.aubry@gmail.com>
2013-09-19 15:28:30 +1200

    Adding ExampleAPI to work around bug reported in http://forum.piwik.org/read.php?2,106317

---
+++ plugins/ExampleAPI/ExampleAPI.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ * @category Piwik_Plugins
+ * @package ExampleAPI
+ */
+namespace Piwik\Plugins\ExampleAPI;
+
+/**
+ * This plugin only provides an API.php file, so we leave the plugin class empty
+ *
+ * @package ExamplePlugin
+ */
+class ExampleAPI extends \Piwik\Plugin
+{
+
+}
