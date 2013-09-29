2b11c2f677d3b468b23fc895bb7691a1a31bc6fe
Fabian Becker <fabian.becker@uni-tuebingen.de>
2013-09-29 21:58:44 +0200

    Add another falsely excluded file

---
+++ core/Updates/2.0-a7.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ * @category Piwik
+ * @package Piwik
+ */
+use Piwik\Common;
+use Piwik\Updater;
+use Piwik\Updates;
+
+/**
+ * @package Updates
+ */
+class Piwik_Updates_2_0_a7 extends Updates
+{
+    static function getSql($schema = 'Myisam')
+    {
+        return array(
+            // ignore existing column name error (1060)
+            'ALTER TABLE ' . Common::prefixTable('logger_message')
+                . " ADD COLUMN tag VARCHAR(50) NULL AFTER idlogger_message" => 1060,
+
+            'ALTER TABLE ' . Common::prefixTable('logger_message')
+                . " ADD COLUMN level TINYINT AFTER timestamp" => 1060,
+        );
+    }
+
+    static function update()
+    {
+        // add tag & level columns to logger_message table
+        Updater::updateDatabase(__FILE__, self::getSql());
+    }
+}
