b79c18f59eb6ba5ec30a195a71bb8c67bbd5aeb5
Thomas Steur <thomas.steur@gmail.com>
2013-09-30 20:56:30 +0000

    refs #546 display the name of the plugin in the confirm message

--- plugins/CorePluginsAdmin/javascripts/pluginOverview.js
+++ plugins/CorePluginsAdmin/javascripts/pluginOverview.js
@@ -7,14 +7,25 @@
 
 $(document).ready(function () {
 
+    var uninstallConfirmMessage = '';
+
     $('#plugins .uninstall').click(function (event) {
         event.preventDefault();
 
         var link = $(this).attr('href');
-        
-        if (!link) {
+        var pluginName = $(this).attr('data-pluginName');
+
+        if (!link || !pluginName) {
             return;
         }
+
+        if (!uninstallConfirmMessage) {
+            uninstallConfirmMessage = $('#uninstallPluginConfirm').text();
+        }
+
+        var messageToDisplay = uninstallConfirmMessage.replace('%s', pluginName);
+
+        $('#uninstallPluginConfirm').text(messageToDisplay);
 
         piwikHelper.modalConfirm('#confirmUninstallPlugin', {
             yes: function () {
