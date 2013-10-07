ded987ac03cf76a297179fbe4cb2189294e40185
mattab <matthieu.aubry@gmail.com>
2013-10-08 12:19:10 +1300

    Fixes #4203 DevicesDetection 1.14 version schema update to account for longer strings ( fields config_os_version and config_device_type are not large enough ) Refs #4204 Plugin schema example with DevicesDetection

---
+++ plugins/DevicesDetection/Updates/1.14.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ * @category Piwik
+ * @package Updates
+ */
+
+namespace Piwik\Plugins\DevicesDetection;
+
+use Piwik\Common;
+use Piwik\Updater;
+use Piwik\Updates;
+
+class Updates_1_14 extends Updates
+{
+    static function getSql($schema = 'Myisam')
+    {
+        return array(
+            'ALTER TABLE `' . Common::prefixTable('log_visit') . '`
+				CHANGE `config_os_version` `config_os_version`  VARCHAR( 100 ) DEFAULT NULL,
+				CHANGE `config_device_type` `config_device_type`  VARCHAR( 100 ) DEFAULT NULL' => false,
+        );
+    }
+
+    static function update()
+    {
+        Updater::updateDatabase(__FILE__, self::getSql());
+    }
+}
