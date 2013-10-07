64a7597c17726e30c0a6164cf27e2e0c0b331d3f
mattab <matthieu.aubry@gmail.com>
2013-10-07 19:00:35 +1300

    Refs #4125  adding backtick so it works if there is no prefix (where `option` is a mysql reserved keyword)

--- core/Updates/2.0-a13.php
+++ core/Updates/2.0-a13.php
@@ -25,21 +25,21 @@ class Piwik_Updates_2_0_a13 extends Updates
         $tables = \Piwik\DbHelper::getTablesInstalled();
         foreach ($tables as $tableName) {
             if (strpos($tableName, 'archive_') !== false) {
-                $sql['UPDATE ' . $tableName . ' SET `name`=REPLACE(`name`, \'Referers_\', \'Referrers_\') WHERE `name` LIKE \'Referers_%\''] = false;
+                $sql['UPDATE `' . $tableName . '` SET `name`=REPLACE(`name`, \'Referers_\', \'Referrers_\') WHERE `name` LIKE \'Referers_%\''] = false;
             }
         }
         $errorCodeTableNotFound = '1146';
 
         // Rename custom segments containing Referers segments
-        $sql['UPDATE ' . Common::prefixTable('segment') . ' SET `definition`=REPLACE(`definition`, \'referer\', \'referrer\') WHERE `definition` LIKE \'%referer%\''] = $errorCodeTableNotFound;
+        $sql['UPDATE `' . Common::prefixTable('segment') . '` SET `definition`=REPLACE(`definition`, \'referer\', \'referrer\') WHERE `definition` LIKE \'%referer%\''] = $errorCodeTableNotFound;
 
         // Rename Referrers reports within scheduled reports
-        $sql['UPDATE ' . Common::prefixTable('report') . ' SET `reports`=REPLACE(`reports`, \'Referer\', \'Referrer\') WHERE `reports` LIKE \'%Referer%\''] = $errorCodeTableNotFound;
+        $sql['UPDATE `' . Common::prefixTable('report') . '` SET `reports`=REPLACE(`reports`, \'Referer\', \'Referrer\') WHERE `reports` LIKE \'%Referer%\''] = $errorCodeTableNotFound;
 
         // Rename Referrers widgets in custom dashboards
-        $sql['UPDATE ' . Common::prefixTable('user_dashboard') . ' SET `layout`=REPLACE(`layout`, \'Referer\', \'Referrer\') WHERE `layout` LIKE \'%Referer%\''] = $errorCodeTableNotFound;
+        $sql['UPDATE `' . Common::prefixTable('user_dashboard') . '` SET `layout`=REPLACE(`layout`, \'Referer\', \'Referrer\') WHERE `layout` LIKE \'%Referer%\''] = $errorCodeTableNotFound;
 
-        $sql['UPDATE ' . Common::prefixTable('option') . ' SET `option_name` = \'version_ScheduledReports\' WHERE `option_name` = \'version_PDFReports\' '] = false;
+        $sql['UPDATE `' . Common::prefixTable('option') . '` SET `option_name` = \'version_ScheduledReports\' WHERE `option_name` = \'version_PDFReports\' '] = false;
 
         return $sql;
     }
