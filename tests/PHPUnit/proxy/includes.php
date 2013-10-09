dd3b7490cd207b7f91b3f51f103079f5d927b49e
mattab <matthieu.aubry@gmail.com>
2013-10-09 19:47:35 +1300

    test proxy endpoints have some commons

---
+++ tests/PHPUnit/proxy/includes.php
@@ -0,0 +1,15 @@
+<?php
+
+// Good old test proxy endpoints have some commons
+
+define('PIWIK_INCLUDE_PATH', realpath(dirname(__FILE__)) . '/../../../');
+define('PIWIK_USER_PATH', PIWIK_INCLUDE_PATH);
+define('PIWIK_PRINT_ERROR_BACKTRACE', true);
+
+require_once PIWIK_INCLUDE_PATH . '/core/Loader.php';
+require_once PIWIK_INCLUDE_PATH . '/core/EventDispatcher.php';
+require_once PIWIK_INCLUDE_PATH . '/core/Piwik.php';
+require_once PIWIK_INCLUDE_PATH . '/libs/upgradephp/upgrade.php';
+require_once PIWIK_INCLUDE_PATH . '/core/functions.php';
+require_once PIWIK_INCLUDE_PATH . '/tests/PHPUnit/TestingEnvironment.php';
+
