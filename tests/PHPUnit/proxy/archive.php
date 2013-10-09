4ef3b7a40107619f92ff8060e618eb128d059918
diosmosis <benakamoorthi@fastmail.fm>
2013-10-09 11:13:29 -0400

    Fixing build.

--- tests/PHPUnit/proxy/archive.php
+++ tests/PHPUnit/proxy/archive.php
@@ -2,9 +2,8 @@
 define('PIWIK_MODE_ARCHIVE', true);
 define('PIWIK_ARCHIVE_NO_TRUNCATE', true);
 
-require realpath(dirname(__FILE__)) . "/includes.php";
-
+require_once realpath(dirname(__FILE__)) . '/tests/PHPUnit/TestingEnvironment.php';
 Piwik_TestingEnvironment::addHooks();
 
 // include archive.php, and let 'er rip
-require_once PIWIK_INCLUDE_PATH . "/misc/cron/archive.php";
+require_once realpath(dirname(__FILE__)) . "/misc/cron/archive.php";
