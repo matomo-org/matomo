381e5d2a942f6a59a8b1c41bc8476419a728716b
Thomas Steur <thomas.steur@gmail.com>
2013-10-14 03:40:48 +0000

    simplify html table

--- plugins/CoreVisualizations/Visualizations/HtmlTable/AllColumns.php
+++ plugins/CoreVisualizations/Visualizations/HtmlTable/AllColumns.php
@@ -11,8 +11,18 @@
 
 namespace Piwik\Plugins\CoreVisualizations\Visualizations\HtmlTable;
 
+use Piwik\DataTable\DataTableInterface;
 use Piwik\Plugins\CoreVisualizations\Visualizations\HtmlTable;
 use Piwik\Visualization\Config;
+use Piwik\Common;
+use Piwik\DataTable\Filter\AddColumnsProcessedMetricsGoal;
+use Piwik\MetricsFormatter;
+use Piwik\Piwik;
+use Piwik\Plugins\Goals\API as APIGoals;
+use Piwik\Site;
+use Piwik\View;
+use Piwik\ViewDataTable\Visualization;
+use Piwik\Visualization\Request;
 
 /**
  * DataTable Visualization that derives from HtmlTable and sets show_extra_columns to true.
@@ -25,6 +35,39 @@ class AllColumns extends HtmlTable
     {
         $properties->visualization_properties->show_extra_columns = true;
 
+        $properties->show_exclude_low_population = true;
+        $properties->datatable_css_class = 'dataTableVizAllColumns';
+
         parent::configureVisualization($properties);
     }
+
+    public function beforeGenericFiltersAreAppliedToLoadedDataTable(DataTableInterface $dataTable, Config $properties, Request $request)
+    {
+        $dataTable->filter('AddColumnsProcessedMetrics');
+
+        $dataTable->filter(function ($dataTable) use ($properties) {
+            $columnsToDisplay = array('label', 'nb_visits');
+
+            if (in_array('nb_uniq_visitors', $dataTable->getColumns())) {
+                $columnsToDisplay[] = 'nb_uniq_visitors';
+            }
+
+            $columnsToDisplay = array_merge(
+                $columnsToDisplay, array('nb_actions', 'nb_actions_per_visit', 'avg_time_on_site', 'bounce_rate')
+            );
+
+            // only display conversion rate for the plugins that do not provide "per goal" metrics
+            // otherwise, conversion rate is meaningless as a whole (since we don't process 'cross goals' conversions)
+            if (!$properties->show_goals) {
+                $columnsToDisplay[] = 'conversion_rate';
+            }
+
+            $properties->columns_to_display = $columnsToDisplay;
+        });
+
+        $prettifyTime = array('\Piwik\MetricsFormatter', 'getPrettyTimeFromSeconds');
+
+        $dataTable->filter('ColumnCallbackReplace', array('avg_time_on_site', $prettifyTime));
+    }
+
 }
