6afd88e64a6cc43e55b9c5b434777d79db3d9040
diosmosis <benakamoorthi@fastmail.fm>
2013-10-21 05:34:01 -0400

    Refs #4200, documented core/DataTable/Simple.php.

--- core/DataTable/Simple.php
+++ core/DataTable/Simple.php
@@ -16,35 +16,28 @@ use Piwik\DataTable;
  * A [DataTable](#) where every row has two columns: **label** and **value**.
  * 
  * Simple DataTables are only used to slightly alter the output of some renderers
- * (namely the XML renderer).
+ * (notably the XML renderer).
  *
  * @package Piwik
  * @subpackage DataTable
+ * @api
  */
 class Simple extends DataTable
 {
     /**
-     * Adds  in the DataTable the array information
+     * Adds rows based on an array mapping label column values to value column
+     * values.
      *
-     * @param array $array Array containing the rows information
-     *                       array(
-     *                             'Label row 1' => $value1,
-     *                             'Label row 2' => $value2,
-     *                       )
+     * @param array $array Array containing the rows, eg,
+     *                     ```
+     *                     array(
+     *                         'Label row 1' => $value1,
+     *                         'Label row 2' => $value2,
+     *                     )
+     *                     ```
      */
     public function addRowsFromArray($array)
     {
         $this->addRowsFromSimpleArray(array($array));
-    }
-
-    /**
-     * Updates the given column with the given value
-     *
-     * @param string $columnName
-     * @param mixed $value
-     */
-    public function setColumn($columnName, $value)
-    {
-        $this->getLastRow()->setColumn($columnName, $value);
     }
 }
