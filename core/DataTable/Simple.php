055eaa81b651c4839d9061311a7e93ceb195c7ed
diosmosis <benakamoorthi@fastmail.fm>
2013-10-21 05:03:14 -0400

    Refs #4200 documented core/API/Request.php and part of core/DataTable/Simple.php

--- core/DataTable/Simple.php
+++ core/DataTable/Simple.php
@@ -13,14 +13,10 @@ namespace Piwik\DataTable;
 use Piwik\DataTable;
 
 /**
- * The DataTable\Simple is used to provide an easy way to create simple DataGrid.
- * A DataTable\Simple is a DataTable with 2 columns: 'label' and 'value'.
- *
- * It is usually best to return a DataTable\Simple instead of
- * a PHP array (or other custom data structure) in API methods:
- * - the generic filters can be applied automatically (offset, limit, pattern search, sort, etc.)
- * - the renderer can be applied (XML, PHP, HTML, etc.)
- * So you don't have to write specific renderer for your data, it is already available in all the formats supported natively by Piwik.
+ * A [DataTable](#) where every row has two columns: **label** and **value**.
+ * 
+ * Simple DataTables are only used to slightly alter the output of some renderers
+ * (namely the XML renderer).
  *
  * @package Piwik
  * @subpackage DataTable
@@ -28,12 +24,12 @@ use Piwik\DataTable;
 class Simple extends DataTable
 {
     /**
-     * Loads (append) in the DataTable the array information
+     * Adds  in the DataTable the array information
      *
      * @param array $array Array containing the rows information
      *                       array(
-     *                             'Label row 1' => Value row 1,
-     *                             'Label row 2' => Value row 2,
+     *                             'Label row 1' => $value1,
+     *                             'Label row 2' => $value2,
      *                       )
      */
     public function addRowsFromArray($array)
