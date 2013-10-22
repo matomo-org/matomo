3fb5c0109015b0da1b587139a96589cba87ca4a1
mattab <matthieu.aubry@gmail.com>
2013-10-22 13:38:17 +1300

    Fix someof build (cherry picked from commit 4dc91fc)

--- core/DataTable/Row.php
+++ core/DataTable/Row.php
@@ -454,7 +454,7 @@ class Row
      *                                     aggregation should be used (min, max). format:
      *                                     `array('column name' => 'function name')`
      */
-    public function sumRow(Row $rowToSum, $enableCopyMetadata = true, $aggregationOperations = null)
+    public function sumRow(Row $rowToSum, $enableCopyMetadata = true, $aggregationOperations = false)
     {
         foreach ($rowToSum->getColumns() as $columnToSumName => $columnToSumValue) {
             if (!isset(self::$unsummableColumns[$columnToSumName])) // make sure we can add this column
