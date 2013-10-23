abcb06605f3ec31723c92a85964563e650358b94
mattab <matthieu.aubry@gmail.com>
2013-10-23 14:19:29 +1300

    Make that message clearer [ci skip]

--- tests/PHPUnit/travis.sh
+++ tests/PHPUnit/travis.sh
@@ -2,7 +2,8 @@
 
 if [ `phpunit --group __nogroup__ | grep "No tests executed" | wc -l` -ne 1 ]
 then
-    echo "There are some tests functions which do not have a @group set. Please add the @group phpdoc comment to these tests:"
+    echo "=====> There are some tests functions which do not have a @group set. "
+    echo "       Please add the @group phpdoc comment to the following tests: <====="
     phpunit --group __nogroup__ --testdox
     exit 1
 else
