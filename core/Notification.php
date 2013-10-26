7b5943a94a57cfc45cc4452735e667f7ddb0796b
Thomas Steur <thomas.steur@gmail.com>
2013-10-26 00:48:54 +0000

    refs #4179 started to work on notifications

---
+++ core/Notification.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ * @category Piwik
+ * @package Piwik
+ */
+namespace Piwik;
+
+/**
+ * @package Piwik
+ * @subpackage Notification
+ * @api
+ */
+class Notification
+{
+    const CONTEXT_SUCCESS = 'success';
+    const CONTEXT_ERROR   = 'error';
+    const CONTEXT_INFO    = 'info';
+    const CONTEXT_WARNING = 'warning';
+
+    const PRIORITY_MIN    = 100;
+    const PRIORITY_LOW    = 50;
+    const PRIORITY_HIGH   = 25;
+    const PRIORITY_MAX    = 1;
+
+    const FLAG_NO_CLEAR   = 1;
+    const FLAG_TOAST      = 2;
+    const FLAG_PERSISTENT = 4;
+    const FLAG_TRANSIENT  = 8;
+
+    public $context;
+    public $title;
+    public $message;
+    public $icon;
+    public $flags;
+    public $priority;
+
+}
---
+++ core/Notification/Manager.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ * @category Piwik
+ * @package Piwik
+ */
+namespace Piwik\Notification;
+
+use Piwik\Notification;
+use Piwik\Session\SessionNamespace;
+
+/**
+ * @package Piwik
+ * @subpackage Notification
+ * @api
+ */
+class Manager
+{
+    private static $session = null;
+
+    /**
+     * Post a notification to be shown in the status bar. If a notification with the same id has already been posted by your application and has not yet been canceled, it will be replaced by the updated information.
+     *
+     * @param string       $id   A unique identifier for this notification
+     * @param Notification $notification
+     */
+    public static function notify($id, Notification $notification)
+    {
+        $session = static::getSession();
+        $session->$id = $notification;
+
+        $session->setExpirationHops(1, $id);
+    }
+
+    public static function getAll()
+    {
+        $session = static::getSession();
+        return $session->getIterator();
+    }
+
+    /**
+     * Cancel a previously registered notification.
+     * @param $id
+     */
+    public static function cancel($id)
+    {
+        $session = static::getSession();
+        unset($session->$id);
+    }
+
+    /**
+     * @return SessionNamespace
+     */
+    private static function getSession()
+    {
+        if (!isset(static::$session)) {
+            static::$session = new SessionNamespace('notification');
+        }
+
+        return static::$session;
+    }
+}
