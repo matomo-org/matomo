91160d978a114fc335866410a77f90ee6cd58eb3
Thomas Steur <thomas.steur@gmail.com>
2013-10-26 23:40:18 +0000

    refs #4179 added some documentation

--- core/Notification.php
+++ core/Notification.php
@@ -22,23 +22,95 @@ class Notification
     const CONTEXT_INFO    = 'info';
     const CONTEXT_WARNING = 'warning';
 
+    /**
+     * Lowest priority
+     */
     const PRIORITY_MIN    = 100;
+
+    /**
+     * Lower priority
+     */
     const PRIORITY_LOW    = 50;
+
+    /**
+     * Higher priority
+     */
     const PRIORITY_HIGH   = 25;
+
+    /**
+     * Highest priority
+     */
     const PRIORITY_MAX    = 1;
 
+    /**
+     * If flag applied, no close icon will be displayed. Please note that persistent notifications always have a close
+     * icon
+     */
     const FLAG_NO_CLEAR   = 1;
 
+    /**
+     * Implies transient. Notification will be displayed for a few seconds and then faded out
+     */
     const TYPE_TOAST      = 'toast';
+
+    /**
+     * Notification will be displayed until the new user explicitly closes the notification
+     */
     const TYPE_PERSISTENT = 'persistent';
+
+    /**
+     * Notification will be displayed only once.
+     */
     const TYPE_TRANSIENT  = 'transient';
 
+    /**
+     * The title of the notification. For instance the plugin name. The title is optional.
+     * @var string
+     */
     public $title;
+
+    /**
+     * The actual message that will be displayed. Must be set.
+     * @var string
+     */
     public $message;
+
+    /**
+     * @var int optional flags. Usage: $notification->flags = Notification::FLAG_BAR | Notification::FLAG_FOO
+     */
     public $flags;
-    public $type     = self::TYPE_TRANSIENT;
-    public $context  = self::CONTEXT_INFO;
+
+    /**
+     * The type of the notification. See self::TYPE_*
+     * @var string
+     */
+    public $type = self::TYPE_TRANSIENT;
+
+    /**
+     * Context of the notification. For instance info, warning, success or error.
+     * @var string
+     */
+    public $context = self::CONTEXT_INFO;
+
+    /**
+     * The priority of the notification, the higher the priority, the higher the order. Notifications having the
+     * highest priority will be displayed first and all other notifications below.
+     * @var int
+     */
     public $priority = self::PRIORITY_LOW;
+
+    /**
+     * @param  string $message   The notification message.
+     * @throws \Exception        In case the message is empty.
+     */
+    public function __construct($message)
+    {
+        if (empty($message)) {
+            throw new \Exception('No notification message given');
+        }
+
+        $this->message = $message;
+    }
 
     public function hasNoClear()
     {
