58f4ab080876856e37e94cf48880824a44b53ff5
mattab <matthieu.aubry@gmail.com>
2013-11-01 15:29:52 +1300

    Removing segment support

--- plugins/PrivacyManager/LogDataPurger.php
+++ plugins/PrivacyManager/LogDataPurger.php
@@ -270,8 +270,7 @@ class LogDataPurger
                                              'idaction_url_ref',
                                              'idaction_name',
                                              'idaction_name_ref',
-                                             'idaction_event_category',
-                                             'idaction_event_action'),
+            ),
 
             'log_conversion'        => array('idaction_url'),
 
