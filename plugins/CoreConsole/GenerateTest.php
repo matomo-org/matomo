a1c6c2e5231c5b0ba7368742b78465f8dd25755e
Thomas Steur <thomas.steur@gmail.com>
2013-11-07 20:07:02 +0000

    refs #1673 automatically attach "Test" to testname if needed, added optional option to define type of test

--- plugins/CoreConsole/GenerateTest.php
+++ plugins/CoreConsole/GenerateTest.php
@@ -11,6 +11,7 @@
 
 namespace Piwik\Plugins\CoreConsole;
 
+use Piwik\Common;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -25,16 +26,24 @@ class GenerateTest extends GeneratePluginBase
         $this->setName('generate:test')
             ->setDescription('Adds a test to an existing plugin')
             ->addOption('pluginname', null, InputOption::VALUE_REQUIRED, 'The name of an existing plugin')
-            ->addOption('testname', null, InputOption::VALUE_REQUIRED, 'The name of the test you want to create');
+            ->addOption('testname', null, InputOption::VALUE_REQUIRED, 'The name of the test you want to create')
+            ->addOption('testtype', 't', InputOption::VALUE_OPTIONAL, 'Whether you want to create a "unit", "integration" or "database" test', 'unit');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $pluginName = $this->getPluginName($input, $output);
         $testName   = $this->getTestName($input, $output);
+        $testClass  = $this->getTestClass($input);
 
         $exampleFolder  = PIWIK_INCLUDE_PATH . '/plugins/ExamplePlugin';
-        $replace        = array('ExamplePlugin' => $pluginName, 'SimpleTest' => $testName);
+        $replace        = array(
+            'ExamplePlugin'               => $pluginName,
+            'SimpleTest'                  => $testName,
+            '\PHPUnit_Framework_TestCase' => $testClass,
+            '@group Plugins'              => '@group ' . $this->getTestType($input)
+         );
+
         $whitelistFiles = array('/tests', '/tests/SimpleTest.php');
 
         $this->copyTemplateToPlugin($exampleFolder, $pluginName, $replace, $whitelistFiles);
@@ -65,10 +74,14 @@ class GenerateTest extends GeneratePluginBase
         };
 
         if (empty($testname)) {
-            $dialog = $this->getHelperSet()->get('dialog');
+            $dialog   = $this->getHelperSet()->get('dialog');
             $testname = $dialog->askAndValidate($output, 'Enter the name of the test: ', $validate);
         } else {
             $validate($testname);
+        }
+
+        if (!Common::stringEndsWith(strtolower($testname), 'test')) {
+            $testname = $testname . 'Test';
         }
 
         $testname = ucfirst($testname);
@@ -108,5 +121,28 @@ class GenerateTest extends GeneratePluginBase
         return $pluginName;
     }
 
+    /**
+     * @param InputInterface $input
+     * @return string
+     */
+    private function getTestClass(InputInterface $input)
+    {
+        $testClass = '\PHPUnit_Framework_TestCase';
+        if ('integration' == $input->getOption('testtype')) {
+            $testClass = '\IntegrationTestCase';
+        } elseif ('database' == $input->getOption('testtype')) {
+            $testClass = '\DatabaseTestCase';
+        }
 
+        return $testClass;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @return string
+     */
+    private function getTestType(InputInterface $input)
+    {
+        return 'unit' == $input->getOption('testtype') ? 'Plugins' : 'Integration';
+    }
 }
