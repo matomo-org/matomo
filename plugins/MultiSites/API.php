0fafe1baa548335cc99e174704d6c86bc594f9c5
Julien Moumné <julien@moumne.com>
2013-11-16 12:49:08 +0100

    fixes #4299 - set site name in past data label column so CalculateEvolutionFilter can lookup divisor

--- plugins/MultiSites/API.php
+++ plugins/MultiSites/API.php
@@ -213,21 +213,7 @@ class API extends \Piwik\Plugin\API
         // $dataTable instanceOf Set
         $dataTable = $archive->getDataTableFromNumeric($fieldsToGet);
 
-        // get rid of the DataTable\Map that is created by the IndexedBySite archive type
-        if ($dataTable instanceof DataTable\Map
-            && $multipleWebsitesRequested
-        ) {
-            $dataTable = $dataTable->mergeChildren();
-        } else {
-            if (!($dataTable instanceof DataTable\Map)
-                && $dataTable->getRowsCount() > 0
-            ) {
-                $firstSite = is_array($idSitesOrIdSite) ? reset($idSitesOrIdSite) : $idSitesOrIdSite;
-
-                $firstDataTableRow = $dataTable->getFirstRow();
-                $firstDataTableRow->setColumn('label', $firstSite);
-            }
-        }
+        $dataTable= $this->mergeDataTableMapAndPopulateLabel($idSitesOrIdSite, $multipleWebsitesRequested, $dataTable);
 
         if ($dataTable instanceof DataTable\Map) {
             foreach ($dataTable->getDataTables() as $table) {
@@ -243,21 +229,21 @@ class API extends \Piwik\Plugin\API
         // if the period isn't a range & a lastN/previousN date isn't used, we get the same
         // data for the last period to show the evolution of visits/actions/revenue
         list($strLastDate, $lastPeriod) = Range::getLastDate($date, $period);
+
         if ($strLastDate !== false) {
+
             if ($lastPeriod !== false) {
                 // NOTE: no easy way to set last period date metadata when range of dates is requested.
                 //       will be easier if DataTable\Map::metadata is removed, and metadata that is
                 //       put there is put directly in DataTable::metadata.
                 $dataTable->setMetadata(self::getLastPeriodMetadataName('date'), $lastPeriod);
             }
+
             $pastArchive = Archive::build($idSitesOrIdSite, $period, $strLastDate, $segment, $_restrictSitesToLogin);
+
             $pastData = $pastArchive->getDataTableFromNumeric($fieldsToGet);
 
-            if ($pastData instanceof DataTable\Map
-                && $multipleWebsitesRequested
-            ) {
-                $pastData = $pastData->mergeChildren();
-            }
+            $pastData = $this->mergeDataTableMapAndPopulateLabel($idSitesOrIdSite, $multipleWebsitesRequested, $pastData);
 
             // use past data to calculate evolution percentages
             $this->calculateEvolutionPercentages($dataTable, $pastData, $apiMetrics);
@@ -520,6 +506,28 @@ class API extends \Piwik\Plugin\API
                 }
             }
         }
+    }
+
+    private function mergeDataTableMapAndPopulateLabel($idSitesOrIdSite, $multipleWebsitesRequested, $dataTable)
+    {
+        // get rid of the DataTable\Map that is created by the IndexedBySite archive type
+        if ($dataTable instanceof DataTable\Map && $multipleWebsitesRequested) {
+
+            return $dataTable->mergeChildren();
+
+        } else {
+
+            if (!$dataTable instanceof DataTable\Map && $dataTable->getRowsCount() > 0) {
+
+                $firstSite = is_array($idSitesOrIdSite) ? reset($idSitesOrIdSite) : $idSitesOrIdSite;
+
+                $firstDataTableRow = $dataTable->getFirstRow();
+
+                $firstDataTableRow->setColumn('label', $firstSite);
+            }
+        }
+
+        return $dataTable;
     }
 }
 
