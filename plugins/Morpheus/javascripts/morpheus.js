f8824a65abe0979691dd03b62ce22aae1f211d6e
mattab <matthieu.aubry@gmail.com>
2013-11-28 22:54:15 +1300

    Refs #4127 Latest update by designers of the new Piwik 2 theme: Morpheus. Work in progress.... Let us know what you think!

--- plugins/Morpheus/javascripts/morpheus.js
+++ plugins/Morpheus/javascripts/morpheus.js
@@ -1,1 +1,10 @@
-console.log('Hello, Neo!');
+$(document).ready(function () {
+    // datepicker actions crashed
+    // $('input').iCheck({
+    //     checkboxClass: 'form-checkbox',
+    //     radioClass: 'form-radio',
+    //     checkedClass: 'checked',
+    //     hoverClass: 'form-hover',
+    // });
+});
+
