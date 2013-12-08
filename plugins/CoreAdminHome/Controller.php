6f53f54f10132b73707f7dd1131aad79be494a55
mattab <matthieu.aubry@gmail.com>
2013-12-09 12:11:29 +1300

    Fixes  #4368 Add support transparent background custom logos. Was Pull request #148, kuddos ademaro

--- plugins/CoreAdminHome/Controller.php
+++ plugins/CoreAdminHome/Controller.php
@@ -341,6 +341,20 @@ class Controller extends \Piwik\Plugin\ControllerAdmin
 
         $logo = imagecreatetruecolor($widthExpected, self::LOGO_HEIGHT);
         $logoSmall = imagecreatetruecolor($smallWidthExpected, self::LOGO_SMALL_HEIGHT);
+
+        // Handle transparency
+        $background = imagecolorallocate($logo, 0, 0, 0);
+        $backgroundSmall = imagecolorallocate($logoSmall, 0, 0, 0);
+        imagecolortransparent($logo, $background);
+        imagecolortransparent($logoSmall, $backgroundSmall);
+
+        if ($_FILES['customLogo']['type'] == 'image/png') {
+            imagealphablending($logo, false);
+            imagealphablending($logoSmall, false);
+            imagesavealpha($logo, true);
+            imagesavealpha($logoSmall, true);
+        }
+
         imagecopyresized($logo, $image, 0, 0, 0, 0, $widthExpected, self::LOGO_HEIGHT, $width, $height);
         imagecopyresized($logoSmall, $image, 0, 0, 0, 0, $smallWidthExpected, self::LOGO_SMALL_HEIGHT, $width, $height);
 
