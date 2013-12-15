e4ec3593a78b6647484f8fdec2a7392d3d9174a4
mattab <matthieu.aubry@gmail.com>
2013-12-16 12:37:10 +1300

    2.0-rc1 with Morpheus theme enabled by default, wowza!! Refs #4127

---
+++ core/Updates/2.0-rc1.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ * @category Piwik
+ * @package Piwik
+ */
+namespace Piwik\Updates;
+
+use Piwik\Filesystem;
+use Piwik\Updates;
+
+/**
+ * @package Updates
+ */
+class Updates_2_0_rc1 extends Updates
+{
+    public static function update()
+    {
+        \Piwik\Plugin\Manager::getInstance()->activatePlugin('Morpheus');
+    }
+}
