dc9965d06643658a716f6dcf8e1dc6a5c8befaea
mattab <matthieu.aubry@gmail.com>
2014-01-20 14:21:35 +1300

    This should not change the test results

--- tests/PHPUnit/config.ini.travis.php
+++ tests/PHPUnit/config.ini.travis.php
@@ -3,18 +3,26 @@
 ; tests on Travis-CI. Do not use this in production.
 
 [superuser]
-login            = admin
-password        = 098f6bcd4621d373cade4e832627b4f6
-email            = hello@example.org
+login = admin
+password = 098f6bcd4621d373cade4e832627b4f6
+email = hello@example.org
 
 [database]
-host            = localhost
-username        = root
-password        =
-dbname            = piwik_tests
-adapter            = PDO_MYSQL ; PDO_MYSQL, MYSQLI, or PDO_PGSQL
-tables_prefix        = piwiktests_
-;charset        = utf8
+host = localhost
+username = root
+password =
+dbname = piwik_tests
+adapter = PDO_MYSQL
+tables_prefix = piwiktests_
+;charset = utf8
+
+[database_tests]
+host = localhost
+username = root
+password =
+dbname = piwik_tests
+adapter = PDO_MYSQL
+tables_prefix = piwiktests_
 
 [log]
 log_writers[] = file
