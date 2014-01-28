3f6381b0e934e4f2c2e3eb0ce0dac3ed641e2402
Fabian Becker <halfdan@xnorfz.de>
2014-01-28 00:00:59 +0000

    Remove remaining occurences of author_home Show Piwik for all core plugins
    fixes #4578

--- core/Plugin/MetadataLoader.php
+++ core/Plugin/MetadataLoader.php
@@ -71,8 +71,7 @@ class MetadataLoader
         return array(
             'description'      => Piwik::translate($descriptionKey),
             'homepage'         => 'http://piwik.org/',
-            'author'           => 'Piwik',
-            'author_homepage'  => 'http://piwik.org/',
+            'authors'          => array(array('name' => 'Piwik', 'homepage'  => 'http://piwik.org/')),
             'license'          => 'GPL v3+',
             'license_homepage' => 'http://www.gnu.org/licenses/gpl.html',
             'version'          => Version::VERSION,
