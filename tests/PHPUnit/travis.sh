123893d8f8b3939d8f6e3aa119101fd1f005970d
diosmosis <benakamoorthi@fastmail.fm>
2014-02-03 05:13:53 +0000

    Run UI unit tests in travis.

--- tests/PHPUnit/travis.sh
+++ tests/PHPUnit/travis.sh
@@ -25,8 +25,6 @@ else
             echo "View UI failures (if any) here http://builds-artifacts.piwik.org/ui-tests.master/$TRAVIS_JOB_NUMBER/screenshot-diffs/diffviewer.html"
             echo "If the new screenshots are valid, then you can copy them over to tests/PHPUnit/UI/expected-ui-screenshots/."
             echo ""
-
-            TEST_DIR="$TEST_DIR/UIIntegrationTest.php"
         fi
 
         phpunit --colors $TEST_DIR
