matomo-org/matomo

commit 36a9626f8151236cd1249815ba5ecf833ec644ce

Streamline config. fixes #4611

core/Db/Schema.php
@@ -49,11 +49,9 @@ class Schema extends Singleton
     public static function getSchemas($adapterName)
     {
         static $allSchemaNames = array(
-            // MySQL storage engines
             'MYSQL' => array(
                 'Mysql',
-//				'Innodb',
-//				'Infinidb',
+                // InfiniDB
             ),
 
             // Microsoft SQL Server
@@ -112,11 +110,7 @@ class Schema extends Singleton
     {
         $config = Config::getInstance();
         $dbInfos = $config->database;
-        if (isset($dbInfos['schema'])) {
-            $schemaName = $dbInfos['schema'];
-        } else {
-            $schemaName = 'Mysql';
-        }
+        $schemaName = $dbInfos['schema'];
         $className = self::getSchemaClassName($schemaName);
         $this->schema = new $className();
     }

config/global.ini.php
@@ -20,6 +20,7 @@ tables_prefix =
 port = 3306
 adapter = PDO_MYSQL
 type = InnoDB
+schema = Mysql
 
 ; if charset is set to utf8, Piwik will ensure that it is storing its data using UTF8 charset.
 ; it will add a sql query SET at each page view.
@@ -35,6 +36,7 @@ tables_prefix = piwiktests_
 port = 3306
 adapter = PDO_MYSQL
 type = InnoDB
+schema = Mysql
 
 [log]
 ; possible values for log: screen, database, file

core/Updates/0.6.3.php
@@ -35,14 +35,12 @@ class Updates_0_6_3 extends Updates
         if (!isset($dbInfos['schema'])) {
             try {
                 if (is_writable(Config::getLocalConfigPath())) {
-                    $dbInfos['schema'] = 'Myisam';
                     $config->database = $dbInfos;
                     $config->forceSave();
                 } else {
                     throw new \Exception('mandatory update failed');
                 }
             } catch (\Exception $e) {
-                throw new \Piwik\UpdaterErrorException("Please edit your config/config.ini.php file and add below <code>[database]</code> the following line: <br /><code>schema = Myisam</code>");
             }
         }
 

core/Updates/1.8.4-b1.php
@@ -72,7 +72,7 @@ class Updates_1_8_4_b1 extends Updates
 				 `before` int(10) unsigned NOT NULL,
 				 `after` int(10) unsigned NOT NULL,
 				 KEY `mainkey` (`before`)
-				) ENGINE=MyISAM;
+				) ENGINE=InnoDB;
 			"                                                            => false,
 
             // grouping by name only would be case-insensitive, so we GROUP BY name,hash

commit 2e9fead092419f9d9bc9a09b760cead4bdda3784

Fixing installation
https://github.com/piwik/piwik/commit/36a9626f8151236cd1249815ba5ecf833ec644ce broke installation process.

core/Db/Schema.php
@@ -110,7 +110,11 @@ class Schema extends Singleton
     {
         $config = Config::getInstance();
         $dbInfos = $config->database;
-        $schemaName = $dbInfos['schema'];
+        if (isset($dbInfos['schema'])) {
+            $schemaName = $dbInfos['schema'];
+        } else {
+            $schemaName = 'Mysql';
+        }
         $className = self::getSchemaClassName($schemaName);
         $this->schema = new $className();
     }