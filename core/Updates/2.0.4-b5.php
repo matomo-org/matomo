475ace294595f6fdc25134dfc215fa22ecef094c
mattab <matthieu.aubry@gmail.com>
2014-02-21 02:17:22 +1300

    Ignore failure

--- core/Updates/2.0.4-b5.php
+++ core/Updates/2.0.4-b5.php
@@ -64,16 +64,20 @@ class Updates_2_0_4_b5 extends Updates
 
         $userApi = UsersManagerApi::getInstance();
 
-        Db::get()->insert(Common::prefixTable('user'), array(
-                'login'      => $superUser['login'],
-                'password'   => $superUser['password'],
-                'alias'      => $superUser['login'],
-                'email'      => $superUser['email'],
-                'token_auth' => $userApi->getTokenAuth($superUser['login'], $superUser['password']),
-                'date_registered'  => Date::now()->getDatetime(),
-                'superuser_access' => 1
-            )
-        );
+        try {
+            Db::get()->insert(Common::prefixTable('user'), array(
+                    'login'      => $superUser['login'],
+                    'password'   => $superUser['password'],
+                    'alias'      => $superUser['login'],
+                    'email'      => $superUser['email'],
+                    'token_auth' => $userApi->getTokenAuth($superUser['login'], $superUser['password']),
+                    'date_registered'  => Date::now()->getDatetime(),
+                    'superuser_access' => 1
+                )
+            );
+        } catch(\Exception $e) {
+            echo "There was an issue, but we proceed: " . $e->getMessage();
+        }
 
         if (array_key_exists('salt', $superUser)) {
             $salt = $superUser['salt'];
