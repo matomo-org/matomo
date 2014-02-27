bf92e42b541553f4bc585c6a210e2c6632aa4d61
diosmosis <benakamoorthi@fastmail.fm>
2014-02-27 05:25:21 +0000

    Refs #4739, add console command to run UI tests.

---
+++ plugins/CoreConsole/Commands/RunUITests.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Piwik - Open source web analytics
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+namespace Piwik\Plugins\CoreConsole\Commands;
+
+use Piwik\Plugin\ConsoleCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RunUITests extends ConsoleCommand
+{
+    protected function configure()
+    {
+        $this->setName('tests:run-ui');
+        $this->setDescription('Run screenshot tests');
+        $this->addArgument('specs', InputArgument::OPTIONAL | InputArgument::IS_ARRAY, 'Run only a specific test spec. Separate multiple specs by comma, for instance core,integration', array());
+        $this->addOption("persist-fixture-data", null, InputOption::VALUE_NONE, "Persist test data in a database and do not execute tear down.");
+        $this->addOption('keep-symlinks', null, InputOption::VALUE_NONE, "Keep recursive directory symlinks so test pages can be viewed in a browser.");
+        $this->addOption('print-logs', null, InputOption::VALUE_NONE, "Print webpage logs even if tests succeed.");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $specs = $input->getArgument('specs');
+        $persistFixtureData = $input->getOption("persist-fixture-data");
+        $keepSymlinks = $input->getOption('keep-symlinks');
+        $printLogs = $input->getOption('print-logs');
+
+        $options = array();
+        if ($persistFixtureData) {
+            $options[] = "--persist-fixture-data";
+        }
+
+        if ($keepSymlinks) {
+            $options[] = "--keep-symlinks";
+        }
+
+        if ($printLogs) {
+            $options[] = "--print-logs";
+        }
+        $options = implode(" ", $options);
+
+        $cmd = "phantomjs '" . PIWIK_INCLUDE_PATH . "/tests/PHPUnit/UI/run-tests.js' $options";
+
+        $output->writeln('Executing command: <info>' . $cmd . '</info>');
+        $output->writeln('');
+        
+        passthru($cmd);
+    }
+}
