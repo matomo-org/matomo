21744125eaea052e66c391c95c079e7b31bcc9fc
diosmosis <benakamoorthi@fastmail.fm>
2014-02-27 06:50:59 +0000

    Updating UI submodule and add dummy DBStats test.

---
+++ plugins/DBStats/tests/UI/DBStats_spec.js
@@ -0,0 +1,16 @@
+/*!
+ * Piwik - Web Analytics
+ *
+ * Screenshot tests for the DBStats plugin.
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+describe("DBStats", function () {
+    this.timeout(0);
+
+    it("should pass", function () {
+        expect(1).to.be.equal(1);
+    });
+});
