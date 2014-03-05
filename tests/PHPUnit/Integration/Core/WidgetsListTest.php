28781afc3c5e9c199f51c4de671a15910879901a
diosmosis <benakamoorthi@fastmail.fm>
2014-03-05 14:00:52 +0000

    Fixing build.

--- tests/PHPUnit/Integration/Core/WidgetsListTest.php
+++ tests/PHPUnit/Integration/Core/WidgetsListTest.php
@@ -51,7 +51,7 @@ class Core_WidgetsListTest extends DatabaseTestCase
             'SEO'                          => 2,
             'Example Widgets'              => 4,
             'DevicesDetection_DevicesDetection' => 7,
-            'Insights_Category' => 1
+            'Insights_WidgetCategory' => 2
         );
 
         // number of main categories
