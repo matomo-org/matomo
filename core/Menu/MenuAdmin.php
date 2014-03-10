45d316ec3d81888d9d5b7bbf39424aa787f34ccf
mattab <matthieu.aubry@gmail.com>
2014-03-10 13:58:51 +1300

    Fix bug in MenuAdmin::removeEntry

--- core/Menu/MenuAdmin.php
+++ core/Menu/MenuAdmin.php
@@ -106,8 +106,8 @@ class MenuAdmin extends MenuAbstract
         return false;
     }
 
-    public static function removeEntry($menuName)
+    public static function removeEntry($menuName, $subMenuName)
     {
-        MenuAdmin::getInstance()->remove('General_Settings', $menuName);
+        MenuAdmin::getInstance()->remove($menuName, $subMenuName);
     }
 }
