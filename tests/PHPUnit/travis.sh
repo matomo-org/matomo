dfc250d1098f4053507e3bf3d7f4abe2d42f916a
diosmosis <benakamoorthi@fastmail.fm>
2014-03-13 14:35:17 +0000

    Debugging travis failure.

--- tests/PHPUnit/travis.sh
+++ tests/PHPUnit/travis.sh
@@ -32,7 +32,7 @@ then
         then
             phantomjs ../lib/screenshot-testing/run-tests.js --assume-artifacts --persist-fixture-data --screenshot-repo=$TRAVIS_REPO_SLUG $PLUGIN_NAME
         else
-            phantomjs ../lib/screenshot-testing/run-tests.js --store-in-ui-tests-repo --persist-fixture-data --assume-artifacts
+            phantomjs ../lib/screenshot-testing/run-tests.js --store-in-ui-tests-repo --persist-fixture-data --assume-artifacts GoalsTable Installation Login
         fi
     else
         if [ -n "$PLUGIN_NAME" ]
