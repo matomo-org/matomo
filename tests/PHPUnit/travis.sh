85d2039170f58dc3ee4edf3dfae164db024c285a
diosmosis <benakamoorthi@fastmail.fm>
2014-03-21 13:03:44 +0000

    Do not do phpunit @group test for UI tests.

--- tests/PHPUnit/travis.sh
+++ tests/PHPUnit/travis.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [ `phpunit --group __nogroup__ | grep "No tests executed" | wc -l` -ne 1 ]
+if [ "$TEST_SUITE" != "UITests" ]
 then
-    echo "=====> There are some tests functions which do not have a @group set. "
-    echo "       Please add the @group phpdoc comment to the following tests: <====="
-    phpunit --group __nogroup__ --testdox | grep "[x]"
-    exit 1
+    if [ `phpunit --group __nogroup__ | grep "No tests executed" | wc -l` -ne 1 ]
+    then
+        echo "=====> There are some tests functions which do not have a @group set. "
+        echo "       Please add the @group phpdoc comment to the following tests: <====="
+        phpunit --group __nogroup__ --testdox | grep "[x]"
+        exit 1
+    fi
 fi
 
 if [ -n "$TEST_SUITE" ]
