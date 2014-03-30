7c1a73db9d5683ccb074012a1e735124f21a74a4
Thomas Steur <thomas.steur@googlemail.com>
2014-03-30 22:21:52 +0200

    refs #4691 assume yes to all queries

--- tests/angularjs/install-ubuntu.sh
+++ tests/angularjs/install-ubuntu.sh
@@ -1,5 +1,5 @@
 sudo apt-get install python-software-properties
-sudo apt-add-repository ppa:chris-lea/node.js
+sudo apt-add-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install nodejs
 sudo apt-get install npm
