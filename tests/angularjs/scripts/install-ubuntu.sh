275b6b0f0a771aa480ec0b5fc7265487263aa464
Thomas Steur <thomas.steur@googlemail.com>
2014-03-30 22:58:27 +0200

    refs #4691 make sure to install correct package

--- tests/angularjs/scripts/install-ubuntu.sh
+++ tests/angularjs/scripts/install-ubuntu.sh
@@ -1,6 +1,9 @@
+DIR=`dirname $0`
+cd $DIR
 sudo apt-get install python-software-properties
 sudo apt-add-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install nodejs
 sudo apt-get install npm
+cd ..
 sudo npm install .
