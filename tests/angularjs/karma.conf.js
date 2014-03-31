cd043e682d6b4ddf58b44a25ed983e2ee690362d
Thomas Steur <thomas.steur@googlemail.com>
2014-03-31 02:35:30 +0200

    refs #4691 use spec and mocha reporter, prepared usage of jshint

--- tests/angularjs/karma.conf.js
+++ tests/angularjs/karma.conf.js
@@ -44,15 +44,12 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-    
     },
-
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
+    reporters: ['spec', 'mocha'],
 
     // web server port
     port: 9876,
