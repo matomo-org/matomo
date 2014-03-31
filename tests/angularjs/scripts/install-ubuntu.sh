4cd9f7efc1276237aeffbfa5a692ac8b65f1b67d
Thomas Steur <thomas.steur@googlemail.com>
2014-03-31 03:10:09 +0200

    less noise when installing packages

--- tests/angularjs/scripts/install-ubuntu.sh
+++ tests/angularjs/scripts/install-ubuntu.sh
@@ -1,10 +1,10 @@
 DIR=`dirname $0`
 cd $DIR
-sudo apt-get install python-software-properties
-sudo apt-add-repository -y ppa:chris-lea/node.js
+sudo apt-get -qq install python-software-properties
+sudo apt-add-repository -y ppa:chris-lea/node.js > /dev/null
 sudo apt-get -qq update
 sudo apt-get -qq install nodejs
 sudo apt-get -qq install npm
 cd ..
-sudo npm config set loglevel warn
+sudo npm config set loglevel error
 sudo npm install .
