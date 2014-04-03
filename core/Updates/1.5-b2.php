7aabc00d02459b364a9d6bcb0625b7a956768fe4
Thomas Steur <thomas.steur@googlemail.com>
2014-04-04 00:23:23 +0200

    maybe fixes test?

--- core/Updates/1.5-b2.php
+++ core/Updates/1.5-b2.php
@@ -30,7 +30,7 @@ class Updates_1_5_b2 extends Updates
 				 ADD  custom_var_k4 VARCHAR(100) DEFAULT NULL,
 				 ADD  custom_var_v4 VARCHAR(100) DEFAULT NULL,
 				 ADD  custom_var_k5 VARCHAR(100) DEFAULT NULL,
-				 ADD  custom_var_v5 VARCHAR(100) DEFAULT NULL' => false,
+				 ADD  custom_var_v5 VARCHAR(100) DEFAULT NULL' => 1060,
         );
     }
 
