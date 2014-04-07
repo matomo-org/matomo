dc26b8f7bf8548b7fe3bf27a6bff342f36cf5859
Fabian Becker <halfdan@xnorfz.de>
2014-04-07 15:20:46 +0200

    Modify check for needed extensions to allow HHVM to run.

--- plugins/Installation/Controller.php
+++ plugins/Installation/Controller.php
@@ -791,8 +791,11 @@ class Controller extends \Piwik\Plugin\ControllerAdmin
             'zlib',
             'SPL',
             'iconv',
-            'Reflection',
         );
+        // HHVM provides the required subset of Reflection but lists Reflections as missing
+        if (!defined('HHVM_VERSION')) {
+            $needed_extensions[] = 'Reflection';
+        }
         $infos['needed_extensions'] = $needed_extensions;
         $infos['missing_extensions'] = array();
         foreach ($needed_extensions as $needed_extension) {
