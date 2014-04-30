0891ed77d2378cd859610ec4dfdecba1c52535e3
mattab <matthieu.aubry@gmail.com>
2014-04-30 13:31:58 +1200

    Prevent some random build failures when connectivity fails, by calling travis_retry which retries up to 3 times
    https://travis-ci.org/piwik/piwik/jobs/23570274
    https://travis-ci.org/piwik/piwik/jobs/23912462
    http://blog.travis-ci.com/2013-05-20-network-timeouts-build-retries/

--- tests/angularjs/scripts/install-ubuntu.sh
+++ tests/angularjs/scripts/install-ubuntu.sh
@@ -1,10 +1,10 @@
 DIR=`dirname $0`
 cd $DIR
-sudo apt-get -qq install python-software-properties
-sudo apt-add-repository -y ppa:chris-lea/node.js > /dev/null
-sudo apt-get -qq update
-sudo apt-get -qq install nodejs
-sudo apt-get -qq install npm
+travis_retry sudo apt-get -qq install python-software-properties
+travis_retry sudo apt-add-repository -y ppa:chris-lea/node.js > /dev/null
+travis_retry sudo apt-get -qq update
+travis_retry sudo apt-get -qq install nodejs
+travis_retry sudo apt-get -qq install npm
 cd ..
 sudo npm config set loglevel error
 sudo npm install .
