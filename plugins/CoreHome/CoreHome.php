cbe08188e2dd4bb92ce117cf2ecf5bfa50466ac4
Thomas Steur <thomas.steur@googlemail.com>
2014-05-01 06:11:19 +0200

    refs #4987 added missing file, make sure footer icons for user country is always displayed

--- plugins/CoreHome/CoreHome.php
+++ plugins/CoreHome/CoreHome.php
@@ -70,6 +70,7 @@ class CoreHome extends \Piwik\Plugin
         $jsFiles[] = "libs/jquery/jquery.mousewheel.js";
         $jsFiles[] = "libs/jquery/mwheelIntent.js";
         $jsFiles[] = "libs/javascript/sprintf.js";
+        $jsFiles[] = "libs/mousetrap/mousetrap.min.js";
         $jsFiles[] = "libs/angularjs/angular.min.js";
         $jsFiles[] = "libs/angularjs/angular-sanitize.min.js";
         $jsFiles[] = "libs/angularjs/angular-animate.min.js";
