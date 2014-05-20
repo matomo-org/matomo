73deedca967f3d2667f8c3a3e3b67d77b23424c7
sgiehl <stefan@piwik.org>
2014-05-20 21:44:54 +0200

    refs #5114 added detection for seesaa.jp

--- core/DataFiles/SearchEngines.php
+++ core/DataFiles/SearchEngines.php
@@ -809,9 +809,12 @@ if (!isset($GLOBALS['Piwik_SearchEngines'])) {
         // Snap.do
         'search.snap.do'                 => array('Snap.do', 'q', '?q={k}'),
 
+        // SeeSaa
+        'search.seesaa.jp'               => array('SeeSaa', '/\/([^\/]+)\/index\.html/', '{k}/index.html'),
+
         // So-net
         'www.so-net.ne.jp'               => array('So-net', 'query', 'search/web/?query=k{}'),
-        'video.so-net.ne.jp'             => array('So-net Video', 'kw', 'search/?kw=k{}'),
+        'video.so-net.ne.jp'             => array('So-net Videos', 'kw', 'search/?kw=k{}'),
 
         // Sogou
         'www.sogou.com'                  => array('Sogou', 'query', 'web?query={k}', 'gb2312'),
