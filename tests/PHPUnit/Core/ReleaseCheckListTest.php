3c2525aed078da74d8afba1cd05bc128afbaf282
mattab <matthieu.aubry@gmail.com>
2014-06-13 10:14:01 +1200

    Display chmod when test fails

--- tests/PHPUnit/Core/ReleaseCheckListTest.php
+++ tests/PHPUnit/Core/ReleaseCheckListTest.php
@@ -214,7 +214,7 @@ class ReleaseCheckListTest extends PHPUnit_Framework_TestCase
             $valid = array('775', '755');
             $command = "find $pluginsPath -type d -exec chmod 755 {} +";
             $this->assertTrue(in_array($chmod, $valid),
-                    "Some directories within plugins/ are not chmod 755. \n For example: $pathToTest \n\n".
+                    "Some directories within plugins/ are not chmod 755 \n\nGot: $chmod for : $pathToTest \n\n".
                     "Run this command to set all directories to 755: \n$command\n");;
         }
     }
