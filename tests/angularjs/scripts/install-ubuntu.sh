6e62a0368bba586556cc0074fa3810287e72eeec
mattab <matthieu.aubry@gmail.com>
2014-06-19 19:23:21 +1200

    Travis said nodejs is available by default in the travis image

--- tests/angularjs/scripts/install-ubuntu.sh
+++ tests/angularjs/scripts/install-ubuntu.sh
@@ -26,7 +26,7 @@ cd $DIR
 travis_retry sudo apt-get -qq install python-software-properties
 travis_retry sudo apt-add-repository -y ppa:chris-lea/node.js > /dev/null
 travis_retry sudo apt-get -qq update
-travis_retry sudo apt-get -qq install nodejs
+#travis_retry sudo apt-get -qq install nodejs
 travis_retry sudo apt-get -qq install npm
 cd ..
 sudo npm config set loglevel error
