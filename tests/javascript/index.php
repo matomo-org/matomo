3a2ff1d4ad5f937d36ee5b1ceba3dd8396c565bc
mattab <matthieu.aubry@gmail.com>
2014-06-26 10:42:45 +1200

    Fix JavaScript tests refs #5384

--- tests/javascript/index.php
+++ tests/javascript/index.php
@@ -348,7 +348,7 @@ function PiwikTest() {
         equal( typeof tracker.setSiteId, 'function', 'setSiteId' );
         equal( typeof tracker.setCustomData, 'function', 'setCustomData' );
         equal( typeof tracker.getCustomData, 'function', 'getCustomData' );
-        equal( typeof tracker.setCustomRequestContentProcessing, 'function', 'setCustomRequestContentProcessing' );
+        equal( typeof tracker.setCustomRequestProcessing, 'function', 'setCustomRequestProcessing' );
         equal( typeof tracker.setCustomVariable, 'function', 'setCustomVariable' );
         equal( typeof tracker.getCustomVariable, 'function', 'getCustomVariable' );
         equal( typeof tracker.deleteCustomVariable, 'function', 'deleteCustomVariable' );
@@ -746,13 +746,13 @@ function PiwikTest() {
         equal( tracker.getRequest('hello=world').indexOf('hello=world&idsite=&rec=1&r='), 0);
     });
 
-    // support for setCustomRequestContentProcessing( customRequestContentProcessingLogic )
-    test("Tracker setCustomRequestContentProcessing() and getRequest()", function() {
+    // support for setCustomRequestProcessing( customRequestContentProcessingLogic )
+    test("Tracker setCustomRequestProcessing() and getRequest()", function() {
         expect(4);
 
         var tracker = Piwik.getTracker("trackerUrl", "42");
 
-        tracker.setCustomRequestContentProcessing(function(request){
+        tracker.setCustomRequestProcessing(function(request){
           var pairs = request.split('&');
           var result = {};
           pairs.forEach(function(pair) {
