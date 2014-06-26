16627e783ba5336ed5a399503b33108e19f9d031
mattab <matthieu.aubry@gmail.com>
2014-06-26 14:02:15 +1200

    print invisible space

--- tests/PHPUnit/travis-helper.sh
+++ tests/PHPUnit/travis-helper.sh
@@ -49,7 +49,7 @@ count=$(($count + 1))
     #echo -ne "Still running ($count of $timeout): $@\r"
 
     # print invisible character
-    echo -ne "\0xE2\0x80\0x8B"
+    echo -e "\xE2\x80\x8B"
     sleep 60
   done
 
