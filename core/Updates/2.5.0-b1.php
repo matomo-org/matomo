8a0e5a3238da11a3fd1772758972df05f286da16
Thomas Steur <thomas.steur@googlemail.com>
2014-06-26 23:14:53 +0200

    prepared an update script to make sure Piwik knows the columns are already there and not tries to modify them

---
+++ core/Updates/2.5.0-b1.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\Updates;
+
+use Piwik\Plugin\ActionDimension;
+use Piwik\Plugin\VisitDimension;
+use Piwik\Updater;
+use Piwik\Updates;
+
+class Updates_2_5_0_b1 extends Updates
+{
+    public static function update()
+    {
+        foreach (VisitDimension::getAllDimensions() as $dimension) {
+            if ($dimension->getColumnName()) {
+                $component = 'log_visit.' . $dimension->getColumnName();
+                Updater::recordComponentSuccessfullyUpdated($component, $dimension->getVersion());
+            }
+        }
+
+        foreach (ActionDimension::getAllDimensions() as $dimension) {
+            if ($dimension->getColumnName()) {
+                $component = 'log_link_visit_action.' . $dimension->getColumnName();
+                Updater::recordComponentSuccessfullyUpdated($component, $dimension->getVersion());
+            }
+        }
+    }
+}
