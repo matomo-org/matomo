1a882aa49334e111bcc2b9cb86a6fd7245e01b8c
Thomas Steur <thomas.steur@googlemail.com>
2014-07-02 04:33:12 +0200

    added command to enable / disable development

---
+++ plugins/CoreConsole/Commands/EnableDevelopment.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+namespace Piwik\Plugins\CoreConsole\Commands;
+
+use Piwik\Config;
+use Piwik\Plugin\ConsoleCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ */
+class EnableDevelopment extends ConsoleCommand
+{
+    protected function configure()
+    {
+        $this->setName('development:enable');
+        $this->setAliases(array('development:disable'));
+        $this->setDescription('Enable or disable development mode. See config/global.ini.php in section [Development] for more information');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $commandName = $input->getFirstArgument();
+        $enable      = (false !== strpos($commandName, 'enable'));
+
+        $config      = Config::getInstance();
+        $development = $config->Development;
+
+        if ($enable) {
+            $development['enabled'] = 1;
+            $development['disable_merged_assets'] = 1;
+            $message = 'Development mode enabled';
+        } else {
+            $development['enabled'] = 0;
+            $development['disable_merged_assets'] = 0;
+            $message = 'Development mode disabled';
+        }
+
+        $config->Development = $development;
+        $config->forceSave();
+
+        $this->writeSuccessMessage($output, array($message));
+    }
+
+}
