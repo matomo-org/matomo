8748f0aed7f1883f06f6b662476dc1ab2145d55c
diosmosis <benakamoorthi@fastmail.fm>
2014-07-06 03:14:53 -0700

    Fix travis failure.

--- tests/PHPUnit/config.ini.travis.php
+++ tests/PHPUnit/config.ini.travis.php
@@ -8,7 +8,8 @@ username = root
 password =
 dbname = piwik_tests
 adapter = PDO\MYSQL
-tables_prefix = piwiktests_
+; no table prefix for tests on travis
+tables_prefix = 
 ;charset = utf8
 
 [database_tests]
