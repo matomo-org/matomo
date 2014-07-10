a750af3b2fe1516c41d53589b96fc444d165bfec
Christian Raue <christian.raue@gmail.com>
2014-07-10 15:41:15 +0200

    fixed wrong variable usage

--- core/API/DataTableGenericFilter.php
+++ core/API/DataTableGenericFilter.php
@@ -156,7 +156,7 @@ class DataTableGenericFilter
                 }
 
                 try {
-                    $value = Common::getRequestVar($name, $defaultValue, $type, $this->request);
+                    $value = Common::getRequestVar($varName, $defaultValue, $type, $this->request);
                     settype($value, $type);
                     $filterParameters[] = $value;
                 } catch (Exception $e) {
