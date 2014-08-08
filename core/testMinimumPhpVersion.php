a2f3959a8c3f730d871fbe0e3cc437a64ac98494
mattab <matthieu.aubry@gmail.com>
2014-08-08 18:31:06 +0200

    refs #5958 add http error code 500 header in case of piwik errors. Could we also add a call to error_log to log in the error.log ? (we cannot use Log::debug or so as the logger may not be initialised yet)

--- core/testMinimumPhpVersion.php
+++ core/testMinimumPhpVersion.php
@@ -96,6 +96,8 @@ if (!function_exists('Piwik_ExitWithMessage')) {
     function Piwik_ExitWithMessage($message, $optionalTrace = false, $optionalLinks = false, $optionalLinkBack = false)
     {
         @header('Content-Type: text/html; charset=utf-8');
+        @header('HTTP/1.1 500 Internal Server Error');
+
         if ($optionalTrace) {
             $optionalTrace = '<span class="exception-backtrace">Backtrace:<br /><pre>' . $optionalTrace . '</pre></span>';
         }
