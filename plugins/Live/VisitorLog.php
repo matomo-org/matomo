1fedaafce77412018ad5e0aa44621b0f832f5ba2
Thomas Steur <thomas.steur@googlemail.com>
2014-08-12 08:49:30 +0200

    refs #5950 sort visitor log by lastActionTimestamp by default

--- plugins/Live/VisitorLog.php
+++ plugins/Live/VisitorLog.php
@@ -41,7 +41,7 @@ class VisitorLog extends Visualization
             $this->requestConfig->filter_limit = 20;
         }
 
-        $this->requestConfig->filter_sort_column = 'idVisit';
+        $this->requestConfig->filter_sort_column = 'lastActionTimestamp';
         $this->requestConfig->disable_generic_filters = true;
 
         $offset = Common::getRequestVar('filter_offset', 0);
