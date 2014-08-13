937bbca5e8c2959040694f9d5b06ae38a4a15f95
diosmosis <benakamoorthi@fastmail.fm>
2014-08-13 13:15:16 +0200

    Refs #5962, add ability for .travis.yml file to check out dependent plugins using DEPENDENT_PLUGINS travis environment variable (set in .travis.yml file manually).

---
+++ tests/travis/checkout_dependent_plugins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# NOTE: should be removed when composer used to handle plugin dependencies
+
+if [ "$DEPENDENT_PLUGINS" == "" ]; then
+    echo "No dependent plugins."
+else
+    echo "Cloning dependent plugins..."
+    echo ""
+    
+    for pluginSlug in "${DEPENDENT_PLUGINS[@]}"
+    do
+        dependentPluginName=`echo "$pluginSlug" | sed -E 's/[a-zA-Z0-9_]+-(.*)/\1/'`
+
+        echo "Cloning $pluginSlug..."
+        git clone --depth=1 "https://$GITHUB_USER_TOKEN:@github.com/$pluginSlug" "plugins/$dependentPluginName"
+    done
+fi
