f56a028c52e7c4e156af5fca98fa68e1bc86a969
Thomas Steur <thomas.steur@googlemail.com>
2014-08-14 19:12:34 +0200

    make sure to delete tracker cache after removing the previous files although this should be done after updating anyway.

---
+++ core/Updates/2.5.0-rc4.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+namespace Piwik\Updates;
+
+use Piwik\Tracker\Cache;
+use Piwik\Updates;
+
+/**
+ * Update for version 2.5.0-rc4.
+ */
+class Updates_2_5_0_rc4 extends Updates
+{
+
+    static function update()
+    {
+        Cache::clearCacheGeneral();
+    }
+}
