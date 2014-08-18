2b78a57658d832f9ccf9269250421a65e90b8e2b
Thomas Steur <thomas.steur@googlemail.com>
2014-08-18 14:15:04 +0200

    fixes #5971 better indication of current state of real time widget. Always display either play or pause icon but not both at the same time

--- plugins/Live/javascripts/live.js
+++ plugins/Live/javascripts/live.js
@@ -270,17 +270,13 @@ $(function() {
     };
 });
 
-var pauseImage = "plugins/Live/images/pause.gif";
-var pauseDisabledImage = "plugins/Live/images/pause_disabled.gif";
-var playImage = "plugins/Live/images/play.gif";
-var playDisabledImage = "plugins/Live/images/play_disabled.gif";
 function onClickPause() {
-    $('#pauseImage').attr('src', pauseImage);
-    $('#playImage').attr('src', playDisabledImage);
+    $('#pauseImage').hide();
+    $('#playImage').show();
     return $('#visitsLive').liveWidget('stop');
 }
 function onClickPlay() {
-    $('#playImage').attr('src', playImage);
-    $('#pauseImage').attr('src', pauseDisabledImage);
+    $('#playImage').hide();
+    $('#pauseImage').show();
     return $('#visitsLive').liveWidget('start');
 }
