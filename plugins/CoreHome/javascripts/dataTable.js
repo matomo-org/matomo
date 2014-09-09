6d0ab4e0c5125887ebc1750ea6d82240218b4da6
diosmosis <benaka@piwik.pro>
2014-09-09 05:26:48 -0700

    Fixes #6178, select columns of a table correctly in handleColumnHighlighting function in dataTable.js.

--- plugins/CoreHome/javascripts/dataTable.js
+++ plugins/CoreHome/javascripts/dataTable.js
@@ -1415,13 +1415,13 @@ $.extend(DataTable.prototype, UIControl.prototype, {
 
                 if (!maxWidth[nthChild]) {
                     maxWidth[nthChild] = 0;
-                    rows.find("td:nth-child(" + (nthChild) + ") .column .value").each(function (index, element) {
+                    rows.find("td:nth-child(" + (nthChild) + ").column .value").each(function (index, element) {
                         var width    = $(element).width();
                         if (width > maxWidth[nthChild]) {
                             maxWidth[nthChild] = width;
                         }
                     });
-                    rows.find("td:nth-child(" + (nthChild) + ") .column .value").each(function (index, element) {
+                    rows.find("td:nth-child(" + (nthChild) + ").column .value").each(function (index, element) {
                         $(element).css({width: maxWidth[nthChild], display: 'inline-block'});
                     });
                 }
