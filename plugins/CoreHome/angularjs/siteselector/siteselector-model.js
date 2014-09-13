d5f1098b1df0b0a347708d60ccda455f9bc96999
diosmosis <benaka@piwik.pro>
2014-09-13 11:09:45 -0700

    Do not abort all requests just last made request in site selector model object.

--- plugins/CoreHome/angularjs/siteselector/siteselector-model.js
+++ plugins/CoreHome/angularjs/siteselector/siteselector-model.js
@@ -45,19 +45,22 @@ angular.module('piwikApp').factory('siteSelectorModel', function (piwikApi, $fil
         }
 
         if (model.isLoading) {
-            piwikApi.abortAll();
+            model.currentRequest.abort();
         }
 
         model.isLoading = true;
 
-        return piwikApi.fetch({
+        model.currentRequest = piwikApi.fetch({
             method: 'SitesManager.getPatternMatchSites',
             pattern: term
         }).then(function (response) {
             return model.updateWebsitesList(response);
         })['finally'](function () {    // .finally() is not IE8 compatible see https://github.com/angular/angular.js/commit/f078762d48d0d5d9796dcdf2cb0241198677582c
             model.isLoading = false;
+            model.currentRequest = null;
         });
+
+        return model.currentRequest;
     };
 
     model.loadSite = function (idsite) {
