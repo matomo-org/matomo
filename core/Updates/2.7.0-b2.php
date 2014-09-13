3c3042cc06db8b591565e13623b748af406ce88a
Thomas Steur <thomas.steur@googlemail.com>
2014-09-13 16:11:42 +0200

    refs #4996 activate contents plugin on update

--- core/Updates/2.7.0-b2.php
+++ core/Updates/2.7.0-b2.php
@@ -9,27 +9,21 @@
 
 namespace Piwik\Updates;
 
-use Piwik\Common;
-use Piwik\Updater;
 use Piwik\Updates;
 
 /**
  */
 class Updates_2_7_0_b2 extends Updates
 {
-    static function getSql()
-    {
-        return array(
-            'ALTER TABLE `' . Common::prefixTable('log_visit') . '`
-			    ADD `user_id` varchar(200) NULL AFTER `config_id`
-			   ' => array(1060),
-        );
-    }
 
     static function update()
     {
-        // Run the SQL
-        Updater::updateDatabase(__FILE__, self::getSql());
+        $pluginManager = \Piwik\Plugin\Manager::getInstance();
+
+        try {
+            $pluginManager->activatePlugin('Contents');
+        } catch(\Exception $e) {
+        }
     }
 }
 
