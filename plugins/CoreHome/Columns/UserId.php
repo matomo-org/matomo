c0a29a9e3bc1e340e112d89b8544a75f9de301ef
Thomas Steur <thomas.steur@googlemail.com>
2014-09-22 18:44:06 +0200

    refs #3490 removed segment definition as it apparently depends on idSites which I do not quite understand why

--- plugins/CoreHome/Columns/UserId.php
+++ plugins/CoreHome/Columns/UserId.php
@@ -19,29 +19,14 @@ use Piwik\Tracker\Action;
 class UserId extends VisitDimension
 {
     /**
-     * This will be the name of the column in the log_visit table if a $columnType is specified.
      * @var string
      */
     protected $columnName = 'user_id';
 
     /**
-     * If a columnType is defined, we will create this a column in the MySQL table having this type. Please make sure
-     * MySQL will understand this type. Once you change the column type the Piwik platform will notify the user to
-     * perform an update which can sometimes take a long time so be careful when choosing the correct column type.
      * @var string
      */
     protected $columnType = 'VARCHAR(200) NOT NULL';
-
-    protected function configureSegments()
-    {
-        /*
-        $segment = new Segment();
-        $segment->setSegment('userId');
-        $segment->setCategory('General_Visit');
-        $segment->setName('CoreHome_UserId');
-        $segment->setAcceptedValues('Here you should explain which values are accepted/useful: Any number, for instance 1, 2, 3 , 99');
-        $this->addSegment($segment);*/
-    }
 
     /**
      * @param Request $request
