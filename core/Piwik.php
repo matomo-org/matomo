ac7827f1d713ffb49f7bb7eb57394338e3847fd3
mattab <matthieu.aubry@gmail.com>
2014-09-23 17:03:32 +1200

    fix regression when HTTPS URL is different from HTTP Tracker URL, both should be used in the generated tracking code refs #344

--- core/Piwik.php
+++ core/Piwik.php
@@ -214,7 +214,9 @@ class Piwik
         self::postEvent('Piwik.getJavascriptCode', array(&$codeImpl, $parameters));
 
         $setTrackerUrl = 'var u="//{$piwikUrl}/";';
+
         if (!empty($codeImpl['httpsPiwikUrl'])) {
+            $setTrackerUrl = 'var u=((document.location.protocol === "https:") ? "https://{$httpsPiwikUrl}/" : "http://{$piwikUrl}/");';
             $codeImpl['httpsPiwikUrl'] = rtrim($codeImpl['httpsPiwikUrl'], "/");
         }
         $codeImpl = array('setTrackerUrl' => htmlentities($setTrackerUrl)) + $codeImpl;
