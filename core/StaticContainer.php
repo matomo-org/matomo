10b58b36b8fbc41a1745e051b4bbdce67befa02e
Matthieu Napoli <matthieu@mnapoli.fr>
2014-10-21 19:07:56 +1300

    Added PHP-DI + StaticContainer class

---
+++ core/StaticContainer.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik;
+
+use DI\Container;
+use DI\ContainerBuilder;
+use Interop\Container\ContainerInterface;
+
+/**
+ * This class provides a static access to the container.
+ *
+ * @deprecated This class is introduced only to keep BC with the current static architecture. It will be removed in 3.0.
+ *     - it is global state (that class makes the container a global variable)
+ *     - using the container directly is the "service locator" anti-pattern (which is not dependency injection)
+ */
+class StaticContainer
+{
+    /**
+     * @var Container
+     */
+    private static $container;
+
+    /**
+     * @return ContainerInterface
+     */
+    public static function getContainer()
+    {
+        if (self::$container === null) {
+            self::$container = self::createContainer();
+        }
+
+        return self::$container;
+    }
+
+    /**
+     * @link http://php-di.org/doc/container-configuration.html
+     */
+    private static function createContainer()
+    {
+        $builder = new ContainerBuilder();
+
+        // TODO add cache
+        // $builder->setDefinitionCache($cache);
+        // $builder->writeProxiesToFile(true, PIWIK_USER_PATH . '/tmp/proxies');
+
+        // Global config
+        $builder->addDefinitions(PIWIK_USER_PATH . '/config/global.php');
+
+        // User config
+        if (file_exists(PIWIK_USER_PATH . '/config/config.php')) {
+            $builder->addDefinitions(PIWIK_USER_PATH . '/config/config.php');
+        }
+
+        return $builder->build();
+    }
+}
