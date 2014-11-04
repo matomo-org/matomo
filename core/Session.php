ffa5c1e86e578ff166d5c55a5edb1ca14f1e3d28
Thomas Steur <thomas.steur@googlemail.com>
2014-11-04 03:31:50 +0100

    refs #6564 refactored out HtmlMessageException and introduced some other exceptions instead

--- core/Session.php
+++ core/Session.php
@@ -9,7 +9,7 @@
 namespace Piwik;
 
 use Exception;
-use Piwik\Exceptions\HtmlMessageException;
+use Piwik\Exception\MissingFilePermissionException;
 use Piwik\Session\SaveHandler\DbTable;
 use Zend_Session;
 
@@ -132,7 +132,10 @@ class Session extends Zend_Session
                 $e->getMessage()
             );
 
-            throw new HtmlMessageException($message, $e->getCode(), $e);
+            $ex = new MissingFilePermissionException($message, $e->getCode(), $e);
+            $ex->setIsHtmlMessage();
+
+            throw $ex;
         }
     }
 
