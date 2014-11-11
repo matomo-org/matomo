41684e0be37e568e6edc3b0de6fa7d6f3f589843
diosmosis <benaka@piwik.pro>
2014-11-11 14:58:06 -0800

    Make sure processed metrics are formatted when requesting data from within Piwik in Overlay.

--- plugins/Overlay/Controller.php
+++ plugins/Overlay/Controller.php
@@ -73,6 +73,7 @@ class Controller extends \Piwik\Plugin\Controller
             . '&period=' . urlencode($period)
             . '&label=' . urlencode($label)
             . '&format=original'
+            . '&force_format_processed_metrics=1'
         );
         $dataTable = $request->process();
 
