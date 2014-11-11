7423aa79c4ae28c2a6e7582368990f8ce26a91ca
Thomas Steur <thomas.steur@googlemail.com>
2014-11-11 23:50:37 +0100

    #6635 rename phpunit.xml to phpunit.xml.dist. did not know it is supported out of the box by phpunit

--- tests/travis/prepare.sh
+++ tests/travis/prepare.sh
@@ -19,6 +19,7 @@ sed "s/PDO\\\MYSQL/${MYSQL_ADAPTER}/g" ./tests/PHPUnit/config.ini.travis.php > .
 
 # Prepare phpunit.xml
 echo "Adjusting phpunit.xml"
+cp ./tests/PHPUnit/phpunit.xml.dist ./tests/PHPUnit/phpunit.xml
 
 if [ -n "$PLUGIN_NAME" ];
 then
