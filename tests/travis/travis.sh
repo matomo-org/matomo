3c6796f46cb84f2086ae44556aaec01e715ee59f
diosmosis <benaka@piwik.pro>
2014-11-11 01:52:48 -0800

    Remove travis debugging change.

--- tests/travis/travis.sh
+++ tests/travis/travis.sh
@@ -56,7 +56,7 @@ then
         then
             phantomjs ../lib/screenshot-testing/run-tests.js --assume-artifacts --persist-fixture-data --screenshot-repo=$TRAVIS_REPO_SLUG --plugin=$PLUGIN_NAME
         else
-            phantomjs ../lib/screenshot-testing/run-tests.js --store-in-ui-tests-repo --persist-fixture-data --assume-artifacts ActionsDataTable
+            phantomjs ../lib/screenshot-testing/run-tests.js --store-in-ui-tests-repo --persist-fixture-data --assume-artifacts
         fi
     elif [ "$TEST_SUITE" = "AllTests" ]
     then
