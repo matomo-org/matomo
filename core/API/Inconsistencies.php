f5618f5edab68b68b027e2df0c47c120292de2fb
diosmosis <benaka@piwik.pro>
2014-11-16 00:35:55 -0800

    Forgot to add Inconsistencies file in earlier commit.

---
+++ core/API/Inconsistencies.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+namespace Piwik\API;
+
+/**
+ * Contains logic to replicate inconsistencies in Piwik's API. This class exists
+ * to provide a way to clean up existing Piwik code and behavior without breaking
+ * backwards compatibility immediately.
+ *
+ * Should be removed before releasing Piwik 3.0. [TODO: note in issue about 3.0.]
+ */
+class Inconsistencies
+{
+    /**
+     * In Piwik 2.X and below, the "raw" API would format percent values but no others.
+     * This method returns the list of percent metrics that were returned from the API
+     * formatted so we can maintain BC.
+     *
+     * Used by DataTablePostProcessor.
+     */
+    public function getPercentMetricsToFormat()
+    {
+        return array(
+            'bounce_rate',
+            'conversion_rate',
+            'interaction_rate',
+            'exit_rate',
+            'bounce_rate_returning'
+             // TODO: what about goal & evolution metrics? need to use regex i guess.
+        );
+    }
+}
