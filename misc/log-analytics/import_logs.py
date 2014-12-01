133a448f2c407acf1319bbfbaf77f5e91ce5a323
mattab <matthieu.aubry@gmail.com>
2014-12-01 17:38:42 +1300

    refs #4936 on IIS when no query string it shows as '-' so we assume '' instead.

--- misc/log-analytics/import_logs.py
+++ misc/log-analytics/import_logs.py
@@ -1606,12 +1606,13 @@ class Parser(object):
 
             try:
                 hit.query_string = format.get('query_string')
-                # IIS detaults to - when there is no query string, but we want empty string
-                if hit.query_string == '-':
-                    hit.query_string = ''
                 hit.path = hit.full_path
             except BaseFormatException:
                 hit.path, _, hit.query_string = hit.full_path.partition(config.options.query_string_delimiter)
+
+            # IIS detaults to - when there is no query string, but we want empty string
+            if hit.query_string == '-':
+                hit.query_string = ''
 
             hit.extension = hit.path.rsplit('.')[-1].lower()
 
