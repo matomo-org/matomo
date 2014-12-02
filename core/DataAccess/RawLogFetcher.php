0727b42882e502b02519630b44b77a52408dbf2c
Marcin Czołnowski <marcin@czolnowski.net>
2014-12-02 12:22:42 +0100

    Remove repository and move classes with DAO to DataAccess namespace.

---
+++ core/DataAccess/RawLogFetcher.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\DataAccess;
+
+use Piwik\Common;
+use Piwik\Db;
+
+class RawLogFetcher
+{
+    /**
+     * @param string $from
+     * @param string $to
+     * @param array $fields
+     * @param int $fromId
+     * @param int $limit
+     * @return array
+     */
+    public function getVisitsWithDatesLimit($from, $to, $fields = array(), $fromId = 0, $limit = 1000)
+    {
+        $sql = array(
+            "SELECT " . implode(', ', $fields),
+            "FROM " . Common::prefixTable('log_visit'),
+            "WHERE visit_first_action_time >= ? AND visit_last_action_time < ?",
+            "AND idvisit > ?",
+            sprintf("LIMIT %d", $limit)
+        );
+
+        $bind = array($from, $to, $fromId);
+
+        return Db::get()->fetchAll(implode(' ', $sql), $bind);
+    }
+
+    /**
+     * @param string $from
+     * @param string $to
+     * @return int
+     */
+    public function countVisitsWithDatesLimit($from, $to)
+    {
+        $sql = array(
+            "SELECT COUNT(*) AS num_rows",
+            "FROM " . Common::prefixTable('log_visit'),
+            "WHERE visit_first_action_time >= ? AND visit_last_action_time < ?"
+        );
+
+        $bind = array($from, $to);
+
+        return (int) Db::get()->fetchOne(implode(' ', $sql), $bind);
+    }
+}
