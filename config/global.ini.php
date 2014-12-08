dca8fe4e075357ab5310d8213a669cc217e092ef
mattab <matthieu.aubry@gmail.com>
2014-12-08 14:54:10 +1300

    Refs #6819 Fix comment

--- config/global.ini.php
+++ config/global.ini.php
@@ -513,8 +513,8 @@ pivot_by_filter_default_column_limit = 10
 
 ; Piwik uses "Privacy by default" model. When one of your users visit multiple of your websites tracked in this Piwik,
 ; Piwik will create for this user a fingerprint that will be different across the multiple websites.
-; If you want to track unique users across websites (for example when using the InterSites plugin) you may set this setting to 0.
-; Note: setting this to 0 reduces your users' privacy.
+; If you want to track unique users across websites (for example when using the InterSites plugin) you may set this setting to 1.
+; Note: setting this to 0 increases your users' privacy.
 enable_fingerprinting_across_websites = 0
 
 ; Piwik uses first party cookies by default. If set to 1,
