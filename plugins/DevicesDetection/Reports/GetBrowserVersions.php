3be405125f59d95d61a5eb2727dc86addc7db440
sgiehl <stefan@piwik.org>
2015-01-06 11:13:05 +0100

    fixes #6490 - enabled search field for some reports

--- plugins/DevicesDetection/Reports/GetBrowserVersions.php
+++ plugins/DevicesDetection/Reports/GetBrowserVersions.php
@@ -26,7 +26,7 @@ class GetBrowserVersions extends Base
 
     public function configureView(ViewDataTable $view)
     {
-        $view->config->show_search = false;
+        $view->config->show_search = true;
         $view->config->show_exclude_low_population = false;
         $view->config->addTranslation('label', $this->dimension->getName());
     }
