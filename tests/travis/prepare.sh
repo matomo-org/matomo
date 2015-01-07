742edc67417859db875bb982de7d82ae9b7bd2ae
mattab <matthieu.aubry@gmail.com>
2015-01-07 17:25:34 +1300

    refs  #6942 silencing apt-get commands to minimise CI log file size

--- tests/travis/prepare.sh
+++ tests/travis/prepare.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-sudo apt-get update
+sudo apt-get update > /dev/null
 
 # Install XMLStarlet
-sudo apt-get install -qq xmlstarlet
+sudo apt-get install -qq xmlstarlet > /dev/null
 
 # Install fonts for UI tests
 if [ "$TEST_SUITE" = "UITests" ];
 then
     sudo sh -c "echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections"
-    sudo apt-get install -qq ttf-mscorefonts-installer
+    sudo apt-get install -qq ttf-mscorefonts-installer > /dev/null
 fi
 
 # Copy Piwik configuration
