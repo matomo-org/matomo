fb50405229759aab78d389a93f16b0036c18a089
diosmosis <benaka@piwik.pro>
2015-01-07 02:45:19 -0800

    Remove ability to test against 'latest_stable'.

--- tests/travis/checkout_test_against_branch.sh
+++ tests/travis/checkout_test_against_branch.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR=`dirname $0`
 
 if [ "$TEST_AGAINST_PIWIK_BRANCH" == "" ]; then
     if [ "$TEST_AGAINST_CORE" == "latest_stable" ]; then # test against the latest stable release of piwik core (including betas & release candidates)
-        export TEST_AGAINST_PIWIK_BRANCH=$(git describe --tags `git rev-list --tags --max-count=1`)
-        export TEST_AGAINST_PIWIK_BRANCH=`echo $TEST_AGAINST_PIWIK_BRANCH | tr -d ' ' | tr -d '\n'`
+        echo "Testing against 'latest_stable' is no longer supported, please test against 'minimum_required_piwik'."
+        exit 1
     elif [[ "$TEST_AGAINST_CORE" == "minimum_required_piwik" && "$PLUGIN_NAME" != "" ]]; then # test against the minimum required Piwik in the plugin.json file
         export TEST_AGAINST_PIWIK_BRANCH=$(php "$SCRIPT_DIR/get_required_piwik_version.php" $PLUGIN_NAME)
     else
