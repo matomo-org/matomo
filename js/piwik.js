105f4b1fbfc23f95216a8bb2067ce1470fa9acea
mattab <matthieu.aubry@gmail.com>
2015-02-27 16:51:41 +1300

    Add note informing developers that there is a tight coupling between header size and another file

--- js/piwik.js
+++ js/piwik.js
@@ -8,6 +8,8 @@
  * @license http://piwik.org/free-software/bsd/ BSD-3 Clause (also in js/LICENSE.txt)
  * @license magnet:?xt=urn:btih:c80d50af7d3db9be66a4d0a86db0286e4fd33292&dn=bsd-3-clause.txt BSD-3-Clause
  */
+// NOTE: if you change this above Piwik comment block, you must also change `$byteStart` in js/tracker.php
+
 // Refer to README.md for build instructions when minifying this file for distribution.
 
 /*
