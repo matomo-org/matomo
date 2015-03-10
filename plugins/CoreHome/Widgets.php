6eb6efba6d478a5097fa51207c0a878d2c4d4a56
Matthieu Napoli <matthieu@mnapoli.fr>
2015-03-11 12:02:38 +1300

    Example of using dependency injection in Widgets

--- plugins/CoreHome/Widgets.php
+++ plugins/CoreHome/Widgets.php
@@ -10,11 +10,24 @@ namespace Piwik\Plugins\CoreHome;
 
 use Piwik\Common;
 use Piwik\Piwik;
+use Piwik\Translation\Translator;
 use Piwik\View;
 
 class Widgets extends \Piwik\Plugin\Widgets
 {
     protected $category = 'Example Widgets';
+
+    /**
+     * @var Translator
+     */
+    private $translator;
+
+    public function __construct(Translator $translator)
+    {
+        $this->translator = $translator;
+
+        parent::__construct();
+    }
 
     protected function init()
     {
@@ -31,7 +44,7 @@ class Widgets extends \Piwik\Plugin\Widgets
 
         if (Common::getRequestVar('widget', false)
             && Piwik::hasUserSuperUserAccess()) {
-            $view->footerMessage = Piwik::translate('CoreHome_OnlyForSuperUserAccess');
+            $view->footerMessage = $this->translator->translate('CoreHome_OnlyForSuperUserAccess');
         }
 
         return $view->render();
@@ -43,8 +56,8 @@ class Widgets extends \Piwik\Plugin\Widgets
     public function getPromoVideo()
     {
         $view = new View('@CoreHome/getPromoVideo');
-        $view->shareText     = Piwik::translate('CoreHome_SharePiwikShort');
-        $view->shareTextLong = Piwik::translate('CoreHome_SharePiwikLong');
+        $view->shareText     = $this->translator->translate('CoreHome_SharePiwikShort');
+        $view->shareTextLong = $this->translator->translate('CoreHome_SharePiwikLong');
         $view->promoVideoUrl = 'https://www.youtube.com/watch?v=OslfF_EH81g';
 
         return $view->render();
