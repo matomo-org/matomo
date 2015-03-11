070135f195694a52552d3bafb687c7fcf7f6988c
diosmosis <benaka@piwik.pro>
2015-03-11 00:20:12 -0700

    Re-add geoipUpdateRows.php w/ message about new command.

---
+++ misc/others/geoipUpdateRows.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "This script has been removed, instead use the 'usercountry:attribute' command.\n";
+echo "For example, run 'php /path/to/piwik/console usercountry:attribute 2012-01-01,2013-01-01'.\n";
+echo "To learn more about the new command, run 'php /path/to/piwik/console help usercountry:attribute'.\n";
