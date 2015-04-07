5cdd503acd4859da6a4ae85221bec89bc4b7e36f
diosmosis <benaka@piwik.pro>
2015-04-06 23:18:11 -0700

    Forgot to add EnvironmentValidator class.

---
+++ core/Application/Kernel/EnvironmentValidator.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Application\Kernel;
+
+use Piwik\Application\Kernel\GlobalSettingsProvider\IniSettingsProvider;
+use Piwik\Common;
+use Piwik\Piwik;
+use Piwik\SettingsServer;
+use Piwik\Translation\Translator;
+
+/**
+ * TODO
+ */
+class EnvironmentValidator
+{
+    /**
+     * @var IniSettingsProvider
+     */
+    protected $iniSettingsProvider;
+
+    /**
+     * @var Translator
+     */
+    protected $translator;
+
+    public function __construct(GlobalSettingsProvider $settingsProvider, Translator $translator)
+    {
+        $this->iniSettingsProvider = $settingsProvider;
+    }
+
+    public function validate()
+    {
+        $inTrackerRequest = SettingsServer::isTrackerApiRequest();
+        $inConsole = Common::isPhpCliMode();
+
+        $this->checkConfigFileExists('global.ini.php');
+        $this->checkConfigFileExists('config.ini.php', $startInstaller = !$inTrackerRequest && !$inConsole);
+    }
+
+    /**
+     * @param $filename
+     * @param bool $startInstaller
+     * @throws \Exception
+     */
+    private function checkConfigFileExists($filename, $startInstaller = false)
+    {
+        $path = PIWIK_INCLUDE_PATH . '/config/' . $filename;
+
+        if (is_readable($path)) {
+            return;
+        }
+
+        $message = $this->translator->translate('General_ExceptionConfigurationFileNotFound', array($path));
+        $exception = new \Exception($message);
+
+        if ($startInstaller) {
+            /**
+             * Triggered when the configuration file cannot be found or read, which usually
+             * means Piwik is not installed yet.
+             *
+             * This event can be used to start the installation process or to display a custom error message.
+             *
+             * @param \Exception $exception The exception that was thrown by `Config::getInstance()`.
+             */
+            Piwik::postEvent('Config.NoConfigurationFile', array($exception), $pending = true);
+        } else {
+            throw $exception;
+        }
+    }
+}
