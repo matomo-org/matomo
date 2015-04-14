290978d6c147a0da344c439ca02742f23e7c4d2d
Matthieu Napoli <matthieu@mnapoli.fr>
2015-04-14 14:13:39 +1200

    Improved test

--- plugins/Diagnostics/Test/Unit/DiagnosticServiceTest.php
+++ plugins/Diagnostics/Test/Unit/DiagnosticServiceTest.php
@@ -11,6 +11,7 @@ namespace Piwik\Plugins\Diagnostics\Test\Unit;
 use Piwik\Plugins\Diagnostics\Diagnostic\DiagnosticResult;
 use Piwik\Plugins\Diagnostics\DiagnosticService;
 use Piwik\Plugins\Diagnostics\Test\Mock\DiagnosticWithError;
+use Piwik\Plugins\Diagnostics\Test\Mock\DiagnosticWithSuccess;
 use Piwik\Plugins\Diagnostics\Test\Mock\DiagnosticWithWarning;
 
 class DiagnosticServiceTest extends \PHPUnit_Framework_TestCase
@@ -22,6 +23,7 @@ class DiagnosticServiceTest extends \PHPUnit_Framework_TestCase
         );
         $optionalDiagnostics = array(
             new DiagnosticWithWarning(),
+            new DiagnosticWithSuccess(),
         );
 
         $service = new DiagnosticService($mandatoryDiagnostics, $optionalDiagnostics);
@@ -30,10 +32,12 @@ class DiagnosticServiceTest extends \PHPUnit_Framework_TestCase
 
         $results = $report->getAllResults();
 
-        $this->assertCount(2, $results);
+        $this->assertCount(3, $results);
         $this->assertEquals('Error', $results[0]->getLabel());
         $this->assertEquals(DiagnosticResult::STATUS_ERROR, $results[0]->getStatus());
         $this->assertEquals('Warning', $results[1]->getLabel());
         $this->assertEquals(DiagnosticResult::STATUS_WARNING, $results[1]->getStatus());
+        $this->assertEquals('Success', $results[2]->getLabel());
+        $this->assertEquals(DiagnosticResult::STATUS_OK, $results[2]->getStatus());
     }
 }
