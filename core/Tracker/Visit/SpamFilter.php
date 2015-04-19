46b7d6a9f7c51a45fefaf8de2f6e072c76b9b098
Matthieu Napoli <matthieu@mnapoli.fr>
2015-04-20 11:45:38 +1200

    #7674 Simpler implementation since we don't have auto-update for now

--- core/Tracker/Visit/SpamFilter.php
+++ core/Tracker/Visit/SpamFilter.php
@@ -3,7 +3,6 @@
 namespace Piwik\Tracker\Visit;
 
 use Piwik\Common;
-use Piwik\Container\StaticContainer;
 use Piwik\Tracker\Request;
 
 /**
@@ -44,16 +43,8 @@ class SpamFilter
             return $this->spammerList;
         }
 
-        $file = StaticContainer::get('path.tmp') . '/spammers.txt';
-        if (!file_exists($file)) {
-            $file = PIWIK_INCLUDE_PATH . '/vendor/piwik/referrer-spam-blacklist/spammers.txt';
-        }
-
+        $file = PIWIK_INCLUDE_PATH . '/vendor/piwik/referrer-spam-blacklist/spammers.txt';
         $this->spammerList = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
-        if (!is_array($this->spammerList)) {
-            throw new \Exception(sprintf('The file %s does not contain a JSON array', $file));
-        }
 
         return $this->spammerList;
     }
