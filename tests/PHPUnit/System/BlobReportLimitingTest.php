5fe7ae348a7c18b170251965a0a9ac1e6fbae3bf
diosmosis <benaka@piwik.pro>
2015-05-22 14:10:08 -0700

    Fixing BlobReportLimitingTest. Fixture::createConfig has no effect now, and setting config must be done after environment is created.

--- tests/PHPUnit/System/BlobReportLimitingTest.php
+++ tests/PHPUnit/System/BlobReportLimitingTest.php
@@ -29,12 +29,6 @@ class BlobReportLimitingTest extends SystemTestCase
      */
     public static $fixture = null; // initialized below class definition
 
-    public static function setUpBeforeClass()
-    {
-        self::setUpConfigOptions();
-        parent::setUpBeforeClass();
-    }
-
     public function setUp()
     {
         Cache::getTransientCache()->flushAll();
@@ -103,6 +97,8 @@ class BlobReportLimitingTest extends SystemTestCase
      */
     public function testApi($api, $params)
     {
+        self::setUpConfigOptions();
+
         $this->runApiTests($api, $params);
     }
 
@@ -187,4 +183,3 @@ class BlobReportLimitingTest extends SystemTestCase
 }
 
 BlobReportLimitingTest::$fixture = new ManyVisitsWithMockLocationProvider();
-BlobReportLimitingTest::$fixture->createConfig = false;
