c84e7a83078e0764314abbfd5062cda9c1507cf9
Matthieu Napoli <matthieu@mnapoli.fr>
2015-05-26 09:34:09 +0200

    Remove scrolling to the site being edited as it's very confusing and not particularly helpful

--- plugins/SitesManager/SitesManager.php
+++ plugins/SitesManager/SitesManager.php
@@ -91,7 +91,6 @@ class SitesManager extends \Piwik\Plugin
         $jsFiles[] = "plugins/SitesManager/angularjs/sites-manager/sites-manager-admin-sites-model.js";
         $jsFiles[] = "plugins/SitesManager/angularjs/sites-manager/multiline-field.directive.js";
         $jsFiles[] = "plugins/SitesManager/angularjs/sites-manager/edit-trigger.directive.js";
-        $jsFiles[] = "plugins/SitesManager/angularjs/sites-manager/scroll.directive.js";
         $jsFiles[] = "plugins/SitesManager/angularjs/sites-manager/sites-manager.controller.js";
         $jsFiles[] = "plugins/SitesManager/angularjs/sites-manager/sites-manager-site.controller.js";
     }
