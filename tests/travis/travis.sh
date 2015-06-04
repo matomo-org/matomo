e38a608ea33b237d323948aa853605400b3ff7c8
diosmosis <benaka@piwik.pro>
2015-06-04 13:39:56 -0700

    Try to debug UI test sluggishness.

--- tests/travis/travis.sh
+++ tests/travis/travis.sh
@@ -63,7 +63,7 @@ then
         then
             phantomjs ../lib/screenshot-testing/run-tests.js --assume-artifacts --persist-fixture-data --screenshot-repo=$TRAVIS_REPO_SLUG --plugin=$PLUGIN_NAME
         else
-            phantomjs ../lib/screenshot-testing/run-tests.js --store-in-ui-tests-repo --persist-fixture-data --assume-artifacts --core
+            phantomjs ../lib/screenshot-testing/run-tests.js --store-in-ui-tests-repo --persist-fixture-data --assume-artifacts --core ActionsDataTable --print-logs
         fi
     elif [ "$TEST_SUITE" = "AllTests" ]
     then
