65c465a550a7a2ac242f2aa5bb49c4e2f4e2584e
sgiehl <stefan@piwik.org>
2015-06-07 21:48:37 +0200

    use plugin.json instead of getInformation() method for SEO plugin

--- plugins/SEO/SEO.php
+++ plugins/SEO/SEO.php
@@ -8,23 +8,8 @@
  */
 namespace Piwik\Plugins\SEO;
 
-use Piwik\Version;
-
 /**
  */
 class SEO extends \Piwik\Plugin
 {
-    /**
-     * @see Piwik\Plugin::getInformation
-     */
-    public function getInformation()
-    {
-        return array(
-            'description'      => 'This Plugin extracts and displays SEO metrics: Alexa web ranking, Google Pagerank, number of Indexed pages and backlinks of the currently selected website.',
-            'authors'          => array(array('name' => 'Piwik', 'homepage' => 'http://piwik.org/')),
-            'version'          => Version::VERSION,
-            'license'          => 'GPL v3+',
-            'license_homepage' => 'http://www.gnu.org/licenses/gpl.html'
-        );
-    }
 }
