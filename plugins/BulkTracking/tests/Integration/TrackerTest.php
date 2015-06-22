marcenuc/piwik

Make sure BulkTracking skips requests for nonexistant sites so other requests will still be tracked. Includes new tests in BulkTracking plugin.

plugins/BulkTracking/tests/Integration/TrackerTest.php
@@ -66,7 +66,7 @@ class TrackerTest extends BulkTrackingTestCase
     {
         $response = $this->tracker->main($this->getHandler(), $this->getEmptyRequestSet());
 
-        $this->assertSame('{"status":"success","tracked":2}', $response);
+        $this->assertSame('{"status":"success","tracked":2,"skipped":0}', $response);
     }
 
     public function test_main_shouldReturnErrorResponse_InCaseOfAnyError()
@@ -79,7 +79,7 @@ class TrackerTest extends BulkTrackingTestCase
 
         $response = $this->tracker->main($handler, $requestSet);
 
-        $this->assertSame('{"status":"error","tracked":0}', $response);
+        $this->assertSame('{"status":"error","tracked":0,"skipped":0}', $response);
     }
 
     public function test_main_shouldReturnErrorResponse_IfNotAuthorized()
@@ -91,7 +91,7 @@ class TrackerTest extends BulkTrackingTestCase
 
         $response = $this->tracker->main($handler, $this->getEmptyRequestSet());
 
-        $this->assertSame('{"status":"error","tracked":0}', $response);
+        $this->assertSame('{"status":"error","tracked":0,"skipped":0}', $response);
     }
 
     public function test_main_shouldActuallyTrack()

plugins/BulkTracking/tests/System/TrackerTest.php
@@ -46,8 +46,12 @@ class TrackerTest extends SystemTestCase
         $this->tracker->doTrackPageView('Test');
         $this->tracker->doTrackPageView('Test');
 
+        // test skipping invalid site errors
+        $this->tracker->setIdSite(5);
+        $this->tracker->doTrackPageView('Test');
+
         $response = $this->tracker->doBulkTrack();
 
-        $this->assertEquals('{"status":"success","tracked":2}', $response);
+        $this->assertEquals('{"status":"success","tracked":2,"skipped":1}', $response);
     }
 }

matomo-org/matomo

plugins/BulkTracking/tests/Integration/TrackerTest.php
@@ -66,7 +66,7 @@ class TrackerTest extends BulkTrackingTestCase
     {
         $response = $this->tracker->main($this->getHandler(), $this->getEmptyRequestSet());
 
-        $this->assertSame('{"status":"success","tracked":2}', $response);
+        $this->assertSame('{"status":"success","tracked":2,"invalid":0}', $response);
     }
 
     public function test_main_shouldReturnErrorResponse_InCaseOfAnyError()
@@ -79,7 +79,7 @@ class TrackerTest extends BulkTrackingTestCase
 
         $response = $this->tracker->main($handler, $requestSet);
 
-        $this->assertSame('{"status":"error","tracked":0}', $response);
+        $this->assertSame('{"status":"error","tracked":0,"invalid":0}', $response);
     }
 
     public function test_main_shouldReturnErrorResponse_IfNotAuthorized()
@@ -91,7 +91,7 @@ class TrackerTest extends BulkTrackingTestCase
 
         $response = $this->tracker->main($handler, $this->getEmptyRequestSet());
 
-        $this->assertSame('{"status":"error","tracked":0}', $response);
+        $this->assertSame('{"status":"error","tracked":0,"invalid":0}', $response);
     }
 
     public function test_main_shouldActuallyTrack()

plugins/BulkTracking/tests/System/TrackerTest.php
@@ -46,8 +46,12 @@ class TrackerTest extends SystemTestCase
         $this->tracker->doTrackPageView('Test');
         $this->tracker->doTrackPageView('Test');
 
+        // test skipping invalid site errors
+        $this->tracker->setIdSite(5);
+        $this->tracker->doTrackPageView('Test');
+
         $response = $this->tracker->doBulkTrack();
 
-        $this->assertEquals('{"status":"success","tracked":2}', $response);
+        $this->assertEquals('{"status":"success","tracked":2,"invalid":1}', $response);
     }
 }