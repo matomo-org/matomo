f445fa067d3be851ac1ce5c31651ded86822e75d
barbushin <barbushin@gmail.com>
2015-06-24 23:09:34 +0300

    Speeding up javascript tests 2.5 times

--- tests/javascript/piwik.php
+++ tests/javascript/piwik.php
@@ -75,8 +75,6 @@ if (isset($_GET['requests'])) {
 
 	echo "<html><head><title>$token</title></head><body>\n";
 
-	sleep(5);
-
 //	$result = $sqlite->query_array("SELECT uri FROM requests");
 	$result = @$sqlite->query_array("SELECT uri FROM requests WHERE token = \"$token\" AND ua = \"$ua\" ORDER BY ts ASC, requestid ASC");
 	if ($result !== false) {
