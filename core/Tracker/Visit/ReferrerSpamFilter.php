ea47e389a94b888a901383128fbeb3754ab9eb8e
Christian Raue <christian.raue@gmail.com>
2015-06-26 08:49:03 +0200

    fix referrer spam blacklist path when Piwik is installed as a Composer dependency

--- core/Tracker/Visit/ReferrerSpamFilter.php
+++ core/Tracker/Visit/ReferrerSpamFilter.php
@@ -68,7 +68,11 @@ class ReferrerSpamFilter
             $this->spammerList = unserialize($list);
         } else {
             // Fallback to reading the bundled list
-            $file = PIWIK_INCLUDE_PATH . '/vendor/piwik/referrer-spam-blacklist/spammers.txt';
+            if (file_exists(PIWIK_INCLUDE_PATH . '/vendor/piwik/referrer-spam-blacklist/spammers.txt')) {
+                $file = PIWIK_INCLUDE_PATH . '/vendor/piwik/referrer-spam-blacklist/spammers.txt'; // Piwik is the main project
+            } else {
+                $file = PIWIK_INCLUDE_PATH . '/../../piwik/referrer-spam-blacklist/spammers.txt'; // Piwik is installed as a dependency
+            }
             $this->spammerList = file($file, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
         }
 
