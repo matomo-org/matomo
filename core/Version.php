662da46454078fdccb99d344d7e201f9f3d4254d
Matthieu Aubry <matt@piwik.org>
2015-07-16 17:52:07 +0200

    2.14.1 stable release

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Piwik version.
      * @var string
      */
-    const VERSION = '2.14.1-rc1';
+    const VERSION = '2.14.1';
 
     public function isStableVersion($version)
     {
