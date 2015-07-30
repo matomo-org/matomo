9426bc012ba5c26ca629c3c2f122c7d3f4c884eb
Thomas Steur <thomas.steur@gmail.com>
2015-07-30 12:13:11 +0000

    refs #7692 Website selector: searching for special strings will show html code

--- plugins/CoreHome/angularjs/common/directives/autocomplete-matched.js
+++ plugins/CoreHome/angularjs/common/directives/autocomplete-matched.js
@@ -19,26 +19,30 @@
     angular.module('piwikApp.directive').directive('piwikAutocompleteMatched', piwikAutocompleteMatched);
 
     function piwikAutocompleteMatched() {
-        return function(scope, element, attrs) {
-            var searchTerm;
 
-            scope.$watch(attrs.piwikAutocompleteMatched, function(value) {
-                searchTerm = value;
-                updateText();
-            });
+        return {
+            priority: 10,
+            link: function (scope, element, attrs) {
+                var searchTerm;
 
-            function updateText () {
-                if (!searchTerm || !element) {
-                    return;
-                }
+                scope.$watch(attrs.piwikAutocompleteMatched, function (value) {
+                    searchTerm = value;
+                    updateText();
+                });
 
-                var content   = element.html();
-                var startTerm = content.toLowerCase().indexOf(searchTerm.toLowerCase());
+                function updateText() {
+                    if (!searchTerm || !element) {
+                        return;
+                    }
 
-                if (-1 !== startTerm) {
-                    var word = content.substr(startTerm, searchTerm.length);
-                    content = content.replace(word, '<span class="autocompleteMatched">' + word + '</span>');
-                    element.html(content);
+                    var content = element.html();
+                    var startTerm = content.toLowerCase().indexOf(searchTerm.toLowerCase());
+
+                    if (-1 !== startTerm) {
+                        var word = content.substr(startTerm, searchTerm.length);
+                        content = content.replace(word, '<span class="autocompleteMatched">' + word + '</span>');
+                        element.html(content);
+                    }
                 }
             }
         };
