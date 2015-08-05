08f113d9c4edae6182bfd458e63016f49a7dd024
Matthieu Aubry <matt@piwik.org>
2015-08-05 21:05:54 +0200

    2.14.3

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Piwik version.
      * @var string
      */
-    const VERSION = '2.14.3-b1';
+    const VERSION = '2.14.3';
 
     public function isStableVersion($version)
     {
