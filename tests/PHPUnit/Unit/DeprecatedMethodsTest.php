342d91e923865839b900f1273756307eef5bb1ff
Matthieu Napoli <matthieu@mnapoli.fr>
2015-08-21 17:24:49 +0200

    Remove a deprecation test and move it to an issue: #8619
    Else unit tests fail.

--- tests/PHPUnit/Unit/DeprecatedMethodsTest.php
+++ tests/PHPUnit/Unit/DeprecatedMethodsTest.php
@@ -68,7 +68,6 @@ class DeprecatedMethodsTest extends \PHPUnit_Framework_TestCase
         $this->assertDeprecatedMethodIsRemoved('Piwik\Archive', 'getBlob', $validTill);
 
         $this->assertDeprecatedMethodIsRemovedInPiwik3('\Piwik\Menu\MenuAbstract', 'add');
-        $this->assertDeprecatedMethodIsRemovedInPiwik3('\Piwik\Archive', 'getDataTableFromArchive');
     }
 
     private function assertDeprecatedMethodIsRemoved($className, $method, $removalDate)
