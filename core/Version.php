nuxwin/piwik

refs #8549 support for multiple release channels

core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Piwik version.
      * @var string
      */
-    const VERSION = '2.15.0-b4';
+    const VERSION = '2.15.0-b6';
 
     public function isStableVersion($version)
     {

matomo-org/matomo

core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Piwik version.
      * @var string
      */
-    const VERSION = '2.15.0-b11';
+    const VERSION = '2.15.0-b12';
 
     public function isStableVersion($version)
     {