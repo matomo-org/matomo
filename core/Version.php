b7028602859c1438f0eea8f8293f813381826227
Thomas Steur <thomas.steur@gmail.com>
2015-10-12 11:58:35 +0000

    make sure to remove no longer needed plugins

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Piwik version.
      * @var string
      */
-    const VERSION = '2.15.0-b19';
+    const VERSION = '2.15.0-b20';
 
     public function isStableVersion($version)
     {
