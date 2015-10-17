0ef3b95695ad984b95bba0dd13f54979e57173d3
diosmosis <benaka@piwik.pro>
2015-10-16 17:27:27 -0700

    Add misc folder as symlink in UI tests.

--- plugins/TestRunner/Commands/TestsSetupFixture.php
+++ plugins/TestRunner/Commands/TestsSetupFixture.php
@@ -154,7 +154,7 @@ class TestsSetupFixture extends ConsoleCommand
     private function createSymbolicLinksForUITests()
     {
         // make sure symbolic links exist (phantomjs doesn't support symlink-ing yet)
-        foreach (array('libs', 'plugins', 'tests', 'piwik.js') as $linkName) {
+        foreach (array('libs', 'plugins', 'tests', 'misc', 'piwik.js') as $linkName) {
             $linkPath = PIWIK_INCLUDE_PATH . '/tests/PHPUnit/proxy/' . $linkName;
             if (!file_exists($linkPath)) {
                 symlink(PIWIK_INCLUDE_PATH . '/' . $linkName, $linkPath);
