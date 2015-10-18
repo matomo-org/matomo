de483434a50e1a262056d63426ad17d1b9159414
diosmosis <benaka@piwik.pro>
2015-10-17 18:18:52 -0700

    Fix failing Dashboard test by removing RSS widget contents for UI tests.

--- config/environment/ui-test.php
+++ config/environment/ui-test.php
@@ -18,6 +18,9 @@ return array(
             $result = str_replace(realpath(PIWIK_INCLUDE_PATH), '', $result);
         }),
 
+        array('Controller.ExampleRssWidget.rssPiwik.end', function (&$result, $parameters) {
+            $result = "";
+        }),
     ),
 
 );
