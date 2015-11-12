b4957269a0238065bdb83c9592121fcfb1a655bd
Thomas Steur <thomas.steur@gmail.com>
2015-11-12 20:20:14 +0000

    refs #9201 do not use DI for request processors

--- plugins/CoreHome/config/config.php
+++ plugins/CoreHome/config/config.php
@@ -3,10 +3,6 @@
 return array(
 
     'Piwik\Plugins\CoreHome\Tracker\VisitRequestProcessor' => DI\object()
-        ->constructorParameter('visitStandardLength', DI\get('ini.Tracker.visit_standard_length')),
-
-    'tracker.request.processors' => DI\add(array(
-        DI\get('Piwik\Plugins\CoreHome\Tracker\VisitRequestProcessor'),
-    )),
+        ->constructorParameter('visitStandardLength', DI\get('ini.Tracker.visit_standard_length'))
 
 );
