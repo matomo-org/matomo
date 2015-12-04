34b615a8d46a8d4cac90dd17dc7a34a18fc232fb
Thomas Steur <thomas.steur@gmail.com>
2015-12-04 03:18:56 +0000

    added documentation for cookieConfigPath detection

--- js/piwik.js
+++ js/piwik.js
@@ -5520,7 +5520,12 @@ if (typeof Piwik !== 'object') {
                 /**
                  * Set array of domains to be treated as local. Also supports path, eg '.piwik.org/subsite1'. In this
                  * case all links that don't go to '*.piwik.org/subsite1/ *' would be treated as outlinks.
-                 * For example a link to 'piwik.org/' or 'piwik.org/subsite2' both would be treated as outlinks
+                 * For example a link to 'piwik.org/' or 'piwik.org/subsite2' both would be treated as outlinks.
+                 *
+                 * We might automatically set a cookieConfigPath to avoid creating several cookies under one domain
+                 * if there is a hostAlias defined with a path. Say a user is visiting 'http://piwik.org/subsite1'
+                 * and '.piwik.org/subsite1' is set as a hostsAlias. Piwik will automatically use '/subsite1' as
+                 * cookieConfigPath.
                  *
                  * @param string|array hostsAlias
                  */
