216e922e2ae34c48e0e56d72cb13808e3158f511
John Maguire <contact@johnmaguire.me>
2015-12-08 21:33:05 -0500

    Manage PiwikTracker through composer (closes #9349)

---
+++ libs/PiwikTracker/PiwikTracker.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+// PiwikTracker.php is now managed by composer. To prevent breaking existing
+// code, this file has been left as a redirect to its new location in the
+// vendor directory.
+if (!class_exists('PiwikTracker')) {
+    require_once __DIR__ . '/../../vendor/piwik/piwik-php-tracker/PiwikTracker.php';
+}
