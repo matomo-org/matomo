9cf076a73bd983fa0b007779e0706dff8d6c2314
Thomas Steur <thomas.steur@gmail.com>
2015-12-13 20:47:17 +0000

    remove all usages of alter ignore

--- core/Updates/1.5-b1.php
+++ core/Updates/1.5-b1.php
@@ -38,21 +38,22 @@ class Updates_1_5_b1 extends Updates
 										          INDEX index_idsite_servertime ( idsite, server_time )
 												)  DEFAULT CHARSET=utf8 '              => 1050,
 
-            'ALTER IGNORE TABLE `' . Common::prefixTable('log_visit') . '`
+            'ALTER TABLE `' . Common::prefixTable('log_visit') . '`
 				 ADD  visitor_days_since_order SMALLINT(5) UNSIGNED NOT NULL AFTER visitor_days_since_last,
 				 ADD  visit_goal_buyer TINYINT(1) NOT NULL AFTER visit_goal_converted' => 1060,
 
-            'ALTER IGNORE TABLE `' . Common::prefixTable('log_conversion') . '`
+            'ALTER TABLE `' . Common::prefixTable('log_conversion') . '`
 				 ADD visitor_days_since_order SMALLINT(5) UNSIGNED NOT NULL AFTER visitor_days_since_first' => 1060,
-            'ALTER IGNORE TABLE `' . Common::prefixTable('log_conversion') . '`
+            'ALTER TABLE `' . Common::prefixTable('log_conversion') . '`
 				 ADD idorder varchar(100) default NULL AFTER buster,
 				 ADD items SMALLINT UNSIGNED DEFAULT NULL,
 				 ADD revenue_subtotal float default NULL,
 				 ADD revenue_tax float default NULL,
 				 ADD  revenue_shipping float default NULL,
 				 ADD revenue_discount float default NULL,
-				 ADD UNIQUE KEY unique_idsite_idorder (idsite, idorder),
 				 MODIFY  idgoal int(10) NOT NULL'                                      => 1060,
+            'ALTER TABLE `' . Common::prefixTable('log_conversion') . '`
+				 ADD UNIQUE KEY unique_idsite_idorder (idsite, idorder)' => 1060,
         );
     }
 
