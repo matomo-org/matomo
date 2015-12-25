41b579a9d94a05f7c0cb2e42bf7c03c2d374ed95
sgiehl <stefan@piwik.org>
2015-12-25 20:18:27 +0100

    fix merge failure

--- plugins/Goals/Goals.php
+++ plugins/Goals/Goals.php
@@ -9,9 +9,9 @@
 namespace Piwik\Plugins\Goals;
 
 use Piwik\ArchiveProcessor;
+use Piwik\Common;
 use Piwik\Db;
 use Piwik\Piwik;
-use Piwik\Plugin\Report;
 use Piwik\Plugin\Reports;
 use Piwik\Tracker\GoalManager;
 use Piwik\Category\Subcategory;
