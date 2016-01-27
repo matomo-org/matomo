f8d3e6fad11413cbeef5eb71080773dadf944012
Matthieu Aubry <matt@piwik.org>
2016-01-27 17:59:26 +1300

    2.16.0-rc1

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Piwik version.
      * @var string
      */
-    const VERSION = '2.16.0-b6';
+    const VERSION = '2.16.0-rc1';
 
     public function isStableVersion($version)
     {
