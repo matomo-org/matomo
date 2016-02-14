5091996c74f5de0e0bbb054e42742de957415bed
Thomas Steur <thomas.steur@gmail.com>
2016-02-14 23:22:38 +0000

    fix some ui tests

--- tests/UI/specs/UIIntegration_spec.js
+++ tests/UI/specs/UIIntegration_spec.js
@@ -155,7 +155,7 @@ describe("UIIntegrationTest", function () { // TODO: Rename to Piwik?
     // actions pages
     it('should load the actions > pages help tooltip, including the "Report generated time"', function (done) {
         expect.screenshot('actions_pages_tooltip_help').to.be.captureSelector('.pageWrap,.expandDataTableFooterDrawer', function (page) {
-            page.load("?" + urlBase + "#" + generalParams + "&module=Actions&action=menuGetPageUrls");
+            page.load("?" + urlBase + "#" + generalParams + "&category=General_Actions&subcategory=General_Pages");
             page.mouseMove('h2[piwik-enriched-headline]');
             page.click(".helpIcon");
             page.evaluate(function () {
