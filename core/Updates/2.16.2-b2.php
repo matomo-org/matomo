645e3509c307dbc21f7705ddbee3de81fa0ccb71
Thomas Steur <thomas.steur@gmail.com>
2016-04-19 03:33:12 +0000

    addding userId plugin description and enable plugin during update

---
+++ core/Updates/2.16.2-b2.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+namespace Piwik\Updates;
+
+use Piwik\Updater;
+use Piwik\Updates as PiwikUpdates;
+
+/**
+ * Update for version 2.16.2-b2.
+ */
+class Updates_2_16_2_b2 extends PiwikUpdates
+{
+
+    public function doUpdate(Updater $updater)
+    {
+        $pluginManager = \Piwik\Plugin\Manager::getInstance();
+        $pluginName = 'UserId';
+
+        try {
+            if (!$pluginManager->isPluginActivated($pluginName)) {
+                $pluginManager->activatePlugin($pluginName);
+            }
+        } catch (\Exception $e) {
+        }
+    }
+}
