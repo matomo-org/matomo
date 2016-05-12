904ec83402488bea9da7b5b01136575a8bf7f9d2
Kamil Zajac <kamil@piwik.pro>
2016-05-12 10:07:10 +0200

    Add possibility to execute plugin test suite for UI tests

--- plugins/TestRunner/Commands/TestsRun.php
+++ plugins/TestRunner/Commands/TestsRun.php
@@ -194,7 +194,7 @@ class TestsRun extends ConsoleCommand
 
     private function getTestsSuites()
     {
-        return array('unit', 'integration', 'system');
+        return array('unit', 'integration', 'system', 'plugin');
     }
 
     /**
