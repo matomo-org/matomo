9878dc13e9167745018e18bc2d5458cbaa6cbf01
Thomas Fasselt <Thomas--F@users.noreply.github.com>
2016-09-27 16:27:16 +0200

    Update VisitExcluded.php

--- core/Tracker/VisitExcluded.php
+++ core/Tracker/VisitExcluded.php
@@ -92,6 +92,9 @@ class VisitExcluded
          * @param Request $request The request object which contains all of the request's information
          * @param bool|string $userAgent The user-agent used by the visitor
          * @param bool|string $ip The IP-address of the visitor
+         *
+         * The additional parameters $userAgent and $ip are needed, because the values in the 
+         * object $request are empty when the constructor is called from the log-import.
          */
         Piwik::postEvent('Tracker.isExcludedVisit', array(&$excluded, $this->request, $this->userAgent, $this->ip));
 
