4651e3b145eee238e63af3285ce52ecae91290a2
mattab <matthieu.aubry@gmail.com>
2016-09-29 14:20:00 +1300

    Make test better

--- tests/resources/Updater/testpluginUpdates/0.5.php
+++ tests/resources/Updater/testpluginUpdates/0.5.php
@@ -7,6 +7,6 @@ class Updates_0_5 extends PiwikUpdates
 {
     function doUpdate(\Piwik\Updater $updater)
     {
-        throw new \Piwik\Exception\MissingFilePermissionException();
+        throw new \Piwik\Exception\MissingFilePermissionException('make sure this exception is thrown');
     }
 }
