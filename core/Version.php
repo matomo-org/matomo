1600c0b8d117468c6a9230bbbceabf53d548267e
Stefan Giehl <stefan@piwik.org>
2016-10-04 23:10:14 +0200

    Fix update script screwing up scheduled report data (#10680)
    * move fix of broken update script to 2.16.5
    
    * increase version to 2.16.5

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Piwik version.
      * @var string
      */
-    const VERSION = '2.16.4';
+    const VERSION = '2.16.5';
 
     public function isStableVersion($version)
     {
