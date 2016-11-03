ac6cfd65b676f80889def6db1c063839d0138430
mattab <matthieu.aubry@gmail.com>
2016-11-03 15:44:04 +1300

    Remove deprecated examples

--- core/Menu/MenuAdmin.php
+++ core/Menu/MenuAdmin.php
@@ -16,19 +16,6 @@ use Piwik\Piwik;
  * Plugins can implement the `configureAdminMenu()` method of the `Menu` plugin class to add, rename of remove
  * items. If your plugin does not have a `Menu` class yet you can create one using `./console generate:menu`.
  *
- * **Example**
- *
- *     public function configureAdminMenu(MenuAdmin $menu)
- *     {
- *         $menu->add(
- *             'MyPlugin_MyTranslatedAdminMenuCategory',
- *             'MyPlugin_MyTranslatedAdminPageName',
- *             array('module' => 'MyPlugin', 'action' => 'index'),
- *             Piwik::isUserHasSomeAdminAccess(),
- *             $order = 2
- *         );
- *     }
- *
  * @method static \Piwik\Menu\MenuAdmin getInstance()
  */
 class MenuAdmin extends MenuAbstract
