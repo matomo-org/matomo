5c7b7e6fc0ab2b6c58f65b050ecdb043872c7dc7
mattab <matthieu.aubry@gmail.com>
2016-11-14 15:18:15 +1300

    typo

--- plugins/ProfessionalServices/Promo.php
+++ plugins/ProfessionalServices/Promo.php
@@ -14,74 +14,78 @@ class Promo
 {
     protected $linkTitles = array('Read more', 'Learn more');
 
-    protected $content = array(
-        // A/B Testing
-        array(
-            'campaignContent' => 'abTesting',
-            'url' => 'https://piwik.org/recommends/ab-testing-learn-more',
-            'text' => 'Ever wondered what A/B testing is and why it is so important? Check out how you can increase conversions and sales on your website and app.'
-        ),
-        array(
-            'campaignContent' => 'abTesting',
-            'url' => ' https://piwik.org/recommends/ab-testing-website',
-            'text' => 'Increase revenue and conversions by comparing different versions of your websites or apps, ad detect the winning variation that will grow your business!'
-        ),
+    protected $content = array();
 
-        // Media Analytics
-        array(
-            'campaignContent' => 'mediaAnalytics',
-            'url' => 'https://piwik.org/recommends/media-analytics-website',
-            'text' => 'Ever wondered how people interact and engage with videos or audios on your website? Well now you can, and it integrates perfectly into your Piwik.',
-        ),
+    public function __construct()
+    {
+        $this->content = array(
+            // A/B Testing
+            array(
+                'campaignContent' => 'abTesting',
+                'url' => 'https://piwik.org/recommends/ab-testing-learn-more',
+                'text' => 'Ever wondered what A/B testing is and why it is so important? Check out how you can increase conversions and sales on your website and app.'
+            ),
+            array(
+                'campaignContent' => 'abTesting',
+                'url' => ' https://piwik.org/recommends/ab-testing-website',
+                'text' => 'Increase revenue and conversions by comparing different versions of your websites or apps, ad detect the winning variation that will grow your business!'
+            ),
 
-        array(
-            'campaignContent' => 'mediaAnalytics',
-            'url' => 'https://piwik.org/recommends/media-analytics',
-            'text' => 'Get powerful insights into how your audience watches your videos and listens to your audio. Media Analytics is now available on the Marketplace.',
-        ),
+            // Media Analytics
+            array(
+                'campaignContent' => 'mediaAnalytics',
+                'url' => 'https://piwik.org/recommends/media-analytics-website',
+                'text' => 'Ever wondered how people interact and engage with videos or audios on your website? Well now you can, and it integrates perfectly into your Piwik.',
+            ),
 
-        // Piwik training
-        array(
-            'campaignContent' => 'userTraining',
-            'url' => 'https://piwik.org/training/?pk_campaign='. Advertising::CAMPAIGN_NAME_PROFESSIONAL_SERVICES .'&pk_source=Piwik_App',
-            'text' => 'Want to know how to use all the exciting features in Piwik? Try a User training to be up to speed with working with Piwik.'
-        ),
+            array(
+                'campaignContent' => 'mediaAnalytics',
+                'url' => 'https://piwik.org/recommends/media-analytics',
+                'text' => 'Get powerful insights into how your audience watches your videos and listens to your audio. Media Analytics is now available on the Marketplace.',
+            ),
+
+            // Piwik training
+            array(
+                'campaignContent' => 'userTraining',
+                'url' => 'https://piwik.org/training/?pk_campaign=' . Advertising::CAMPAIGN_NAME_PROFESSIONAL_SERVICES . '&pk_source=Piwik_App',
+                'text' => 'Want to know how to use all the exciting features in Piwik? Try a User training to be up to speed with working with Piwik.'
+            ),
 
 
-        // Piwik PRO
-        // https://piwik.org/recommends/piwik-pro-from-app
-        array(
-            'campaignContent' => 'discoverPower',
-            'url' => 'https://piwik.org/recommends/piwik-pro-from-app',
-            'text' => 'Discover the power of open-source combined with enterprise-grade support and premium functionalities.'
-        ),
-        array(
-            'campaignContent' => 'bringEnterpriseLevel',
-            'url' => 'https://piwik.org/recommends/piwik-pro-from-app',
-            'text' => 'Bring your analytics to enterprise level. Upgrade your Piwik platform and receive access to numerous premium features and assistance from experts.'
-        ),
-        array(
-            'campaignContent' => 'monitoringAndIncident',
-            'url' => 'https://piwik.org/recommends/piwik-pro-from-app',
-            'text' => 'Do you need 24/7 Monitoring and Incident Handling for your Piwik? Get Premium features and enterprise-grade support.'
-        ),
-        array(
-            'campaignContent' => 'slowingDown',
-            'url' => 'https://piwik.org/recommends/piwik-pro-from-app',
-            'text' => 'Is your Piwik slowing down? Piwik experts can help with your server setup!'
-        ),
-        array(
-            'campaignContent' => 'whiteLabel',
-            'url' => 'https://piwik.org/recommends/piwik-pro-from-app',
-            'text' => 'Did you know you can adjust the look and feel of Piwik to your brand, and even replace "Piwik" with your product name? Try the White Label product!',
-        ),
-        array(
-            'campaignContent' => 'metaSites',
-            'url' => 'https://piwik.org/recommends/piwik-pro-from-app',
-            'text' => 'Did you know you can aggregate the collected data across hundreds of sites and display it in a single dashboard? Get Premium features and enterprise-grade support.',
-        ),
-
-    );
+            // Piwik PRO
+            // https://piwik.org/recommends/piwik-pro-from-app
+            array(
+                'campaignContent' => 'discoverPower',
+                'url' => 'https://piwik.org/recommends/piwik-pro-from-app',
+                'text' => 'Discover the power of open-source combined with enterprise-grade support and premium functionalities.'
+            ),
+            array(
+                'campaignContent' => 'bringEnterpriseLevel',
+                'url' => 'https://piwik.org/recommends/piwik-pro-from-app',
+                'text' => 'Bring your analytics to enterprise level. Upgrade your Piwik platform and receive access to numerous premium features and assistance from experts.'
+            ),
+            array(
+                'campaignContent' => 'monitoringAndIncident',
+                'url' => 'https://piwik.org/recommends/piwik-pro-from-app',
+                'text' => 'Do you need 24/7 Monitoring and Incident Handling for your Piwik? Get Premium features and enterprise-grade support.'
+            ),
+            array(
+                'campaignContent' => 'slowingDown',
+                'url' => 'https://piwik.org/recommends/piwik-pro-from-app',
+                'text' => 'Is your Piwik slowing down? Piwik experts can help with your server setup!'
+            ),
+            array(
+                'campaignContent' => 'whiteLabel',
+                'url' => 'https://piwik.org/recommends/piwik-pro-from-app',
+                'text' => 'Did you know you can adjust the look and feel of Piwik to your brand, and even replace "Piwik" with your product name? Try the White Label product!',
+            ),
+            array(
+                'campaignContent' => 'metaSites',
+                'url' => 'https://piwik.org/recommends/piwik-pro-from-app',
+                'text' => 'Did you know you can aggregate the collected data across hundreds of sites and display it in a single dashboard? Get Premium features and enterprise-grade support.',
+            )
+        );
+    }
 
     public function getLinkTitle()
     {
