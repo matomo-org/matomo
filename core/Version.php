6d05c73409d4ce3733112e81c4835fbdb0f7340a
Matthieu Aubry <mattab@users.noreply.github.com>
2016-11-14 15:35:30 +1300

    2.17.1-rc1

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Piwik version.
      * @var string
      */
-    const VERSION = '2.17.0';
+    const VERSION = '2.17.1-rc1';
 
     public function isStableVersion($version)
     {
