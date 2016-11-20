b5c4385a3aa2b1a415c5ca795bd3e9811fe55dcb
Stefan Giehl <stefan@piwik.org>
2016-11-21 00:44:49 +0100

    Fix goal metric processing for device type / model / brand (#10873)
    * always persist visitor property if it's dimension implements `onAnyGoalConversion` event
    
    * small code improvements
    
    * adds some tests to prove converted goals are processed for device type / model / brand
    
    * update tests

---
+++ plugins/DevicesDetection/tests/System/GoalReportForDevicesTest.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+namespace Piwik\Plugins\DevicesDetection\tests\System;
+
+use Piwik\Plugins\DevicesDetection\tests\Fixtures\MultiDeviceGoalConversions;
+use Piwik\Tests\Framework\TestCase\SystemTestCase;
+
+/**
+ *
+ * @group Plugins
+ * @group DevicesDetection
+ */
+class GoalReportForDevicesTest extends SystemTestCase
+{
+    public static $fixture = null; // initialized below class definition
+
+    public static function getOutputPrefix()
+    {
+        return '';
+    }
+
+    public static function getPathToTestDirectory()
+    {
+        return dirname(__FILE__);
+    }
+
+    public function getApiForTesting()
+    {
+        $idSite   = self::$fixture->idSite;
+        $dateTime = self::$fixture->dateTime;
+
+        return array(
+            array('DevicesDetection.getType', array('idSite'  => $idSite, 'date' => $dateTime)),
+            array('DevicesDetection.getBrand', array('idSite' => $idSite, 'date' => $dateTime)),
+            array('DevicesDetection.getModel', array('idSite' => $idSite, 'date' => $dateTime)),
+        );
+    }
+
+    /**
+     * @dataProvider getApiForTesting
+     */
+    public function testApi($api, $params)
+    {
+        $this->runApiTests($api, $params);
+    }
+}
+
+GoalReportForDevicesTest::$fixture = new MultiDeviceGoalConversions();
