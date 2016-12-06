5b94ec76e74c3ce74b99690f92b5d5b2b489e9da
Matthieu Aubry <mattab@users.noreply.github.com>
2016-12-07 00:01:37 +1300

    Add a system test reproducing #10857  (#10970)
    * testing referrer website with both HTTP and HTTPS
    for https://github.com/piwik/piwik/issues/10857
    
    * Add a system test reproducing #10857
    
    Add a system test reproducing #10857 -> confirmed that Piwik will track referrers from the same HTTP and HTTPS page as different entries.

--- tests/PHPUnit/Fixtures/TwoSitesTwoVisitorsDifferentDays.php
+++ tests/PHPUnit/Fixtures/TwoSitesTwoVisitorsDifferentDays.php
@@ -135,7 +135,8 @@ class TwoSitesTwoVisitorsDifferentDays extends Fixture
             // Temporary, until we implement 1st party cookies in PiwikTracker
             $visitorB->DEBUG_APPEND_URL .= '&_idvc=2&_viewts=' . Date::factory($dateTime)->getTimestamp();
 
-            $visitorB->setUrlReferrer('http://referrer.com/Other_Page.htm');
+            $protocol = (0 === $days % 2) ? 'http' : 'https';
+            $visitorB->setUrlReferrer($protocol . '://referrer.com/Other_Page.htm');
             if( in_array($days, array(2,3,4,$daysToGenerateVisitsFor-1)) ) {
                 $visitorB->setUrl( self::URL_IS_GOAL_WITH_CAMPAIGN_PARAMETERS );
             } else {
