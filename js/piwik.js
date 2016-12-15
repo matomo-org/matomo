b14e8150b4b770dae63048812d578465c0eac6cb
Thomas Steur <thomas.steur@gmail.com>
2016-12-15 14:33:15 +0000

    added test

--- js/piwik.js
+++ js/piwik.js
@@ -1031,7 +1031,7 @@ if (typeof JSON2 !== 'object' && typeof window.JSON === 'object' && window.JSON.
 /*members push */
 /*global Piwik:true */
 /*members addPlugin, getTracker, getAsyncTracker, getAsyncTrackers, addTracker, trigger, on, off, retryMissedPluginCalls,
-          DOM, onLoad, onReady*/
+          DOM, onLoad, onReady, JSON */
 /*global Piwik_Overlay_Client */
 /*global AnalyticsTracker:true */
 /*members initialize */
