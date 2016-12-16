6233a8e412118f4d27d03761086750e232e9562b
Matthieu Aubry <mattab@users.noreply.github.com>
2016-12-16 16:39:12 +1300

    3.0.0-rc4 was never released

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Piwik version.
      * @var string
      */
-    const VERSION = '3.0.0-rc5';
+    const VERSION = '3.0.0-rc4';
 
     public function isStableVersion($version)
     {
