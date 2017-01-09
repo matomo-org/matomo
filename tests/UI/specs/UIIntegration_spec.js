1b41ba442308c848c397b31fb2498f00007906c6
Matthieu Aubry <mattab@users.noreply.github.com>
2017-01-10 11:07:25 +1300

    Fix MySQLi system test (#11162)
    * Fix MySQLi system test
    
    * Restoring one .to.be.skippedOnAbort();

--- tests/UI/specs/UIIntegration_spec.js
+++ tests/UI/specs/UIIntegration_spec.js
@@ -369,6 +369,7 @@ describe("UIIntegrationTest", function () { // TODO: Rename to Piwik?
     it('should load the widgetized visitor log correctly', function (done) {
         this.retries(3);
         expect.screenshot('widgetize_visitor_log').to.be.capture(function (page) {
+            expect.screenshot("widgetize_visitor_log").to.be.skippedOnAbort();
             page.load("?" + widgetizeParams + "&" + generalParams + "&moduleToWidgetize=Live&actionToWidgetize=getVisitorLog");
             page.evaluate(function () {
                 $('.expandDataTableFooterDrawer').click();
