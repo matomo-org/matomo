ba7baeb6378f110517c006047959c30e014654d7
Lukas Winkler <l.winkler23@mailbox.org>
2017-03-04 10:42:18 +0100

    better match for non dist icon files

--- tests/PHPUnit/Integration/ReleaseCheckListTest.php
+++ tests/PHPUnit/Integration/ReleaseCheckListTest.php
@@ -741,7 +741,7 @@ class ReleaseCheckListTest extends \PHPUnit_Framework_TestCase
         $files = Filesystem::globr(PIWIK_INCLUDE_PATH, '*');
 
         // ignore icon source files as they are large, but not included in the final package
-        $files = array_filter($files, function($value) { return !preg_match('~icons/src~', $value); });
+        $files = array_filter($files, function($value) { return !preg_match('~Morpheus/icons/(?!dist)~', $value); });
 
         $filesizes = array();
         foreach ($files as $file) {
