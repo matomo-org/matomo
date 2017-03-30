e6bac81abdf0d32429d208ce751f0e3333bacb20
sgiehl <stefan@piwik.org>
2017-03-30 22:41:45 +0200

    small improvement

--- plugins/DevicesDetection/Archiver.php
+++ plugins/DevicesDetection/Archiver.php
@@ -87,9 +87,7 @@ class Archiver extends \Piwik\Plugin\Archiver
             }
 
             while ($conversionRow = $query->fetch()) {
-                if (isset($conversionRow[$labelSQL])) {
-                    $metrics->sumMetricsGoals($conversionRow[$labelSQL], $conversionRow);
-                }
+                $metrics->sumMetricsGoals(isset($conversionRow[$labelSQL]) ? $conversionRow[$labelSQL] : null, $conversionRow);
             }
             $metrics->enrichMetricsWithConversions();
         }
