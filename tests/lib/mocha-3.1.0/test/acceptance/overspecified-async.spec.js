6a97e43ff69795c53c1b8dff63fc6a5cbdba8ca8
Stefan Giehl <stefan@piwik.org>
2017-04-18 20:43:51 +0200

    update mocha to 3.1.2 (#11625)

--- tests/lib/mocha-3.1.0/test/acceptance/overspecified-async.spec.js
+++
@@ -1,8 +0,0 @@
-describe('overspecified asynchronous resolution method', function() {
-  it('should fail when multiple methods are used', function(done) {
-    setTimeout(done, 0);
-
-    // uncomment
-    // return { then: function() {} };
-  });
-});
--- tests/lib/mocha-3.1.0/test/integration/fixtures/regression/issue-2417.fixture.js
+++
@@ -1,7 +0,0 @@
-describe('outer describe', function() {
-  describe.only('outer describe.only', function() {
-    it.only('inner it.only', function() {
-      // should run and exit without error
-    });
-  });
-});
