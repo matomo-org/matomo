d5b7c848aa38a2e47ea48b232787f1c7102b1ceb
Matthieu Aubry <mattab@users.noreply.github.com>
2017-05-08 19:56:59 +0200

    JS Tracker: prevent IE 7 error  (#11668)
    * Use typeof(console) to prevent IE 7 error
    
    When using directly console !== undefined, older IE versions
    throw an error as they're actually tring to access an undefined
    value. Use globally supported typeof(console) !== 'undefined'.
    
    Changed typeof(<var>) to typeof <var> as it's not a function
    
    Thanks @sgiehl
    
    * Change other occurences of console !== undefined
    
    * JSLint: Remove console available global as it is not in IE7 and other older browsers
    
    * Minified piwik.js
    
    * JSlint validation
    
    * Minified files after merging 3.x-dev
    
    * Fix regression

--- js/piwik.js
+++ js/piwik.js
@@ -1037,8 +1037,8 @@ if (typeof JSON_PIWIK !== 'object' && typeof window.JSON === 'object' && window.
 /*global AnalyticsTracker:true */
 /*members initialize */
 /*global define */
+/*global console */
 /*members amd */
-/*global console:true */
 /*members error */
 /*members log */
 
@@ -1168,7 +1168,9 @@ if (typeof window.Piwik !== 'object') {
          * @param message
          */
         function logConsoleError(message) {
-            if (console !== undefined && console && console.error) {
+            // needed to write it this way for jslint
+            var consoleType = typeof console;
+            if (consoleType !== 'undefined' && console && console.error) {
                 console.error(message);
             }
         }
@@ -6845,7 +6847,9 @@ if (typeof window.Piwik !== 'object') {
                 var contentNodes = content.findContentNodes();
                 var contents = content.collectContent(contentNodes);
 
-                if (console !== undefined && console && console.log) {
+                // needed to write it this way for jslint
+                var consoleType = typeof console;
+                if (consoleType !== 'undefined' && console && console.log) {
                     console.log(contents);
                 }
             };
@@ -7355,7 +7359,9 @@ if (typeof window.Piwik !== 'object') {
             window.Piwik.addTracker();
         } else {
             _paq = {push: function (args) {
-                if (console !== undefined && console && console.error) {
+                // needed to write it this way for jslint
+                var consoleType = typeof console;
+                if (consoleType !== 'undefined' && console && console.error) {
                     console.error('_paq.push() was used but Piwik tracker was not initialized before the piwik.js file was loaded. Make sure to configure the tracker via _paq.push before loading piwik.js. Alternatively, you can create a tracker via Piwik.addTracker() manually and then use _paq.push but it may not fully work as tracker methods may not be executed in the correct order.', args);
                 }
             }};
