765753ea2f948e44e9ced5b48393c1bdeb07a9fd
diosmosis <diosmosis@users.noreply.github.com>
2017-07-15 15:21:34 -0700

    Move bulk of period selector code from directive to controller, & fix variable name in date range picker template.

--- plugins/CoreHome/angularjs/period-selector/period-selector.controller.js
+++ plugins/CoreHome/angularjs/period-selector/period-selector.controller.js
@@ -7,9 +7,155 @@
 (function () {
     angular.module('piwikApp').controller('PeriodSelectorController', PeriodSelectorController);
 
-    PeriodSelectorController.$inject = [];
+    PeriodSelectorController.$inject = ['piwik', '$location', 'piwikPeriods'];
 
-    function PeriodSelectorController() {
-        // TODO
+    function PeriodSelectorController(piwik, $location, piwikPeriods) {
+        var vm = this;
+
+        // the period & date currently being viewed
+        vm.periodValue = null;
+        vm.dateValue = null;
+
+        vm.selectedPeriod = null;
+
+        vm.startRangeDate = null;
+        vm.endRangeDate = null;
+
+        vm.getCurrentlyViewingText = getCurrentlyViewingText;
+        vm.changeViewedPeriod = changeViewedPeriod;
+        vm.setPiwikPeriodAndDate = setPiwikPeriodAndDate;
+        vm.onApplyClicked = onApplyClicked;
+        vm.updateSelectedValuesFromHash = updateSelectedValuesFromHash;
+
+        vm.updateSelectedValuesFromHash();
+        initTopControls(); // must be called when a top control changes width
+
+        function updateSelectedValuesFromHash() {
+            var search = $location.search();
+
+            vm.periodValue = search.period;
+            vm.selectedPeriod = search.period;
+
+            if (search.period === 'range') {
+                var parts = search.date.split(',');
+                vm.startRangeDate = parts[0];
+                vm.endRangeDate = parts[1];
+
+                vm.dateValue = parseDate(parts[0]);
+            } else {
+                vm.dateValue = parseDate(search.date);
+
+                var range = piwikPeriods.parse(search.period, search.date).getDateRange();
+                vm.startRangeDate = formatDate(range[0]);
+                vm.endRangeDate = formatDate(range[1]);
+            }
+        }
+
+        function getCurrentlyViewingText() {
+            var search = $location.search();
+            return piwikPeriods.parse(search.period, search.date).getPrettyString();
+        }
+
+        function changeViewedPeriod(period) {
+            // only change period if it's different from what's being shown currently
+            if (period === vm.periodValue) {
+                return;
+            }
+
+            // can't just change to a range period, w/o setting two new dates
+            if (period === 'range') {
+                return;
+            }
+
+            setPiwikPeriodAndDate(period, vm.dateValue);
+        }
+
+        function onApplyClicked() {
+            if (vm.selectedPeriod === 'range') {
+                var dateFrom = vm.startRangeDate,
+                    dateTo = vm.endRangeDate,
+                    oDateFrom = parseDate(dateFrom),
+                    oDateTo = parseDate(dateTo);
+
+                if (!isValidDate(oDateFrom)
+                    || !isValidDate(oDateTo)
+                    || oDateFrom > oDateTo
+                ) {
+                    // TODO: use a notification instead?
+                    $('#alert').find('h2').text(_pk_translate('General_InvalidDateRange'));
+                    piwikHelper.modalConfirm('#alert', {});
+                    return;
+                }
+
+                propagateNewUrlParams(dateFrom + ',' + dateTo, 'range');
+                return;
+            }
+
+            setPiwikPeriodAndDate(vm.selectedPeriod, vm.dateValue);
+
+        }
+
+        function setPiwikPeriodAndDate(period, date) {
+            period = period || vm.periodValue;
+            date = date || vm.dateValue;
+
+            piwik.period = period;
+            piwik.currentDateString = formatDate(date);
+
+            var dateRange = piwikPeriods.parse(period, piwik.currentDateString).getDateRange();
+            piwik.startDateString = formatDate(dateRange[0]);
+            piwik.endDateString = formatDate(dateRange[1]);
+
+            propagateNewUrlParams(piwik.currentDateString, vm.selectedPeriod);
+            initTopControls();
+        }
+
+        function propagateNewUrlParams(date, period) {
+            vm.closePeriodSelector(); // defined in directive
+
+            var $search = $location.search();
+            if (date !== $search.date || period !== $search.period) {
+                // eg when using back button the date might be actually already changed in the URL and we do not
+                // want to change the URL again
+                $search.date = date;
+                $search.period = period;
+                $location.search($search);
+            }
+        }
+
+        function parseDate(strDate) {
+            if (strDate === 'today') {
+                return getToday();
+            }
+
+            if (strDate === 'yesterday') {
+                var yesterday = getToday();
+                yesterday.setDate(yesterday.getDate() - 1);
+                return yesterday;
+            }
+
+            return $.datepicker.parseDate('yy-mm-dd', strDate);
+        }
+
+        function getToday() {
+            var date = new Date();
+            date.setHours(0);
+            date.setMinutes(0);
+            date.setSeconds(0);
+            date.setMilliseconds(0);
+            return date;
+        }
+
+        function isValidDate(d) {
+            if (Object.prototype.toString.call(d) !== "[object Date]") {
+                return false;
+            }
+
+            return !isNaN(d.getTime());
+        }
+
+        function formatDate(date) {
+            return $.datepicker.formatDate('yy-mm-dd', date);
+        }
     }
 })();
--- plugins/CoreHome/angularjs/period-selector/period-selector.directive.js
+++ plugins/CoreHome/angularjs/period-selector/period-selector.directive.js
@@ -12,9 +12,9 @@
 (function () {
     angular.module('piwikApp').directive('piwikPeriodSelector', piwikPeriodSelector);
 
-    piwikPeriodSelector.$inject = ['piwik', '$location', 'piwikPeriods'];
+    piwikPeriodSelector.$inject = ['piwik'];
 
-    function piwikPeriodSelector(piwik, $location, piwikPeriods) {
+    function piwikPeriodSelector(piwik) {
         return {
             restrict: 'A',
             scope: {
@@ -26,147 +26,12 @@
             controllerAs: 'periodSelector',
             bindToController: true,
             link: function (scope, element) {
-                // the period & date currently being viewed
-                scope.periodSelector.periodValue = null;
-                scope.periodSelector.dateValue = null;
+                scope.periodSelector.closePeriodSelector = closePeriodSelector;
 
-                scope.periodSelector.selectedPeriod = null;
+                scope.$on('$locationChangeSuccess', scope.periodSelector.updateSelectedValuesFromHash);
 
-                scope.periodSelector.startRangeDate = null;
-                scope.periodSelector.endRangeDate = null;
-
-                scope.periodSelector.getCurrentlyViewingText = getCurrentlyViewingText;
-                scope.periodSelector.changeViewedPeriod = changeViewedPeriod;
-                scope.periodSelector.setPiwikPeriodAndDate = setPiwikPeriodAndDate;
-                scope.periodSelector.onApplyClicked = onApplyClicked;
-
-                scope.$on('$locationChangeSuccess', updateSelectedValuesFromHash);
-
-                updateSelectedValuesFromHash();
-                initTopControls(); // must be called when a top control changes width
-
-                function updateSelectedValuesFromHash() {
-                    var search = $location.search();
-
-                    scope.periodSelector.periodValue = search.period;
-                    scope.periodSelector.selectedPeriod = search.period;
-
-                    if (search.period === 'range') {
-                        var parts = search.date.split(',');
-                        scope.periodSelector.startRangeDate = parts[0];
-                        scope.periodSelector.endRangeDate = parts[1];
-
-                        scope.periodSelector.dateValue = parseDate(parts[0]);
-                    } else {
-                        scope.periodSelector.dateValue = parseDate(search.date);
-
-                        var range = piwikPeriods.parse(search.period, search.date).getDateRange();
-                        scope.periodSelector.startRangeDate = $.datepicker.formatDate('yy-mm-dd', range[0]);
-                        scope.periodSelector.endRangeDate = $.datepicker.formatDate('yy-mm-dd', range[1]);
-                    }
-                }
-
-                function getCurrentlyViewingText() {
-                    var search = $location.search();
-                    return piwikPeriods.parse(search.period, search.date).getPrettyString();
-                }
-
-                function changeViewedPeriod(period) {
-                    // only change period if it's different from what's being shown currently
-                    if (period === scope.periodSelector.periodValue) {
-                        return;
-                    }
-
-                    // can't just change to a range period, w/o setting two new dates
-                    if (period === 'range') {
-                        return;
-                    }
-
-                    setPiwikPeriodAndDate(period, scope.periodSelector.dateValue);
-                }
-
-                function onApplyClicked() {
-                    if (scope.periodSelector.selectedPeriod === 'range') {
-                        var dateFrom = scope.periodSelector.startRangeDate,
-                            dateTo = scope.periodSelector.endRangeDate,
-                            oDateFrom = $.datepicker.parseDate('yy-mm-dd', dateFrom),
-                            oDateTo = $.datepicker.parseDate('yy-mm-dd', dateTo);
-
-                        if (!isValidDate(oDateFrom)
-                            || !isValidDate(oDateTo)
-                            || oDateFrom > oDateTo
-                        ) {
-                            // TODO: use a notification instead?
-                            $('#alert').find('h2').text(_pk_translate('General_InvalidDateRange'));
-                            piwikHelper.modalConfirm('#alert', {});
-                            return;
-                        }
-
-                        propagateNewUrlParams(dateFrom + ',' + dateTo, 'range');
-                        return;
-                    }
-
-                    setPiwikPeriodAndDate(scope.periodSelector.selectedPeriod, scope.periodSelector.dateValue);
-
-                }
-
-                function setPiwikPeriodAndDate(period, date) {
-                    period = period || scope.periodSelector.periodValue;
-                    date = date || scope.periodSelector.dateValue;
-
-                    piwik.period = period;
-                    piwik.currentDateString = $.datepicker.formatDate('yy-mm-dd', date);
-
-                    var dateRange = piwikPeriods.parse(period, piwik.currentDateString).getDateRange();
-                    piwik.startDateString = $.datepicker.formatDate('yy-mm-dd', dateRange[0]);
-                    piwik.endDateString = $.datepicker.formatDate('yy-mm-dd', dateRange[1]);
-
-                    propagateNewUrlParams(piwik.currentDateString, scope.periodSelector.selectedPeriod);
-                    initTopControls();
-                }
-
-                function propagateNewUrlParams(date, period) {
+                function closePeriodSelector() {
                     element.find('#periodString').removeClass('expanded');
-
-                    var $search = $location.search();
-                    if (date !== $search.date || period !== $search.period) {
-                        // eg when using back button the date might be actually already changed in the URL and we do not
-                        // want to change the URL again
-                        $search.date = date;
-                        $search.period = period;
-                        $location.search($search);
-                    }
-                }
-
-                function parseDate(strDate) {
-                    if (strDate === 'today') {
-                        return getToday();
-                    }
-
-                    if (strDate === 'yesterday') {
-                        var yesterday = getToday();
-                        yesterday.setDate(yesterday.getDate() - 1);
-                        return yesterday;
-                    }
-
-                    return $.datepicker.parseDate('yy-mm-dd', strDate);
-                }
-
-                function getToday() {
-                    var date = new Date();
-                    date.setHours(0);
-                    date.setMinutes(0);
-                    date.setSeconds(0);
-                    date.setMilliseconds(0);
-                    return date;
-                }
-
-                function isValidDate(d) {
-                    if (Object.prototype.toString.call(d) !== "[object Date]") {
-                        return false;
-                    }
-
-                    return !isNaN(d.getTime());
                 }
             }
         };
