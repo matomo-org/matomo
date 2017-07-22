65b7e155fb0809d516f2b96684d15fa864658441
Jason Miele <jball@jball.org>
2017-07-22 19:30:01 -0400

    Update script created for 3.1.0 to Fix #11596

---
+++ core/Updates/3.1.0.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+namespace Piwik\Updates;
+
+use Piwik\Common;
+use Piwik\Db;
+use Piwik\Updater;
+use Piwik\Updater\Migration;
+use Piwik\Updater\Migration\Factory as MigrationFactory;
+use Piwik\Updates;
+
+/**
+ * Update for version 3.1.0.
+ */
+class Updates_3_1_0 extends Updates
+{
+    /**
+     * @var MigrationFactory
+     */
+    private $migration;
+
+    private $pluginSettingsTable = 'plugin_setting';
+    private $siteSettingsTable = 'site_setting';
+	private $logProfilingTable = 'log_profiling';
+
+    public function __construct(MigrationFactory $factory)
+    {
+        $this->migration = $factory;
+    }
+
+    /**
+     * Here you can define one or multiple SQL statements that should be executed during the update.
+     * @param Updater $updater
+     * @return Migration[]
+     */
+    public function getMigrations(Updater $updater)
+    {
+        $migrations = [];
+        $migrations = $this->getPluginSettingsMigrations($migrations);
+        $migrations = $this->getSiteSettingsMigrations($migrations);
+        $migrations = $this->getLogProfilingMigrations($migrations);
+
+        return $migrations;
+    }
+
+    public function doUpdate(Updater $updater)
+    {
+        $updater->executeMigrations(__FILE__, $this->getMigrations($updater));
+    }
+
+    /**
+     * @param Migration[] $queries
+     * @return Migration[]
+     */
+    private function getPluginSettingsMigrations($queries)
+    {
+		$queries[] = $this->migration->db->addColumn($this->$pluginSettingsTable, 'plugin_setting_id', 'BIGINT UNSIGNED NOT NULL AUTO_INCREMENT', 'user_login');
+		$queries[] = $this->migration->db->addPrimaryKey($this->$pluginSettingsTable, 'plugin_setting_id');
+
+        return $queries;
+    }
+
+    /**
+     * @param Migration[] $queries
+     * @return Migration[]
+     */
+    private function getSiteSettingsMigrations($queries)
+    {
+		$queries[] = $this->migration->db->addColumn($this->siteSettingsTable, 'site_setting_id', 'BIGINT UNSIGNED NOT NULL AUTO_INCREMENT', 'setting_value');
+		$queries[] = $this->migration->db->addPrimaryKey($this->siteSettingsTable, 'site_setting_id');
+
+        return $queries;
+    }
+
+    /**
+     * @param Migration[] $queries
+     * @return Migration[]
+     */
+    private function getLogProfilingMigrations($queries)
+    {
+		$queries[] = $this->migration->db->addColumn($this->$logProfilingTable, 'log_profiling_id', 'BIGINT UNSIGNED NOT NULL AUTO_INCREMENT', 'sum_time_ms');
+		$queries[] = $this->migration->db->addPrimaryKey($this->$logProfilingTable, 'log_profiling_id');
+
+        return $queries;
+    }
+}
