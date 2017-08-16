d61d175c43c66a7baafbea3fb7e4a0cfcc4c89c6
Matthieu Aubry <mattab@users.noreply.github.com>
2017-08-16 17:03:24 +1200

    3.0.5-b2

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Piwik version.
      * @var string
      */
-    const VERSION = '3.0.5-b1';
+    const VERSION = '3.0.5-b2';
 
     public function isStableVersion($version)
     {
