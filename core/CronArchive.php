8a23b7e718ed107abeba1efbfbfbdd2d6b9ab58e
Thomas Steur <tsteur@users.noreply.github.com>
2017-09-04 17:02:11 +1200

    Allow different reports pre-archiving frequency for each period (#11979)
    * Allow different reports pre-archiving frequency for each period
    
    * Add new config settings for archive frequency
    
    * added default config for range dates
    
    * print changed archive periods
    
    * minor change to log output
    
    * Clarify wording, processed != requested.
    
    * Fix integration test

--- core/CronArchive.php
+++ core/CronArchive.php
@@ -1279,8 +1279,18 @@ class CronArchive
         }
         $this->logger->info("- Reports for today will be processed at most every " . $this->todayArchiveTimeToLive
             . " seconds. You can change this value in Piwik UI > Settings > General Settings.");
-        $this->logger->info("- Reports for the current week/month/year will be refreshed at most every "
+
+        $this->logger->info("- Reports for the current week/month/year will be requested at most every "
             . $this->processPeriodsMaximumEverySeconds . " seconds.");
+
+        foreach (array('week', 'month', 'year', 'range') as $period) {
+            $ttl = Rules::getPeriodArchiveTimeToLiveDefault($period);
+
+            if (!empty($ttl) && $ttl !== $this->todayArchiveTimeToLive) {
+                $this->logger->info("- Reports for the current $period will be processed at most every " . $ttl
+                    . " seconds. You can change this value in config/config.ini.php by editing 'time_before_" . $period . "_archive_considered_outdated' in the '[General]' section.");
+            }
+        }
 
         // Try and not request older data we know is already archived
         if ($this->lastSuccessRunTimestamp !== false) {
