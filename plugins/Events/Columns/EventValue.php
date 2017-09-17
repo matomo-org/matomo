a320b4d6899ee97b6b16bb037278c8a938b18e18
Thomas Steur <tsteur@users.noreply.github.com>
2017-09-17 11:30:22 +0000

    add event value

--- plugins/Events/Columns/EventValue.php
+++ plugins/Events/Columns/EventValue.php
@@ -6,7 +6,7 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  *
  */
-namespace Piwik\Plugins\Actions\Columns;
+namespace Piwik\Plugins\Events\Columns;
 
 use Piwik\Columns\DimensionMetricFactory;
 use Piwik\Columns\Discriminator;
@@ -17,16 +17,16 @@ use Piwik\Plugin\ComputedMetric;
 use Piwik\Plugin\Dimension\ActionDimension;
 use Piwik\Tracker\Action;
 
-class PageGenerationTime extends ActionDimension
+class EventValue extends ActionDimension
 {
-    protected $nameSingular = 'General_ColumnPageGenerationTime';
+    protected $nameSingular = 'Events_EventValue';
     protected $columnName = 'custom_float';
-    protected $category = 'General_Actions';
-    protected $type = self::TYPE_DURATION_MS;
+    protected $category = 'Events_Events';
+    protected $type = self::TYPE_FLOAT;
 
     public function getDbDiscriminator()
     {
-        return new Discriminator('log_action', 'type', Action::TYPE_PAGE_URL);
+        return new Discriminator('log_action', 'type', Action::TYPE_EVENT);
     }
 
     public function configureMetrics(MetricsList $metricsList, DimensionMetricFactory $dimensionMetricFactory)
@@ -35,16 +35,22 @@ class PageGenerationTime extends ActionDimension
         $metricsList->addMetric($metric1);
 
         $metric2 = $dimensionMetricFactory->createMetric(ArchivedMetric::AGGREGATION_MAX);
+        $metric2->setDocumentation(Piwik::translate('Events_MaxValueDocumentation'));
         $metricsList->addMetric($metric2);
 
+        $metric4 = $dimensionMetricFactory->createMetric(ArchivedMetric::AGGREGATION_MIN);
+        $metric4->setDocumentation(Piwik::translate('Events_MinValueDocumentation'));
+        $metricsList->addMetric($metric4);
+
         $metric3 = $dimensionMetricFactory->createMetric(ArchivedMetric::AGGREGATION_COUNT_WITH_NUMERIC_VALUE);
-        $metric3->setName('pageviews_with_generation_time');
-        $metric3->setTranslatedName(Piwik::translate('General_ColumnViewsWithGenerationTime'));
+        $metric3->setName('events_with_event_value');
+        $metric3->setTranslatedName(Piwik::translate('Events_EventsWithValue'));
+        $metric3->setDocumentation(Piwik::translate('Events_EventsWithValueDocumentation'));
         $metricsList->addMetric($metric3);
 
         $metric = $dimensionMetricFactory->createComputedMetric($metric1->getName(), $metric3->getName(), ComputedMetric::AGGREGATION_AVG);
-        $metric->setName('avg_page_generation_time');
-        $metric->setTranslatedName(Piwik::translate('General_ColumnAverageGenerationTime'));
+        $metric->setName('avg_event_value');
+        $metric->setTranslatedName(Piwik::translate('Events_AvgValue'));
         $metricsList->addMetric($metric);
     }
 }
