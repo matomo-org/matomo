f9ec83f35ee3aa5563a4a58333b143aeb4e3ed84
sgiehl <stefan@piwik.org>
2017-11-15 22:42:50 +0100

    Hide SEO rankings widget if internet is disabled in config

--- plugins/SEO/SEO.php
+++ plugins/SEO/SEO.php
@@ -7,9 +7,27 @@
  *
  */
 namespace Piwik\Plugins\SEO;
+use Piwik\SettingsPiwik;
+use Piwik\Widget\WidgetsList;
 
 /**
  */
 class SEO extends \Piwik\Plugin
 {
+    public function registerEvents()
+    {
+        return [
+            'Widget.filterWidgets' => 'filterWidgets'
+        ];
+    }
+
+    /**
+     * @param WidgetsList $list
+     */
+    public function filterWidgets($list)
+    {
+        if (!SettingsPiwik::isInternetEnabled()) {
+            $list->remove('SEO', 'SEO_SeoRankings');
+        }
+    }
 }
