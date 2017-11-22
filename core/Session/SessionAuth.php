c46388242f3dc16b92bf9939181b3c4ca80b64c4
diosmosis <diosmosis@users.noreply.github.com>
2017-11-22 15:56:47 -0800

    Clarify session destruction/invalidation logic in SessionAuth.

--- core/Session/SessionAuth.php
+++ core/Session/SessionAuth.php
@@ -71,10 +71,10 @@ class SessionAuth implements Auth
 
     public function authenticate()
     {
-        $sessionId = new SessionFingerprint();
+        $sessionFingerprint = new SessionFingerprint();
         $userModel = new UsersModel();
 
-        $userForSession = $sessionId->getUser();
+        $userForSession = $sessionFingerprint->getUser();
         if (empty($userForSession)) {
             return $this->makeAuthFailure();
         }
@@ -84,37 +84,24 @@ class SessionAuth implements Auth
             return $this->makeAuthFailure();
         }
 
-        if (!$sessionId->isMatchingCurrentRequest()) {
-            // this user should be using a different session, so generate a new ID
-            // NOTE: Zend_Session cannot be used since it will destroy the old
-            // session.
-            if ($this->shouldDestroySession) {
-                session_regenerate_id();
-            }
-            $sessionId->clear();
-
+        if (!$sessionFingerprint->isMatchingCurrentRequest()) {
+            $this->initNewBlankSession($sessionFingerprint);
             return $this->makeAuthFailure();
         }
 
         $tsPasswordModified = !empty($user['ts_password_modified']) ? $user['ts_password_modified'] : 0;
-        if ($this->isSessionStartedBeforePasswordChange($sessionId, $tsPasswordModified)) {
-            // Note: can't use Session::destroy() since Zend prohibits starting a new session
-            // after session_destroy() is called.
-            $sessionId->clear();
-            if ($this->shouldDestroySession) {
-                Session::regenerateId();
-            }
-
+        if ($this->isSessionStartedBeforePasswordChange($sessionFingerprint, $tsPasswordModified)) {
+            $this->destroyCurrentSession($sessionFingerprint);
             return $this->makeAuthFailure();
         }
 
         return $this->makeAuthSuccess($user);
     }
 
-    private function isSessionStartedBeforePasswordChange(SessionFingerprint $sessionId, $tsPasswordModified)
+    private function isSessionStartedBeforePasswordChange(SessionFingerprint $sessionFingerprint, $tsPasswordModified)
     {
         // if the session start time doesn't exist for some reason, log the user out
-        $sessionStartTime = $sessionId->getSessionStartTime();
+        $sessionStartTime = $sessionFingerprint->getSessionStartTime();
         if (empty($sessionStartTime)) {
             return true;
         }
@@ -135,5 +122,32 @@ class SessionAuth implements Auth
         $code = $isSuperUser ? AuthResult::SUCCESS_SUPERUSER_AUTH_CODE : AuthResult::SUCCESS;
 
         return new AuthResult($code, $user['login'], $user['token_auth']);
+    }
+
+    private function initNewBlankSession(SessionFingerprint $sessionFingerprint)
+    {
+        // this user should be using a different session, so generate a new ID
+        // NOTE: Zend_Session cannot be used since it will destroy the old
+        // session.
+        if ($this->shouldDestroySession) {
+            session_regenerate_id();
+        }
+
+        // regenerating the ID will create a new session w/ a new ID, but will
+        // copy over the existing session data. we want the new session for the
+        // unauthorized user to be different, so we clear the session fingerprint.
+        $sessionFingerprint->clear();
+    }
+
+    private function destroyCurrentSession(SessionFingerprint $sessionFingerprint)
+    {
+        // Note: can't use Session::destroy() since Zend prohibits starting a new session
+        // after session_destroy() is called. Instead we clear the session fingerprint for
+        // the existing session and generate a new session. Both the old session &
+        // new session should have no stored data.
+        $sessionFingerprint->clear();
+        if ($this->shouldDestroySession) {
+            Session::regenerateId();
+        }
     }
 }
