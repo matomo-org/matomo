3d550b9180c25c6e4966b48bdd1c70c499c7ef52
sgiehl <stefan@piwik.org>
2017-12-16 15:49:45 +0100

    deprecates historyService alogn with some broadcast methods

--- plugins/CoreHome/javascripts/broadcast.js
+++ plugins/CoreHome/javascripts/broadcast.js
@@ -52,6 +52,9 @@ var broadcast = {
 
     /**
      * Initializes broadcast object
+     *
+     * @deprecated in 3.2.2, will be removed in Piwik 4
+     *
      * @return {void}
      */
     init: function (noLoadingMessage) {
@@ -77,6 +80,8 @@ var broadcast = {
      * 3. after pushing "Go Back" button of a browser
      *
      * * Note: the method is manipulated in Overlay/javascripts/Piwik_Overlay.js - keep this in mind when making changes.
+     *
+     * @deprecated since 3.2.2, will be removed in Piwik 4
      *
      * @param {string}  hash to load page with
      * @return {void}
