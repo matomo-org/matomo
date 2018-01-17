9b26427c55f89f9295b7b58f52fafbaf854f7cf7
danika chhour <d.chhour28@gmail.com>
2018-01-17 13:54:28 +1300

    Removed totals

--- plugins/Events/Events.php
+++ plugins/Events/Events.php
@@ -45,8 +45,8 @@ class Events extends \Piwik\Plugin
     public function getMetricDocumentation()
     {
         $documentation = array(
-            'nb_events'            => 'Events_TotalEventsDocumentation',
-            'sum_event_value'      => 'Events_TotalValueDocumentation',
+            'nb_events'            => 'Events_EventsDocumentation',
+            'sum_event_value'      => 'Events_EventValueDocumentation',
             'min_event_value'      => 'Events_MinValueDocumentation',
             'max_event_value'      => 'Events_MaxValueDocumentation',
             'avg_event_value'      => 'Events_AvgValueDocumentation',
@@ -59,8 +59,8 @@ class Events extends \Piwik\Plugin
     public function getMetricTranslations()
     {
         $metrics = array(
-            'nb_events'            => 'Events_TotalEvents',
-            'sum_event_value'      => 'Events_TotalValue',
+            'nb_events'            => 'Events_Events',
+            'sum_event_value'      => 'Events_EventValue',
             'min_event_value'      => 'Events_MinValue',
             'max_event_value'      => 'Events_MaxValue',
             'avg_event_value'      => 'Events_AvgValue',
