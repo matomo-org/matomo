bcb2d283ebc3f637c8c0d95c4b3e4ef9b759c66f
Thomas Steur <tsteur@users.noreply.github.com>
2018-01-30 13:40:57 +1300

    Add more date segments

---
+++ plugins/CoreHome/Columns/VisitLastActionWeekOfYear.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\Plugins\CoreHome\Columns;
+
+use Piwik\Columns\DimensionMetricFactory;
+use Piwik\Columns\MetricsList;
+use Piwik\Plugin\Dimension\VisitDimension;
+use Piwik\Metrics\Formatter;
+
+class VisitLastActionWeekOfYear extends VisitDimension
+{
+    protected $columnName = 'visit_last_action_time';
+    protected $type = self::TYPE_DATETIME;
+    protected $segmentName = 'visitEndServerWeekOfYear';
+    protected $nameSingular = 'VisitTime_ColumnVisitEndServerWeekOfYear';
+    protected $sqlSegment = 'WEEKOFYEAR(log_visit.visit_last_action_time)';
+    protected $acceptValues = '1, 2, 3, 4, ..., 51, 52, 53';
+
+    public function __construct()
+    {
+        $this->suggestedValuesCallback = function ($idSite, $maxValuesToReturn) {
+            return range(1, min(53, $maxValuesToReturn));
+        };
+    }
+
+    public function configureMetrics(MetricsList $metricsList, DimensionMetricFactory $dimensionMetricFactory)
+    {
+        // no metrics for this dimension
+    }
+
+    public function formatValue($value, $idSite, Formatter $formatter)
+    {
+        return $value;
+    }
+}
