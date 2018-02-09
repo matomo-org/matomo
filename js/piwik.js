29b0a1e9bd1aefb84db1c1de6dbcec94475027a9
mattab <matthieu.aubry@gmail.com>
2018-02-09 20:42:02 +1300

    Fix bug

--- js/piwik.js
+++ js/piwik.js
@@ -7084,9 +7084,10 @@ if (typeof window.Piwik !== 'object') {
                             apply(paq[iterator]);
                             delete paq[iterator];
 
-                            var isMethodCallableMultipleTimes = ["addTracker"];
+                            var isMethodCallableMultipleTimes = ["addTracker"],
+                                shouldMethodBeCalledOnceOnly = isMethodCallableMultipleTimes.indexOf(methodName) === -1;
                             if (appliedMethods[methodName] > 1
-                                && isMethodCallableMultipleTimes.indexOf(methodName) !== -1) {
+                                && shouldMethodBeCalledOnceOnly) {
                                 logConsoleError('The method ' + methodName + ' is registered more than once in "_paq" variable. Only the last call has an effect. Please have a look at the multiple Piwik trackers documentation: https://developer.piwik.org/guides/tracking-javascript-guide#multiple-piwik-trackers');
                             }
 
