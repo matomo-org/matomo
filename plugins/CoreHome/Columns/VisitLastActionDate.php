3b8a0990784e16ed149000b527a0841523799017
Thomas Steur <tsteur@users.noreply.github.com>
2018-02-19 20:55:20 +1300

    Add more date segments (#12521)
    * Add more date segments
    
    * fix segment name
    
    * fix compatibility with php 5.6
    
    * fix some tests
    
    * make sure to not get random failures
    
    * fix more tests
    
    * fix suggested values for year
    
    * Fix dimension translation was using wrong key
    
    * fix some tests
    
    * fix test

---
+++ plugins/CoreHome/Columns/VisitLastActionDate.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\Plugins\CoreHome\Columns;
+
+use Piwik\Columns\DimensionMetricFactory;
+use Piwik\Columns\MetricsList;
+use Piwik\Date;
+use Piwik\Plugin\Dimension\VisitDimension;
+use Piwik\Metrics\Formatter;
+
+class VisitLastActionDate extends VisitDimension
+{
+    protected $columnName = 'visit_last_action_time';
+    protected $type = self::TYPE_DATETIME;
+    protected $segmentName = 'visitEndServerDate';
+    protected $nameSingular = 'VisitTime_ColumnVisitEndServerDate';
+    protected $sqlSegment = 'DATE(log_visit.visit_last_action_time)';
+    protected $acceptValues = '2018-12-31, 2018-03-20, ...';
+
+    public function __construct()
+    {
+        $this->suggestedValuesCallback = function ($idSite, $maxValuesToReturn) {
+            if (defined('PIWIK_TEST_MODE') && PIWIK_TEST_MODE) {
+                $date = Date::factory('2018-02-02');
+            } else {
+                $date = Date::now();
+            }
+            $return = array($date->toString());
+            for ($i = 0; $i < $maxValuesToReturn; $i++) {
+                $date = $date->subDay(1);
+                $return[] = $date->toString();
+            }
+            return $return;
+        };
+    }
+
+    public function configureMetrics(MetricsList $metricsList, DimensionMetricFactory $dimensionMetricFactory)
+    {
+        // no metrics for this dimension
+    }
+
+    public function formatValue($value, $idSite, Formatter $formatter)
+    {
+        return $value;
+    }
+}
