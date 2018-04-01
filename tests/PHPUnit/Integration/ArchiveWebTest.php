1bfec923ec9593f76b97f69610b9c5de45ae3673
sgiehl <stefan@piwik.org>
2018-04-01 13:37:54 +0200

    move ArchiveWebTest to Integration tests

--- tests/PHPUnit/Integration/ArchiveWebTest.php
+++ tests/PHPUnit/Integration/ArchiveWebTest.php
@@ -5,7 +5,7 @@
  * @link    http://piwik.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
-namespace Piwik\Tests\System;
+namespace Piwik\Tests\Integration;
 
 use Piwik\Option;
 use Piwik\Http;
