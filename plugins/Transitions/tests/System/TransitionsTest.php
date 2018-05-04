4de5408ac20729f04638682fdd35c660740b05ce
sgiehl <stefan@piwik.org>
2018-05-04 17:57:36 +0200

    Move Transitions system test to plugin

--- plugins/Transitions/tests/System/TransitionsTest.php
+++ plugins/Transitions/tests/System/TransitionsTest.php
@@ -5,7 +5,7 @@
  * @link    http://piwik.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
-namespace Piwik\Tests\System;
+namespace Piwik\Plugins\Transitions\tests\System;
 
 use Piwik\Tests\Framework\TestCase\SystemTestCase;
 use Piwik\Tests\Fixtures\SomeVisitsManyPageviewsWithTransitions;
