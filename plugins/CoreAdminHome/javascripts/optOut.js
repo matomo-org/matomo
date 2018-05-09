a325ab139f2affbc009e67d6f0ecf68ccb52b5e4
Michael Pietsch <michael@mi-pietsch.de>
2018-05-09 17:16:09 +0200

    on opt-out page move js to separate file to fix inline-script (CSP)

---
+++ plugins/CoreAdminHome/javascripts/optOut.js
@@ -0,0 +1,21 @@
+function submitForm(e, form) {
+    if (e.preventDefault) { // IE8 and below do not support preventDefault
+        e.preventDefault();
+    }
+
+    var now = Date.now ? Date.now() : (+(new Date())), // Date.now does not exist in < IE8
+    newWindow = window.open(form.action + '&time=' + now);
+
+    setInterval(function () {
+        if (newWindow.closed) {
+            window.location.reload();
+        }
+    }, 1000);
+    return false;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var trackVisitsCheckbox = document.getElementById('trackVisits');
+    if (typeof trackVisitsCheckbox === "undefined") trackVisitsCheckbox.addEventListener('click', function(event) { submitForm(event, this.form); });
+});
+
