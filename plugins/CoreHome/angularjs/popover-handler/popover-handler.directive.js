e006d63d4fef91d9e27df158eb7a082b02b23a0f
diosmosis <diosmosis@users.noreply.github.com>
2018-05-24 14:57:10 -0700

    Reset popover stack as precaution.

--- plugins/CoreHome/angularjs/popover-handler/popover-handler.directive.js
+++ plugins/CoreHome/angularjs/popover-handler/popover-handler.directive.js
@@ -57,6 +57,10 @@
                     var popoverParam = $location.search().popover;
                     if (popoverParam) {
                         open(popoverParam);
+                    } else {
+                        // the URL should only be set to an empty popover if there are no popovers in the stack.
+                        // to avoid avoid any strange inconsistent states, we reset the popover stack here.
+                        broadcast.popoverParamStack = [];
                     }
                 }
 
