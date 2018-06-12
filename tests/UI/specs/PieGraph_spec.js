205bc62e0942c32eb09c4cc1913246dd7806a869
diosmosis <diosmosis@users.noreply.github.com>
2018-06-12 00:50:10 -0700

    Convert PeriodSelector, PieGraph & PivotByDimension UI tests.

--- tests/UI/specs/PieGraph_spec.js
+++ tests/UI/specs/PieGraph_spec.js
@@ -8,32 +8,33 @@
  */
 
 describe("PieGraph", function () {
-    this.timeout(0);
-
-    var url = "?module=Widgetize&action=iframe&moduleToWidgetize=Referrers&idSite=1&period=year&date=2012-08-09&"
-            + "actionToWidgetize=getKeywords&viewDataTable=graphPie&isFooterExpandedInDashboard=1";
+    const url = "?module=Widgetize&action=iframe&moduleToWidgetize=Referrers&idSite=1&period=year&date=2012-08-09&"
+              + "actionToWidgetize=getKeywords&viewDataTable=graphPie&isFooterExpandedInDashboard=1";
 
     it("should load correctly", async function () {
-        expect.screenshot("load").to.be.capture(function (page) {
-            page.goto(url);
-        }, done);
+        await page.goto(url);
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('load');
     });
 
     it("should show tooltip on hover", async function () {
-        expect.screenshot("pie_segment_tooltip").to.be.capture(function (page) {
-            page.mouseMove('.piwik-graph');
-        }, done);
+        await page.hover('.piwik-graph');
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('pie_segment_tooltip');
     });
 
     it("should display the metric picker on hover of metric picker icon", async function () {
-        expect.screenshot('metric_picker_shown').to.be.capture(function (page) {
-            page.mouseMove('.jqplot-seriespicker');
-        }, done);
+        await page.hover('.jqplot-seriespicker');
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('metric_picker_shown');
     });
 
     it("should change displayed metric when another metric picked", async function () {
-        expect.screenshot('other_metric').to.be.capture(function (page) {
-            page.click('.jqplot-seriespicker-popover input:not(:checked):first + label');
-        }, done);
+        const element = await page.jQuery('.jqplot-seriespicker-popover input:not(:checked):first + label');
+        await element.click();
+
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('other_metric');
     });
 });
--- tests/UI/specs/PivotByDimension_spec.js
+++ tests/UI/specs/PivotByDimension_spec.js
@@ -8,42 +8,51 @@
  */
 
 describe("PivotByDimension", function () {
-    this.timeout(0);
-
     var eventsUrl = "?module=CoreHome&action=index&idSite=1&period=year&date=2012-08-09#?idSite=1&period=year&date=2012-08-09&category=General_Actions&subcategory=Events_Events",
         actionsUrl = "?module=CoreHome&action=index&idSite=1&period=year&date=2012-08-09#?idSite=1&period=year&date=2012-08-09&category=General_Actions&subcategory=General_Pages",
         cvarsUrl = "?module=CoreHome&action=index&idSite=1&period=year&date=2012-08-09#?idSite=1&period=year&date=2012-08-09&category=General_Visitors&subcategory=CustomVariables_CustomVariables"
         ;
 
-    function showDataTableFooter(page)
-    {
-        page.mouseMove('.dataTableFeatures');
+    function showDataTableFooter(page) {
+        return page.hover('.dataTableFeatures');
     }
 
     it("should pivot a report correctly when the pivot cog option is selected", async function () {
-        expect.screenshot('pivoted').to.be.captureSelector('.dataTable,.expandDataTableFooterDrawer', function (page) {
-            page.goto(eventsUrl);
-            page.click('.dimension:contains(Event Names)');
-            showDataTableFooter(page);
-            page.evaluate(function(){
-                $('.dropdownConfigureIcon').click();
-                $('.dataTablePivotBySubtable').click();
-            }, 2000);
-            page.mouseMove({x: -15, y: -15}); // make sure nothing is highlighted
-        }, done);
+        await page.goto(eventsUrl);
+
+        let element = await page.jQuery('.dimension:contains(Event Names)');
+        await element.click();
+        await page.waitForNetworkIdle();
+
+        await showDataTableFooter(page);
+        await page.evaluate(function(){
+            $('.dropdownConfigureIcon').click();
+            $('.dataTablePivotBySubtable').click();
+        });
+        await page.waitForNetworkIdle();
+
+        await page.mouse.move(-150, -150); // make sure nothing is highlighted
+
+        element = await page.$('.theWidgetContent');
+        expect(await element.screenshot()).to.matchImage('pivoted');
     });
 
     it("should not display the pivot option on actions reports", async function () {
-        expect.page(actionsUrl).not.contains('.dataTablePivotBySubtable', function () {}, done);
+        await page.goto(actionsUrl);
+
+        const element = await page.$('.dataTablePivotBySubtable');
+        expect(element).to.be.not.ok;
     });
 
     it("should display the pivot option on reports that set a custom columns_to_display", async function () {
-        expect.screenshot('pivoted_columns_report').to.be.captureSelector('.dataTable,.expandDataTableFooterDrawer', function (page) {
-            page.goto(cvarsUrl);
-            showDataTableFooter(page);
-            page.click('.dropdownConfigureIcon');
-            page.click('.dataTablePivotBySubtable');
-            page.mouseMove({x: -15, y: -15}); // make sure nothing is highlighted
-        }, done);
+        await page.goto(cvarsUrl);
+        await showDataTableFooter(page);
+        await page.click('.dropdownConfigureIcon');
+        await page.click('.dataTablePivotBySubtable');
+        await page.waitForNetworkIdle();
+
+        await page.mouse.move(-15, -15); // make sure nothing is highlighted
+
+        expect(await page.screenshotSelector('.dataTable,.expandDataTableFooterDrawer')).to.matchImage('pivoted_columns_report');
     });
 });
