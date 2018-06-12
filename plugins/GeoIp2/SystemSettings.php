d2fe5a42c53df4fa92ca668f486f6359e5930d55
Stefan Giehl <stefan@piwik.org>
2018-06-12 19:42:20 +0200

    Hide GeoIP2 server module settings by default (#13053)
    * Hide GeoIP2 server module settings by default
    
    * upates tests

--- plugins/GeoIp2/SystemSettings.php
+++ plugins/GeoIp2/SystemSettings.php
@@ -22,11 +22,20 @@ class SystemSettings extends \Piwik\Settings\Plugin\SystemSettings
     /** @var Setting[] */
     public $geoIp2variables;
 
+    /** @var Setting */
+    public $useCustomVars;
+
     protected function init()
     {
         $this->title = Piwik::translate('GeoIp2_ServerBasedVariablesConfiguration');
 
         $geoIpAdminEnabled = UserCountry::isGeoLocationAdminEnabled();
+
+        $this->useCustomVars = $this->makeSetting('geoip2usecustom', false, FieldConfig::TYPE_BOOL, function (FieldConfig $field) {
+            $field->title = Piwik::translate('GeoIp2_ShowCustomServerVariablesConfig');
+            $field->uiControl = FieldConfig::UI_CONTROL_CHECKBOX;
+        });
+        $this->useCustomVars->setIsWritableByCurrentUser($geoIpAdminEnabled);
 
         foreach (ServerModule::$defaultGeoIpServerVars as $name => $value) {
             $this->geoIp2variables[$name] = $this->createGeoIp2ServerVarSetting($name, $value);
@@ -39,6 +48,7 @@ class SystemSettings extends \Piwik\Settings\Plugin\SystemSettings
         return $this->makeSetting('geoip2var_'.$name, $default = $defaultValue, FieldConfig::TYPE_STRING, function (FieldConfig $field) use ($name) {
             $field->title = Piwik::translate('GeoIp2_ServerVariableFor', '<strong>' . str_replace('_', ' ', $name) . '</strong>');
             $field->uiControl = FieldConfig::UI_CONTROL_TEXT;
+            $field->condition = 'geoip2usecustom==1';
         });
     }
 }
