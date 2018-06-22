368dcc4bc9fa45097fcc6c3e4775a12620a5076c
diosmosis <diosmosis@users.noreply.github.com>
2018-06-22 01:08:13 -0700

    Initial working version of single metric view.

--- plugins/CoreVisualizations/Widgets/SingleMetricView.php
+++ plugins/CoreVisualizations/Widgets/SingleMetricView.php
@@ -9,8 +9,11 @@
 
 namespace Piwik\Plugins\CoreVisualizations\Widgets;
 
-
 use Piwik\Common;
+use Piwik\Period\Range;
+use Piwik\Piwik;
+use Piwik\Plugin\ReportsProvider;
+use Piwik\Site;
 use Piwik\Widget\WidgetConfig;
 
 class SingleMetricView extends \Piwik\Widget\Widget
@@ -19,18 +22,39 @@ class SingleMetricView extends \Piwik\Widget\Widget
     {
         parent::configure($config);
 
-        $config->addParameters([
-            'column' => Common::getRequestVar('column', 'nb_visits', 'string'),
-        ]);
-        $config->setCategoryId('Generic'); // TODO: translate
-        $config->setName('Single Metric View'); // TODO: translate
+        $column = Common::getRequestVar('column', 'nb_visits', 'string');
+
+        $translations = self::getApiGetMetricTranslations();
+        $columnTranslation = isset($translations[$column]) ? $translations[$column] : $column;
+
+        $config->addParameters(['column' => $column]);
+        $config->setCategoryId(Piwik::translate('General_General'));
+        $config->setName($columnTranslation);
         $config->setIsWidgetizable();
         $config->setIsReusable();
     }
 
     public function render()
     {
-        $metricName = Common::getRequestVar('column', $default = null, 'string');
-        return "<piwik-single-metric-view metric-name=\"$metricName\" />";
+        $metricName = Common::sanitizeInputValue(Common::getRequestVar('column', $default = null, 'string'));
+
+        $metricTranslations = json_encode(self::getApiGetMetricTranslations());
+        $metricTranslations = Common::sanitizeInputValue($metricTranslations);
+
+        $period = Common::getRequestVar('period');
+        $endDate = Common::getRequestVar('date', 'yesterday', 'string');
+
+        $idSite = Common::getRequestVar('idSite');
+
+        $sparklineRange = Range::getRelativeToEndDate($period, 'last30', $endDate, new Site($idSite));
+        $sparklineRange = Common::sanitizeInputValue($sparklineRange);
+
+        return "<piwik-single-metric-view metric=\"'$metricName'\" metric-translations=\"$metricTranslations\" sparkline-range=\"'$sparklineRange'\" />";
+    }
+
+    private static function getApiGetMetricTranslations()
+    {
+        $report = ReportsProvider::factory('API', 'get');
+        return $report->getMetrics();
     }
 }
--- plugins/CoreVisualizations/angularjs/single-metric-view/single-metric-view.component.js
+++ plugins/CoreVisualizations/angularjs/single-metric-view/single-metric-view.component.js
@@ -13,22 +13,63 @@
     angular.module('piwikApp').component('piwikSingleMetricView', {
         templateUrl: 'plugins/CoreVisualizations/angularjs/single-metric-view/single-metric-view.component.html?cb=' + piwik.cacheBuster,
         bindings: {
-            // showAllSitesItem: '<'
+            metric: '<',
+            metricTranslations: '<',
+            sparklineRange: '<'
         },
         controller: SingleMetricViewController
     });
 
-    SingleMetricViewController.$inject = [];
+    SingleMetricViewController.$inject = ['piwik', 'piwikApi', '$element', '$httpParamSerializer'];
 
-    function SingleMetricViewController() {
-        // remember to keep controller very simple. Create a service/factory (model) if needed
-
+    function SingleMetricViewController(piwik, piwikApi, $element, $httpParamSerializer) {
         var vm = this;
-        vm.myProperty  = 'component';
-        vm.doSomething = doSomething;
+        vm.metricValue = null;
+        vm.$onChanges = $onChanges;
+        vm.getSparklineUrl = getSparklineUrl;
 
-        function doSomething() {
+        function $onChanges(changes) {
+            setWidgetTitle();
 
+            if (vm.metric === changes.metric) {
+                return;
+            }
+
+            // done manually due to 'random' query param. since it changes the URL on each digest, depending on angular
+            // results in an infinite digest
+            updateSparklineSrc();
+
+            piwikApi.fetch({
+                method: 'API.get',
+            }).then(function (response) {
+                vm.metricValue = response[vm.metric];
+            });
+        }
+
+        function setWidgetTitle() {
+            var translation = vm.metricTranslations[vm.metric];
+            $element.closest('[widgetId').find('.widgetTop > .widgetName > span').text(translation);
+        }
+
+        function updateSparklineSrc() {
+            $element.find('img').attr('src', getSparklineUrl());
+        }
+
+        function getSparklineUrl() {
+            var urlParams = piwikApi.mixinDefaultGetParams({
+                forceView: '1',
+                viewDataTable: 'sparkline',
+                module: 'API',
+                action: 'get',
+                widget: '1',
+                showtitle: '1',
+                columns: vm.metric,
+                colors: JSON.stringify(piwik.getSparklineColors()),
+                date: vm.sparklineRange,
+                random: Date.now()
+            });
+
+            return '?' + $httpParamSerializer(urlParams);
         }
     }
 })();
