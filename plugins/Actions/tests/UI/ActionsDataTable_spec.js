969f2a41b629532ad13c79f84fc1688fef2cf855
sgiehl <stefan@piwik.org>
2018-06-25 20:20:22 +0200

    Converts ActionsDataTable UI tests

--- plugins/Actions/tests/UI/ActionsDataTable_spec.js
+++ plugins/Actions/tests/UI/ActionsDataTable_spec.js
@@ -8,119 +8,133 @@
  */
 
 describe("ActionsDataTable", function () {
-    this.timeout(0);
-
-    var url = "?module=Widgetize&action=iframe&idSite=1&period=year&date=2012-08-09&moduleToWidgetize=Actions&actionToWidgetize=getPageUrls&isFooterExpandedInDashboard=1";
+    const url = "?module=Widgetize&action=iframe&idSite=1&period=year&date=2012-08-09&moduleToWidgetize=Actions&actionToWidgetize=getPageUrls&isFooterExpandedInDashboard=1";
 
     it("should load correctly", async function() {
-        expect.screenshot('initial').to.be.capture(function (page) {
-            page.load(url);
-        }, done);
+        await page.goto(url);
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('initial');
     });
 
     it("should sort column correctly when column header clicked", async function() {
-        expect.screenshot('column_sorted').to.be.capture(function (page) {
-            page.click('th#avg_time_on_page', 3000);
-        }, done);
+        await page.click('th#avg_time_on_page');
+        await page.mouse.move(-10, -10);
+        await page.waitForNetworkIdle();
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('column_sorted');
     });
 
     it("should load subtables correctly when row clicked", async function() {
-        expect.screenshot('subtables_loaded').to.be.capture(function (page) {
-            page.evaluate(function(){
-               $('tr.subDataTable:first').click();
-               $('tr.subDataTable:eq(2)').click();
-            });
-        }, done);
+        firstRow = await page.jQuery('tr.subDataTable:first');
+        await firstRow.click();
+        secondRow = await page.jQuery('tr.subDataTable:eq(2)');
+        await secondRow.click();
+        await page.mouse.move(-10, -10);
+
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('subtables_loaded');
     });
 
     it("should show configuration options", async function() {
-        expect.screenshot('configuration_options').to.be.captureSelector('.tableConfiguration', function (page) {
-            page.click('.dropdownConfigureIcon');
-        }, done);
+        await page.click('.dropdownConfigureIcon');
+        await page.mouse.move(-10, -10);
+        const element = await page.$('.tableConfiguration');
+        expect(await element.screenshot()).to.matchImage('configuration_options');
     });
 
     it("should flatten table when flatten link clicked", async function() {
-        expect.screenshot('flattened').to.be.capture(function (page) {
-            page.click('.dataTableFlatten');
-        }, done);
+        await page.click('.dataTableFlatten');
+        await page.mouse.move(-10, -10);
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('flattened');
     });
 
     it("should exclude low population rows when exclude low population link clicked", async function() {
-        expect.screenshot('exclude_low_population').to.be.capture(function (page) {
-            page.click('.dropdownConfigureIcon');
-            page.click('.dataTableExcludeLowPopulation');
-        }, done);
+        await page.click('.dropdownConfigureIcon');
+        await page.click('.dataTableExcludeLowPopulation');
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('exclude_low_population');
     });
 
     it("should load normal view when switch to view hierarchical view link is clicked", async function() {
-        expect.screenshot('unflattened').to.be.capture(function (page) {
-            page.click('.dropdownConfigureIcon span');
-            page.click('.dataTableFlatten');
-            page.mouseMove({x: -10, y: -10}); // mae sure no row is highlighted
-        }, done);
+        await page.click('.dropdownConfigureIcon');
+        await page.click('.dataTableFlatten');
+        await page.waitForNetworkIdle();
+        await page.mouse.move(-10, -10);
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('unflattened');
     });
 
     it("should display pageview percentages when hovering over pageviews column", async function() {
-        this.retries(3);
-        expect.screenshot('pageview_percentages').to.be.capture(function (page) {
-            page.mouseMove('tr:contains("thankyou") td.column:eq(1)');
-            page.wait(1000);
-        }, done);
+        const elem = await page.jQuery('tr:contains("thankyou") td.column:eq(1)');
+        await elem.hover();
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('pageview_percentages');
     });
 
     it("should generate a proper title for the visitor log segmented by the current row", async function() {
-        this.retries(3);
-        expect.screenshot('segmented_visitor_log_hover').to.be.capture(function (page) {
-            var row = 'tr:contains("thankyou") ';
-            page.mouseMove(row + 'td.column:first');
-            page.mouseMove(row + 'td.label .actionSegmentVisitorLog');
-            page.wait(1000);
-        }, done);
+        const row = 'tr:contains("thankyou") ';
+        const first = await page.jQuery(row + 'td.column:first');
+        await first.hover();
+        const second = await page.jQuery(row + 'td.label .actionSegmentVisitorLog');
+        await second.hover();
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('segmented_visitor_log_hover');
     });
 
     it("should open the visitor log segmented by the current row", async function() {
-        expect.screenshot('segmented_visitor_log').to.be.captureSelector('.ui-dialog', function (page) {
-            page.evaluate(function(){
-                $('tr:contains("thankyou") td.label .actionSegmentVisitorLog').click();
-            }, 3000);
-        }, done);
+        await page.evaluate(function(){
+            $('tr:contains("thankyou") td.label .actionSegmentVisitorLog').click();
+        });
+        await page.mouse.move(-10, -10);
+        await page.waitForSelector('.ui-dialog');
+        await page.waitForNetworkIdle();
+        const element = await page.$('.ui-dialog');
+        expect(await element.screenshot()).to.matchImage('segmented_visitor_log');
     });
 
     it("should display unique pageview percentages when hovering over unique pageviews column", async function() {
-        this.retries(3);
-        expect.screenshot('unique_pageview_percentages').to.be.capture(function (page) {
-            page.click('.ui-widget .ui-dialog-titlebar-close');
-
-            page.mouseMove('tr:contains("thankyou") td.column:eq(2)');
-            page.wait(1000);
-        }, done);
+        await page.click('.ui-widget .ui-dialog-titlebar-close');
+        const elem = await page.jQuery('tr:contains("thankyou") td.column:eq(2)');
+        await elem.hover();
+        await page.waitFor(100);
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('unique_pageview_percentages');
     });
 
     it("should show the search when clicking on the search icon", async function() {
-        expect.screenshot('search_visible').to.be.capture(function (page) {
-            page.click('.dataTableAction.searchAction');
-        }, done);
+        await page.click('.dataTableAction.searchAction');
+        await page.waitFor(500);
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('search_visible');
     });
 
     it("should search through table when search input entered and search button clicked and input should be visible", async function() {
-        expect.screenshot('search').to.be.capture(function (page) {
-            page.sendKeys('.searchAction .dataTableSearchInput', 'i');
-            page.click('.searchAction .icon-search', 1500);
-            page.mouseMove('#logo', 100);
-        }, done);
+        await page.type('.searchAction .dataTableSearchInput', 'i');
+        await page.click('.searchAction .icon-search');
+        await page.waitForNetworkIdle();
+        await page.mouse.move(-10, -10);
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('search');
     });
 
     it("should close search when clicking on the x icon", async function() {
-        expect.screenshot('search_closed').to.be.capture(function (page) {
-            page.click('.searchAction .icon-close');
-        }, done);
+        await page.click('.searchAction .icon-close');
+        await page.waitForNetworkIdle();
+        await page.mouse.move(-10, -10);
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('search_closed');
     });
 
     it("should automatically expand subtables if it contains only one folder", async function() {
-        expect.screenshot('auto_expand').to.be.capture(function (page) {
-            page.load(url + '&viewDataTable=table');
-            page.click('tr .value:contains("blog")');
-            page.click('tr .value:contains("2012")');
-        }, done);
+        await page.goto(url + '&viewDataTable=table');
+
+        await page.waitForFunction("$('tr .value:contains(\"blog\")').length > 0");
+        const first = await page.jQuery('tr .value:contains("blog")');
+        await first.click();
+        await page.waitForFunction("$('tr .value:contains(\"2012\")').length > 0");
+        const second = await page.jQuery('tr .value:contains("2012")');
+        await second.click();
+
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('auto_expand');
     });
 });
