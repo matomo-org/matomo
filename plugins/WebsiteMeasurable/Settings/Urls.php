d6c9c65d1b7036a1d511c1908e51a09b9393845a
sgiehl <stefan@piwik.org>
2018-07-02 12:09:51 +0200

    Make it possible to use line breaks in placeholders
    for safari those line breaks will be replaced with 200 white spaces, as line breaks aren't supported there

--- plugins/WebsiteMeasurable/Settings/Urls.php
+++ plugins/WebsiteMeasurable/Settings/Urls.php
@@ -41,7 +41,7 @@ class Urls extends \Piwik\Settings\Measurable\MeasurableProperty
         $config->uiControlAttributes = array(
           'cols' => '25',
           'rows' => '3',
-          'placeholder' => "https://siteUrl.com/                                                 https://siteUrl2.com/",
+          'placeholder' => "https://siteUrl.com/\nhttps://siteUrl2.com/",
         );
 
         $self = $this;
