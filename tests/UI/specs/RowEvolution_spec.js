799e01dd62e3817d46ad8ed6f4a057affdedf963
sgiehl <stefan@piwik.org>
2018-07-06 16:44:07 +0200

    Converts RowEvolution UI tests

--- tests/UI/specs/RowEvolution_spec.js
+++ tests/UI/specs/RowEvolution_spec.js
@@ -10,57 +10,85 @@
 describe("RowEvolution", function () {
     this.timeout(0);
 
-    var viewDataTableUrl = "?module=Widgetize&action=iframe&moduleToWidgetize=Referrers&idSite=1&period=week&date=2012-02-09&"
+    const viewDataTableUrl = "?module=Widgetize&action=iframe&moduleToWidgetize=Referrers&idSite=1&period=week&date=2012-02-09&"
                          + "actionToWidgetize=getKeywords&viewDataTable=table&filter_limit=5";
 
-    var ecommerceItemReportWidgetized = "?module=Widgetize&action=iframe&moduleToWidgetize=Goals&actionToWidgetize=getItemsSku&idGoal=ecommerceAbandonedCart"
+    const ecommerceItemReportWidgetized = "?module=Widgetize&action=iframe&moduleToWidgetize=Goals&actionToWidgetize=getItemsSku&idGoal=ecommerceAbandonedCart"
                                       + "&idSite=1&period=year&date=2012-02-09&viewDataTable=ecommerceAbandonedCart&filter_limit=-1";
 
     it('should load when icon clicked in ViewDataTable', async function() {
-        expect.screenshot('row_evolution').to.be.captureSelector('.ui-dialog', function (page) {
-            page.goto(viewDataTableUrl);
-            page.mouseMove('tbody tr:first-child');
-            page.mouseMove('a.actionRowEvolution:visible'); // necessary to get popover to display
-            page.click('a.actionRowEvolution:visible');
-        }, done);
+        await page.goto(viewDataTableUrl);
+        const row = await page.waitForSelector('tbody tr:first-child');
+        await row.hover();
+
+        const icon = await page.waitForSelector('tbody tr:first-child a.actionRowEvolution');
+        await icon.click();
+
+        await page.waitForSelector('.ui-dialog');
+        await page.waitForNetworkIdle();
+
+        const dialog = await page.$('.ui-dialog');
+        expect(await dialog.screenshot()).to.matchImage('row_evolution');
     });
 
     it('should change the metric shown when a metric sparkline row is clicked', async function() {
-        expect.screenshot('row_evolution_other_metric').to.be.captureSelector('.ui-dialog', function (page) {
-            page.click('table.metrics tr[data-i=1]');
-        }, done);
+        await page.click('table.metrics tr[data-i="1"]');
+        await page.waitForNetworkIdle();
+
+        const dialog = await page.$('.ui-dialog');
+        expect(await dialog.screenshot()).to.matchImage('row_evolution_other_metric');
     });
 
     it('should show two serieses when a metric sparkline row is shift+clicked', async function() {
-        expect.screenshot('row_evolution_multiple_series').to.be.captureSelector('.ui-dialog', function (page) {
-            page.click('table.metrics tr[data-i=2]', ['shift']);
-        }, done);
+        await page.keyboard.down('Shift');
+        await page.click('table.metrics tr[data-i="2"]', ['shift']);
+        await page.keyboard.up('Shift');
+        await page.waitForNetworkIdle();
+
+        const dialog = await page.$('.ui-dialog');
+        expect(await dialog.screenshot()).to.matchImage('row_evolution_multiple_series');
     });
 
     it('should load multi-row evolution correctly', async function() {
-        expect.screenshot('multirow_evolution').to.be.captureSelector('.ui-dialog', function (page) {
-            page.click('.rowevolution-startmulti');
-            page.mouseMove('tbody tr:nth-child(2)');
-            page.mouseMove('a.actionRowEvolution:visible');
-            page.click('a.actionRowEvolution:visible');
-        }, done);
+        await page.click('.rowevolution-startmulti');
+
+        const row = await page.waitForSelector('tbody tr:nth-child(2)');
+        await row.hover();
+
+        const icon = await page.waitForSelector('tbody tr:nth-child(2) a.actionRowEvolution');
+        await icon.click();
+
+        await page.waitForSelector('.ui-dialog');
+        await page.waitForNetworkIdle();
+
+        const dialog = await page.$('.ui-dialog');
+        expect(await dialog.screenshot()).to.matchImage('multirow_evolution');
     });
 
     it('should display a different row evolution metric when the metric selection is changed', async function() {
-        expect.screenshot('multirow_evolution_other_metric').to.be.captureSelector('.ui-dialog', function (page) {
-            page.evaluate(function () {
-                $('select.multirowevoltion-metric').val($('select.multirowevoltion-metric option:nth-child(3)').val()).change();
-            });
-            page.wait(1000);
-        }, done);
+        await page.evaluate(function () {
+            $('select.multirowevoltion-metric').val($('select.multirowevoltion-metric option:nth-child(3)').val()).change();
+        });
+
+        await page.waitForSelector('.ui-dialog');
+        await page.waitForNetworkIdle();
+
+        const dialog = await page.$('.ui-dialog');
+        expect(await dialog.screenshot()).to.matchImage('multirow_evolution_other_metric');
     });
 
     it('should display row evolution for an ecommerce item report correctly', async function() {
-        expect.screenshot('row_evolution_ecommerce_item').to.be.captureSelector('.ui-dialog', function (page) {
-            page.goto(ecommerceItemReportWidgetized);
-            page.mouseMove('tbody tr:first-child');
-            page.mouseMove('a.actionRowEvolution:visible'); // necessary to get popover to display
-            page.click('a.actionRowEvolution:visible');
-        }, done);
+        await page.goto(ecommerceItemReportWidgetized);
+        const row = await page.waitForSelector('tbody tr:first-child');
+        await row.hover();
+
+        const icon = await page.waitForSelector('tbody tr:first-child a.actionRowEvolution');
+        await icon.click();
+
+        await page.waitForSelector('.ui-dialog');
+        await page.waitForNetworkIdle();
+
+        const dialog = await page.$('.ui-dialog');
+        expect(await dialog.screenshot()).to.matchImage('row_evolution_ecommerce_item');
     });
 });
