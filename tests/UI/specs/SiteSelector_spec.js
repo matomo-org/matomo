d617409fc444ee2cf3fc7b9d0629fa4b84432803
sgiehl <stefan@piwik.org>
2018-07-06 16:59:16 +0200

    Converts SiteSelector UI tests

--- tests/UI/specs/SiteSelector_spec.js
+++ tests/UI/specs/SiteSelector_spec.js
@@ -8,46 +8,58 @@
  */
 
 describe("SiteSelector", function () {
-    var selectorToCapture = '[piwik-siteselector],[piwik-siteselector] .dropdown';
-
     this.timeout(0);
 
-    var url = "?module=UsersManager&action=userSettings&idSite=1&period=day&date=yesterday";
+    const selectorToCapture = '[piwik-siteselector],[piwik-siteselector] .dropdown';
+    const url = "?module=UsersManager&action=userSettings&idSite=1&period=day&date=yesterday";
 
     it("should load correctly", async function() {
-        expect.screenshot("loaded").to.be.captureSelector(selectorToCapture, function (page) {
-            page.goto(url);
-        }, done);
+        await page.goto(url);
+        await page.waitForNetworkIdle();
+
+        dialog = await page.$(selectorToCapture);
+        expect(await dialog.screenshot()).to.matchImage('loaded');
     });
 
     it("should display expanded when clicked", async function() {
-        expect.screenshot("expanded").to.be.captureSelector(selectorToCapture, function (page) {
-            page.click('.sites_autocomplete .title');
-        }, done);
+        await page.click('.sites_autocomplete .title');
+
+        await page.waitForSelector('.custom_select_ul_list');
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshotSelector(selectorToCapture)).to.matchImage('expanded');
     });
 
     it("should show no results when search returns no results", async function() {
-        expect.screenshot("search_no_results").to.be.captureSelector(selectorToCapture, function (page) {
-            page.sendKeys(".websiteSearch", "abc");
-        }, done);
+        await page.type(".websiteSearch", "abc");
+        await page.waitFor(500);
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshotSelector(selectorToCapture)).to.matchImage('search_no_results');
     });
 
     it("should search when one character typed into search input", async function() {
-        expect.screenshot("search_one_char").to.be.captureSelector(selectorToCapture, function (page) {
-            page.click('.reset');
-            page.sendKeys(".websiteSearch", "s");
-        }, done);
+        await page.click('.reset');
+        await page.type(".websiteSearch", "s");
+        await page.waitForNetworkIdle();
+        await page.waitFor(500);
+
+        expect(await page.screenshotSelector(selectorToCapture)).to.matchImage('search_one_char');
     });
 
     it("should search again when second character typed into search input", async function() {
-        expect.screenshot("search_two_chars").to.be.captureSelector(selectorToCapture, function (page) {
-            page.sendKeys(".websiteSearch", "st");
-        }, done);
+        await page.type(".websiteSearch", "t");
+        await page.waitForNetworkIdle();
+        await page.waitFor(500);
+
+        expect(await page.screenshotSelector(selectorToCapture)).to.matchImage('search_two_chars');
     });
 
     it("should change the site when a site is selected", async function() {
-        expect.screenshot("site_selected").to.be.captureSelector(selectorToCapture, function (page) {
-            page.click(".custom_select_ul_list>li:visible");
-        }, done);
+        elem = await page.jQuery(".custom_select_ul_list>li:visible");
+        elem.click();
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshotSelector(selectorToCapture)).to.matchImage('site_selected');
     });
 });
