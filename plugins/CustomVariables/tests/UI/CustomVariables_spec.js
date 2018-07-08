6e36a41e2a53227faef72268fb32e69541aacd89
sgiehl <stefan@piwik.org>
2018-07-08 15:08:03 +0200

    Converts CustomVariables UI test

--- plugins/CustomVariables/tests/UI/CustomVariables_spec.js
+++ plugins/CustomVariables/tests/UI/CustomVariables_spec.js
@@ -13,13 +13,14 @@ describe("CustomVariables", function () {
     this.fixture = "Piwik\\Plugins\\CustomVariables\\tests\\Fixtures\\VisitWithManyCustomVariables";
 
     it('should show an overview of all used custom variables', async function() {
-        expect.screenshot('manage').to.be.captureSelector('.pageWrap', function (page) {
-            page.goto("?idSite=1&period=day&date=2010-01-03&module=CustomVariables&action=manage");
-        }, done);
+        await page.goto("?idSite=1&period=day&date=2010-01-03&module=CustomVariables&action=manage");
+        await page.waitForNetworkIdle();
+
+        pageWrap = await page.$('.pageWrap');
+        expect(await pageWrap.screenshot()).to.matchImage('manage');
     });
 
     it('should be visible in the menu', async function() {
-        expect.screenshot('link_in_menu').to.be.captureSelector('li:contains(Diagnostic)', function (page) {
-        }, done);
+        expect(await page.screenshotSelector('li:contains(Diagnostic)')).to.matchImage('link_in_menu');
     });
 });
