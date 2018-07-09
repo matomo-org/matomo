5811c5dc7394e65a69c52953f73ac99e68cac213
sgiehl <stefan@piwik.org>
2018-07-09 20:42:40 +0200

    Converts Installation UI tests

--- plugins/Installation/tests/UI/Installation_spec.js
+++ plugins/Installation/tests/UI/Installation_spec.js
@@ -6,7 +6,8 @@
  * @link https://matomo.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
-var fs = require('fs');
+var fs = require('fs'),
+    path = require('../../../../tests/lib/screenshot-testing/support/path');
 
 describe("Installation", function () {
     this.timeout(0);
@@ -34,148 +35,157 @@ describe("Installation", function () {
     });
 
     it("should display an error message when trying to access a resource w/o a config.ini.php file", async function() {
-        expect.screenshot("access_no_config").to.be.capture(function (page) {
-            page.load("?module=CoreHome&action=index&ignoreClearAllViewDataTableParameters=1");
+        await page.goto("?module=CoreHome&action=index&ignoreClearAllViewDataTableParameters=1");
 
-            page.evaluate(function () {
-                // ensure screenshots are reporting travis config file for comparison
-                // no jQuery existing on these error pages...
-                document.body.innerHTML = document.body.innerHTML.replace(
-                    /{\/.*\/test\.config\.ini\.php}/,
-                    '{/home/travis/build/piwik/piwik/tests/lib/screenshot-testing/../../../tmp/test.config.ini.php}'
-                );
-            });
-        }, done);
+        await page.evaluate(function () {
+            // ensure screenshots are reporting travis config file for comparison
+            // no jQuery existing on these error pages...
+            document.body.innerHTML = document.body.innerHTML.replace(
+                /{\/.*\/test\.config\.ini\.php}/,
+                '{/home/travis/build/piwik/piwik/tests/lib/screenshot-testing/../../../tmp/test.config.ini.php}'
+            );
+        });
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('access_no_config');
     });
 
     it("should start the installation process when the index is visited w/o a config.ini.php file", async function() {
-        expect.screenshot("start").to.be.capture(function (page) {
-            page.load("?ignoreClearAllViewDataTableParameters=1");
-        }, done);
+        await page.goto("?ignoreClearAllViewDataTableParameters=1");
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('start');
     });
 
     it("should display the system check page when next is clicked on the first page", async function() {
-        expect.screenshot("system_check").to.be.capture(function (page) {
-            page.click('.next-step .btn');
-        }, done);
+        await page.click('.next-step .btn');
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('system_check');
     });
 
-    var pageUrl;
+    let pageUrl;
     it("should have already created a tmp/sessions/index.htm file to prevent directory listing", async function() {
-        expect.screenshot('nothing_to_see_here').to.be.capture(function (page) {
-            pageUrl = page.url();
+        pageUrl = page.url();
 
-            // page.load will load by default the proxy ie. http://localhost/piwik/tests/PHPUnit/proxy/
-            // but we need here to check in: http://localhost/piwik/tmp/sessions/
-            page.load("../../../tmp/sessions/index.htm");
+        // page.load will load by default the proxy ie. http://localhost/piwik/tests/PHPUnit/proxy/
+        // but we need here to check in: http://localhost/piwik/tmp/sessions/
+        await page.goto("../../../tmp/sessions/index.htm");
 
-        }, done);
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('nothing_to_see_here');
     });
 
     it("should display the database setup page when next is clicked on the system check page", async function() {
-        expect.screenshot("db_setup").to.be.capture(function (page) {
-            page.load(pageUrl);
+        await page.goto(pageUrl);
 
-            page.click('.next-step .btn');
-        }, done);
+        page.click('.next-step .btn');
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('db_setup');
     });
 
     it("should fail when the next button is clicked and no database info is entered in the form", async function() {
-        expect.screenshot("db_setup_fail").to.be.capture(function (page) {
-            page.click('.btn');
-        }, done);
+        await page.click('.btn');
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('db_setup_fail');
     });
 
     it("should display the tables created page when next is clicked on the db setup page w/ correct info entered in the form", async function() {
-        expect.screenshot("db_created").to.be.capture(function (page) {
-            var dbInfo = testEnvironment.readDbInfoFromConfig();
-            var username = dbInfo.username;
-            var password = dbInfo.password;
+        const dbInfo = testEnvironment.readDbInfoFromConfig();
+        const username = dbInfo.username;
+        const password = dbInfo.password;
 
-            page.sendKeys('input[name=username]', username);
+        await page.type('input[name="username"]', username);
 
-            if (password) {
-                page.sendKeys('input[name=password]', password);
-            }
+        if (password) {
+            await page.type('input[name="password"]', password);
+        }
 
-            page.sendKeys('input[name=dbname]', 'newdb');
-            page.click('.btn');
-        }, done);
+        await page.type('input[name="dbname"]', 'newdb');
+        await page.click('.btn');
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('db_created');
     });
 
     it("should display the superuser configuration page when next is clicked on the tables created page", async function() {
-        expect.screenshot("superuser").to.be.capture(function (page) {
-            page.click('.next-step .btn');
-        }, done);
+        await page.click('.next-step .btn');
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('superuser');
     });
 
-    var pageUrl, pageUrlDe;
+    let pageUrlDe;
 
     it("should un-select Professional Services newsletter checkbox when language is German", async function() {
-        expect.screenshot("superuser_de").to.be.capture(function (page) {
-            pageUrl = page.url();
-            pageUrlDe = pageUrl + '&language=de'
-            page.load(pageUrlDe);
-        }, done);
+        pageUrl = await page.url();
+        pageUrlDe = pageUrl + '&language=de';
+        await page.goto(pageUrlDe);
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('superuser_de');
     });
 
     it("should fail when incorrect information is entered in the superuser configuration page", async function() {
-        expect.screenshot("superuser_fail").to.be.capture(function (page) {
-            page.load(pageUrl);
-            page.click('.btn');
-        }, done);
+        await page.goto(pageUrl);
+        await page.click('.btn');
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('superuser_fail');
     });
 
     it("should display the setup a website page when next is clicked on the filled out superuser config page", async function() {
-        expect.screenshot("setup_website").to.be.capture(function (page) {
-            page.sendKeys('input[name=login]', 'thesuperuser');
-            page.sendKeys('input[name=password]', 'thepassword');
-            page.sendKeys('input[name=password_bis]', 'thepassword');
-            page.sendKeys('input[name=email]', 'hello@piwik.org');
-            page.click('.btn');
-            page.wait(3000);
-        }, done);
+        await page.type('input[name="login"]', 'thesuperuser');
+        await page.type('input[name="password"]', 'thepassword');
+        await page.type('input[name="password_bis"]', 'thepassword');
+        await page.type('input[name="email"]', 'hello@piwik.org');
+        await page.click('.btn');
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('setup_website');
     });
 
     it("should should fail when incorrect information is entered in the setup a website page", async function() {
-        expect.screenshot("setup_website_fail").to.be.capture(function (page) {
-            page.click('.btn');
-        }, done);
+        await page.click('.btn');
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('setup_website_fail');
     });
 
     it("should display the javascript tracking page when correct information is entered in the setup website page and next is clicked", async function() {
-        expect.screenshot("js_tracking").to.be.capture(function (page) {
-            page.sendKeys('input[name=siteName]', 'Serenity');
-            page.evaluate(function () {
-                // cannot use sendKeys since quickform does not use placeholder attribute
-                $('input[name=url]').val('serenity.com');
-                
-                $('select[name=timezone]').val('Europe/Paris');
-                $('select[name=ecommerce]').val('1');
-            });
-            page.click('.btn');
-            page.wait(3000);
+        await page.type('input[name="siteName"]', 'Serenity');
+        await page.evaluate(function () {
+            // cannot use sendKeys since quickform does not use placeholder attribute
+            $('input[name=url]').val('serenity.com');
 
-            // manually remove port in tracking code, since ui-test.php won't be using the correct INI config file
-            page.evaluate(function () {
-                $('pre').each(function () {
-                    var html = $(this).html();
-                    html = html.replace(/localhost\:[0-9]+/g, 'localhost');
-                    $(this).html(html);
-                });
+            $('select[name=timezone]').val('Europe/Paris');
+            $('select[name=ecommerce]').val('1');
+        });
+        await page.click('.btn');
+        await page.waitForNetworkIdle();
+
+        // manually remove port in tracking code, since ui-test.php won't be using the correct INI config file
+        await page.evaluate(function () {
+            $('pre').each(function () {
+                var html = $(this).html();
+                html = html.replace(/localhost\:[0-9]+/g, 'localhost');
+                $(this).html(html);
             });
-        }, done);
+        });
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('js_tracking');
     });
 
     it("should display the congratulations page when next is clicked on the javascript tracking page", async function() {
-        expect.screenshot("congrats").to.be.capture(function (page) {
-            page.click('.next-step .btn');
-        }, done);
+        await page.click('.next-step .btn');
+        await page.waitForNetworkIdle();
+
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('congrats');
     });
 
     it("should continue to piwik after submitting on the privacy settings form in the congrats page", async function() {
-        expect.current_page.contains('.loginForm', function (page) {
-            page.click('.btn');
-        }, done);
+        await page.click('.btn');
+        await page.waitForNetworkIdle();
+
+        // check login form is displayed
+        await page.waitForSelector('.loginForm');
     });
 });
