71450d7341c856d8d9a49d45f752220868ae4177
diosmosis <diosmosis@users.noreply.github.com>
2018-07-10 22:15:57 -0700

    Add/edit user functionality.

--- plugins/UsersManager/angularjs/user-edit-form/user-edit-form.component.js
+++ plugins/UsersManager/angularjs/user-edit-form/user-edit-form.component.js
@@ -20,25 +20,35 @@
         controller: UserEditFormController
     });
 
-    UserEditFormController.$inject = ['$element'];
+    UserEditFormController.$inject = ['$element', 'piwikApi'];
 
-    function UserEditFormController($element) {
+    function UserEditFormController($element, piwikApi) {
         var vm = this;
         vm.activeTab = 'basic';
         vm.permissionsForIdSite = 1;
+        vm.isSavingUserInfo = false;
+        vm.isPasswordChanged = false;
+
         vm.$onInit = $onInit;
+        vm.$onChanges = $onChanges;
         vm.confirmSuperUserChange = confirmSuperUserChange;
         vm.getFormTitle = getFormTitle;
         vm.getSaveButtonLabel = getSaveButtonLabel;
         vm.toggleSuperuserAccess = toggleSuperuserAccess;
+        vm.saveUserInfo = saveUserInfo;
 
         function $onInit() {
             if (vm.user) {
                 vm.isAdd = false;
-                vm.user.password = 'XXXXXXXX';
             } else {
                 vm.isAdd = true;
                 vm.user = {};
+            }
+        }
+
+        function $onChanges() {
+            if (!vm.isAdd) {
+                vm.user.password = 'XXXXXXXX'; // make sure password is not stored in the client after update/save
             }
         }
 
@@ -47,7 +57,7 @@
         }
 
         function getSaveButtonLabel() {
-            return vm.isAdd ? 'Create User' : 'Save';
+            return vm.isAdd ? 'Create User' : 'Save Basic Info';
         }
 
         function confirmSuperUserChange() {
@@ -56,6 +66,47 @@
 
         function toggleSuperuserAccess() {
             alert('toggle superuser access'); // TODO
+        }
+
+        function saveUserInfo() {
+            if (vm.isAdd) {
+                createUser();
+            } else {
+                updateUser();
+            }
+        }
+
+        function createUser() {
+            vm.isSavingUserInfo = true;
+            piwikApi.post({
+                method: 'UsersManager.addUser',
+                userLogin: vm.user.login,
+                password: vm.user.password,
+                email: vm.user.email,
+                alias: vm.user.alias
+            }).catch(function () {
+                // ignore (error is still displayed to user)
+            }).then(function () {
+                vm.isSavingUserInfo = false;
+                vm.isAdd = false;
+                vm.isPasswordChanged = false;
+            });
+        }
+
+        function updateUser() {
+            vm.isSavingUserInfo = true;
+            piwikApi.post({
+                method: 'UsersManager.updateUser',
+                userLogin: vm.user.login,
+                password: vm.isPasswordChanged ? vm.user.password : undefined,
+                email: vm.user.email,
+                alias: vm.user.alias
+            }).catch(function () {
+                // ignore (error is still displayed to user)
+            }).then(function () {
+                vm.isSavingUserInfo = false;
+                vm.isPasswordChanged = false;
+            });
         }
     }
 })();
