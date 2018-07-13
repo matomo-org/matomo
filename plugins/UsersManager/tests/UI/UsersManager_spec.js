5c2334e2a77d93b51a6188071a6a98328f32700d
diosmosis <diosmosis@users.noreply.github.com>
2018-07-12 20:39:11 -0700

    Starting on UI tests.

--- plugins/UsersManager/tests/UI/UsersManager_spec.js
+++ plugins/UsersManager/tests/UI/UsersManager_spec.js
@@ -13,29 +13,6 @@ describe("UsersManager", function () {
 
     var url = "?module=UsersManager&action=index";
 
-    function assertScreenshotEquals(screenshotName, done, test)
-    {
-        expect.screenshot(screenshotName).to.be.captureSelector('#content', test, done);
-    }
-
-    function openGiveAccessForm(page) {
-        page.click('#showGiveViewAccessForm');
-    }
-
-    function setLoginOrEmailForGiveAccessForm(page, loginOrEmail)
-    {
-        page.evaluate(function () {
-            $('#user_invite').val('');
-        });
-        page.sendKeys('#user_invite', loginOrEmail);
-    }
-
-    function submitGiveAccessForm(page)
-    {
-        page.click('#giveUserAccessToViewReports');
-        page.wait(1000); // we wait in case error notification is still fading in and not fully visible yet
-    }
-
     before(function () {
         testEnvironment.idSitesAdminAccess = [1,2];
         testEnvironment.save();
@@ -46,65 +23,135 @@ describe("UsersManager", function () {
         testEnvironment.save();
     });
 
-    it("should show only users having access to same site", function (done) {
-        assertScreenshotEquals("loaded_as_admin", done, function (page) {
-            page.load(url);
-        });
+    it('should display the manage users page correctly', function () {
+        // TODO
     });
 
-    it("should open give view access form when clicking on button", function (done) {
-        assertScreenshotEquals("adminuser_give_view_access_form_opened", done, function (page) {
-            openGiveAccessForm(page);
-        });
+    it('should change the results page when next is clicked', function () {
+        // TODO
     });
 
-    it("should show an error when nothing entered", function (done) {
-        assertScreenshotEquals("adminuser_give_view_access_no_user_entered", done, function (page) {
-            submitGiveAccessForm(page);
-        });
+    it('should filter by username and access level when the inputs are filled', function () {
+        // TODO
     });
 
-    it("should show an error when no such user found", function (done) {
-        assertScreenshotEquals("adminuser_give_view_access_user_not_found", done, function (page) {
-            setLoginOrEmailForGiveAccessForm(page, 'anyNoNExistingUser');
-            submitGiveAccessForm(page);
-        });
+    it('should display access for a different site when the roles for select is changed', function () {
+        // TODO
     });
 
-    it("should show an error if user already has access", function (done) {
-        assertScreenshotEquals("adminuser_give_view_access_user_already_has_access", done, function (page) {
-            setLoginOrEmailForGiveAccessForm(page, 'login2');
-            submitGiveAccessForm(page);
-        });
+    it('should select rows when individual row select is clicked', function () {
+        // TODO
     });
 
-    it("should add a user by login", function (done) {
-        assertScreenshotEquals("adminuser_give_view_access_via_login", done, function (page) {
-            setLoginOrEmailForGiveAccessForm(page, 'login3');
-            submitGiveAccessForm(page);
-        });
+    it('should select all rows when all row select is clicked', function () {
+        // TODO
     });
 
-    it("should add a user by email", function (done) {
-        assertScreenshotEquals("adminuser_give_view_access_via_email", done, function (page) {
-            page.load(url);
-            openGiveAccessForm(page);
-            setLoginOrEmailForGiveAccessForm(page, 'login4@example.com');
-            submitGiveAccessForm(page);
-        });
+    it('should select all rows in search when link in table is clicked', function () {
+        // TODO
     });
 
-    it("should ask for confirmation when all sites selected", function (done) {
-        assertScreenshotEquals("adminuser_all_users_loaded", done, function (page) {
-            page.load(url + '&idSite=all');
-        });
+    it('should change access for all rows in search when bulk change access option used', function () {
+        // TODO
     });
 
-    it("should ask for confirmation when all sites selected", function (done) {
-        expect.screenshot("adminuser_all_users_confirmation").to.be.captureSelector('.modal.open', function (page) {
-            openGiveAccessForm(page);
-            setLoginOrEmailForGiveAccessForm(page, 'login5@example.com');
-            submitGiveAccessForm(page);
-        }, done);
+    it('should deselect all rows in search except for displayed rows when link in table is clicked again', function () {
+        // TODO
+    });
+
+    it('should remove access to the currently selected site when the bulk remove access option is clicked', function () {
+        // TODO
+    });
+
+    it('should go back to first page when previous button is clicked', function () {
+        // TODO
+    });
+
+    it('should delete a single user when the delete icon is clicked', function () {
+        // TODO
+    });
+
+    it('should delete selected users when delete users bulk action is used', function () {
+        // TODO
+    });
+
+    it('should show the add new user form when the add new user button is clicked', function () {
+        // TODO
+    });
+
+    it('should create a user and show the edit user form when the create user button is clicked', function () {
+        // TODO
+    });
+
+    it('should show the permissions edit when the permissions tab is clicked', function () {
+        // TODO
+    });
+
+    it('should add access to all websites when all websites selected in the site selector', function () {
+        // TODO
+    });
+
+    it('should go to the next results page when the next button is clicked', function () {
+        // TODO
+    });
+
+    it('should filter the permissions when the filters are used', function () {
+        // TODO
+    });
+
+    it('should remove access to a single site when the trash icon is used', function () {
+        // TODO
+    });
+
+    it('should select multiple rows when individual row selects are clicked', function () {
+        // TODO
+    });
+
+    it('should select all displayed rows when the select all checkbox is clicked', function () {
+        // TODO
+    });
+
+    it('should remove access to displayed rows when remove bulk access is clicked', function () {
+        // TODO
+    });
+
+    it('should select all sites in search when in table link is clicked', function () {
+        // TODO
+    });
+
+    it('should set access to single site when select in table is used', function () {
+        // TODO
+    });
+
+    it('should set access to all sites in search when set bulk access is used', function () {
+        // TODO
+    });
+
+    it('should display the superuser access tab when the superuser tab is clicked', function () {
+        // TODO
+    });
+
+    it('should give the user superuser access when the superuser toggle is clicked', function () {
+        // TODO
+    });
+
+    it('should go back to the manage users page when the back link is clicked', function () {
+        // TODO
+    });
+
+    it('should show the edit user form when the edit icon in a row is clicked', function () {
+        // TODO
+    });
+
+    it('should hide columns & functionality if an admin user views the manage user page', function () {
+        // TODO
+    });
+
+    it('should show the add user form for admin users', function () {
+        // TODO
+    });
+
+    it('should allow editing user permissions for admin users but not basic info', function () {
+        // TODO
     });
 });
