d8c0da56a8ad3068f3606d4d75a8b00b087073d6
sgiehl <stefan@piwik.org>
2018-07-14 21:28:40 +0200

    Converts CoreUpdater UI tests

--- plugins/CoreUpdater/tests/UI/CoreUpdaterCode_spec.js
+++ plugins/CoreUpdater/tests/UI/CoreUpdaterCode_spec.js
@@ -15,26 +15,25 @@ describe("CoreUpdaterCode", function () {
     var url = "?module=CoreUpdater&action=newVersionAvailable";
 
     it("should show a new version is available", async function() {
-        expect.screenshot("newVersion").to.be.capture(function (page) {
-            page.load(url);
-        }, done);
+        await page.goto(url);
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('newVersion');
     });
 
     it("should offer to retry using https when updating over https fails", async function() {
-        expect.screenshot("httpsUpdateFail").to.be.capture(function (page) {
-            page.click('#updateAutomatically');
-        }, done);
+        await page.click('#updateAutomatically');
+        await page.waitForNetworkIdle();
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('httpsUpdateFail');
     });
 
     it("should offer to retry over http when updating over https fails", async function() {
-        expect.screenshot("httpsUpdateFail").to.be.capture(function (page) {
-            page.click('#updateUsingHttps');
-        }, done);
+        await page.click('#updateUsingHttps');
+        await page.waitForNetworkIdle();
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('httpsUpdateFail');
     });
 
     it("should show the update steps when updating over http succeeds", async function() {
-        expect.screenshot("httpUpdateSuccess").to.be.capture(function (page) {
-            page.click('#updateUsingHttp');
-        }, done);
+        await page.click('#updateUsingHttp');
+        await page.waitForNetworkIdle();
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('httpUpdateSuccess');
     });
 });
