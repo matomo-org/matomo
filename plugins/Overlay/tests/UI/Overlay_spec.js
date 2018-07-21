e68f489f8394b14c011430d5818582a02e2fd00d
sgiehl <stefan@piwik.org>
2018-07-21 10:04:06 +0200

    Converts Overlay UI tests

--- plugins/Overlay/tests/UI/Overlay_spec.js
+++ plugins/Overlay/tests/UI/Overlay_spec.js
@@ -7,15 +7,13 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 describe("Overlay", function () {
-    this.retries(3);
-
     this.timeout(0);
 
     var url = null;
     var urlWithSegment;
 
-    function removeOptOutIframe(page) {
-        page.evaluate(function () {
+    async function removeOptOutIframe() {
+        await page.evaluate(function () {
             $('iframe#optOutIframe', $('iframe').contents()).remove();
         });
     }
@@ -27,112 +25,109 @@ describe("Overlay", function () {
         url = baseUrl + hash;
         urlWithSegment = baseUrl + '&segment=' + encodeURIComponent('visitIp==20.56.34.67') + hash;
 
-        testEnvironment.callApi("SitesManager.addSiteAliasUrls", {idSite: 3, urls: [config.piwikUrl]}, done);
+        await testEnvironment.callApi("SitesManager.addSiteAliasUrls", {idSite: 3, urls: [config.piwikUrl]});
     });
 
     after(async function() {
-        testEnvironment.callApi("SitesManager.setSiteAliasUrls", {idSite: 3, urls: []}, done);
+        await testEnvironment.callApi("SitesManager.setSiteAliasUrls", {idSite: 3, urls: []});
     });
 
     it("should load correctly", async function() {
-        expect.screenshot("loaded").to.be.capture(function (page) {
-            page.load(url);
+        await page.goto(url);
 
-            removeOptOutIframe(page);
-        }, done);
+        await removeOptOutIframe();
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('loaded');
     });
 
     it("should show clicks when hover over link in iframe", async function() {
-        expect.screenshot("page_link_clicks").to.be.capture(function (page) {
-            var pos = page.webpage.evaluate(function () {
-                var iframe = $('iframe'),
-                    innerOffset = $('.btn.btn-large', iframe.contents()).offset();
-                return {
-                    x: iframe.offset().left + innerOffset.left,
-                    y: iframe.offset().top + innerOffset.top
-                };
-            });
-            page.sendMouseEvent('mousemove', pos);
+        var pos = await page.webpage.evaluate(() => {
+            var iframe = $('iframe'),
+                innerOffset = $('.btn.btn-large', iframe.contents()).offset();
+            return {
+                x: iframe.offset().left + innerOffset.left,
+                y: iframe.offset().top + innerOffset.top
+            };
+        });
+        await page.mouse.move(pos.x, pos.y);
 
-            page.evaluate(function () {
-                $('div#PIS_StatusBar', $('iframe').contents()).each(function () {
-                    var html = $(this).html();
-                    html = html.replace(/localhost\:[0-9]+/g, 'localhost');
-                    $(this).html(html);
-                });
+        await page.evaluate(function () {
+            $('div#PIS_StatusBar', $('iframe').contents()).each(function () {
+                var html = $(this).html();
+                html = html.replace(/localhost\:[0-9]+/g, 'localhost');
+                $(this).html(html);
             });
-
-            removeOptOutIframe(page);
-        }, done);
+        });
+        await removeOptOutIframe();
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('page_link_clicks');
     });
 
     it("should show stats for new links when dropdown opened", async function() {
-        expect.screenshot("page_new_links").to.be.capture(function (page) {
-            page.reload(2500);
-            page.evaluate(function(){
-                $('.dropdown-toggle', $('iframe').contents())[0].click();
-            }, 500);
-            removeOptOutIframe(page);
-        }, done);
+        await page.reload();
+        await page.evaluate(function(){
+            $('.dropdown-toggle', $('iframe').contents())[0].click();
+        });
+        await page.waitFor(1000);
+
+        await removeOptOutIframe();
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('page_new_links');
     });
 
     it("should change page when clicking on internal iframe link", async function() {
-        expect.screenshot("page_change").to.be.capture(function (page) {
-            var pos = page.webpage.evaluate(function () {
-                var iframe = $('iframe'),
-                    innerOffset = $('ul.nav>li:nth-child(2)>a', iframe.contents()).offset();
-                return {
-                    x: iframe.offset().left + innerOffset.left + 32, // position is incorrect for some reason w/o adding pixels
-                    y: iframe.offset().top + innerOffset.top
-                };
-            });
-            page.sendMouseEvent('click', pos);
+        var pos = await page.webpage.evaluate(() => {
+            var iframe = $('iframe'),
+                innerOffset = $('ul.nav>li:nth-child(2)>a', iframe.contents()).offset();
+            return {
+                x: iframe.offset().left + innerOffset.left + 32, // position is incorrect for some reason w/o adding pixels
+                y: iframe.offset().top + innerOffset.top
+            };
+        });
+        await page.mouse.click(pos.x, pos.y);
+        await page.waitForNetworkIdle();
 
-            removeOptOutIframe(page);
-        }, done);
+        await removeOptOutIframe();
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('page_change');
     });
 
     it("should change date range when period changed", async function() {
-        expect.screenshot("period_change").to.be.capture(function (page) {
-            page.evaluate(function () {
-                $('#overlayDateRangeSelect').val('day;yesterday').trigger('change');
-            });
+        await page.waitForSelector('#overlayDateRangeSelect');
+        await page.webpage.evaluate(function () {
+            $('#overlayDateRangeSelect').val('day;yesterday').trigger('change');
+        });
 
-            removeOptOutIframe(page);
-        }, done);
+        await page.waitForNetworkIdle();
+        await page.waitFor(1000);
+
+        await removeOptOutIframe();
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('period_change');
     });
 
     it("should open row evolution popup when row evolution link clicked", async function() {
-        expect.screenshot("row_evolution").to.be.capture(function (page) {
-            page.evaluate(function () {
-                $('#overlayRowEvolution').click();
-            }, 500);
-            page.evaluate(function () {
-                $('.jqplot-xaxis').hide(); // xaxis will change every day so hide it
-            });
+        await page.evaluate(function(){
+            $('#overlayRowEvolution').click();
+        });
+        await page.waitForNetworkIdle();
+        await page.evaluate(function () {
+            $('.jqplot-xaxis').hide(); // xaxis will change every day so hide it
+        });
 
-            removeOptOutIframe(page);
-        }, done);
+        await removeOptOutIframe();
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('row_evolution');
     });
 
     it("should open transitions popup when transitions link clicked", async function() {
-        expect.screenshot("transitions").to.be.capture(function (page) {
-            page.evaluate(function () {
-                $('button.ui-dialog-titlebar-close').click();
-            }, 500);
-            page.evaluate(function () {
-                $('#overlayTransitions').click();
-            }, 500);
+        await page.click('button.ui-dialog-titlebar-close');
+        await page.click('#overlayTransitions');
+        await page.waitForNetworkIdle();
 
-            removeOptOutIframe(page);
-        }, done);
+        await removeOptOutIframe();
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('transitions');
     });
 
     it("should load an overlay with segment", async function() {
-        expect.screenshot("loaded_with_segment").to.be.capture(function (page) {
-            page.load(urlWithSegment);
+        await page.goto(urlWithSegment);
+        await page.waitForNetworkIdle();
 
-            removeOptOutIframe(page);
-        }, done);
+        await removeOptOutIframe();
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('loaded_with_segment');
     });
 });
