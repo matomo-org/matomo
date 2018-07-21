7d48935acd3a08faf93773c3f126f84506cf90e8
sgiehl <stefan@piwik.org>
2018-07-21 17:43:15 +0200

    Converts SegmentEditor UI tests

--- plugins/SegmentEditor/tests/UI/SegmentSelectorEditor_spec.js
+++ plugins/SegmentEditor/tests/UI/SegmentSelectorEditor_spec.js
@@ -15,215 +15,210 @@ describe("SegmentSelectorEditorTest", function () {
     var generalParams = 'idSite=1&period=year&date=2012-08-09';
     var url = '?module=CoreHome&action=index&' + generalParams + '#?' + generalParams + '&category=General_Actions&subcategory=General_Pages';
 
-    function selectFieldValue(page, fieldName, textToSelect)
+    async function selectFieldValue(fieldName, textToSelect)
     {
-        page.execCallback(function () {
-            page.webpage.evaluate(function(fieldName) {
-                $(fieldName + ' input.select-dropdown').click();
-            }, fieldName);
-        });
-        page.execCallback(function () {
-            page.webpage.evaluate(function(fieldName, textToSelect) {
-                $(fieldName + ' .dropdown-content.active li:contains("' + textToSelect + '"):first').click();
-            }, fieldName, textToSelect);
-        });
+        await page.webpage.evaluate((fieldName) => {
+            $(fieldName + ' input.select-dropdown').click();
+        }, fieldName);
+        await page.webpage.evaluate((fieldName, textToSelect) => {
+            $(fieldName + ' .dropdown-content.active li:contains("' + textToSelect + '"):first').click();
+        }, fieldName, textToSelect);
     }
 
-    function selectDimension(page, prefixSelector, category, name)
+    async function selectDimension(prefixSelector, category, name)
     {
-        page.click(prefixSelector + ' .metricListBlock .select-wrapper');
-        page.click(prefixSelector + ' .metricListBlock .expandableList h4:contains(' + category + ')');
-        page.click(prefixSelector + ' .metricListBlock .expandableList .secondLevel li:contains(' + name + ')');
+        await (await page.jQuery(prefixSelector + ' .metricListBlock .select-wrapper')).click();
+        await (await page.jQuery(prefixSelector + ' .metricListBlock .expandableList h4:contains(' + category + ')')).click();
+        await (await page.jQuery(prefixSelector + ' .metricListBlock .expandableList .secondLevel li:contains(' + name + ')')).click();
     }
 
     it("should load correctly", async function() {
-        expect.screenshot("0_initial").to.be.captureSelector(selectorsToCapture, function (page) {
-            page.load(url);
-        }, done);
+        await page.goto(url);
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('0_initial');
     });
 
     it("should open selector when control clicked", async function() {
-        expect.screenshot("1_selector_open").to.be.captureSelector(selectorsToCapture, function (page) {
-            page.click('.segmentationContainer .title');
-        }, done);
+        await page.click('.segmentationContainer .title');
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('1_selector_open');
     });
 
     it("should open segment editor when edit link clicked for existing segment", async function() {
-        expect.screenshot("2_segment_editor_update").to.be.captureSelector(selectorsToCapture, function (page) {
-            page.evaluate(function() {
-                $('.segmentList .editSegment:first').click()
-            }, 200);
-        }, done);
+        await page.evaluate(function() {
+            $('.segmentList .editSegment:first').click()
+        });
+        await page.waitForNetworkIdle();
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('2_segment_editor_update');
     });
 
     it("should start editing segment name when segment name edit link clicked", async function() {
-        expect.screenshot("3_segment_editor_edit_name").to.be.captureSelector(selectorsToCapture, function (page) {
-            page.click('.segmentEditorPanel .editSegmentName');
-        }, done);
+        await page.click('.segmentEditorPanel .editSegmentName');
+        await page.waitFor(250); // animation
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('3_segment_editor_edit_name');
     });
 
     it("should show the egment editor's available segments dropdown", async function() {
-        expect.screenshot("6_segment_editor_droplist").to.be.captureSelector(selectorsToCapture, function (page) {
-            page.mouseMove('.available_segments a.dropList');
-            page.click('.available_segments a.dropList');
-        }, done);
+        var elem = await page.$('.available_segments a.dropList');
+        await elem.hover();
+        await page.click('.available_segments a.dropList');
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('6_segment_editor_droplist');
     });
 
     it("should change segment when another available segment clicked in segment editor's available segments dropdown", async function() {
-        expect.screenshot("6_segment_editor_different").to.be.captureSelector(selectorsToCapture, function (page) {
-            page.click('.ui-menu-item a:contains(Add new segment)');
-        }, done);
+        await (await page.jQuery('.ui-menu-item a:contains(Add new segment)')).click();
+        await page.waitForNetworkIdle();
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('6_segment_editor_different');
     });
 
     it("should close the segment editor when the close link is clicked", async function() {
-        expect.screenshot("7_segment_editor_closed").to.be.captureSelector(selectorsToCapture, function (page) {
-            page.evaluate(function () {
-                $('.segmentEditorPanel .segment-footer .close').click();
-            });
-        }, done);
+        await page.evaluate(function () {
+            $('.segmentEditorPanel .segment-footer .close').click();
+        });
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('7_segment_editor_closed');
     });
 
     it("should open blank segment editor when create new segment link is clicked", async function() {
-        expect.screenshot("8_segment_editor_create").to.be.captureSelector(selectorsToCapture, function (page) {
-            page.click('.segmentationContainer .title');
-            page.click('.add_new_segment');
-        }, done);
+        await page.click('.segmentationContainer .title');
+        await page.click('.add_new_segment');
+        await page.waitForNetworkIdle();
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('8_segment_editor_create');
     });
 
     it("should update segment expression when selecting different segment", async function() {
-        expect.screenshot("dimension_drag_drop").to.be.captureSelector(selectorsToCapture, function (page) {
-            selectDimension(page, '.segmentRow0', 'Actions', 'Action URL');
-        }, done);
+        await selectDimension('.segmentRow0', 'Actions', 'Action URL');
+        await page.waitForNetworkIdle();
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('dimension_drag_drop');
     });
 
     // phantomjs won't take screenshots of dropdown windows, so skip this test
     it.skip("should show suggested segment values when a segment value input is focused", async function() {
-        expect.screenshot("suggested_values").to.be.captureSelector(selectorsToCapture, function (page) {
-            page.click('.segmentEditorPanel .ui-autocomplete-input');
-        }, done);
+        await page.click('.segmentEditorPanel .ui-autocomplete-input');
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('suggested_values');
     });
 
     it("should add an OR condition when clicking on add OR", async function() {
-        expect.screenshot("add_new_or_condition").to.be.captureSelector(selectorsToCapture, function (page) {
-            page.click('.segmentEditorPanel .segment-add-or');
-        }, done);
+        await page.click('.segmentEditorPanel .segment-add-or');
+        await page.waitForNetworkIdle();
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('add_new_or_condition');
     });
 
     it("should add an OR condition when a segment dimension is selected in the OR placeholder section", async function() {
-        expect.screenshot("drag_or_condition").to.be.captureSelector(selectorsToCapture, function (page) {
-            selectDimension(page, '.segmentRow0 .segment-row:last', 'Actions', 'Clicked URL');
-        }, done);
+        await selectDimension('.segmentRow0 .segment-row:last', 'Actions', 'Clicked URL');
+        await page.waitForNetworkIdle();
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('drag_or_condition');
     });
 
     it("should add an AND condition when clicking on add AND", async function() {
-        expect.screenshot("add_new_and_condition").to.be.captureSelector(selectorsToCapture, function (page) {
-            page.click('.segmentEditorPanel .segment-add-row');
-        }, done);
+        await page.click('.segmentEditorPanel .segment-add-row');
+        await page.waitForNetworkIdle();
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('add_new_and_condition');
     });
 
     it("should add an AND condition when a segment dimension is dragged to the AND placeholder section", async function() {
-        expect.screenshot("drag_and_condition").to.be.captureSelector(selectorsToCapture, function (page) {
-            selectDimension(page, '.segmentRow1', 'Actions', 'Clicked URL');
-        }, done);
+        await selectDimension('.segmentRow1', 'Actions', 'Clicked URL');
+        await page.waitForNetworkIdle();
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('drag_and_condition');
     });
 
     it("should save a new segment and add it to the segment list when the form is filled out and the save button is clicked", async function() {
-        expect.screenshot("saved").to.be.captureSelector(selectorsToCapture, function (page) {
-            page.evaluate(function () {
-                $('.metricValueBlock input').each(function (index) {
-                    $(this).val('value ' + index).change();
-                });
+        await page.evaluate(function () {
+            $('.metricValueBlock input').each(function (index) {
+                $(this).val('value ' + index).change();
             });
+        });
 
-            page.sendKeys('input.edit_segment_name', 'new segment');
-            page.click('.segmentRow0 .segment-or'); // click somewhere else to save new name
+        await page.type('input.edit_segment_name', 'new segment');
+        await page.click('.segmentRow0 .segment-or'); // click somewhere else to save new name
 
-            page.evaluate(function () {
-                $('button.saveAndApply').click();
-            });
+        await page.evaluate(function () {
+            $('button.saveAndApply').click();
+        });
+        await page.waitForNetworkIdle();
 
-            page.click('.segmentationContainer');
-        }, done);
+        await page.click('.segmentationContainer');
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('saved');
     });
 
     it("should show the new segment after page reload", async function() {
-        expect.screenshot("saved").to.be.captureSelector("saved_reload", selectorsToCapture, function (page) {
-            page.reload();
-            page.click('.segmentationContainer .title');
-        }, done);
+        await page.reload();
+        await page.click('.segmentationContainer .title');
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('saved');
     });
 
     it("should correctly load the new segment's details when the new segment is edited", async function() {
-        expect.screenshot("saved_details").to.be.captureSelector(selectorsToCapture, function (page) {
-            page.click('.segmentList li[data-idsegment=4] .editSegment');
-        }, done);
+        await page.click('.segmentList li[data-idsegment="4"] .editSegment');
+        await page.waitForNetworkIdle();
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('saved_details');
     });
 
     it("should correctly should show a confirmation when changing segment definition", async function() {
-        expect.screenshot("update_confirmation").to.be.captureSelector('.modal.open', function (page) {
-            page.click('.segmentEditorPanel .editSegmentName');
-            page.evaluate(function () {
-                $('input.edit_segment_name').val('').change();
-            });
-            page.sendKeys('input.edit_segment_name', 'edited segment');
-            page.click('.segmentRow0 .segment-or:first'); // click somewhere else to save new name
+        await page.click('.segmentEditorPanel .editSegmentName');
+        await page.evaluate(function () {
+            $('input.edit_segment_name').val('').change();
+        });
+        await page.type('input.edit_segment_name', 'edited segment');
+        await (await page.jQuery('.segmentRow0 .segment-or:first')).click(); // click somewhere else to save new name
 
-            selectFieldValue(page, '.segmentRow0 .segment-row:first .metricMatchBlock', 'Is not');
-            selectFieldValue(page, '.segmentRow0 .segment-row:last .metricMatchBlock', 'Is not');
-            selectFieldValue(page, '.segmentRow1 .segment-row .metricMatchBlock', 'Is not');
+        await selectFieldValue('.segmentRow0 .segment-row:first .metricMatchBlock', 'Is not');
+        await selectFieldValue('.segmentRow0 .segment-row:last .metricMatchBlock', 'Is not');
+        await selectFieldValue('.segmentRow1 .segment-row .metricMatchBlock', 'Is not');
 
-            page.evaluate(function () {
-                $('.metricValueBlock input').each(function (index) {
-                    $(this).val('new value ' + index).change();
-                });
+        await page.evaluate(function () {
+            $('.metricValueBlock input').each(function (index) {
+                $(this).val('new value ' + index).change();
             });
+        });
 
-            page.evaluate(function () {
-                $('button.saveAndApply').click();
-            });
-        }, done);
+        await page.evaluate(function () {
+            $('button.saveAndApply').click();
+        });
+        await page.waitForNetworkIdle();
+        await page.waitFor(500); // animation to show confirm
+
+        expect(await page.screenshotSelector('.modal.open')).to.matchImage('update_confirmation');
     });
 
     it("should correctly update the segment when saving confirmed", async function() {
-        expect.screenshot("updated").to.be.captureSelector(selectorsToCapture, function (page) {
-            page.click('.modal.open .modal-footer a:contains(Yes):visible');
-            page.click('.segmentationContainer');
-        }, done);
+        var elem = await page.jQuery('.modal.open .modal-footer a:contains(Yes):visible');
+        await elem.click();
+        await page.waitForNetworkIdle();
+        await page.click('.segmentationContainer');
+        await page.waitForNetworkIdle();
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('updated');
     });
 
     it("should show the updated segment after page reload", async function() {
-        expect.screenshot("updated_reload").to.be.captureSelector("updated_reload", selectorsToCapture, function (page) {
-            page.reload();
-            page.click('.segmentationContainer .title');
-        }, done);
+        await page.reload();
+        await page.click('.segmentationContainer .title');
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('updated');
     });
 
     it("should correctly load the updated segment's details when the updated segment is edited", async function() {
-        expect.screenshot("updated_details").to.be.captureSelector(selectorsToCapture, function (page) {
-            page.click('.segmentList li[data-idsegment=4] .editSegment');
-        }, done);
+        await page.click('.segmentList li[data-idsegment="4"] .editSegment');
+        await page.waitForNetworkIdle();
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('updated_details');
     });
 
     it("should correctly show delete dialog when the delete link is clicked", async function() {
-        expect.screenshot('deleted_dialog').to.be.captureSelector('.modal.open', function (page) {
-            page.evaluate(function () {
-                $('.segmentEditorPanel a.delete').click();
-            });
-        }, done);
+        await page.click('.segmentEditorPanel a.delete');
+        await page.waitFor(500); // animation
+        expect(await page.screenshotSelector('.modal.open')).to.matchImage('deleted_dialog');
     });
 
     it("should correctly remove the segment when the delete dialog is confirmed", async function() {
-        expect.screenshot('deleted').to.be.captureSelector(selectorsToCapture + ',.modal.open', function (page) {
-            page.click('.modal.open .modal-footer a:contains(Yes):visible');
+        var elem = await page.jQuery('.modal.open .modal-footer a:contains(Yes):visible');
+        await elem.click();
+        await page.waitForNetworkIdle();
+        await page.waitForSelector('.segmentationContainer .title');
 
-            page.click('.segmentationContainer .title');
-        }, done);
+        await page.click('.segmentationContainer .title');
+        expect(await page.screenshotSelector(selectorsToCapture + ',.modal.open')).to.matchImage('deleted');
     });
 
     it("should not show the deleted segment after page reload", async function() {
-        expect.screenshot('deleted').to.be.captureSelector('deleted_reload', selectorsToCapture, function (page) {
-            page.reload();
-            page.click('.segmentationContainer .title');
-        }, done);
+        await page.reload();
+        await page.waitForSelector('.segmentationContainer .title');
+
+        await page.click('.segmentationContainer .title');
+        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('deleted');
     });
 });
--- plugins/SegmentEditor/tests/UI/UnprocessedSegment_spec.js
+++ plugins/SegmentEditor/tests/UI/UnprocessedSegment_spec.js
@@ -16,17 +16,17 @@ describe("UnprocessedSegmentTest", function () {
     var customSegment = 'languageCode==fr';
     var url = '?module=CoreHome&action=index&' + generalParams + '#?' + generalParams + '&category=General_Visitors&subcategory=General_Overview';
 
-    before(function (done) {
-        testEnvironment.callApi('SegmentEditor.add', {
+    before(async function () {
+        await testEnvironment.callApi('SegmentEditor.add', {
             name: '<script>alert("testsegment");</script>',
             definition: segment,
             idSite: 1,
             autoArchive: 1,
             enableAllUsers: 1,
-        }, done);
+        });
     });
 
-    before(function () {
+    before(async function () {
         testEnvironment.configOverride.General = {
             browser_archiving_disabled_enforce: '1',
             enable_browser_archiving_triggering: '0',
@@ -37,19 +37,21 @@ describe("UnprocessedSegmentTest", function () {
         testEnvironment.save();
     });
 
-    after(function (done) {
-        testEnvironment.callApi('SegmentEditor.delete', { idSegment: 1 }, done);
+    after(async function () {
+        await testEnvironment.callApi('SegmentEditor.delete', { idSegment: 1 });
     });
 
-    it("should show a notification for unprocessed segments", function (done) {
-        expect.screenshot("unprocessed_segment").to.be.captureSelector('.pageWrap', function (page) {
-            page.load(url + '&segment=' + encodeURIComponent(segment));
-        }, done);
+    it("should show a notification for unprocessed segments", async function () {
+        await page.goto(url + '&segment=' + encodeURIComponent(segment));
+
+        pageWrap = await page.$('.pageWrap');
+        expect(await pageWrap.screenshot()).to.matchImage('unprocessed_segment');
     });
 
-    it('should not show a notification for custom segments that are not preprocessed', function (done) {
-        expect.screenshot("custom_segment").to.be.captureSelector('.pageWrap', function (page) {
-            page.load(url + '&segment=' + encodeURIComponent(customSegment));
-        }, done);
+    it('should not show a notification for custom segments that are not preprocessed', async function () {
+        await page.goto(url + '&segment=' + encodeURIComponent(customSegment));
+
+        pageWrap = await page.$('.pageWrap');
+        expect(await pageWrap.screenshot()).to.matchImage('custom_segment');
     });
 });
