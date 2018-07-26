52527ee0507967a5fcd4268fc782fb17e4e32693
diosmosis <diosmosis@users.noreply.github.com>
2018-07-26 16:39:28 -0700

    update file headers

--- plugins/UsersManager/Sql/SiteAccessFilter.php
+++ plugins/UsersManager/Sql/SiteAccessFilter.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: benakamoorthi
- * Date: 7/10/18
- * Time: 4:47 PM
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 
 namespace Piwik\Plugins\UsersManager\Sql;
--- plugins/UsersManager/Sql/UserTableFilter.php
+++ plugins/UsersManager/Sql/UserTableFilter.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: benakamoorthi
- * Date: 7/9/18
- * Time: 7:34 PM
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 
 namespace Piwik\Plugins\UsersManager\Sql;
