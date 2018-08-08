af58fc2ed6807d330ac534b85c2dc16dafacd229
diosmosis <diosmosis@users.noreply.github.com>
2018-08-08 12:38:55 -0700

    fix update

--- core/Updates/3.6.0-b4.php
+++ core/Updates/3.6.0-b4.php
@@ -32,7 +32,7 @@ class Updates_3_6_0_b4 extends Updates
     public function getMigrations(Updater $updater)
     {
         return array(
-            $this->migration->db->changeColumn('user', 'ts_password_modified', 'ts_password_modified', 'ts_password_modified TIMESTAMP NULL'),
+            $this->migration->db->changeColumn('user', 'ts_password_modified', 'ts_password_modified', 'TIMESTAMP NULL'),
             $this->migration->db->sql('UPDATE `' . Common::prefixTable('user') . '` SET ts_password_modified = NULL'),
         );
     }
