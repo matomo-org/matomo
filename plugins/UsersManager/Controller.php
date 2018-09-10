a977e87ecf20b075c8e9c0daf53469d8b6d075db
diosmosis <diosmosis@users.noreply.github.com>
2018-09-09 18:48:38 -0700

    New events + some other misc changes (#13388)
    * Add Access.modifyUserAccess event.
    
    * Add some template events & use request::process for LanguagesManager API.
    
    * Use the result of Sites.setSites in SitesManager API.
    
    * More comments for Site::setSitesFromArray().
    
    * fixing plugin test.

--- plugins/UsersManager/Controller.php
+++ plugins/UsersManager/Controller.php
@@ -376,8 +376,15 @@ class Controller extends ControllerAdmin
             $this->processPasswordChange($userLogin);
 
             LanguagesManager::setLanguageForSession($language);
-            APILanguagesManager::getInstance()->setLanguageForUser($userLogin, $language);
-            APILanguagesManager::getInstance()->set12HourClockForUser($userLogin, $timeFormat);
+
+            Request::processRequest('LanguagesManager.setLanguageForUser', [
+                'login' => $userLogin,
+                'languageCode' => $language,
+            ]);
+            Request::processRequest('LanguagesManager.set12HourClockForUser', [
+                'login' => $userLogin,
+                'use12HourClock' => $timeFormat,
+            ]);
 
             APIUsersManager::getInstance()->setUserPreference($userLogin,
                 APIUsersManager::PREFERENCE_DEFAULT_REPORT,
