5ce09984c66b0f5d76fba8a5b9609696685aa660
Burak Ozdemir <burakozdemir@protonmail.ch>
2018-10-11 20:27:34 +0300

    use mini link for Alexa, use DomXPath to filter out the global ranking instead of regex

--- plugins/SEO/Metric/Alexa.php
+++ plugins/SEO/Metric/Alexa.php
@@ -17,8 +17,9 @@ use Psr\Log\LoggerInterface;
  */
 class Alexa implements MetricsProvider
 {
-    const URL = 'http://data.alexa.com/data?cli=10&url=';
-    const LINK = 'http://www.alexa.com/siteinfo/';
+    const URL = 'https://data.alexa.com/data?cli=10&url=';
+    const URL_FALLBACK = 'https://www.alexa.com/minisiteinfo/';
+    const LINK = 'https://www.alexa.com/siteinfo/';
 
     /**
      * @var LoggerInterface
@@ -34,7 +35,6 @@ class Alexa implements MetricsProvider
     {
         try {
             $response = Http::sendHttpRequest(self::URL . urlencode($domain), $timeout = 10, @$_SERVER['HTTP_USER_AGENT']);
-
             $xml = @simplexml_load_string($response);
             $value = $xml ? NumberFormatter::getInstance()->formatNumber((int)$xml->SD->POPULARITY['TEXT']) : null;
         } catch (\Exception $e) {
@@ -53,10 +53,13 @@ class Alexa implements MetricsProvider
     private function tryFallbackMethod($domain)
     {
         try {
-            $response = Http::sendHttpRequest(self::LINK . urlencode($domain), $timeout = 10, @$_SERVER['HTTP_USER_AGENT']);
-            $response = preg_replace(array('#\s+#', '#<!--(.*?)-->#'), ' ', $response);
-            if (preg_match('#<strong class="metrics-data align-vmiddle">(.*?)<\/strong>#', $response, $p)) {
-                return NumberFormatter::getInstance()->formatNumber((int)str_replace(array(',', '.'), '', $p[1]));
+            $response = Http::sendHttpRequest(self::URL_FALLBACK . urlencode($domain), $timeout = 10, @$_SERVER['HTTP_USER_AGENT']);
+            $dom = new \DomDocument();
+            $dom->loadHTML($response);
+            $nodes = (new \DomXPath($dom))->query("//div[contains(@class, 'data')]");
+            if (isset($nodes[0]->nodeValue)) {
+                $globalRanking = (int) str_replace(array(',', '.'), '', $nodes[0]->nodeValue);
+                return NumberFormatter::getInstance()->formatNumber($globalRanking);
             }
         } catch (\Exception $e) {
             $this->logger->warning('Error while getting Alexa SEO stats via fallback method: {message}', array('message' => $e->getMessage()));
