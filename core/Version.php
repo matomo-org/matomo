5444afdf5fd130515150e4987cc39f23b5972b3d
Matthieu Aubry <mattab@users.noreply.github.com>
2018-10-18 09:32:52 +1300

    3.6.1

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    const VERSION = '3.6.1-rc1';
+    const VERSION = '3.6.1';
 
     public function isStableVersion($version)
     {
