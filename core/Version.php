a0a04f2868547f4ec8be81696742b4f059bbe8c6
Matthieu Aubry <mattab@users.noreply.github.com>
2018-11-19 14:38:59 +1300

    3.7.0-rc3

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    const VERSION = '3.7.0-rc2';
+    const VERSION = '3.7.0-rc3';
 
     public function isStableVersion($version)
     {
