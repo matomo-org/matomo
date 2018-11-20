31ed49c5d8e81f5d68a2cd561c2a51d7cc31712f
Thomas Steur <tsteur@users.noreply.github.com>
2018-11-21 09:52:21 +1300

    Prevent possible fatal during update (#13732)

--- core/Updates/3.7.0-b1.php
+++ core/Updates/3.7.0-b1.php
@@ -29,8 +29,8 @@ class Updates_3_7_0_b1 extends PiwikUpdates
     public function getMigrations(Updater $updater)
     {
         return [
-            $this->migration->db->addColumn(Model::$rawPrefix, 'evolution_graph_within_period', 'TINYINT(4) NOT NULL DEFAULT 0'),
-            $this->migration->db->addColumn(Model::$rawPrefix, 'evolution_graph_period_n', 'INT(11) NULL'),
+            $this->migration->db->addColumn('report', 'evolution_graph_within_period', 'TINYINT(4) NOT NULL DEFAULT 0'),
+            $this->migration->db->addColumn('report', 'evolution_graph_period_n', 'INT(11) NULL'),
         ];
     }
 
