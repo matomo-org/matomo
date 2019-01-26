1a58b28b85d5291b8758d60db0c353a07ec1e212
diosmosis <diosmosis@users.noreply.github.com>
2019-01-26 10:04:02 -0800

    Escaping file names.

--- core/Filesystem.php
+++ core/Filesystem.php
@@ -326,7 +326,8 @@ class Filesystem
         }
 
         if (!$success) {
-            $ex = new FailedCopyException("Error while creating/copying file from $source to <code>$dest</code>. Content of copied file is different.");
+            $ex = new FailedCopyException("Error while creating/copying file from $source to <code>" . Common::sanitizeInputValue($dest)
+                . "</code>. Content of copied file is different.");
             $ex->setIsHtmlMessage();
             throw $ex;
         }
@@ -419,7 +420,7 @@ class Filesystem
                        'B' => 1);
 
         if (!array_key_exists($unit, $units)) {
-            throw new Exception('Invalid unit given');
+            throw new \Exception('Invalid unit given');
         }
 
         if (!file_exists($pathToFile)) {
@@ -511,7 +512,7 @@ class Filesystem
         if (!@copy($source, $dest)) {
             @chmod($dest, 0755);
             if (!@copy($source, $dest)) {
-                $message = "Error while creating/copying file to <code>$dest</code>. <br />"
+                $message = "Error while creating/copying file to <code>" . Common::sanitizeInputValue($dest) . "</code>. <br />"
                     . Filechecks::getErrorMessageMissingPermissions(self::getPathToPiwikRoot());
                 $ex = new FailedCopyException($message);
                 $ex->setIsHtmlMessage();
