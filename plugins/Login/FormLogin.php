06ff7d6b27659c3ddefd8868a15dcc86708c9d33
diosmosis <diosmosis@users.noreply.github.com>
2019-02-08 11:41:22 -0800

    Use actual login plugin name.

--- plugins/Login/FormLogin.php
+++ plugins/Login/FormLogin.php
@@ -19,7 +19,7 @@ class FormLogin extends QuickForm2
 {
     function __construct($id = 'login_form', $method = 'post', $attributes = null, $trackSubmit = false)
     {
-        $attributes = array_merge($attributes ?: [], [ 'action' => '?module=Login' ]);
+        $attributes = array_merge($attributes ?: [], [ 'action' => '?module=' . Piwik::getLoginPluginName() ]);
         parent::__construct($id, $method, $attributes, $trackSubmit);
     }
 
