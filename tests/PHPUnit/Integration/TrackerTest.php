a0b260c00303c288fd1aeaa6f44db3f185608210
Thomas Steur <tsteur@users.noreply.github.com>
2019-02-25 22:46:59 +1300

    Noindex, nofollow for login page and tracker default output (#14121)
    * Noindex, nofollow for login page
    
    * Update Response.php
    
    * update tests
    
    * update UI files

--- tests/PHPUnit/Integration/TrackerTest.php
+++ tests/PHPUnit/Integration/TrackerTest.php
@@ -250,7 +250,7 @@ class TrackerTest extends IntegrationTestCase
 
         $response = $this->tracker->main($this->getDefaultHandler(), $requestSet);
 
-        $expected = "This resource is part of Matomo. Keep full control of your data with the leading free and open source <a href='https://matomo.org' target='_blank' rel='noopener noreferrer'>web analytics & conversion optimisation platform</a>.";
+        $expected = "This resource is part of Matomo. Keep full control of your data with the leading free and open source <a href='https://matomo.org' target='_blank' rel='noopener noreferrer nofollow'>web analytics & conversion optimisation platform</a>.";
         $this->assertEquals($expected, $response);
     }
 
