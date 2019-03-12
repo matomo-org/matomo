7f2c73315ea897e865dd8a941a4f406e4121ce1a
diosmosis <diosmosis@users.noreply.github.com>
2019-03-11 22:20:17 -0700

    Move cron archiving diagnostics next to each other.

--- plugins/Diagnostics/config/config.php
+++ plugins/Diagnostics/config/config.php
@@ -25,11 +25,11 @@ return array(
         DI\get('Piwik\Plugins\Diagnostics\Diagnostic\RecommendedFunctionsCheck'),
         DI\get('Piwik\Plugins\Diagnostics\Diagnostic\NfsDiskCheck'),
         DI\get('Piwik\Plugins\Diagnostics\Diagnostic\CronArchivingCheck'),
+        DI\get(CronArchivingLastRunCheck::class),
         DI\get('Piwik\Plugins\Diagnostics\Diagnostic\LoadDataInfileCheck'),
         Di\get('Piwik\Plugins\Diagnostics\Diagnostic\DbOverSSLCheck'),
         Di\get('Piwik\Plugins\Diagnostics\Diagnostic\DbMaxPacket'),
         Di\get('Piwik\Plugins\Diagnostics\Diagnostic\ForceSSLCheck'),
-        DI\get(CronArchivingLastRunCheck::class),
     ),
     // Allows other plugins to disable diagnostics that were previously registered
     'diagnostics.disabled' => array(),
