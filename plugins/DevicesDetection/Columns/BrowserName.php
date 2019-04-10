beff637e0ec74154b81ca18de50f7c9d30092e8e
Stefan Giehl <stefan@matomo.org>
2019-04-10 21:59:31 +0200

    Readable segment values for browser, os and country segments (#13929)
    * Use readable segment value for browser and os segments instead of short codes
    
    * readds original segments using short codes
    
    * Adds additional segment country name to segment by (english) country name
    
    * compare browser and os names case sensitive for segments
    
    * use correct segments for reports
    
    * update tests

--- plugins/DevicesDetection/Columns/BrowserName.php
+++ plugins/DevicesDetection/Columns/BrowserName.php
@@ -8,7 +8,10 @@
  */
 namespace Piwik\Plugins\DevicesDetection\Columns;
 
+use DeviceDetector\Parser\Client\Browser;
+use Piwik\Common;
 use Piwik\Metrics\Formatter;
+use Piwik\Plugin\Segment;
 use Piwik\Tracker\Request;
 use Piwik\Tracker\Visitor;
 use Piwik\Tracker\Action;
@@ -17,11 +20,44 @@ class BrowserName extends Base
 {
     protected $columnName = 'config_browser_name';
     protected $columnType = 'VARCHAR(10) NULL';
-    protected $segmentName = 'browserCode';
+    protected $segmentName = 'browserName';
     protected $nameSingular = 'DevicesDetection_ColumnBrowser';
     protected $namePlural = 'DevicesDetection_Browsers';
-    protected $acceptValues = 'FF, IE, CH, SF, OP, etc.';
+    protected $acceptValues = 'FireFox, Internet Explorer, Chrome, Safari, Opera etc.';
     protected $type = self::TYPE_TEXT;
+
+    public function __construct()
+    {
+        $this->sqlFilterValue = function ($val) {
+            $browsers = Browser::getAvailableBrowsers();
+            array_map(function($val) {
+                return Common::mb_strtolower($val);
+            }, $browsers);
+            $result   = array_search(Common::mb_strtolower($val), $browsers);
+
+            if ($result === false) {
+                $result = 'UNK';
+            }
+
+            return $result;
+        };
+        $this->suggestedValuesCallback = function ($idSite, $maxValuesToReturn) {
+            return array_values(Browser::getAvailableBrowsers() + ['Unknown']);
+        };
+    }
+
+    protected function configureSegments()
+    {
+        parent::configureSegments();
+
+        $segment = new Segment();
+        $segment->setSegment('browserCode');
+        $segment->setName('DevicesDetection_BrowserCode');
+        $segment->setAcceptedValues('FF, IE, CH, SF, OP etc.');
+        $this->suggestedValuesCallback = null;
+        $this->sqlFilterValue = null;
+        $this->addSegment($segment);
+    }
 
     public function formatValue($value, $idSite, Formatter $formatter)
     {
