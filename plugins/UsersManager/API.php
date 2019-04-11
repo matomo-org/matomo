843bf1b2a961a75839a7d762aab8283f8bb47dda
Thomas Steur <tsteur@users.noreply.github.com>
2019-04-11 15:15:11 +1200

    Update API.php

--- plugins/UsersManager/API.php
+++ plugins/UsersManager/API.php
@@ -853,7 +853,7 @@ class API extends \Piwik\Plugin\API
                                $_isPasswordHashed = false, $passwordConfirmation = false)
     {
         $requirePasswordConfirmation = self::$UPDATE_USER_REQUIRE_PASSWORD_CONFIRMATION;
-        $isEmailNotificationOnInConfig = Config::getInstance()->General['api_update_users_email_notifications'];
+        $isEmailNotificationOnInConfig = Config::getInstance()->General['enable_update_users_email'];
         self::$UPDATE_USER_REQUIRE_PASSWORD_CONFIRMATION = true;
 
         Piwik::checkUserHasSuperUserAccessOrIsTheUser($userLogin);
