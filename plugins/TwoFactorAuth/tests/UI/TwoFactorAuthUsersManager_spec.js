d5f199f0261ebe9ec70d687fdb379563916fc232
diosmosis <diosmosis@users.noreply.github.com>
2019-05-01 18:34:01 -0700

    typo

--- plugins/TwoFactorAuth/tests/UI/TwoFactorAuthUsersManager_spec.js
+++ plugins/TwoFactorAuth/tests/UI/TwoFactorAuthUsersManager_spec.js
@@ -44,7 +44,7 @@ describe("TwoFactorAuthUsersManager", function () {
         await page.click('.userEditForm .twofa-reset .resetTwoFa .btn');
         await page.waitFor(500);
         const modal = await page.$('.modal.open');
-        expect(await modal.selector()).to.matchImage('edit_with_2fa_reset_confirm');
+        expect(await modal.screenshot()).to.matchImage('edit_with_2fa_reset_confirm');
     });
 
     it('should be possible to confirm the reset', async function () {
