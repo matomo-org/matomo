098f0ca1b14c7e9b1478e7e6edc26da49b9a1ca0
Thomas Steur <tsteur@users.noreply.github.com>
2019-06-28 06:50:46 +0200

    use Common::mb_substr

--- core/DeviceDetectorFactory.php
+++ core/DeviceDetectorFactory.php
@@ -9,6 +9,7 @@
 namespace Piwik;
 
 use DeviceDetector\DeviceDetector;
+use Piwik\Common;
 
 class DeviceDetectorFactory
 {
@@ -21,7 +22,7 @@ class DeviceDetectorFactory
      */
     public static function getInstance($userAgent)
     {
-        $userAgent = substr($userAgent, 0, 500);
+        $userAgent = Common::mb_substr($userAgent, 0, 500);
 
         if (array_key_exists($userAgent, self::$deviceDetectorInstances)) {
             return self::$deviceDetectorInstances[$userAgent];
