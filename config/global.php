2eaa4034a79ae0b08f8444c0d867e9ba17b93517
diosmosis <diosmosis@users.noreply.github.com>
2019-07-11 17:37:58 -0700

    If new visit is forced, known visitors should still be recognized (#14491)
    * Enforcing a new visit should not result in the visitor becoming unrecognized.
    
    * Adding test for bug.
    
    * update expected test files
    
    * Fix integration test.
    
    * Update submodule
    
    * Update test files.

--- config/global.php
+++ config/global.php
@@ -211,8 +211,7 @@ return array(
     'Piwik\Tracker\VisitorRecognizer' => DI\object()
         ->constructorParameter('trustCookiesOnly', DI\get('ini.Tracker.trust_visitors_cookies'))
         ->constructorParameter('visitStandardLength', DI\get('ini.Tracker.visit_standard_length'))
-        ->constructorParameter('lookbackNSecondsCustom', DI\get('ini.Tracker.window_look_back_for_visitor'))
-        ->constructorParameter('trackerAlwaysNewVisitor', DI\get('ini.Debug.tracker_always_new_visitor')),
+        ->constructorParameter('lookbackNSecondsCustom', DI\get('ini.Tracker.window_look_back_for_visitor')),
 
     'Piwik\Tracker\Settings' => DI\object()
         ->constructorParameter('isSameFingerprintsAcrossWebsites', DI\get('ini.Tracker.enable_fingerprinting_across_websites')),
