e9fef5e3800173a738462bb88fac6e1ccf7224d6
Lukas Winkler <git@lw1.at>
2019-07-14 22:08:40 +0200

    send Referrer-Policy header (#14482)
    * send Referrer-Policy header
    
    * use same-origin instead
    
    sending the full referrer to the same origin, but no referrer for others

--- core/View.php
+++ core/View.php
@@ -281,6 +281,8 @@ class View implements ViewInterface
         if(!empty($this->xFrameOptions)) {
             Common::sendHeader('X-Frame-Options: ' . (string)$this->xFrameOptions);
         }
+        // don't send Referer-Header for outgoing links
+        Common::sendHeader('Referrer-Policy: same-origin');
 
         return $this->renderTwigTemplate();
     }
