f6e5037f11554a3189ed304c2d6f2e8f2824c88b
sgiehl <stefan@matomo.org>
2019-07-15 00:35:14 +0200

    Fix some failing UI tests

--- plugins/Dashboard/tests/UI/Dashboard_spec.js
+++ plugins/Dashboard/tests/UI/Dashboard_spec.js
@@ -262,6 +262,7 @@ describe("Dashboard", function () {
         await page.click('li[data-action="createDashboard"]');
         await page.waitForSelector('#createDashboardName'); // await animation
         await page.type('#createDashboardName', 'newdash2');
+        await page.waitFor(100);
         var button = await page.jQuery('.modal.open .modal-footer a:contains(Ok)');
         await button.click();
         await page.mouse.move(-10, -10);
