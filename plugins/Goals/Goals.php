8a110aaac4c6cf687d5c3f28069fc97c7910f26f
Thomas Steur <tsteur@users.noreply.github.com>
2019-07-29 14:19:36 +1200

    Update Goals.php

--- plugins/Goals/Goals.php
+++ plugins/Goals/Goals.php
@@ -368,6 +368,6 @@ class Goals extends \Piwik\Plugin
         $translationKeys[] = 'Goals_Ecommerce';
         $translationKeys[] = 'Goals_Optional';
         $translationKeys[] = 'Goals_TimeInMinutes';
-        $translationKeys[] = 'Goals_Patterns';
+        $translationKeys[] = 'Goals_Pattern';
     }
 }
