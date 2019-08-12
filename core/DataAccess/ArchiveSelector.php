74126bd8e19306f4849bc72792ac040ceae02c52
Thomas Steur <tsteur@users.noreply.github.com>
2019-08-12 17:17:58 +1200

    no longer use reader to get archives

--- core/DataAccess/ArchiveSelector.php
+++ core/DataAccess/ArchiveSelector.php
@@ -138,7 +138,6 @@ class ArchiveSelector
      * @param array $periods
      * @param Segment $segment
      * @param array $plugins List of plugin names for which data is being requested.
-     * @param bool $canUseReaderDb if enabled, will try to read archive from reader
      * @return array Archive IDs are grouped by archive name and period range, ie,
      *               array(
      *                   'VisitsSummary.done' => array(
@@ -147,7 +146,7 @@ class ArchiveSelector
      *               )
      * @throws
      */
-    public static function getArchiveIds($siteIds, $periods, $segment, $plugins, $canUseReaderDb)
+    public static function getArchiveIds($siteIds, $periods, $segment, $plugins)
     {
         if (empty($siteIds)) {
             throw new \Exception("Website IDs could not be read from the request, ie. idSite=");
@@ -174,11 +173,7 @@ class ArchiveSelector
             $monthToPeriods[$table][] = $period;
         }
 
-        if ($canUseReaderDb) {
-            $db = Db::getReader();
-        } else {
-            $db = Db::get();
-        }
+        $db = Db::get();
 
         // for every month within the archive query, select from numeric table
         $result = array();
@@ -235,19 +230,14 @@ class ArchiveSelector
      * @param string $archiveDataType The archive data type (either, 'blob' or 'numeric').
      * @param int|null|string $idSubtable  null if the root blob should be loaded, an integer if a subtable should be
      *                                     loaded and 'all' if all subtables should be loaded.
-     * @param bool $canUseReaderDb if enabled, will try to read archive from reader DB
      * @return array
      *@throws Exception
      */
-    public static function getArchiveData($archiveIds, $recordNames, $archiveDataType, $idSubtable, $canUseReaderDb)
+    public static function getArchiveData($archiveIds, $recordNames, $archiveDataType, $idSubtable)
     {
         $chunk = new Chunk();
 
-        if ($canUseReaderDb) {
-            $db = Db::getReader();
-        } else {
-            $db = Db::get();
-        }
+        $db = Db::get();
 
         // create the SQL to select archive data
         $loadAllSubtables = $idSubtable == Archive::ID_SUBTABLE_LOAD_ALL_SUBTABLES;
