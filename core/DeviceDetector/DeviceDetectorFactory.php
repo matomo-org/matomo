f78efc5aafb51028340aacb4558063c1ad22a4c6
katebutler <kate@innocraft.com>
2019-08-14 10:00:26 +1200

    Move device detector cache into separate plugin

--- core/DeviceDetector/DeviceDetectorFactory.php
+++ core/DeviceDetector/DeviceDetectorFactory.php
@@ -16,12 +16,23 @@ class DeviceDetectorFactory
     protected static $deviceDetectorInstances = array();
 
     /**
-     * Returns a Singleton instance of DeviceDetector for the given user agent
+     * Returns a Singleton instance of DeviceDetector for the given user agent.
      * @param string $userAgent
-     * @param bool $useFileCache
      * @return DeviceDetector
+     * @deprecated Should get a factory via StaticContainer and call makeInstance() on it instead
      */
-    public static function getInstance($userAgent, $useFileCache = true)
+    public static function getInstance($userAgent)
+    {
+        return (new DeviceDetectorFactory())->makeInstance($userAgent);
+    }
+
+    /**
+     * Returns an instance of DeviceDetector for the given user agent. Uses template method pattern
+     * and calls getDeviceDetectionInfo() when it doesn't find a matching instance in the cache.
+     * @param string $userAgent
+     * @return DeviceDetector|mixed
+     */
+    public function makeInstance($userAgent)
     {
         $userAgent = Common::mb_substr($userAgent, 0, 500);
 
@@ -29,17 +40,25 @@ class DeviceDetectorFactory
             return self::$deviceDetectorInstances[$userAgent];
         }
 
-        if ($useFileCache && DeviceDetectorCacheEntry::isCached($userAgent)) {
-            $deviceDetector = new DeviceDetectorCacheEntry($userAgent);
-        } else {
-            $deviceDetector = new DeviceDetector($userAgent);
-            $deviceDetector->discardBotInformation();
-            $deviceDetector->setCache(new DeviceDetectorCache(86400));
-            $deviceDetector->parse();
-        }
+        $deviceDetector = $this->getDeviceDetectionInfo($userAgent);
 
         self::$deviceDetectorInstances[$userAgent] = $deviceDetector;
 
+        return $deviceDetector;
+    }
+
+    /**
+     * Creates a new DeviceDetector for the user agent. Called by makeInstance() when no matching instance
+     * was found in the cache.
+     * @param $userAgent
+     * @return DeviceDetector
+     */
+    protected function getDeviceDetectionInfo($userAgent)
+    {
+        $deviceDetector = new DeviceDetector($userAgent);
+        $deviceDetector->discardBotInformation();
+        $deviceDetector->setCache(new DeviceDetectorCache(86400));
+        $deviceDetector->parse();
         return $deviceDetector;
     }
 
