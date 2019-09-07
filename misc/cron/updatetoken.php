06be8b58de8aa015d331e51aedbe657dfddd617e
Thomas Steur <tsteur@users.noreply.github.com>
2019-09-08 08:35:40 +1200

    fix error

--- misc/cron/updatetoken.php
+++ misc/cron/updatetoken.php
@@ -23,7 +23,7 @@ define('PIWIK_ENABLE_DISPATCH', false);
 define('PIWIK_ENABLE_ERROR_HANDLER', false);
 define('PIWIK_ENABLE_SESSION_START', false);
 
-require_once PIWIK_INCLUDE_PATH . "/index.php";
+require_once PIWIK_DOCUMENT_ROOT . "/index.php";
 
 if (!Common::isPhpCliMode()) {
     return;
