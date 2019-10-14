e86968b23507261f58a3f0e9f20d61d0e47ae331
Thomas Steur <tsteur@users.noreply.github.com>
2019-10-15 10:00:47 +1300

    Revert "no longer include Js in twig file (#14994)" (#15004)
    This reverts commit 425654523dc835f1530a95b0a90fb5a7459198b1.

--- plugins/SEO/javascripts/rank.js
+++ plugins/SEO/javascripts/rank.js
@@ -4,8 +4,9 @@
  * @link http://piwik.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
-var SEOWidget = {
-    getRank: function () {
+
+$(document).ready(function () {
+    function getRank() {
         var ajaxRequest = new ajaxHelper();
         ajaxRequest.setLoadingElement('#ajaxLoadingSEO');
         ajaxRequest.addParams({
@@ -21,4 +22,10 @@ var SEOWidget = {
         ajaxRequest.setFormat('html');
         ajaxRequest.send();
     }
-}
+
+    // click on Rank button
+    $('#rankbutton').on('click', function () {
+        getRank();
+        return false;
+    });
+});
