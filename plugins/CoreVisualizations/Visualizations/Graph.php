matomo-org/matomo

commit b6683b5b60d6a98cc063a0ea61aae1445a7e39e1

Hide unavailable metrics in graphs (#14996)

plugins/CoreVisualizations/Visualizations/Graph.php
@@ -8,6 +8,7 @@
  */
 namespace Piwik\Plugins\CoreVisualizations\Visualizations;
 
+use Piwik\Common;
 use Piwik\DataTable;
 use Piwik\DataTable\Row;
 use Piwik\Plugin\Metric;
@@ -15,6 +16,7 @@ use Piwik\Plugins\AbTesting\Columns\Metrics\ProcessedMetric;
 use Piwik\Plugins\CoreVisualizations\Metrics\Formatter\Numeric;
 use Piwik\Piwik;
 use Piwik\Plugin\Visualization;
+use Piwik\SettingsPiwik;
 
 /**
  * This is an abstract visualization that should be the base of any 'graph' visualization.
@@ -236,7 +238,7 @@ abstract class Graph extends Visualization
             $allColumns = $this->getDefaultColumnsToDisplay();
         }
 
-        $this->config->columns_to_display = array_intersect($columnsToDisplay, $allColumns);
+        $this->config->columns_to_display = $this->removeUnavailableMetrics(array_intersect($columnsToDisplay, $allColumns));
     }
 
     private function getDefaultColumnsToDisplay()
@@ -247,5 +249,16 @@ abstract class Graph extends Visualization
             'nb_uniq_visitors',
             'nb_users'
         );
+    }
+
+    private function removeUnavailableMetrics($metrics)
+    {
+        $currentPeriod = Common::getRequestVar('period', false);
+
+        if (!SettingsPiwik::isUniqueVisitorsEnabled($currentPeriod)) {
+            $metrics = array_diff($metrics, ['nb_uniq_visitors', 'nb_users']);
+        }
+
+        return $metrics;
     }
 }

plugins/CoreVisualizations/Visualizations/JqplotGraph/Evolution.php
@@ -238,4 +238,14 @@ class Evolution extends JqplotGraph
     {
         return true;
     }
+
+    protected function ensureValidColumnsToDisplay()
+    {
+        parent::ensureValidColumnsToDisplay();
+
+        $columnsToDisplay = $this->config->columns_to_display;
+
+        // Use a sensible default if the columns_to_display is empty
+        $this->config->columns_to_display = $columnsToDisplay ? : array('nb_visits');
+    }
 }

commit 932ca01e5d07634ab8594638fe0d25dc9b0f8c81

Request different columns when no columns will be available
refs https://github.com/matomo-org/matomo/issues/15033 
it might fix that issue

fixes a regression from https://github.com/matomo-org/matomo/pull/14996

When you have the Visits over time for example in the dashboard, view day period, select unique visitors, then select year period, it will then show visits metric instead of unique visitors because unique visitors is not available for year. The visits metric will not show any data though because it had the `&columns=nb_unique_visitors` param applied and the visits metric was never fetched. That's because in https://github.com/matomo-org/matomo/pull/14996 the metric was replaced after the dataTable was being requested. Trying to do this now before requesting dataTable too. Not sure what other regressions this could potentially introduce...

plugins/CoreVisualizations/Visualizations/Graph.php
@@ -73,6 +73,19 @@ abstract class Graph extends Visualization
             $this->requestConfig->request_parameters_to_modify['keep_totals_row_label'] = Piwik::translate('General_Total');
         }
 
+        if (!empty($this->config->columns_to_display)) {
+            $metrics = $this->removeUnavailableMetrics($this->config->columns_to_display);
+            if (empty($metrics)) {
+                if (!empty($this->config->selectable_columns)) {
+                    $this->config->columns_to_display = array(reset($this->config->selectable_columns));
+                } else {
+                    $this->config->columns_to_display = array('nb_visit');
+                }
+                $this->requestConfig->request_parameters_to_modify['columns'] = 'nb_visits';
+                $this->requestConfig->request_parameters_to_modify['columns_to_display'] = 'nb_visits';
+            }
+        }
+
         $this->metricsFormatter = new Numeric();
     }
 