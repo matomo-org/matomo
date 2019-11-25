7b051503a15b52d033592abdf5e727c99e934a2e
Thomas Steur <tsteur@users.noreply.github.com>
2019-11-26 09:39:47 +1300

    Update LogAggregator.php

--- core/DataAccess/LogAggregator.php
+++ core/DataAccess/LogAggregator.php
@@ -366,9 +366,10 @@ class LogAggregator
             $query['sql'] = 'SELECT /* ' . $this->queryOriginHint . ' */' . substr($query['sql'], strlen($select));
         }
 
-        if (!$segment->isEmpty() && is_array($query) && 0 === strpos(trim($query['sql']), $select)) {
+        if (empty($bind) && is_array($query) && 0 === strpos(trim($query['sql']), $select)) {
+            // bind is empty when a segment table was applied...
             $query['sql'] = trim($query['sql']);
-            $query['sql'] = 'SELECT /* ' . $this->dateStart->toString() . ',' . $this->dateEnd->toString() . ':' . implode(array_map('intval', $this->sites)). ' */' . substr($query['sql'], strlen($select));
+            $query['sql'] = 'SELECT /* ' . $this->dateStart->toString() . ',' . $this->dateEnd->toString() . ' ' . implode(',', array_map('intval', $this->sites)). ' */' . substr($query['sql'], strlen($select));
         }
  
     	// Log on DEBUG level all SQL archiving queries
