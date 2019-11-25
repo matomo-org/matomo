2002903f53c54804a3547f13c6240163858b5d0b
Thomas Steur <tsteur@users.noreply.github.com>
2019-11-26 09:45:24 +1300

    add segment hash...

--- core/DataAccess/LogAggregator.php
+++ core/DataAccess/LogAggregator.php
@@ -366,10 +366,9 @@ class LogAggregator
             $query['sql'] = 'SELECT /* ' . $this->queryOriginHint . ' */' . substr($query['sql'], strlen($select));
         }
 
-        if (empty($bind) && is_array($query) && 0 === strpos(trim($query['sql']), $select)) {
-            // bind is empty when a segment table was applied...
+        if (!$this->getSegment()->isEmpty() && is_array($query) && 0 === strpos(trim($query['sql']), $select)) {
             $query['sql'] = trim($query['sql']);
-            $query['sql'] = 'SELECT /* ' . $this->dateStart->toString() . ',' . $this->dateEnd->toString() . ' ' . implode(',', array_map('intval', $this->sites)). ' */' . substr($query['sql'], strlen($select));
+            $query['sql'] = 'SELECT /* ' . $this->dateStart->toString() . ',' . $this->dateEnd->toString() . ' sites ' . implode(',', array_map('intval', $this->sites)) . ' segmenthash ' . $this->getSegment()->getHash(). ' */' . substr($query['sql'], strlen($select));
         }
  
     	// Log on DEBUG level all SQL archiving queries
