aa9139f9b48830c74092d482c3876f3c80c9b4f7
Thomas Steur <tsteur@users.noreply.github.com>
2019-12-03 07:18:00 +1300

    remove tabs

--- core/DataTable/Filter/ColumnDelete.php
+++ core/DataTable/Filter/ColumnDelete.php
@@ -161,13 +161,13 @@ class ColumnDelete extends BaseFilter
             }
             foreach ($this->columnsToRemove as $column) {
                 if (is_array($row )) {
-                	if (!array_key_exists($column, $row)) {
-		                continue;
-	                }
+                    if (!array_key_exists($column, $row)) {
+                        continue;
+                    }
                 } else {
-                	if (!property_exists($row, $column)) {
-		                continue;
-	                }
+                    if (!property_exists($row, $column)) {
+                        continue;
+                    }
                 }
                 if ($this->deleteIfZeroOnly) {
                     $value = $row[$column];
