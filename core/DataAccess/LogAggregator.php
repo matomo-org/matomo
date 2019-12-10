d150c5e3e3b4cea7bdc32e3a4fc6dec49095ea53
Thomas Steur <tsteur@users.noreply.github.com>
2019-12-11 12:40:02 +1300

    Add more debug info to archiving queries (#15196)
    * Add more debug info to archiving queries
    
    Otherwise it is hard to find out  for what  period etc the query was. Ideally we would also make the segmentDecorator work somehow but not having enough time right now. As we otherwise can't know which segment was applied because we can only see the
    
    * Update LogAggregator.php
    
    * add segment hash...

--- core/DataAccess/LogAggregator.php
+++ core/DataAccess/LogAggregator.php
@@ -366,6 +366,11 @@ class LogAggregator
             $query['sql'] = 'SELECT /* ' . $this->queryOriginHint . ' */' . substr($query['sql'], strlen($select));
         }
 
+        if (!$this->getSegment()->isEmpty() && is_array($query) && 0 === strpos(trim($query['sql']), $select)) {
+            $query['sql'] = trim($query['sql']);
+            $query['sql'] = 'SELECT /* ' . $this->dateStart->toString() . ',' . $this->dateEnd->toString() . ' sites ' . implode(',', array_map('intval', $this->sites)) . ' segmenthash ' . $this->getSegment()->getHash(). ' */' . substr($query['sql'], strlen($select));
+        }
+ 
     	// Log on DEBUG level all SQL archiving queries
         $this->logger->debug($query['sql']);
 
