a511c2672dc4c42d44641b71fd8a13c2c7200c27
katebutler <kate@innocraft.com>
2019-12-18 14:52:47 +1300

    Database migration

--- core/Updates/4.0.0-b1.php
+++ core/Updates/4.0.0-b1.php
@@ -50,6 +50,13 @@ class Updates_4_0_0_b1 extends PiwikUpdates
             $migrations[] = $this->migration->plugin->activate('CustomJsTracker');
         }
 
+        // Move the site search fields of log_visit out of custom variables into their own fields
+        $migrations[] = $this->migration->db->addColumn('log_link_visit_action', 'search_cat', 'VARCHAR(200)');
+        $migrations[] = $this->migration->db->addColumn('log_link_visit_action', 'search_count', 'VARCHAR(200)');
+        $visitActionTable = Common::prefixTable('log_link_visit_action');
+        $migrations[] = $this->migration->db->boundSql("UPDATE $visitActionTable SET search_cat = custom_var_v4 WHERE custom_var_k4 = '_pk_scat'");
+        $migrations[] = $this->migration->db->boundSql("UPDATE $visitActionTable SET search_count = custom_var_v5 WHERE custom_var_k4 = '_pk_scount'");
+
         if ($this->usesGeoIpLegacyLocationProvider()) {
             // activate GeoIp2 plugin for users still using GeoIp2 Legacy (others might have it disabled on purpose)
             $migrations[] = $this->migration->plugin->activate('GeoIp2');
