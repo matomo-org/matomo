3c2b5f714f85457796e3e190781d3e592822ebf7
diosmosis <diosmosis@users.noreply.github.com>
2020-01-08 00:05:41 -0800

    Do not proceed with archiving if a valid archive exists for the parameter combination. (#14937)
    * Do not proceed with archiving if a valid archive exists for the parameter combination.
    
    * test manually and get to work
    
    * Apply some review feedback.
    
    * apply more review feedback
    
    * Do not abort archiving if archive found for non-day period so segments will still archive.
    
    * Apply more review feedback.
    
    * Make new optimization aware of whole periods.
    
    * update submodule
    
    * In CronArchive modify lastN date parameter to use oldest invalidated archive.
    
    * Move date changing logic to method and unit test.
    
    * Fix test.

--- core/ArchiveProcessor/Rules.php
+++ core/ArchiveProcessor/Rules.php
@@ -291,13 +291,15 @@ class Rules
     /**
      * Returns done flag values allowed to be selected
      *
-     * @return string
+     * @return string[]
      */
-    public static function getSelectableDoneFlagValues()
+    public static function getSelectableDoneFlagValues($includeInvalidated = true)
     {
         $possibleValues = array(ArchiveWriter::DONE_OK, ArchiveWriter::DONE_OK_TEMPORARY);
 
-        if (!Rules::isRequestAuthorizedToArchive()) {
+        if (!Rules::isRequestAuthorizedToArchive()
+            && $includeInvalidated
+        ) {
             //If request is not authorized to archive then fetch also invalidated archives
             $possibleValues[] = ArchiveWriter::DONE_INVALIDATED;
         }
