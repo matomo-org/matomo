bee0186e559a1bcaa3d16d51d3c65eb063a29818
Thomas Steur <tsteur@users.noreply.github.com>
2020-01-21 09:32:27 +1300

    fix tests

--- tests/PHPUnit/System/BackwardsCompatibility1XTest.php
+++ tests/PHPUnit/System/BackwardsCompatibility1XTest.php
@@ -119,6 +119,9 @@ class BackwardsCompatibility1XTest extends SystemTestCase
             // those reports generate a different segment as a different raw value was stored that time
             'DevicesDetection.getOsVersions',
             'DevicesDetection.getBrowserFamilies',
+            'DevicesDetection.getBrowserVersions',
+            'DevicesDetection.getBrowserEngines',
+            'DevicesDetection.getBrowsers',
             'Goals.get',
 
             // Following #9345
