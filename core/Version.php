baaca8b2bbf1eecb283e45f6347dfd3c3274b818
Matthieu Aubry <mattab@users.noreply.github.com>
2020-02-10 16:16:44 +1300

    3.13.2

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    const VERSION = '3.13.2-rc2';
+    const VERSION = '3.13.2';
 
     public function isStableVersion($version)
     {
