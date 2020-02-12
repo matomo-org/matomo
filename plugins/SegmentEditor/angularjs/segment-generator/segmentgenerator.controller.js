924c9e19a1caaf46b8d29a431c14379579aa547b
Thomas Steur <tsteur@users.noreply.github.com>
2020-02-12 21:02:33 +1300

    Empty auto complete results in segment editor when changing the segment (#15556)
    Avoids seeing results from previous segment while the new list of suggestion is not loaded yet

--- plugins/SegmentEditor/angularjs/segment-generator/segmentgenerator.controller.js
+++ plugins/SegmentEditor/angularjs/segment-generator/segmentgenerator.controller.js
@@ -149,7 +149,13 @@
             orCondition.isLoading = true;
 
             this.updateSegmentDefinition();
-
+            
+            var inputElement = $('.orCondId' + orCondition.id + " .metricValueBlock input");
+            inputElement.autocomplete({
+                source: [],
+                minLength: 0
+            });
+            
             var resolved = false;
 
             var promise = piwikApi.fetch({
