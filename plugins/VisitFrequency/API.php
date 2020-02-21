b25fcee82367e2b6e0df148743591eba1fb8518b
Stefan Giehl <stefan@matomo.org>
2020-02-21 09:22:28 +0100

    Fix VisitorFrequency.get API for idsites=all (#15601)

--- plugins/VisitFrequency/API.php
+++ plugins/VisitFrequency/API.php
@@ -49,7 +49,10 @@ class API extends \Piwik\Plugin\API
         $columns = Piwik::getArrayFromApiParameter($columns);
 
         /** @var \Piwik\DataTable\DataTableInterface $resultSet */
-        if (Period::isMultiplePeriod($date, $period)) {
+        if ($idSite === 'all') {
+            $resultSet = new DataTable\Map();
+            $resultSet->setKeyName('idSite');
+        } else if (Period::isMultiplePeriod($date, $period)) {
             $resultSet = new DataTable\Map();
             $resultSet->setKeyName('period');
         } else {
