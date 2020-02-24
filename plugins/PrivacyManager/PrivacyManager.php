b0913cb1dbe5e2ef3d1fd4175e735910f9973609
sgiehl <stefan@matomo.org>
2020-02-24 13:46:33 +0100

    Makes gdpr tools directive translatable

--- plugins/PrivacyManager/PrivacyManager.php
+++ plugins/PrivacyManager/PrivacyManager.php
@@ -195,6 +195,14 @@ class PrivacyManager extends Plugin
     public function getClientSideTranslationKeys(&$translationKeys)
     {
         $translationKeys[] = 'General_Visit';
+        $translationKeys[] = 'General_Action';
+        $translationKeys[] = 'General_Details';
+        $translationKeys[] = 'General_VisitId';
+        $translationKeys[] = 'General_VisitorID';
+        $translationKeys[] = 'General_VisitorIP';
+        $translationKeys[] = 'General_UserId';
+        $translationKeys[] = 'General_Website';
+        $translationKeys[] = 'Live_ViewVisitorProfile';
         $translationKeys[] = 'CoreAdminHome_SettingsSaveSuccess';
         $translationKeys[] = 'CoreAdminHome_OptOutExplanation';
         $translationKeys[] = 'CoreAdminHome_OptOutExplanationIntro';
@@ -221,6 +229,28 @@ class PrivacyManager extends Plugin
         $translationKeys[] = 'PrivacyManager_UnsetVisitColumnsHelp';
         $translationKeys[] = 'PrivacyManager_UnsetActionColumns';
         $translationKeys[] = 'PrivacyManager_UnsetActionColumnsHelp';
+        $translationKeys[] = 'PrivacyManager_SearchForDataSubject';
+        $translationKeys[] = 'PrivacyManager_FindDataSubjectsBy';
+        $translationKeys[] = 'PrivacyManager_NoDataSubjectsFound';
+        $translationKeys[] = 'PrivacyManager_DeleteVisitsConfirm';
+        $translationKeys[] = 'PrivacyManager_ResultTruncated';
+        $translationKeys[] = 'PrivacyManager_AddVisitorIdToSearch';
+        $translationKeys[] = 'PrivacyManager_AddVisitorIPToSearch';
+        $translationKeys[] = 'PrivacyManager_AddUserIdToSearch';
+        $translationKeys[] = 'PrivacyManager_ExportSelectedVisits';
+        $translationKeys[] = 'PrivacyManager_DeleteSelectedVisits';
+        $translationKeys[] = 'PrivacyManager_SelectWebsite';
+        $translationKeys[] = 'PrivacyManager_MatchingDataSubjects';
+        $translationKeys[] = 'PrivacyManager_VisitsMatchedCriteria';
+        $translationKeys[] = 'PrivacyManager_ExportingNote';
+        $translationKeys[] = 'PrivacyManager_DeletionFromMatomoOnly';
+        $translationKeys[] = 'PrivacyManager_ResultIncludesAllVisits';
+        $translationKeys[] = 'PrivacyManager_GdprTools';
+        $translationKeys[] = 'PrivacyManager_GdprToolsPageIntro1';
+        $translationKeys[] = 'PrivacyManager_GdprToolsPageIntro2';
+        $translationKeys[] = 'PrivacyManager_GdprToolsPageIntroAccessRight';
+        $translationKeys[] = 'PrivacyManager_GdprToolsPageIntroEraseRight';
+        $translationKeys[] = 'PrivacyManager_GdprToolsOverviewHint';
     }
 
     public function setTrackerCacheGeneral(&$cacheContent)
