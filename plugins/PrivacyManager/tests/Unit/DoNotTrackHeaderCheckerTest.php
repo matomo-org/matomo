2616f5e6208fe5ed78bfa218b16031a2d441fbef
sgiehl <stefan@matomo.org>
2020-03-17 15:49:50 +0100

    fix some namespaces

--- plugins/PrivacyManager/tests/Unit/DoNotTrackHeaderCheckerTest.php
+++ plugins/PrivacyManager/tests/Unit/DoNotTrackHeaderCheckerTest.php
@@ -6,7 +6,7 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 
-namespace Piwik\Plugins\PrivacyManager\tests;
+namespace Piwik\Plugins\PrivacyManager\tests\Unit;
 
 
 use Piwik\Plugins\PrivacyManager\Config;
