b70d9a9f4c6071fb60a8c4b4ccdd8463d9bed1bf
diosmosis <diosmosis@users.noreply.github.com>
2020-03-29 19:48:37 -0700

    one test

--- tests/PHPUnit/Integration/CronArchive/ArchiveFilterTest.php
+++ tests/PHPUnit/Integration/CronArchive/ArchiveFilterTest.php
@@ -31,5 +31,43 @@ class ArchiveFilterTest extends IntegrationTestCase
         $this->assertEquals($expectedSegments, array_values($cronarchive->getSegmentsToForce()));
     }
 
+    public function test_filterArchive_filtersSegmentArchives_IfSegmentArchivingIsDisabled()
+    {
+        Fixture::createWebsite('2014-12-12 00:01:02');
+        SegmentAPI::getInstance()->add('foo', 'actions>=1', 1, true, true);
+        SegmentAPI::getInstance()->add('barb', 'actions>=2', 1, true, true);
+        SegmentAPI::getInstance()->add('burb', 'actions>=3', 1, true, true);
+        SegmentAPI::getInstance()->add('sub', 'actions>=4', 1, true, true);
+
+        $filter = new ArchiveFilter();
+        $filter->setSegmentsToForceFromSegmentIds([1, 3]);
+
+        $result = $filter->filterArchive(['segment' => 'actions>=1']);
+        $this->assertFalse($result);
+
+        $result = $filter->filterArchive(['segment' => 'actions>=2']);
+        $this->assertTrue($result);
+
+        $result = $filter->filterArchive(['segment' => 'actions>=3']);
+        $this->assertFalse($result);
+
+        $result = $filter->filterArchive(['segment' => 'actions>=4']);
+        $this->assertTrue($result);
+    }
+
+    public function test_filterArchive_filtersSegmentArchives_IfSegmentIsNotInSegmentsToForce()
+    {
+        // TODO
+    }
+
+    public function test_filterArchive_filtersArchivesWhoseDateIsNotWithinArchiveDateRange()
+    {
+        // TODO
+    }
+
+    public function test_filterArchive_filtersArchivesWhosePeriodIsNotInForcePeriods()
+    {
+        // TODO
+    }
     // TODO: other tests
 }
