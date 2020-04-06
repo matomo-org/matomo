7af49eeac6dde210a2fcfe1da760c0244881a6cb
Thomas Steur <tsteur@users.noreply.github.com>
2020-04-07 08:43:32 +1200

    Avoid possible error subtable already exists but not loaded (#15779)

--- core/Archive/DataTableFactory.php
+++ core/Archive/DataTableFactory.php
@@ -404,13 +404,17 @@ class DataTableFactory
             foreach ($dataTable->getRowsWithoutSummaryRow() as $row) {
                 $row->removeSubtable();
             }
+            $summaryRow = $dataTable->getRowFromId(DataTable::ID_SUMMARY_ROW);
+            if ($summaryRow) {
+                $summaryRow->removeSubtable();
+            }
 
             return;
         }
 
         $dataName = reset($this->dataNames);
 
-        foreach ($dataTable->getRowsWithoutSummaryRow() as $row) {
+        foreach ($dataTable->getRows() as $row) {
             $sid = $row->getIdSubDataTable();
             if ($sid === null) {
                 continue;
