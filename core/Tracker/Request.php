53f3e32686d848ca24295e1320e7ac50932b031e
Stefan Giehl <stefan@matomo.org>
2020-05-06 23:10:00 +0200

    Require authentication when sending custom ip with tracking requests (#15888)

--- core/Tracker/Request.php
+++ core/Tracker/Request.php
@@ -892,7 +892,7 @@ class Request
 
         if (!$this->isAuthenticated()) {
             Common::printDebug("WARN: Tracker API 'cip' was used with invalid token_auth");
-            return IP::getIpFromHeader();
+            throw new InvalidRequestParameterException("Tracker API 'cip' was used, requires valid token_auth");
         }
 
         return $cip;
