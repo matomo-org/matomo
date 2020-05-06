7fca5e5f1f6a5ece0251ce48549a1e2e1c2170e8
sgiehl <stefan@matomo.org>
2020-05-06 13:27:40 +0200

    use parameter to define additional parameters & fix tests

--- plugins/Referrers/tests/Integration/Columns/ReferrerUrlTest.php
+++ plugins/Referrers/tests/Integration/Columns/ReferrerUrlTest.php
@@ -67,25 +67,29 @@ class ReferrerUrlTest extends IntegrationTestCase
     {
         // $referrerUrl, $expectedUrl
         return [
-            // instagram referrer urls (remove s & e)
+            // instagram referrer urls
             ['https://l.instagram.com/?u=https%3A%2F%2Fexample.com%2Fexample.com&amp;e=BTPcuqWixl6Mf5hgYPp6wXIlstuaEdJssdYEvT9s8-6yme_lb275lY2Bwc-YvE-fZNtSKux4QB-v8xNk&amp;s=1',
                   'https://l.instagram.com/?u=https%3A%2F%2Fexample.com%2Fexample.com'],
             ['https://m.instagram.com/?u=https%3A%2F%2Fexample.com%2Fexample.com&e=BTPcuqWixl6Mf5hgYPp6wXIlstuaEdJssdYEvT9s8-6yme_lb275lY2Bwc-YvE-fZNtSKux4QB-v8xNk',
                   'https://m.instagram.com/?u=https%3A%2F%2Fexample.com%2Fexample.com'],
 
-            // facebook referrer urls (remove h)
+            // facebook referrer urls
             ['http://l.facebook.com/l.php?u=http://www.example.com.com/&h=BL0RXrrUUyk_ZbqijDe_mVGBi3ZsyVxJEvOfIhjlUEiRy4zkKwYMDUWbuoICNzhC6pKm6zbGCPAJQP4s8e2psymaokRV3dhp7FPx4Zk6B4x0fBbYTi54xynmBsoBRFB7f5t',
                 'http://l.facebook.com/l.php?u=http://www.example.com.com/'],
             ['http://lm.facebook.com/l.php?u=http://example.com/foobar&h=BT2Dh3r3VDLoabL3Rb1lpmN-_s0lFtReSGzBED3kfUGnaO5fPF-x8LspJAfJN9kkee5ptpybYgyIx68yzgo9kPAN6snSZL_eNcmgu5xhuUcLXJukNKvi0XMOY78Ca9NKexnpJKxKUDeVApPcfB',
                 'http://lm.facebook.com/l.php?u=http://example.com/foobar'],
 
-            // google referrer urls (remove ust & usg)
+            // google referrer urls
             ['https://www.google.com/url?q=https://example.com/foo&sa=D&ust=1689581471834000&usg=BCQjCNFw5f1S7rLgPNephpTW_4-i2KnAGA',
-                'https://www.google.com/url?q=https://example.com/foo&sa=D'],
+                'https://www.google.com/url?q=https://example.com/foo'],
 
-            // bing referrer urls (remove cvid, refig, elv, plvar)
+            // bing referrer urls
             ['https://www.bing.com/search?q=foo+bar&form=EDGTCK&qs=AB&cvid=ff8399e313a74fb592b0ca1d91c42224&refig=4540178a841b46ce8de1664920449112&cc=BE&setlang=4k-NL&elv=AXXfrEiqqD9r3GuelwApuloWthKnH5oOVtTkjmeLPBeagbGxe4rwyaaV!5HJFcbCTxaO4q5w7QqvI8XbCTXyJKn1N4PzqCvVFSdBSr*sdwlB&plvar=0',
-                'https://www.bing.com/search?q=foo+bar&form=EDGTCK&qs=AB&cc=BE&setlang=4k-NL']
+                'https://www.bing.com/search?q=foo+bar'],
+
+            // ensure custom url still keep those parameters
+            ['http://www.example.com/index.php?s=test&e=val&h=param&cvid=custom',
+                'http://www.example.com/index.php?s=test&e=val&h=param&cvid=custom']
         ];
     }
 
