c870770157a3e9c893308967dc274c8feac5d4be
Jean Baptiste Noblot <noblot.jb@gmail.com>
2020-05-12 22:46:00 +0200

    triple equals (#15762)

--- core/DataTable/Filter/AddColumnsProcessedMetrics.php
+++ core/DataTable/Filter/AddColumnsProcessedMetrics.php
@@ -81,11 +81,11 @@ class AddColumnsProcessedMetrics extends BaseFilter
     private function deleteRowsWithNoVisit(DataTable $table)
     {
         foreach ($table->getRows() as $key => $row) {
-            $nbVisits  = Metric::getMetric($row, 'nb_visits');
-            $nbActions = Metric::getMetric($row, 'nb_actions');
+            $nbVisits  = (int)Metric::getMetric($row, 'nb_visits');
+            $nbActions = (int)Metric::getMetric($row, 'nb_actions');
 
-            if ($nbVisits == 0
-                && $nbActions == 0
+            if ($nbVisits === 0
+                && $nbActions === 0
             ) {
                 // case of keyword/website/campaign with a conversion for this day, but no visit, we don't show it
                 $table->deleteRow($key);
