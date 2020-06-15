561b50d7f5ee8a042848994b235ffa5dbb774271
Thomas Steur <tsteur@users.noreply.github.com>
2020-06-15 12:00:21 +1200

    Add legacy autoloader

---
+++ LegacyAutoloader.php
@@ -0,0 +1,26 @@
+<?php
+
+class LegacyAutoloader
+{
+    public function __construct()
+    {
+        spl_autoload_register(array($this, 'load_class'));
+    }
+
+    public static function register()
+    {
+        new LegacyAutoloader();
+    }
+
+    public function load_class($className)
+    {
+        if (strpos($className, 'Matomo\\') === 0) {
+            $newName = 'Piwik' . substr($className, 6);
+            if (class_exists($newName)) {
+                class_alias($newName, $className);
+            }
+        }
+    }
+}
+
+LegacyAutoloader::register();
