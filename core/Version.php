c987fcd56e6c7ab60c105fc93b6e19306a821d14
Thomas Steur <tsteur@users.noreply.github.com>
2020-06-16 10:12:09 +1200

    Update Version.php

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Piwik version.
      * @var string
      */
-    const VERSION = '2.18.0';
+    const VERSION = '2.18.1';
 
     public function isStableVersion($version)
     {
