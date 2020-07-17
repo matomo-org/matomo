494ece0c2bdaab2a05e1e75fe5be974306ff45d3
Thomas Steur <tsteur@users.noreply.github.com>
2020-07-17 15:23:40 +1200

    Add new dimension profilable

---
+++ plugins/CoreHome/Columns/Profilable.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\Plugins\CoreHome\Columns;
+
+use Piwik\Common;
+use Piwik\Piwik;
+use Piwik\Plugin\Dimension\VisitDimension;
+use Piwik\Plugin\Segment;
+use Piwik\Tracker\Request;
+use Piwik\Tracker\Visitor;
+use Piwik\Tracker\Action;
+
+class Profilable extends VisitDimension
+{
+    protected $columnName = 'profilable';
+    protected $columnType = 'TINYINT(1) NULL';
+    protected $nameSingular = 'CoreHome_Profilable';
+    protected $segmentName = 'profilable';
+    protected $type = self::TYPE_BOOL;
+
+    protected $acceptValues = '1 for profilable (eg cookies were used), 0 for not profilable (eg no cookies were used)';
+
+    public function __construct()
+    {
+        $this->suggestedValuesCallback = function ($idSite, $maxValuesToReturn) {
+            return ['0', '1'];
+        };
+    }
+
+    public function onNewVisit(Request $request, Visitor $visitor, $action)
+    {
+        $value = $request->getVisitorId();
+
+        if (empty($value)) {
+            return 0;
+        }
+
+        return 1;
+    }
+
+    public function onExistingVisit(Request $request, Visitor $visitor, $action)
+    {
+        if ($visitor->getVisitorColumn($this->columnName)) {
+            // once it is 1, we don't set it back to 0 if user disables cookies later cause user would be still idenfied
+            // for this same visit with the fingerprint
+            return 1;
+        }
+
+        return $this->onNewVisit($request, $visitor, $action);
+    }
+
+}
