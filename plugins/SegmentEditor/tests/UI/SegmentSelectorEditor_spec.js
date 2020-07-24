c54f52de7304f43ae9cd86278752ff191f3fcb4d
sgiehl <stefan@matomo.org>
2020-07-24 15:12:02 +0200

    update tests

--- plugins/SegmentEditor/tests/UI/SegmentSelectorEditor_spec.js
+++ plugins/SegmentEditor/tests/UI/SegmentSelectorEditor_spec.js
@@ -86,7 +86,7 @@ describe("SegmentSelectorEditorTest", function () {
     });
 
     it("should update segment expression when selecting different segment", async function() {
-        await selectDimension('.segmentRow0', 'Actions', 'Action URL');
+        await selectDimension('.segmentRow0', 'Behaviour', 'Action URL');
         await page.waitForNetworkIdle();
         expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('dimension_drag_drop');
     });
@@ -106,7 +106,7 @@ describe("SegmentSelectorEditorTest", function () {
     });
 
     it("should add an OR condition when a segment dimension is selected in the OR placeholder section", async function() {
-        await selectDimension('.segmentRow0 .segment-row:last', 'Actions', 'Clicked Outlink');
+        await selectDimension('.segmentRow0 .segment-row:last', 'Behaviour', 'Clicked Outlink');
         await page.waitForNetworkIdle();
         expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('drag_or_condition');
     });
@@ -119,7 +119,7 @@ describe("SegmentSelectorEditorTest", function () {
     });
 
     it("should add an AND condition when a segment dimension is dragged to the AND placeholder section", async function() {
-        await selectDimension('.segmentRow1', 'Actions', 'Clicked Outlink');
+        await selectDimension('.segmentRow1', 'Behaviour', 'Clicked Outlink');
         await page.waitForNetworkIdle();
         expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('drag_and_condition');
     });
