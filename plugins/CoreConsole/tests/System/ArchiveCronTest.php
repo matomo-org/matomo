4241f68125d788604e504db04e4118d049eb4f80
diosmosis <diosmosis@users.noreply.github.com>
2020-07-29 00:15:16 -0700

    more debugging

--- plugins/CoreConsole/tests/System/ArchiveCronTest.php
+++ plugins/CoreConsole/tests/System/ArchiveCronTest.php
@@ -165,11 +165,17 @@ class ArchiveCronTest extends SystemTestCase
         $sequence = new Sequence('ExamplePlugin_archiveCount');
         $beforeCount = $sequence->getCurrentId();
 
+        $archivesOld = Db::fetchAll("SELECT idsite, date1, date2, period, value FROM " . ArchiveTableCreator::getNumericTable(Date::factory('2007-04-05'))
+            . " WHERE `name` = 'done.ExamplePlugin' ORDER BY ts_archived DESC LIMIT 8");
+print_r($archivesOld);
         $output = $this->runArchivePhpCron(['-vvv' => null]);
-print "$output\n";
+
         $afterCount = $sequence->getCurrentId();
 
         $this->assertNotEquals($beforeCount, $afterCount, 'example plugin archiving was not triggered');
+        $archivesOld2 = Db::fetchAll("SELECT idsite, date1, date2, period, value FROM " . ArchiveTableCreator::getNumericTable(Date::factory('2007-04-05'))
+            . " WHERE `name` = 'done.ExamplePlugin' ORDER BY ts_archived DESC LIMIT 8");
+        print_r($archivesOld2);
 
         $this->runApiTests('ExamplePlugin.getExampleArchivedMetric', [
             'idSite' => 'all',
