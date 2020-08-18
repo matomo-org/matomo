matomo-org/matomo

commit 934ad5ab3551e2d0994e4168e0b6503f3bde1cf4

One click update in two parts so new code is loaded for second. (#15770)
* One click update in two parts so new code is loaded for second.

* remove no longer needed code

plugins/CoreUpdater/Updater.php
@@ -23,6 +23,7 @@ use Piwik\Plugins\Marketplace\Marketplace;
 use Piwik\SettingsServer;
 use Piwik\Translation\Translator;
 use Piwik\Unzip;
+use Piwik\Url;
 use Piwik\Version;
 
 class Updater
@@ -114,21 +115,6 @@ class Updater
             $this->verifyDecompressedArchive($extractedArchiveDirectory);
             $messages[] = $this->translator->translate('CoreUpdater_VerifyingUnpackedFiles');
 
-            if (Marketplace::isMarketplaceEnabled()) {
-                // we need to load the marketplace already here, otherwise it will use the new, updated file in Piwik 3
-
-                // we also need to make sure to create a new instance here as otherwise we would change the "global"
-                // environment, but we only want to change piwik version temporarily for this task here
-                $environment = StaticContainer::getContainer()->make('Piwik\Plugins\Marketplace\Environment');
-                $environment->setPiwikVersion($newVersion);
-                /** @var \Piwik\Plugins\Marketplace\Api\Client $marketplaceClient */
-                $marketplaceClient = StaticContainer::getContainer()->make('Piwik\Plugins\Marketplace\Api\Client', array(
-                    'environment' => $environment
-                ));
-                require_once PIWIK_DOCUMENT_ROOT . '/plugins/CorePluginsAdmin/PluginInstaller.php';
-                require_once PIWIK_DOCUMENT_ROOT . '/plugins/Marketplace/Api/Exception.php';
-            }
-
             $this->installNewFiles($extractedArchiveDirectory);
             $messages[] = $this->translator->translate('CoreUpdater_InstallingTheLatestVersion');
 
@@ -138,9 +124,45 @@ class Updater
             throw new UpdaterException($e, $messages);
         }
 
+        $partTwoUrl = Url::getCurrentUrlWithoutQueryString() . Url::getCurrentQueryStringWithParametersModified([
+            'action' => 'oneClickUpdatePartTwo',
+        ]);
+
+        $response = Http::sendHttpRequest($partTwoUrl, 300);
+        $response = @json_decode($response, $assoc = true);
+
+        $messages = array_merge($messages, $response);
+
+        try {
+            $disabledPluginNames = $this->disableIncompatiblePlugins($newVersion);
+            if (!empty($disabledPluginNames)) {
+                $messages[] = $this->translator->translate('CoreUpdater_DisablingIncompatiblePlugins', implode(', ', $disabledPluginNames));
+            }
+        } catch (Exception $e) {
+            throw new UpdaterException($e, $messages);
+        }
+
+        return $messages;
+    }
+
+    public function oneClickUpdatePartTwo()
+    {
+        $messages = [];
+
+        $newVersion = Version::VERSION;
+
+        // we also need to make sure to create a new instance here as otherwise we would change the "global"
+        // environment, but we only want to change piwik version temporarily for this task here
+        $environment = StaticContainer::getContainer()->make('Piwik\Plugins\Marketplace\Environment');
+        $environment->setPiwikVersion($newVersion);
+        /** @var \Piwik\Plugins\Marketplace\Api\Client $marketplaceClient */
+        $marketplaceClient = StaticContainer::getContainer()->make('Piwik\Plugins\Marketplace\Api\Client', array(
+            'environment' => $environment
+        ));
+
         try {
 
-            if (Marketplace::isMarketplaceEnabled() && !empty($marketplaceClient)) {
+            if (Marketplace::isMarketplaceEnabled()) {
                 $messages[] = $this->translator->translate('CoreUpdater_CheckingForPluginUpdates');
                 $pluginManager = PluginManager::getInstance();
                 $pluginManager->loadAllPluginsAndGetTheirInfo();
@@ -152,22 +174,13 @@ class Updater
                 foreach ($pluginsWithUpdate as $pluginWithUpdate) {
                     $pluginName = $pluginWithUpdate['name'];
                     $messages[] = $this->translator->translate('CoreUpdater_UpdatingPluginXToVersionY',
-                                                               array($pluginName, $pluginWithUpdate['version']));
+                        array($pluginName, $pluginWithUpdate['version']));
                     $pluginInstaller = new PluginInstaller($marketplaceClient);
                     $pluginInstaller->installOrUpdatePluginFromMarketplace($pluginName);
                 }
             }
         } catch (MarketplaceApi\Exception $e) {
             // there is a problem with the connection to the server, ignore for now
-        } catch (Exception $e) {
-            throw new UpdaterException($e, $messages);
-        }
-
-        try {
-            $disabledPluginNames = $this->disableIncompatiblePlugins($newVersion);
-            if (!empty($disabledPluginNames)) {
-                $messages[] = $this->translator->translate('CoreUpdater_DisablingIncompatiblePlugins', implode(', ', $disabledPluginNames));
-            }
         } catch (Exception $e) {
             throw new UpdaterException($e, $messages);
         }

plugins/CoreUpdater/Controller.php
@@ -12,6 +12,7 @@ use Exception;
 use Piwik\AssetManager;
 use Piwik\Common;
 use Piwik\Config;
+use Piwik\DataTable\Renderer\Json;
 use Piwik\DbHelper;
 use Piwik\Filechecks;
 use Piwik\FileIntegrity;
@@ -170,6 +171,17 @@ class Controller extends \Piwik\Plugin\Controller
         $view->feedbackMessages = $messages;
         $this->addCustomLogoInfo($view);
         return $view->render();
+    }
+
+    public function oneClickUpdatePartTwo()
+    {
+        Piwik::checkUserHasSuperUserAccess();
+
+        Json::sendHeaderJSON();
+
+        $messages = $this->updater->oneClickUpdatePartTwo();
+
+        echo json_encode($messages);
     }
 
     public function oneClickResults()

commit 6310e7875a38a464579779568b694f953567f6b2

Fix plugins not being updated when updating core
Seems to have regressed in https://github.com/matomo-org/matomo/pull/15770

Haven't tested it yet but this should work. We'll then also need to fix this in Matomo 4 separately because the token_auth check wouldn't work there because it would check against the new app specific token auth table which wouldn't exist yet at the time this is executed.

plugins/CoreUpdater/Updater.php
@@ -15,6 +15,7 @@ use Piwik\Filechecks;
 use Piwik\Filesystem;
 use Piwik\Http;
 use Piwik\Option;
+use Piwik\Piwik;
 use Piwik\Plugin\Manager as PluginManager;
 use Piwik\Plugin\ReleaseChannels;
 use Piwik\Plugins\CorePluginsAdmin\PluginInstaller;
@@ -126,6 +127,7 @@ class Updater
 
         $partTwoUrl = Url::getCurrentUrlWithoutQueryString() . Url::getCurrentQueryStringWithParametersModified([
             'action' => 'oneClickUpdatePartTwo',
+            'token_auth' => Piwik::getCurrentUserTokenAuth()
         ]);
 
         $response = Http::sendHttpRequest($partTwoUrl, 300);