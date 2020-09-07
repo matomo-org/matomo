4c9150ce04bd098db069e53b2f982a1de03b4bc0
Thomas Steur <tsteur@users.noreply.github.com>
2020-09-07 14:28:36 +1200

    Activate Matomo 4 by default

--- plugins/PagePerformance/Columns/TimeOnLoad.php
+++ plugins/PagePerformance/Columns/TimeOnLoad.php
@@ -21,8 +21,11 @@ use Piwik\Tracker\Visitor;
 
 class TimeOnLoad extends ActionDimension
 {
-    protected $columnName = 'time_on_load';
-    protected $columnType = 'MEDIUMINT(10) UNSIGNED NULL';
+    const COLUMN_TYPE = 'MEDIUMINT(10) UNSIGNED NULL';
+    const COLUMN_NAME = 'time_on_load';
+
+    protected $columnName = self::COLUMN_NAME;
+    protected $columnType = self::COLUMN_TYPE;
     protected $type = self::TYPE_DURATION_MS;
     protected $nameSingular = 'PagePerformance_ColumnTimeOnLoad';
 
