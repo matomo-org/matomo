3c64683bf85aee739bc7aa18eeefe9dcf4c7896c
diosmosis <diosmosis@users.noreply.github.com>
2020-11-10 11:27:54 -0800

    Set name and default value for certain dimensions. (#16684)

--- plugins/DevicePlugins/Columns/PluginSilverlight.php
+++ plugins/DevicePlugins/Columns/PluginSilverlight.php
@@ -9,6 +9,7 @@
 namespace Piwik\Plugins\DevicePlugins\Columns;
 
 use Piwik\Common;
+use Piwik\Piwik;
 use Piwik\Tracker\Request;
 use Piwik\Tracker\Visitor;
 use Piwik\Tracker\Action;
@@ -18,6 +19,11 @@ class PluginSilverlight extends DevicePluginColumn
     protected $columnName = 'config_silverlight';
     protected $columnType = 'TINYINT(1) NULL';
     protected $type = self::TYPE_BOOL;
+
+    public function getName()
+    {
+        return Piwik::translate('General_Plugin') . ' (Silverlight)';
+    }
 
     /**
      * @param Request $request
