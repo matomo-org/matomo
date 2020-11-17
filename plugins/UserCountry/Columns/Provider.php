72936e15a743f752d3a487c4f4504811f37103c4
Stefan Giehl <stefan@matomo.org>
2020-11-17 20:32:53 +0100

    Improve newly added dimensions/metrics (#16731)

--- plugins/UserCountry/Columns/Provider.php
+++ plugins/UserCountry/Columns/Provider.php
@@ -19,8 +19,7 @@ class Provider extends Base
     protected $columnName = 'location_provider';
     protected $type = self::TYPE_TEXT;
     protected $category = 'UserCountry_VisitLocation';
-    protected $nameSingular = 'UserCountry_InternetServiceProvider';
-    protected $namePlural = 'UserCountry_InternetServiceProviderPlural';
+
     /**
      * @param Request $request
      * @param Visitor $visitor
