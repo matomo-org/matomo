cc5f582d1f7afaefd245e5d7de8bc564a65b01e1
diosmosis <diosmosis@users.noreply.github.com>
2020-11-22 22:28:28 -0800

    start on automated test

---
+++ tests/PHPUnit/System/NoProfilableDataTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+namespace Piwik\Tests\System;
+
+use Piwik\Tests\Fixtures\NonProfilableData;
+use Piwik\Tests\Framework\TestCase\SystemTestCase;
+
+class NoProfilableDataTest extends SystemTestCase
+{
+    /**
+     * @var NonProfilableData
+     */
+    public static $fixture = null;
+
+    /**
+     * @dataProvider getApiForTesting
+     */
+    public function testApi($api, $params)
+    {
+        $this->runApiTests($api, $params);
+    }
+
+    public function getApiForTesting()
+    {
+        $dateTime = self::$fixture->dateTime;
+        $idSite = self::$fixture->idSite;
+
+        return [
+            array('VisitsSummary.get', array('idSite' => $idSite,
+                'date' => $dateTime,
+            )),
+        ];
+    }
+}
+
+NoProfilableDataTest::$fixture = new NonProfilableData();
