8417ac09c3aeb8a72f3e432bad954e249c9ca617
Stefan Giehl <stefan@matomo.org>
2020-11-26 22:26:57 +0100

    Update session ids on update (#16814)

--- core/Updates/4.0.0-b1.php
+++ core/Updates/4.0.0-b1.php
@@ -11,6 +11,7 @@ namespace Piwik\Updates;
 
 use Piwik\DataAccess\TableMetadata;
 use Piwik\Date;
+use Piwik\Db;
 use Piwik\DbHelper;
 use Piwik\Plugin\Manager;
 use Piwik\Plugins\CoreHome\Columns\Profilable;
@@ -28,6 +29,7 @@ use Piwik\Common;
 use Piwik\Config;
 use Piwik\Plugins\UserCountry\LocationProvider;
 use Piwik\Plugins\VisitorInterest\Columns\VisitorSecondsSinceLast;
+use Piwik\SettingsPiwik;
 use Piwik\Updater;
 use Piwik\Updates as PiwikUpdates;
 use Piwik\Updater\Migration\Factory as MigrationFactory;
@@ -103,6 +105,9 @@ class Updates_4_0_0_b1 extends PiwikUpdates
 
         $migrations[] = $this->migration->db->dropColumn('user', 'alias');
         $migrations[] = $this->migration->db->dropColumn('user', 'token_auth');
+
+        // prevent possible duplicates when shorting session id
+        $migrations[] = $this->migration->db->sql('DELETE FROM `' . Common::prefixTable('session') . '` WHERE length(id) > 190');
 
         $migrations[] = $this->migration->db->changeColumnType('session', 'id', 'VARCHAR(191)');
         $migrations[] = $this->migration->db->changeColumnType('site_url', 'url', 'VARCHAR(190)');
@@ -252,6 +257,16 @@ class Updates_4_0_0_b1 extends PiwikUpdates
 
     public function doUpdate(Updater $updater)
     {
+        $salt = SettingsPiwik::getSalt();
+        $sessions = Db::fetchAll('SELECT id from ' . Common::prefixTable('session'));
+
+        foreach ($sessions as $session) {
+            if (!empty($session['id']) && Common::mb_strlen($session['id']) != 128) {
+                $bind = [ hash('sha512', $session['id'] . $salt), $session['id'] ];
+                Db::query(sprintf('UPDATE %s SET id = ? WHERE id = ?', Common::prefixTable('session')), $bind);
+            }
+        }
+
         $updater->executeMigrations(__FILE__, $this->getMigrations($updater));
 
         if ($this->usesGeoIpLegacyLocationProvider()) {
