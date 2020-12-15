f51e793384ca7b8d8986fdfb597d211f0a8f1fc2
sgiehl <stefan@matomo.org>
2020-12-15 11:23:03 +0100

    improve ui test

--- tests/UI/specs/PeriodSelector_spec.js
+++ tests/UI/specs/PeriodSelector_spec.js
@@ -128,7 +128,9 @@ describe("PeriodSelector", function () {
     });
 
     it('should show an error when invalid date/period combination is given', async function () {
-        await page.goto(url.replace(/date=[^&]+/, 'date=2020-08-08,2020-08-09'));
+        await page.goto('about:blank');
+        await page.goto(url.replace(/date=[^&#]+&/, 'date=2020-08-08,2020-08-09&'));
+        await page.waitFor(250);
 
         expect(await page.screenshotSelector(selector + ',#notificationContainer')).to.matchImage('invalid');
     });
