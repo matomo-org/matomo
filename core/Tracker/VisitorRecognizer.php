4422c2a2b6569bf5f61bd084706cb73c7b4d52f7
Thomas Steur <tsteur@users.noreply.github.com>
2020-12-24 11:12:15 +1300

    add test

--- core/Tracker/VisitorRecognizer.php
+++ core/Tracker/VisitorRecognizer.php
@@ -108,10 +108,11 @@ class VisitorRecognizer
 
         $visitRow = $this->model->findVisitor($idSite, $configId, $idVisitor, $userId, $persistedVisitAttributes, $shouldMatchOneFieldOnly, $isVisitorIdToLookup, $timeLookBack, $timeLookAhead);
 
-        if (!empty($maxActions) && $maxActions > 0 && !empty($visitRow['visit_total_actions'])
+        if (!empty($maxActions) && $maxActions > 0
+            && !empty($visitRow['visit_total_actions'])
             && $maxActions <= $visitRow['visit_total_actions']) {
             $this->visitRow = false;
-            return;
+            return false;
         }
 
         $this->visitRow = $visitRow;
