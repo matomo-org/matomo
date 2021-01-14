6e115821747d57bf4b1f8e08b016cda48053d8b2
sgiehl <stefan@matomo.org>
2021-01-14 15:52:39 +0100

    Adds some RSS tests

--- core/DataTable/Renderer/Rss.php
+++ core/DataTable/Renderer/Rss.php
@@ -108,7 +108,7 @@ class Rss extends Renderer
      */
     protected function getRssHeader()
     {
-        $generationDate = date('r');
+        $generationDate = date('r', Date::getNowTimestamp());
         $header = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
 <rss version=\"2.0\">
   <channel>
@@ -118,7 +118,7 @@ class Rss extends Renderer
     <pubDate>$generationDate</pubDate>
     <generator>matomo</generator>
     <language>en</language>
-    <lastBuildDate>$generationDate</lastBuildDate>";
+    <lastBuildDate>$generationDate</lastBuildDate>\n";
         return $header;
     }
 
