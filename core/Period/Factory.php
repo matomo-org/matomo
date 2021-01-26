d4e731062ea305d239bbcd30784de6e22cbfb9ad
sgiehl <stefan@matomo.org>
2021-01-26 09:21:25 +0100

    improve readability

--- core/Period/Factory.php
+++ core/Period/Factory.php
@@ -168,8 +168,11 @@ abstract class Factory
         } else {
             if (!($date instanceof Date)) {
                 if (preg_match('/^(now|today|yesterday|yesterdaySameTime|last[ -]?(?:week|month|year))$/i', $date)) {
-                    // calculate the date taking the current time into account
-                    $date = Date::factory(Date::factory($date)->setTime(date('H:i:s', Date::getNowTimestamp()))->getTimestamp(), $timezone)->getDatetime();
+                    // most magic keywords will automatically set the time to 00:00:00
+                    // we need to set the time manually, so the timezone can be applied correctly
+                    $timestampWithCurrentTime = Date::factory($date)->setTime(date('H:i:s', Date::getNowTimestamp()))->getTimestamp();
+                    // apply the timezone and convert it to a string date
+                    $date = Date::factory($timestampWithCurrentTime, $timezone)->toString('Y-m-d');
                 }
                 $date = Date::factory($date);
             }
