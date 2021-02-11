17770a349ad9d2ffe79bc7387cb89a5471ef2855
Thomas Steur <tsteur@users.noreply.github.com>
2021-02-12 10:00:51 +1300

    Revert previous change accidentally committed to 4.x

--- core/Updater.php
+++ core/Updater.php
@@ -498,9 +498,6 @@ class Updater
                     if ($name == 'core') {
                         $coreError = true;
                         break;
-                    } elseif ($pluginManager->isPluginActivated($name) && $pluginManager->isPluginBundledWithCore($name)) {
-                        $coreError = true;
-                        break;
                     } elseif ($pluginManager->isPluginActivated($name)) {
                         $pluginManager->deactivatePlugin($name);
                         $deactivatedPlugins[] = $name;
