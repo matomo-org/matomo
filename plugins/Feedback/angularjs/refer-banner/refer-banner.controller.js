22c37adbba71a97ee674242d2773df53d2105083
Zoltan Flamis <zoltan@innocraft.com>
2021-02-22 09:37:23 +1300

    share buttons

--- plugins/Feedback/angularjs/refer-banner/refer-banner.controller.js
+++ plugins/Feedback/angularjs/refer-banner/refer-banner.controller.js
@@ -19,12 +19,20 @@
 
         let closeBanner = function() {
             $scope.referBanner.show = false;
+
             setNextReminder(6 * 30);
         };
+
+        let share = function() {
+            $scope.referBanner.show = false;
+
+            setNextReminder(-1);
+        }
 
         let init = function() {
             $scope.referBanner.show = false;
             $scope.referBanner.closeBanner = closeBanner;
+            $scope.referBanner.share = share;
 
             if ($scope.promptForRefer === 1) {
                 $scope.referBanner.show = true;
@@ -34,11 +42,10 @@
         init();
 
         $scope.socialUrl = function (type) {
-             // {% set tweetParams = {'text': 'Did you know Google is using your data for "own purposes"? This means Google owns your data and uses it to monetise their advertising platforms. If you’re using Google Analytics, stay in control by switching to an ethical alternative like Matomo now!', 'url': 'https://matomo.org/google-owns-your-data?pk_campaign=share&pk_kwd=onpremise'}|url_encode %}
-            // {% set facebookParams = {'t': 'Did you know Google is using your data for "own purposes"? This means Google owns your data and uses it to monetise their advertising platforms. If you’re using Google Analytics, stay in control by switching to an ethical alternative like Matomo now!', 'u': 'https://matomo.org/google-owns-your-data?pk_campaign=share&pk_kwd=onpremise'}|url_encode %}
-            // {% set linkedParams = { 'mini': 'true', 'title': 'Did you know Google is using your data for "own purposes"?', 'summary': 'This means Google owns your data and uses it to monetise their advertising platforms. If you’re using Google Analytics, stay in control by switching to an ethical alternative like Matomo now!', 'url': 'https://matomo.org/google-owns-your-data?pk_campaign=share&pk_kwd=onpremise', 'source': 'matomo.org'}|url_encode %}
+            let title = 'Did you know Google is using your data for "own purposes"?';
             let text = 'Did you know Google is using your data for "own purposes"? This means Google owns your data and uses it to monetise their advertising platforms. If you’re using Google Analytics, stay in control by switching to an ethical alternative like Matomo now!';
             let url = 'https://matomo.org/google-owns-your-data?pk_campaign=share&pk_kwd=onpremise';
+            let source = 'matomo.org';
 
             if (type === 'twitter') {
                 let base = 'https://twitter.com/intent/tweet?';
@@ -48,6 +55,26 @@
 
                 return `${base}${paramString.toString()}`;
             }
+
+            if (type === 'facebook') {
+                let base = 'https://www.facebook.com/sharer.php?';
+
+                let params = { t: text, u: url};
+                let paramString = new URLSearchParams(params);
+
+                return `${base}${paramString.toString()}`;
+            }
+
+            if (type === 'linkedin') {
+                let base = 'https://www.linkedin.com/shareArticle?';
+
+                let params = { mini: 'true', title, summary: text, url, source};
+                let paramString = new URLSearchParams(params);
+
+                return `${base}${paramString.toString()}`;
+            }
+
+            return '#';
         };
     }
 })();
