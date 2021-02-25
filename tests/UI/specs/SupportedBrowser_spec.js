71b6c92cc7b8eb5b3d91bd6071c65c2952e42bf9
Zoltan Flamis <zoltan@innocraft.com>
2021-02-25 13:40:38 +1300

    Update SupportedBrowser_spec.js

--- tests/UI/specs/SupportedBrowser_spec.js
+++ tests/UI/specs/SupportedBrowser_spec.js
@@ -8,16 +8,44 @@
  */
 
 describe("SupportedBrowser", function () {
-    const widgetUrl = "?module=Widgetize&action=iframe&moduleToWidgetize=Referrers&idSite=1&period=year&date=2012-08-09&"
-            + "actionToWidgetize=getKeywords&viewDataTable=graphVerticalBar&isFooterExpandedInDashboard=1&";
+    const widgetUrl = "?module=Widgetize&action=iframe&containerId=VisitOverviewWithGraph&disableLink=0&widget=1&moduleToWidgetize=CoreHome&actionToWidgetize=renderWidgetContainer&idSite=1&period=range&date=previous7&disableLink=1&widget=1";
     const generalParams = 'idSite=1&period=year&date=2009-01-04';
     const pageUrl = 'module=CoreHome&action=index&' + generalParams;
     const ie10UserAgent = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)";
     const firefoxUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.2; rv:85.0) Gecko/20100101 Firefox/85.0";
 
+    afterEach(function () {
+        delete testEnvironment.idSitesViewAccess;
+        delete testEnvironment.idSitesWriteAccess;
+        delete testEnvironment.idSitesAdminAccess;
+        testEnvironment.idSitesViewAccess = [1];
+        testEnvironment.testUseMockAuth = 1;
+        testEnvironment.save();
+    });
+
+    beforeEach(function () {
+        delete testEnvironment.idSitesViewAccess;
+        delete testEnvironment.idSitesWriteAccess;
+        delete testEnvironment.idSitesAdminAccess;
+        testEnvironment.idSitesViewAccess = [1];
+        testEnvironment.save();
+    });
+
+    it("should load widget when browser supported", async function () {
+        page.setUserAgent(firefoxUserAgent);
+        await page.goto(widgetUrl);
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('widget_loads_when_browser_supported');
+    });
+
+    it("should load page when browser supported", async function () {
+        page.setUserAgent(firefoxUserAgent);
+        await page.goto(pageUrl);
+        expect(await page.screenshot({ fullPage: true })).to.matchImage('page_loads_when_browser_supported');
+    });
+
     it("should fail load widget when browser not supported", async function () {
         page.setUserAgent(ie10UserAgent);
-        await page.goto(widgetUrl);
+        await page.goto(pageUrl);
         expect(await page.screenshot({ fullPage: true })).to.matchImage('widget_load_fails_when_browser_not_supported');
     });
 
@@ -25,17 +53,5 @@ describe("SupportedBrowser", function () {
         page.setUserAgent(ie10UserAgent);
         await page.goto(pageUrl);
         expect(await page.screenshot({ fullPage: true })).to.matchImage('page_load_fails_when_browser_not_supported');
-    });
-
-    it("should load widget when browser supported", async function () {
-        page.setUserAgent(firefoxUserAgent);
-        await page.goto(widgetUrl);
-        expect(await page.screenshot({ fullPage: true })).to.matchImage('widget_loads_when_browsertsupported');
-    });
-
-    it("should load page when browser supported", async function () {
-        page.setUserAgent(firefoxUserAgent);
-        await page.goto(pageUrl);
-        expect(await page.screenshot({ fullPage: true })).to.matchImage('page_loads_when_browsertsupported');
     });
 });
