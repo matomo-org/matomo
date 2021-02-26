09a6bfa1c8dd3421402821fbbb235c9ac0f502df
Zoltan Flamis <flamisz@gmail.com>
2021-02-26 13:38:26 +1300

    Create SupportedBrowserTest.php

---
+++ tests/PHPUnit/Integration/SupportedBrowserTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+namespace Piwik\Tests\Integration;
+
+use Piwik\Http;
+use Piwik\Site;
+use Piwik\Piwik;
+use Piwik\SupportedBrowser;
+use Piwik\Tests\Framework\Fixture;
+use Piwik\Plugins\SitesManager\API;
+use Piwik\Exception\NotSupportedBrowserException;
+use Piwik\Tests\Framework\TestCase\IntegrationTestCase;
+
+/**
+ * @group Core
+ */
+class SupportedBrowserTest extends IntegrationTestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function test_new_firefox_is_supported()
+    {
+        $firefoxUserAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11.2; rv:85.0) Gecko/20100101 Firefox/85.0";
+        $_SERVER['HTTP_USER_AGENT'] = $firefoxUserAgent;
+
+        $this->assertNull(SupportedBrowser::checkIfBrowserSupported());
+    }
+
+    public function test_old_ie_is_not_supported()
+    {
+        $this->expectException(NotSupportedBrowserException::class);
+
+        $ie10UserAgent = "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)";
+        $_SERVER['HTTP_USER_AGENT'] = $ie10UserAgent;
+
+        SupportedBrowser::checkIfBrowserSupported();
+    }
+}
