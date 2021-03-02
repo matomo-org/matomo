5b07a224820003cd6de727fff179528852cbf3a7
dizzy <diosmosis@users.noreply.github.com>
2021-03-01 22:53:35 -0800

    In core:invalidate-report-data match segments by ID, name or definition ... (#16994)
    * starting on change
    
    * In core:invalidate-report-data match segments by ID, name or definition and warn when a definition does not match any known segment.
    
    * fix test
    
    * remove --yes option since it can break bc
    
    * add test

--- plugins/CoreAdminHome/Commands/InvalidateReportData.php
+++ plugins/CoreAdminHome/Commands/InvalidateReportData.php
@@ -13,14 +13,19 @@ use Piwik\Date;
 use Piwik\Period;
 use Piwik\Period\Range;
 use Piwik\Piwik;
+use Piwik\Plugins\SegmentEditor\API;
 use Piwik\Segment;
 use Piwik\Plugin\ConsoleCommand;
 use Piwik\Plugins\SitesManager\API as SitesManagerAPI;
 use Piwik\Site;
 use Piwik\Period\Factory as PeriodFactory;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
 
 /**
  * Provides a simple interface for invalidating report data by date ranges, site IDs and periods.
@@ -28,6 +33,8 @@ use Symfony\Component\Console\Output\OutputInterface;
 class InvalidateReportData extends ConsoleCommand
 {
     const ALL_OPTION_VALUE = 'all';
+
+    private $allSegments = null;
 
     protected function configure()
     {
@@ -43,7 +50,8 @@ class InvalidateReportData extends ConsoleCommand
             . 'week, month, year or "all" for all of them.',
             self::ALL_OPTION_VALUE);
         $this->addOption('segment', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED,
-            'List of segments to invalidate report data for.');
+            'List of segments to invalidate report data for. This can be the segment string itself, the segment name from the UI or the ID of the segment.'
+            . ' If specifying the segment definition, make sure it is encoded properly (it should be the same as the segment parameter in the URL.');
         $this->addOption('cascade', null, InputOption::VALUE_NONE,
             'If supplied, invalidation will cascade, invalidating child period types even if they aren\'t specified in'
             . ' --periods. For example, if --periods=week, --cascade will cause the days within those weeks to be '
@@ -70,7 +78,7 @@ class InvalidateReportData extends ConsoleCommand
         $sites = $this->getSitesToInvalidateFor($input);
         $periodTypes = $this->getPeriodTypesToInvalidateFor($input);
         $dateRanges = $this->getDateRangesToInvalidateFor($input);
-        $segments = $this->getSegmentsToInvalidateFor($input, $sites);
+        $segments = $this->getSegmentsToInvalidateFor($input, $sites, $output);
 
         foreach ($periodTypes as $periodType) {
             if ($periodType === 'range') {
@@ -214,7 +222,7 @@ class InvalidateReportData extends ConsoleCommand
         return $result;
     }
 
-    private function getSegmentsToInvalidateFor(InputInterface $input, $idSites)
+    private function getSegmentsToInvalidateFor(InputInterface $input, $idSites, OutputInterface $output)
     {
         $segments = $input->getOption('segment');
         $segments = array_map('trim', $segments);
@@ -225,9 +233,56 @@ class InvalidateReportData extends ConsoleCommand
         }
 
         $result = array();
-        foreach ($segments as $segmentString) {
-            $result[] = new Segment($segmentString, $idSites);
+        foreach ($segments as $segmentOptionValue) {
+            $segmentDefinition = $this->findSegment($segmentOptionValue, $idSites, $input, $output);
+            if (empty($segmentDefinition)) {
+                continue;
+            }
+
+            $result[] = new Segment($segmentDefinition, $idSites);
         }
         return $result;
+    }
+
+    private function findSegment($segmentOptionValue, $idSites, InputInterface $input, OutputInterface $output)
+    {
+        $logger = StaticContainer::get(LoggerInterface::class);
+
+        $allSegments = $this->getAllSegments();
+        foreach ($allSegments as $segment) {
+            if (!empty($segment['enable_only_idsite'])
+                && !in_array($segment['enable_only_idsite'], $idSites)
+            ) {
+                continue;
+            }
+
+            if ($segmentOptionValue == $segment['idsegment']) {
+                $logger->debug("Matching '$segmentOptionValue' by idsegment with segment {segment}.", ['segment' => json_encode($segment)]);
+                return $segment['definition'];
+            }
+
+            if (strtolower($segmentOptionValue) == strtolower($segment['name'])) {
+                $logger->debug("Matching '$segmentOptionValue' by name with segment {segment}.", ['segment' => json_encode($segment)]);
+                return $segment['definition'];
+            }
+
+            if ($segment['definition'] == $segmentOptionValue
+                || $segment['definition'] == urldecode($segmentOptionValue)
+            ) {
+                $logger->debug("Matching '{value}' by definition with segment {segment}.", ['value' => $segmentOptionValue, 'segment' => json_encode($segment)]);
+                return $segment['definition'];
+            }
+        }
+
+        $logger->warning("'$segmentOptionValue' did not match any stored segment, but invalidating it anyway.");
+        return $segmentOptionValue;
+    }
+
+    private function getAllSegments()
+    {
+        if ($this->allSegments === null) {
+            $this->allSegments = API::getInstance()->getAll();
+        }
+        return $this->allSegments;
     }
 }
