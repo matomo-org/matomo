dabc1caf64b16f40298bd6b7c43cf93cd0071b07
Zoltan Flamis <flamisz@gmail.com>
2021-03-18 09:52:07 +1300

    Ask users to refer us to other friends (#17234)
    * skeleton for the refer banner
    
    * refer banner wip
    
    * share buttons
    
    * show refer banner logic
    
    * fix linkedin share, hide mastodon
    
    * fix double call for remind me later and error on response
    
    * add tests for refer banner
    
    * fix travis tests
    
    * fix travis tests
    
    * add thanks feedback and language files
    
    * hide fb share
    
    * turn on fb share
    
    * update refer banner copies
    
    * add changes according to pr comments
    
    * normalize lfs
    
    Co-authored-by: diosmosis <diosmosis@users.noreply.github.com>

---
+++ plugins/Feedback/tests/Fixtures/ReferBannerFixture.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Piwik\Plugins\Feedback\tests\Fixtures;
+
+use Piwik\Date;
+use Piwik\Option;
+use Piwik\Tests\Fixtures\UITestFixture;
+
+class ReferBannerFixture extends UITestFixture
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        $yesterday = Date::yesterday();
+        Option::set('Feedback.nextReferReminder.superUserLogin', $yesterday->toString('Y-m-d'));
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        Option::delete('Feedback.nextReferReminder.superUserLogin');
+    }
+
+}
