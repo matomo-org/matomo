35994411bb9ffd56fe2c187db6105a76aff5563d
Thomas Steur <tsteur@users.noreply.github.com>
2021-03-22 09:57:46 +1300

    Ensure second parameter in gmdate is int, not float (#17365)
    see https://forum.matomo.org/t/problem-with-the-two-factor-authentication-setting/41128/12?
    
    > WARNING: /Piwik/core/Cookie.php(155): Warning - gmdate() expects parameter 2 to be int, float given - Matomo 4.1.1 - Please report this message in the Matomo forums: https://forum.matomo.org (please do a search first as it might have been reported already) (Module: UsersManager, Action: setIgnoreCookie, In CLI mode: false)

--- core/Cookie.php
+++ core/Cookie.php
@@ -152,7 +152,7 @@ class Cookie
         }
 
         $header = 'Set-Cookie: ' . rawurlencode($Name) . '=' . rawurlencode($Value)
-            . (empty($Expires) ? '' : '; expires=' . gmdate('D, d-M-Y H:i:s', $Expires) . ' GMT')
+            . (empty($Expires) ? '' : '; expires=' . gmdate('D, d-M-Y H:i:s', (int) $Expires) . ' GMT')
             . (empty($Path) ? '' : '; path=' . $Path)
             . (empty($Domain) ? '' : '; domain=' . rawurlencode($Domain))
             . (!$Secure ? '' : '; secure')
