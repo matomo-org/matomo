9c0c07e7230da443355c338091dee9efc1ce4f9f
diosmosis <diosmosis@users.noreply.github.com>
2021-03-24 11:38:04 -0700

    actually just dont perform the check if the period does not include today

--- core/CronArchive.php
+++ core/CronArchive.php
@@ -904,6 +904,10 @@ class CronArchive
         $isYesterday = $params->getPeriod()->getLabel() == 'day' && $params->getPeriod()->getDateStart()->toString() == Date::factory('yesterday')->toString();
 
         $isPeriodIncludesToday = $params->getPeriod()->isDateInPeriod($today);
+        if (!$isPeriodIncludesToday) {
+            return false; // don't perform the check if the period does not include today
+        }
+
         $minArchiveProcessedTime = Date::now()->subSeconds(Rules::getPeriodArchiveTimeToLiveDefault($params->getPeriod()->getLabel()));
 
         // empty plugins param since we only check for an 'all' archive
