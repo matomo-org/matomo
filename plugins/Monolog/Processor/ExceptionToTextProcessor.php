b1bb297af991a948cbb5ab7af198e3a72cb51e1d
dizzy <diosmosis@users.noreply.github.com>
2021-03-25 11:38:03 -0700

    allow forcing backtrace to print in exceptiontotextprocessor (#17369)
    * Allow forcing backtrace to print and do not print redundant message in exceptiontotextprocessor
    
    * fix test
    
    * consistent output when exception is an array
    
    * fix tests
    
    * fix another test

--- plugins/Monolog/Processor/ExceptionToTextProcessor.php
+++ plugins/Monolog/Processor/ExceptionToTextProcessor.php
@@ -20,6 +20,13 @@ use Piwik\SettingsPiwik;
  */
 class ExceptionToTextProcessor
 {
+    private $forcePrintBacktrace = false;
+
+    public function __construct($forcePrintBacktrace = false)
+    {
+        $this->forcePrintBacktrace = $forcePrintBacktrace;
+    }
+
     public function __invoke(array $record)
     {
         if (! $this->contextContainsException($record)) {
@@ -34,10 +41,9 @@ class ExceptionToTextProcessor
         }
 
         $exceptionStr = sprintf(
-            "%s(%d): %s\n%s",
+            "%s(%d): %s",
             $exception instanceof \Exception ? $exception->getFile() : $exception['file'],
             $exception instanceof \Exception ? $exception->getLine() : $exception['line'],
-            $this->getMessage($exception),
             $this->getStackTrace($exception)
         );
 
@@ -79,7 +85,7 @@ class ExceptionToTextProcessor
 
     private function getStackTrace($exception)
     {
-        return Log::$debugBacktraceForTests ?: self::getMessageAndWholeBacktrace($exception);
+        return Log::$debugBacktraceForTests ?: self::getMessageAndWholeBacktrace($exception, $this->forcePrintBacktrace ? true : null);
     }
 
     /**
@@ -94,12 +100,13 @@ class ExceptionToTextProcessor
         }
 
         if (is_array($exception)) {
+            $message = $exception['message'] ?? '';
             if ($shouldPrintBacktrace && isset($exception['backtrace'])) {
                 $trace = $exception['backtrace'];
                 $trace = self::replaceSensitiveValues($trace);
-                return $trace;
+                return $message . "\n" . $trace;
             } else {
-                return '';
+                return $message;
             }
         }
 
