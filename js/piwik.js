13f7d9a93ceeed5285161f4a38c92f15379c73ae
Jan <56178675+JE4GLE@users.noreply.github.com>
2021-06-10 00:59:08 +0200

    Add option to set page view id manually (#17655)
    * Set custom pageView ID
    
    * Semicolon
    
    * Added method to the method list https://github.com/matomo-org/matomo/pull/17655#issuecomment-857370462
    
    * configIdPageViewSetManually

--- js/piwik.js
+++ js/piwik.js
@@ -63,7 +63,7 @@
     setCustomVariable, getCustomVariable, deleteCustomVariable, storeCustomVariablesInCookie, setCustomDimension, getCustomDimension,
     deleteCustomVariables, deleteCustomDimension, setDownloadExtensions, addDownloadExtensions, removeDownloadExtensions,
     setDomains, setIgnoreClasses, setRequestMethod, setRequestContentType, setGenerationTimeMs,
-    setReferrerUrl, setCustomUrl, setAPIUrl, setDocumentTitle, getPiwikUrl, getMatomoUrl, getCurrentUrl,
+    setReferrerUrl, setCustomUrl, setAPIUrl, setDocumentTitle, setPageViewId, getPiwikUrl, getMatomoUrl, getCurrentUrl,
     setDownloadClasses, setLinkClasses,
     setCampaignNameKey, setCampaignKeywordKey,
     getConsentRequestsQueue, requireConsent, getRememberedConsent, hasRememberedConsent, isConsentRequired,
@@ -2368,6 +2368,8 @@ if (typeof window.Matomo !== 'object') {
                 domainHash,
 
                 configIdPageView,
+                // Boolean indicating that a page view ID has been set manually
+                configIdPageViewSetManually = false,
 
                 // we measure how many pageviews have been tracked so plugins can use it to eg detect if a
                 // pageview was already tracked or not
@@ -3894,7 +3896,9 @@ if (typeof window.Matomo !== 'object') {
              * Log the page view / visit
              */
             function logPageView(customTitle, customData, callback) {
-                configIdPageView = generateUniqueId();
+                if (!configIdPageViewSetManually) {
+                    configIdPageView = generateUniqueId();
+                }
 
                 var request = getRequest('action_name=' + encodeWrapper(titleFixup(customTitle || configTitle)), customData, 'log');
 
@@ -5596,6 +5600,16 @@ if (typeof window.Matomo !== 'object') {
              */
             this.setDocumentTitle = function (title) {
                 configTitle = title;
+            };
+
+            /**
+             * Override PageView id
+             *
+             * @param string pageView
+             */
+            this.setPageViewId = function (pageView) {
+                configIdPageView = pageView;
+                configIdPageViewSetManually = true;
             };
 
             /**
