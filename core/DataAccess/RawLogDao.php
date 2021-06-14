9eac428014bac6487d9e2ecbf3cde4448df0645b
dizzy <diosmosis@users.noreply.github.com>
2021-06-13 21:09:57 -0700

    use getDatetime()

--- core/DataAccess/RawLogDao.php
+++ core/DataAccess/RawLogDao.php
@@ -468,13 +468,13 @@ class RawLogDao
 	// use visit_first_action_time as well, but there is no index on that column.
 
 	$sql = 'SELECT idvisit FROM ' . $logVisit . ' WHERE idvisit > ? AND visit_last_action_time < ? AND idsite = ?';
-        $visitWithGreaterIdVisitButLowerTime = Db::getReader()->fetchOne($sql, [$middleVisit['idvisit'], Date::factory($middleVisit['visit_last_action_time'])->subDay(1), $idSite]);
+        $visitWithGreaterIdVisitButLowerTime = Db::getReader()->fetchOne($sql, [$middleVisit['idvisit'], Date::factory($middleVisit['visit_last_action_time'])->subDay(1)->getDatetime(), $idSite]);
         if (!empty($visitWithGreaterIdVisitButLowerTime)) {
             return [true, $middleVisit['idvisit'], $visitWithGreaterIdVisitButLowerTime];
         }
 
         $sql = 'SELECT idvisit FROM ' . $logVisit . ' WHERE idvisit < ? AND visit_last_action_time > ? AND idsite = ?';
-        $visitWithLowerIdVisitButGreaterTime = Db::getReader()->fetchOne($sql, [$middleVisit['idvisit'], Date::factory($middleVisit['visit_last_action_time'])->addDay(1), $idSite]);
+        $visitWithLowerIdVisitButGreaterTime = Db::getReader()->fetchOne($sql, [$middleVisit['idvisit'], Date::factory($middleVisit['visit_last_action_time'])->addDay(1)->getDatetime(), $idSite]);
         if (!empty($visitWithLowerIdVisitButGreaterTime)) {
             return [true, $middleVisit['idvisit'], $visitWithLowerIdVisitButGreaterTime];
         }
