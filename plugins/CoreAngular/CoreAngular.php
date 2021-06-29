51bfecaec8f55fce5513f96286b3a575e5158ade
diosmosis <diosmosis@users.noreply.github.com>
2021-06-29 08:14:10 -0700

    new plugin

---
+++ plugins/CoreAngular/CoreAngular.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\CoreAngular;
+
+class CoreAngular extends \Piwik\Plugin
+{
+    public function registerEvents()
+    {
+        return [
+            'CronArchive.getArchivingAPIMethodForPlugin' => 'getArchivingAPIMethodForPlugin',
+        ];
+    }
+
+    // support archiving just this plugin via core:archive
+    public function getArchivingAPIMethodForPlugin(&$method, $plugin)
+    {
+        if ($plugin == 'CoreAngular') {
+            $method = 'CoreAngular.getExampleArchivedMetric';
+        }
+    }
+}
---
+++ plugins/CoreAngular/README.md
@@ -0,0 +1,6 @@
+# Matomo CoreAngular Plugin
+
+## Description
+
+Add your plugin description here.
+
