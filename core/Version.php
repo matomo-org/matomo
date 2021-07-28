686abd235607cb21db15aa6969a0c4a9b37ddcfb
Thomas Steur <tsteur@users.noreply.github.com>
2021-07-28 14:16:56 +1200

    Bump 3.X branch to 3.14.2-rc1 (#17825)
    see https://github.com/matomo-org/matomo/issues?q=is%3Aclosed+milestone%3A3.14.2

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    const VERSION = '3.14.1';
+    const VERSION = '3.14.2-rc1';
 
     public function isStableVersion($version)
     {
