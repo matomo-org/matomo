5dde905b1a8fd3d51a1d0a67fe05719607742fef
Matthieu Aubry <mattab@users.noreply.github.com>
2021-07-28 15:23:24 +1200

    Update Version.php

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    const VERSION = '4.4.0-rc3';
+    const VERSION = '4.4.0';
     const MAJOR_VERSION = 4;
 
     public function isStableVersion($version)
