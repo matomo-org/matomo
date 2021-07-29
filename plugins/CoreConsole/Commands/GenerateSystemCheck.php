5a0c566342ebd6e256b29f987323a1e75366a090
Thomas Steur <tsteur@users.noreply.github.com>
2021-07-30 10:54:05 +1200

    Update plugins/CoreConsole/Commands/GenerateSystemCheck.php
    Co-authored-by: Stefan Giehl <stefan@matomo.org>

--- plugins/CoreConsole/Commands/GenerateSystemCheck.php
+++ plugins/CoreConsole/Commands/GenerateSystemCheck.php
@@ -48,7 +48,7 @@ class GenerateSystemCheck extends GeneratePluginBase
 
         $this->writeSuccessMessage($output, array(
             sprintf('plugins/%s/Diagnostic/%s.php for %s generated.', $pluginName, $className, $pluginName),
-            'You should now implement the method called <comment>execute</comment> in API.php',
+            sprintf('You should now implement the method called <comment>execute</comment> in %.php', $className),
             'You also need to make the diagnostic check known to Matomo in your "plugins/'.$pluginName.'/config/config.php".',
             'Read more about this here: https://developer.matomo.org/guides/system-check',
             'Enjoy!'
