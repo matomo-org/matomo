b12bbbc5ada9989c920326a47a8a3003ccc0aa84
mwithheld <796986+mwithheld@users.noreply.github.com>
2021-08-03 13:24:48 -0700

    Use Monolog constructor-default facility
    Issue #17764

--- plugins/Monolog/config/config.php
+++ plugins/Monolog/config/config.php
@@ -105,7 +105,8 @@ return array(
         ->method('setFormatter', DI\get('log.lineMessageFormatter.file')),
 
     '\Monolog\Handler\SyslogHandler' => DI\create()
-        ->constructor(DI\get('log.syslog.ident'), 'syslog', DI\get('log.level.syslog'))
+        ->constructorParameter('ident' => DI\get('log.syslog.ident'))
+        ->constructorParameter('level' => DI\get('log.level.syslog'))
         ->method('setFormatter', DI\get('log.lineMessageFormatter.file')),
 
     'Piwik\Plugins\Monolog\Handler\DatabaseHandler' => DI\create()
