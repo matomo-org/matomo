baa1c843b9421f75722b4905d718ac1be2f43dde
diosmosis <diosmosis@users.noreply.github.com>
2021-08-11 18:28:19 -0700

    bump version correctly

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    const VERSION = '4.4.1-b1';
+    const VERSION = '4.5.0-b1';
 
     const MAJOR_VERSION = 4;
 
