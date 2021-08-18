51ffd33fd9626b24f8b7c4e39eee09872cf576c4
diosmosis <diosmosis@users.noreply.github.com>
2021-08-18 09:29:00 -0700

    revert debugging changes

--- tests/PHPUnit/System/ImportLogsTest.php
+++ tests/PHPUnit/System/ImportLogsTest.php
@@ -78,7 +78,7 @@ class ImportLogsTest extends SystemTestCase
             // Mysqli rounds latitude/longitude
             $apiMethods = array('Live.getLastVisitsDetails');
         }
-        $apiMethods[] = 'Actions.getPageUrls';
+        $apiMethods[] = 'Actions';
         $apiMethods[] = 'VisitorInterest';
         $apiMethods[] = 'VisitFrequency';
         $apis[] = array($apiMethods, array(
@@ -103,6 +103,62 @@ class ImportLogsTest extends SystemTestCase
         ));
 
         return $apis;
+    }
+
+    /**
+     * NOTE: This test must be last since the new sites that get added are added in
+     *       random order.
+     * NOTE: This test combines two tests in order to avoid executing the log importer another time.
+     *       If the log importer were refactored, the invalid requests test could be a unit test in
+     *       python.
+     */
+    public function test_LogImporter_CreatesSitesWhenDynamicResolverUsed_AndReportsOnInvalidRequests()
+    {
+        $this->simulateInvalidTrackerRequest();
+
+        $output = self::$fixture->logVisitsWithDynamicResolver($maxPayloadSize = 3);
+
+        // reload access so new sites are viewable
+        Access::getInstance()->setSuperUserAccess(true);
+
+        // make sure sites aren't created twice
+        $piwikDotNet = API::getInstance()->getSitesIdFromSiteUrl('http://piwik.net');
+        $this->assertEquals(1, count($piwikDotNet));
+
+        $anothersiteDotCom = API::getInstance()->getSitesIdFromSiteUrl('http://anothersite.com');
+        $this->assertEquals(1, count($anothersiteDotCom));
+
+        $whateverDotCom = API::getInstance()->getSitesIdFromSiteUrl('http://whatever.com');
+        $this->assertEquals(1, count($whateverDotCom));
+
+        // make sure invalid requests are reported correctly
+        self::assertStringContainsString('The Matomo tracker identified 2 invalid requests on lines: 10, 11', $output);
+        self::assertStringContainsString("The following lines were not tracked by Matomo, either due to a malformed tracker request or error in the tracker:\n\n10, 11", $output);
+    }
+
+    public function test_LogImporter_RetriesWhenServerFails()
+    {
+        $this->simulateTrackerFailure();
+
+        $logFile = PIWIK_INCLUDE_PATH . '/tests/resources/access-logs/fake_logs_enable_all.log';
+
+        $options = array(
+            '--idsite'                    => self::$fixture->idSite,
+            '--token-auth'                => Fixture::getTokenAuth(),
+            '--retry-max-attempts'        => 5,
+            '--retry-delay'               => 1
+        );
+
+        $output = Fixture::executeLogImporter($logFile, $options, $allowFailure = true);
+        $output = implode("\n", $output);
+
+        for ($i = 2; $i != 6; ++$i) {
+            self::assertStringContainsString("Retrying request, attempt number $i", $output);
+        }
+
+        self::assertStringNotContainsString("Retrying request, attempt number 6", $output);
+
+        self::assertStringContainsString("Max number of attempts reached, server is unreachable!", $output);
     }
 
     private function simulateTrackerFailure()
