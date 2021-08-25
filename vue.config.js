0e951dae71d72204acfc5d0661da8967f5ef56e6
diosmosis <diosmosis@users.noreply.github.com>
2021-08-24 22:14:25 -0700

    add externals config via vue config

---
+++ vue.config.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const pluginExternals = scanPluginExternals();
+
+function scanPluginExternals() {
+  const pluginExternals = {};
+
+  const pluginsDir = path.join(__dirname, '..', '..');
+  for (let pluginName of fs.readdirSync(pluginsDir)) {
+    const vuePackageJsonPath = path.join(pluginsDir, pluginName, 'vue', 'package.json');
+    if (!fs.existsSync(vuePackageJsonPath)) {
+      continue;
+    }
+
+    const vuePackageJson = require(vuePackageJsonPath);
+    pluginExternals[vuePackageJson.name] = vuePackageJson.name;
+  }
+
+  return pluginExternals;
+}
+
+module.exports = {
+  chainWebpack: config => {
+    config.externals(pluginExternals);
+  },
+};
