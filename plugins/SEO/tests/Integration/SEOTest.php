de96268b7036c494e83be34c12cf2b029eb166bb
Tim-Hinnerk Heuer <tim@innocraft.com>
2021-09-14 16:50:41 +1200

    change bogus user agent that fails on bing.com
    fixes #17919

--- plugins/SEO/tests/Integration/SEOTest.php
+++ plugins/SEO/tests/Integration/SEOTest.php
@@ -36,7 +36,7 @@ class SEOTest extends IntegrationTestCase
         $user_agents = array(
             'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Safari/605.1.15',
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
         );
 
         $_SERVER['HTTP_USER_AGENT'] = $user_agents[mt_rand(0, count($user_agents) - 1)];
