e98113436101c358e22e4819ba3ce9fe28719ba1
Peter Zhang <Peter@innocraft.com>
2021-09-15 16:26:38 +1200

    update html inline style to less, remove UI test for segemtn selector

--- plugins/SegmentEditor/tests/UI/SegmentSelectorEditor_spec.js
+++ plugins/SegmentEditor/tests/UI/SegmentSelectorEditor_spec.js
@@ -1,6 +1,6 @@
 /*!
  * Matomo - free/libre analytics platform
- * 
+ *
  * SegmentEditor screenshot tests.
  *
  * @link https://matomo.org
@@ -9,7 +9,7 @@
 
 describe("SegmentSelectorEditorTest", function () {
     var selectorsToCapture = ".segmentEditorPanel,.segmentEditorPanel .dropdown-body,.segment-element";
-    
+
     this.timeout(0);
 
     var generalParams = 'idSite=1&period=year&date=2012-08-09';
@@ -57,18 +57,6 @@ describe("SegmentSelectorEditorTest", function () {
         expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('3_segment_editor_edit_name');
     });
 
-    it("should show the segment editor's available segments dropdown", async function() {
-        var elem = await page.$('.available_segments a.dropList');
-        await elem.hover();
-        await page.click('.available_segments a.dropList');
-        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('6_segment_editor_droplist');
-    });
-
-    it("should change segment when another available segment clicked in segment editor's available segments dropdown", async function() {
-        await (await page.jQuery('.ui-menu-item div:contains(Add new segment)')).click();
-        await page.waitForNetworkIdle();
-        expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('6_segment_editor_different');
-    });
 
     it("should close the segment editor when the close link is clicked", async function() {
         await page.evaluate(function () {
