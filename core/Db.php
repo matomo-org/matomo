a6eaf7d97bc47863dd739c0e1566bb4069dce4ae
Ben Burgess <ben.burgess@innocraft.com>
2021-09-20 12:59:27 +1200

    Optimise tables one by one

--- core/Db.php
+++ core/Db.php
@@ -420,7 +420,7 @@ class Db
      * @param string|array $tables The name of the table to optimize or an array of tables to optimize.
      *                             Table names must be prefixed (see {@link Piwik\Common::prefixTable()}).
      * @param bool $force If true, the `OPTIMIZE TABLE` query will be run even if InnoDB tables are being used.
-     * @return \Zend_Db_Statement
+     * @return bool|\Zend_Db_Statement
      */
     public static function optimizeTables($tables, $force = false)
     {
@@ -461,7 +461,16 @@ class Db
         }
 
         // optimize the tables
-        return self::query("OPTIMIZE TABLE " . implode(',', $tables));
+        $success = true;
+        foreach ($tables as &$t) {
+            $ok = self::query('OPTIMIZE TABLE ' . $t);
+            if (!$ok) {
+                $success = false;
+            }
+        }
+
+        return $success;
+
     }
 
     private static function getTableStatus()
