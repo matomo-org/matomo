55c04dec118456bfbb6132964d9bec4f80fb846d
diosmosis <diosmosis@users.noreply.github.com>
2021-10-01 23:41:30 -0700

    progress

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  preset: '@vue/cli-plugin-unit-jest',
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.ts$': 'ts-jest',
+  },
   testMatch: [
     '**/plugins/*/vue/src/**/*.spec.[jt]s',
   ],
