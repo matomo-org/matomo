6c88184b1c122bc9a48d1696419f131f2f70f061
diosmosis <diosmosis@users.noreply.github.com>
2021-10-02 17:40:18 -0700

    add missing file headers

--- plugins/CoreHome/vue/src/Alert/Alert.vue
+++ plugins/CoreHome/vue/src/Alert/Alert.vue
@@ -1,3 +1,10 @@
+<!--
+  Matomo - free/libre analytics platform
+
+  @link https://matomo.org
+  @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+-->
+
 <template>
   <div
     class="alert"
--- plugins/CoreHome/vue/src/index.ts
+++ plugins/CoreHome/vue/src/index.ts
@@ -1,3 +1,10 @@
+/*!
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
 export { default as activityIndicatorAdapter } from './ActivityIndicator/ActivityIndicator.adapter';
 export { default as ActivityIndicator } from './ActivityIndicator/ActivityIndicator.vue';
 export { default as translate } from './translate';
