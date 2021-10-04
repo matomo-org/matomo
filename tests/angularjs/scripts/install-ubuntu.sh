c9c687a7de32759110ff0f9f21ccbe432aaaae11
diosmosis <diosmosis@users.noreply.github.com>
2021-10-03 18:06:25 -0700

    use latest node

--- tests/angularjs/scripts/install-ubuntu.sh
+++ tests/angularjs/scripts/install-ubuntu.sh
@@ -2,6 +2,10 @@
 DIR=`dirname $0`
 source $DIR/../../travis/travis-helper.sh
 
+source ~/.nvm/nvm.sh
+nvm install 14 && nvm alias default 14 && nvm use default
+node --version
+
 cd ..
 npm config set loglevel error
 travis_retry npm install .
