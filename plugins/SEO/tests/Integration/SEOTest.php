75074d5118360193155034be7dd425b999a17e44
Peter Zhang <Peter@innocraft.com>
2021-10-13 11:06:24 +1300

    update seo Test
    update seo Test

--- plugins/SEO/tests/Integration/SEOTest.php
+++ plugins/SEO/tests/Integration/SEOTest.php
@@ -41,7 +41,13 @@ class SEOTest extends IntegrationTestCase
      */
     public function test_API()
     {
-        $ip = $_SERVER['REMOTE_ADDR'] ?? '';
+        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        } else {
+            $ip = $_SERVER['REMOTE_ADDR'] ?? '';
+        }
         $ranks = $this->apiFunction();
         foreach ($ranks as $rank) {
             if ($rank['rank'] == Piwik::translate('General_Error')) {
