a671908079373e7efbc7ee986f29d6870dc22e24
diosmosis <diosmosis@users.noreply.github.com>
2021-11-03 23:28:40 -0700

    add node version check to vue:build command

--- plugins/CoreVue/Commands/Build.php
+++ plugins/CoreVue/Commands/Build.php
@@ -20,6 +20,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Build extends ConsoleCommand
 {
+    const RECOMMENDED_NODE_VERSION = '16.0.0';
+    const RECOMMENDED_NPM_VERSION = '7.0.0';
+
     protected function configure()
     {
         $this->setName('vue:build');
@@ -38,6 +41,7 @@ class Build extends ConsoleCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         self::checkVueCliServiceAvailable();
+        $this->checkNodeJsVersion($output);
 
         $clearWebpackCache = $input->getOption('clear-webpack-cache');
         if ($clearWebpackCache) {
@@ -187,5 +191,26 @@ class Build extends ConsoleCommand
     {
         $path = PIWIK_INCLUDE_PATH . '/node_modules/.cache';
         Filesystem::unlinkRecursive($path, true);
+    }
+
+    private function checkNodeJsVersion(OutputInterface $output)
+    {
+        $nodeVersion = ltrim(`node -v`, 'v');
+        $npmVersion = ltrim(`npm -v`, 'v');
+
+        if (version_compare($nodeVersion, self::RECOMMENDED_NODE_VERSION, '<')) {
+            $output->writeln(sprintf("<comment>The recommended node version for working with Vue is version %s or "
+                . "greater and it looks like you're using %s. Building Vue files may not work with an older version, so "
+                . "we recommend upgrading. nvm can be used to easily install new node versions.</comment>",
+                self::RECOMMENDED_NODE_VERSION, $nodeVersion));
+        }
+
+        if (version_compare($npmVersion, self::RECOMMENDED_NPM_VERSION, '<')) {
+            $output->writeln(sprintf("<comment>The recommended npm version for working with Vue is version %s "
+                . "or greater and it looks like you're using %s. Using an older version may result in improper "
+                . "dependencies being used, so we recommend upgrading. You can upgrade to the latest version with the "
+                . "command %s</comment>",
+                self::RECOMMENDED_NPM_VERSION, $npmVersion, 'npm install -g npm@latest'));
+        }
     }
 }
