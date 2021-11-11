50bf1e1fb4a95cd66959fb0bddac8b2cdff731c4
diosmosis <diosmosis@users.noreply.github.com>
2021-11-11 23:49:38 +0000

    built vue files

--- plugins/CoreHome/vue/dist/CoreHome.umd.js
+++ plugins/CoreHome/vue/dist/CoreHome.umd.js
@@ -364,10 +364,10 @@ Matomo_piwik.postEvent = function postMatomoEvent(eventName) {
 
   Matomo_piwik.postEventNoEmit.apply(Matomo_piwik, [eventName].concat(args)); // required until angularjs is removed
 
-  angular.element(function () {
+  window.angular.element(function () {
     var $rootScope = Matomo_piwik.helper.getAngularDependency('$rootScope'); // eslint-disable-line
 
-    return $rootScope.$oldEmit.apply($rootScope, [eventName].concat(args));
+    $rootScope.$oldEmit.apply($rootScope, [eventName].concat(args));
   });
 };
 
@@ -1206,7 +1206,9 @@ function initPiwikService(piwik, $rootScope) {
       args[_key - 1] = arguments[_key];
     }
 
-    return Matomo_Matomo.postEvent.apply(Matomo_Matomo, [name].concat(args));
+    Matomo_Matomo.postEvent.apply(Matomo_Matomo, [name].concat(args)); // can't always get the result. it's not really used in angularjs though, so it should be ok.
+
+    return null;
   };
 
   $rootScope.$oldBroadcast = $rootScope.$broadcast; // eslint-disable-line
