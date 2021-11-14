dd1aad6e9c9beaf636eb884a498c36f155c77411
Peter Zhang <Peter@innocraft.com>
2021-11-15 10:17:47 +1300

    Update UIIntegration_spec.js
    update table random UI

--- tests/UI/specs/UIIntegration_spec.js
+++ tests/UI/specs/UIIntegration_spec.js
@@ -85,7 +85,9 @@ describe("UIIntegrationTest", function () { // TODO: Rename to Piwik?
             await page.goto("?" + urlBase + "#?" + generalParams + "&category=Dashboard_Dashboard&subcategory=3");
             await page.waitForSelector('.widget');
             await page.waitForNetworkIdle();
-
+            await page.evaluate(() => { // give table headers constant width so the screenshot stays the same
+              $('.dataTableScroller').css('overflow-x', 'scroll');
+            });
             pageWrap = await page.$('.pageWrap');
             expect(await pageWrap.screenshot()).to.matchImage('dashboard3');
         });
@@ -236,7 +238,9 @@ describe("UIIntegrationTest", function () { // TODO: Rename to Piwik?
             // use columns query param to make sure columns works when supplied in URL fragment
             await page.goto("?" + urlBase + "#?" + generalParams + "&category=General_Visitors&subcategory=General_Overview&columns=nb_visits,nb_actions");
             await page.waitForNetworkIdle();
-
+            await page.evaluate(() => { // give table headers constant width so the screenshot stays the same
+              $('.dataTableScroller').css('overflow-x', 'scroll');
+            });
             pageWrap = await page.$('.pageWrap');
             expect(await pageWrap.screenshot()).to.matchImage('visitors_overview_columns');
         });
@@ -470,6 +474,9 @@ describe("UIIntegrationTest", function () { // TODO: Rename to Piwik?
         });
 
         it("should show all corresponding content pieces when clicking on a content name", async function () {
+            await page.evaluate(() => { // give table headers constant width so the screenshot stays the same
+              $('.dataTableScroller').css('overflow-x', 'scroll');
+            });
             elem = await page.jQuery('.dataTable .subDataTable .value:contains(ImageAd)');
             await elem.click();
             await page.waitForNetworkIdle();
@@ -489,6 +496,9 @@ describe("UIIntegrationTest", function () { // TODO: Rename to Piwik?
         });
 
         it("should show all corresponding content names when clicking on a content piece", async function () {
+            await page.evaluate(() => { // give table headers constant width so the screenshot stays the same
+              $('.dataTableScroller').css('overflow-x', 'scroll');
+            });
             elem = await page.jQuery('.dataTable .subDataTable .value:contains(Click NOW)');
             await elem.click();
             await page.waitForNetworkIdle();
