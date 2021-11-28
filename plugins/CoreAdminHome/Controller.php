matomo-org/matomo

commit 087678f4435465ea528a077756042a72e0147d40

Improve password handling in SMTP configuration (#18387)

plugins/CoreAdminHome/Controller.php
@@ -160,8 +160,8 @@ class Controller extends ControllerAdmin
             $mail['username'] = Common::unsanitizeInputValue(Common::getRequestVar('mailUsername', ''));
             $mail['password'] = Common::unsanitizeInputValue(Common::getRequestVar('mailPassword', ''));
 
-            if (!array_key_exists('mailPassword', $_POST)) {
-                // use old password if it wasn't set in request
+            if (!array_key_exists('mailPassword', $_POST) && Config::getInstance()->mail['password'] !== $mail['host']) {
+                // use old password if it wasn't set in request (and the host wasn't changed)
                 $mail['password'] = Config::getInstance()->mail['password'];
             }
 

plugins/CoreAdminHome/templates/generalSettings.twig
@@ -96,6 +96,7 @@
 
                 <div piwik-field uicontrol="text" name="mailHost"
                      ng-model="mailSettings.mailHost"
+                     ng-change="mailSettings.passwordChanged || ((mailSettings.mailPassword = '') || (mailSettings.passwordChanged = true))"
                      data-title="{{ 'General_SmtpServerAddress'|translate|e('html_attr') }}"
                      value="{{ mail.host|e('html_attr') }}">
                 </div>
@@ -128,6 +129,7 @@
                 <div piwik-field uicontrol="password" name="mailPassword"
                      ng-model="mailSettings.mailPassword"
                      ng-change="mailSettings.passwordChanged = true"
+                     ng-click="!mailSettings.passwordChanged && $event.target.select();"
                      data-title="{{ 'General_SmtpPassword'|translate|e('html_attr') }}"
                      value="{{ mail.password ? '******' }}" inline-help="{{ help|e('html_attr') }}"
                      autocomplete="off">

commit cc6d43afd23cb57785be040ee914c7661cb3e3a4

fix incorrect condition introduced in #18387

plugins/CoreAdminHome/Controller.php
@@ -160,7 +160,7 @@ class Controller extends ControllerAdmin
             $mail['username'] = Common::unsanitizeInputValue(Common::getRequestVar('mailUsername', ''));
             $mail['password'] = Common::unsanitizeInputValue(Common::getRequestVar('mailPassword', ''));
 
-            if (!array_key_exists('mailPassword', $_POST) && Config::getInstance()->mail['password'] !== $mail['host']) {
+            if (!array_key_exists('mailPassword', $_POST) && Config::getInstance()->mail['host'] === $mail['host']) {
                 // use old password if it wasn't set in request (and the host wasn't changed)
                 $mail['password'] = Config::getInstance()->mail['password'];
             }