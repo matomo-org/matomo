69ce86b5ed3668631a23262026ebb8fadf5d9309
Peter Zhang <peter@innocraft.com>
2021-12-06 14:27:10 +1300

    Update plugin-settings.controller.js
    add modal background field readonly

--- plugins/CorePluginsAdmin/angularjs/plugin-settings/plugin-settings.controller.js
+++ plugins/CorePluginsAdmin/angularjs/plugin-settings/plugin-settings.controller.js
@@ -51,9 +51,15 @@
                     }
 
                     $element.find('.confirm-password-modal').modal({ dismissible: false, onOpenEnd: function () {
-                        $('.modal.open #currentUserPassword').focus();
-                        $('.modal.open #currentUserPassword').off('keypress').keypress(onEnter);
-                    }}).modal('open');
+                            $('.modal.open #currentUserPassword').focus();
+                            $('.modal.open #currentUserPassword').off('keypress').keypress(onEnter);
+                            $('.pluginSettings input').prop("readonly",true);
+                            $('.confirm-password-modal input').prop("readonly",false);
+                        },
+                        onCloseStart: function () {
+                            $('.pluginSettings input').prop('readonly', false);
+                        },
+                    }).modal('open');
 
                     return;
                 } else {
