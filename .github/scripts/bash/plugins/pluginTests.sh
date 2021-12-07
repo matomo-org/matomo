ffca6a2e8ef26d44985999dd27208917e1bd0d41
Peter Zhang <peter@innocraft.com>
2021-12-07 13:49:23 +1300

    update test

--- .github/scripts/bash/plugins/pluginTests.sh
+++ .github/scripts/bash/plugins/pluginTests.sh
@@ -29,8 +29,7 @@ then
             # a better fix would be to decouple the Matomo testing framework from Matomo in a way that allowed us to
             # change code for all versions of Matomo as well as selectively for individual Matomo versions.
             # git checkout 4.x-dev ../../plugins/TestRunner/Commands/TestsRunUI.php
-
-            ./console tests:run-ui --assume-artifacts --persist-fixture-data --plugin=$PLUGIN_NAME --extra-options="$UITEST_EXTRA_OPTIONS"
+            ./console tests:run-ui --store-in-ui-tests-repo --persist-fixture-data --assume-artifacts --plugin=$PLUGIN_NAME --extra-options="$UITEST_EXTRA_OPTIONS"
         else
             ./console tests:run-ui --store-in-ui-tests-repo --persist-fixture-data --assume-artifacts --core --extra-options="$UITEST_EXTRA_OPTIONS"
         fi
