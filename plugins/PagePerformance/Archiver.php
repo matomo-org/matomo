07db3afa801a6be7b3c730ee35477cd28f5487ec
Peter Zhang <peter@innocraft.com>
2021-12-20 16:51:09 +1300

    Revert "Update Archiver.php"
    This reverts commit 71ad61f59750d613eccc8f6fe1eca586a61763d2.

--- plugins/PagePerformance/Archiver.php
+++ plugins/PagePerformance/Archiver.php
@@ -54,7 +54,7 @@ class Archiver extends \Piwik\Plugin\Archiver
             $column = $dimension->getColumnName();
             $selects[] = "sum($table.$column) as {$column}_total";
             $selects[] = "sum(if($table.$column is null, 0, 1)) as {$column}_hits";
-            $totalColumns[] = "IFNULL($table.$column,0)";
+            $totalColumns[] = "$table.$column";
             $hitsColumns[] = "if($table.$column is null, 0, 1)";
         }
 
