0745d657752273288e14e0237dce262d93f08d9f
Peter <peter@innocraft.com>
2022-01-05 17:03:54 +1300

    update mysql config

--- .github/scripts/bash/setup_mysql_tmpfs.sh
+++ .github/scripts/bash/setup_mysql_tmpfs.sh
@@ -14,16 +14,15 @@ sudo ln -s /mnt/ramdisk/mysql /var/lib/mysql
 
 # print out mysql information
 mysql --version
-sudo mysql -e 'CREATE DATABASE $DB_DATABASE;' -u$DB_DATABASE -p$DB_PASSWORD
-
+mysql -e 'CREATE DATABASE ${{ env.DB_DATABASE }};' -u${{ env.DB_USER }} -p${{ env.DB_PASSWORD }}
 # configure mysql
-mysql -e "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES'"  -u$DB_DATABASE -p$DB_PASSWORD  # Travis default
+mysql -e "SET GLOBAL sql_mode = 'NO_ENGINE_SUBSTITUTION,STRICT_TRANS_TABLES'" -u${{ env.DB_USER }} -p${{ env.DB_PASSWORD }}  # Travis default
 # try to avoid 'mysql has gone away' errors
-mysql -e "SET GLOBAL wait_timeout = 36000;"  -u$DB_DATABASE -p$DB_PASSWORD
-mysql -e "SET GLOBAL max_allowed_packet = 134209536;"  -u$DB_DATABASE -p$DB_PASSWORD
-mysql -e "SHOW VARIABLES LIKE 'max_allowed_packet';"  -u$DB_DATABASE -p$DB_PASSWORD
-mysql -e "SHOW VARIABLES LIKE 'wait_timeout';" -u$DB_DATABASE -p$DB_PASSWORD
+mysql -e "SET GLOBAL wait_timeout = 36000;" -u${{ env.DB_USER }} -p${{ env.DB_PASSWORD }}
+mysql -e "SET GLOBAL max_allowed_packet = 134209536;"  -u${{ env.DB_USER }} -p${{ env.DB_PASSWORD }}
+mysql -e "SHOW VARIABLES LIKE 'max_allowed_packet';" -u${{ env.DB_USER }} -p${{ env.DB_PASSWORD }}
+mysql -e "SHOW VARIABLES LIKE 'wait_timeout';" -u${{ env.DB_USER }} -p${{ env.DB_PASSWORD }}
 
-mysql -e "SELECT @@sql_mode;" -u$DB_DATABASE -p$DB_PASSWORD
+mysql -e "SELECT @@sql_mode;" -u${{ env.DB_USER }} -p${{ env.DB_PASSWORD }}
 # - mysql -e "SHOW GLOBAL VARIABLES;"
 
