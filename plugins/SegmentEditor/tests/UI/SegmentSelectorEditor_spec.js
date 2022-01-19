d57771916e60836b7340b7f2ee0c8be4e3434a57
Peter <peter@innocraft.com>
2022-01-19 18:32:29 +1300

    add click before typo
    add click before typo

--- plugins/SegmentEditor/tests/UI/SegmentSelectorEditor_spec.js
+++ plugins/SegmentEditor/tests/UI/SegmentSelectorEditor_spec.js
@@ -201,21 +201,24 @@ describe("SegmentSelectorEditorTest", function () {
     it('should display autocomplete dropdown options correctly with lower case', async function() {
         await page.click('.expandableSelector .select-wrapper');
         await page.waitForSelector('.expandableList');
-        await page.type('.expandableSelector', 'event');
+        await page.click('.expandableSelector');
+        await page.type('input.expandableSelector', 'event');
         await page.waitForTimeout(100);
         expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('autocomplete_lowercase');
     });
 
     it('should display autocomplete dropdown options correctly with upper case', async function() {
+        await page.click('.expandableSelector');
         await page.type('.expandableSelector', '');
-        await page.type('.expandableSelector', 'EVENT');
+        await page.type('input.expandableSelector', 'EVENT');
         await page.waitForTimeout(100);
         expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('autocomplete_uppercase');
     });
 
     it('should display autocomplete dropdown options correctly with capitalized', async function() {
+        await page.click('.expandableSelector');
         await page.type('.expandableSelector', '');
-        await page.type('.expandableSelector', 'Event');
+        await page.type('input.expandableSelector', 'Event');
         await page.waitForTimeout(100);
         expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('autocomplete_capitalized');
     });
