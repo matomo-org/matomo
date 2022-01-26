da4be81ebbe1e9225e2ac7b4eb8c272e33eeb088
Peter Zhang <peter@innocraft.com>
2022-01-26 13:16:57 +1300

    Revert "add date, period params into common function (#18653)" (#18685)
    This reverts commit a0705d8fd53f3818c517fa386c0e377196937942.

--- plugins/VisitsSummary/Reports/Get.php
+++ plugins/VisitsSummary/Reports/Get.php
@@ -49,7 +49,7 @@ class Get extends \Piwik\Plugin\Report
             'max_actions'
         );
 
-        $period = Piwik::getPeriod('day');
+        $period = Common::getRequestVar('period', 'day');
         if (SettingsPiwik::isUniqueVisitorsEnabled($period)) {
             $this->metrics = array_merge(['nb_uniq_visitors'], $this->metrics);
         }
