1b77ba9e086bfa137e357a51876be30e3b9b6e28
diosmosis <diosmosis@users.noreply.github.com>
2022-02-03 01:39:56 -0800

    style fix

--- plugins/UsersManager/vue/src/UserEditForm/UserEditForm.vue
+++ plugins/UsersManager/vue/src/UserEditForm/UserEditForm.vue
@@ -35,6 +35,7 @@
             <a
               href=""
               @click.prevent="activeTab = 'permissions'"
+              style="margin-right:3.5px"
             >
               {{ translate('UsersManager_Permissions') }}
             </a>
