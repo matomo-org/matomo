a66a9a6f40188a90b9deeb6e6cabaa3bde3be4d4
Peter <peter@innocraft.com>
2022-02-04 18:39:16 +1300

    Revert "Update TrackFewVisitsAndCreateUsers.php"
    This reverts commit 83957b2f0e2baf7f8e0da42717e9f608efd5155a.

--- plugins/UserId/tests/Fixtures/TrackFewVisitsAndCreateUsers.php
+++ plugins/UserId/tests/Fixtures/TrackFewVisitsAndCreateUsers.php
@@ -16,7 +16,7 @@ use Piwik\Tests\Framework\Fixture;
  */
 class TrackFewVisitsAndCreateUsers extends Fixture
 {
-    public $dateTime = '2010-03-01 11:22:33';
+    public $dateTime = '2010-02-01 11:22:33';
     public $idSite = 1;
 
     public function setUp(): void
