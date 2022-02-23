5448b816c1d9eadc59d64529bb164aa14cd33811
diosmosis <diosmosis@users.noreply.github.com>
2022-02-22 18:31:38 -0800

    use realpath on test PIWIK_INCLUDE_PATH so search/replace on paths for relative path will work

--- tests/PHPUnit/proxy/includes.php
+++ tests/PHPUnit/proxy/includes.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!defined('PIWIK_INCLUDE_PATH')) {
-    define('PIWIK_INCLUDE_PATH', realpath(dirname(__FILE__)) . '/../../../');
+    define('PIWIK_INCLUDE_PATH', realpath(dirname(__FILE__) . '/../../../'));
 }
 if (!defined('PIWIK_USER_PATH')) {
     define('PIWIK_USER_PATH', PIWIK_INCLUDE_PATH);
