f2f6c3d1e7fe00cceb86feaa184049737f3ba45e
Justin Velluppillai <justin@innocraft.com>
2022-03-08 13:42:04 +1300

    Update version to 4.8.0 (#18893)

--- core/Version.php
+++ core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    const VERSION = '4.8.0-rc1';
+    const VERSION = '4.8.0';
 
     const MAJOR_VERSION = 4;
 
