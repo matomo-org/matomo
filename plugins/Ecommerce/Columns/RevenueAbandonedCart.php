9a4721c48e951c9edb9799ed816489da7ee12402
Peter <peter@innocraft.com>
2022-03-17 13:40:20 +1300

    update cart
    update cart

--- plugins/Ecommerce/Columns/RevenueAbandonedCart.php
+++ plugins/Ecommerce/Columns/RevenueAbandonedCart.php
@@ -8,6 +8,16 @@
  */
 namespace Piwik\Plugins\Ecommerce\Columns;
 
+use Piwik\Columns\DimensionSegmentFactory;
+use Piwik\Columns\Discriminator;
+use Piwik\Piwik;
+use Piwik\Plugin\Segment;
+use Piwik\Segment\SegmentsList;
+use Piwik\Tracker\Action;
+use Piwik\Tracker\GoalManager;
+use Piwik\Tracker\Request;
+use Piwik\Tracker\Visitor;
+
 class RevenueAbandonedCart extends BaseConversion
 {
     protected $columnName = 'revenue';
@@ -16,5 +26,66 @@ class RevenueAbandonedCart extends BaseConversion
     protected $category = 'Goals_Ecommerce';
     protected $nameSingular = 'Ecommerce_RevenueLeftInCart';
     protected $segmentName = 'revenueAbandonedCart';
+
+
+    public function configureSegments(SegmentsList $segmentsList, DimensionSegmentFactory $dimensionSegmentFactory)
+    {
+        $segment = new Segment();
+        $segment->setCategory($this->category);
+        $segment->setType('dimension');
+        $segment->setName(Piwik::translate('Ecommerce_RevenueLeftInCart'));
+        $segment->setSegment('revenueAbandonedCart');
+
+    }
+
+    public function getDbDiscriminator()
+    {
+        return new Discriminator($this->dbTableName, 'idgoal', GoalManager::IDGOAL_ORDER);
+    }
+
+    /**
+     * @param Request $request
+     * @param Visitor $visitor
+     * @param Action|null $action
+     * @param GoalManager $goalManager
+     *
+     * @return mixed|false
+     */
+    public function onGoalConversion(Request $request, Visitor $visitor, $action, GoalManager $goalManager)
+    {
+        $defaultRevenue = $goalManager->getGoalColumn('revenue');
+        $revenue        = $request->getGoalRevenue($defaultRevenue);
+
+        return $this->roundRevenueIfNeeded($revenue);
+    }
+
+    /**
+     * @param Request $request
+     * @param Visitor $visitor
+     * @param Action|null $action
+     * @param GoalManager $goalManager
+     *
+     * @return mixed|false
+     */
+    public function onEcommerceOrderConversion(Request $request, Visitor $visitor, $action, GoalManager $goalManager)
+    {
+        $defaultRevenue = 0;
+        $revenue = $request->getGoalRevenue($defaultRevenue);
+
+        return $this->roundRevenueIfNeeded($revenue);
+    }
+
+    /**
+     * @param Request $request
+     * @param Visitor $visitor
+     * @param Action|null $action
+     * @param GoalManager $goalManager
+     *
+     * @return mixed|false
+     */
+    public function onEcommerceCartUpdateConversion(Request $request, Visitor $visitor, $action, GoalManager $goalManager)
+    {
+        return $this->onEcommerceOrderConversion($request, $visitor, $action, $goalManager);
+    }
 
 }
--- plugins/Ecommerce/Columns/RevenueOrder.php
+++ plugins/Ecommerce/Columns/RevenueOrder.php
@@ -8,6 +8,12 @@
  */
 namespace Piwik\Plugins\Ecommerce\Columns;
 
+use Piwik\Columns\Discriminator;
+use Piwik\Tracker\Action;
+use Piwik\Tracker\GoalManager;
+use Piwik\Tracker\Request;
+use Piwik\Tracker\Visitor;
+
 class RevenueOrder extends BaseConversion
 {
     protected $columnName = 'revenue';
@@ -16,5 +22,55 @@ class RevenueOrder extends BaseConversion
     protected $category = 'Goals_Ecommerce';
     protected $nameSingular = 'Ecommerce_OrderRevenue';
     protected $segmentName = 'revenueOrder';
+
+    public function getDbDiscriminator()
+    {
+        return new Discriminator($this->dbTableName, 'idgoal', GoalManager::IDGOAL_ORDER);
+    }
+
+    /**
+     * @param Request $request
+     * @param Visitor $visitor
+     * @param Action|null $action
+     * @param GoalManager $goalManager
+     *
+     * @return mixed|false
+     */
+    public function onGoalConversion(Request $request, Visitor $visitor, $action, GoalManager $goalManager)
+    {
+        $defaultRevenue = $goalManager->getGoalColumn('revenue');
+        $revenue        = $request->getGoalRevenue($defaultRevenue);
+
+        return $this->roundRevenueIfNeeded($revenue);
+    }
+
+    /**
+     * @param Request $request
+     * @param Visitor $visitor
+     * @param Action|null $action
+     * @param GoalManager $goalManager
+     *
+     * @return mixed|false
+     */
+    public function onEcommerceOrderConversion(Request $request, Visitor $visitor, $action, GoalManager $goalManager)
+    {
+        $defaultRevenue = 0;
+        $revenue = $request->getGoalRevenue($defaultRevenue);
+
+        return $this->roundRevenueIfNeeded($revenue);
+    }
+
+    /**
+     * @param Request $request
+     * @param Visitor $visitor
+     * @param Action|null $action
+     * @param GoalManager $goalManager
+     *
+     * @return mixed|false
+     */
+    public function onEcommerceCartUpdateConversion(Request $request, Visitor $visitor, $action, GoalManager $goalManager)
+    {
+        return $this->onEcommerceOrderConversion($request, $visitor, $action, $goalManager);
+    }
 
 }
