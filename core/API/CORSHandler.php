68a0579a16a32926201a37d685ee45707692c063
Peter <peter@innocraft.com>
2022-04-28 14:07:26 +1200

    Revert "update cors check prority"
    This reverts commit a0d0681bc6ed6b79eb0e365154dc9699bc014d4a.

--- core/API/CORSHandler.php
+++ core/API/CORSHandler.php
@@ -28,17 +28,18 @@ class CORSHandler
 
         // set default header
         Common::sendHeader('Vary: Origin');
+        Common::sendHeader('Access-Control-Allow-Credentials: true');
         Common::sendHeader('Access-Control-Allow-Origin: *');
 
-        if (!empty($this->domains) && !in_array('*', $this->domains) && !in_array($_SERVER['HTTP_ORIGIN'],
-            $this->domains, true)) {
-            Common::stripHeader('Access-Control-Allow-Credentials');
-        }
 
         // when origin is set, response http origin as response
         if (!empty($_SERVER['HTTP_ORIGIN'])) {
-            Common::sendHeader('Access-Control-Allow-Credentials: true');
             Common::sendHeader('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+        }
+
+        if (!empty($this->domains) && !in_array('*', $this->domains) && !in_array($_SERVER['HTTP_ORIGIN'],
+            $this->domains, true)) {
+            Common::stripHeader('Access-Control-Allow-Origin');
         }
 
         if ($this->isPreFlightCorsRequest()) {
