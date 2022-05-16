dc7e2d4699d4fcfc16e969115c20f893e5c1c37f
Ben Burgess <88810029+bx80@users.noreply.github.com>
2022-05-17 11:45:27 +1200

    Minor text tweak

--- config/global.ini.php
+++ config/global.ini.php
@@ -800,8 +800,8 @@ enable_update_communication = 1
 ; If you may need to download GeoIP updates or other stuff using other protocols like ftp you may need to extend this list.
 allowed_outgoing_protocols = 'http,https'
 
-; This option force matomo marketplace and matomo api request use Https for security reason
-; If you have problem loading marketplace, please disable this config
+; This option forces matomo marketplace and matomo api requests to use Https for improved security
+; If you have a problem loading the marketplace, please disable this config option
 force_matomo_ssl_request = 1
 
 ; Comma separated list of plugin names for which console commands should be loaded (applies when Matomo is not installed yet)
