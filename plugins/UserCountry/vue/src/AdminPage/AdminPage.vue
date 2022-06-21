b2f75a47bcb20ed3be2754f8e6a8fc162cf772b0
diosmosis <diosmosis@users.noreply.github.com>
2022-06-21 16:09:29 -0700

    make Sparkline component work more like sparkline twig macro

---
+++ plugins/UserCountry/vue/src/AdminPage/AdminPage.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "AdminPage.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ plugins/UserCountry/vue/src/GetDistinctCountries/GetDistinctCountries.vue
@@ -0,0 +1,33 @@
+<!--
+  Matomo - free/libre analytics platform
+  @link https://matomo.org
+  @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+-->
+
+<template>
+  <ContentBlock>
+    <div class="sparkline">
+      <Sparkline :params="urlSparklineCountries"/>
+      <div>
+        {{ 'UserCountry_DistinctCountries'|translate("<strong>"~numberDistinctCountries|number~"</strong>")|raw }}
+      </div>
+    </div>
+    <br style="clear:left"/>
+  </ContentBlock>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { ContentBlock, Sparkline } from 'CoreHome';
+
+export default defineComponent({
+  props: {
+    // TODO
+  },
+  components: {
+    ContentBlock,
+    Sparkline,
+  },
+});
+</script>
