ac8ea46e7ad845615a220beb6df1f06db8a5fb7e
diosmosis <diosmosis@users.noreply.github.com>
2022-06-21 16:10:25 -0700

    move contents of twig templates that use angularjs to vue components

--- plugins/UserCountry/vue/src/GetDistinctCountries/GetDistinctCountries.vue
+++ plugins/UserCountry/vue/src/GetDistinctCountries/GetDistinctCountries.vue
@@ -8,26 +8,38 @@
   <ContentBlock>
     <div class="sparkline">
       <Sparkline :params="urlSparklineCountries"/>
-      <div>
-        {{ 'UserCountry_DistinctCountries'|translate("<strong>"~numberDistinctCountries|number~"</strong>")|raw }}
-      </div>
+      <div v-html="$sanitize(distinctCountriesText)"></div>
     </div>
     <br style="clear:left"/>
   </ContentBlock>
-
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { ContentBlock, Sparkline } from 'CoreHome';
+import { translate, ContentBlock, Sparkline } from 'CoreHome';
 
 export default defineComponent({
   props: {
-    // TODO
+    numberDistinctCountries: {
+      type: Number,
+      required: true,
+    },
+    urlSparklineCountries: {
+      type: [Object, String],
+      required: true,
+    },
   },
   components: {
     ContentBlock,
     Sparkline,
+  },
+  computed: {
+    distinctCountriesText() {
+      return translate(
+        'UserCountry_DistinctCountries',
+        `<strong>${this.numberDistinctCountries}</strong>`,
+      );
+    },
   },
 });
 </script>
