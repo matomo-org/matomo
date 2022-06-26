4e4903ebf8a610f96a4d8df0a7d8357673caa7d0
diosmosis <diosmosis@users.noreply.github.com>
2022-06-26 10:41:59 -0700

    start removing angularjs from Marketplace plugin

--- plugins/Marketplace/vue/src/index.ts
+++ plugins/Marketplace/vue/src/index.ts
@@ -10,3 +10,7 @@ import './PluginName/PluginName.adapter';
 export { default as Marketplace } from './Marketplace/Marketplace.vue';
 export { default as LicenseKey } from './LicenseKey/LicenseKey.vue';
 export { default as PluginName } from './PluginName/PluginName';
+export { default as UploadPluginDialog } from './UploadPluginDialog/UploadPluginDialog.vue';
+export { default as GetNewPlugins } from './GetNewPlugins/GetNewPlugins.vue';
+export { default as GetNewPluginsAdmin } from './GetNewPluginsAdmin/GetNewPluginsAdmin.vue';
+export { default as GetPremiumFeatures } from './GetPremiumFeatures/GetPremiumFeatures.vue';
