1a48c21fbdf2a2a1a9612e2871332a9869232fc0
Peter Zhang <peter@innocraft.com>
2022-06-27 14:32:12 +1200

    Update core/Updates/4.12.0-b1.php
    Co-authored-by: Ben Burgess <88810029+bx80@users.noreply.github.com>

--- core/Updates/4.12.0-b1.php
+++ core/Updates/4.12.0-b1.php
@@ -40,10 +40,10 @@ class Updates_4_12_0_b1 extends PiwikUpdates
 
         return [
           $this->migration->db->dropColumn('user', 'invite_status'),
-          $this->migration->db->addColumns('user', 'invite_token', 'VARCHAR( 191 ) DEFAULT null'),
-          $this->migration->db->addColumns('user', 'invited_by', 'VARCHAR( 100 ) DEFAULT null'),
-          $this->migration->db->addColumns('user', 'invite_expired_at', 'TIMESTAMP DEFAULT null'),
-          $this->migration->db->addColumns('user', 'invite_accept_at', 'TIMESTAMP DEFAULT null'),
+          $this->migration->db->addColumns('user', ['invite_token' => 'VARCHAR(191) DEFAULT null']),
+          $this->migration->db->addColumns('user', ['invited_by' => 'VARCHAR(100) DEFAULT null']),
+          $this->migration->db->addColumns('user', ['invite_expired_at' => 'TIMESTAMP null DEFAULT null']),
+          $this->migration->db->addColumns('user', ['invite_accept_at' => 'TIMESTAMP null DEFAULT null']),
 
         ];
     }
