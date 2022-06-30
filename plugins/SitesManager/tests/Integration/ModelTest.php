92896ae6d1814d8d084238e554c84836cfbb1b98
Stefan Giehl <stefan@matomo.org>
2022-06-30 11:58:26 +0200

    Provide possibility to configure referrer exclusion list (#19302)
    * Adds database column to store referrer exclusion
    
    * Adds global setting for referrer exclusion
    
    * Adds measurable setting to configure excluded referrers
    
    * adjust SitesManager tests
    
    * Implement referrer exclusion
    
    * Make referrer exclusion code easier to understand
    
    * fix tests
    
    * Adds validation for referrer exclusion
    
    * fix test
    
    * Implement referrer exclusion in tracker js
    
    * rebuilt tracker js
    
    * Adds some javascript tests
    
    * ensure setIgnoredReferrers is executed before tracking
    
    * adjust wording to excluded referrers (instead of ignored)
    
    * ignore www subdomain in javascript
    
    * Included excluded referrer in tracking code generator
    
    * Don't require protocol for excluded referrers and add better description
    
    * built vue files
    
    * update expected test files
    
    * updates expected UI files
    
    * add changelog
    
    * removes unused variable
    
    * Allow wildcard subdomains
    
    * updates expected test files
    
    * apply review feedback
    
    * Fix typo
    
    Co-authored-by: sgiehl <sgiehl@users.noreply.github.com>
    Co-authored-by: Ben Burgess <88810029+bx80@users.noreply.github.com>

--- plugins/SitesManager/tests/Integration/ModelTest.php
+++ plugins/SitesManager/tests/Integration/ModelTest.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Matomo - free/libre analytics platform
  *
@@ -31,21 +32,21 @@ class ModelTest extends IntegrationTestCase
         $this->model = new Model();
     }
 
-    public function test_getUsedTypeIds_shouldReturnNoType_IfNoSitesExist()
+    public function testGetUsedTypeIdsShouldReturnNoTypeIfNoSitesExist()
     {
-        $this->assertSame(array(), $this->model->getUsedTypeIds());
+        $this->assertSame([], $this->model->getUsedTypeIds());
     }
 
-    public function test_getUsedTypeIds_shouldReturnOnlyOneType_IfAllSitesUseSameType()
+    public function testGetUsedTypeIdsShouldReturnOnlyOneTypeIfAllSitesUseSameType()
     {
         for ($i = 0; $i < 9; $i++) {
             $this->createMeasurable('website');
         }
 
-        $this->assertSame(array('website'), $this->model->getUsedTypeIds());
+        $this->assertSame(['website'], $this->model->getUsedTypeIds());
     }
 
-    public function test_getUsedTypeIds_shouldReturnAnotherType_IfDifferentOnesAreUsed()
+    public function testGetUsedTypeIdsShouldReturnAnotherTypeIfDifferentOnesAreUsed()
     {
         for ($i = 0; $i < 9; $i++) {
             $this->createMeasurable('website');
@@ -53,10 +54,10 @@ class ModelTest extends IntegrationTestCase
             $this->createMeasurable('mobileapp');
         }
 
-        $this->assertSame(array('website', 'universal', 'mobileapp'), $this->model->getUsedTypeIds());
+        $this->assertSame(['website', 'universal', 'mobileapp'], $this->model->getUsedTypeIds());
     }
 
-    public function test_getAllKnownUrlsForAllSites_shouldReturnAllUrls()
+    public function testGetAllKnownUrlsForAllSitesShouldReturnAllUrls()
     {
         $idSite = $this->createMeasurable('website', 'http://apache.piwik');
         $this->model->insertSiteUrl($idSite, 'http://example.apache.piwik');
@@ -68,45 +69,52 @@ class ModelTest extends IntegrationTestCase
 
         $idSite3 = $this->createMeasurable('website', 'http://example.pro');
 
-        $expected = array(
-            array(
+        $expected = [
+            [
                 'idsite' => $idSite,
                 'url' => 'http://apache.piwik'
-            ),
-            array(
+            ],
+            [
                 'idsite' => $idSite2,
                 'url' => 'http://piwik.net'
-            ),
-            array(
+            ],
+            [
                 'idsite' => $idSite3,
                 'url' => 'http://example.pro'
-            ),
-            array(
+            ],
+            [
                 'idsite' => $idSite,
                 'url' => 'http://example.apache.piwik'
-            ),
-            array(
+            ],
+            [
                 'idsite' => $idSite,
                 'url' => 'http://example.org'
-            ),
-            array(
+            ],
+            [
                 'idsite' => $idSite2,
                 'url' => 'http://example.com'
-            ),
-            array(
+            ],
+            [
                 'idsite' => $idSite2,
                 'url' => 'http://example.org'
-            )
+            ]
 
-        );
+        ];
         $this->assertEquals($expected, $this->model->getAllKnownUrlsForAllSites());
     }
 
     private function createMeasurable($type, $siteUrl = false)
     {
-        return Fixture::createWebsite('2015-01-01 00:00:00',
-            $ecommerce = 0, $siteName = false, $siteUrl,
-            $siteSearch = 1, $searchKeywordParameters = null,
-            $searchCategoryParameters = null, $timezone = null, $type);
+        return Fixture::createWebsite(
+            '2015-01-01 00:00:00',
+            $ecommerce = 0,
+            $siteName = false,
+            $siteUrl,
+            $siteSearch = 1,
+            $searchKeywordParameters = null,
+            $searchCategoryParameters = null,
+            $timezone = null,
+            $type
+        );
     }
 }
