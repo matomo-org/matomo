72d1ded224dbe1e1c9386052e4bfecb172a4fe9b
diosmosis <diosmosis@users.noreply.github.com>
2022-07-07 15:45:18 -0700

    optimize math.js use

--- plugins/CorePluginsAdmin/vue/src/expressions.ts
+++ plugins/CorePluginsAdmin/vue/src/expressions.ts
@@ -5,48 +5,48 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
 */
 
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
 import {
   create as createMathJs,
-  addDependencies,
-  subtractDependencies,
-  multiplyDependencies,
-  divideDependencies,
-  equalDependencies,
-  notDependencies,
-  andDependencies,
-  orDependencies,
   evaluateDependencies,
-  largerDependencies,
-  largerEqDependencies,
-  smallerEqDependencies,
-  smallerDependencies,
-  unequalDependencies,
-} from 'mathjs';
+  // @ts-ignore
+} from 'mathjs/lib/esm/number';
 
 const math = createMathJs({
-  addDependencies,
-  subtractDependencies,
-  multiplyDependencies,
-  divideDependencies,
-  equalDependencies,
-  notDependencies,
-  andDependencies,
-  orDependencies,
   evaluateDependencies,
-  largerDependencies,
-  largerEqDependencies,
-  smallerEqDependencies,
-  smallerDependencies,
-  unequalDependencies,
 });
 
-// support natural equal for strings (or any variable)
+// add our own simple operators to avoid having to import math.js' ones, to keep the
+// generated size down.
 math.import(
   {
-    // eslint-disable-next-line eqeqeq
-    equal: (a: unknown, b: unknown) => a == b,
-    // eslint-disable-next-line eqeqeq
-    unequal: (a: unknown, b: unknown) => a != b,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    add: (a: any, b: any) => a + b,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    subtract: (a: any, b: any) => a - b,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    multiply: (a: any, b: any) => a * b,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    divide: (a: any, b: any) => a / b,
+    // eslint-disable-next-line
+    equal: (a: any, b: any) => a == b,
+    // eslint-disable-next-line
+    unequal: (a: any, b: any) => a != b,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    not: (a: any) => !a,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    and: (a: any, b: any) => a && b,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    or: (a: any, b: any) => a || b,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    largerEq: (a: any, b: any) => a >= b,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    larger: (a: any, b: any) => a > b,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    smallerEq: (a: any, b: any) => a <= b,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    smaller: (a: any, b: any) => a < b,
   },
   {
     override: true,
