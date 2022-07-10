7bfed9406491cdf9d2bbfaac5bc822f786a14fab
diosmosis <diosmosis@users.noreply.github.com>
2022-07-09 18:10:09 -0700

    get dashboard to load

--- plugins/CoreHome/javascripts/corehome.js
+++ plugins/CoreHome/javascripts/corehome.js
@@ -6,6 +6,7 @@
  */
 
 window.addEventListener('DOMContentLoaded', function () {
+  $(function () {
     //
     // 'check for updates' behavior
     //
@@ -16,112 +17,112 @@ window.addEventListener('DOMContentLoaded', function () {
 
     // when 'check for updates...' link is clicked, force a check & display the result
     headerMessageParent.on('click', '#updateCheckLinkContainer', function (e) {
-        var headerMessage = $(this).closest('#header_message');
+      var headerMessage = $(this).closest('#header_message');
 
-        var $titleElement = headerMessage.find('.title');
-        if ($titleElement.attr('target')) { // if this is an external link, internet access is not available on the server
-            return;
-        }
+      var $titleElement = headerMessage.find('.title');
+      if ($titleElement.attr('target')) { // if this is an external link, internet access is not available on the server
+        return;
+      }
 
-        e.preventDefault();
+      e.preventDefault();
 
-        var ajaxRequest = new ajaxHelper();
-        ajaxRequest.setLoadingElement('#header_message .loadingPiwik');
-        ajaxRequest.addParams({
-            module: 'CoreHome',
-            action: 'checkForUpdates'
-        }, 'get');
+      var ajaxRequest = new ajaxHelper();
+      ajaxRequest.setLoadingElement('#header_message .loadingPiwik');
+      ajaxRequest.addParams({
+        module: 'CoreHome',
+        action: 'checkForUpdates'
+      }, 'get');
 
-        ajaxRequest.withTokenInUrl();
+      ajaxRequest.withTokenInUrl();
 
-        $titleElement.addClass('activityIndicator');
+      $titleElement.addClass('activityIndicator');
 
-        ajaxRequest.setCallback(function (response) {
-            headerMessage.fadeOut('slow', function () {
-                response = $('#header_message', $('<div>' + response + '</div>'));
+      ajaxRequest.setCallback(function (response) {
+        headerMessage.fadeOut('slow', function () {
+          response = $('#header_message', $('<div>' + response + '</div>'));
 
-                $titleElement.removeClass('activityIndicator');
+          $titleElement.removeClass('activityIndicator');
 
-                var newVersionAvailable = response.hasClass('update_available');
-                if (newVersionAvailable) {
-                    headerMessage.replaceWith(response);
-                    headerMessage.show();
-                }
-                else {
-                    headerMessage.find('.title').html(_pk_translate('CoreHome_YouAreUsingTheLatestVersion'));
-                    headerMessage.show();
-                    setTimeout(function () {
-                        headerMessage.fadeOut('slow', function () {
-                            headerMessage.replaceWith(response);
-                        });
-                    }, 4000);
-                }
-            });
+          var newVersionAvailable = response.hasClass('update_available');
+          if (newVersionAvailable) {
+            headerMessage.replaceWith(response);
+            headerMessage.show();
+          } else {
+            headerMessage.find('.title').html(_pk_translate('CoreHome_YouAreUsingTheLatestVersion'));
+            headerMessage.show();
+            setTimeout(function () {
+              headerMessage.fadeOut('slow', function () {
+                headerMessage.replaceWith(response);
+              });
+            }, 4000);
+          }
         });
-        ajaxRequest.setFormat('html');
-        ajaxRequest.send();
+      });
+      ajaxRequest.setFormat('html');
+      ajaxRequest.send();
 
-        return false;
+      return false;
     });
 
     // when clicking the header message, show the long message w/o needing to hover
     headerMessageParent.on('click', '#header_message', function (e) {
-        if (e.target.tagName.toLowerCase() != 'a') {
-            $(this).toggleClass('expanded');
-        }
+      if (e.target.tagName.toLowerCase() != 'a') {
+        $(this).toggleClass('expanded');
+      }
     });
 
-});
+  });
 
 
-window.addEventListener('DOMContentLoaded', function () {
-    $('.accessibility-skip-to-content').click(function(e){
-        $('a[name="main"]').attr('tabindex', -1).focus();
-        $(window).scrollTo($('a[name="main"]'));
+  $(function () {
+    $('.accessibility-skip-to-content').click(function (e) {
+      $('a[name="main"]').attr('tabindex', -1).focus();
+      $(window).scrollTo($('a[name="main"]'));
     });
 
     $("#mobile-top-menu").sideNav({
-        closeOnClick: true,
-        edge: 'right'
+      closeOnClick: true,
+      edge: 'right'
     });
 
     $('.navbar.collapsible').collapsible();
 
     $('select').not('.ui-datepicker select').material_select();
 
-    piwikHelper.registerShortcut('?', _pk_translate('CoreHome_ShortcutHelp') , function (event) {
-        // don't open if an modal is already shown
-        if (event.altKey || $('.modal.open').length) {
-            return;
+    piwikHelper.registerShortcut('?', _pk_translate('CoreHome_ShortcutHelp'), function (event) {
+      // don't open if an modal is already shown
+      if (event.altKey || $('.modal.open').length) {
+        return;
+      }
+      if (event.preventDefault) {
+        event.preventDefault();
+      } else {
+        event.returnValue = false; // IE
+      }
+
+      var list = $('#shortcuthelp dl');
+      list.empty();
+
+      var keys = Object.keys(piwikHelper.shortcuts).sort();
+
+      jQuery.each(keys, function (i, key) {
+        if (piwikHelper.shortcuts.hasOwnProperty(key)) {
+          list.append($('<dt />').append($('<kbd />').text(key)));
+          list.append($('<dd />').text(piwikHelper.shortcuts[key]));
         }
-        if (event.preventDefault) {
-            event.preventDefault();
-        } else {
-            event.returnValue = false; // IE
-        }
+      });
 
-        var list = $('#shortcuthelp dl');
-        list.empty();
+      var isMac = navigator.userAgent.indexOf('Mac OS X') != -1;
 
-        var keys = Object.keys(piwikHelper.shortcuts).sort();
+      list.append($('<dt />').append($('<kbd />').text(_pk_translate(isMac ? "CoreHome_MacPageUp" : "CoreHome_HomeShortcut"))));
 
-        jQuery.each(keys, function(i, key) {
-            if (piwikHelper.shortcuts.hasOwnProperty(key)) {
-                list.append($('<dt />').append($('<kbd />').text(key)));
-                list.append($('<dd />').text(piwikHelper.shortcuts[key]));
-            }
-        });
+      list.append($('<dd />').text(_pk_translate('CoreHome_PageUpShortcutDescription')));
 
-        var isMac = navigator.userAgent.indexOf('Mac OS X') != -1;
+      list.append($('<dt />').append($('<kbd />').text(_pk_translate(isMac ? "CoreHome_MacPageDown" : "CoreHome_EndShortcut"))));
 
-        list.append($('<dt />').append($('<kbd />').text(_pk_translate(isMac ? "CoreHome_MacPageUp" : "CoreHome_HomeShortcut"))));
+      list.append($('<dd />').text(_pk_translate('CoreHome_PageDownShortcutDescription')));
 
-        list.append($('<dd />').text(_pk_translate('CoreHome_PageUpShortcutDescription')));
-
-        list.append($('<dt />').append($('<kbd />').text(_pk_translate(isMac ? "CoreHome_MacPageDown" : "CoreHome_EndShortcut"))));
-
-        list.append($('<dd />').text(_pk_translate('CoreHome_PageDownShortcutDescription')));
-
-        piwikHelper.modalConfirm('#shortcuthelp');
+      piwikHelper.modalConfirm('#shortcuthelp');
     });
+  });
 });
--- plugins/Login/javascripts/login.js
+++ plugins/Login/javascripts/login.js
@@ -5,36 +5,37 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 window.addEventListener('DOMContentLoaded', function () {
+  $(function () {
     var switchForm = function (fromFormId, toFormId) {
-        var fromFormSelector = '#' + fromFormId;
-        var toFormSelector = '#' + toFormId;
+      var fromFormSelector = '#' + fromFormId;
+      var toFormSelector = '#' + toFormId;
 
-        var fromLoginInputId = fromFormSelector + '_login',
-            toLoginInputId = toFormSelector + '_login',
-            toPasswordInputId = toFormSelector + '_password';
+      var fromLoginInputId = fromFormSelector + '_login',
+        toLoginInputId = toFormSelector + '_login',
+        toPasswordInputId = toFormSelector + '_password';
 
-        if ($(toLoginInputId).val() === '') {
-            $(toLoginInputId).val($(fromLoginInputId).val());
-        }
+      if ($(toLoginInputId).val() === '') {
+        $(toLoginInputId).val($(fromLoginInputId).val());
+      }
 
-        var contentFrom = $(fromFormSelector).parents('.contentForm').first();
-        var contentTo = $(toFormSelector).parents('.contentForm').first();
+      var contentFrom = $(fromFormSelector).parents('.contentForm').first();
+      var contentTo = $(toFormSelector).parents('.contentForm').first();
 
-        // hide the bottom portion of the login screen & show the password reset bits
-        $(contentFrom).fadeOut(500, function () {
-            // focus on login or password control based on whether a login exists
-            Materialize.updateTextFields();
+      // hide the bottom portion of the login screen & show the password reset bits
+      $(contentFrom).fadeOut(500, function () {
+        // focus on login or password control based on whether a login exists
+        Materialize.updateTextFields();
 
-            $(contentTo).fadeIn(500, function () {
+        $(contentTo).fadeIn(500, function () {
 
-                if ($(toLoginInputId).val() === '') {
-                    $(toLoginInputId).focus();
-                } else {
-                    $(toPasswordInputId).focus();
-                }
+          if ($(toLoginInputId).val() === '') {
+            $(toLoginInputId).focus();
+          } else {
+            $(toPasswordInputId).focus();
+          }
 
-            });
         });
+      });
     };
 
     // set login form redirect url
@@ -42,58 +43,61 @@ window.addEventListener('DOMContentLoaded', function () {
 
     // 'lost your password?' on click
     $('#login_form_nav').click(function (e) {
-        e.preventDefault();
-        switchForm('login_form', 'reset_form');
-        return false;
+      e.preventDefault();
+      switchForm('login_form', 'reset_form');
+      return false;
     });
 
     // 'cancel' on click
     $('#reset_form_nav,#alternate_reset_nav').click(function (e) {
-        e.preventDefault();
-        switchForm('reset_form', 'login_form');
-        return false;
+      e.preventDefault();
+      switchForm('reset_form', 'login_form');
+      return false;
     });
 
     // password reset on submit
     $('#reset_form_submit').click(function (e) {
-        e.preventDefault();
+      e.preventDefault();
 
-        var ajaxDone = function (response) {
-            $('.loadingPiwik').hide();
+      var ajaxDone = function (response) {
+        $('.loadingPiwik').hide();
 
-            var isSuccess = response.indexOf('form-errors="null"') !== -1,
-                fadeOutIds = '.resetForm .message_container';
-            if (isSuccess) {
-                fadeOutIds += ',#reset_form,#reset_form_nav';
-            }
+        var isSuccess = response.indexOf('form-errors="null"') !== -1,
+          fadeOutIds = '.resetForm .message_container';
+        if (isSuccess) {
+          fadeOutIds += ',#reset_form,#reset_form_nav';
+        }
 
-            $(fadeOutIds).fadeOut(300, function () {
-                if (isSuccess) {
-                    $('#alternate_reset_nav').show();
-                }
+        $(fadeOutIds).fadeOut(300, function () {
+          if (isSuccess) {
+            $('#alternate_reset_nav').show();
+          }
 
-                $('.resetForm .message_container').html(response).fadeIn(300);
-                piwikHelper.compileVueEntryComponents($('.resetForm .message_container'));
-            });
-        };
-
-        $('.loadingPiwik').show();
-
-        // perform reset password request
-        $.ajax({
-            type: 'POST',
-            url: 'index.php',
-            dataType: 'html',
-            async: true,
-            error: function () { ajaxDone('<div id="login_error"><strong>HTTP Error</strong></div>'); },
-            success: ajaxDone,	// Callback when the request succeeds
-            data: $('#reset_form').serialize()
+          $('.resetForm .message_container').html(response).fadeIn(300);
+          piwikHelper.compileVueEntryComponents($('.resetForm .message_container'));
         });
+      };
 
-        return false;
+      $('.loadingPiwik').show();
+
+      // perform reset password request
+      $.ajax({
+        type: 'POST',
+        url: 'index.php',
+        dataType: 'html',
+        async: true,
+        error: function () {
+          ajaxDone('<div id="login_error"><strong>HTTP Error</strong></div>');
+        },
+        success: ajaxDone,	// Callback when the request succeeds
+        data: $('#reset_form').serialize()
+      });
+
+      return false;
     });
 
     $('#login_form_login').focus();
 
     Materialize.updateTextFields();
+  });
 });
