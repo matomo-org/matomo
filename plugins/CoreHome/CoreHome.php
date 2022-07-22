4641280a0abe1482410d1106f2025e0647d2cab3
Peter <peter@innocraft.com>
2022-07-22 14:38:30 +1200

    update screenshots and add translation
    update screenshots and add translation

--- plugins/CoreHome/CoreHome.php
+++ plugins/CoreHome/CoreHome.php
@@ -378,7 +378,8 @@ class CoreHome extends \Piwik\Plugin
         $translationKeys[] = 'CoreHome_ShortcutWebsiteSelector';
         $translationKeys[] = 'CoreHome_ShortcutCalendar';
         $translationKeys[] = 'CoreHome_ShortcutSearch';
-        $translationKeys[] = 'CoreHome_ShortcutHelp';
+        $translationKeys[] = 'CoreHome_ShortcutSearch';
+        $translationKeys[] = 'CoreHome_ShortcutRefresh';
         $translationKeys[] = 'CoreHome_StandardReport';
         $translationKeys[] = 'CoreHome_ReportWithMetadata';
         $translationKeys[] = 'CoreHome_ReportType';
