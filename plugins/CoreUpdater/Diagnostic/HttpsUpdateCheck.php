162dcecc55359a6dcc3e7890e150515d04d2e60e
Peter <peter@innocraft.com>
2022-07-22 14:29:23 +1200

    remove translations
    remove translations

--- plugins/CoreUpdater/Diagnostic/HttpsUpdateCheck.php
+++ plugins/CoreUpdater/Diagnostic/HttpsUpdateCheck.php
@@ -5,6 +5,7 @@
  * @link https://matomo.org
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
+
 namespace Piwik\Plugins\CoreUpdater\Diagnostic;
 
 use Piwik\Config\GeneralConfig;
@@ -42,13 +43,11 @@ class HttpsUpdateCheck implements Diagnostic
             return array(DiagnosticResult::singleResult($label, DiagnosticResult::STATUS_INFORMATIONAL, $comment));
         } elseif (!Http::isUpdatingOverHttps()) {
             // failed to request over https
-            $comment = $this->translator->translate('Installation_MatomoHttpsNotSupportWarning', $faqLinks);
-            return array(DiagnosticResult::singleResult($label, DiagnosticResult::STATUS_WARNING, $comment));
+            $warning = 'We will soon switch to HTTPS by default. Please make sure that HTTPS works on your environment or turn on `force_matomo_http_request`, otherwise it could cause Matomo updates to fail in the future. For more details please read our <a href="https://matomo.org/faq/faq-how-to-disable-https-for-matomo-org-and-api-matomo-org-requests" rel="noreferrer noopener" target="_blank">FAQ</a>.';
+            return array(DiagnosticResult::singleResult($label, DiagnosticResult::STATUS_WARNING, $warning));
         } else {
-
             // successful using https
             return array(DiagnosticResult::singleResult($label, DiagnosticResult::STATUS_OK));
         }
-
     }
 }
