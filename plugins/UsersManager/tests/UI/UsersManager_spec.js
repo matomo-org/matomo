6cc723685d3c1c9b399b09e9b691b9f2dba80bce
Stefan Giehl <stefan@matomo.org>
2022-07-25 10:58:34 +0200

    Refactor and improve password confirmation in UI & API (#19525)
    * Extract password confirmation into own vue component
    
    * Require password confirmation for Anonymize data section
    
    * Adjust tests
    
    * Move password confirmation method to abstract class
    
    * Use new password confirmation component also for setting super user access
    
    * Use new password confirmation component also for changing user & 2FA reset
    
    * Introduce new event Login.userRequiresPasswordConfirmation to allow plugins to disable the password confirmation for users
    
    * adjust ui tests
    
    * Use new password confirmation component also for user settings
    
    * cleans up some vue code
    
    * built vue files
    
    * clean up php code
    
    * improve 2fa tab & adjust tests
    
    * add warning to event
    
    Co-authored-by: sgiehl <sgiehl@users.noreply.github.com>

--- plugins/UsersManager/tests/UI/UsersManager_spec.js
+++ plugins/UsersManager/tests/UI/UsersManager_spec.js
@@ -439,15 +439,15 @@ describe("UsersManager", function () {
 
     it('should show superuser confirm modal when the superuser toggle is clicked', async function () {
         await page.click('.userEditForm #superuser_access+span');
-        await page.waitForSelector('.superuser-confirm-modal');
+        await page.waitForSelector('.modal.open');
         await page.waitForTimeout(500);
 
-        expect(await page.screenshotSelector('.superuser-confirm-modal')).to.matchImage('superuser_confirm');
+        expect(await page.screenshotSelector('.modal.open')).to.matchImage('superuser_confirm');
     });
 
     it('should fail to set superuser access if password is wrong', async function () {
-        await page.type('input#currentUserPasswordForSuperUser', 'wrongpassword');
-        await (await page.jQuery('.superuser-confirm-modal .modal-close:not(.modal-no):visible')).click();
+        await page.type('.modal.open #currentUserPassword', 'wrongpassword');
+        await (await page.jQuery('.modal.open .modal-close:not(.modal-no):visible')).click();
         await page.waitForNetworkIdle();
 
         await page.waitForSelector('.notification-error', { visible: true });
@@ -460,8 +460,8 @@ describe("UsersManager", function () {
         await page.click('.userEditForm #superuser_access+span');
         await page.waitForTimeout(500);
 
-        await page.type('input#currentUserPasswordForSuperUser', 'superUserPass');
-        await (await page.jQuery('.superuser-confirm-modal .modal-close:not(.modal-no):visible')).click();
+        await page.type('.modal.open #currentUserPassword', 'superUserPass');
+        await (await page.jQuery('.modal.open .modal-close:not(.modal-no):visible')).click();
         await page.waitForNetworkIdle();
         await page.waitForTimeout(500);
 
@@ -508,9 +508,9 @@ describe("UsersManager", function () {
     });
 
     it('should show error when wrong password entered', async function () {
-        await page.type('.modal.open #currentUserPasswordChangePwd', 'test123456');
+        await page.type('.modal.open #currentUserPassword', 'test123456');
 
-        var btnNo = await page.jQuery('.change-password-modal .modal-close:not(.modal-no):visible');
+        var btnNo = await page.jQuery('.confirm-password-modal .modal-close:not(.modal-no):visible');
         await btnNo.click();
 
         await page.waitForTimeout(500); // animation
