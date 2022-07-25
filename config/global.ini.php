797b60687a65040b87d4c9cd459a3163ab13d1d8
Peter Zhang <peter@innocraft.com>
2022-07-25 12:07:27 +1200

    Update HTTPs security check, when client is using HTTP just throw a warning on diagnostic (#19527)
    * default Matomo request to http
    
    default Matomo request to http
    
    * Update HttpsUpdateCheck.php
    
    update checks
    
    * Update plugins/Marketplace/config/config.php
    
    Co-authored-by: Justin Velluppillai <justin@innocraft.com>
    
    * update lang and links
    
    update lang and links
    
    * Update config.php
    
    update phpcs
    
    * Update config.php
    
    update checks
    
    * Update plugins/Installation/lang/en.json
    
    Co-authored-by: Stefan Giehl <stefan@matomo.org>
    
    * Update Client.php
    
    revert code style changes
    
    * update checks and changes
    
    update checks and changes
    
    * Update UIIntegrationTest_admin_diagnostics_configfile.png
    
    update screenshots
    
    * Update plugins/Installation/lang/en.json
    
    Co-authored-by: Justin Velluppillai <justin@innocraft.com>
    
    * Update HttpsUpdateCheck.php
    
    update link
    
    * Update config/global.ini.php
    
    * update https to http as default
    
    update https to http as default
    
    * Update plugins/Installation/lang/en.json
    
    Co-authored-by: Justin Velluppillai <justin@innocraft.com>
    
    * remove https
    
    remove https
    
    * Update config.php
    
    fix phpcs
    
    * remove translations
    
    remove translations
    
    * remove comma
    
    * Update en.json
    
    update lang
    
    * Update UIIntegrationTest_admin_diagnostics_configfile.png
    
    update ui
    
    Co-authored-by: Justin Velluppillai <justin@innocraft.com>
    Co-authored-by: Stefan Giehl <stefan@matomo.org>

--- config/global.ini.php
+++ config/global.ini.php
@@ -709,7 +709,7 @@ enable_trusted_host_check = 1
 ; The API server is an essential part of the Matomo infrastructure/ecosystem to
 ; provide services to Matomo installations, e.g., getLatestVersion and
 ; subscribeNewsletter.
-api_service_url = https://api.matomo.org
+api_service_url = http://api.matomo.org
 
 ; When the ImageGraph plugin is activated, report metadata have an additional entry : 'imageGraphUrl'.
 ; This entry can be used to request a static graph for the requested report.
@@ -803,9 +803,9 @@ enable_update_communication = 1
 ; If you may need to download GeoIP updates or other stuff using other protocols like ftp you may need to extend this list.
 allowed_outgoing_protocols = 'http,https'
 
-; This option forces matomo marketplace and matomo api requests to use Https for improved security
-; If you have a problem loading the marketplace, please disable this config option
-force_matomo_ssl_request = 1
+; This option forces matomo marketplace and matomo api requests to use HTTP, as default we use HTTPS to improve security
+; If you have a problem loading the marketplace, please enable this config option
+force_matomo_http_request = 0
 
 ; Comma separated list of plugin names for which console commands should be loaded (applies when Matomo is not installed yet)
 always_load_commands_from_plugin=
