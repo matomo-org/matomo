908ea17c764255239c4e6573189b65c637700b1f
Ben Burgess <88810029+bx80@users.noreply.github.com>
2022-08-02 21:43:26 +1200

    Config option to specify database error codes to ignore during updates (#19578)
    * Added config option for database error codes to ignore during updates
    
    * Update config/global.ini.php
    
    Co-authored-by: Stefan Giehl <stefan@matomo.org>
    
    * Update core/Updater/Migration/Db/Sql.php
    
    Co-authored-by: Stefan Giehl <stefan@matomo.org>
    
    Co-authored-by: Stefan Giehl <stefan@matomo.org>

--- config/global.ini.php
+++ config/global.ini.php
@@ -45,6 +45,10 @@ ssl_no_verify =
 ; Matomo should work correctly without this setting but we recommend to have a charset set.
 charset = utf8
 
+; Database error codes to ignore during updates
+;
+;ignore_error_codes[] = 1105
+
 ; If configured, the following queries will be executed on the reader instead of the writer.
 ; * archiving queries that hit a log table
 ; * live queries that hit a log table
