e17f3936c7fbe75774b3595db974d9a185ec307a
dizzy <diosmosis@users.noreply.github.com>
2022-08-04 08:49:13 -0500

    [Vue] remove use of angularjs from Ecommerce plugin (#19432)
    * remove use of angularjs from Ecommerce plugin
    
    * built vue files
    
    * Allow composer plugins for dev dependency codesniffer to fix travis builds.
    
    * fix spacing issue
    
    * update expected screenshots
    
    * remove use of unneeded html_attr twig escape filter [ci skip]
    
    * fix component name
    
    * remove obsolete files
    
    * updates expected test files
    
    Co-authored-by: Stefan Giehl <stefan@matomo.org>

--- plugins/CoreVue/types/index.d.ts
+++ plugins/CoreVue/types/index.d.ts
@@ -207,6 +207,10 @@ declare global {
     new (actionType: string, actionName: string, rowAction: unknown|null, overrideParams: string): Transitions;
   }
 
+  interface SegmentedVisitorLogService {
+    show(apiMethod: string, segment: string, extraParams: Record<string|number, unknown>): void;
+  }
+
   interface Window {
     angular: IAngularStatic;
     globalAjaxQueue: GlobalAjaxQueue;
@@ -222,6 +226,7 @@ declare global {
     Piwik_Popover: PiwikPopoverGlobal;
     NumberFormatter: NumberFormatter;
     Piwik_Transitions: TransitionsGlobal;
+    SegmentedVisitorLog: SegmentedVisitorLogService;
 
     _pk_translate(translationStringId: string, values: (string|number|boolean)[]): string;
     require(p: string): any;
