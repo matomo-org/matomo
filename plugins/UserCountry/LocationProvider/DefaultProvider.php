e97f7f84ed792d6e7a9df51a7c2cec6e5c026531
sgiehl <stefan@matomo.org>
2022-08-10 09:32:46 +0200

    Improve check fpr provider plugin

--- plugins/UserCountry/LocationProvider/DefaultProvider.php
+++ plugins/UserCountry/LocationProvider/DefaultProvider.php
@@ -62,6 +62,7 @@ class DefaultProvider extends LocationProvider
     {
         if (
             !Manager::getInstance()->isPluginInstalled('Provider')
+            || !class_exists('Piwik\Plugins\Provider\Provider')
             || Common::getRequestVar('dp', 0, 'int') === 1
         ) {
             return false;
