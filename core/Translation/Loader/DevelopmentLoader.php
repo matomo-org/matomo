4b0ebec38d5472e6ae90e9bd6a0d362e906182c5
Lukas Winkler <git@lw1.at>
2022-08-14 16:45:54 +0200

    very rough draft of Pseudo Localization in Matomo

--- core/Translation/Loader/DevelopmentLoader.php
+++ core/Translation/Loader/DevelopmentLoader.php
@@ -16,6 +16,66 @@ class DevelopmentLoader implements LoaderInterface
     const LANGUAGE_ID = 'dev';
 
     /**
+     * from https://github.com/tryggvigy/pseudo-localization/blob/master/src/localize.js
+     * by Tryggvi Gylfason
+     * MIT license
+     */
+    const MAP = [
+        "a" => 'ȧ',
+        "A" => 'Ȧ',
+        "b" => 'ƀ',
+        "B" => 'Ɓ',
+        "c" => 'ƈ',
+        "C" => 'Ƈ',
+        "d" => 'ḓ',
+        "D" => 'Ḓ',
+        "e" => 'ḗ',
+        "E" => 'Ḗ',
+        "f" => 'ƒ',
+        "F" => 'Ƒ',
+        "g" => 'ɠ',
+        "G" => 'Ɠ',
+        "h" => 'ħ',
+        "H" => 'Ħ',
+        "i" => 'ī',
+        "I" => 'Ī',
+        "j" => 'ĵ',
+        "J" => 'Ĵ',
+        "k" => 'ķ',
+        "K" => 'Ķ',
+        "l" => 'ŀ',
+        "L" => 'Ŀ',
+        "m" => 'ḿ',
+        "M" => 'Ḿ',
+        "n" => 'ƞ',
+        "N" => 'Ƞ',
+        "o" => 'ǿ',
+        "O" => 'Ǿ',
+        "p" => 'ƥ',
+        "P" => 'Ƥ',
+        "q" => 'ɋ',
+        "Q" => 'Ɋ',
+        "r" => 'ř',
+        "R" => 'Ř',
+//        "s" => 'ş', // avoid breaking format strings
+        "S" => 'Ş',
+        "t" => 'ŧ',
+        "T" => 'Ŧ',
+        "v" => 'ṽ',
+        "V" => 'Ṽ',
+        "u" => 'ŭ',
+        "U" => 'Ŭ',
+        "w" => 'ẇ',
+        "W" => 'Ẇ',
+        "x" => 'ẋ',
+        "X" => 'Ẋ',
+        "y" => 'ẏ',
+        "Y" => 'Ẏ',
+        "z" => 'ẑ',
+        "Z" => 'Ẑ',
+    ];
+
+    /**
      * Decorated loader.
      *
      * @var LoaderInterface
@@ -44,7 +104,7 @@ class DevelopmentLoader implements LoaderInterface
             return $this->loader->load($language, $directories);
         }
 
-        return $this->getDevelopmentTranslations($directories);
+        return $this->getPseudoLocale($directories);
     }
 
     private function getDevelopmentTranslations(array $directories)
@@ -52,12 +112,28 @@ class DevelopmentLoader implements LoaderInterface
         $fallbackTranslations = $this->loader->load($this->fallbackLanguage, $directories);
 
         $translations = array();
-
         foreach ($fallbackTranslations as $section => $sectionFallbackTranslations) {
             $translationIds = array_keys($sectionFallbackTranslations);
             $sectionTranslations = $this->prefixTranslationsWithSection($section, $translationIds);
 
             $translations[$section] = array_combine($translationIds, $sectionTranslations);
+        }
+
+        return $translations;
+    }
+
+    private function getPseudoLocale(array $directories)
+    {
+        $fallbackTranslations = $this->loader->load($this->fallbackLanguage, $directories);
+
+        $translations = array();
+        foreach ($fallbackTranslations as $section => $sectionFallbackTranslations) {
+
+            $translations[$section] = array_map(function ($translation) {
+                $accented = strtr($translation, self::MAP);
+                return "[" . $accented . "]";
+
+            }, $sectionFallbackTranslations);
         }
 
         return $translations;
