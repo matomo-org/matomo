309332f21128a922162d508b44e2e017e0373377
dizzy <diosmosis@users.noreply.github.com>
2022-08-16 05:03:54 -0500

    [Vue] remove angularjs from marketplace (#19416)
    * start removing angularjs from Marketplace plugin
    
    * finish removing angularjs from marketplace and some twig templates in CorePluginsAdmin
    
    * built vue files
    
    * fixing UI tests locally
    
    * add missing translations
    
    * couple small UI test fixes
    
    * move UploadPluginDialog to CorePluginsAdmin so it will be available when internet access is not available and the Marketplace plugin is deactivated
    
    * revert last tweak
    
    * styling tweak
    
    * Allow composer plugins for dev dependency codesniffer to fix travis builds.
    
    * styling tweaks
    
    * comma spacing tweak
    
    * update screenshots + fix v-show condition
    
    * remove duplicate tokens
    
    * remove use of unneeded twig escape filter html_attr
    
    * apply review feedback + fix some other issues found while testing quickly
    
    * updates expected test files
    
    Co-authored-by: sgiehl <stefan@matomo.org>

---
+++ plugins/CorePluginsAdmin/vue/src/PluginsIntro/PluginsIntro.vue
@@ -0,0 +1,86 @@
+<!--
+  Matomo - free/libre analytics platform
+  @link https://matomo.org
+  @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+-->
+
+<template>
+  <div v-content-intro>
+    <h2>
+      <EnrichedHeadline>{{ translate('CorePluginsAdmin_PluginsManagement') }}</EnrichedHeadline>
+    </h2>
+
+    <p>
+      {{ translate('CorePluginsAdmin_PluginsExtendPiwik') }}
+      {{ translate('CorePluginsAdmin_OncePluginIsInstalledYouMayActivateHere') }}
+
+      <span
+        v-if="isMarketplaceEnabled || isPluginUploadEnabled"
+        v-html="$sanitize(teaserExtendMatomoByPluginText)"
+        style="margin-right:3.5px"
+      ></span>
+
+      <span v-if="!isPluginsAdminEnabled" style="margin-right:3.5px">
+        <br/>{{ translate('CorePluginsAdmin_DoMoreContactPiwikAdmins') }}
+      </span>
+
+      <span v-html="$sanitize(changeLookByManageThemesText)"></span>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import {
+  ContentIntro,
+  EnrichedHeadline,
+  translate,
+  MatomoUrl,
+} from 'CoreHome';
+
+export default defineComponent({
+  props: {
+    isMarketplaceEnabled: Boolean,
+    isPluginUploadEnabled: Boolean,
+    isPluginsAdminEnabled: Boolean,
+  },
+  components: {
+    EnrichedHeadline,
+  },
+  directives: {
+    ContentIntro,
+  },
+  computed: {
+    teaserExtendMatomoByPluginText() {
+      const link = `?${MatomoUrl.stringify({
+        ...MatomoUrl.urlParsed.value,
+        module: 'Marketplace',
+        action: 'overview',
+        sort: null,
+        activated: null,
+      })}`;
+
+      return translate(
+        'CorePluginsAdmin_TeaserExtendPiwikByPlugin',
+        `<a href="${link}">`,
+        '</a>',
+        '<a href="#" class="uploadPlugin">',
+        '</a>',
+      );
+    },
+    changeLookByManageThemesText() {
+      const link = `?${MatomoUrl.stringify({
+        ...MatomoUrl.urlParsed.value,
+        action: 'themes',
+        activated: null,
+      })}`;
+
+      return translate(
+        'CorePluginsAdmin_ChangeLookByManageThemes',
+        `<a href="${link}">`,
+        '</a>',
+      );
+    },
+  },
+});
+</script>
