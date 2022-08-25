f679725746eab469f3b3d7d0d6eac0d71e53002b
dizzy <diosmosis@users.noreply.github.com>
2022-08-25 05:46:53 -0700

    [Vue] remove angularjs from the Live plugin (#19418)
    * Remove use of angularjs from SitesManager plugin and add VueEntryContainer for more convenient embedding of vue-entry use from within other Vue components (primarily for supporting twig Template... events in Vue).
    
    * remove use of angularjs from Live plugin
    
    * built vue files
    
    * forgot to export
    
    * built vue files
    
    * fix ui test issues
    
    * move inline script to vue directive + fix a couple vue warnings
    
    * remove use of unneeded html_attr escape
    
    * remove use of unneeded twig html_attr escape
    
    * use html filter explicitly
    
    * use href so command+click works
    
    * get rid of two vue warnings
    
    * use anonymous function for jquery event so this can be used properly
    
    * destroy/compile vue components when refreshing live widget
    
    * built vue files
    
    * try to fix live refresh issue
    
    * improve css, so whitespaces are always hidden between images
    
    Co-authored-by: sgiehl <stefan@matomo.org>
    Co-authored-by: sgiehl <sgiehl@users.noreply.github.com>

---
+++ plugins/Live/vue/src/LivePage/LivePage.vue
@@ -0,0 +1,103 @@
+<!--
+  Matomo - free/libre analytics platform
+  @link https://matomo.org
+  @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+-->
+
+<template>
+  <div>
+    <component
+      :is="!isWidgetized ? 'ContentBlock' : 'Passthrough'"
+      :content-title="!isWidgetized ? translate('Live_VisitorsInRealTime') : undefined"
+    >
+      <div v-live-widget-refresh="{liveRefreshAfterMs: liveRefreshAfterMs}">
+        <VueEntryContainer :html="initialTotalVisitors"/>
+        <VueEntryContainer :html="visitors"/>
+      </div>
+
+      <div class="visitsLiveFooter">
+        <a
+          :title="translate('Live_OnClickPause', translate('Live_VisitorsInRealTime'))"
+          @click.prevent="onClickPause()"
+        >
+          <img id="pauseImage" border="0" src="plugins/Live/images/pause.png" />
+        </a>
+        <a
+          :title="translate('Live_OnClickStart', translate('Live_VisitorsInRealTime'))"
+          @click="onClickPlay();"
+        >
+          <img
+            id="playImage"
+            style="display: none;"
+            border="0"
+            src="plugins/Live/images/play.png"
+          />
+        </a>
+        <span v-if="!disableLink">
+        &nbsp;
+        <a
+          class="rightLink"
+          :href="visitorLogUrl"
+        >
+          {{ translate('Live_LinkVisitorLog') }}
+        </a>
+        </span>
+      </div>
+    </component>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import {
+  ContentBlock,
+  Passthrough,
+  MatomoUrl,
+  VueEntryContainer,
+} from 'CoreHome';
+import LiveWidgetRefresh from '../LiveWidget/LiveWidgetRefresh';
+import TotalVisitors from '../TotalVisitors/TotalVisitors.vue';
+
+declare global {
+  interface Window {
+    onClickPause(): void;
+    onClickPlay(): void;
+  }
+}
+
+export default defineComponent({
+  props: {
+    disableLink: Boolean,
+    visitors: String,
+    initialTotalVisitors: String,
+    liveRefreshAfterMs: Number,
+    isWidgetized: Boolean,
+  },
+  components: {
+    TotalVisitors,
+    VueEntryContainer,
+    ContentBlock,
+    Passthrough,
+  },
+  directives: {
+    LiveWidgetRefresh,
+  },
+  computed: {
+    visitorLogUrl() {
+      return `#?${MatomoUrl.stringify({
+        ...MatomoUrl.hashParsed.value,
+        category: 'General_Visitors',
+        subcategory: 'Live_VisitorLog',
+      })}`;
+    },
+  },
+  methods: {
+    onClickPause() {
+      window.onClickPause();
+    },
+    onClickPlay() {
+      window.onClickPlay();
+    },
+  },
+});
+</script>
