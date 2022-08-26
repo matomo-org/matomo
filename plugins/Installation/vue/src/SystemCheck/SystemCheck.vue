e33e09422561e507c7440a9afdd3f0b9827f72eb
dizzy <diosmosis@users.noreply.github.com>
2022-08-26 00:59:02 -0700

    [Vue] remove angularjs from Installation plugin (#19425)
    * Remove use of angularjs from SitesManager plugin and add VueEntryContainer for more convenient embedding of vue-entry use from within other Vue components (primarily for supporting twig Template... events in Vue).
    
    * remove use of angularjs from Live plugin
    
    * built vue files
    
    * forgot to export
    
    * built vue files
    
    * start removing angularjs from Installation
    
    * remove angularjs from Installation plugin
    
    * allow ContentIntro to be applied conditionally
    
    * make Diagnostic results json serializable
    
    * built vue files
    
    * fix ui test issues
    
    * fix spacing
    
    * couple fixes
    
    * more ui test fixes
    
    * make sure installation umd is loaded during installation (as well as translations)
    
    * do not use html_attr to escape and in UI tests replace path with escaped slashes so json data gets replaced
    
    * update screenshots
    
    * Allow composer plugins for dev dependency codesniffer to fix travis builds.
    
    * move inline script to vue directive + fix a couple vue warnings
    
    * remove inline script in system check widget
    
    * use common method to load umd file
    
    * remove use of unneeded html_attr escape
    
    * remove use of unneeded twig html_attr escape
    
    * use html filter explicitly
    
    * use href so command+click works
    
    * get rid of two vue warnings
    
    * use anonymous function for jquery event so this can be used properly
    
    * destroy/compile vue components when refreshing live widget
    
    * update expected screenshots
    
    * remove incorrectly added use
    
    * include client side translations in twig template layout for Installation not cached JS
    
    Co-authored-by: sgiehl <stefan@matomo.org>

---
+++ plugins/Installation/vue/src/SystemCheck/SystemCheck.vue
@@ -0,0 +1,101 @@
+<!--
+  Matomo - free/libre analytics platform
+  @link https://matomo.org
+  @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+-->
+
+<template>
+  <div>
+    <div v-if="!showNextStep">
+      <SystemCheckLegend
+        :url="systemCheckLegendUrl"
+      />
+
+      <br style="clear:both;"/>
+    </div>
+
+    <h2>{{ translate('Installation_SystemCheck') }}</h2>
+
+    <SystemCheckSection
+      :error-type="errorType"
+      :warning-type="warningType"
+      :informational-type="informationalType"
+      :system-check-info="systemCheckInfo"
+      :mandatory-results="mandatoryResults"
+      :optional-results="optionalResults"
+      :informational-results="informationalResults"
+      :is-installation="isInstallation"
+    />
+
+    <div v-if="!showNextStep">
+      <p v-if="!showNextStep">
+        <span class="icon-export"></span>
+        <a target="_blank" rel="noreferrer noopener" href="https://matomo.org/docs/requirements/">
+          {{ translate('Installation_Requirements') }}
+        </a>
+      </p>
+
+      <SystemCheckLegend
+        :url="systemCheckLegendUrl"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SystemCheckLegend from './SystemCheckLegend.vue';
+import SystemCheckSection from './SystemCheckSection.vue';
+
+const { $ } = window;
+
+export default defineComponent({
+  props: {
+    showNextStep: Boolean,
+    systemCheckLegendUrl: {
+      type: String,
+      required: true,
+    },
+    errorType: {
+      type: String,
+      required: true,
+    },
+    warningType: {
+      type: String,
+      required: true,
+    },
+    informationalType: {
+      type: String,
+      required: true,
+    },
+    systemCheckInfo: {
+      type: String,
+      required: true,
+    },
+    mandatoryResults: {
+      type: Array,
+      required: true,
+    },
+    optionalResults: {
+      type: Array,
+      required: true,
+    },
+    informationalResults: {
+      type: Array,
+      required: true,
+    },
+    isInstallation: Boolean,
+  },
+  components: {
+    SystemCheckSection,
+    SystemCheckLegend,
+  },
+  mounted() {
+    // client-side test for https to handle the case where the server is behind a reverse proxy
+    if (document.location.protocol === 'https:') {
+      const link = $('p.next-step a');
+      link.attr('href', `${link.attr('href')}&clientProtocol=https`);
+    }
+  },
+});
+</script>
---
+++ plugins/Installation/vue/src/SystemCheck/SystemCheckPage.vue
@@ -0,0 +1,89 @@
+<!--
+  Matomo - free/libre analytics platform
+  @link https://matomo.org
+  @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+-->
+
+<template>
+  <ContentBlock :content-title="translate('Installation_SystemCheck')" feature="true">
+    <div class="alert alert-danger" v-if="hasErrors">
+      <span v-html="$sanitize(thereWereErrorsText)"></span>
+      {{ translate('Installation_SeeBelowForMoreInfo') }}
+    </div>
+    <div class="alert alert-warning" v-else-if="hasWarnings">
+      {{ translate('Installation_SystemCheckSummaryThereWereWarnings') }}
+      {{ translate('Installation_SeeBelowForMoreInfo') }}
+    </div>
+    <div class="alert alert-success" v-else>
+      {{ translate('Installation_SystemCheckSummaryNoProblems') }}
+    </div>
+
+    <SystemCheckSection
+      :error-type="errorType"
+      :warning-type="warningType"
+      :informational-type="informationalType"
+      :system-check-info="systemCheckInfo"
+      :mandatory-results="mandatoryResults"
+      :optional-results="optionalResults"
+      :informational-results="informationalResults"
+      :is-installation="isInstallation"
+    />
+  </ContentBlock>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { ContentBlock, translate } from 'CoreHome';
+import SystemCheckSection from './SystemCheckSection.vue';
+
+export default defineComponent({
+  props: {
+    errorType: {
+      type: String,
+      required: true,
+    },
+    warningType: {
+      type: String,
+      required: true,
+    },
+    informationalType: {
+      type: String,
+      required: true,
+    },
+    systemCheckInfo: {
+      type: String,
+      required: true,
+    },
+    mandatoryResults: {
+      type: Array,
+      required: true,
+    },
+    optionalResults: {
+      type: Array,
+      required: true,
+    },
+    informationalResults: {
+      type: Array,
+      required: true,
+    },
+    isInstallation: Boolean,
+    hasErrors: Boolean,
+    hasWarnings: Boolean,
+  },
+  components: {
+    ContentBlock,
+    SystemCheckSection,
+  },
+  computed: {
+    thereWereErrorsText() {
+      return translate(
+        'Installation_SystemCheckSummaryThereWereErrors',
+        '<strong>',
+        '</strong>',
+        '<strong>',
+        '</strong>',
+      );
+    },
+  },
+});
+</script>
