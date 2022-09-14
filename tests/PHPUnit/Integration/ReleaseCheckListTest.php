ad780cdbceb158740618c1db662d82f00ce3fe88
Peter <peter@innocraft.com>
2022-09-14 13:19:17 +1200

    update release check
    update release check

--- tests/PHPUnit/Integration/ReleaseCheckListTest.php
+++ tests/PHPUnit/Integration/ReleaseCheckListTest.php
@@ -48,12 +48,12 @@ class ReleaseCheckListTest extends \PHPUnit\Framework\TestCase
 
     public function test_woff2_fileIsUpToDate()
     {
-        link(PIWIK_INCLUDE_PATH . "/plugins/Morpheus/fonts/matomo.ttf", "temp.ttf");
-        $command = PIWIK_INCLUDE_PATH . "/../travis_woff2/woff2_compress 'temp.ttf'";
-        $log = shell_exec($command);
-        $log = shell_exec("woff2_compress 'temp.ttf'");
+        copy(PIWIK_INCLUDE_PATH."/plugins/Morpheus/fonts/matomo.ttf", PIWIK_INCLUDE_PATH."/tmp/temp.ttf");
+        $log = shell_exec("woff2_compress ".PIWIK_INCLUDE_PATH."/tmp/temp.ttf");
 
-        $this->assertFileEquals('temp.woff2', PIWIK_INCLUDE_PATH . "/plugins/Morpheus/fonts/matomo.woff2", "woff2 file is out of date.\nCommand output:\n" . $log);
+        $this->assertFileEquals(PIWIK_INCLUDE_PATH."/tmp/temp.woff2",
+            PIWIK_INCLUDE_PATH."/plugins/Morpheus/fonts/matomo.woff2",
+            "woff2 file is out of date.\nCommand output:\n".$log);
     }
 
     public function test_minimumPHPVersion_isEnforced()
