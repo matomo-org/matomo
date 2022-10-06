bbb545a8657a664db3959651252c7b61bc5d3707
snake14 <snake14@users.noreply.github.com>
2022-10-06 06:11:15 -0700

    Updated the text and links in the Matomo Help Centre. (#19798)
    * Updated the text and links in the Matomo Help Centre.
    
    * Fixing a couple translations that shouldn't have been changed.
    
    * Updated screenshot to include changes.
    
    * Making suggested code review change.
    
    * Removed translation key that's no longer being used.
    
    Co-authored-by: sgiehl <stefan@matomo.org>

--- plugins/Feedback/config/config.php
+++ plugins/Feedback/config/config.php
@@ -1,9 +1,12 @@
 <?php
-return array(
-    'popularHelpTopics' => array(
-        array('title' => 'Feedback_NotTrackingVisits', 'url' => 'https://matomo.org/faq/troubleshooting/faq_58/'),
-        array('title' => 'Feedback_TrackMultipleSites', 'url' => 'https://matomo.org/faq/troubleshooting/faq_104/'),
-        array('title' => 'Feedback_RemoveOtherLabel', 'url' => 'https://matomo.org/faq/troubleshooting/faq_54/'),
-        array('title' => 'Feedback_PurgeOldData', 'url' => 'https://matomo.org/faq/troubleshooting/faq_42/'),
-    )
-);
+return [
+    'popularHelpTopics' => [
+        ['title' => 'Feedback_NotTrackingVisits', 'url' => 'https://matomo.org/faq/troubleshooting/faq_58/'],
+        ['title' => 'Feedback_TrackMultipleSites', 'url' => 'https://matomo.org/faq/troubleshooting/faq_104/'],
+        ['title' => 'Feedback_HowToMigrateFromGA', 'url' => 'https://matomo.org/faq/how-to/faq_102/'],
+        ['title' => 'Feedback_HowToDefineAndTrackGoals', 'url' => 'https://matomo.org/docs/tracking-goals-web-analytics/'],
+        ['title' => 'Feedback_HowToGetStartedWithMtm', 'url' => 'https://matomo.org/guide/tag-manager/getting-started-with-tag-manager/'],
+        ['title' => 'Feedback_HowToMigrateFromGtm', 'url' => 'https://matomo.org/faq/tag-manager/migrating-from-google-tag-manager/'],
+        ['title' => 'Feedback_HowToTrackEcommerce', 'url' => 'https://matomo.org/docs/ecommerce-analytics/'],
+    ]
+];
