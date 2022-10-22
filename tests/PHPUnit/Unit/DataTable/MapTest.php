4d46702341c93b482959decb1078bc7cbd6f77c7
diosmosis <diosmosis@users.noreply.github.com>
2022-10-22 12:53:01 -0700

    add multiFilter utility method to DataTable classes so it is easier to iterate over multiple DataTables concurrently

--- tests/PHPUnit/Unit/DataTable/MapTest.php
+++ tests/PHPUnit/Unit/DataTable/MapTest.php
@@ -17,6 +17,93 @@ class MapTest extends \PHPUnit\Framework\TestCase
         Manager::getInstance()->deleteAll();
     }
 
+    public function testMultiFilter()
+    {
+        // first map
+        $innerTable1 = new DataTable();
+        $innerTable1->addRowsFromSimpleArray([
+            ['label' => '2020-03-04.1', 'value' => 1],
+        ]);
+        $innerTable2 = new DataTable();
+        $innerTable2->addRowsFromSimpleArray([
+            ['label' => '2020-03-04.2', 'value' => 3],
+        ]);
+        $innerTable3 = new DataTable();
+        $innerTable3->addRowsFromSimpleArray([
+            ['label' => '2020-05-05.3', 'value' => 5],
+        ]);
+        $innerTable4 = new DataTable();
+        $innerTable4->addRowsFromSimpleArray([
+            ['label' => '2020-05-05.4', 'value' => 7],
+        ]);
+
+        $innerMap1 = new DataTable\Map();
+        $innerMap1->addTable($innerTable1, '1');
+        $innerMap1->addTable($innerTable2, '2');
+
+        $innerMap2 = new DataTable\Map();
+        $innerMap2->addTable($innerTable3, '3');
+        $innerMap2->addTable($innerTable4, '4');
+
+        $outerMap1 = new DataTable\Map();
+        $outerMap1->addTable($innerMap1, '2020-03-04');
+        $outerMap1->addTable($innerMap2, '2020-05-05');
+
+        // second map
+        $innerTable5 = new DataTable();
+        $innerTable5->addRowsFromSimpleArray([
+            ['label' => '2020-03-04.1', 'value' => 9],
+        ]);
+        $innerTable6 = new DataTable();
+        $innerTable6->addRowsFromSimpleArray([
+            ['label' => '2020-03-04.2', 'value' => 11],
+        ]);
+        $innerTable7 = new DataTable();
+        $innerTable7->addRowsFromSimpleArray([
+            ['label' => '2020-05-06.5', 'value' => 13],
+        ]);
+        $innerTable8 = new DataTable();
+        $innerTable8->addRowsFromSimpleArray([
+            ['label' => '2020-05-06.4', 'value' => 15],
+        ]);
+
+        $innerMap3 = new DataTable\Map();
+        $innerMap3->addTable($innerTable5, '1');
+        $innerMap3->addTable($innerTable6, '2');
+
+        $innerMap4 = new DataTable\Map();
+        $innerMap4->addTable($innerTable7, '5');
+        $innerMap4->addTable($innerTable8, '4');
+
+        $outerMap2 = new DataTable\Map();
+        $outerMap2->addTable($innerMap3, '2020-03-04');
+        $outerMap2->addTable($innerMap4, '2020-05-06');
+
+        $visitedLabels = [];
+        $result = $outerMap1->multiFilter([$outerMap2], function ($table1, $table2) use (&$visitedLabels) {
+            $label1 = $table1->getFirstRow()->getColumn('label');
+            $value1 = $table1->getFirstRow()->getColumn('value');
+
+            $label2 = empty($table2) ? false : $table2->getFirstRow()->getColumn('label');
+            $value2 = empty($table2) ? 0 : $table2->getFirstRow()->getColumn('value');
+
+            $visitedLabels[] = [$label1, $label2];
+
+            return $value1 + $value2;
+        });
+
+        $this->assertEquals([
+            '2020-03-04' => ['1' => 10, '2' => 14],
+            '2020-05-05' => ['3' => 5, '4' => 7],
+        ], $result);
+        $this->assertEquals([
+            ['2020-03-04.1', '2020-03-04.1'],
+            ['2020-03-04.2', '2020-03-04.2'],
+            ['2020-05-05.3', false],
+            ['2020-05-05.4', false],
+        ], $visitedLabels);
+    }
+
     private function createTestDataTable()
     {
         $result = new DataTable();
