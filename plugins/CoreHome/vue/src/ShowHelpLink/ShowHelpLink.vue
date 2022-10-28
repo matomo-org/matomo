e1e93063e9a5e42b82604e7bc283f94ecfe73611
Peter <peter@innocraft.com>
2022-10-28 14:04:35 +1300

    add help link vue
    add help link vue

---
+++ plugins/CoreHome/vue/src/ShowHelpLink/ShowHelpLink.vue
@@ -0,0 +1,47 @@
+<!--
+  Matomo - free/libre analytics platform
+  @link https://matomo.org
+  @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+-->
+
+<template>
+<a
+  class="item-help-icon"
+  tabindex="5"
+  href="javascript:"
+  v-if="message"
+  @click="showHelp"
+>
+<span class="icon-help" />
+</a>
+</template>
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+import { NotificationsStore } from '../Notification';
+
+const REPORTING_HELP_NOTIFICATION_ID = 'reportingmenu-help';
+
+export default defineComponent({
+  props: {
+    message: {
+      type: String,
+      default: null,
+    },
+  },
+  methods: {
+    showHelp() {
+      NotificationsStore.show({
+        context: 'info',
+        id: REPORTING_HELP_NOTIFICATION_ID,
+        type: 'help',
+        noclear: true,
+        class: 'help-notification',
+        message: this.message,
+        placeat: '#notificationContainer',
+        prepend: true,
+      });
+    },
+  },
+});
+</script>
