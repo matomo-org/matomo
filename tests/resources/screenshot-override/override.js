1f724504c59465a4400a3e6ec1d22308ecaf3c36
sgiehl <stefan@matomo.org>
2023-01-09 17:49:53 +0100

    hide some details in system check test

--- tests/resources/screenshot-override/override.js
+++ tests/resources/screenshot-override/override.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
         $('.system-check tr:contains(Datetime) td:nth-child(2)').text('Not showing in tests');
         $('.system-check tr:contains(Version) td:nth-child(2)').text('Not showing in tests');
         $('.system-check tr:contains(User Agent) td:nth-child(2)').text('Not showing in tests');
+        $('.system-check tr:contains(PHP_BINARY) td:nth-child(2)').text('Not showing in tests');
+        $('.system-check tr:contains(Server Info) td:nth-child(2)').text('Not showing in tests');
     }
     updateSystemCheck();
 
