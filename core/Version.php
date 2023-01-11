0a174ec529c9b5ab523f3cfd39fd82658f5b3dd6
Justin Velluppillai <justin@innocraft.com>
2023-01-11 13:38:43 +1300

    Release Matomo 4.13.1-rc1

--- core/Version.php
+++ core/Version.php
@@ -21,7 +21,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    const VERSION = '4.13.0';
+    const VERSION = '4.13.1-rc1';
 
     const MAJOR_VERSION = 4;
 
