8d7ba69eb938c61cdfc80ef0272e4aa630e847e3
Stefan Giehl <stefan@matomo.org>
2023-03-16 08:45:18 +0100

    Implement custom console formatter to allow logging request id (#20474)

---
+++ plugins/Monolog/Formatter/ConsoleFormatter.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\Monolog\Formatter;
+
+use Symfony\Bridge\Monolog\Formatter\ConsoleFormatter AS SymfonyConsoleFormatter;
+
+class ConsoleFormatter extends SymfonyConsoleFormatter
+{
+    public function format(array $record)
+    {
+        $formatted = parent::format($record);
+
+        foreach ($record['extra'] as $var => $val) {
+            if (false !== strpos($formatted, '%extra.' . $var . '%')) {
+                $formatted = str_replace('%extra.' . $var . '%', $val, $formatted);
+                unset($record['extra'][$var]);
+            }
+        }
+
+        return $formatted;
+    }
+}
