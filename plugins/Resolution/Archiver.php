ee676c807ef3c4cf234c5529ce18ff6346198d41
diosmosis <diosmosis@users.noreply.github.com>
2023-05-17 20:52:24 -0700

    move Resolution archiving code to RecordBuilders

--- plugins/Resolution/Archiver.php
+++ plugins/Resolution/Archiver.php
@@ -9,9 +9,6 @@
 
 namespace Piwik\Plugins\Resolution;
 
-use Piwik\DataTable;
-use Piwik\Metrics;
-
 /**
  * Archiver for Resolution Plugin
  *
@@ -23,53 +20,4 @@ class Archiver extends \Piwik\Plugin\Archiver
     const CONFIGURATION_RECORD_NAME = 'Resolution_configuration';
     const RESOLUTION_DIMENSION = "log_visit.config_resolution";
     const CONFIGURATION_DIMENSION = "CONCAT(log_visit.config_os, ';', log_visit.config_browser_name, ';', log_visit.config_resolution)";
-
-    public function aggregateDayReport()
-    {
-        $this->aggregateByResolution();
-        $this->aggregateByConfiguration();
-    }
-
-    /**
-     * Period archiving: simply sums up daily archives
-     */
-    public function aggregateMultipleReports()
-    {
-        $dataTableRecords = array(
-            self::RESOLUTION_RECORD_NAME,
-            self::CONFIGURATION_RECORD_NAME,
-        );
-        $columnsAggregationOperation = null;
-        $this->getProcessor()->aggregateDataTableRecords(
-            $dataTableRecords,
-            $this->maximumRows,
-            $maximumRowsInSubDataTable = null,
-            $columnToSortByBeforeTruncation = null,
-            $columnsAggregationOperation,
-            $columnsToRenameAfterAggregation = null,
-            $countRowsRecursive = array());
-    }
-
-    protected function aggregateByConfiguration()
-    {
-        $metrics = $this->getLogAggregator()->getMetricsFromVisitByDimension(self::CONFIGURATION_DIMENSION)->asDataTable();
-        $this->insertTable(self::CONFIGURATION_RECORD_NAME, $metrics);
-    }
-
-    protected function aggregateByResolution()
-    {
-        $table = $this->getLogAggregator()->getMetricsFromVisitByDimension(self::RESOLUTION_DIMENSION)->asDataTable();
-        $table->filter('ColumnCallbackDeleteRow', array('label', function ($value) {
-            return strlen($value) <= 5;
-        }));
-        $this->insertTable(self::RESOLUTION_RECORD_NAME, $table);
-        return $table;
-    }
-
-    protected function insertTable($recordName, DataTable $table)
-    {
-        $report = $table->getSerialized($this->maximumRows, null, Metrics::INDEX_NB_VISITS);
-        $this->getProcessor()->insertBlobRecord($recordName, $report);
-    }
-
 }
---
+++ plugins/Resolution/RecordBuilders/Resolution.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+namespace Piwik\Plugins\Resolution\RecordBuilders;
+
+use Piwik\ArchiveProcessor;
+use Piwik\ArchiveProcessor\Record;
+use Piwik\ArchiveProcessor\RecordBuilder;
+use Piwik\Config as PiwikConfig;
+use Piwik\DataTable;
+use Piwik\Metrics;
+use Piwik\Plugins\Resolution\Archiver;
+
+class Resolution extends RecordBuilder
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->maxRowsInTable = PiwikConfig::getInstance()->General['datatable_archiving_maximum_rows_standard'];
+        $this->columnToSortByBeforeTruncation = Metrics::INDEX_NB_VISITS;
+    }
+
+    public function getRecordMetadata(ArchiveProcessor $archiveProcessor)
+    {
+        return [
+            Record::make(Record::TYPE_BLOB, Archiver::RESOLUTION_RECORD_NAME),
+        ];
+    }
+
+    protected function aggregate(ArchiveProcessor $archiveProcessor)
+    {
+        $record = new DataTable();
+
+        $cursor = $archiveProcessor->getLogAggregator()->queryVisitsByDimension(['label' => Archiver::RESOLUTION_DIMENSION]);
+        while ($row = $cursor->fetch()) {
+            $record->sumRowWithLabel($row['label'], $row);
+        }
+
+        $record->filter(DataTable\Filter\ColumnCallbackDeleteRow::class, ['label', function ($value) {
+            return strlen($value) <= 5;
+        }]);
+
+        return [Archiver::RESOLUTION_RECORD_NAME => $record];
+    }
+}
