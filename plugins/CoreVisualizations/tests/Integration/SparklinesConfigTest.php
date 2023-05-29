d4c3eb38154930ab9d1c650c85d4fca053acbc9c
Michal Kleiner <michal@innocraft.com>
2023-05-29 22:24:42 +1200

    Add test for 'lower value is better' sparkline

--- plugins/CoreVisualizations/tests/Integration/SparklinesConfigTest.php
+++ plugins/CoreVisualizations/tests/Integration/SparklinesConfigTest.php
@@ -169,6 +169,23 @@ class SparklinesConfigTest extends IntegrationTestCase
         ), $sparklines[''][0]['evolution']);
     }
 
+    public function test_addSparkline_shouldAddEvolutionWhereLowerValueIsBetter()
+    {
+        $evolution = array('currentValue' => 20, 'pastValue' => 41,
+                            'tooltip' => '2 bounces compared to 1 bounce',
+                            'isLowerValueBetter' => true);
+        $this->config->addSparkline($this->sparklineParams(), $value = 10, $description = 'Bounces', $evolution);
+
+        $sparklines = $this->config->getSortedSparklines();
+
+        $this->assertSame(array (
+            'percent'  => '-51.2%',
+            'isLowerValueBetter' => true,
+            'tooltip' => '2 bounces compared to 1 bounce',
+            'trend' => -21
+        ), $sparklines[''][0]['evolution']);
+    }
+
     public function test_addSparkline_shouldAddOrder()
     {
         $this->config->addSparkline($this->sparklineParams(), $value = 10, $description = 'Visits', $evolution = null, $order = '42');
