479d6837eba094e40d22d3e19cd3cff4164e2935
Ayke Halder <rr-it@users.noreply.github.com>
2023-07-05 15:08:33 +0200

    Experimental: add deprecated functions to `externs` definition for `piwik.js`

--- js/piwik.externs.js
+++ js/piwik.externs.js
@@ -28,3 +28,37 @@ window.define.amd = {};
  */
 
 var Matomo = {}
+
+/**
+ * @deprecated
+ */
+window.Piwik = Matomo;
+
+/*
+ ************************************************************
+ * Deprecated functionality below
+ * Legacy piwik.js compatibility ftw
+ ************************************************************
+ */
+
+/**
+ * Track page visit
+ * @deprecated
+ *
+ * @param {string} documentTitle
+ * @param {int|string} siteId
+ * @param {string} matomoUrl
+ * @param {*} customData
+ */
+window.piwik_log = function(documentTitle, siteId, matomoUrl, customData) {};
+
+/**
+ * Track click manually (function is defined below)
+ * @deprecated
+ *
+ * @param {string} sourceUrl
+ * @param {int|string} siteId
+ * @param {string} matomoUrl
+ * @param {string} linkType
+ */
+window.piwik_track = function(sourceUrl, siteId, matomoUrl, linkType) {};
