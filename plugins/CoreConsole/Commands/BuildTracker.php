56d792607bf29d6b0060bdf78bf10de82d8ffa74
Ayke Halder <rr-it@users.noreply.github.com>
2023-07-05 14:44:09 +0200

    Experimental: use Closure Compiler with `ADVANCED_OPTIMIZATIONS`

--- plugins/CoreConsole/Commands/BuildTracker.php
+++ plugins/CoreConsole/Commands/BuildTracker.php
@@ -57,7 +57,7 @@ class BuildTracker extends ConsoleCommand
             }
             passthru($command);
 
-            $command = "cd $jsPath && sed '/<DEBUG>/,/<\\/DEBUG>/d' < piwik.js | sed 's/eval/replacedEvilString/' | java -jar closure-compiler-v20230502.jar --language_in ECMASCRIPT3 --language_out ECMASCRIPT3 --compilation_level SIMPLE_OPTIMIZATIONS | sed 's/replacedEvilString/eval/' | sed 's/^[/][*]/\\/*!/' > piwik.experimental.min.js";
+            $command = "cd $jsPath && sed '/<DEBUG>/,/<\\/DEBUG>/d' < piwik.js | sed 's/eval/replacedEvilString/' | java -jar closure-compiler-v20230502.jar --language_in ECMASCRIPT3 --language_out ECMASCRIPT3 --compilation_level ADVANCED_OPTIMIZATIONS | sed 's/replacedEvilString/eval/' | sed 's/^[/][*]/\\/*!/' > piwik.experimental.min.js";
             if ($output->isVerbose()) {
                 $output->writeln("Command (experimental): $command");
             }
