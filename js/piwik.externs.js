31e8d6409bf2ba4c66399fa590f8769442cc34fb
Ayke Halder <rr-it@users.noreply.github.com>
2023-07-05 15:04:49 +0200

    Experimental: add `externs` base definition for `piwik.js` used by Closure Compiler

---
+++ js/piwik.externs.js
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview Public API of JavaScript tracking client
+ * Matomo - free/libre analytics platform
+ *
+ * @externs
+ * @suppress {checkTypes}
+ */
+
+/*
+ ************************************************************
+ * Global data and methods used by Matomo
+ ************************************************************
+ */
+
+/**
+ * @param {string} [id]
+ * @param {Array} [dependencies]
+ * @param {Function} [factory]
+ */
+window.define = function(id, dependencies, factory) {};
+
+window.define.amd = {};
+
+/*
+ ************************************************************
+ * Matomo: Public data and methods
+ ************************************************************
+ */
+
+var Matomo = {}
