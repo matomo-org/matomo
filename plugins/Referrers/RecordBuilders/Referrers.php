abf90ee6484b1d1d0bc2522804df49f560673bcf
diosmosis <diosmosis@users.noreply.github.com>
2023-07-11 04:37:20 -0700

    convert metrics to ints in referrers record builder

--- plugins/Referrers/RecordBuilders/Referrers.php
+++ plugins/Referrers/RecordBuilders/Referrers.php
@@ -129,14 +129,14 @@ class Referrers extends RecordBuilder
     protected function aggregateVisitRow($row, array $reports, array &$distinctUrls): void
     {
         $columns = [
-            Metrics::INDEX_NB_UNIQ_VISITORS => $row[Metrics::INDEX_NB_UNIQ_VISITORS],
-            Metrics::INDEX_NB_VISITS => $row[Metrics::INDEX_NB_VISITS],
-            Metrics::INDEX_NB_ACTIONS => $row[Metrics::INDEX_NB_ACTIONS],
-            Metrics::INDEX_NB_USERS => $row[Metrics::INDEX_NB_USERS],
-            Metrics::INDEX_MAX_ACTIONS => $row[Metrics::INDEX_MAX_ACTIONS],
-            Metrics::INDEX_SUM_VISIT_LENGTH => $row[Metrics::INDEX_SUM_VISIT_LENGTH],
-            Metrics::INDEX_BOUNCE_COUNT => $row[Metrics::INDEX_BOUNCE_COUNT],
-            Metrics::INDEX_NB_VISITS_CONVERTED => $row[Metrics::INDEX_NB_VISITS_CONVERTED],
+            Metrics::INDEX_NB_UNIQ_VISITORS => (int)$row[Metrics::INDEX_NB_UNIQ_VISITORS],
+            Metrics::INDEX_NB_VISITS => (int)$row[Metrics::INDEX_NB_VISITS],
+            Metrics::INDEX_NB_ACTIONS => (int)$row[Metrics::INDEX_NB_ACTIONS],
+            Metrics::INDEX_NB_USERS => (int)$row[Metrics::INDEX_NB_USERS],
+            Metrics::INDEX_MAX_ACTIONS => (int)$row[Metrics::INDEX_MAX_ACTIONS],
+            Metrics::INDEX_SUM_VISIT_LENGTH => (int)$row[Metrics::INDEX_SUM_VISIT_LENGTH],
+            Metrics::INDEX_BOUNCE_COUNT => (int)$row[Metrics::INDEX_BOUNCE_COUNT],
+            Metrics::INDEX_NB_VISITS_CONVERTED => (int)$row[Metrics::INDEX_NB_VISITS_CONVERTED],
         ];
 
         switch ($row['referer_type']) {
