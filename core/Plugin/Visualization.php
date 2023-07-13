898386a6147843928216707dd986404e425474b8
Stefan Giehl <stefan@matomo.org>
2023-07-13 15:08:01 +0200

    Fix tag cloud visualization failing on formatted values (#21001) (#21007)
    * Change the cloud tag visualization to generate before metric formatting filters
    
    * Use request parameter to avoid formatting metrics instead of changing visualization generation timing
    
    * Reapply metric formatting after storing tag cloud size values
    
    Co-authored-by: Ben Burgess <88810029+bx80@users.noreply.github.com>

--- core/Plugin/Visualization.php
+++ core/Plugin/Visualization.php
@@ -905,4 +905,19 @@ class Visualization extends ViewDataTable
 
         return $request;
     }
+
+    /**
+     * Apply the metrics formatting filter to the dataset
+     *
+     * This is a workaround to allow visualizations to access unformatted data via format_metrics=0 and then
+     * subsequently apply formatting without needed to reload the dataset or reapply other filters. This method may
+     * be removed in the future.
+     *
+     * @internal
+     */
+    protected function applyMetricsFormatting()
+    {
+        $postProcessor = $this->makeDataTablePostProcessor(); // must be created after requestConfig is final
+        $this->dataTable = $postProcessor->applyMetricsFormatting($this->dataTable);
+    }
 }
