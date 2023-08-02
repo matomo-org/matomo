636894806ca4ec1ebf1bdf9372967bed11f971a9
Altamash Shaikh <altu9594@gmail.com>
2023-08-02 13:15:27 +0530

    Added code to trigger Archiving.getIdSitesToMarkArchivesAsInvalidated on PrivacyManager.deleteDataSubjects, #PG-2965 (#21103)

--- core/Archive/ArchiveInvalidator.php
+++ core/Archive/ArchiveInvalidator.php
@@ -313,8 +313,9 @@ class ArchiveInvalidator
          * @param string $period A string containing the period to be invalidated.
          * @param Segment $segment A Segment Object containing segment to invalidate.
          * @param string $name A string containing the name of the archive to be invalidated.
+         * @param bool $isPrivacyDeleteData A boolean value if event is triggered via Privacy delete visit action.
          */
-        Piwik::postEvent('Archiving.getIdSitesToMarkArchivesAsInvalidated', array(&$idSites, $dates, $period, $segment, $name));
+        Piwik::postEvent('Archiving.getIdSitesToMarkArchivesAsInvalidated', array(&$idSites, $dates, $period, $segment, $name, $isPrivacyDeleteData = false));
         // we trigger above event on purpose here and it is good that the segment was created like
         // `new Segment($segmentString, $idSites)` because when a user adds a site via this event, the added idSite
         // might not have this segment meaning we avoid a possible error. For the workflow to work, any added or removed
