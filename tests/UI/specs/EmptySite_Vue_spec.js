1b7dde49802438c40f8cea4b4e05a8e07484ea03
sgiehl <stefan@matomo.org>
2023-08-03 11:42:00 +0200

    add some timeouts

--- tests/UI/specs/EmptySite_Vue_spec.js
+++ tests/UI/specs/EmptySite_Vue_spec.js
@@ -16,6 +16,7 @@ describe("EmptySite_Vue", function () {
   it('should show the tracking code if the website has no recorded data and Vue guide', async function () {
     const urlToTest = "?" + generalParams + "&module=CoreHome&action=index";
     await page.goto(urlToTest);
+    await page.waitForTimeout(200); // svg takes some time to render
 
     const pageElement = await page.$('.page');
     expect(await pageElement.screenshot()).to.matchImage('emptySiteDashboard');
