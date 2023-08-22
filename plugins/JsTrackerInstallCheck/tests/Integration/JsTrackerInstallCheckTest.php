f5c0800c3ac07d7acd650727fab45e00f29a523e
Jacob Ransom <jacob@innocraft.com>
2023-08-22 13:57:06 +1200

    Adding some more test cases

--- plugins/JsTrackerInstallCheck/tests/Integration/JsTrackerInstallCheckTest.php
+++ plugins/JsTrackerInstallCheck/tests/Integration/JsTrackerInstallCheckTest.php
@@ -74,6 +74,124 @@ class JsTrackerInstallCheckTest extends JsTrackerInstallCheckIntegrationTestCase
         $this->assertTrue($this->isNonceForSiteSuccessFul($this->idSite1));
     }
 
+    public function testCheckForJsTrackerInstallTestSuccessNonExistantNonce()
+    {
+        $this->assertFalse($this->jsTrackerInstallCheck->checkForJsTrackerInstallTestSuccess($this->idSite1));
+    }
+
+    public function testCheckForJsTrackerInstallTestSuccessFalse()
+    {
+        $this->createNonceOption($this->idSite1);
+        $this->assertFalse($this->jsTrackerInstallCheck->checkForJsTrackerInstallTestSuccess($this->idSite1));
+    }
+
+    public function testCheckForJsTrackerInstallTestSuccess()
+    {
+        $nonce = $this->createNonceOption($this->idSite1);
+        $this->setNonceCheckAsSuccessful($this->idSite1);
+        $this->assertTrue($this->jsTrackerInstallCheck->checkForJsTrackerInstallTestSuccess($this->idSite1));
+    }
+
+    public function testCheckForJsTrackerInstallTestSuccessNonceProvided()
+    {
+        $nonce = $this->createNonceOption($this->idSite1);
+        $this->setNonceCheckAsSuccessful($this->idSite1);
+        $this->assertTrue($this->jsTrackerInstallCheck->checkForJsTrackerInstallTestSuccess($this->idSite1, $nonce));
+    }
+
+    public function testCheckForJsTrackerInstallTestSuccessNonceDifferent()
+    {
+        $this->createNonceOption($this->idSite1);
+        $this->setNonceCheckAsSuccessful($this->idSite1);
+        $this->assertFalse($this->jsTrackerInstallCheck->checkForJsTrackerInstallTestSuccess($this->idSite1, 'abc123'));
+    }
+
+    public function testInitiateJsTrackerInstallTest()
+    {
+        $result = $this->jsTrackerInstallCheck->initiateJsTrackerInstallTest($this->idSite1);
+        $this->assertIsArray($result);
+        $this->assertNotEmpty($result['url']);
+        $this->assertNotEmpty($result['nonce']);
+        $option = $this->getOptionForSite($this->idSite1);
+        $this->assertNotEmpty($option);
+        $decodedOption = json_decode($option, true);
+        $this->assertIsArray($decodedOption);
+        $this->assertArrayHasKey('nonce', $decodedOption);
+        $this->assertSame($result['nonce'], $decodedOption['nonce']);
+        $this->assertStringContainsString($decodedOption['nonce'], $result['url']);
+        $this->assertSame('http://piwik.net?' . JsTrackerInstallCheck::QUERY_PARAM_NAME . '=' . $decodedOption['nonce'], $result['url']);
+    }
+
+    public function testInitiateJsTrackerInstallTestProvideUrl()
+    {
+        $testUrl = 'https://some-test-site.com?test=1';
+        $result = $this->jsTrackerInstallCheck->initiateJsTrackerInstallTest($this->idSite1, $testUrl);
+        $this->assertIsArray($result);
+        $this->assertNotEmpty($result['url']);
+        $this->assertNotEmpty($result['nonce']);
+        $option = $this->getOptionForSite($this->idSite1);
+        $this->assertNotEmpty($option);
+        $decodedOption = json_decode($option, true);
+        $this->assertIsArray($decodedOption);
+        $this->assertArrayHasKey('nonce', $decodedOption);
+        $this->assertSame($result['nonce'], $decodedOption['nonce']);
+        $this->assertSame($testUrl . '&' . JsTrackerInstallCheck::QUERY_PARAM_NAME . '=' . $decodedOption['nonce'], $result['url']);
+    }
+
+    public function testInitiateJsTrackerInstallTestProvideInvalidUrl()
+    {
+        $result = $this->jsTrackerInstallCheck->initiateJsTrackerInstallTest($this->idSite1, 'abc123');
+        $this->assertIsArray($result);
+        $this->assertNotEmpty($result['url']);
+        $this->assertNotEmpty($result['nonce']);
+        $option = $this->getOptionForSite($this->idSite1);
+        $this->assertNotEmpty($option);
+        $decodedOption = json_decode($option, true);
+        $this->assertIsArray($decodedOption);
+        $this->assertArrayHasKey('nonce', $decodedOption);
+        $this->assertSame($result['nonce'], $decodedOption['nonce']);
+        $this->assertStringContainsString($decodedOption['nonce'], $result['url']);
+        $this->assertSame('http://piwik.net?' . JsTrackerInstallCheck::QUERY_PARAM_NAME . '=' . $decodedOption['nonce'], $result['url']);
+    }
+
+    public function testInitiateJsTrackerInstallTestReuseNonce()
+    {
+        $nonce = $this->createNonceOption($this->idSite1);
+        $result = $this->jsTrackerInstallCheck->initiateJsTrackerInstallTest($this->idSite1);
+        $this->assertIsArray($result);
+        $this->assertNotEmpty($result['url']);
+        $this->assertNotEmpty($result['nonce']);
+        $this->assertSame($nonce, $result['nonce']);
+        $option = $this->getOptionForSite($this->idSite1);
+        $this->assertNotEmpty($option);
+        $decodedOption = json_decode($option, true);
+        $this->assertIsArray($decodedOption);
+        $this->assertArrayHasKey('nonce', $decodedOption);
+        $this->assertSame($nonce, $decodedOption['nonce']);
+        $this->assertStringContainsString($nonce, $result['url']);
+        $this->assertSame('http://piwik.net?' . JsTrackerInstallCheck::QUERY_PARAM_NAME . '=' . $nonce, $result['url']);
+    }
+
+    public function testInitiateJsTrackerInstallTestReplaceNonce()
+    {
+        $nonce = $this->createNonceOption($this->idSite1);
+        // Make the new nonce expired so that we can test that it gets replaced
+        $this->setNonceCheckTimestamp($this->idSite1, Date::getNowTimestamp() - JsTrackerInstallCheck::MAX_NONCE_AGE_SECONDS);
+        $result = $this->jsTrackerInstallCheck->initiateJsTrackerInstallTest($this->idSite1);
+        $this->assertIsArray($result);
+        $this->assertNotEmpty($result['url']);
+        $this->assertNotEmpty($result['nonce']);
+        $this->assertNotSame($nonce, $result['nonce']);
+        $option = $this->getOptionForSite($this->idSite1);
+        $this->assertNotEmpty($option);
+        $decodedOption = json_decode($option, true);
+        $this->assertIsArray($decodedOption);
+        $this->assertArrayHasKey('nonce', $decodedOption);
+        $this->assertSame($result['nonce'], $decodedOption['nonce']);
+        $this->assertStringContainsString($decodedOption['nonce'], $result['url']);
+        $this->assertSame('http://piwik.net?' . JsTrackerInstallCheck::QUERY_PARAM_NAME . '=' . $decodedOption['nonce'], $result['url']);
+    }
+
     private function createRequestMock(bool $hasParam, array $allParams = [], int $idSite = 0)
     {
         $mock = $this->getMockBuilder('\Piwik\Tracker\Request')
