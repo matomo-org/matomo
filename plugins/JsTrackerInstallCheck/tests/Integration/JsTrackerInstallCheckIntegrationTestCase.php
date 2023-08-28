5916136f113817468a055ebd131ecb17bca5f0dc
Jacob Ransom <jacob@innocraft.com>
2023-08-28 16:54:54 +1200

    Fixing PHPCS errors

--- plugins/JsTrackerInstallCheck/tests/Integration/JsTrackerInstallCheckIntegrationTestCase.php
+++ plugins/JsTrackerInstallCheck/tests/Integration/JsTrackerInstallCheckIntegrationTestCase.php
@@ -2,10 +2,6 @@
 
 namespace Piwik\Plugins\JsTrackerInstallCheck\tests\Integration;
 
-use Piwik\Option;
-use Piwik\Plugins\JsTrackerInstallCheck\JsTrackerInstallCheck;
-use Piwik\Plugins\JsTrackerInstallCheck\NonceOption\JsTrackerInstallCheckOption;
-use Piwik\Tests\Framework\Fixture;
 use Piwik\Tests\Framework\TestCase\IntegrationTestCase;
 
 class JsTrackerInstallCheckIntegrationTestCase extends IntegrationTestCase
