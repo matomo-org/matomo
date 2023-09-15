aca51466b988d7a4e759bfef576d9934b0a0c5c1
Marc Neudert <marc@innocraft.com>
2023-09-15 17:54:39 +0200

    Prefill period selector comparison values from URL hash (#21242)
    * Test reload with compare preselection
    
    * Prefill period selector comparison varlues after reloads
    
    * Build vue components
    
    * Update tests/screenshots
    
    * Apply suggestions from code review
    
    Co-authored-by: caddoo <matthew.caddoo@gmail.com>
    
    * Condense UI tests
    
    * Expand tests for individual comparePeriodType values
    
    * Clarify inner workings of URL/store handling
    
    * Build vue components
    
    ---------
    
    Co-authored-by: caddoo <matthew.caddoo@gmail.com>

--- plugins/CoreHome/vue/src/PeriodSelector/PeriodSelector.vue
+++ plugins/CoreHome/vue/src/PeriodSelector/PeriodSelector.vue
@@ -180,6 +180,8 @@ const Field = useExternalPluginComponent('CorePluginsAdmin', 'Field');
 
 const NBSP = Matomo.helper.htmlDecode('&nbsp;');
 
+const COMPARE_PERIOD_TYPES = ['custom', 'previousPeriod', 'previousYear'];
+
 const COMPARE_PERIOD_OPTIONS = [
   { key: 'custom', value: translate('General_Custom') },
   {
@@ -259,13 +261,16 @@ export default defineComponent({
       window.$(this.$refs.root as HTMLElement).parent('#periodString').show();
     });
 
-    this.updateSelectedValuesFromHash();
-    watch(() => MatomoUrl.parsed.value, this.updateSelectedValuesFromHash);
-
     this.isComparing = ComparisonsStore.isComparingPeriods();
     watch(() => ComparisonsStore.isComparingPeriods(), (newVal) => {
       this.isComparing = newVal;
     });
+
+    this.updateSelectedValuesFromHash();
+    watch(() => MatomoUrl.parsed.value, this.updateSelectedValuesFromHash);
+
+    this.updateComparisonValuesFromStore();
+    watch(() => ComparisonsStore.getPeriodComparisons(), this.updateComparisonValuesFromStore);
 
     window.initTopControls(); // must be called when a top control changes width
 
@@ -310,6 +315,7 @@ export default defineComponent({
       if (this.comparePeriodType === 'custom') {
         return {
           comparePeriods: ['range'],
+          comparePeriodType: 'custom',
           compareDates: [`${this.compareStartDate},${this.compareEndDate}`],
         };
       }
@@ -317,6 +323,7 @@ export default defineComponent({
       if (this.comparePeriodType === 'previousPeriod') {
         return {
           comparePeriods: [this.selectedPeriod],
+          comparePeriodType: 'previousPeriod',
           compareDates: [this.previousPeriodDateToSelectedPeriod],
         };
       }
@@ -336,12 +343,14 @@ export default defineComponent({
         if (this.selectedPeriod === 'range') {
           return {
             comparePeriods: ['range'],
+            comparePeriodType: 'previousYear',
             compareDates: [`${format(currentDateRange[0])},${format(currentDateRange[1])}`],
           };
         }
 
         return {
           comparePeriods: [this.selectedPeriod],
+          comparePeriodType: 'previousYear',
           compareDates: [format(currentDateRange[0])],
         };
       }
@@ -440,6 +449,7 @@ export default defineComponent({
       // get params without comparePeriods/compareSegments/compareDates
       const paramsWithoutCompare = { ...baseParams };
       delete paramsWithoutCompare.comparePeriods;
+      delete paramsWithoutCompare.comparePeriodType;
       delete paramsWithoutCompare.compareDates;
 
       MatomoUrl.updateLocation({
@@ -463,6 +473,46 @@ export default defineComponent({
       }
 
       this.setPiwikPeriodAndDate(this.selectedPeriod, this.dateValue!);
+    },
+    updateComparisonValuesFromStore() {
+      this.comparePeriodType = 'previousPeriod';
+      this.compareStartDate = '';
+      this.compareEndDate = '';
+
+      // first is selected period, second is period to compare to
+      const comparePeriods = ComparisonsStore.getPeriodComparisons();
+
+      if (comparePeriods.length < 2) {
+        return;
+      }
+
+      const comparePeriodType = MatomoUrl.parsed.value.comparePeriodType as string;
+
+      if (!COMPARE_PERIOD_TYPES.includes(comparePeriodType)) {
+        return;
+      }
+
+      this.comparePeriodType = comparePeriodType;
+
+      if (this.comparePeriodType !== 'custom' || comparePeriods[1].params.period !== 'range') {
+        return;
+      }
+
+      let periodObj;
+
+      try {
+        periodObj = Periods.parse(
+          comparePeriods[1].params.period,
+          comparePeriods[1].params.date,
+        ) as Range;
+      } catch {
+        return;
+      }
+
+      const [startDate, endDate] = periodObj.getDateRange();
+
+      this.compareStartDate = format(startDate);
+      this.compareEndDate = format(endDate);
     },
     updateSelectedValuesFromHash() {
       const date = MatomoUrl.parsed.value.date as string;
