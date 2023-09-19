1b1a72dc6eea4b713545103c25175005ed87fe45
sgiehl <stefan@matomo.org>
2023-09-19 17:10:33 +0200

    avoid unnecessary screenshots in updater tests

--- tests/UI/specs/OneClickLastForcedUpdate_spec.js
+++ tests/UI/specs/OneClickLastForcedUpdate_spec.js
@@ -90,9 +90,9 @@ describe("OneClickLastForcedUpdate", function () {
             }
         }
 
-        await page.waitForSelector('#start-tracking-method-list', { visible: true });
-        await page.waitForNetworkIdle();
-        expect(await element.screenshot()).to.matchImage('login');
+        // avoid taking an unnecessary screenshot, as knowing we land on #site-without-data is enough
+        await page.waitForSelector('#site-without-data', { visible: true });
+        await page.evaluate(() => window.stop()); // stop ongoing requests
     });
 
     it('should have a working cron archiving process', async function () {
