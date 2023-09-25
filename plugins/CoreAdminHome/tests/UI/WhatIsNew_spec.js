199dc7563a00efac6910fec0546800ea22b3d1c2
Ben Burgess <88810029+bx80@users.noreply.github.com>
2023-09-25 23:12:03 +1300

    Update plugins/CoreAdminHome/tests/UI/WhatIsNew_spec.js
    Co-authored-by: Stefan Giehl <stefan@matomo.org>

--- plugins/CoreAdminHome/tests/UI/WhatIsNew_spec.js
+++ plugins/CoreAdminHome/tests/UI/WhatIsNew_spec.js
@@ -26,7 +26,7 @@ describe("WhatIsNew", function () {
     it('should show the what is new changes popup', async function() {
         await page.goto('?module=CoreHome');
         await page.waitForSelector('.whatisnew', {visible:true});
-        await page.waitForNetworkIdle(500);
+        await page.waitForNetworkIdle();
         expect(await page.screenshot({ fullPage: true })).to.matchImage('what_is_new');
     });
 });
