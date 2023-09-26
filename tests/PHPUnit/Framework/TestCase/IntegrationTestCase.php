58d3aaef575c84929024147482fcf035c465b76a
Marc Neudert <marc@innocraft.com>
2023-09-26 21:56:53 +0200

    Do not drop database during integration test teardown

--- tests/PHPUnit/Framework/TestCase/IntegrationTestCase.php
+++ tests/PHPUnit/Framework/TestCase/IntegrationTestCase.php
@@ -114,8 +114,9 @@ abstract class IntegrationTestCase extends SystemTestCase
      */
     protected static function configureFixture($fixture)
     {
-        $fixture->createSuperUser     = false;
-        $fixture->configureComponents = false;
+        $fixture->createSuperUser        = false;
+        $fixture->configureComponents    = false;
+        $fixture->dropDatabaseInTearDown = false;
 
         $fixture->extraTestEnvVars['loadRealTranslations'] = false;
     }
