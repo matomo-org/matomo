178910f357585cf873f3e1f30fa68df5843b20f7
Michal Kleiner <michal@innocraft.com>
2023-10-15 16:50:18 +1300

    Adjust content

--- plugins/ProfessionalServices/Controller.php
+++ plugins/ProfessionalServices/Controller.php
@@ -18,16 +18,15 @@ class Controller extends \Piwik\Plugin\Controller
         $view = new View('@ProfessionalServices/pluginAdvertising');
         $this->setGeneralVariablesView($view);
 
-        $view->title = 'Unlock the power of Heatmaps';
+        $view->title = 'Unlock the Power of Heatmaps';
         $view->helpUrl = 'https://matomo.org/'; // where should this go if anywhere? It's required for the rating.
         $view->imageName = 'ad-heatmaps.png';
         $view->imageAlt = $view->title; // adjust if needed per plugin
         $view->listOfFeatures = [
-            "Get visual representations of user interactions on your website, making it easy to understand how visitors engage with your content. Get visual representations of user interactions on your website, making it easy to understand how visitors engage with your content.",
+            "Get visual representations of user interactions on your website, making it easy to understand how visitors engage with your content.",
             "Get actionable data to optimise your website's layout, content placement, and user experience.",
             "Identify and address user behaviour patterns, to increase conversion rates and achieve better results from your digital efforts.",
         ];
-        $view->legalInfo = 'This is some legal information worth knowing about. Can have a link to <a href="#">learn more</a>.';
 
         return $view->render();
     }
