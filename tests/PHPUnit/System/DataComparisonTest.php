001e99fc3f57e217a5f40b99c571688458ad02a2
sgiehl <stefan@matomo.org>
2023-11-20 14:43:18 +0100

    Adjust tests to avoid regressions

--- tests/PHPUnit/System/DataComparisonTest.php
+++ tests/PHPUnit/System/DataComparisonTest.php
@@ -10,7 +10,9 @@ namespace Piwik\Tests\System;
 
 use Piwik\API\Request;
 use Piwik\DataTable;
+use Piwik\Plugins\CustomDimensions\CustomDimensions;
 use Piwik\Tests\Fixtures\ManySitesImportedLogs;
+use Piwik\Tests\Framework\Fixture;
 use Piwik\Tests\Framework\TestCase\SystemTestCase;
 /**
  * Testing Data comparison
@@ -23,6 +25,20 @@ class DataComparisonTest extends SystemTestCase
      * @var ManySitesImportedLogs
      */
     public static $fixture;
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+
+        // Configures a custom dimensions and adds a segment for it. This segment will only be available for
+        // the specific site and will be invalid in global context
+        // Added to avoid further regressions like: https://github.com/matomo-org/matomo/issues/21573
+        $idDimension = \Piwik\Plugins\CustomDimensions\API::getInstance()->configureNewCustomDimension(
+            self::$fixture->idSite, 'test', CustomDimensions::SCOPE_VISIT,  true
+        );
+        Fixture::clearInMemoryCaches(false);
+        \Piwik\Plugins\SegmentEditor\API::getInstance()->add('custom dimension', "dimension$idDimension==test", self::$fixture->idSite);
+    }
 
     /**
      * @dataProvider getApiForTesting
@@ -48,7 +64,7 @@ class DataComparisonTest extends SystemTestCase
             ['Referrers.getWebsites', 'Referrers.getUrlsFromWebsiteId'],
         ];
 
-        foreach ($apiToTest as list($superApiMethod, $subtableApiMethod)) {
+        foreach ($apiToTest as [$superApiMethod, $subtableApiMethod]) {
             /** @var DataTable $topLevelComparisons */
             $topLevelComparisons = Request::processRequest($superApiMethod, [
                 'idSite' => self::$fixture->idSite,
