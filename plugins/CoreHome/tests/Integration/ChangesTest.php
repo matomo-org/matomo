2a9f7ba4be7cad598b4d7205fbaf12dbef7eb422
Stefan Giehl <stefan@matomo.org>
2023-11-28 09:18:58 +0100

    fix tests

--- plugins/CoreHome/tests/Integration/ChangesTest.php
+++ plugins/CoreHome/tests/Integration/ChangesTest.php
@@ -27,21 +27,21 @@ class ChangesTest extends IntegrationTestCase
 
     public function test_CoreHomeChanges_ShouldSortChangeListMostRecentFirst()
     {
-        $json = '{"idchange":6,"plugin_name":"CoreHome","version":"4.6.0b5","title":"New feature x added","description":"Now you can do a with b like this","link_name":"For more information go here","link":"https:\/\/www.matomo.org"}';
+        $json = '{"plugin_name":"CoreHome","version":"4.6.0b5","title":"New feature x added","description":"Now you can do a with b like this","link_name":"For more information go here","link":"https:\/\/www.matomo.org"}';
         $changesModel = new ChangesModel();
         $changes = $changesModel->getChangeItems();
         $r = reset($changes);
-        unset($r['created_time']);
+        unset($r['created_time'], $r['idchange']);
         $this->assertEquals($json, json_encode($r, true));
     }
 
     public function test_CoreHomeChanges_ShouldAllowChangeItemAddWithoutLink()
     {
-        $json = '{"idchange":5,"plugin_name":"CoreHome","version":"4.5.0","title":"New feature y added","description":"Now you can do c with d like this","link_name":null,"link":null}';
+        $json = '{"plugin_name":"CoreHome","version":"4.5.0","title":"New feature y added","description":"Now you can do c with d like this","link_name":null,"link":null}';
         $changesModel = new ChangesModel();
         $changes = $changesModel->getChangeItems();
         $r = $changes[1];
-        unset($r['created_time']);
+        unset($r['created_time'], $r['idchange']);
         $this->assertEquals($json, json_encode($r, true));
     }
 
