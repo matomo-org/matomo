e774da9caffd892bc9a160a14cfdb2da21059762
Ben <ben.burgess@innocraft.com>
2023-12-11 22:34:55 +1300

    Update tests (minor whitespace change)

--- tests/PHPUnit/Integration/DataAccess/LogAggregatorTest.php
+++ tests/PHPUnit/Integration/DataAccess/LogAggregatorTest.php
@@ -431,7 +431,7 @@ class LogAggregatorTest extends IntegrationTestCase
 				AND log_visit.visit_last_action_time <= ?
 				AND log_visit.idsite IN (?) )
                 AND
-                ( log_visit.user_id = ? )
+                (log_visit.user_id = ?)
 			ORDER BY
 				log_visit.idvisit ASC",
             'bind' => [
@@ -460,7 +460,7 @@ class LogAggregatorTest extends IntegrationTestCase
 				AND log_visit.visit_last_action_time <= ?
 				AND log_visit.idsite IN (?) )
                 AND
-                ( log_visit.user_id = ? )
+                (log_visit.user_id = ?)
 			ORDER BY
 				log_visit.idvisit ASC",
             'bind' => [
