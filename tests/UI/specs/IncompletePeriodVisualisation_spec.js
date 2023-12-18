de2725bb3e23bb7e221dc9a424c60bb649b0149c
Marc Neudert <marc@innocraft.com>
2023-12-18 16:28:08 +0100

    Test content of "incomplete if today" graph tooltip

--- tests/UI/specs/IncompletePeriodVisualisation_spec.js
+++ tests/UI/specs/IncompletePeriodVisualisation_spec.js
@@ -7,17 +7,31 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 
-describe("IncompletePeriodVisualisation", function () {
-    this.fixture = "Piwik\\Tests\\Fixtures\\SomeVisitsLastYearAndThisYear";
+describe('IncompletePeriodVisualisation', function () {
+    this.fixture = 'Piwik\\Tests\\Fixtures\\SomeVisitsLastYearAndThisYear';
 
     const generalParams = 'idSite=1&period=year&date=today';
     const pageUrl = '?module=CoreHome&action=index&' + generalParams;
 
     it('should load visitors > overview page and show incomplete period', async function () {
-        await page.goto(pageUrl + generalParams + "&segment=&category=General_Visitors&subcategory=General_Overview#?idSite=1&period=year&date=today&segment=&category=General_Visitors&subcategory=General_Overview");
+        await page.goto(pageUrl + generalParams + '&segment=&category=General_Visitors&subcategory=General_Overview#?idSite=1&period=year&date=today&segment=&category=General_Visitors&subcategory=General_Overview');
         await page.waitForNetworkIdle();
         const pageWrap = await page.$('.pageWrap');
+
         expect(await pageWrap.screenshot()).to.matchImage('visitors_overview');
     });
 
+    it('tooltip for incomplete period should say "incomplete period"', async function () {
+        const graph = await page.$('.piwik-graph');
+        const boundingBox = await graph.boundingBox();
+
+        await page.mouse.move(
+          boundingBox.x + boundingBox.width - 50,
+          boundingBox.y + boundingBox.height - 50
+        );
+
+        const tooltipContent = await page.evaluate(() => $('.ui-tooltip').text().trim());
+
+        expect(tooltipContent).to.contain('Incomplete Period');
+    });
 });
