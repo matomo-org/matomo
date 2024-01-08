5af4eb29f6f7b277db82398ca22761f4b5fa38d1
sgiehl <stefan@matomo.org>
2024-01-08 14:52:45 +0100

    Add update script to ensure SEO plugin is removed on update

---
+++ core/Updates/5.0.2-b1.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+namespace Piwik\Updates;
+
+use Piwik\Updater;
+use Piwik\Updates as PiwikUpdates;
+use Piwik\Updater\Migration;
+use Piwik\Updater\Migration\Factory as MigrationFactory;
+
+/**
+ * Update for version 5.0.2-b1
+ */
+class Updates_5_0_2_b1 extends PiwikUpdates
+{
+    /**
+     * @var MigrationFactory
+     */
+    private $migration;
+
+    public function __construct(MigrationFactory $factory)
+    {
+        $this->migration = $factory;
+    }
+
+    /**
+     * @param Updater $updater
+     *
+     * @return Migration[]
+     */
+    public function getMigrations(Updater $updater)
+    {
+        $migrations = [];
+
+        // remove discontinued SEO plugin
+        $migrations[] = $this->migration->plugin->deactivate('SEO');
+        $migrations[] = $this->migration->plugin->uninstall('SEO');
+
+        return $migrations;
+    }
+
+    public function doUpdate(Updater $updater)
+    {
+        $updater->executeMigrations(__FILE__, $this->getMigrations($updater));
+    }
+}
