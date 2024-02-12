8ea2657cdde3f02afff16c8a154f587c4a0344b0
Stefan Giehl <stefan@matomo.org>
2024-02-12 21:07:59 +0100

    Never show changes older than 6 months in What's new popover (#21917)

--- plugins/CoreHome/tests/Integration/ChangesTest.php
+++ plugins/CoreHome/tests/Integration/ChangesTest.php
@@ -24,9 +24,17 @@ class ChangesTest extends IntegrationTestCase
      */
     public static $fixture;
 
-    public function test_CoreHomeChanges_ShouldSortChangeListMostRecentFirst()
+    public function testCoreHomeChangesShouldNotReturnChangesOlderThan6Months()
     {
-        $json = '{"idchange":3,"plugin_name":"CoreHome","version":"4.6.0b5","title":"New feature x added","description":"Now you can do a with b like this","link_name":"For more information go here","link":"https:\/\/www.matomo.org"}';
+        $changesModel = new ChangesModel();
+        $changes = $changesModel->getChangeItems();
+        self::assertCount(3, $changes);
+        self::assertNotContains(1, array_column($changes, 'idchange'));
+    }
+
+    public function testCoreHomeChangesShouldSortChangeListMostRecentFirst()
+    {
+        $json = '{"idchange":4,"plugin_name":"CoreHome","version":"4.6.0b5","title":"New feature x added","description":"Now you can do a with b like this","link_name":"For more information go here","link":"https:\/\/www.matomo.org"}';
         $changesModel = new ChangesModel();
         $changes = $changesModel->getChangeItems();
         $r = reset($changes);
@@ -34,9 +42,9 @@ class ChangesTest extends IntegrationTestCase
         $this->assertEquals($json, json_encode($r, true));
     }
 
-    public function test_CoreHomeChanges_ShouldAllowChangeItemAddWithoutLink()
+    public function testCoreHomeChangesShouldAllowChangeItemAddWithoutLink()
     {
-        $json = '{"idchange":2,"plugin_name":"CoreHome","version":"4.5.0","title":"New feature y added","description":"Now you can do c with d like this","link_name":null,"link":null}';
+        $json = '{"idchange":3,"plugin_name":"CoreHome","version":"4.5.0","title":"New feature y added","description":"Now you can do c with d like this","link_name":null,"link":null}';
         $changesModel = new ChangesModel();
         $changes = $changesModel->getChangeItems();
         $r = $changes[1];
