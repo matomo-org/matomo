ad1a3767d93765d6a6ff0ea411a505714e9fb447
sgiehl <stefan@matomo.org>
2024-02-15 15:34:51 +0100

    Simplify cta container conditions

--- plugins/Marketplace/vue/src/PluginList/CTAContainer.vue
+++ plugins/Marketplace/vue/src/PluginList/CTAContainer.vue
@@ -32,21 +32,18 @@
       >{{ translate('General_Help') }}</a>)</span>
     </div>
 
-    <a v-else-if="plugin.canBeUpdated && 0 == plugin.missingRequirements.length
-                                  && isAutoUpdatePossible"
-       tabindex="7" class="btn btn-block"
-       :href="linkTo({
+    <template v-else-if="plugin.canBeUpdated && 0 == plugin.missingRequirements.length">
+      <a v-if="isAutoUpdatePossible"
+         tabindex="7" class="btn btn-block"
+         :href="linkTo({
                           module: 'Marketplace',
                           action: 'updatePlugin',
                           pluginName: plugin.name,
                           nonce: updateNonce
                        })">
-      {{ translate('CoreUpdater_UpdateTitle') }}
-    </a>
-
-    <template v-else-if="plugin.missingRequirements.length > 0 || !isAutoUpdatePossible">
-
-      <div v-if="plugin.canBeUpdated && 0 === plugin.missingRequirements.length"
+        {{ translate('CoreUpdater_UpdateTitle') }}
+      </a>
+      <div v-else
            class="alert alert-warning alert-no-background">
         {{ translate('Marketplace_CannotUpdate') }}
         <span
@@ -62,81 +59,41 @@
           :is-auto-update-possible="isAutoUpdatePossible"
         />)</span>
       </div>
-
-      <div v-else-if="plugin.isInstalled"
-           class="alert alert-success alert-no-background">
-        {{ translate('General_Installed') }}
-        (<DownloadButton
-        :plugin="plugin"
-        :show-or="false"
-        :is-auto-update-possible="isAutoUpdatePossible"
-      />)
-      </div>
-
-      <a v-else-if="plugin.isEligibleForFreeTrial && isValidConsumer"
-         tabindex="7"
-         class="btn btn-block purchaseable"
-         href="#"
-         v-plugin-start-free-trial="{ pluginName: plugin.name }"
-         :title="translate('Marketplace_StartFreeTrial')"
-      >
-        {{ translate('Marketplace_StartFreeTrial') }}
-      </a>
-
-      <a v-else-if="!plugin.isDownloadable"
-         tabindex="7"
-         class="btn btn-block"
-         href="#"
-         v-plugin-name="{ pluginName: plugin.name }"
-         :title="translate('General_MoreDetails')"
-      >
-        {{ translate('General_MoreDetails') }}
-      </a>
-
-      <div v-else
-           class="alert alert-warning alert-no-background">
-        {{ translate('Marketplace_CannotInstall') }}
-        <span
-          style="white-space:nowrap"
-        >(<a tabindex="7"
-             class="plugin-details"
-             href="#"
-             v-plugin-name="{ pluginName: plugin.name }"
-             :title="translate('General_MoreDetails')"
-        >{{ translate('General_Help') }}</a><DownloadButton
-          :plugin="plugin"
-          :show-or="false"
-          :is-auto-update-possible="isAutoUpdatePossible"
-        />)</span>
-      </div>
     </template>
 
     <div v-else-if="plugin.isInstalled"
          class="alert alert-success alert-no-background">
       {{ translate('General_Installed') }}
 
-      <template v-if="!plugin.isInvalid && !isMultiServerEnvironment && isPluginsAdminEnabled">
+      <template v-if="plugin.missingRequirements.length > 0 || !isAutoUpdatePossible">
+        (<DownloadButton
+          :plugin="plugin"
+          :show-or="false"
+          :is-auto-update-possible="isAutoUpdatePossible"
+        />)
+      </template>
+      <template v-else-if="!plugin.isInvalid && !isMultiServerEnvironment && isPluginsAdminEnabled">
         (<a v-if="plugin.isActivated"
-           tabindex="7"
-           :href="linkTo({
+            tabindex="7"
+            :href="linkTo({
                               module: 'CorePluginsAdmin',
                               action: 'deactivate',
                               pluginName: plugin.name,
                               nonce: deactivateNonce,
                               redirectTo: 'referrer' })"
-        >{{ translate('CorePluginsAdmin_Deactivate') }}</a
-        ><template v-else-if="plugin.missingRequirements.length > 0">
-          -
-        </template
-        ><a v-else
-           tabindex="7"
-           :href="linkTo({
+      >{{ translate('CorePluginsAdmin_Deactivate') }}</a
+      ><template v-else-if="plugin.missingRequirements.length > 0">
+        -
+      </template
+      ><a v-else
+          tabindex="7"
+          :href="linkTo({
                               module: 'CorePluginsAdmin',
                               action: 'activate',
                               pluginName: plugin.name,
                               nonce: activateNonce,
                               redirectTo: 'referrer' })"
-        >{{ translate('CorePluginsAdmin_Activate') }}</a>)
+      >{{ translate('CorePluginsAdmin_Activate') }}</a>)
       </template>
     </div>
 
@@ -150,7 +107,8 @@
       {{ translate('Marketplace_StartFreeTrial') }}
     </a>
 
-    <a v-else-if="plugin.isPaid && !plugin.isDownloadable"
+    <a v-else-if="!plugin.isDownloadable
+       && (plugin.isPaid || plugin.missingRequirements.length > 0 || !isAutoUpdatePossible)"
        tabindex="7"
        class="btn btn-block"
        href="#"
@@ -159,6 +117,23 @@
     >
       {{ translate('General_MoreDetails') }}
     </a>
+
+    <div v-else-if="plugin.missingRequirements.length > 0 || !isAutoUpdatePossible"
+         class="alert alert-warning alert-no-background">
+      {{ translate('Marketplace_CannotInstall') }}
+      <span
+        style="white-space:nowrap"
+      >(<a tabindex="7"
+           class="plugin-details"
+           href="#"
+           v-plugin-name="{ pluginName: plugin.name }"
+           :title="translate('General_MoreDetails')"
+      >{{ translate('General_Help') }}</a><DownloadButton
+        :plugin="plugin"
+        :show-or="true"
+        :is-auto-update-possible="isAutoUpdatePossible"
+      />)</span>
+    </div>
 
     <a v-else
        tabindex="7"
