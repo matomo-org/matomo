7a8c03b722e8f6682f4154a5871f7c8ebbfa42a6
Marc Neudert <marc@innocraft.com>
2024-03-07 23:20:06 +0100

    Cache additional site urls in live visitor details (#21992)
    * Cache additional site urls in live visitor details
    
    * Apply review feedback
    
    * Replace transient cache with caching variable
    
    * Fix CS
    
    ---------
    
    Co-authored-by: Michal Kleiner <michal@innocraft.com>

--- plugins/Live/VisitorDetails.php
+++ plugins/Live/VisitorDetails.php
@@ -25,6 +25,11 @@ use Piwik\View;
 
 class VisitorDetails extends VisitorDetailsAbstract
 {
+    /**
+     * @var array<int, array<string>>
+     */
+    private $cachedAdditionalSiteUrls = [];
+
     public function extendVisitorDetails(&$visitor)
     {
         $idSite     = $this->getIdSite();
@@ -99,16 +104,16 @@ class VisitorDetails extends VisitorDetailsAbstract
             return;
         }
 
-        $sitesModel = new \Piwik\Plugins\SitesManager\Model();
-
         if (isset($action['type']) && in_array($action['type'], ['outlink', 'download']) && isset($action['url'])) {
             $action['url'] = html_entity_decode($action['url'], ENT_QUOTES, "UTF-8");
         }
 
+        $idSite = $this->getIdSite();
+
         $view                 = new View($template);
         $view->sendHeadersWhenRendering = false;
-        $view->mainUrl        = trim(Site::getMainUrlFor($this->getIdSite()));
-        $view->additionalUrls = $sitesModel->getAliasSiteUrlsFromId($this->getIdSite());
+        $view->mainUrl        = trim(Site::getMainUrlFor($idSite));
+        $view->additionalUrls = $this->getAdditionalUrlsForSite($idSite);
         $view->action         = $action;
         $view->previousAction = $previousAction;
         $view->visitInfo      = $visitorDetails;
@@ -308,5 +313,22 @@ class VisitorDetails extends VisitorDetailsAbstract
         }
 
         return $visit->getColumn('referrerName');
+    }
+
+    /**
+     * @return array<int, array<string>>
+     */
+    private function getAdditionalUrlsForSite(int $idSite): array
+    {
+        if (isset($this->cachedAdditionalSiteUrls[$idSite])) {
+            return $this->cachedAdditionalSiteUrls[$idSite];
+        }
+
+        $sitesModel = new \Piwik\Plugins\SitesManager\Model();
+        $additionalSiteUrls = $sitesModel->getAliasSiteUrlsFromId($idSite);
+
+        $this->cachedAdditionalSiteUrls[$idSite] = $additionalSiteUrls;
+
+        return $additionalSiteUrls;
     }
 }
