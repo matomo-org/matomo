8afd6fa7c73b60dc5ab0f147949ea2ddf5b90dde
Marc Neudert <marc@innocraft.com>
2024-04-10 17:44:42 +0200

    Add UI tests for "create account and start free trial" workflow

--- plugins/Marketplace/tests/UI/Marketplace_StartFreeTrial_spec.js
+++ plugins/Marketplace/tests/UI/Marketplace_StartFreeTrial_spec.js
@@ -10,19 +10,13 @@
 describe('Marketplace_StartFreeTrial', function () {
   this.fixture = "Piwik\\Plugins\\Marketplace\\tests\\Fixtures\\SimpleFixtureTrackFewVisits";
 
+  const parentSuite = this;
+
   const errorModalSelector = '#startFreeTrial .trial-start-error';
   const inProgressModalSelector = '#startFreeTrial .trial-start-in-progress';
+  const noLicenseModalSelector = '#startFreeTrial .trial-start-no-license';
   const pluginsUrl = '?module=Marketplace&action=overview';
   const startFreeTrialSelector = '.card-content .cta-container .btn.purchaseable';
-
-  function setEnvironment(startFreeTrialSuccess) {
-    testEnvironment.overrideConfig('General', 'enable_plugins_admin', '1');
-
-    testEnvironment.consumer = 'validLicense';
-    testEnvironment.mockMarketplaceApiService = 1;
-    testEnvironment.startFreeTrialSuccess = startFreeTrialSuccess;
-    testEnvironment.save();
-  }
 
   async function goToPluginsPage(){
     await page.goto(pluginsUrl);
@@ -43,37 +37,139 @@ describe('Marketplace_StartFreeTrial', function () {
     expect(await page.screenshotSelector(selector)).to.matchImage(name);
   }
 
-  it ('should display an error if the start trial process fails', async function() {
-    setEnvironment(false);
+  describe('create new account and start free trial', function() {
+    this.title = parentSuite.title; // to make sure the screenshot prefix is the same
 
-    await goToPluginsPage();
+    function setEnvironment(createAccountResponseCode) {
+      testEnvironment.overrideConfig('General', 'enable_plugins_admin', '1');
 
-    await page.click(startFreeTrialSelector);
-    await page.waitForSelector(inProgressModalSelector, { visible: true });
-    await page.waitForSelector(inProgressModalSelector, { hidden: true });
-    await page.waitForSelector(errorModalSelector, { visible: true });
+      testEnvironment.mockMarketplaceApiService = 1;
+      testEnvironment.createAccountResponseCode = createAccountResponseCode;
+      testEnvironment.startFreeTrialSuccess = true;
+      testEnvironment.save();
+    }
 
-    const error = await page.$(`${errorModalSelector} p:first-of-type`);
-    const errorMessage = await error.getProperty('textContent');
+    async function typeEmail(email) {
+      const emailInputSelector = `${noLicenseModalSelector} input[name="email"]`;
 
-    expect(errorMessage).to.match(/There was an error starting your free trial/i);
+      await page.evaluate(function(emailInputSelector) {
+        $(emailInputSelector).val('');
+      }, (emailInputSelector));
 
-    await screenshotModalSelector(errorModalSelector, 'start_free_trial_error_modal');
+      await page.type(emailInputSelector, email);
+    }
+
+    it('should display a modal to create a new account', async function () {
+      setEnvironment(null);
+
+      await goToPluginsPage();
+      await page.click(startFreeTrialSelector);
+      await page.waitForSelector(noLicenseModalSelector, { visible: true });
+
+      await screenshotModalSelector(noLicenseModalSelector, 'no_license_modal');
+    });
+
+    it('should display an error message if email is rejected by validation', async function () {
+      await typeEmail('<this&does;not"validate');
+
+      await page.click(`${noLicenseModalSelector} .btn`);
+      await page.waitForSelector(noLicenseModalSelector, { hidden: true });
+      await page.waitForSelector(inProgressModalSelector, { visible: true });
+      await page.waitForSelector(inProgressModalSelector, { hidden: true });
+      await page.waitForSelector(noLicenseModalSelector, { visible: true });
+
+      await screenshotModalSelector(noLicenseModalSelector, 'no_license_modal_invalid');
+    });
+
+    it('should display an error message if marketplace rejects the email', async function () {
+      setEnvironment(400);
+
+      await typeEmail('reject@example.org');
+
+      await page.click(`${noLicenseModalSelector} .btn`);
+      await page.waitForSelector(noLicenseModalSelector, { hidden: true });
+      await page.waitForSelector(inProgressModalSelector, { visible: true });
+      await page.waitForSelector(inProgressModalSelector, { hidden: true });
+      await page.waitForSelector(noLicenseModalSelector, { visible: true });
+
+      await screenshotModalSelector(noLicenseModalSelector, 'no_license_modal_rejected');
+    });
+
+    it('should display an error message if marketplace flags email as duplicate', async function () {
+      setEnvironment(409);
+
+      await typeEmail('duplicate@example.org');
+
+      await page.click(`${noLicenseModalSelector} .btn`);
+      await page.waitForSelector(noLicenseModalSelector, { hidden: true });
+      await page.waitForSelector(inProgressModalSelector, { visible: true });
+      await page.waitForSelector(inProgressModalSelector, { hidden: true });
+      await page.waitForSelector(noLicenseModalSelector, { visible: true });
+
+      await screenshotModalSelector(noLicenseModalSelector, 'no_license_modal_duplicate');
+    });
+
+    it('should display a success message if the process succeeds', async function () {
+      setEnvironment(200);
+
+      await typeEmail('hello@example.org');
+
+      await page.click(`${noLicenseModalSelector} .btn`);
+      await page.waitForSelector(inProgressModalSelector, { visible: true });
+      await page.waitForSelector(inProgressModalSelector, { hidden: true });
+      await page.waitForSelector('.notification-success', { visible: true });
+
+      const notification = await page.$('.notification-success');
+      const notificationText = await notification.getProperty('textContent');
+
+      expect(notificationText).to.match(/added your License key, .* free trial for PaidPlugin1/i);
+    });
   });
 
-  it ('should display a success message if the start trial process succeeds', async function() {
-    setEnvironment(true);
+  describe('with existing license', function () {
+    this.title = parentSuite.title; // to make sure the screenshot prefix is the same
 
-    await goToPluginsPage();
+    function setEnvironment(startFreeTrialSuccess) {
+      testEnvironment.overrideConfig('General', 'enable_plugins_admin', '1');
 
-    await page.click(startFreeTrialSelector);
-    await page.waitForSelector(inProgressModalSelector, { visible: true });
-    await page.waitForSelector(inProgressModalSelector, { hidden: true });
-    await page.waitForSelector('.notification-success', { visible: true });
+      testEnvironment.consumer = 'validLicense';
+      testEnvironment.mockMarketplaceApiService = 1;
+      testEnvironment.startFreeTrialSuccess = startFreeTrialSuccess;
+      testEnvironment.save();
+    }
 
-    const notification = await page.$('.notification-success');
-    const notificationText = await notification.getProperty('textContent');
+    it('should display an error if the start trial process fails', async function () {
+      setEnvironment(false);
 
-    expect(notificationText).to.match(/free trial has started .+ PaidPlugin1/i);
+      await goToPluginsPage();
+
+      await page.click(startFreeTrialSelector);
+      await page.waitForSelector(inProgressModalSelector, { visible: true });
+      await page.waitForSelector(inProgressModalSelector, { hidden: true });
+      await page.waitForSelector(errorModalSelector, { visible: true });
+
+      const error = await page.$(`${errorModalSelector} p:first-of-type`);
+      const errorMessage = await error.getProperty('textContent');
+
+      expect(errorMessage).to.match(/There was an error starting your free trial/i);
+
+      await screenshotModalSelector(errorModalSelector, 'start_free_trial_error_modal');
+    });
+
+    it('should display a success message if the start trial process succeeds', async function () {
+      setEnvironment(true);
+
+      await goToPluginsPage();
+
+      await page.click(startFreeTrialSelector);
+      await page.waitForSelector(inProgressModalSelector, { visible: true });
+      await page.waitForSelector(inProgressModalSelector, { hidden: true });
+      await page.waitForSelector('.notification-success', { visible: true });
+
+      const notification = await page.$('.notification-success');
+      const notificationText = await notification.getProperty('textContent');
+
+      expect(notificationText).to.match(/free trial has started .+ PaidPlugin1/i);
+    });
   });
 });
