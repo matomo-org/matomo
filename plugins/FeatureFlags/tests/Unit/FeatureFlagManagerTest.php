0037bed4bc823a4cfc5a8fea88d67d165fb3b456
caddoo <matthew.caddoo@gmail.com>
2024-05-15 19:44:52 +1200

    Add license info to files

--- plugins/FeatureFlags/tests/Unit/FeatureFlagManagerTest.php
+++ plugins/FeatureFlags/tests/Unit/FeatureFlagManagerTest.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
 namespace Piwik\Plugins\FeatureFlags\tests\Unit;
 
 use PHPUnit\Framework\TestCase;
