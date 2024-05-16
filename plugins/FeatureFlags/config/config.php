d639e41af78ef68f4e024d22f8e3e745131a0efa
caddoo <matthew.caddoo@gmail.com>
2024-05-16 17:12:38 +1200

    Update plugins/FeatureFlags/config/config.php
    Co-authored-by: Michal Kleiner <michal@innocraft.com>

--- plugins/FeatureFlags/config/config.php
+++ plugins/FeatureFlags/config/config.php
@@ -7,7 +7,7 @@ return [
      * The first one will be overwritten by the second one (if set).
      */
     'featureflag.storages' => [
-        Piwik\DI::get('Piwik\Plugins\FeatureFlags\Storage\ConfigFeatureFlagStorage')
+        Piwik\DI::get('Piwik\Plugins\FeatureFlags\Storage\ConfigFeatureFlagStorage'),
     ],
     'Piwik\Plugins\FeatureFlags\FeatureFlagManager' => Piwik\DI::autowire()
         ->constructor(Piwik\DI::get('featureflag.storages'))
