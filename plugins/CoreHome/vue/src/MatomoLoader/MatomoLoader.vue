dab6fc9dc1436ef36a69807b2791388c4ac6e193
Michal Kleiner <michal@innocraft.com>
2024-05-22 03:55:15 +1200

    Add pure CSS Matomo loader

---
+++ plugins/CoreHome/vue/src/MatomoLoader/MatomoLoader.vue
@@ -0,0 +1,16 @@
+<!--
+  Matomo - free/libre analytics platform
+
+  @link    https://matomo.org
+  @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+-->
+
+<template>
+    <span class="matomo-loader"><span></span><span></span><span></span></span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
