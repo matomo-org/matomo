fc0d11c9c953542027aaebbe233f08649876ef15
caddoo <matthew.caddoo@gmail.com>
2024-05-29 19:56:50 +1200

    Bump version number

--- core/Version.php
+++ core/Version.php
@@ -22,7 +22,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    public const VERSION = '5.2.0-alpha';
+    public const VERSION = '5.2.0-alpha.20240529033030';
 
     public const MAJOR_VERSION = 5;
 
