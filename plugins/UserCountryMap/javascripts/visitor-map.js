79f4bd138af126a8ffdb49bf9981ed90e15b770f
caddoo <matthew.caddoo@gmail.com>
2024-05-31 18:04:14 +1200

    Add the -2 back and change order

--- plugins/UserCountryMap/javascripts/visitor-map.js
+++ plugins/UserCountryMap/javascripts/visitor-map.js
@@ -1359,7 +1359,7 @@
                 h = Math.min(maxHeight, h);
             }
 
-            map.container.height(h);
+            map.container.height(h - 2);
             map.resize(w, h);
 
             if (w < 355) $('.UserCountryMap .tableIcon span').hide();
