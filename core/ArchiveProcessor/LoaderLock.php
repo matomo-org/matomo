aae8c31ef5e43208dff037964ba518813b0059ce
caddoo <matthew.caddoo@gmail.com>
2024-07-25 16:49:48 +1200

    Change Do -> Select for Mysqli Tidb Compat

--- core/ArchiveProcessor/LoaderLock.php
+++ core/ArchiveProcessor/LoaderLock.php
@@ -42,7 +42,7 @@ class LoaderLock
 
     public function unLock()
     {
-        Db::query('DO RELEASE_LOCK(?)', array($this->id));
+        Db::query('SELECT RELEASE_LOCK(?)', array($this->id));
     }
 
     public function getId()
