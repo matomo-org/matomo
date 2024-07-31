34caa6df171fffc7fab6ab04f34d722e8d1e7123
sgiehl <stefan@matomo.org>
2024-07-31 18:41:58 +0200

    allow database specific configs

--- config/environment/ui-test.php
+++ config/environment/ui-test.php
@@ -138,16 +138,6 @@ return [
             }
         })],
 
-        ['API.ScheduledReports.getReports.end', Piwik\DI::value(function (&$result, $parameters) {
-            // TiDb uses a different collation, causing the report order to be slightly different.
-            // To have a consistent sorting, we manually sort here in PHP again.
-            if (\Piwik\Config\DatabaseConfig::getConfigValue('schema') === 'Tidb') {
-                usort($result, function ($a, $b) {
-                    return strcmp($a['description'], $b['description']);
-                });
-            }
-        })],
-
         \Piwik\Tests\Framework\XssTesting::getJavaScriptAddEvent(),
     ]),
 
