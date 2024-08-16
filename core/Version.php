448795e9071cb61a17b682ad650c3f958fefe7e9
sgiehl <sgiehl@users.noreply.github.com>
2024-08-16 15:05:37 +0000

    Update version to 5.2.0-alpha.20240816150537

--- core/Version.php
+++ core/Version.php
@@ -22,7 +22,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    public const VERSION = '5.2.0-alpha';
+    public const VERSION = '5.2.0-alpha.20240816150537';
 
     public const MAJOR_VERSION = 5;
 
