812fbdd89b0a836975ac91e1b68e28c53368ec0c
Stefan Giehl <stefan@matomo.org>
2024-09-20 15:58:32 +0200

    Ensure LatestStable release channel uses the version provided from API (#22603)
    * Ensure LatestStable release channel uses the version provided from API
    
    * Adjust test and link in UI
    
    * always download latest stable if no version provided

--- plugins/CoreUpdater/tests/Integration/ReleaseChannelTest.php
+++ plugins/CoreUpdater/tests/Integration/ReleaseChannelTest.php
@@ -48,7 +48,7 @@ class ReleaseChannelTest extends IntegrationTestCase
 
     public function test_getDownloadUrlWithoutScheme_shouldReturnUrlWithVersionNumberButWithoutScheme()
     {
-        $this->assertSame('://builds.matomo.org/piwik-2.15.0-b5.zip', $this->channel->getDownloadUrlWithoutScheme('2.15.0-b5'));
+        $this->assertSame('://builds.matomo.org/matomo-2.15.0-b5.zip', $this->channel->getDownloadUrlWithoutScheme('2.15.0-b5'));
     }
 
     public function test_getUrlToCheckForLatestAvailableVersion()
