013e7714b3558a5488bf0b2d0036c24c223929c2
Stefan Giehl <stefan@matomo.org>
2024-10-07 17:43:36 +0200

    Increase version

--- core/Version.php
+++ core/Version.php
@@ -22,7 +22,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    public const VERSION = '5.2.0-b3';
+    public const VERSION = '5.2.0-b4';
 
     public const MAJOR_VERSION = 5;
 
