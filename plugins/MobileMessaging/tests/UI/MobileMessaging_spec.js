55182ec5c0710e07aa07d7b44602af1a8e377c47
sgiehl <stefan@matomo.org>
2024-10-14 09:33:36 +0200

    adds some more UI tests

--- plugins/MobileMessaging/tests/UI/MobileMessaging_spec.js
+++ plugins/MobileMessaging/tests/UI/MobileMessaging_spec.js
@@ -10,6 +10,11 @@
 describe("MobileMessaging", function () {
   this.fixture = "Piwik\\Tests\\Fixtures\\EmptySite";
 
+  // required to ensure no provider is set initially
+  this.optionsOverride = {
+    'persist-fixture-data': false
+  };
+
   async function screenshotPageWrap() {
     const pageWrap = await page.$('.pageWrap');
     const screenshot = await pageWrap.screenshot();
@@ -32,5 +37,29 @@ describe("MobileMessaging", function () {
     await page.waitForTimeout(200);
 
     expect(await screenshotPageWrap()).to.matchImage('admin_provider');
+  });
+
+  it('should show phone number management when provider was selected', async function () {
+    await page.evaluate(function () {
+      $('[name=smsProviders]').val('string:StubbedProvider').trigger('change');
+    });
+
+    await page.waitForSelector('input#apiKey', {visible: true});
+    await page.type('input#apiKey', '0123456789');
+    await page.evaluate(() => $('#apiAccountSubmit input').click());
+
+    await page.waitForNetworkIdle();
+
+    expect(await screenshotPageWrap()).to.matchImage('admin_numbers_initial');
+  });
+
+  it('should add a phone number for validation', async function () {
+    await page.type('input#countryCallingCode', '44');
+    await page.type('input#newPhoneNumber', '112233445566');
+    await page.click('.addNumber input');
+
+    await page.waitForNetworkIdle();
+
+    expect(await screenshotPageWrap()).to.matchImage('admin_numbers_added');
   });
 });
