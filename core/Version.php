49a0415a41fd14c9e0cb50689eed344cf4adcb40
caddoo <matthew.caddoo@gmail.com>
2025-02-11 12:31:03 +1300

    Prepare preview release

--- core/Version.php
+++ core/Version.php
@@ -22,7 +22,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    public const VERSION = '5.3.0-b1.202502101842';
+    public const VERSION = '5.3.0-b1.20250210232950';
 
     public const MAJOR_VERSION = 5;
 
