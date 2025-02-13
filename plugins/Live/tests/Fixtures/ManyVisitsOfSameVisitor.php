8b8bfe352cc1924db80ab0c1b034642f67aeb520
Nathan Gavin <nathangavin987@gmail.com>
2025-02-14 12:25:37 +1300

    Add system test for new API endpoint

--- plugins/Live/tests/Fixtures/ManyVisitsOfSameVisitor.php
+++ plugins/Live/tests/Fixtures/ManyVisitsOfSameVisitor.php
@@ -20,6 +20,8 @@ class ManyVisitsOfSameVisitor extends Fixture
     public $dateTime = '2010-02-01 11:22:33';
     public $idSite = 1;
     public $idSite2 = 2;
+    public $idSite3 = 3;
+    public $idSite4 = 4;
 
     public function setUp(): void
     {
@@ -30,6 +32,16 @@ class ManyVisitsOfSameVisitor extends Fixture
         if (!self::siteCreated($this->idSite2)) {
             self::createWebsite($this->dateTime);
         }
+        
+        if (!self::siteCreated($this->idSite3)) {
+            self::createWebsite($this->dateTime);
+            $this->setSiteVisitorLogsDisabled($this->idSite3);
+        }
+
+        if (!self::siteCreated($this->idSite4)) {
+            self::createWebsite($this->dateTime);
+            $this->setSiteVisitorProfilesDisabled($this->idSite4);
+        }
 
         $this->trackVisits();
     }
@@ -37,6 +49,20 @@ class ManyVisitsOfSameVisitor extends Fixture
     public function tearDown(): void
     {
         // empty
+    }
+
+    private function setSiteVisitorLogsDisabled($idSite)
+    {
+        $settings = new \Piwik\Plugins\Live\MeasurableSettings($idSite);
+        $settings->disableVisitorLog->setValue(true);
+        $settings->save();
+    }
+
+    private function setSiteVisitorProfilesDisabled($idSite)
+    {
+        $settings = new \Piwik\Plugins\Live\MeasurableSettings($idSite);
+        $settings->disableVisitorProfile->setValue(true);
+        $settings->save();
     }
 
     private function trackVisits()
