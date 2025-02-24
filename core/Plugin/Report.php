b3594f53cdd1bc085503061c0adbfb7f12e916aa
Altamash Shaikh <altu9594@gmail.com>
2025-02-24 14:53:39 +0530

    Applied PR feedback

--- core/Plugin/Report.php
+++ core/Plugin/Report.php
@@ -843,22 +843,23 @@ class Report
     public function getDimensions()
     {
         $dimensions = [];
+        $tableDimensionId = null;
 
         if (!empty($this->getDimension())) {
             $tableDimensionId = $this->getDimension()->getId();
             $dimensionId = str_replace('.', '_', $tableDimensionId);
             $dimensions[$dimensionId] = $this->getDimension()->getName();
-
-            for ($level = 1; $level <= 100; $level++) {
-                $subDimension = $this->getNthLevelTableDimension($level);
-                if (empty($subDimension) || $tableDimensionId === $subDimension->getId()) {
-                    break;
-                }
-                $tableDimensionId = $subDimension->getId();
-                $subDimensionId = str_replace('.', '_', $tableDimensionId);
-                $dimensions[$subDimensionId] = $subDimension->getName();
-            };
         }
+
+        for ($level = 1; $level <= 100; $level++) {
+            $subDimension = $this->getNthLevelTableDimension($level);
+            if (empty($subDimension) || $tableDimensionId === $subDimension->getId()) {
+                break;
+            }
+            $tableDimensionId = $subDimension->getId();
+            $subDimensionId = str_replace('.', '_', $tableDimensionId);
+            $dimensions[$subDimensionId] = $subDimension->getName();
+        };
 
         return $dimensions;
     }
