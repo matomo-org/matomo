205db32e962ef5c04d6b52af4f38b0344c2fa45b
caddoo <caddoo@users.noreply.github.com>
2025-03-02 01:32:17 +0000

    Update version to 5.3.0-rc2.20250302013216

--- core/Version.php
+++ core/Version.php
@@ -22,7 +22,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    public const VERSION = '5.3.0-rc1';
+    public const VERSION = '5.3.0-rc2.20250302013216';
 
     public const MAJOR_VERSION = 5;
 
