d1ef6e72848fce60055f18d84e890664f07b0d04
sgiehl <stefan@matomo.org>
2025-03-12 15:21:06 +0100

    Adjust metric documentation for ecommerce product reports

--- plugins/Ecommerce/Reports/BaseItem.php
+++ plugins/Ecommerce/Reports/BaseItem.php
@@ -48,9 +48,17 @@ abstract class BaseItem extends Base
 
     public function getMetricsDocumentation()
     {
-        // we do not check whether it is abandon carts if not set re performance improvements
-        if ($this->isAbandonedCart($fetchIfNotSet = false)) {
-            return array(
+        if ($this->isAbandonedCart(false)) {
+            return [
+                'revenue'         => Piwik::translate('Goals_ColumnRevenueLeftInCartDocumentation'),
+                'quantity'        => Piwik::translate('Goals_ColumnQuantityLeftInCartDocumentation', $this->name),
+                'avg_price'       => Piwik::translate('Goals_ColumnAveragePriceDocumentation', $this->name),
+                'avg_quantity'    => Piwik::translate('Goals_ColumnAverageQuantityDocumentation', $this->name),
+                'nb_visits'       => Piwik::translate('Goals_ColumnVisitsProductDocumentation', $this->name),
+                'abandoned_carts' => Piwik::translate('Goals_ColumnAbandonedCartsDocumentation', $this->name),
+            ];
+        } else {
+            return [
                 'revenue'         => Piwik::translate(
                     'Goals_ColumnRevenueDocumentation',
                     Piwik::translate('Goals_DocumentationRevenueGeneratedByProductSales')
@@ -61,10 +69,8 @@ abstract class BaseItem extends Base
                 'avg_quantity'    => Piwik::translate('Goals_ColumnAverageQuantityDocumentation', $this->name),
                 'nb_visits'       => Piwik::translate('Goals_ColumnVisitsProductDocumentation', $this->name),
                 'conversion_rate' => Piwik::translate('Goals_ColumnConversionRateProductDocumentation', $this->name),
-            );
+            ];
         }
-
-        return array();
     }
 
     public function configureWidgets(WidgetsList $widgetsList, ReportWidgetFactory $factory)
