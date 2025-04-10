e6d670fd62e82dc1c7acab0b92f2a2141fc51193
sgiehl <stefan@matomo.org>
2025-04-10 11:38:38 +0200

    improve datatable renderer tests

---
+++ tests/PHPUnit/Unit/DataTable/Renderer/JsonTest.php
@@ -0,0 +1,167 @@
+<?php
+
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Tests\Unit\DataTable\Renderer;
+
+use Piwik\DataTable\Renderer\Json;
+
+/**
+ * @group DataTableTest
+ */
+class JsonTest extends RendererTestCase
+{
+
+    /**
+     * @dataProvider getTestCases
+     */
+    public function testRender($table, string $expected, callable $rendererCallback = null)
+    {
+        $renderer = new Json();
+
+        if (is_callable($rendererCallback)) {
+            $rendererCallback($renderer);
+        }
+
+        $renderer->setTable($table);
+        $renderer->setRenderSubTables(true);
+        $rendered = $renderer->render();
+        $this->assertEquals($expected, $rendered);
+    }
+
+    public function getTestCases(): iterable
+    {
+        yield 'render normal datatable' => [
+            self::getDataTable(),
+            '[{"label":"Google\u00a9","bool":false,"goals":{"idgoal=1":{"revenue":5.5,"nb_conversions":10}},"nb_uniq_visitors":11,"nb_visits":11,"nb_actions":17,"max_actions":"5","sum_visit_length":517,"bounce_count":9,"url":"http:\/\/www.google.com\/display\"and,properly","logo":".\/plugins\/Morpheus\/icons\/dist\/searchEngines\/www.google.com.png"},{"label":"Yahoo!","nb_uniq_visitors":15,"bool":true,"nb_visits":151,"nb_actions":147,"max_actions":"50","sum_visit_length":517,"bounce_count":90,"url":"http:\/\/www.yahoo.com","logo":".\/plugins\/Morpheus\/icons\/dist\/searchEngines\/www.yahoo.com.png","idsubdatatable":2,"subtable":[{"label":"sub1","count":1,"bool":false},{"label":"sub2","count":2,"bool":true}]}]',
+        ];
+
+        yield 'render normal datatable without metadata' => [
+            self::getDataTable(),
+            '[{"label":"Google\u00a9","bool":false,"goals":{"idgoal=1":{"revenue":5.5,"nb_conversions":10}},"nb_uniq_visitors":11,"nb_visits":11,"nb_actions":17,"max_actions":"5","sum_visit_length":517,"bounce_count":9},{"label":"Yahoo!","nb_uniq_visitors":15,"bool":true,"nb_visits":151,"nb_actions":147,"max_actions":"50","sum_visit_length":517,"bounce_count":90,"subtable":[{"label":"sub1","count":1,"bool":false},{"label":"sub2","count":2,"bool":true}]}]',
+            function ($renderer) {
+                $renderer->setHideMetadataFromResponse(true);
+            },
+        ];
+
+        yield 'render simple datatable' => [
+            self::getDataTableSimple(),
+            '{"max_actions":14,"nb_uniq_visitors":57,"nb_visits":66,"nb_actions":151,"sum_visit_length":5118,"bounce_count":44}',
+        ];
+
+        yield 'render simple datatable with one row' => [
+            self::getDataTableSimpleOneRow(),
+            '{"value":14}',
+        ];
+
+        yield 'render simple datatable with one row having a zero value' => [
+            self::getDataTableSimpleOneZeroRow(),
+            '{"value":0}',
+        ];
+
+        yield 'render simple datatable with one row having a false value' => [
+            self::getDataTableSimpleOneFalseRow(),
+            '{"value":false}',
+        ];
+
+        yield 'render empty datatable' => [
+            self::getDataTableEmpty(),
+            '[]',
+        ];
+
+        yield 'render datatable with array in row metadata' => [
+            self::getDataTableHavingAnArrayInRowMetadata(),
+            // array in row metadata is rendered
+            '[{"label":"sub1","count":1},{"label":"sub2","count":2,"test":"render"},{"label":"sub3","count":2,"test":"renderMe","testArray":"ignore"},{"label":"sub4","count":6,"testArray":["do not render"]},{"label":"sub5","count":2,"testArray":"do ignore","mymeta":"should be rendered"},{"label":"sub6","count":3,"mymeta":"renderrrrrr"}]',
+        ];
+
+        yield 'render datatable map' => [
+            self::getDataTableMap(),
+            '{"date1":[{"label":"Google","nb_uniq_visitors":11,"nb_visits":11,"url":"http:\/\/www.google.com","logo":".\/plugins\/Morpheus\/icons\/dist\/searchEngines\/www.google.com.png"},{"label":"Yahoo!","nb_uniq_visitors":15,"nb_visits":151,"url":"http:\/\/www.yahoo.com","logo":".\/plugins\/Morpheus\/icons\/dist\/searchEngines\/www.yahoo.com.png"}],"date2":[{"label":"Google1\u00a9","nb_uniq_visitors":110,"nb_visits":110,"url":"http:\/\/www.google.com1","logo":".\/plugins\/Morpheus\/icons\/dist\/searchEngines\/www.google.com.png1"},{"label":"Yahoo!1","nb_uniq_visitors":150,"nb_visits":1510,"url":"http:\/\/www.yahoo.com1","logo":".\/plugins\/Morpheus\/icons\/dist\/searchEngines\/www.yahoo.com.png1"}],"date3":[]}',
+        ];
+
+        yield 'render datatable map without metadata' => [
+            self::getDataTableMap(),
+            '{"date1":[{"label":"Google","nb_uniq_visitors":11,"nb_visits":11},{"label":"Yahoo!","nb_uniq_visitors":15,"nb_visits":151}],"date2":[{"label":"Google1\u00a9","nb_uniq_visitors":110,"nb_visits":110},{"label":"Yahoo!1","nb_uniq_visitors":150,"nb_visits":1510}],"date3":[]}',
+            function ($renderer) {
+                $renderer->setHideMetadataFromResponse(true);
+            },
+        ];
+
+        yield 'render simple datatable map' => [
+            self::getDataTableSimpleMap(),
+            '{"row1":{"max_actions":14,"nb_uniq_visitors":57},"row2":{"max_actions":140,"nb_uniq_visitors":570},"row3":[]}',
+        ];
+
+        yield 'render datatable map holding simple tables with one row only' => [
+            self::getDataTableSimpleOneRowMap(),
+            '{"row1":14,"row2":15,"row3":[]}',
+        ];
+
+        yield 'render map of datatable maps with normal datatables' => [
+            self::getDataTableMapContainsDataTableMapNormal(),
+            '{"idSite":{"date1":[{"label":"Google","nb_uniq_visitors":11,"nb_visits":11,"url":"http:\/\/www.google.com","logo":".\/plugins\/Morpheus\/icons\/dist\/searchEngines\/www.google.com.png"},{"label":"Yahoo!","nb_uniq_visitors":15,"nb_visits":151,"url":"http:\/\/www.yahoo.com","logo":".\/plugins\/Morpheus\/icons\/dist\/searchEngines\/www.yahoo.com.png"}],"date2":[{"label":"Google1\u00a9","nb_uniq_visitors":110,"nb_visits":110,"url":"http:\/\/www.google.com1","logo":".\/plugins\/Morpheus\/icons\/dist\/searchEngines\/www.google.com.png1"},{"label":"Yahoo!1","nb_uniq_visitors":150,"nb_visits":1510,"url":"http:\/\/www.yahoo.com1","logo":".\/plugins\/Morpheus\/icons\/dist\/searchEngines\/www.yahoo.com.png1"}],"date3":[]}}',
+        ];
+
+        yield 'render map of datatable maps with normal datatables without metadata' => [
+            self::getDataTableMapContainsDataTableMapNormal(),
+            '{"idSite":{"date1":[{"label":"Google","nb_uniq_visitors":11,"nb_visits":11},{"label":"Yahoo!","nb_uniq_visitors":15,"nb_visits":151}],"date2":[{"label":"Google1\u00a9","nb_uniq_visitors":110,"nb_visits":110},{"label":"Yahoo!1","nb_uniq_visitors":150,"nb_visits":1510}],"date3":[]}}',
+            function ($renderer) {
+                $renderer->setHideMetadataFromResponse(true);
+            },
+        ];
+
+        yield 'render map of datatable maps with simple datatables' => [
+            self::getDataTableMapContainsDataTableMapSimple(),
+            '{"idSite":{"row1":{"max_actions":14,"nb_uniq_visitors":57},"row2":{"max_actions":140,"nb_uniq_visitors":570},"row3":[]}}',
+        ];
+
+        yield 'render map of datatable maps with datatables having one row only' => [
+            self::getDataTableMapContainsDataTableMapSimpleOneRow(),
+            '{"idSite":{"row1":14,"row2":15,"row3":[]}}',
+        ];
+
+        yield 'render empty array' => [
+            [],
+            '[]',
+        ];
+
+        yield 'render value array' => [
+            ['a', 'b', 'c'],
+            '["a","b","c"]',
+        ];
+
+        yield 'render key / value array' => [
+            ['a' => 'b', 'c' => 'd', 'e' => 'f', 5 => 'g'],
+            '[{"a":"b","c":"d","e":"f","5":"g"}]',
+        ];
+
+        yield 'render multi dimensional key / value array' => [
+            ['a' => 'b', 'c' => [1, 2, 3, 4], 'e' => ['f' => 'g', 'h' => 'i', 'j' => 'k']],
+            '{"a":"b","c":[1,2,3,4],"e":{"f":"g","h":"i","j":"k"}}',
+        ];
+
+        yield 'render key / value array with one element' => [
+            ['a' => 'b'],
+            '[{"a":"b"}]',
+        ];
+
+        yield 'handles comparison metadata correctly' => [
+            self::getComparisonDataTable(),
+            '[{"nb_visits":5,"nb_random":10,"comparisons":[{"nb_visits":6,"nb_random":7},{"nb_visits":8,"nb_random":9}]}]',
+        ];
+
+        yield 'handles comparison metadata correctly when metadata hidden' => [
+            self::getComparisonDataTable(),
+            '[{"nb_visits":5,"nb_random":10}]',
+            function ($renderer) {
+                $renderer->setHideMetadataFromResponse(true);
+            },
+        ];
+    }
+}
---
+++ tests/PHPUnit/Unit/DataTable/Renderer/TsvTest.php
@@ -0,0 +1,221 @@
+<?php
+
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace DataTable\Renderer;
+
+use Piwik\DataTable;
+use Piwik\DataTable\Renderer\Tsv;
+use Piwik\Tests\Unit\DataTable\Renderer\RendererTestCase;
+
+/**
+ * @group DataTableTest
+ */
+class TsvTest extends RendererTestCase
+{
+    /**
+     * @dataProvider getTestCases
+     */
+    public function testRender($table, string $expected, callable $rendererCallback = null)
+    {
+        $renderer = new Tsv();
+
+        if (is_callable($rendererCallback)) {
+            $rendererCallback($renderer);
+        }
+
+        $renderer->setTable($table);
+        $renderer->convertToUnicode = false;
+        $rendered = $renderer->render();
+        $this->assertEquals($expected, $rendered);
+    }
+
+    public function getTestCases(): iterable
+    {
+        yield 'render normal datatable' => [
+            self::getDataTable(),
+            "label\tbool\tgoals_idgoal=1_revenue\tgoals_idgoal=1_nb_conversions\tnb_uniq_visitors\tnb_visits\tnb_actions\tmax_actions\tsum_visit_length\tbounce_count\tmetadata_url\tmetadata_logo\n" .
+            "Google©\t0\t5.5\t10\t11\t11\t17\t5\t517\t9\t\"http://www.google.com/display\"\"and,properly\"\t./plugins/Morpheus/icons/dist/searchEngines/www.google.com.png\n" .
+            "Yahoo!\t1\t\t\t15\t151\t147\t50\t517\t90\thttp://www.yahoo.com\t./plugins/Morpheus/icons/dist/searchEngines/www.yahoo.com.png",
+        ];
+
+        yield 'render normal datatable without metadata' => [
+            self::getDataTable(),
+            "label\tbool\tgoals_idgoal=1_revenue\tgoals_idgoal=1_nb_conversions\tnb_uniq_visitors\tnb_visits\tnb_actions\tmax_actions\tsum_visit_length\tbounce_count\n" .
+            "Google©\t0\t5.5\t10\t11\t11\t17\t5\t517\t9\n" .
+            "Yahoo!\t1\t\t\t15\t151\t147\t50\t517\t90",
+            function ($renderer) {
+                $renderer->setHideMetadataFromResponse(true);
+            },
+        ];
+
+        yield 'render simple datatable' => [
+            self::getDataTableSimple(),
+            "max_actions\tnb_uniq_visitors\tnb_visits\tnb_actions\tsum_visit_length\tbounce_count\n14\t57\t66\t151\t5118\t44",
+        ];
+
+        yield 'render simple datatable with one row' => [
+            self::getDataTableSimpleOneRow(),
+            "value\n14",
+        ];
+
+        yield 'render simple datatable with one row having a zero value' => [
+            self::getDataTableSimpleOneZeroRow(),
+            "value\n0",
+        ];
+
+        yield 'render simple datatable with one row having a false value' => [
+            self::getDataTableSimpleOneFalseRow(),
+            "value\n0",
+        ];
+
+        yield 'render empty datatable' => [
+            self::getDataTableEmpty(),
+            'No data available',
+        ];
+
+        yield 'render datatable with array in row metadata' => [
+            self::getDataTableHavingAnArrayInRowMetadata(),
+            // array in row metadata is not rendered
+            "label\tcount\tmetadata_test\tmetadata_mymeta
+sub1\t1\t\t
+sub2\t2\trender\t
+sub3\t2\trenderMe\t
+sub4\t6\t\t
+sub5\t2\t\tshould be rendered
+sub6\t3\t\trenderrrrrr",
+        ];
+
+        yield 'render datatable map' => [
+            self::getDataTableMap(),
+            "testKey\tlabel\tnb_uniq_visitors\tnb_visits\tmetadata_url\tmetadata_logo\n" .
+            "date1\tGoogle\t11\t11\thttp://www.google.com\t./plugins/Morpheus/icons/dist/searchEngines/www.google.com.png\n" .
+            "date1\tYahoo!\t15\t151\thttp://www.yahoo.com\t./plugins/Morpheus/icons/dist/searchEngines/www.yahoo.com.png\n" .
+            "date2\tGoogle1©\t110\t110\thttp://www.google.com1\t./plugins/Morpheus/icons/dist/searchEngines/www.google.com.png1\n" .
+            "date2\tYahoo!1\t150\t1510\thttp://www.yahoo.com1\t./plugins/Morpheus/icons/dist/searchEngines/www.yahoo.com.png1",
+        ];
+
+        yield 'render datatable map without metadata' => [
+            self::getDataTableMap(),
+            "testKey\tlabel\tnb_uniq_visitors\tnb_visits\n" .
+            "date1\tGoogle\t11\t11\n" .
+            "date1\tYahoo!\t15\t151\n" .
+            "date2\tGoogle1©\t110\t110\n" .
+            "date2\tYahoo!1\t150\t1510",
+            function ($renderer) {
+                $renderer->setHideMetadataFromResponse(true);
+            },
+        ];
+
+        yield 'render simple datatable map' => [
+            self::getDataTableSimpleMap(),
+            "testKey\tmax_actions\tnb_uniq_visitors\nrow1\t14\t57\nrow2\t140\t570",
+        ];
+
+        yield 'render datatable map holding simple tables with one row only' => [
+            self::getDataTableSimpleOneRowMap(),
+            "testKey\tvalue\nrow1\t14\nrow2\t15",
+        ];
+
+        yield 'render map of datatable maps with normal datatables' => [
+            self::getDataTableMapContainsDataTableMapNormal(),
+            "parentArrayKey\ttestKey\tlabel\tnb_uniq_visitors\tnb_visits\tmetadata_url\tmetadata_logo\n" .
+            "idSite\tdate1\tGoogle\t11\t11\thttp://www.google.com\t./plugins/Morpheus/icons/dist/searchEngines/www.google.com.png\n" .
+            "idSite\tdate1\tYahoo!\t15\t151\thttp://www.yahoo.com\t./plugins/Morpheus/icons/dist/searchEngines/www.yahoo.com.png\n" .
+            "idSite\tdate2\tGoogle1©\t110\t110\thttp://www.google.com1\t./plugins/Morpheus/icons/dist/searchEngines/www.google.com.png1\n" .
+            "idSite\tdate2\tYahoo!1\t150\t1510\thttp://www.yahoo.com1\t./plugins/Morpheus/icons/dist/searchEngines/www.yahoo.com.png1",
+        ];
+
+        yield 'render map of datatable maps with normal datatables without metadata' => [
+            self::getDataTableMapContainsDataTableMapNormal(),
+            "parentArrayKey\ttestKey\tlabel\tnb_uniq_visitors\tnb_visits\n" .
+            "idSite\tdate1\tGoogle\t11\t11\n" .
+            "idSite\tdate1\tYahoo!\t15\t151\n" .
+            "idSite\tdate2\tGoogle1©\t110\t110\n" .
+            "idSite\tdate2\tYahoo!1\t150\t1510",
+            function ($renderer) {
+                $renderer->setHideMetadataFromResponse(true);
+            },
+        ];
+
+        yield 'render map of datatable maps with simple datatables' => [
+            self::getDataTableMapContainsDataTableMapSimple(),
+            "parentArrayKey\ttestKey\tmax_actions\tnb_uniq_visitors\nidSite\trow1\t14\t57\nidSite\trow2\t140\t570",
+        ];
+
+        yield 'render map of datatable maps with datatables having one row only' => [
+            self::getDataTableMapContainsDataTableMapSimpleOneRow(),
+            "parentArrayKey\ttestKey\tvalue\nidSite\trow1\t14\nidSite\trow2\t15",
+        ];
+
+        yield 'render empty array' => [
+            [],
+            'No data available',
+        ];
+
+        yield 'render value array' => [
+            ['a', 'b', 'c'],
+            'a
+b
+c',
+        ];
+
+        yield 'render key / value array' => [
+            ['a' => 'b', 'c' => 'd', 'e' => 'f', 5 => 'g'],
+            "a\tc\te\t5
+b\td\tf\tg",
+        ];
+
+        yield 'render key / value array with one element' => [
+            ['a' => 'b'],
+            "a\nb",
+        ];
+
+        yield "render formula starting with =, should be escaped with leading '" => [
+            ['=SUM(A)' => '=SUM(A;B)'],
+            "'=SUM(A)\n\"'=SUM(A;B)\"",
+        ];
+
+        yield "render formula starting with +, should be escaped with leading '" => [
+            ['+A1' => '+A2,B3'],
+            "'+A1\n\"'+A2,B3\"",
+        ];
+
+        yield "render formula starting with -, should be escaped with leading '" => [
+            ['-A1' => '-A2,B3'],
+            "'-A1\n\"'-A2,B3\"",
+        ];
+
+        yield "render formula with leading null byte, should still be escaped with leading '" => [
+            ["\0-A1" => '%00=SUM(A)'],
+            "'\0-A1\n'%00=SUM(A)",
+        ];
+
+        yield "render formula with leading null bytes, should still be escaped with leading '" => [
+            ["\0%00\0%00=@A1" => "%00\0%00%00=SUM(A)"],
+            "'\0%00\0%00=@A1\n'%00\0%00%00=SUM(A)",
+        ];
+
+        yield 'renders headers and values correctly escaped' => [
+            self::getDataTableSimpleWithCommasInCells(),
+            "\"col, 1\"\t\"col;2\"
+\"val\"\"1\"\t\"val\"\" 2\"
+val	val#2"
+        ];
+    }
+
+    protected static function getDataTableSimpleWithCommasInCells()
+    {
+        $table = new DataTable();
+        $table->addRowsFromSimpleArray([
+            ["col,\t1" => "val\"1", "col;2" => "val\"\t2"],
+            ["col,\t1" => "val", "col;2" => "val#2"],
+        ]);
+        return $table;
+    }
+}
