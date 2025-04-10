763c3cc842fca341cac587fd4d75eda92266daa3
sgiehl <stefan@matomo.org>
2025-04-10 15:38:07 +0200

    show metadata in tests

--- tests/PHPUnit/Framework/TestRequest/Collection.php
+++ tests/PHPUnit/Framework/TestRequest/Collection.php
@@ -130,6 +130,7 @@ class Collection
 
             'language'       => $this->testConfig->language ?: 'en',
             'idSites'        => $this->testConfig->idSite,
+            'showMetadata' => 1,
         );
         $parametersToSet = array_merge($parametersToSet, $this->testConfig->otherRequestParameters);
         if (!empty($this->testConfig->apiModule)) {
@@ -230,6 +231,7 @@ class Collection
                                                           'idSite'    => $parametersToSet['idSite'],
                                                           'period'    => $parametersToSet['period'],
                                                           'date'      => $parametersToSet['date'],
+                                                          'showMetadata' => '1',
                                                           'format'    => 'json',
                                                      ));
 
