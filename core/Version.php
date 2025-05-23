da07d61a0d18b7a79d5ba527ae03b763e8eeca8f
Matt <1169490+caddoo@users.noreply.github.com>
2025-05-23 15:02:07 +1200

    update preview version

--- core/Version.php
+++ core/Version.php
@@ -22,7 +22,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    public const VERSION = '5.4.0-b1';
+    public const VERSION = '5.4.0-b2.20250523045918';
 
     public const MAJOR_VERSION = 5;
 
