c3539aaa72459b341eb2dcd19a74fc8d7d5c2b05
Nathan Gavin <nathangavin987@gmail.com>
2025-06-19 14:47:07 +1200

    use more appropriate function for formatting error message

--- core/Auth/PasswordStrength.php
+++ core/Auth/PasswordStrength.php
@@ -93,7 +93,7 @@ class PasswordStrength
             return '';
         }
 
-        $concatenatedRules = implode(', ', array_map('strtolower', $brokenRules));
+        $concatenatedRules = implode(', ', array_map('lcfirst', $brokenRules));
 
         return Piwik::translate('General_PasswordStrengthValidationFailed', $concatenatedRules);
     }
