0be7ceb1354bcc3fc85587e2e508ee26ed01215d
Nathan Gavin <nathangavin987@gmail.com>
2025-06-24 12:31:38 +1200

    Test the added backend checks

---
+++ plugins/Login/tests/Integration/ControllerTest.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\Login\tests\Integration;
+
+use Piwik\Tests\Framework\TestCase\IntegrationTestCase;
+use Piwik\Plugins\Login\Controller;
+use Piwik\Nonce;
+use Piwik\Auth\PasswordStrength;
+use Piwik\Date;
+use Piwik\Plugins\UsersManager\Model;
+
+/**
+ * @group Login
+ * @group ControllerTest
+ * @group Plugins
+ */
+class ControllerTest extends IntegrationTestCase
+{
+    /**
+     * @var Controller
+     */
+    private $controller;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->controller = new Controller(
+            $passwordResetter = null,
+            $auth = null,
+            $sessionInitializer = null,
+            $passwordVerify = null,
+            $bruteForceDetection = null,
+            $systemSettings = null,
+            $passwordStrength = new PasswordStrength(true)
+        );
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    public function testResetPasswordStrengthCheck()
+    {
+        $_POST['form_nonce'] = Nonce::getNonce('Login.login');
+        $_POST['form_login'] = 'test';
+        $_POST['form_password'] = 'password';
+        $_POST['form_password_bis'] = 'password';
+
+        $response = $this->controller->resetPassword();
+        $this->assertStringContainsString('General_PasswordStrengthValidationFailed', $response);
+    }
+
+    public function testAcceptInvitationPasswordStrengthCheck()
+    {
+        // generate new user
+        $userLogin = 'test';
+        $userEmail = 'test@test.com';
+        $usersModel = new Model();
+        $usersModel->addUser($userLogin, $passwordHash = '', $userEmail, Date::now()->getDatetime());
+        $token = $usersModel->generateRandomInviteToken();
+        $usersModel->attachInviteToken($userLogin, $token, $expiryInDays = 1);
+
+        // simulate completing accept invitation form
+
+        $_POST['token'] = $token;
+        $_POST['password'] = 'password';
+        $_POST['passwordConfirmation'] = 'password';
+        $_POST['email'] = $userEmail;
+        $_POST['invitation_form'] = 'Confirm';
+        $_POST['conditionCheck'] = true;
+        $response = $this->controller->acceptInvitation();
+        $this->assertStringContainsString('General_PasswordStrengthValidationFailed', $response);
+    }
+}
---
+++ plugins/UsersManager/tests/Integration/ControllerTest.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\UsersManager\tests\Integration;
+
+use Piwik\Tests\Framework\TestCase\IntegrationTestCase;
+use Piwik\Plugins\UsersManager\Controller;
+use Piwik\Nonce;
+use Piwik\Auth\PasswordStrength;
+use Piwik\Plugins\UsersManager\Model;
+use Piwik\Plugins\Login\PasswordVerifier;
+use Piwik\Translation\Loader\DevelopmentLoader;
+use Piwik\Translation\Loader\JsonFileLoader;
+use Piwik\Translation\Translator;
+
+/**
+ * @group UsersManager
+ * @group ControllerTest
+ * @group Plugins
+ */
+class ControllerTest extends IntegrationTestCase
+{
+    /**
+     * @var Controller
+     */
+    private $controller;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->controller = new Controller(
+            $translator = new Translator(new DevelopmentLoader(new JsonFileLoader())),
+            $passwordVerify = new PasswordVerifier(),
+            $userModel = new Model(),
+            $passwordStrength = new PasswordStrength(true)
+        );
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    public function testRecordPasswordChangePasswordStrengthCheck()
+    {
+        $_POST['nonce'] = Nonce::getNonce('changePasswordNonce');
+        $_POST['password'] = 'password1';
+        $_POST['passwordBis'] = 'password1';
+        // original password (irrelevant for test)
+        $_POST['passwordConfirmation'] = '';
+        
+        try {
+            $this->controller->recordPasswordChange();
+        } catch (\Exception $e) {
+            $this->assertStringContainsString('General_PasswordStrengthValidationFailed', $e->getMessage());
+        }
+    }
+}
