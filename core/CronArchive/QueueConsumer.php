02b1a64fa162cb7c4e01e613479128f4bc2e08c5
sgiehl <stefan@matomo.org>
2025-06-27 11:07:49 +0200

    don'T skip processing an invalidation if there are no visits

--- core/CronArchive/QueueConsumer.php
+++ core/CronArchive/QueueConsumer.php
@@ -275,13 +275,6 @@ class QueueConsumer
                 continue;
             }
 
-            if ($this->canSkipArchiveBecauseNoPoint($invalidatedArchive)) {
-                $this->logger->debug("Found invalidated archive we can skip (no visits): $invalidationDesc");
-                $this->addInvalidationToExclude($invalidatedArchive);
-                $this->model->deleteInvalidations([$invalidatedArchive]);
-                continue;
-            }
-
             $reason = $this->shouldSkipArchiveBecauseLowerPeriodOrSegmentIsInProgress($invalidatedArchive);
             if ($reason !== null) {
                 $this->logger->debug("Skipping invalidated archive, $reason: $invalidationDesc");
@@ -400,26 +393,6 @@ class QueueConsumer
         }
 
         return false;
-    }
-
-    // public for tests
-    public function canSkipArchiveBecauseNoPoint(array $invalidatedArchive): bool
-    {
-        $site = new Site($invalidatedArchive['idsite']);
-
-        $periodLabel = $this->periodIdsToLabels[$invalidatedArchive['period']];
-        $dateStr = $periodLabel == 'range' ? ($invalidatedArchive['date1'] . ',' . $invalidatedArchive['date2']) : $invalidatedArchive['date1'];
-        $period = PeriodFactory::build($periodLabel, $dateStr);
-
-        $segment = new Segment($invalidatedArchive['segment'], [$invalidatedArchive['idsite']]);
-
-        $params = new Parameters($site, $period, $segment);
-        if (!empty($invalidatedArchive['plugin'])) {
-            $params->setRequestedPlugin($invalidatedArchive['plugin']);
-        }
-
-        $loader = new Loader($params);
-        return $loader->canSkipThisArchive(); // if no point in archiving, skip
     }
 
     public function shouldSkipArchiveBecauseLowerPeriodOrSegmentIsInProgress(array $archiveToProcess): ?string
