477bed2b0b3af8e430eb9bf037f4bb77e486c33e
Michal Kleiner <michal@innocraft.com>
2025-07-01 21:23:24 +1200

    Remove readonly filter from event emit to keep the object further updatable

--- plugins/UsersManager/vue/src/UserEditForm/UserEditForm.vue
+++ plugins/UsersManager/vue/src/UserEditForm/UserEditForm.vue
@@ -288,7 +288,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, readonly } from 'vue';
+import { defineComponent } from 'vue';
 import {
   ContentBlock,
   SiteRef,
@@ -484,7 +484,7 @@ export default defineComponent({
 
         this.resetPasswordVar();
         this.showUserCreatedNotification();
-        this.$emit('updated', { user: readonly(this.theUser) });
+        this.$emit('updated', { user: this.theUser });
       });
     },
     resetPasswordVar() {
@@ -546,7 +546,7 @@ export default defineComponent({
 
         this.resetPasswordVar();
         this.showUserSavedNotification();
-        this.$emit('updated', { user: readonly(this.theUser) });
+        this.$emit('updated', { user: this.theUser });
       }).catch(() => {
         this.isSavingUserInfo = false;
       });
