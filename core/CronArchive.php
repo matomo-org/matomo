6d7a3827aed86317c073aceb0773938634853e70
Matt <1169490+caddoo@users.noreply.github.com>
2025-07-11 16:04:29 +1200

    Add extra logging around when we can skip archiving specifically for cron

--- core/CronArchive.php
+++ core/CronArchive.php
@@ -1000,7 +1000,9 @@ class CronArchive
         $params = new Parameters(new Site($idSite), $period, new Segment('', [$idSite], $period->getDateStart(), $period->getDateEnd()));
 
         $loader = new Loader($params);
-        if ($loader->canSkipThisArchive()) {
+        $canSkip = $loader->canSkipThisArchiveWithReason();
+        if ($canSkip[0] === true) {
+            $this->logger->info('Can skip archiving for period because: '. $canSkip[1]);
             $this->logger->debug("  " . ucfirst($dateStr) . " archive can be skipped due to no visits for idSite = $idSite, skipping invalidation...");
             return;
         }
