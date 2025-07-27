7c1478948dcb6f2d8a9daa18b196e9bb3697ff4b
Michal Kleiner <michal@innocraft.com>
2025-07-28 11:15:10 +1200

    Update migration to use SQL queries

--- core/Updates/5.4.0-b4.php
+++ core/Updates/5.4.0-b4.php
@@ -9,8 +9,7 @@
 
 namespace Piwik\Updates;
 
-use Piwik\Option;
-use Piwik\Plugins\UsersManager\Model;
+use Piwik\Common;
 use Piwik\Updater;
 use Piwik\Updater\Migration;
 use Piwik\Updater\Migration\Factory as MigrationFactory;
@@ -26,9 +25,21 @@ class Updates_5_4_0_b4 extends PiwikUpdates
      */
     private $migration;
 
+    /**
+     * @var string
+     */
+    private $userTable;
+
+    /**
+     * @var string
+     */
+    private $optionTable;
+
     public function __construct(MigrationFactory $factory)
     {
         $this->migration = $factory;
+        $this->userTable = Common::prefixTable('user');
+        $this->optionTable = Common::prefixTable('option');
     }
 
     /**
@@ -39,26 +50,37 @@ class Updates_5_4_0_b4 extends PiwikUpdates
     public function getMigrations(Updater $updater)
     {
         return [
-          $this->migration->db->addColumns('user', ['ts_last_seen' => 'TIMESTAMP null DEFAULT null']),
-          $this->migration->db->addColumns('user', ['ts_inactivity_notified' => 'TIMESTAMP null DEFAULT null']),
+            $this->migration->db->addColumns('user', ['ts_last_seen' => 'TIMESTAMP null DEFAULT null']),
+            $this->migration->db->addColumns('user', ['ts_inactivity_notified' => 'TIMESTAMP null DEFAULT null']),
+            $this->migration->db->sql($this->migrateLastLoginFromOptionsTableSql()),
+            $this->migration->db->sql($this->removeLegacyValuesFromOptionsTableSql()),
         ];
     }
 
-    private function migrateLastLoginFromOptionsTable()
+    private function migrateLastLoginFromOptionsTableSql(): string
     {
-        $userModel = new Model();
-        $optionPrefix = 'UsersManager.lastSeen.';
-        foreach (Option::getLike($optionPrefix . '%') as $name => $value) {
-            $username = str_replace($optionPrefix, '', $name);
-            $userModel->setLastSeenTimestamp($username, $value);
-            Option::delete($name);
-        }
+        return sprintf(
+            "UPDATE `%s` u JOIN (
+                SELECT 
+                    SUBSTRING_INDEX(o.option_name, 'UsersManager.lastSeen.', -1) AS login,
+                    o.option_value AS last_seen
+                FROM `%s` o
+                WHERE o.option_name LIKE 'UsersManager.lastSeen.%%'
+            ) AS userData
+            ON u.login = userData.login
+            SET u.ts_last_seen = CONVERT_TZ(FROM_UNIXTIME(userData.last_seen), 'SYSTEM', '+00:00')",
+            $this->userTable,
+            $this->optionTable
+        );
+    }
+
+    private function removeLegacyValuesFromOptionsTableSql(): string
+    {
+        return sprintf("DELETE FROM `%s` WHERE option_name LIKE 'UsersManager.lastSeen.%%'", $this->optionTable);
     }
 
     public function doUpdate(Updater $updater)
     {
         $updater->executeMigrations(__FILE__, $this->getMigrations($updater));
-
-        $this->migrateLastLoginFromOptionsTable();
     }
 }
