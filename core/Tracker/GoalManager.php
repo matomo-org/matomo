79331eac335d94c430a825c627bae4a06bab3d00
sgiehl <stefan@matomo.org>
2025-07-29 16:07:16 +0200

    Log a warning if conversion fails to insert

--- core/Tracker/GoalManager.php
+++ core/Tracker/GoalManager.php
@@ -812,10 +812,13 @@ class GoalManager
         $wasInserted = $this->getModel()->createConversion($conversion);
         if (
             !$wasInserted
-            && !empty($idorder)
         ) {
-            $idSite = $request->getIdSite();
-            throw new InvalidRequestParameterException("Invalid non-unique idsite/idorder combination ($idSite, $idorder), conversion was not inserted.");
+            if (!empty($idorder)) {
+                $idSite = $request->getIdSite();
+                throw new InvalidRequestParameterException("Invalid non-unique idsite/idorder combination ($idSite, $idorder), conversion was not inserted.");
+            } elseif ($conversion['buster'] > 0) {
+                StaticContainer::get(LoggerInterface::class)->warning("Failed to insert goal due to duplicate idvisit/idgoal/buster combination ({$conversion['idvisit']}, {$conversion['idgoal']}, {$conversion['buster']})");
+            }
         }
 
         return $wasInserted;
