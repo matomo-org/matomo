1ce7426a48d8f8479e63dd4c1a9b46f071a84a94
Michal Kleiner <michal@innocraft.com>
2025-08-20 00:17:29 +1200

    Add index in a separate db call

--- plugins/Annotations/Model.php
+++ plugins/Annotations/Model.php
@@ -29,12 +29,16 @@ class Model
     {
         $annotation = "  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
                          `idsite` INTEGER UNSIGNED NOT NULL,
-					     `note` TEXT NOT NULL,
 					     `date` DATETIME NOT NULL,
+					     `note` TEXT NOT NULL,
 					     `starred` TINYINT(1) NOT NULL DEFAULT 0 ,
-					     PRIMARY KEY ( `id` )
-					     INDEX index_id_idsite_date (`id`, `idsite`, `date`)";
+					     PRIMARY KEY ( `id` )";
         DbHelper::createTable(self::$rawPrefix, $annotation);
+
+        Db::exec(sprintf(
+            'ALTER TABLE %s ADD INDEX index_id_idsite_date (`id`, `idsite`, `date`)',
+            Common::prefixTable(self::$rawPrefix)
+        ));
     }
 
     public static function uninstall()
