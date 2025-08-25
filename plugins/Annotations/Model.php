80e02fbe57cbc7f5710f6685a6dce1e2993d681a
Michal Kleiner <michal@innocraft.com>
2025-08-25 20:27:11 +1200

    Remove install and uninstall methods from model class

--- plugins/Annotations/Model.php
+++ plugins/Annotations/Model.php
@@ -11,39 +11,7 @@
 
 namespace Piwik\Plugins\Annotations;
 
-use Piwik\Common;
-use Piwik\Db;
-use Piwik\DbHelper;
-
 class Model
 {
-    private static $rawPrefix = 'annotations';
-
-    public static function install()
-    {
-        $table = Common::prefixTable(self::$rawPrefix);
-
-        if (!DbHelper::tableExists($table)) {
-            $annotation = "  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-                             `idsite` INTEGER UNSIGNED NOT NULL,
-                             `date` DATETIME NOT NULL,
-                             `note` TEXT NOT NULL,
-                             `starred` TINYINT(1) NOT NULL DEFAULT 0,
-                             `user` VARCHAR(100) NOT NULL,
-                             PRIMARY KEY ( `id` )";
-            DbHelper::createTable(self::$rawPrefix, $annotation);
-        }
-
-        if (!DbHelper::tableHasIndex($table, 'index_id_idsite_date')) {
-            Db::exec(sprintf(
-                'ALTER TABLE %s ADD INDEX index_id_idsite_date (`id`, `idsite`, `date`)',
-                $table
-            ));
-        }
-    }
-
-    public static function uninstall()
-    {
-        Db::dropTables(Common::prefixTable(self::$rawPrefix));
-    }
+//    private static $rawPrefix = 'annotations';
 }
