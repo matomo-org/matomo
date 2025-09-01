51286a2096c0714b889d45900f41f4d5e9692a26
Michal Kleiner <michal@innocraft.com>
2025-09-01 15:41:36 +0200

    Use custom migrations to migrate and purge annotations

--- core/Updates/5.5.0-b1.php
+++ core/Updates/5.5.0-b1.php
@@ -9,36 +9,18 @@
 
 namespace Piwik\Updates;
 
-use Piwik\Common;
-use Piwik\Option;
-use Piwik\Plugins\SitesManager\Model;
+use Piwik\Plugins\CoreAdminHome\Commands\MigrateAnnotations;
+use Piwik\Plugins\CoreAdminHome\Commands\PurgeLegacyAnnotations;
 use Piwik\Updater;
+use Piwik\Updater\Migration\Custom as CustomMigration;
 use Piwik\Updates as PiwikUpdates;
 use Piwik\Updater\Migration;
-use Piwik\Updater\Migration\Factory as MigrationFactory;
 
 /**
  * Update for version 5.5.0-b1
  */
 class Updates_5_5_0_b1 extends PiwikUpdates
 {
-    /**
-     * @var MigrationFactory
-     */
-    private $migration;
-
-    /**
-     * How many annotations in one insert per site id
-     *
-     * @var int
-     */
-    private $chunkSize = 50;
-
-    public function __construct(MigrationFactory $factory)
-    {
-        $this->migration = $factory;
-    }
-
     /**
      * Migrations
      *
@@ -50,77 +32,15 @@ class Updates_5_5_0_b1 extends PiwikUpdates
     {
         $migrations = [];
 
-        // create new annotations table
-        $migrations[] = $this->migration->db->createTable('annotations', [
-                'id' => 'BIGINT UNSIGNED NOT NULL AUTO_INCREMENT',
-                'idsite' => 'INTEGER UNSIGNED NOT NULL',
-                'date' => 'DATETIME NOT NULL',
-                'note' => 'TEXT NOT NULL',
-                'starred' => 'TINYINT(1) NOT NULL DEFAULT 0',
-                'user' => 'VARCHAR(100) NOT NULL',
-            ], $primaryKey = 'id');
-        $migrations[] = $this->migration->db->addIndex('annotations', ['idsite', 'date']);
+        $migrateCmd = './console core:matomo550-migrate-annotations';
+        $annotationsMigrate = new CustomMigration([MigrateAnnotations::class, 'migrate'], $migrateCmd);
+        $migrations[] = $annotationsMigrate;
 
-        // insert values from options table
-        foreach ($this->getAnnotationInsertsAndBindValues() as $migrationEntry) {
-            $migrations[] = $this->migration->db->boundSql($migrationEntry['sql'], $migrationEntry['bind']);
-        }
-
-        // TODO: Remove annotations from option table in Matomo 6
+        $purgeCmd = './console core:matomo550-purge-legacy-annotations';
+        $annotationsPurge = new CustomMigration([PurgeLegacyAnnotations::class, 'purge'], $purgeCmd);
+        $migrations[] = $annotationsPurge;
 
         return $migrations;
-    }
-
-    private function getAnnotationsForSite(int $idSite): array
-    {
-        $optionName = sprintf('%s_annotations', $idSite);
-        $serialized = Option::get($optionName);
-
-        if ($serialized !== false) {
-            $result = Common::safe_unserialize($serialized);
-            if (!empty($result)) {
-                return $result;
-            }
-        }
-
-        return [];
-    }
-
-    private function getAnnotationInsertsAndBindValues(): array
-    {
-        $table = Common::prefixTable('annotations');
-        $data = [];
-
-        $model = new Model();
-        foreach ($model->getSitesId() as $siteID) {
-            $annotations = $this->getAnnotationsForSite($siteID);
-            $chunks = array_chunk($annotations, $this->chunkSize);
-
-            foreach ($chunks as $chunk) {
-                $bindValues = [];
-                $placeholders = [];
-                foreach ($chunk as $annotation) {
-                    $bindValues[] = $values = [
-                        $siteID,
-                        $annotation['date'],
-                        $annotation['note'],
-                        $annotation['starred'],
-                        $annotation['user'],
-                    ];
-                    $placeholders[] = Common::getSqlStringFieldsArray($values);
-                }
-
-                // chunk always has aray items, so it's safe to assume we have some bind values and placeholders
-                $sql = sprintf(
-                    'INSERT INTO `%s` (`idsite`, `date`, `note`, `starred`, `user`) VALUES (%s)',
-                    $table,
-                    implode('), (', $placeholders)
-                );
-                $data[] = ['sql' => $sql, 'bind' => Common::flattenArray($bindValues)];
-            }
-        }
-
-        return $data;
     }
 
     public function doUpdate(Updater $updater)
---
+++ plugins/CoreAdminHome/Commands/PurgeLegacyAnnotations.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\CoreAdminHome\Commands;
+
+use Piwik\Common;
+use Piwik\Container\StaticContainer;
+use Piwik\Plugin\ConsoleCommand;
+use Piwik\Updater;
+use Piwik\Updater\Migration\Factory as MigrationFactory;
+
+/**
+ * Command to purge annotations from option table after migrating them to a separate db table.
+ */
+class PurgeLegacyAnnotations extends ConsoleCommand
+{
+    protected function configure()
+    {
+        $this->setName('core:matomo550-purge-legacy-annotations');
+        $this->setDescription('Purge legacy annotations from option table after migrating them to a separate annotations table. ');
+        $this->setDescription('Only needed for Matomo 5.5.0-b1 upgrade');
+    }
+
+    protected function doExecute(): int
+    {
+        self::purge();
+        $this->getOutput()->writeln('Done');
+
+        return self::SUCCESS;
+    }
+
+    public static function purge(): void
+    {
+        $migration = StaticContainer::get(MigrationFactory::class);
+        $migrations = [];
+
+        // remove legacy option table values
+        $migrations[] = $migration->db->sql(self::removeLegacyValuesFromOptionsTableSql());
+
+        $updater = StaticContainer::get(Updater::class);
+        $updater->executeMigrations(__FILE__, $migrations);
+    }
+
+    private static function removeLegacyValuesFromOptionsTableSql(): string
+    {
+        return sprintf("DELETE FROM `%s` WHERE `option_name` LIKE '%%_annotations'", Common::prefixTable('option'));
+    }
+}
