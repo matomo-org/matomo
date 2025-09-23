c6f15a0f53f4aa9b045fc3f9c9a3b3e1fb1edae5
Michal Kleiner <michal@innocraft.com>
2025-09-23 16:20:48 +0200

    Refactor token queries to users manager model similar to user notification queries

--- plugins/UsersManager/TokenNotifications/TokenRotationNotificationProvider.php
+++ plugins/UsersManager/TokenNotifications/TokenRotationNotificationProvider.php
@@ -24,23 +24,7 @@ class TokenRotationNotificationProvider extends TokenNotificationProvider
 
     protected function getTokensToNotify(string $periodThreshold): array
     {
-        $db = Db::get();
-        // Join on user table is done, to ensure we only fetch tokens, where the user still exists
-        $sql = "SELECT * FROM " . Common::prefixTable('user_token_auth') . " t"
-            . " JOIN  " . Common::prefixTable('user') . " u ON t.login = u.login"
-            . " WHERE (t.date_expired is null or t.date_expired > ?)"
-            . " AND (t.date_created <= ?)"
-            . " AND t.ts_rotation_notified is null"
-            . " AND t.system_token = 0"
-            . " AND t.login != ?";
-
-        $tokensToNotify = $db->fetchAll($sql, [
-            $this->today,
-            $periodThreshold,
-            'anonymous',
-        ]);
-
-        return $tokensToNotify;
+        return $this->userModel->getTokensRequiringRotation($periodThreshold);
     }
 
     protected function createNotification(array $token): TokenNotification
