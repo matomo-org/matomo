fd59bb37d2fda18d6d7c90275970824445df648a
Michal Kleiner <michal@innocraft.com>
2025-10-02 15:35:13 +1300

    Fix namespace

--- plugins/PrivacyManager/tests/Integration/PrivacyManagerConfigTest.php
+++ plugins/PrivacyManager/tests/Integration/PrivacyManagerConfigTest.php
@@ -7,7 +7,7 @@
  * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 
-namespace Piwik\Plugins\PrivacyManager\tests;
+namespace Piwik\Plugins\PrivacyManager\tests\Integration;
 
 use Piwik\Config;
 use Piwik\Container\StaticContainer;
