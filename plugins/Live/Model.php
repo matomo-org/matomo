4410b325961e74fe10083de71e191b81b9dde692
sgiehl <stefan@matomo.org>
2025-10-27 14:05:49 +0100

    more Model improvements

--- plugins/Live/Model.php
+++ plugins/Live/Model.php
@@ -536,7 +536,7 @@ class Model
             $visitLastActionTimeCondition = 'SLEEP(1)';
         }
 
-        $segment = new Segment($segment, $idSite, $dateOneDayAgo, $dateOneDayInFuture);
+        $segment = new Segment($segment, [$idSite], $dateOneDayAgo, $dateOneDayInFuture);
         $queryInfo = $segment->getSelectQuery($select, $from, $where, $whereBind, $orderBy, $groupBy);
 
         $sql = "SELECT /* Live.queryAdjacentVisitorId */ sub.idvisitor, sub.visit_last_action_time FROM ({$queryInfo['sql']}) as sub
@@ -709,8 +709,8 @@ class Model
     /**
      * @param string $whereClause
      * @param array $bindIdSites
-     * @param Date $startDate
-     * @param Date $endDate
+     * @param Date|null $startDate
+     * @param Date|null $endDate
      * @param $visitorId
      * @param $minTimestamp
      * @return array
@@ -718,7 +718,7 @@ class Model
      */
     private function getWhereClauseAndBind($whereClause, $bindIdSites, $startDate, $endDate, $visitorId, $minTimestamp)
     {
-        $where = array();
+        $where = [];
         if (!empty($whereClause)) {
             $where[] = $whereClause;
         }
