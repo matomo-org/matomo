78009608ec28bddc926795578df11b48b7da8856
chippison <chip.pison@innocraft.com>
2025-11-17 14:49:24 +1300

    adding new test for widgetloader that it redirects to new page if session is cleared

---
+++ plugins/CoreHome/tests/UI/WidgetLoader_spec.js
@@ -0,0 +1,37 @@
+/*!
+ * Matomo - free/libre analytics platform
+ *
+ * WidgetLoader screenshot tests.
+ *
+ * @link    https://matomo.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+describe('WidgetLoader', function () {
+  this.timeout(0);
+
+  this.fixture = "Piwik\\Tests\\Fixtures\\OneVisit";
+
+  before(function () {
+      testEnvironment.testUseMockAuth = 0;
+      testEnvironment.save();
+    });
+
+  it('should redirect to the landing page when the session cookie is cleared during widget loading', async function () {
+    // We try to do an actual login
+    await page.goto("");
+    await page.type("#login_form_login", superUserLogin);
+    await page.type("#login_form_password", superUserPassword);
+    await page.evaluate(function(){
+      $('#login_form_submit').click();
+    });
+    await page.waitForNetworkIdle();
+    await page.clearCookies();
+
+    await page.reload();
+    await page.waitForSelector('#login_form');
+
+    const screenshot = await page.screenshot();
+    expect(screenshot).to.matchImage('not_logged_in');
+  });
+});
