69b2eeb8daaebf562958827670e53e494483847e
innocraft-automation <innocraft-automation@users.noreply.github.com>
2025-11-19 02:32:55 +0000

    Build vue files

--- plugins/CoreHome/vue/dist/CoreHome.umd.js
+++ plugins/CoreHome/vue/dist/CoreHome.umd.js
@@ -3670,6 +3670,7 @@ class Comparisons_store_ComparisonsStore {
     });
   }
   parseSegmentComparisons() {
+    console.log('i will now try to parse segment comparisons:');
     const {
       availableSegments
     } = Segments_store.state;
