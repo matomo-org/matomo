04288395803dd7ec1936a3ae8a0be922bfb54c25
chippison <chip.pison@innocraft.com>
2025-11-21 21:18:46 +1300

    finished test using only DOM checks

--- plugins/SegmentEditor/tests/UI/SegmentSelectorEditor_spec.js
+++ plugins/SegmentEditor/tests/UI/SegmentSelectorEditor_spec.js
@@ -355,41 +355,50 @@ describe("SegmentSelectorEditorTest", function () {
         expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('enabled_create_realtime_segments_saved');
     });
 
-    it('should have 6 segments in the list at this point', async function() {
+    it('should not allow comparing segments more than the limit set', async function() {
       const maxSegments = 3;
       testEnvironment.overrideConfig('General', 'data_comparison_segment_limit', maxSegments);
       testEnvironment.save();
       await page.goto(url);
       await page.waitForNetworkIdle();
-      const message = await page.evaluate(
+      // We grab the max limit message
+      const maxLimitMessage = await page.evaluate(
         (limit) => _pk_translate('General_MaximumNumberOfSegmentsComparedIs', [limit]),
       maxSegments);
-      console.log('max reached title: ' + message);
+
+      // We check that the title attribute is still not the max limit message
+      let title = await page.$eval('.segmentationContainer .segmentList li:last-child', (el) => el.getAttribute('title'));
+      expect(title).to.not.equal(maxLimitMessage);
+
       await page.waitForSelector('.segmentationContainer');
-      const lisLen = await page.$$eval('.segmentListContainer .segmentList li', (e) => e.length);
-      console.log('my list length is: ' + lisLen);
+      // We check that the number of <li> elements is greater than the limit we set
+      const liElemLength = await page.$$eval('.segmentListContainer .segmentList li', (e) => e.length);
+      expect(liElemLength).to.be.greaterThan(maxSegments);
+
+      // We check that the number of segments compared is 1 at the start
       let comparedCount = await page.$$eval(
         '.segmentListContainer .segmentList li.comparedSegment',
         (nodes) => nodes.length,
       );
-      console.log('my current compare count is: ' + comparedCount);
+      expect(comparedCount).to.equal(1);
 
-      for (let i=0; i<lisLen; i++) {
+      // We want to click all the segments so that we can check that it stops at the limit
+      for (let i=0; i<liElemLength; i++) {
         await page.click('.segmentationContainer .title');
         const segments = await page.$$('.segmentListContainer .segmentList li span.compareSegment');
         await segments[i].click();
         await page.waitForTimeout(50);
       }
+
+      // We check that the number of segments compared is now equal to the limit we set
       comparedCount = await page.$$eval(
         '.segmentListContainer .segmentList li.comparedSegment',
         (nodes) => nodes.length,
       );
-      console.log('my current compare count is: ' + comparedCount);
-      await page.waitForSelector('.segmentationContainer');
-      await page.click('.segmentationContainer .title');
+      expect(comparedCount).to.equal(maxSegments);
 
-      const title = await page.getAttribute('.segmentListContainer .segmentList li:last-child', 'title');
-      console.log('the title is ', title);
-      expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('akon_tilaw');
+      // Last we check that the title attribute has changed to reflect the limit has been reached
+      title = await page.$eval('.segmentationContainer .segmentList li:last-child', (el) => el.getAttribute('title'));
+      expect(title).to.equal(maxLimitMessage);
     });
 });
