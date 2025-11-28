2f6d39d01447d3c253b490cf627f5f8e4bc15e60
Matt <1169490+caddoo@users.noreply.github.com>
2025-11-28 15:05:08 +1300

    Add new order ID randomization setting

--- plugins/PrivacyManager/Config.php
+++ plugins/PrivacyManager/Config.php
@@ -19,6 +19,7 @@ use Piwik\Plugins\PrivacyManager\Settings\ReferrerAnonymisation as ReferrerAnony
 use Piwik\Tracker\Cache;
 use Piwik\Plugins\PrivacyManager\Settings\IpAddressMaskLength as IpAddressMaskLengthSetting;
 use Piwik\Plugins\PrivacyManager\Settings\IPAnonymisation as IPAnonymisationSetting;
+use Piwik\Plugins\PrivacyManager\Settings\OrderIdAnonymization as OrderIdAnonymizationSetting;
 
 /**
  * @property bool $doNotTrackEnabled    Enable / Disable Do Not Track {@see DoNotTrackHeaderChecker}
@@ -138,6 +139,8 @@ class Config
                 return IPAnonymisationSetting::getInstance($idSite)->getValue();
             } elseif ($name === 'anonymizeReferrer') {
                 return ReferrerAnonymizationSettings::getInstance($idSite)->getValue();
+            } elseif ($name === 'anonymizeOrderId') {
+                return OrderIdAnonymizationSetting::getInstance($idSite)->getValue();
             }
         }
 
---
+++ plugins/PrivacyManager/Settings/OrderIdAnonymization.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Piwik\Plugins\PrivacyManager\Settings;
+
+use Piwik\Piwik;
+use Piwik\Plugins\PrivacyManager\Config;
+use Piwik\Policy\CnilPolicy;
+use Piwik\Settings\Interfaces\CustomSettingInterface;
+use Piwik\Settings\Interfaces\PolicyComparisonInterface;
+use Piwik\Settings\Interfaces\SettingValueInterface;
+use Piwik\Settings\Interfaces\Traits\Getters\CustomGetterTrait;
+use Piwik\Settings\Interfaces\Traits\PolicyComparisonTrait;
+
+/**
+ * @implements CustomSettingInterface<bool>
+ * @implements PolicyComparisonInterface<bool>
+ * @implements SettingValueInterface<bool>
+ */
+class OrderIdAnonymization implements
+    CustomSettingInterface,
+    PolicyComparisonInterface,
+    SettingValueInterface
+{
+    /** @use PolicyComparisonTrait<bool> */
+    use PolicyComparisonTrait;
+
+    /** @use CustomGetterTrait<bool> */
+    use CustomGetterTrait;
+
+    /** @var bool */
+    private $value;
+
+    private function __construct(bool $value)
+    {
+        $this->value = $value;
+    }
+
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    protected static function getCustomSettingName(): string
+    {
+        return 'anonymizeOrderId';
+    }
+
+    public static function getCustomValue(?int $idSite = null)
+    {
+        return (new Config($idSite))->getFromOption(self::getCustomSettingName(), $allowPolicyComplianceOverride = false);
+    }
+
+    public static function getTitle(): string
+    {
+        return Piwik::translate('PrivacyManager_OrderIdAnonymizationSettingTitle');
+    }
+
+    public static function getComplianceRequirementNote(?int $idSite = null): string
+    {
+        return Piwik::translate('PrivacyManager_OrderIdAnonymizationSettingRequirementNote');
+    }
+
+    public static function getInlineHelp(): string
+    {
+        return '';
+    }
+
+    public static function getPolicyRequirements(): array
+    {
+        return [
+            CnilPolicy::class => true,
+        ];
+    }
+
+    public static function getInstance(?int $idSite = null): self
+    {
+        $values = self::getPolicyRequiredValues($idSite);
+        $values['system'] = (bool) self::getCustomValue();
+        $values['measurable'] = is_null($idSite) ? null : (bool) self::getCustomValue($idSite);
+
+        $strictest = (bool) self::getStrictestValueFromArray($values);
+        return new self($strictest);
+    }
+
+    public static function isCompliant(string $policy, ?int $idSite = null): bool
+    {
+        $policyValues = self::getPolicyRequirements();
+
+        if (!array_key_exists($policy, $policyValues)) {
+            return true;
+        }
+
+        $currentValue = self::getInstance($idSite)->getValue();
+
+        return $currentValue === $policyValues[$policy];
+    }
+
+    protected static function compareStrictness($value1, $value2)
+    {
+        return ($value1 || $value2);
+    }
+}
