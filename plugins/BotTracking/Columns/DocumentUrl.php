4fc9a326263d12a70c2e7cadddda317256862037
Stefan Giehl <stefan@matomo.org>
2025-12-04 14:24:31 +0100

    Adds detail report around requests made by AI bots (#23793)
    * Adds archiving for new report(s)
    
    * Add API methods for new report
    
    * Add report classes
    
    * Move AI Agent reports to bottom
    
    * Add system tests for new report
    
    * Adds UI tests for new report
    
    * updates expected UI test files
    
    * fix other tests
    
    * do not double count acquired visits
    
    * remove explicit plugin loading from ui tests
    
    * updates expected UI test file

---
+++ plugins/BotTracking/Columns/DocumentUrl.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+declare(strict_types=1);
+
+namespace Piwik\Plugins\BotTracking\Columns;
+
+use Piwik\Columns\Dimension;
+
+class DocumentUrl extends Dimension
+{
+    protected $nameSingular = 'BotTracking_DocumentUrl';
+    protected $type = self::TYPE_URL;
+}
