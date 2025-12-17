9b40cd7276a6350def99c055905fe0b2995eab6b
chippison <chip.pison@innocraft.com>
2025-12-17 15:32:50 +1300

    refactoring datatable test so that it does not repeat common code blocks when testing normal and anonymous users

--- plugins/CoreHome/tests/UI/DataTable_spec.js
+++ plugins/CoreHome/tests/UI/DataTable_spec.js
@@ -10,6 +10,17 @@
 describe('DataTable', function () {
   const devicesUrl = "?module=CoreHome&action=index&idSite=3&period=day&date=yesterday&category=General_Visitors&subcategory=DevicesDetection_Devices";
   const widgetSelector = '#widgetDevicesDetectiongetType';
+  const selectors = {
+    metricColumn: `${widgetSelector} #nb_uniq_visitors`,
+    limitDropdownTrigger: `${widgetSelector} .limitSelection .select-wrapper input.select-dropdown`,
+    limitDropdownMenu: `${widgetSelector} .limitSelection .select-wrapper ul.select-dropdown`,
+    visualizationTrigger: `${widgetSelector} .dataTableHeaderControls .dataTableControls a.activateVisualizationSelection`,
+    visualizationTriggerIcon: `${widgetSelector} .dataTableHeaderControls .dataTableControls a.activateVisualizationSelection > span, ${widgetSelector} .dataTableHeaderControls .dataTableControls a.activateVisualizationSelection > img`,
+    visualizationMenu: `${widgetSelector} .dataTableHeaderControls .dataTableControls ul.dropdown-content.dataTableFooterIcons`,
+    visualizationButtons: `${widgetSelector} .dataTableHeaderControls .dataTableControls ul.dropdown-content.dataTableFooterIcons li .tableIcon[data-footer-icon-id]`,
+    configureTrigger: `${widgetSelector} .dataTableHeaderControls .dataTableControls a.dropdownConfigureIcon`,
+    totalsRowToggle: '.dataTableShowTotalsRow',
+  };
 
   async function openDevicesDetectionWidget() {
     await page.goto(devicesUrl);
@@ -18,134 +29,152 @@ describe('DataTable', function () {
     await page.waitForNetworkIdle();
   }
 
-  before(async function () {
+  async function loadWidget() {
+    await openDevicesDetectionWidget();
+    const widget = await page.$(widgetSelector);
+    expect(widget).to.be.ok;
+    return widget;
+  }
 
-  });
+  async function trackViewDataTableRequests(action) {
+    let ajaxRequestCount = 0;
+    const requestHandler = (request) => {
+      if (request.resourceType() === 'xhr' && request.url().indexOf('saveViewDataTableParameters') !== -1) {
+        ajaxRequestCount += 1;
+      }
+    };
+    page.webpage.on('request', requestHandler);
+    try {
+      await action();
+    } finally {
+      page.webpage.removeListener('request', requestHandler);
+    }
+    return ajaxRequestCount;
+  }
+
+  async function interactWithColumnSortingAndLimit() {
+    await page.waitForSelector(selectors.metricColumn, { visible: true });
+    await page.click(selectors.metricColumn);
+    await page.waitForNetworkIdle();
+    await page.click(selectors.metricColumn);
+    await page.waitForNetworkIdle();
+
+    await page.waitForSelector(selectors.limitDropdownTrigger, { visible: true });
+    await page.click(selectors.limitDropdownTrigger);
+
+    await page.waitForSelector(selectors.limitDropdownMenu, { visible: true });
+    const dropdown = await page.$(selectors.limitDropdownMenu);
+    expect(dropdown).to.be.ok;
+    await page.click(`${selectors.limitDropdownMenu} li:nth-child(1)`);
+    await page.waitForNetworkIdle();
+  }
+
+  async function changeVisualization() {
+    await page.waitForSelector(selectors.visualizationTriggerIcon, { visible: true });
+    await page.click(selectors.visualizationTriggerIcon);
+    await page.waitForSelector(selectors.visualizationMenu, { visible: true });
+
+    await page.waitForFunction((selector) => document.querySelectorAll(selector).length >= 2, {}, selectors.visualizationButtons);
+    await page.$$eval(selectors.visualizationButtons, (buttons) => {
+      if (buttons.length < 2) {
+        throw new Error('Not enough visualization buttons found');
+      }
+      buttons[1].click();
+    });
+    await page.waitForNetworkIdle();
+  }
+
+  async function toggleTotalsRow(times = 1) {
+    await page.waitForSelector(selectors.configureTrigger, { visible: true });
+    for (let i = 0; i < times; i += 1) {
+      await page.click(selectors.configureTrigger);
+      await page.waitForSelector(selectors.totalsRowToggle, { visible: true });
+      await page.click(selectors.totalsRowToggle);
+      await page.waitForNetworkIdle();
+    }
+  }
 
   after(async function () {
     testEnvironment.testUseMockAuth = 1;
     testEnvironment.save();
   });
 
-  it('should trigger an ajax call to save preference for normal user when doing some datatable actions', async function () {
-    await openDevicesDetectionWidget();
-
-    const table = await page.$(widgetSelector);
-    expect(table).to.be.ok;
-
+  it('should allow saving of preference for normal user when changing sorting and table limits', async function () {
+    await loadWidget();
     const reportPage = await page.$('.reporting-page');
     expect(reportPage).to.be.ok;
 
-    let ajaxRequestCount = 0;
-    const requestHandler = (request) => {
-      if (request.resourceType() === 'xhr' && request.url().indexOf('saveViewDataTableParameters') !== -1) {
-        console.log('i am inside the request handler');
-        console.log(request.url());
-        ajaxRequestCount += 1;
-      }
-    };
-    console.log('adding listener');
-    page.webpage.on('request', requestHandler);
-
-    await page.waitForSelector(`${widgetSelector} #nb_uniq_visitors`, { visible: true });
-    await page.click(`${widgetSelector} #nb_uniq_visitors`);
-    await page.waitForNetworkIdle();
-    await page.click(`${widgetSelector} #nb_uniq_visitors`);
-    await page.waitForNetworkIdle();
-
-    const limitDropdownTrigger = `${widgetSelector} .limitSelection .select-wrapper input.select-dropdown`;
-    const limitDropdownMenu = `${widgetSelector} .limitSelection .select-wrapper ul.select-dropdown`;
-
-    await page.waitForSelector(limitDropdownTrigger, { visible: true });
-    await page.click(limitDropdownTrigger);
-
-    await page.waitForSelector(limitDropdownMenu, { visible: true });
-    let dropdown = await page.$(limitDropdownMenu);
-    expect(dropdown).to.be.ok;
-
-    // Pick the first option to trigger an AJAX call
-    await page.click(`${limitDropdownMenu} li:nth-child(1)`);
-
-    // give the AJAX call a moment to fire
-    await page.waitForNetworkIdle();
-    page.webpage.removeListener('request', requestHandler);
-    expect(ajaxRequestCount).to.be.equal(3);
-
-  });
-
-  it('should allow changing the visualization of the devices widget', async function () {
-    await openDevicesDetectionWidget();
-
-    const table = await page.$(widgetSelector);
-    expect(table).to.be.ok;
-
-    let ajaxRequestCount = 0;
-    const requestHandler = (request) => {
-      if (request.resourceType() === 'xhr' && request.url().indexOf('saveViewDataTableParameters') !== -1) {
-        console.log('diri naman', request.url());
-        ajaxRequestCount += 1;
-      }
-    };
-    page.webpage.on('request', requestHandler);
-
-    const visualizationTrigger = `${widgetSelector} .dataTableHeaderControls .dataTableControls a.activateVisualizationSelection`;
-    const visualizationTriggerIcon = `${visualizationTrigger} > span, ${visualizationTrigger} > img`;
-    const visualizationMenu = `${widgetSelector} .dataTableHeaderControls .dataTableControls ul.dropdown-content.dataTableFooterIcons`;
-    const visualizationButtonSelector = `${visualizationMenu} li .tableIcon[data-footer-icon-id]`;
-
-    await page.waitForSelector(visualizationTriggerIcon, { visible: true });
-    await page.click(visualizationTriggerIcon);
-    await page.waitForSelector(visualizationMenu, { visible: true });
-
-    await page.waitForFunction((selector) => document.querySelectorAll(selector).length >= 2, {}, visualizationButtonSelector);
-    await page.$$eval(visualizationButtonSelector, (buttons) => {
-      if (buttons.length < 2) {
-        throw new Error('Not enough visualization buttons found');
-      }
-      console.log('button html', buttons[1].outerHTML);
-      buttons[1].click();
+    const ajaxRequestCount = await trackViewDataTableRequests(async () => {
+      await interactWithColumnSortingAndLimit();
     });
 
-    await page.waitForNetworkIdle();
-    page.webpage.removeListener('request', requestHandler);
+    expect(ajaxRequestCount).to.be.equal(3);
+  });
+
+  it('should allow saving preference when changing the visualization of the devices widget', async function () {
+    await loadWidget();
+
+    const ajaxRequestCount = await trackViewDataTableRequests(async () => {
+      await changeVisualization();
+    });
+
     expect(ajaxRequestCount).to.be.equal(1);
   });
 
-  it('should allow toggling the totals row via the configuration menu', async function () {
-    await openDevicesDetectionWidget();
+  it('should allow saving preference when toggling the totals row via the configuration menu', async function () {
+    await loadWidget();
 
-    const table = await page.$(widgetSelector);
-    expect(table).to.be.ok;
+    const ajaxRequestCount = await trackViewDataTableRequests(async () => {
+      await toggleTotalsRow(2);
+    });
 
-    let ajaxRequestCount = 0;
-    const requestHandler = (request) => {
-      if (request.resourceType() === 'xhr' && request.url().indexOf('saveViewDataTableParameters') !== -1) {
-        ajaxRequestCount += 1;
-      }
-    };
-    page.webpage.on('request', requestHandler);
-
-    const configureTrigger = `${widgetSelector} .dataTableHeaderControls .dataTableControls a.dropdownConfigureIcon`;
-    const totalsRowToggle = '.dataTableShowTotalsRow';
-
-    const toggleTotalsRow = async () => {
-      await page.click(configureTrigger);
-      await page.waitForSelector(totalsRowToggle, { visible: true });
-      await page.click(totalsRowToggle);
-    };
-
-    await page.waitForSelector(configureTrigger, { visible: true });
-    await toggleTotalsRow();
-    await page.waitForNetworkIdle();
-
-    const reportPage = await page.$('.reporting-page');
-    expect(reportPage).to.be.ok;
-
-    await toggleTotalsRow();
-    await page.waitForNetworkIdle();
-
-    page.webpage.removeListener('request', requestHandler);
     expect(ajaxRequestCount).to.be.equal(2);
+  });
 
+  describe('As anonymous user', function () {
+    before(async function () {
+      await testEnvironment.callApi('UsersManager.setUserAccess', {
+        userLogin: 'anonymous',
+        access: 'view',
+        idSites: [3],
+      });
+      testEnvironment.testUseMockAuth = 0;
+      testEnvironment.save();
+    });
+
+    after(async function () {
+      testEnvironment.testUseMockAuth = 1;
+      testEnvironment.save();
+    });
+
+    it('should not save preferences when anonymous user performs datatable actions', async function () {
+      await loadWidget();
+
+      const ajaxRequestCount = await trackViewDataTableRequests(async () => {
+        await interactWithColumnSortingAndLimit();
+      });
+
+      expect(ajaxRequestCount).to.be.equal(0);
+    });
+
+    it('should not save preferences when anonymous user changes visualization', async function () {
+      await loadWidget();
+
+      const ajaxRequestCount = await trackViewDataTableRequests(async () => {
+        await changeVisualization();
+      });
+
+      expect(ajaxRequestCount).to.be.equal(0);
+    });
+
+    it('should not save preferences when anonymous user toggles totals row', async function () {
+      await loadWidget();
+
+      const ajaxRequestCount = await trackViewDataTableRequests(async () => {
+        await toggleTotalsRow(2);
+      });
+
+      expect(ajaxRequestCount).to.be.equal(0);
+    });
   });
 });
