0d7280a70b9fb6fc6f18b5e0558a26ef0401cf7e
chippison <chip.pison@innocraft.com>
2025-12-18 16:42:27 +1300

    moved the new test to its own suite to test if this works w/ a new test state

--- plugins/SegmentEditor/tests/UI/SegmentSelectorEditor_spec.js
+++ plugins/SegmentEditor/tests/UI/SegmentSelectorEditor_spec.js
@@ -103,66 +103,6 @@ describe("SegmentSelectorEditorTest", function () {
         await page.waitForNetworkIdle();
         expect(await page.screenshotSelector(selectorsToCapture)).to.matchImage('2_segment_editor_update');
     });
-  it('should not allow comparing segments more than the limit set', async function() {
-    const configLimit = 2;
-    const maxSegments = configLimit + 1;
-    testEnvironment.overrideConfig('General', 'data_comparison_segment_limit', configLimit);
-    await testEnvironment.save();
-    const dashUrl = '?module=CoreHome&action=index&' + generalParams + '#?' + generalParams + '&category=Dashboard_Dashboard&subcategory=1'
-    // Need to reload here since overrideConfig above does not really
-    // reflect well when the config is used in javascript
-    // start a fresh navigation so the new config is injected
-    await page.goto('about:blank');
-    await page.waitForNetworkIdle();
-    await page.goto(dashUrl);
-    await page.waitForNetworkIdle();
-    // We grab the max limit message
-    const maxLimitMessage = await page.evaluate(
-      (limit) => _pk_translate('General_MaximumNumberOfSegmentsComparedIs', [limit]),
-      maxSegments);
-
-    // We check that the title attribute is still not the max limit message
-    let title = await page.$eval('.segmentationContainer .segmentList li:last-child', (el) => el.getAttribute('title'));
-    expect(title).to.not.equal(maxLimitMessage);
-
-    await page.waitForSelector('.segmentationContainer');
-    // We check that the number of <li> elements is greater than the limit we set
-    const liElemLength = await page.$$eval('.segmentListContainer .segmentList li', (e) => e.length);
-    expect(liElemLength).to.be.greaterThan(maxSegments);
-
-    // We check that the number of segments compared is 1 at the start
-    let comparedCount = await page.$$eval(
-      '.segmentListContainer .segmentList li.comparedSegment',
-      (nodes) => nodes.length,
-    );
-    expect(comparedCount).to.equal(1);
-
-    // Making sure that the list is closed initially before the loop starts
-    const segmentListIsExpanded = await page.evaluate(() => !!document.querySelector('.segmentEditorPanel.expanded'));
-    console.log('segmentListIsExpanded', segmentListIsExpanded);
-    if (segmentListIsExpanded) {
-      await page.click('.segmentationContainer .title');
-      await page.waitForTimeout(100);
-    }
-
-    // We want to click all the segments so that we can check that it stops at the limit
-    for (let i=0; i<liElemLength; i++) {
-      await page.click('.segmentationContainer .title');
-      await page.waitForTimeout(100);
-      const elements = await page.$$('.segmentListContainer .segmentList li button.compareSegment');
-      console.log('i', i);
-      if (!elements[i]) break;
-      await elements[i].click();
-      await page.waitForTimeout(100);
-    }
-
-    // We check that the number of segments compared is now equal to the limit we set
-    comparedCount = await page.$$eval(
-      '.segmentListContainer .segmentList li.comparedSegment',
-      (nodes) => nodes.length,
-    );
-    expect(comparedCount).to.equal(maxSegments);
-  });
 
     it("should start editing segment name when segment name edit link clicked", async function() {
         await page.click('.segmentEditorPanel .editSegmentName');
