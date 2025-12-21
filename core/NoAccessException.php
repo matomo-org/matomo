22ca0cbc1a11a8550c6fda06ad3738663a15dc06
chippison <chip.pison@innocraft.com>
2025-12-22 08:26:13 +1300

    adding a flag to indicate session has expired

--- core/NoAccessException.php
+++ core/NoAccessException.php
@@ -19,8 +19,31 @@ use Piwik\Http\HttpCodeException;
  */
 class NoAccessException extends InvalidRequestParameterException implements HttpCodeException
 {
-    public function __construct($message, $code = 401)
+    /**
+     * @var bool
+     */
+    private $sessionExpired = false;
+
+    public function __construct($message, $code = 401, $sessionExpired = false)
     {
         parent::__construct($message, $code);
+        $this->sessionExpired = (bool) $sessionExpired;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasSessionExpired()
+    {
+        return $this->sessionExpired;
+    }
+
+    /**
+     * @param bool $sessionExpired
+     * @return void
+     */
+    public function setSessionExpired($sessionExpired)
+    {
+        $this->sessionExpired = (bool) $sessionExpired;
     }
 }
