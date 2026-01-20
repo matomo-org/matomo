101f0785e8aaf2e7f8898db34dbb3802c3846574
Marc Neudert <marc@innocraft.com>
2026-01-20 21:53:15 +0100

    Improve no data skip link target

--- plugins/BotTracking/Controller.php
+++ plugins/BotTracking/Controller.php
@@ -14,6 +14,7 @@ namespace Piwik\Plugins\BotTracking;
 use Piwik\DataTable\Renderer\Json;
 use Piwik\Piwik;
 use Piwik\Plugins\BotTracking\BotTrackingMethod\BotTrackingMethodAbstract;
+use Piwik\Plugin\Menu;
 use Piwik\Plugin\Manager;
 use Piwik\Request;
 use Piwik\SiteContentDetector;
@@ -25,8 +26,12 @@ class Controller extends \Piwik\Plugin\Controller
     {
         $this->checkSitePermission();
 
+        $request = Request::fromRequest();
+        $period  = $request->getStringParameter('period', '');
+        $date    = $request->getStringParameter('date', '');
+
         return $this->renderTemplateAs('siteWithoutData', [
-            'backToMatomoLink' => Url::getCurrentUrlWithoutQueryString(),
+            'backToMatomoLink' => $this->buildAIBotsOverviewUrl($period, $date),
             'hideWhatIsNew'    => true,
         ], $viewType = 'basic');
     }
@@ -149,11 +154,25 @@ class Controller extends \Piwik\Plugin\Controller
     {
         $this->checkSitePermission();
 
-        $noDataUrl = 'index.php?' . Url::getQueryStringFromParameters([
+        $request = Request::fromRequest();
+        $period  = $request->getStringParameter('period', '');
+        $date    = $request->getStringParameter('date', '');
+
+        $noDataParams = [
             'module' => 'BotTracking',
             'action' => 'siteWithoutData',
             'idSite' => $this->idSite,
-        ]);
+        ];
+
+        if ($period !== '') {
+            $noDataParams['period'] = $period;
+        }
+
+        if ($date !== '') {
+            $noDataParams['date'] = $date;
+        }
+
+        $noDataUrl = 'index.php?' . Url::getQueryStringFromParameters($noDataParams);
 
         return $this->renderTemplate('noRecentRequestsMessage', [
             'noDataUrl' => $noDataUrl,
@@ -189,5 +208,30 @@ class Controller extends \Piwik\Plugin\Controller
     protected function buildSiteContentDetector(): SiteContentDetector
     {
         return new SiteContentDetector();
+    }
+
+    protected function buildAIBotsOverviewUrl(string $period, string $date): string
+    {
+        $menu             = new Menu();
+        $params           = $menu->urlForDefaultUserParams($this->idSite);
+        $params['module'] = 'CoreHome';
+        $params['action'] = 'index';
+
+        if ($period !== '') {
+            $params['period'] = $period;
+        }
+
+        if ($date !== '') {
+            $params['date'] = $date;
+        }
+
+        $hashParams                = $params;
+        $hashParams['category']    = 'General_AIAssistants';
+        $hashParams['subcategory'] = 'BotTracking_AIBotsOverview';
+
+        return 'index.php?'
+            . Url::getQueryStringFromParameters($params)
+            . '#?'
+            . Url::getQueryStringFromParameters($hashParams);
     }
 }
