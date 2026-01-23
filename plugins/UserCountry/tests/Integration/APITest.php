43a2c0705a720e9507ea8bd3161a9c34c8dfddc0
Nathan Gavin <nathangavin987@gmail.com>
2026-01-23 17:19:54 +1300

    write api test to generate orders and fetch country

--- plugins/UserCountry/tests/Integration/APITest.php
+++ plugins/UserCountry/tests/Integration/APITest.php
@@ -9,9 +9,12 @@
 
 namespace Piwik\Plugins\UserCountry\tests\Integration;
 
+use DateTime;
 use Piwik\Access;
+use Piwik\API\Request;
 use Piwik\Common;
 use Piwik\Config;
+use Piwik\DataTable\Row;
 use Piwik\Plugins\UserCountry\API;
 use Piwik\Plugins\GeoIp2\LocationProvider\GeoIp2;
 use Piwik\Plugins\UserCountry\LocationProvider;
@@ -50,6 +53,56 @@ class APITest extends IntegrationTestCase
         $locationProvider = DefaultProvider::ID;
         $this->api->setLocationProvider($locationProvider);
         $this->assertEquals($locationProvider, Common::getCurrentLocationProviderId());
+    }
+
+    public function testGetCountry()
+    {
+        $dateTime = '2026-01-01 00:00:00';
+        $idSite = static::$fixture::createWebsite(
+            $dateTime,
+            $ecommerce = 1,
+        );
+
+        $this->createManyEcommerceOrders($idSite, $dateTime, 100);
+
+        $result = Request::processRequest('UserCountry.getCountry', [
+            'idSite' => $idSite,
+            'period' => 'year',
+            'date' => $dateTime,
+            'flat' => '1',
+        ]);
+        /** @var Row $row */
+        foreach ($result->getRows() as $row) {
+            var_dump($row->getColumns());
+        }
+
+    }
+
+    public function createManyEcommerceOrders($siteId, $dateTime, $numberOfOrders)
+    {
+        static::$fixture::createSuperUser($removeExisting = true);
+
+        $visitorIds = [
+            'visit-1',
+            'visit-2',
+            'visit-3',
+        ];
+
+        $orderNumber = 1001;
+
+        $dateTimeObj = new DateTime($dateTime);
+        $interval = new \DateInterval('P1D');
+
+        for ($i = 0; $i < $numberOfOrders; $i++) {
+            $dateTimeStr = date_format($dateTimeObj, 'Y-m-d H:i:s');
+            $tracker = static::$fixture::getTracker($siteId, $dateTimeStr, $defaultInit = true, $useLocal = true);
+            $tracker->setIp('151.100.101.92'); // Italy
+            $tracker->setVisitorId(substr(md5($visitorIds[$i % 3]), $offset = 0, $tracker::LENGTH_VISITOR_ID));
+            $tracker->setTokenAuth(static::$fixture::getTokenAuth());
+            $tracker->addEcommerceItem('SKU-' . ($orderNumber + $i), 'Product ' . ($orderNumber + $i), 'Category', 100, 1);
+            static::$fixture::checkResponse($tracker->doTrackEcommerceOrder($orderNumber + $i, 111.11, 100, 11));
+            $dateTimeObj = date_add($dateTimeObj, $interval);
+        }
     }
 
     public function testSetLocationProviderInvalid()
