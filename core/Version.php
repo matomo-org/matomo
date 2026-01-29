67636a477bb23d28513205563234f3508e1936ff
caddoo <caddoo@users.noreply.github.com>
2026-01-29 01:36:06 +0000

    Update version to 5.8.0-alpha.20260129013605

--- core/Version.php
+++ core/Version.php
@@ -22,7 +22,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    public const VERSION = '5.8.0-alpha';
+    public const VERSION = '5.8.0-alpha.20260129013605';
 
     public const MAJOR_VERSION = 5;
 
