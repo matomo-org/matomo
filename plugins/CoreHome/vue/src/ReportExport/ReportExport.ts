f23f5daaa9c0e78a291550c704f45b2add8556da
chippison <chip.pison@innocraft.com>
2026-02-16 11:46:33 +1300

    refactoring to fix hasSubtables bug

--- plugins/CoreHome/vue/src/ReportExport/ReportExport.ts
+++ plugins/CoreHome/vue/src/ReportExport/ReportExport.ts
@@ -45,6 +45,8 @@ export default {
       const optionShowDimensions = dataTable.param.show_dimensions === true
         || dataTable.param.show_dimensions === 1
         || dataTable.param.show_dimensions === '1';
+      const hasSubtables = dataTable.numberOfSubtables > 0;
+      const hasSubtableOptionControls = optionFlat || hasSubtables;
 
       const props = {
         initialReportType: 'default',
@@ -54,7 +56,8 @@ export default {
         initialOptionShowDimensions: optionShowDimensions,
         initialOptionExpanded: true,
         initialOptionFormatMetrics: false,
-        hasSubtables: optionFlat || dataTable.numberOfSubtables > 0,
+        hasSubtableOptionControls,
+        hasSubtables,
         availableReportFormats: {
           default: formats,
           processed: {
