581ed27028b8a5920f1ae210bce11b6900894deb
mneudert <mneudert@users.noreply.github.com>
2026-02-23 17:58:46 +0000

    Update version to 5.8.0-b1.20260223175845

--- core/Version.php
+++ core/Version.php
@@ -22,7 +22,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    public const VERSION = '5.8.0-b1';
+    public const VERSION = '5.8.0-b1.20260223175845';
 
     public const MAJOR_VERSION = 5;
 
