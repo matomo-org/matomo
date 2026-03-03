9daed58cb6e7feb7bf54c6a15a0ce00edbbbd411
chippison <chip.pison@innocraft.com>
2026-03-04 12:53:03 +1300

    refactoring PeriodSelector.vue so that we split to a couple more vue components

---
+++ plugins/CoreHome/vue/src/PeriodSelector/PeriodSelector.types.ts
@@ -0,0 +1,95 @@
+/*!
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+import Matomo from '../Matomo/Matomo';
+import { translate } from '../translate';
+import type {
+  PresetDateRangeId,
+  PresetDateRangeSelection,
+} from './PresetDateRangeResolver';
+import type { UiSelection as HashSyncUiSelection } from './PeriodSelectorHashSync';
+
+const NBSP = Matomo.helper.htmlDecode('&nbsp;');
+
+export const COMPARE_PERIOD_TYPES = ['custom', 'previousPeriod', 'previousYear'];
+
+export const COMPARE_PERIOD_OPTIONS = [
+  { key: 'custom', value: translate('General_Custom') },
+  {
+    key: 'previousPeriod',
+    value: translate('General_PreviousPeriod').replace(/\s+/, NBSP),
+  },
+  {
+    key: 'previousYear',
+    value: translate('General_PreviousYear').replace(/\s+/, NBSP),
+  },
+];
+
+// the date when the site was created
+export const siteMinAllowedDate = new Date(
+  Matomo.minDateYear,
+  Matomo.minDateMonth - 1,
+  Matomo.minDateDay,
+);
+// today/now
+export const siteMaxAllowedDate = new Date(
+  Matomo.maxDateYear,
+  Matomo.maxDateMonth - 1,
+  Matomo.maxDateDay,
+);
+
+export const RANGE_PERIOD = 'range';
+
+export type InteractionSource = 'period' | 'preset' | 'calendar' | 'range' | null;
+export type SingleCalendarPeriod = 'day' | 'week' | 'month' | 'year';
+export type CalendarViewport = 'single' | 'range';
+export type UiSelection = HashSyncUiSelection<PresetDateRangeId>;
+
+export function isValidDate(candidateDate: unknown): boolean {
+  if (Object.prototype.toString.call(candidateDate) !== '[object Date]') {
+    return false;
+  }
+
+  return !Number.isNaN((candidateDate as Date).getTime());
+}
+
+export function isSingleCalendarPeriod(period: string): period is SingleCalendarPeriod {
+  return period === 'day'
+    || period === 'week'
+    || period === 'month'
+    || period === 'year';
+}
+
+export interface PeriodSelectorState {
+  uiSelection: UiSelection;
+  lastInteractionSource: InteractionSource;
+  nextHashUiSelection: UiSelection|null;
+  nextHashSelectionKey: string|null;
+  lastKnownHashSelectionKey: string|null;
+  lastKnownHashContextKey: string|null;
+  minAllowedDate: Date;
+  maxAllowedDate: Date;
+  activePresetId: PresetDateRangeId|null;
+  pendingPresetSelection: PresetDateRangeSelection|null;
+  appliedPeriod: string;
+  appliedAnchorDate: Date|null;
+  uiSelectedPeriod: string;
+  calendarViewport: CalendarViewport;
+  singleCalendarPeriod: SingleCalendarPeriod;
+  singleCalendarSelectedDate: Date|null;
+  appliedRangeStartDate: string|null;
+  appliedRangeEndDate: string|null;
+  stagedPresetRangeStartDate: string|null;
+  stagedPresetRangeEndDate: string|null;
+  isRangeValid: boolean|null;
+  isLoadingNewPage: boolean;
+  isComparing: null|boolean;
+  comparePeriodType: string;
+  compareStartDate: string;
+  compareEndDate: string;
+  compareAppliedSignature: string;
+}
