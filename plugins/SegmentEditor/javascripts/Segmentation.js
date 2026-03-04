665f4d3a8056cdcd4f106d0b0e1738a7a125d532
Thomas ZILLIOX <thomas.zilliox@innocraft.com>
2026-03-04 01:55:05 +0100

    Fix - Allow to remove 'All Visits' from comparisons

--- plugins/SegmentEditor/javascripts/Segmentation.js
+++ plugins/SegmentEditor/javascripts/Segmentation.js
@@ -538,11 +538,9 @@ Segmentation = (function($) {
                   return false;
                 }
                 const comparisonService = window.CoreHome.ComparisonsStoreInstance;
-                const segmentDefinition = $button.closest('li').data('definition');
+                const segmentDefinition = $button.closest('li').data('definition') ?? '';
                 if ($button.attr('data-state') === 'active') {
-                  if (segmentDefinition) {
-                    comparisonService.removeSegmentComparisonByDefinition(segmentDefinition);
-                  }
+                  comparisonService.removeSegmentComparisonByDefinition(segmentDefinition);
                 } else {
                   comparisonService.addSegmentComparison({
                     segment: segmentDefinition,
