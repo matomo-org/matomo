93ed62ce4d69d3f9cce82f5643af2f47f51e5d0a
Nathan Gavin <nathangavin987@gmail.com>
2026-03-17 09:01:21 +1300

    remove outdated comment, fix test include path backslashes where appropriate

--- tests/PHPUnit/proxy/includes.php
+++ tests/PHPUnit/proxy/includes.php
@@ -1,12 +1,11 @@
 <?php
 
 if (!defined('PIWIK_INCLUDE_PATH')) {
-    // NOTE: PIWIK_INCLUDE_PATH must end in '/' or some parts of matomo will break
-    define('PIWIK_INCLUDE_PATH', realpath(dirname(__FILE__) . '/../../..') . '/');
+    define('PIWIK_INCLUDE_PATH', realpath(dirname(__FILE__) . '/../../..'));
 }
 
 if (!defined('PIWIK_USER_PATH')) {
-    define('PIWIK_USER_PATH', rtrim(PIWIK_INCLUDE_PATH, '/'));
+    define('PIWIK_USER_PATH', PIWIK_INCLUDE_PATH);
 }
 if (!defined('PIWIK_TEST_MODE')) {
     define('PIWIK_TEST_MODE', true);
@@ -14,7 +13,7 @@ if (!defined('PIWIK_TEST_MODE')) {
 
 $GLOBALS['MATOMO_PLUGIN_DIRS'] = array(
     array(
-        'pluginsPathAbsolute' => realpath(PIWIK_INCLUDE_PATH . 'tests/resources/custompluginsdir'),
+        'pluginsPathAbsolute' => realpath(PIWIK_INCLUDE_PATH . '/tests/resources/custompluginsdir'),
         'webrootDirRelativeToMatomo' => '../../resources/custompluginsdir'
     ),
 );
