7fff3473731d92dba419faf3430483c779b27ed9
Thomas ZILLIOX <thomas.zilliox@innocraft.com>
2026-03-26 17:41:57 +0100

    Update UI screenshots and add UserManager Theme change test

--- plugins/UsersManager/tests/Integration/ControllerTest.php
+++ plugins/UsersManager/tests/Integration/ControllerTest.php
@@ -9,13 +9,18 @@
 
 namespace Piwik\Plugins\UsersManager\tests\Integration;
 
+use Piwik\Config;
 use Piwik\Tests\Framework\TestCase\IntegrationTestCase;
 use Piwik\Plugins\UsersManager\Controller;
 use Piwik\Nonce;
 use Piwik\Auth\PasswordStrength;
 use Piwik\Date;
+use Piwik\Plugin\ThemeStyles;
+use Piwik\Plugins\UsersManager\API as UsersManagerAPI;
 use Piwik\Plugins\UsersManager\Model;
 use Piwik\Plugins\Login\PasswordVerifier;
+use Piwik\Plugins\UsersManager\UserPreferences;
+use Piwik\Tests\Framework\Mock\FakeAccess;
 use Piwik\Translation\Loader\DevelopmentLoader;
 use Piwik\Translation\Loader\JsonFileLoader;
 use Piwik\Translation\Translator;
@@ -32,6 +37,9 @@ class ControllerTest extends IntegrationTestCase
      */
     private $controller;
     private $post;
+    private $get;
+    private $request;
+    private $enableUsersAdmin;
 
     public function setUp(): void
     {
@@ -44,12 +52,23 @@ class ControllerTest extends IntegrationTestCase
             $passwordStrength = new PasswordStrength(true)
         );
         $this->post = $_POST;
+        $this->get = $_GET;
+        $this->request = $_REQUEST;
+        $this->enableUsersAdmin = Config::getInstance()->General['enable_users_admin'];
+
+        FakeAccess::$superUser = true;
+
+        $identity = FakeAccess::$identity;
+        UsersManagerAPI::getInstance()->addUser($identity, 'Password111!', 'controller-test@example.com');
     }
 
     public function tearDown(): void
     {
         parent::tearDown();
         $_POST = $this->post;
+        $_GET = $this->get;
+        $_REQUEST = $this->request;
+        Config::getInstance()->General['enable_users_admin'] = $this->enableUsersAdmin;
     }
 
     public function testRecordPasswordChangePasswordStrengthCheckWeakPassword()
@@ -77,6 +96,27 @@ class ControllerTest extends IntegrationTestCase
         $this->controller->recordPasswordChange();
     }
 
+    public function testRecordUserSettingsShouldRejectInvalidThemeMode()
+    {
+        Config::getInstance()->General['enable_users_admin'] = 0;
+
+        $_GET = [
+            'format' => 'json',
+            'themeMode' => 'invalid',
+            'defaultReport' => '1',
+            'defaultDate' => 'today',
+            'language' => 'en',
+            'timeformat' => '0',
+        ];
+        $_POST = [];
+        $_REQUEST = $_GET;
+
+        $response = $this->controller->recordUserSettings();
+
+        $this->assertStringContainsString('Invalid theme mode', $response);
+        $this->assertSame(ThemeStyles::LIGHT_MODE, (new UserPreferences())->getThemeMode());
+    }
+
     private function setupPostStateWithPassword(string $password)
     {
         $_POST['nonce'] = Nonce::getNonce('changePasswordNonce');
@@ -84,5 +124,12 @@ class ControllerTest extends IntegrationTestCase
         $_POST['passwordBis'] = $password;
         // original password (irrelevant for test)
         $_POST['passwordConfirmation'] = '';
+    }
+
+    public function provideContainerConfig()
+    {
+        return [
+            'Piwik\Access' => new FakeAccess(),
+        ];
     }
 }
