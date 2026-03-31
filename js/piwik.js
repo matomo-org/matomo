043df7bf7cdf5f4f59245b4a8b348a3708337738
sgiehl <stefan@matomo.org>
2026-03-31 17:54:28 +0200

    Adds javascript tracker tests

--- js/piwik.js
+++ js/piwik.js
@@ -117,7 +117,7 @@
      "", "\b", "\t", "\n", "\f", "\r", "\"", "\\", apply, call, charCodeAt, getUTCDate, getUTCFullYear, getUTCHours,
     getUTCMinutes, getUTCMonth, getUTCSeconds, hasOwnProperty, join, lastIndex, length, parse, prototype, push, replace,
     sort, slice, stringify, test, toJSON, toString, valueOf, objectToJSON, addTracker, removeAllAsyncTrackersButFirst,
-    optUserOut, forgetUserOptOut, isUserOptedOut, withCredentials, visibilityState, enableFileTracking, setReferrerUrlMaxLength
+    optUserOut, forgetUserOptOut, isUserOptedOut, withCredentials, prerendering, visibilityState, enableFileTracking, setReferrerUrlMaxLength
  */
 /*global _paq:true */
 /*members push */
