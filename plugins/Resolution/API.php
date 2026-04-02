2f02ce959db332934c2dd437df79cc1b989dbdd9
sgiehl <stefan@matomo.org>
2026-04-02 16:20:16 +0200

    Add native type hints and fix multi site handling for CNIL check

--- plugins/Resolution/API.php
+++ plugins/Resolution/API.php
@@ -11,8 +11,9 @@ namespace Piwik\Plugins\Resolution;
 
 use Exception;
 use Piwik\Archive;
-use Piwik\Container\StaticContainer;
+use Piwik\DataTable;
 use Piwik\Piwik;
+use Piwik\Site;
 
 /**
  * @see plugins/Resolution/functions.php
@@ -27,14 +28,11 @@ require_once PIWIK_INCLUDE_PATH . '/plugins/Resolution/functions.php';
 class API extends \Piwik\Plugin\API
 {
     /**
-     * @param string $name
      * @param int|string|int[] $idSite
-     * @param string $period
-     * @param string $date
      * @param string|null|false $segment
      * @return \Piwik\DataTable|\Piwik\DataTable\Map
      */
-    protected function getDataTable($name, $idSite, $period, $date, $segment)
+    protected function getDataTable(string $name, $idSite, string $period, string $date, $segment)
     {
         Piwik::checkUserHasViewAccess($idSite);
         $archive = Archive::build($idSite, $period, $date, $segment);
@@ -59,16 +57,25 @@ class API extends \Piwik\Plugin\API
      * @param string|null|false $segment Custom segment to filter the report.
      *                                   Example: "referrerName==example.com"
      *                                   Supports AND (;) and OR (,) operators.
-     * @return \Piwik\DataTable|\Piwik\DataTable\Map Screen resolution metrics for the requested site and period.
+     * @return DataTable|DataTable\Map Screen resolution metrics for the requested site and period.
      */
-    public function getResolution($idSite, $period, $date, $segment = false)
+    public function getResolution($idSite, string $period, string $date, $segment = false)
     {
-        $translator = StaticContainer::get('Piwik\Translation\Translator');
-        if (Resolution::isScreenResolutionDetectionDisabledByCompliancePolicy($idSite)) {
-            throw new Exception($translator->translate('Resolution_ScreenResolutionReportDisabledByCompliancePolicy'));
+        Piwik::checkUserHasViewAccess($idSite);
+
+        $idSites = Site::getIdSitesFromIdSitesString($idSite);
+
+        // filter sites where model report disabled by compliance
+        $idSitesFiltered = array_filter($idSites, function ($idSite) {
+            return !Resolution::isScreenResolutionDetectionDisabledByCompliancePolicy((int)$idSite);
+        });
+
+        // show an error only if none of the requested sites is left
+        if (count($idSitesFiltered) === 0 && count($idSites) !== count($idSitesFiltered)) {
+            throw new Exception(Piwik::translate('Resolution_ScreenResolutionReportDisabledByCompliancePolicy'));
         }
 
-        $dataTable = $this->getDataTable(Archiver::RESOLUTION_RECORD_NAME, $idSite, $period, $date, $segment);
+        $dataTable = $this->getDataTable(Archiver::RESOLUTION_RECORD_NAME, $idSitesFiltered, $period, $date, $segment);
         $dataTable->filter('AddSegmentValue');
         return $dataTable;
     }
@@ -88,9 +95,9 @@ class API extends \Piwik\Plugin\API
      * @param string|null|false $segment Custom segment to filter the report.
      *                                   Example: "referrerName==example.com"
      *                                   Supports AND (;) and OR (,) operators.
-     * @return \Piwik\DataTable|\Piwik\DataTable\Map Device configuration metrics for the requested site and period.
+     * @return DataTable|DataTable\Map Device configuration metrics for the requested site and period.
      */
-    public function getConfiguration($idSite, $period, $date, $segment = false)
+    public function getConfiguration($idSite, string $period, string $date, $segment = false)
     {
         $dataTable = $this->getDataTable(Archiver::CONFIGURATION_RECORD_NAME, $idSite, $period, $date, $segment);
         // use GroupBy filter to avoid duplicate rows if old reports are displayed
