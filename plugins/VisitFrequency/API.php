11788b4f2ea75645ba2f6426086aa3d33c89ea6f
Stefan Giehl <stefan@matomo.org>
2026-04-05 00:26:57 +0200

    Apply suggestion from @tzi
    Co-authored-by: Thomas ZILLIOX <thomas.zilliox@innocraft.com>

--- plugins/VisitFrequency/API.php
+++ plugins/VisitFrequency/API.php
@@ -42,7 +42,7 @@ class API extends \Piwik\Plugin\API
      *                                 - Multiple site IDs (e.g. [1, 4, 5])
      *                                 - Comma-separated list ("1,4,5") or "all"
      * @param 'day'|'week'|'month'|'year'|'range' $period The period to process, processes data for the period
-     *                                                   containing the specified date.
+     *                                                    containing the specified date.
      * @param string $date The date or date range to process.
      *                     'YYYY-MM-DD', magic keywords (today, yesterday, lastWeek, lastMonth, lastYear),
      *                     or date range (ie, 'YYYY-MM-DD,YYYY-MM-DD', lastX, previousX).
