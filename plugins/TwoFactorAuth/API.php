d46a61ed4dcec3a6c7ff4a93bed0b2a9474d0ce0
Stefan Giehl <stefan@matomo.org>
2026-04-08 06:18:18 +0200

    Update API documentation for TwoFactorAuth (#24326)

--- plugins/TwoFactorAuth/API.php
+++ plugins/TwoFactorAuth/API.php
@@ -11,6 +11,11 @@ namespace Piwik\Plugins\TwoFactorAuth;
 
 use Piwik\Piwik;
 
+/**
+ * Provides API methods for managing two-factor authentication.
+ *
+ * @method static \Piwik\Plugins\TwoFactorAuth\API getInstance()
+ */
 class API extends \Piwik\Plugin\API
 {
     /**
@@ -23,10 +28,17 @@ class API extends \Piwik\Plugin\API
         $this->twoFa = $twoFa;
     }
 
+    /**
+     * Disables two-factor authentication for the specified user.
+     *
+     * @param string $userLogin The login of the user whose two-factor authentication should be reset.
+     * @param string $passwordConfirmation The current superuser's password confirmation.
+     * @return void
+     */
     public function resetTwoFactorAuth(
-        $userLogin,
+        string $userLogin,
         #[\SensitiveParameter]
-        $passwordConfirmation = ''
+        string $passwordConfirmation = ''
     ) {
         Piwik::checkUserHasSuperUserAccess();
 
