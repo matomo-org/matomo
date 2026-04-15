f45a31b3ab83607a6832655201605cb3d917a415
Nathan Gavin <nathangavin987@gmail.com>
2026-04-15 16:46:38 +1200

    update compliance dashboard to work without an idsite present

--- plugins/PrivacyManager/vue/src/Compliance/Compliance.vue
+++ plugins/PrivacyManager/vue/src/Compliance/Compliance.vue
@@ -39,12 +39,51 @@ import {
   EnrichedHeadline,
   Matomo,
   MatomoUrl,
+  SitesStore,
   SiteSelector,
   SiteRef,
   translate,
 } from 'CoreHome';
 import { fetchCompliancePolicies, CompliancePolicy } from './Compliance.store';
 import ComplianceOverview from './ComplianceOverview.vue';
+
+function getInitialSite(urlIdSite: string|number|undefined, multiSitesSummary: string): SiteRef {
+  if (urlIdSite != null && urlIdSite !== '') {
+    if (String(urlIdSite) === 'all') {
+      return { id: 'all', name: multiSitesSummary };
+    }
+
+    if (String(Matomo.idSite) === String(urlIdSite) && Matomo.siteName) {
+      return {
+        id: urlIdSite,
+        name: Matomo.helper.htmlDecode(Matomo.siteName),
+      };
+    }
+
+    return { id: urlIdSite, name: String(urlIdSite) };
+  }
+
+  return { id: 'all', name: multiSitesSummary };
+}
+
+function getSiteFromUrlId(
+  urlIdSite: string|number,
+  sites: ReadonlyArray<{ idsite: string|number; name: string }>,
+  multiSitesSummary: string,
+): SiteRef {
+  if (String(urlIdSite) === 'all') {
+    return { id: 'all', name: multiSitesSummary };
+  }
+
+  const selectedSite = sites.find(
+    (availableSite) => String(availableSite.idsite) === String(urlIdSite),
+  );
+  if (selectedSite) {
+    return { id: selectedSite.idsite, name: selectedSite.name };
+  }
+
+  return getInitialSite(urlIdSite, multiSitesSummary);
+}
 
 export default defineComponent({
   components: {
@@ -53,11 +92,10 @@ export default defineComponent({
     SiteSelector,
   },
   setup() {
-    const site = ref<SiteRef>({
-      id: Matomo.idSite ?? MatomoUrl.urlParsed.value.idSite,
-      name: Matomo.siteName ? Matomo.helper.htmlDecode(Matomo.siteName) : translate('General_MultiSitesSummary'),
-    });
-    const siteId = ref(String(Matomo.idSite ?? MatomoUrl.urlParsed.value.idSite));
+    const multiSitesSummary = translate('General_MultiSitesSummary');
+    const urlIdSite = MatomoUrl.urlParsed.value.idSite as string|number|undefined;
+    const site = ref<SiteRef>(getInitialSite(urlIdSite, multiSitesSummary));
+    const siteId = ref(urlIdSite != null && urlIdSite !== '' ? String(urlIdSite) : '');
 
     watch(site, (newSite) => {
       siteId.value = newSite?.id != null ? String(newSite.id) : '';
@@ -65,7 +103,26 @@ export default defineComponent({
 
     const complianceTypes = ref<CompliancePolicy[]>([]);
     onMounted(async () => {
-      complianceTypes.value = await fetchCompliancePolicies();
+      const [policies, sites] = await Promise.all([
+        fetchCompliancePolicies(),
+        SitesStore.loadInitialSites(),
+      ]);
+
+      complianceTypes.value = policies;
+
+      if (urlIdSite != null && urlIdSite !== '') {
+        site.value = getSiteFromUrlId(urlIdSite, sites || [], multiSitesSummary);
+        return;
+      }
+
+      if (sites?.length === 1) {
+        site.value = { id: sites[0].idsite, name: sites[0].name };
+        return;
+      }
+
+      if (sites?.length) {
+        site.value = { id: 'all', name: multiSitesSummary };
+      }
     });
 
     return {
