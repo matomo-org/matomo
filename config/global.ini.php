a9941d175a84a512963d258107f74652ca6b6e7d
sgiehl <stefan@matomo.org>
2026-04-16 11:52:56 +0200

    [TEMP] enable flat archiving

--- config/global.ini.php
+++ config/global.ini.php
@@ -626,7 +626,7 @@ datatable_archiving_maximum_rows_subtable_custom_dimensions = 1000
 datatable_archiving_maximum_rows_actions = 500
 ; maximum number of rows used when archiving flat page/title actions before rebuilding hierarchy
 ; if set to 0, legacy hierarchical-only Actions archiving is used
-datatable_archiving_maximum_rows_actions_flat = 0
+datatable_archiving_maximum_rows_actions_flat = 50000
 ; maximum number of rows for pages in categories (sub pages, when clicking on the + for a page category)
 ; note: should not exceed the display limit in Piwik\Actions\Controller::ACTIONS_REPORT_ROWS_DISPLAY
 ; because each subdirectory doesn't have paging at the bottom, so all data should be displayed if possible.
