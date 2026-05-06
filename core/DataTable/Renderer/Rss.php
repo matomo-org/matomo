7e7abf672fe377bd684dc02697dca86105dc694a
Nathan Gavin <nathangavin987@gmail.com>
2026-05-06 17:55:17 +1200

    harden RSS rendering to properly escape values

--- core/DataTable/Renderer/Rss.php
+++ core/DataTable/Renderer/Rss.php
@@ -161,7 +161,7 @@ class Rss extends Renderer
                 if ($this->translateColumnNames) {
                     $name = $this->translateColumnName($name);
                 }
-                $html .= "\n\t<td><strong>$name</strong></td>";
+                $html .= "\n\t<td><strong>" . self::formatValueXml($name) . "</strong></td>";
             }
         }
         $html .= "\n</tr>";
@@ -172,7 +172,7 @@ class Rss extends Renderer
                 if ($toDisplay !== false) {
                     $value = "-";
                     if (isset($row[$columnName])) {
-                        $value = urldecode($row[$columnName]);
+                        $value = self::formatValueXml(urldecode($row[$columnName]));
                     }
 
                     $html .= "\n\t<td>$value</td>";
