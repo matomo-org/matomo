0223464a6597033543060f16ea975ac16d162d80
Altamash Shaikh <altu9594@gmail.com>
2026-06-25 09:37:06 +0530

    Update file moved

--- core/Updates/5.12.0-b1.php
+++ core/Updates/5.12.0-b1.php
@@ -7,7 +7,7 @@
  * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  */
 
-namespace Piwik\Plugins\Marketplace;
+namespace Piwik\Updates;
 
 use Piwik\Updater;
 use Piwik\Updater\Migration;
