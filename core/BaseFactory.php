2c62d17a24c12b329e377cde50ae2d92053dd834
sgiehl <stefan@matomo.org>
2026-07-01 18:30:11 +0200

    Fix overly-specific return type in BaseFactory::factory()
    BaseFactory is a generic base class meant to be extended by many
    different factory types (per its own class docblock), but its
    factory() method was documented as always returning a
    \Piwik\DataTable\Renderer, which only made sense for one specific
    subclass's usage.

--- core/BaseFactory.php
+++ core/BaseFactory.php
@@ -27,7 +27,7 @@ abstract class BaseFactory
      * Creates a new instance of a class using a string ID.
      *
      * @param string $classId The ID of the class.
-     * @return \Piwik\DataTable\Renderer
+     * @return object
      * @throws Exception if $classId is invalid.
      */
     public static function factory($classId)
