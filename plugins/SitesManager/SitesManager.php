cd96d0ea9cf8986651472305b9b67bcddd4abf90
sgiehl <stefan@matomo.org>
2026-07-03 08:01:22 +0200

    Fix copy-pasted docblock on SitesManager::getTimezoneFromWebsite()
    Summary was copied from shouldKeepURLFragmentsFor() and described URL
    fragments; the method returns the site's timezone.

--- plugins/SitesManager/SitesManager.php
+++ plugins/SitesManager/SitesManager.php
@@ -227,7 +227,7 @@ class SitesManager extends \Piwik\Plugin
     }
 
     /**
-     * Returns whether we should keep URL fragments for a specific site.
+     * Returns the timezone configured for a specific site.
      *
      * @param array $site DB data for the site.
      * @return ?string
