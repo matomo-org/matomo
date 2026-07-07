4cbca3d114b1fd33d20ac42f81f4f7e7c12035f2
sgiehl <stefan@matomo.org>
2026-07-07 17:40:06 +0200

    Correct ServerModule getLocation/isWorking return docblocks
    getLocation() returns false when no fallback resolves the location, and
    isWorking() returns a translated error string when the server var is missing.

--- plugins/GeoIp2/LocationProvider/GeoIp2/ServerModule.php
+++ plugins/GeoIp2/LocationProvider/GeoIp2/ServerModule.php
@@ -60,7 +60,7 @@ class ServerModule extends GeoIp2
      * information will not be available to Piwik.
      *
      * @param array $info Must have an 'ip' field.
-     * @return array
+     * @return array|false
      */
     public function getLocation($info)
     {
@@ -178,7 +178,7 @@ class ServerModule extends GeoIp2
     /**
      * Returns true if the MMDB_ADDR server variable is defined.
      *
-     * @return bool
+     * @return bool|string
      */
     public function isWorking()
     {
