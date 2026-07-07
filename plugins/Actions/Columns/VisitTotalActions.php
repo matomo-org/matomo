f4e319796bdd92109c2c795a3f8325cb8908fd31
sgiehl <stefan@matomo.org>
2026-07-07 11:13:58 +0200

    Correct onExistingVisit return docblock in VisitTotalActions

--- plugins/Actions/Columns/VisitTotalActions.php
+++ plugins/Actions/Columns/VisitTotalActions.php
@@ -76,7 +76,7 @@ class VisitTotalActions extends VisitDimension
 
     /**
      * @param Action|null $action
-     * @return int
+     * @return string|false
      */
     public function onExistingVisit(Request $request, Visitor $visitor, $action)
     {
