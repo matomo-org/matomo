05aa4462082e2882749386fd402f4c3410ff5bf4
chippison <chip.pison@innocraft.com>
2026-07-08 12:24:13 +1200

    Add DateAtom sparkline-card component
    Introduces DateAtom, a small presentational Vue atom that renders a
    single compared-date label (the backend's localised pretty period
    string) with muted, single-line styling for the sparklines redesign.
    It is the foundation for the date-comparison card body, kept as its own
    component so that it can be reused in other cards that can be worked with in parallel
    
    Adds the component, its stylesheet (registered in getStylesheetFiles)
    and a unit spec.

--- plugins/CoreVisualizations/CoreVisualizations.php
+++ plugins/CoreVisualizations/CoreVisualizations.php
@@ -61,6 +61,7 @@ class CoreVisualizations extends \Piwik\Plugin
         $stylesheets[] = "plugins/CoreVisualizations/vue/src/SingleMetricView/SingleMetricView.less";
         $stylesheets[] = "plugins/CoreVisualizations/vue/src/SparklinesGrid/SparklinesGrid.less";
         $stylesheets[] = "plugins/CoreVisualizations/vue/src/Sparklines/SparklineCard.less";
+        $stylesheets[] = "plugins/CoreVisualizations/vue/src/Sparklines/DateAtom.less";
 
         $stylesheets[] = "plugins/CoreVisualizations/stylesheets/dataTableVisualizations.less";
         $stylesheets[] = "plugins/CoreVisualizations/stylesheets/jqplot.less";
