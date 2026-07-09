ca6751ffc8b8fc2ea0e3b9f86b4cd8bded8f68cc
sgiehl <stefan@matomo.org>
2026-07-09 14:54:10 +0200

    Set version to 6.0.0-b1
    Use a beta version so bundled plugins requiring Matomo >=6.0.0-b1 are
    considered compatible (6.0.0-alpha would sort below the b1 constraint).

--- core/Version.php
+++ core/Version.php
@@ -22,7 +22,7 @@ final class Version
      * The current Matomo version.
      * @var string
      */
-    public const VERSION = '6.0.0-alpha';
+    public const VERSION = '6.0.0-b1';
 
     public const MAJOR_VERSION = 6;
 
