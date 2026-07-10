1f8c2b2b11981b5bc65ac3123430278d1a7868f4
Stefan Giehl <stefan@matomo.org>
2026-07-10 15:06:31 +0200

    Apply suggestion from @sgiehl

--- plugins/TestRunner/Commands/GenerateGitHubTestActionFile.php
+++ plugins/TestRunner/Commands/GenerateGitHubTestActionFile.php
@@ -20,7 +20,7 @@ use Piwik\View;
 class GenerateGitHubTestActionFile extends ConsoleCommand
 {
     public const COMMAND_NAME = 'generate:test-action';
-    public const LATEST_PHP_VERSION = '8.4';
+    public const LATEST_PHP_VERSION = '8.5';
 
     protected $plugin = null;
     protected $phpVersions = null;
