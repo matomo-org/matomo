f4a7144eb5b5ea3b3c1d941dd9c1c27709e641d8
Marc Neudert <marc@innocraft.com>
2026-08-10 17:22:20 +0200

    Import AIProviders plugin (#25038)
    * Import AIProviders plugin
    
    * Clean up external-plugin-only data
    
    * Add AIProviders to core plugin list
    
    * ws / cs
    
    * Build vue files
    
    * Update test expectations

--- config/global.ini.php
+++ config/global.ini.php
@@ -1343,6 +1343,7 @@ Plugins[] = JsTrackerInstallCheck
 Plugins[] = FeatureFlags
 Plugins[] = AIAgents
 Plugins[] = BotTracking
+Plugins[] = AIProviders
 
 [PluginsInstalled]
 PluginsInstalled[] = Diagnostics
