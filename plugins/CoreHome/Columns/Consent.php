8aa4f4839584c2f984f483ffc464ea4a9c576f92
sgiehl <stefan@matomo.org>
2026-08-13 11:49:22 +0200

    Keep the consent dimension out of Custom Reports
    An unnamed dimension is skipped by CustomReports::getAvailableDimensions(),
    so drop the name to avoid offering an always empty dimension there. Without
    a name no metric is generated either, which makes the configureMetrics()
    override obsolete.

--- plugins/CoreHome/Columns/Consent.php
+++ plugins/CoreHome/Columns/Consent.php
@@ -11,8 +11,6 @@ declare(strict_types=1);
 
 namespace Piwik\Plugins\CoreHome\Columns;
 
-use Piwik\Columns\DimensionMetricFactory;
-use Piwik\Columns\MetricsList;
 use Piwik\Plugin\Dimension\VisitDimension;
 
 /**
@@ -20,14 +18,8 @@ use Piwik\Plugin\Dimension\VisitDimension;
  */
 class Consent extends VisitDimension
 {
-    public const COLUMN_TYPE = 'TINYINT(1) UNSIGNED NULL DEFAULT NULL';
+    public const COLUMN_TYPE = 'TINYINT(1) UNSIGNED NULL';
 
-    protected $columnName   = 'consent';
-    protected $columnType   = self::COLUMN_TYPE;
-    protected $nameSingular = 'CoreHome_Consent';
-
-    public function configureMetrics(MetricsList $metricsList, DimensionMetricFactory $dimensionMetricFactory)
-    {
-        // no metric, the column isn't tracked yet
-    }
+    protected $columnName = 'consent';
+    protected $columnType = self::COLUMN_TYPE;
 }
