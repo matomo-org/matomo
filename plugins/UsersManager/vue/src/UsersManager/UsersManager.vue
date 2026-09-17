89ba4412923aa82ecbdaecee25587c42b69e18b1
chippison <chip.pison@innocraft.com>
2026-09-17 14:47:04 +1200

    Open the invite form from the users manager quick link
    The showadduser URL parameter still opened the edit form, which since #23470 has
    no add mode and fetched permissions for an empty login.

--- plugins/UsersManager/vue/src/UsersManager/UsersManager.vue
+++ plugins/UsersManager/vue/src/UsersManager/UsersManager.vue
@@ -226,7 +226,7 @@ export default defineComponent({
   },
   data(): UsersManagerState {
     return {
-      isEditing: !!MatomoUrl.urlParsed.value.showadduser,
+      isEditing: false,
       isInviting: false,
       isCurrentUserSuperUser: true,
       users: [],
@@ -248,6 +248,11 @@ export default defineComponent({
   },
   created() {
     this.fetchUsers();
+
+    // the quick links widget links here with showadduser=1 to open the invite form
+    if (MatomoUrl.urlParsed.value.showadduser === '1') {
+      this.onAddNewUser();
+    }
   },
   watch: {
     limit() {
